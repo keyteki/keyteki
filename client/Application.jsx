124ed519a81cda79290248defa73900e2be6fc69
Stuart Walsh <stu@ipng.org.uk>
2016-11-09 21:32:33 +0000

    try (and fail) to fix scroll

--- client/Application.jsx
+++ client/Application.jsx
@@ -32,12 +32,21 @@ var leftMenu = [
 ];
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.onMessageSend = this.onMessageSend.bind(this);
+
+        this.state = {
+            lastPath: '/'
+        };
+    }
     componentWillMount() {
         this.props.fetchCards();
         this.props.fetchPacks();
 
         $(document).ajaxError((event, xhr) => {
-            if(xhr.status === 401) {
+            if (xhr.status === 401) {
                 this.props.navigate('/login');
             }
         });
@@ -66,7 +75,7 @@ class App extends React.Component {
             socket.on('leavegame', (game, player) => {
                 var isMe = false;
 
-                if(player.name === this.props.username) {
+                if (player.name === this.props.username) {
                     isMe = true;
                 }
 
@@ -87,6 +96,10 @@ class App extends React.Component {
         });
     }
 
+    onMessageSend(message) {
+        this.props.socket.emit('lobbymsg', message);
+    }
+
     render() {
         var rightMenu = this.props.loggedIn ? authedMenu : notAuthedMenu;
         var component = {};
@@ -102,7 +115,7 @@ class App extends React.Component {
 
         switch(path) {
             case '/':
-                component = <Lobby />;
+                component = <Lobby messages={this.props.messages} onMessageSend={this.onMessageSend} />;
                 break;
             case '/login':
                 component = <Login />;
@@ -148,6 +161,7 @@ App.propTypes = {
     fetchPacks: React.PropTypes.func,
     games: React.PropTypes.array,
     loggedIn: React.PropTypes.bool,
+    messages: React.PropTypes.array,
     navigate: React.PropTypes.func,
     packs: React.PropTypes.array,
     path: React.PropTypes.string,
@@ -159,6 +173,7 @@ App.propTypes = {
     receiveLobbyMessages: React.PropTypes.func,
     receiveNewGame: React.PropTypes.func,
     receiveUpdateGame: React.PropTypes.func,
+    socket: React.PropTypes.object,
     socketConnected: React.PropTypes.func,
     token: React.PropTypes.string,
     username: React.PropTypes.string
@@ -170,9 +185,11 @@ function mapStateToProps(state) {
         cards: state.cards.cards,
         currentGame: state.games.currentGame,
         games: state.games.games,
+        messages: state.chat.messages,
         packs: state.cards.packs,
         path: state.navigation.path,
         loggedIn: state.auth.loggedIn,
+        socket: state.socket.socket,
         token: state.auth.token,
         username: state.auth.username
     };
--- client/Lobby.jsx
+++ client/Lobby.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import _ from 'underscore';
 import $ from 'jquery';
 import moment from 'moment';
 
-import * as actions from './actions';
-
-class InnerLobby extends React.Component {
+class Lobby extends React.Component {
     constructor() {
         super();
 
@@ -20,7 +17,7 @@ class InnerLobby extends React.Component {
     }
 
     componentDidUpdate() {
-        $(this.refs.messages).scrollTop(999999);
+        this.refs.messages.scrollTop = 999999;
     }
 
     sendMessage() {
@@ -28,7 +25,9 @@ class InnerLobby extends React.Component {
             return;
         }
 
-        this.props.socket.emit('lobbychat', this.state.message);
+        if (this.props.onMessageSend) {
+            this.props.onMessageSend(this.state.message);
+        }    
 
         this.setState({ message: '' });
     }
@@ -52,17 +51,18 @@ class InnerLobby extends React.Component {
     }
 
     render() {
+        var index = 0;
         var messages = _.map(this.props.messages, message => {
             var timestamp = moment(message.time).format('MMM Do H:mm:ss');
             return (
-                <div key={timestamp + message.user.username}>
+                <div key={timestamp + message.user.username + index++}>
                     <img className='avatar pull-left' />
                     <span className='username'>{message.user.username}</span><span>{timestamp}</span>
                     <div className='message'>{message.message}</div>
                 </div>);
         });
         return (
-            <div ref='lala'>
+            <div>
                 <h1>Play A Game Of Thrones 2nd Edition</h1>
                 <div className='lobby-chat col-sm-10'>
                     <div className='panel lobby-messages' ref='messages'>
@@ -84,19 +84,10 @@ class InnerLobby extends React.Component {
     }
 }
 
-InnerLobby.displayName = 'Lobby';
-InnerLobby.propTypes = {
+Lobby.displayName = 'Lobby';
+Lobby.propTypes = {
     messages: React.PropTypes.array,
-    socket: React.PropTypes.object
+    onMessageSend: React.PropTypes.func
 };
-
-function mapStateToProps(state) {
-    return {
-        messages: state.chat.messages,
-        socket: state.socket.socket
-    };
-}
-
-const Lobby = connect(mapStateToProps, actions, null)(InnerLobby);
 
 export default Lobby;
