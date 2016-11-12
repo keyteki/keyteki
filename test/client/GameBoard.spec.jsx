e5a75ea1df8cf9fc4673b048270e23fc2e887f56
Stuart Walsh <stu@ipng.org.uk>
2016-11-12 08:19:44 +0000

    update tests

--- test/client/GameBoard.spec.jsx
+++ test/client/GameBoard.spec.jsx
@@ -19,39 +19,39 @@ const store = {
     }
 };
 
-describe('the <GameBoard /> component', function () {
+describe('the <GameBoard /> component', function() {
     var node, component;
 
     stubComponent(PlayerStats);
     stubComponent(InnerPlayerStats);
 
-    beforeEach(function () {
+    beforeEach(function() {
         node = document.createElement('div');
 
         component = ReactDOM.render(<InnerGameBoard />, node);
 
-        state.games.currentGame.players['1'] = { id: 1};
-        state.games.currentGame.players['2'] = { id: 2};
+        state.games.currentGame.players['1'] = { id: 1 };
+        state.games.currentGame.players['2'] = { id: 2 };
         state.socket.socket = {};
         state.socket.socket.id = '1';
         state.games.state = state.games.currentGame;
     });
 
-    describe('when initially rendered', function () {
+    describe('when initially rendered', function() {
         var divs = undefined;
 
-        beforeEach(function () {
+        beforeEach(function() {
             divs = TestUtils.scryRenderedDOMComponentsWithTag(component, 'div');
         });
 
-        it('should render a waiting div', function () {
+        it('should render a waiting div', function() {
             expect(divs[0].innerText).toBe('Waiting for server...');
         });
     });
 
-    describe('when player has cards in play', function () {
-        describe('that include locations followed by characters', function () {
-            beforeEach(function () {
+    describe('when player has cards in play', function() {
+        describe('that include locations followed by characters', function() {
+            beforeEach(function() {
                 state.games.currentGame.players['1'].cardsInPlay = [
                     { card: { code: '0001', type_code: 'location', label: 'Test Location' } },
                     { card: { code: '0002', type_code: 'character', label: 'Test Character' } }
@@ -61,7 +61,7 @@ describe('the <GameBoard /> component', function () {
                 component = TestUtils.findRenderedComponentWithType(component, GameBoard).getWrappedInstance();
             });
 
-            it('should render locations on the bottom and characters on the top', function () {
+            it('should render locations on the bottom and characters on the top', function() {
                 var rows = TestUtils.scryRenderedDOMComponentsWithClass(component, 'card-row');
 
                 // <div class='card-row'><div class='card-wrapper'><div class='card-frame'><div class='card'><div><img></img></div></div></div></div></div>
@@ -70,8 +70,8 @@ describe('the <GameBoard /> component', function () {
             });
         });
 
-        describe('that include characters followed by locations', function () {
-            beforeEach(function () {
+        describe('that include characters followed by locations', function() {
+            beforeEach(function() {
                 state.games.currentGame.players['1'].cardsInPlay = [
                     { card: { code: '0002', type_code: 'character', label: 'Test Character' } },
                     { card: { code: '0001', type_code: 'location', label: 'Test Location' } }
@@ -81,7 +81,7 @@ describe('the <GameBoard /> component', function () {
                 component = TestUtils.findRenderedComponentWithType(component, GameBoard).getWrappedInstance();
             });
 
-            it('should render locations on the bottom and characters on the top', function () {
+            it('should render locations on the bottom and characters on the top', function() {
                 var rows = TestUtils.scryRenderedDOMComponentsWithClass(component, 'card-row');
 
                 // <div class='card-row'><div class='card-wrapper'><div class='card-frame'><div class='card'><div><img></img></div></div></div></div></div>
@@ -90,8 +90,8 @@ describe('the <GameBoard /> component', function () {
             });
         });
 
-        describe('that include characters mixed with locations', function () {
-            beforeEach(function () {
+        describe('that include characters mixed with locations', function() {
+            beforeEach(function() {
                 state.games.currentGame.players['1'].cardsInPlay = [
                     { card: { code: '0002', type_code: 'character', label: 'Test Character' } },
                     { card: { code: '0001', type_code: 'location', label: 'Test Location' } },
@@ -102,7 +102,7 @@ describe('the <GameBoard /> component', function () {
                 component = TestUtils.findRenderedComponentWithType(component, GameBoard).getWrappedInstance();
             });
 
-            it('should render locations on the bottom and characters on the top', function () {
+            it('should render locations on the bottom and characters on the top', function() {
                 var rows = TestUtils.scryRenderedDOMComponentsWithClass(component, 'card-row');
 
                 // <div class='card-row'><div class='card-wrapper'><div class='card-frame'><div class='card'><div><img></img></div></div></div></div></div>
@@ -113,9 +113,9 @@ describe('the <GameBoard /> component', function () {
         });
     });
 
-    describe('when other player has cards in play', function () {
-        describe('that include locations followed by characters', function () {
-            beforeEach(function () {
+    describe('when other player has cards in play', function() {
+        describe('that include locations followed by characters', function() {
+            beforeEach(function() {
                 state.games.currentGame.players['1'].cardsInPlay = [
                     { card: { code: '0001', type_code: 'location', label: 'Test Location' } },
                     { card: { code: '0002', type_code: 'character', label: 'Test Character' } }
@@ -129,7 +129,7 @@ describe('the <GameBoard /> component', function () {
                 component = TestUtils.findRenderedComponentWithType(component, GameBoard).getWrappedInstance();
             });
 
-            it('should render locations on the top and characters on the bottom', function () {
+            it('should render locations on the top and characters on the bottom', function() {
                 var rows = TestUtils.scryRenderedDOMComponentsWithClass(component, 'card-row');
 
                 // <div class='card-row'><div class='card-wrapper'><div class='card-frame'><div class='card'><div><img></img></div></div></div></div></div>
@@ -138,8 +138,8 @@ describe('the <GameBoard /> component', function () {
             });
         });
 
-        describe('that include characters followed by locations', function () {
-            beforeEach(function () {
+        describe('that include characters followed by locations', function() {
+            beforeEach(function() {
                 state.games.currentGame.players['1'].cardsInPlay = [
                     { card: { code: '0002', type_code: 'character', label: 'Test Character' } },
                     { card: { code: '0001', type_code: 'location', label: 'Test Location' } }
@@ -153,7 +153,7 @@ describe('the <GameBoard /> component', function () {
                 component = TestUtils.findRenderedComponentWithType(component, GameBoard).getWrappedInstance();
             });
 
-            it('should render locations on the bottom and characters on the top', function () {
+            it('should render locations on the top and characters on the bottom', function() {
                 var rows = TestUtils.scryRenderedDOMComponentsWithClass(component, 'card-row');
 
                 // <div class='card-row'><div class='card-wrapper'><div class='card-frame'><div class='card'><div><img></img></div></div></div></div></div>
@@ -162,8 +162,8 @@ describe('the <GameBoard /> component', function () {
             });
         });
 
-        describe('that include characters mixed with locations', function () {
-            beforeEach(function () {
+        describe('that include characters mixed with locations', function() {
+            beforeEach(function() {
                 state.games.currentGame.players['1'].cardsInPlay = [
                     { card: { code: '0002', type_code: 'character', label: 'Test Character' } },
                     { card: { code: '0001', type_code: 'location', label: 'Test Location' } },
@@ -179,13 +179,38 @@ describe('the <GameBoard /> component', function () {
                 component = TestUtils.findRenderedComponentWithType(component, GameBoard).getWrappedInstance();
             });
 
-            it('should render locations on the bottom and characters on the top', function () {
+            it('should render locations on the top and characters on the bottom', function() {
                 var rows = TestUtils.scryRenderedDOMComponentsWithClass(component, 'card-row');
 
                 // <div class='card-row'><div class='card-wrapper'><div class='card-frame'><div class='card'><div><img></img></div></div></div></div></div>
                 expect(rows[0].children[0].children[0].children[0].children[0].children[0].src.indexOf('0001')).not.toBe(-1);
                 expect(rows[1].children[1].children[0].children[0].children[0].children[0].src.indexOf('0002')).not.toBe(-1);
                 expect(rows[1].children[0].children[0].children[0].children[0].children[0].src.indexOf('0003')).not.toBe(-1);
+            });
+        });
+
+        describe('when the control is re-rendered', function() {
+            beforeEach(function() {
+                state.games.currentGame.players['1'].cardsInPlay = [
+                    { card: { code: '0001', type_code: 'location', label: 'Test Location' } },
+                    { card: { code: '0002', type_code: 'character', label: 'Test Character' } }
+                ];
+                state.games.currentGame.players['2'].cardsInPlay = [
+                    { card: { code: '0001', type_code: 'location', label: 'Test Location' } },
+                    { card: { code: '0002', type_code: 'character', label: 'Test Character' } }
+                ];
+
+                component = ReactDOM.render(<Provider store={store}><GameBoard /></Provider>, node);
+                component = ReactDOM.render(<Provider store={store}><GameBoard /></Provider>, node);
+                component = TestUtils.findRenderedComponentWithType(component, GameBoard).getWrappedInstance();
+            });
+
+            it('should still render the locations on top and characters on bottom', function() {
+                var rows = TestUtils.scryRenderedDOMComponentsWithClass(component, 'card-row');
+
+                // <div class='card-row'><div class='card-wrapper'><div class='card-frame'><div class='card'><div><img></img></div></div></div></div></div>
+                expect(rows[0].children[0].children[0].children[0].children[0].children[0].src.indexOf('0001')).not.toBe(-1);
+                expect(rows[1].children[0].children[0].children[0].children[0].children[0].src.indexOf('0002')).not.toBe(-1);
             });
         });
     });
