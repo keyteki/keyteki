541c1530ba80bf08997c039a93d3fc4034585615
Stuart Walsh <stu@ipng.org.uk>
2016-11-22 18:34:02 +0000

    implement fortified position

---
+++ server/game/cards/plots/01/fortifiedposition.js
@@ -0,0 +1,31 @@
+const _ = require('underscore');
+
+const PlotCard = require('../../../plotcard.js');
+
+class CountingCoppers extends PlotCard {
+    flipFaceup() {
+        super.flipFaceup();
+
+        _.each(this.game.getPlayers(), player => {
+            player.cardsInPlay.each(card => {
+                if(card.getType() === 'character') {
+                    card.setBlank();
+                }
+            });
+        });
+    }
+
+    leavesPlay() {   
+        _.each(this.game.getPlayers(), player => {
+            player.cardsInPlay.each(card => {
+                if(card.getType() === 'character') {
+                    card.clearBlank();
+                }
+            });
+        });
+    }
+}
+
+CountingCoppers.code = '01012';
+
+module.exports = CountingCoppers;
