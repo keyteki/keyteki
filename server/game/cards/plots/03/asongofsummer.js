6b25dc83c1c5313938b7ee054f490262bd8a6893
Stuart Walsh <stu@ipng.org.uk>
2016-11-24 18:26:38 +0000

    fix a song of summer

--- server/game/cards/plots/03/asongofsummer.js
+++ server/game/cards/plots/03/asongofsummer.js
@@ -3,16 +3,18 @@ const _ = require('underscore');
 const PlotCard = require('../../../plotcard.js');
 
 class ASongOfSummer extends PlotCard {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+
+        this.registerEvents(['onCardPlayed']);
+    }
+
     flipFaceup() {
         super.flipFaceup();
 
         var otherPlayer = this.game.getOtherPlayer(this.owner);
 
-        if(!otherPlayer) {
-            return;
-        }
-
-        if(!otherPlayer.selectedPlot.hasTrait('Winter')) {
+        if(!otherPlayer || !otherPlayer.selectedPlot.hasTrait('Winter')) {
             this.owner.cardsInPlay.each(card => {
                 if(card.getType() === 'character') {
                     card.strengthModifier++;
@@ -21,17 +23,28 @@ class ASongOfSummer extends PlotCard {
         }
     }
 
-    leavesPlay() {
-        var otherPlayer = this.game.getOtherPlayer(this.owner);
-
-        if(!otherPlayer) {
+    onCardPlayed(player, cardId) {
+        if(!this.inPlay || this.owner !== player) {
             return;
         }
 
-        if(otherPlayer.activePlot && !otherPlayer.activePlot.hasTrait('Winter')) {
+        var card = player.findCardInPlayByUuid(cardId);
+        if(!card) {
+            return;
+        }
+
+        if(card.getType() === 'character') {
+            card.strengthModifier++;
+        }
+    }
+
+    leavesPlay() {
+        var otherPlayer = this.game.getOtherPlayer(this.owner);
+
+        if(!otherPlayer || (otherPlayer.activePlot && !otherPlayer.activePlot.hasTrait('Winter'))) {
             this.owner.cardsInPlay.each(card => {
                 if(card.getType() === 'character') {
-                    card.strengthModifier;
+                    card.strengthModifier--;
                 }
             });
         }
