b9c18249c4732f673a5268cadcac69a5c28c5a76
Stuart Walsh <stu@ipng.org.uk>
2016-11-30 13:29:59 +0000

    missed an icon changing attachment

---
+++ server/game/cards/attachments/01/syriostraining.js
@@ -0,0 +1,15 @@
+const DrawCard = require('../../../drawcard.js');
+ 
+class SyriosTraining extends DrawCard {
+    attach(player, card) {
+        card.setIcon('military');
+    }
+
+    leavesPlay() {
+        this.parent.setIcon('military');
+    }
+}
+
+SyriosTraining.code = '01037';
+
+module.exports = SyriosTraining;
