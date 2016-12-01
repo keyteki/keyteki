d9387bfe8f68ab37bb059ac67a8a9c82d50ee0cf
Stuart Walsh <stu@ipng.org.uk>
2016-12-01 14:29:54 +0000

    handle null strengths

--- server/game/drawcard.js
+++ server/game/drawcard.js
@@ -89,10 +89,10 @@ class DrawCard extends BaseCard {
 
     getStrength() {
         if(this.owner.phase === 'setup') {
-            return this.cardData.strength;
+            return this.cardData.strength || 0;
         }
 
-        return this.strengthModifier + this.cardData.strength;
+        return this.strengthModifier + this.cardData.strength || 0;
     }
 
     setRenown() {
