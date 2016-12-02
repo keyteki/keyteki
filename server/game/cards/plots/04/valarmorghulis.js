f45a026aba91fa843bc12fda5dbcde1007cd7579
Brian Upton <ystros@gmail.com>
2016-12-02 10:03:23 -0800

    Implement Valar Morghulis (#100)

---
+++ server/game/cards/plots/04/valarmorghulis.js
@@ -0,0 +1,29 @@
+const _ = require('underscore');
+
+const PlotCard = require('../../../plotcard.js');
+
+class ValarMorghulis extends PlotCard {
+    onReveal(player) {
+        if(!this.inPlay || this.owner !== player) {
+            return true;
+        }
+
+        _.each(this.game.getPlayersInFirstPlayerOrder(), player => {
+            this.killAllCharacters(player);
+        });
+
+        return true;
+    }
+
+    killAllCharacters(player) {
+        var characters = player.cardsInPlay.filter(card => card.getType() === 'character');
+
+        _.each(characters, character => {
+            player.killCharacter(character);
+        });
+    }
+}
+
+ValarMorghulis.code = '04080';
+
+module.exports = ValarMorghulis;
