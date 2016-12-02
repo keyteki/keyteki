091dcf98256fb2940b9f27bc7c4a1dd1ec28bdcf
Stuart Walsh <stu@ipng.org.uk>
2016-12-02 14:51:11 +0000

    fix navbar styling on mobile

--- client/NavBar.jsx
+++ client/NavBar.jsx
@@ -38,24 +38,24 @@ class InnerNavBar extends React.Component {
             <nav className='navbar navbar-inverse navbar-fixed-top'>
                 <div className='container-fluid'>
                     <div className='navbar-header'>
-                        <button className='navbar-toggle collapsed' type='button' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar' />
-                        <span className='sr-only' />
-                        <span className='sr-only'>Toggle Navigation</span>
-                        <span className='icon-bar' />
-                        <span className='icon-bar' />
-                        <span className='icon-bar' />
+                        <button className='navbar-toggle collapsed' type='button' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>
+                            <span className='sr-only'>Toggle Navigation</span>
+                            <span className='icon-bar' />
+                            <span className='icon-bar' />
+                            <span className='icon-bar' />
+                        </button>
+                        <Link href='/' className='navbar-brand'>{this.props.title}</Link>
                     </div>
-                </div>
-                <Link href='/' className='navbar-brand'>{this.props.title}</Link>
-                <div id='navbar' className='collapse navbar-collapse'>
-                    <ul className='nav navbar-nav'>
-                        {leftMenuToRender}
-                    </ul>
-                    <ul className='nav navbar-nav navbar-right'>
-                        {contextMenu}
-                        {numGames}
-                        {rightMenuToRender}
-                    </ul>
+                    <div id='navbar' className='collapse navbar-collapse'>
+                        <ul className='nav navbar-nav'>
+                            {leftMenuToRender}
+                        </ul>
+                        <ul className='nav navbar-nav navbar-right'>
+                            {contextMenu}
+                            {numGames}
+                            {rightMenuToRender}
+                        </ul>
+                    </div>
                 </div>
             </nav>);
     }
