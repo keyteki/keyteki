55065d65c05df4d1f373581cda8b1638f3d901f5
Stuart Walsh <stu@ipng.org.uk>
2016-12-10 20:21:23 +0000

    finish littlefinger

--- server/game/cards/characters/01/littlefinger.js
+++ server/game/cards/characters/01/littlefinger.js
@@ -1,6 +1,46 @@
 const DrawCard = require('../../../drawcard.js');
  
 class LittleFinger extends DrawCard {
+    play(player) {
+        super.play(player);
+
+        if(player.phase !== 'marshal') {
+            return;
+        }
+
+        this.game.promptWithMenu(player, this, {
+            activePrompt: {
+                menuTitle: 'Trigger ' + this.name + '?',
+                buttons: [
+                    { text: 'Draw 2 Cards', command: 'menuButton', method: 'trigger' },
+                    { text: 'Cancel', command: 'menuButton', method: 'cancel' }
+                ]
+            },
+            waitingPromptTitle: 'Waiting for opponent to use ' + this.name
+        });
+    }
+
+    trigger(player) {
+        if(!this.inPlay || this.isBlank() || this.controller !== player) {
+            return false;
+        }
+
+        player.drawCardsToHand(2);
+
+        this.game.addMessage('{0} uses {1} to draw 2 cards', player, this);
+
+        return true;
+    }
+
+    cancel(player) {
+        if(!this.inPlay || this.isBlank() || this.controller !== player) {
+            return false;
+        }
+
+        this.game.addMessage('{0} declines to trigger {1}', player, this);
+        return true;
+    }
+
     getIncome() {
         return 1;
     }
