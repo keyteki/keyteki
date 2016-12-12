2d103783652a714d2e5a4087824104c4847efc67
Stuart Walsh <stu@ipng.org.uk>
2016-12-12 13:50:07 +0000

    implement put to the torch

---
+++ server/game/cards/events/01/puttothetorch.js
@@ -0,0 +1,43 @@
+const DrawCard = require('../../../drawcard.js');
+
+class PutToTheTorch extends DrawCard {
+    canPlay(player, card) {
+        if(player !== this.controller || this !== card) {
+            return false;
+        }
+
+        var currentChallenge = this.game.currentChallenge;
+
+        if(!currentChallenge || currentChallenge.winner !== this.controller || currentChallenge.attacker !== this.controller || currentChallenge.strengthDifference < 5 ||
+                currentChallenge.challengeType !== 'military') {
+            return false;
+        }
+
+        return true;
+    }
+
+    play(player) {
+        if(this.controller !== player) {
+            return;
+        }
+
+        this.game.promptForSelect(player, {
+            activePromptTitle: 'Select a location to discard',
+            waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
+            cardCondition: card => card.inPlay && card.controller !== player && card.getType() === 'location',
+            onSelect: (p, card) => this.onCardSelected(p, card)
+        });
+    }
+
+    onCardSelected(player, card) {
+        card.controller.moveCard(card, 'discard pile');
+
+        this.game.addMessage('{0} uses {1} to discard {2}', player, this, card);
+
+        return true;
+    }
+}
+
+PutToTheTorch.code = '01042';
+
+module.exports = PutToTheTorch;
