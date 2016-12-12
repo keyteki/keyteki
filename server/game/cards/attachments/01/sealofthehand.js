1613af5277c013694a061617f3034e5bf688d3ee
Stuart Walsh <stu@ipng.org.uk>
2016-12-12 13:07:21 +0000

    implement seal of the hand

---
+++ server/game/cards/attachments/01/sealofthehand.js
@@ -0,0 +1,32 @@
+const DrawCard = require('../../../drawcard.js');
+ 
+class SealOfTheHand extends DrawCard {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+
+        this.menu.push({ text: 'Kneel', command: 'card', method: 'kneel' });
+    }
+
+    kneel(player) {
+        if(!this.inPlay || !this.parent || !this.parent.kneeled) {
+            return;
+        }
+
+        this.parent.kneeled = false;
+        this.kneeled = true;
+
+        this.game.addMessage('{0} kneels {1} to stand {2}', player, this, this.parent);
+    }
+
+    canAttach(player, card) {
+        if(!card.hasTrait('Lady') && !card.hasTrait('Lord')) {
+            return false;
+        }
+
+        return super.canAttach(player, card);
+    }
+}
+
+SealOfTheHand.code = '01032';
+
+module.exports = SealOfTheHand;
