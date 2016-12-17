cb57e718c0f91be9f983e15ad61b69e7ce22c846
Stuart Walsh <stu@ipng.org.uk>
2016-12-17 10:03:11 +0000

    Add sentry.io stuff

--- client/index.prod.jsx
+++ client/index.prod.jsx
@@ -1,4 +1,4 @@
-/*global user, authToken */
+/*global user, authToken, Raven */
 import React from 'react';
 import {render} from 'react-dom';
 import Application from './Application.jsx';
@@ -6,6 +6,8 @@ import {Provider} from 'react-redux';
 import configureStore from './configureStore';
 import {navigate, login} from './actions';
 import 'bootstrap/dist/js/bootstrap';
+
+Raven.config('https://cd45fd5119a4404eab0e23ab65cc3f86@sentry.io/123019', { release: 'dev'}).install();
 
 const store = configureStore();
 
