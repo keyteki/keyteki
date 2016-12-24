cee57a4594a3ecd054528d8afd053f2a8d046f43
Stuart Walsh <stu@ipng.org.uk>
2016-12-24 14:40:58 +0000

    fix red cloaks to be once per phase instead of once per round

--- server/game/cards/characters/02/redcloaks.js
+++ server/game/cards/characters/02/redcloaks.js
@@ -4,16 +4,16 @@ class RedCloaks extends DrawCard {
     constructor(owner, cardData) {
         super(owner, cardData);
 
-        this.registerEvents(['onAttackersDeclared', 'onAfterTaxation']);
+        this.registerEvents(['onAttackersDeclared', 'onPhaseEnd']);
 
         this.menu.push({ text: 'Move 1 gold from your gold pool to this card', command: 'card', method: 'addGold' });
 
         this.tokens['gold'] = 0;
-        this.usedThisRound = false;
+        this.usedThisPhase = false;
     }
 
     addGold(player) {
-        if(!this.inPlay || this.controller !== player || this.usedThisRound || player.gold <= 0) {
+        if(!this.inPlay || this.controller !== player || this.usedThisPhase || player.gold <= 0) {
             return;
         }
 
@@ -22,12 +22,11 @@ class RedCloaks extends DrawCard {
 
         this.game.addMessage('{0} moves 1 gold from their gold pool to {1}', this.controller, this);
 
-        this.usedThisRound = true;
+        this.usedThisPhase = true;
     }
 
-    onAfterTaxation() {
-        // XXX this should be per phase, will change this when the new phase stuff is done and have a phase change event
-        this.usedThisRound = false;
+    onPhaseEnd() {
+        this.usedThisPhase = false;
     }
 
     onAttackersDeclared(e, challenge) {
