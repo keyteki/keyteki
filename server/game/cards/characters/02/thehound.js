bb5d6f7d89c4720e73be9854a8259f2b550b4805
Stuart Walsh <stu@ipng.org.uk>
2016-12-24 16:04:14 +0000

    implement the hound

---
+++ server/game/cards/characters/02/thehound.js
@@ -0,0 +1,50 @@
+const DrawCard = require('../../../drawcard.js');
+
+class TheHound extends DrawCard {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+
+        this.registerEvents(['afterChallenge']);
+    }
+
+    afterChallenge(event, challenge) {
+        if(!this.inPlay || challenge.winner !== this.controller) {
+            return;
+        }
+
+        if(!challenge.isParticipating(this)) {
+            return;
+        }
+
+        this.game.promptWithMenu(this.controller, this, {
+            activePrompt: {
+                menuTitle: 'Discard 1 card at random for ' + this.name + '?',
+                buttons: [
+                    { text: 'Yes', method: 'discardCard' },
+                    { text: 'No', method: 'returnToHand' }
+                ]
+            },
+            waitingPromptTitle: 'Waiting for opponent to use ' + this.name
+        });
+    }
+
+    discardCard(player) {
+        this.game.addMessage('{0} is forced to discard a card to keep {1} in play', player, this);
+
+        player.discardAtRandom(1);
+
+        return true;
+    }
+
+    returnToHand(player) {
+        this.game.addMessage('{0} is forced to return {1} to their hand', player, this);
+
+        player.moveCard(this, 'hand');
+
+        return true;
+    }
+}
+
+TheHound.code = '02009';
+
+module.exports = TheHound;
