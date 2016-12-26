92e640589fa6a0a0c549716e64f28746c673605b
Stuart Walsh <stu@ipng.org.uk>
2016-12-26 21:12:20 +0000

    implement tyrion lannister

---
+++ server/game/cards/characters/01/tyrionlannister.js
@@ -0,0 +1,50 @@
+const DrawCard = require('../../../drawcard.js');
+
+class TyrionLannister extends DrawCard {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+
+        this.registerEvents(['onChallenge', 'onBeginRound']);
+    }
+
+    onBeginRound() {
+        this.abilityUsed = 0;
+    }
+
+    onChallenge(event, challenge) {
+        if(!this.inPlay || challenge.challengeType !== 'intrigue' || this.abilityUsed >= 2) {
+            return;
+        }
+
+        this.game.promptWithMenu(this.controller, this, {
+            activePrompt: {
+                menuTitle: 'Trigger ' + this.name + '?',
+                buttons: [
+                    { text: 'Yes', method: 'gainGold' },
+                    { text: 'No', method: 'cancel' }
+                ]
+            },
+            waitingPromptTitle: 'Waiting for opponent to use ' + this.name
+        });
+    }
+
+    gainGold(player) {
+        player.gold += 2;
+
+        this.game.addMessage('{0} uses {1} to gain 2 gold as an intrigue challenge has been declared', player, this);
+
+        this.abilityUsed++;
+
+        return true;
+    }
+
+    cancel(player) {
+        this.game.addMessage('{0} declines to trigger {1}', player, this);
+
+        return true;
+    }
+}
+
+TyrionLannister.code = '01089';
+
+module.exports = TyrionLannister;
