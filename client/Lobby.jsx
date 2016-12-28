0988393b0194484f4df2987e24be47cbb2182679
Stuart Walsh <stu@ipng.org.uk>
2016-12-28 09:59:55 +0000

    update news

--- client/Lobby.jsx
+++ client/Lobby.jsx
@@ -80,9 +80,9 @@ class InnerLobby extends React.Component {
         return (
             <div>
                 <div className='alert alert-info'>
+                    <div><span className='icon-military' />New cards: The Tickler, Tyrion Lannister, Widow's Wail, Cersei's Wheelhouse, Street of the Sisters, Jon Snow.  Fix Great Kraken, Seen in Flame</div>                
                     <div><span className='icon-power' />New cards: Old Nan, Bitterbridge Encampment, Cersei Lannister(LoCR), Gold Cloaks, Shae, The Hound. Fix Ice and Lord of the Crossing</div>
-                    <div><span className='icon-intrigue' />New card: Ice.  (Hopefully) Fixed a bug causing players not to be able to leave games. Fixed a bug allowing you to kneel opponents cards.</div>
-                    <div><span className='icon-military' />New cards: Close call.  Fixed reconnecting to games when the connection is lost.  You can now also refresh and stay in a game.</div>
+                    <div><span className='icon-intrigue' />New card: Ice.  (Hopefully) Fixed a bug causing players not to be able to leave games. Fixed a bug allowing you to kneel opponents cards.</div>                    
                 </div>
                 <div className='row'>
                     <span className='col-sm-9 text-center'><h1>Play A Game Of Thrones 2nd Edition</h1></span>
