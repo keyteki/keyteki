078a09d6edc4df8d4af972aac52722113c4a226f
Stuart Walsh <stu@ipng.org.uk>
2017-01-03 13:32:24 +0000

    implement victarion greyjoy

---
+++ server/game/cards/characters/05/victariongreyjoy.js
@@ -0,0 +1,48 @@
+const DrawCard = require('../../../drawcard.js');
+ 
+class VictarionGreyjoy extends DrawCard {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+
+        this.registerEvents(['onKillingCharacter']);
+    }
+
+    onKillingCharacter(event, player, card, allowSave) {
+        if(!allowSave || player !== this.controller || card !== this || this.power < 2) {
+            return;
+        }
+
+        this.game.promptWithMenu(player, this, {
+            activePrompt: {
+                menuTitle: 'Save ' + this.name + '?',
+                buttons: [
+                    { text: 'Yes', method: 'save' },
+                    { text: 'No', method: 'cancel' }
+                ]
+            },
+            waitingPromptTitle: 'Waiting for opponent to use save effects'
+        });
+
+        event.cancel = true;
+    }
+
+    save(player) {
+        this.power -= 2;
+
+        this.game.addMessage('{0} uses {1} to save {2}', player, this, this);
+
+        return true;
+    }
+
+    cancel(player) {
+        player.killCharacter(this);
+
+        this.game.addMessage('{0} declines to trigger {1}', player, this);
+
+        return true;
+    }
+}
+
+VictarionGreyjoy.code = '05027';
+
+module.exports = VictarionGreyjoy;
