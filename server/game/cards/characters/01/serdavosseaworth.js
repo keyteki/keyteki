5050365592b5e343cd9af46f13f8404719bd0aa4
Stuart Walsh <stu@ipng.org.uk>
2017-01-03 12:05:34 +0000

    implement ser davos seaworth

---
+++ server/game/cards/characters/01/serdavosseaworth.js
@@ -0,0 +1,44 @@
+const DrawCard = require('../../../drawcard.js');
+ 
+class SerDavosSeaworth extends DrawCard {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+
+        this.registerEvents(['onCharacterKilled']);
+    }
+
+    onCharacterKilled(event, player, card) {
+        if(player !== this.controller || card !== this) {
+            return;
+        }
+
+        this.game.promptWithMenu(player, this, {
+            activePrompt: {
+                menuTitle: 'Trigger ' + this.name + '?',
+                buttons: [
+                    { text: 'Yes', method: 'returnToHand' },
+                    { text: 'No', method: 'cancel' }
+                ]
+            },
+            waitingPromptTitle: 'Waiting for opponent to use ' + this.name
+        });
+    }
+
+    returnToHand(player) {
+        this.game.addMessage('{0} uses {1} to return {1} to their hand instead of their dead pile', player, this, this);
+
+        player.moveCard(this, 'hand');
+
+        return true;
+    }
+
+    cancel(player) {
+        this.game.addMessage('{0} declines to trigger {1}', player, this);
+
+        return true;
+    }
+}
+
+SerDavosSeaworth.code = '01050';
+
+module.exports = SerDavosSeaworth;
