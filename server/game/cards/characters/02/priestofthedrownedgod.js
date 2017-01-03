89315f3cb340fe7ceefbb70a78714a7dfebb72c2
Stuart Walsh <stu@ipng.org.uk>
2017-01-03 13:16:47 +0000

    implement priest of the drowned god

---
+++ server/game/cards/characters/02/priestofthedrownedgod.js
@@ -0,0 +1,43 @@
+const DrawCard = require('../../../drawcard.js');
+
+class PriestOfTheDrownedGod extends DrawCard {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+
+        this.registerEvents(['onCardPlayed']);
+    }
+
+    play(player) {
+        super.play(player);
+
+        player.cardsInPlay.each(card => {
+            if(card.getType() === 'character' && card.hasTrait('Drowned God')) {
+                card.strengthModifier++;
+            }
+        });
+    }
+
+    onCardPlayed(e, player, card) {
+        if(this.controller !== player) {
+            return;
+        }
+
+        if(card.getType() === 'character' && card.hasTrait('Drowned God')) {
+            card.strengthModifier++;
+        }
+    }
+
+    leavesPlay() {
+        super.leavesPlay();
+
+        this.controller.cardsInPlay.each(card => {
+            if(card.getType() === 'character' && card.hasTrait('Drowned God')) {
+                card.strengthModifier--;
+            }
+        });
+    }
+}
+
+PriestOfTheDrownedGod.code = '02072';
+
+module.exports = PriestOfTheDrownedGod;
