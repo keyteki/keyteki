bb0b8e87d237a768e0068ff2c78aaa2e46061a67
Stuart Walsh <stu@ipng.org.uk>
2017-01-03 14:20:03 +0000

    implement raiding longship

---
+++ server/game/cards/locations/02/raidinglongship.js
@@ -0,0 +1,41 @@
+const DrawCard = require('../../../drawcard.js');
+ 
+class RaidingLongship extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Kneel this card',
+            method: 'kneel'
+        });
+    }    
+
+    kneel(player) {
+        if(this.location !== 'play area' || !this.game.currentChallenge || !player.firstPlayer) {
+            return false;
+        }
+
+        this.game.promptForSelect(player, {
+            cardCondition: card => this.cardCondition(card),
+            activePromptTitle: 'Select character to remove from challenge',
+            waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
+            onSelect: (player, card) => this.onCardSelected(player, card)
+        });
+    }
+
+    cardCondition(card) {
+        return card.getType() === 'character' && card.location === 'play area' && this.game.currentChallenge.isDefending(card) && card.attachments.size() === 0;
+    }
+
+    onCardSelected(player, card) {
+        this.kneeled = true;
+
+        this.game.currentChallenge.removeFromChallenge(card);
+
+        this.game.addMessage('{0} kneels {1} to remove {2}\'s STR from the challenge', player, this, card);
+
+        return true;
+    }
+}
+
+RaidingLongship.code = '02032';
+
+module.exports = RaidingLongship;
