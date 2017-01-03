a814f8c74667f03a3101c1cda581b24747af868f
Stuart Walsh <stu@ipng.org.uk>
2017-01-03 13:29:45 +0000

    implement theon greyjoy

---
+++ server/game/cards/characters/01/theongreyjoy.js
@@ -0,0 +1,44 @@
+const DrawCard = require('../../../drawcard.js');
+
+class TheonGreyjoy extends DrawCard {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+
+        this.registerEvents(['onUnopposedWin']);
+    }
+
+    onUnopposedWin(event, challenge) {
+        var winner = challenge.winner;
+        if(this.isBlank() || this.controller !== winner || !challenge.isParticipating(this)) {
+            return;
+        }
+
+        this.game.promptWithMenu(winner, this, {
+            activePrompt: {
+                menuTitle: 'Trigger ' + this.name + '?',
+                buttons: [
+                    { text: 'Gain 1 power', method: 'gainPower' },
+                    { text: 'No', method: 'cancel' }
+                ]
+            },
+            waitingPromptTitle: 'Waiting for opponent to perform reactions'
+        });
+    }
+
+    gainPower(player) {
+        this.power++;
+        this.game.addMessage('{0} gains 1 power on {1} after winning an unopposed challenge', player, this);
+
+        return true;
+    }
+
+    cancel(player) {
+        this.game.addMessage('{0} declines to trigger {1}', player, this);
+
+        return true;
+    }
+}
+
+TheonGreyjoy.code = '01071';
+
+module.exports = TheonGreyjoy;
