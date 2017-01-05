561c2ff3bf0b2c548c5a16fdcb3cfa8d3b6900e9
Stuart Walsh <stu@ipng.org.uk>
2017-01-05 15:40:11 +0000

    implement highgarden

--- server/game/cards/locations/01/highgarden.js
+++ server/game/cards/locations/01/highgarden.js
@@ -1,6 +1,6 @@
 const DrawCard = require('../../../drawcard.js');
  
-class RaidingLongship extends DrawCard {
+class Highgarden extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Kneel this card',
@@ -9,33 +9,35 @@ class RaidingLongship extends DrawCard {
     }    
 
     kneel(player) {
-        if(this.kneeled || this.location !== 'play area' || !this.game.currentChallenge || !player.firstPlayer) {
+        if(this.kneeled || this.controller.gold < 1 || this.location !== 'play area' || !this.game.currentChallenge) {
             return false;
         }
 
         this.game.promptForSelect(player, {
             cardCondition: card => this.cardCondition(card),
-            activePromptTitle: 'Select character to remove from challenge',
+            activePromptTitle: 'Select character',
             waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
             onSelect: (player, card) => this.onCardSelected(player, card)
         });
     }
 
     cardCondition(card) {
-        return card.getType() === 'character' && card.location === 'play area' && this.game.currentChallenge.isDefending(card) && card.attachments.size() === 0;
+        return card.getType() === 'character' && card.location === 'play area' && this.game.currentChallenge.isAttacking(card);
     }
 
     onCardSelected(player, card) {
         player.kneelCard(this);
+        this.controller.gold -= 1;
 
         this.game.currentChallenge.removeFromChallenge(card);
+        card.controller.standCard(card);
 
-        this.game.addMessage('{0} kneels {1} to remove {2}\'s STR from the challenge', player, this, card);
+        this.game.addMessage('{0} kneels {1} and pays 1 gold to stand {2} and remove it from the challenge', player, this, card);
 
         return true;
     }
 }
 
-RaidingLongship.code = '02032';
+Highgarden.code = '01192';
 
-module.exports = RaidingLongship;
+module.exports = Highgarden;
