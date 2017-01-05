e72a74f2a0feeddcd22006743a68d594b2f75b65
Stuart Walsh <stu@ipng.org.uk>
2017-01-05 10:16:51 +0000

    implement magister illyrio

---
+++ server/game/cards/characters/01/magisterillyrio.js
@@ -0,0 +1,39 @@
+const DrawCard = require('../../../drawcard.js');
+
+class MagisterIllyrio extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Pay 2 gold to stand a character',
+            method: 'stand',
+            limit: { amount: 1, period: 'phase' }
+        });
+    }
+
+    stand(player) {
+        if(this.controller !== player || player.gold < 2) {
+            return;
+        }
+
+        this.game.promptForSelect(player, {
+            activePromptTitle: 'Select a character to stand',
+            waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
+            cardCondition: card => card.location === 'play area' && card.getType() === 'character',
+            onSelect: (p, card) => this.onStandSelected(p, card)
+        });        
+
+        return true;
+    }
+
+    onStandSelected(player, card) {
+        this.game.addMessage('{0} uses {1} to pay 2 gold and stand {2}', player, this, card);
+
+        player.standCard(card);
+        player.gold -= 2;
+
+        return true;
+    }
+}
+
+MagisterIllyrio.code = '01163';
+
+module.exports = MagisterIllyrio;
