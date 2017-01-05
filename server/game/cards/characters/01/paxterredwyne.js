d57b422afc0a86fd607113e4d932b4d746a59e28
Stuart Walsh <stu@ipng.org.uk>
2017-01-05 14:50:07 +0000

    implement paxter redwyne

--- server/game/cards/characters/01/paxterredwyne.js
+++ server/game/cards/characters/01/paxterredwyne.js
@@ -1,8 +1,48 @@
 const DrawCard = require('../../../drawcard.js');
 
 class PaxterRedwyne extends DrawCard {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+
+        this.registerEvents(['onBeginRound']);
+    }
+
+    onBeginRound() {
+        this.abilityUsed = false;
+    }
+
+    canReduce(player, card) {
+        if(this.controller !== player || this.abilityUsed || card.getType() !== 'event') {
+            return false;
+        }
+
+        return true;
+    }
+
+    reduce(card, currentCost, spending) {
+        if(!this.abilityUsed) {
+            var cost = currentCost - 1;
+
+            if(spending) {
+                this.abilityUsed = true;
+
+                this.game.addMessage('{0} uses {1} to reduce the cost of {2} by 1', this.controller, this, card);                
+            }
+
+            if(cost < 0) {
+                cost = 0;
+            }
+
+            return cost;
+        }
+
+        return currentCost;
+    }
+
     getIncome() {
-        return 1;
+        if(!this.isBlank()) {
+            return 1;
+        }
     }
 }
 
