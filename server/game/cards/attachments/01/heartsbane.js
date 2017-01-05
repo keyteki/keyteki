0f0d956b19a5c7db1a0650dad4c93e17cf46d8d8
Stuart Walsh <stu@ipng.org.uk>
2017-01-05 15:33:44 +0000

    implement heartsbane

---
+++ server/game/cards/attachments/01/heartsbane.js
@@ -0,0 +1,47 @@
+const _ = require('underscore');
+
+const DrawCard = require('../../../drawcard.js');
+
+class Heartsbane extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Kneel this card to give attached card +3 STR',
+            method: 'kneel'
+        });
+    }
+
+    kneel(player) {
+        if(!this.parent || this.kneeled || !this.game.currentChallenge || !this.game.currentChallenge.isParticipating(this.parent)) {
+            return false;
+        }
+
+        this.triggered = true;
+        this.parent.strengthModifier += 3;
+
+        this.game.once('onChallengeFinished', this.onChallengeFinished.bind(this));
+
+        this.game.addMessage('{0} kneels {1} to give {2} +3 STR until the end of the challenge', player, this, this.parent);
+
+        return true;
+    }
+
+    canAttach(player, card) {
+        if(!card.isUnique() || card.getFaction() !== 'tyrell') {
+            return false;
+        }
+
+        return super.canAttach(player, card);
+    }
+
+    onChallengeFinished() {
+        if(this.triggered) {
+            this.parent.strengthModifier -= 3;
+
+            this.triggered = false;
+        }
+    }
+}
+
+Heartsbane.code = '01191';
+
+module.exports = Heartsbane;
