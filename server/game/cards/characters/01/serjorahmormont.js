5dfe0b0bba6853178c33cd9fbca8ddd55f3d93f8
Stuart Walsh <stu@ipng.org.uk>
2017-01-05 10:57:00 +0000

    implement ser jorah mormont

---
+++ server/game/cards/characters/01/serjorahmormont.js
@@ -0,0 +1,31 @@
+const _ = require('underscore');
+
+const DrawCard = require('../../../drawcard.js');
+
+class SerJorahMormont extends DrawCard {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+
+        this.registerEvents(['afterChallenge']);
+    }
+
+    afterChallenge(event, challenge) {
+        if(challenge.winner !== this.controller || !challenge.isParticipating(this)) {
+            return;
+        }
+
+        this.addToken('betrayal', 1);
+
+        this.game.addMessage('{0} is forced to place a betrayal token on {1} after winning a challenge in which he was participating', this.controller, this);
+
+        if(this.tokens['betrayal'] >= 3) {
+            this.controller.sacrificeCard(this);
+
+            this.game.addMessage('{0} sacrifices {1} as it has 3 or more betrayal tokens', this.controller, this);
+        }
+    }
+}
+
+SerJorahMormont.code = '01165';
+
+module.exports = SerJorahMormont;
