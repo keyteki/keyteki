66ad72ca402d5bb2a9208806176339c217439f24
Stuart Walsh <stu@ipng.org.uk>
2017-01-07 15:25:17 +0000

    implement throwing axe

---
+++ server/game/cards/attachments/01/throwingaxe.js
@@ -0,0 +1,43 @@
+const DrawCard = require('../../../drawcard.js');
+const AbilityLimit = require('../../../abilitylimit.js');
+
+class ThrowingAxe extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: (event, challenge) => challenge.winner === this.controller && challenge.isAttacking(this.parent)
+            },
+            limit: AbilityLimit.perPhase(1),
+            handler: () => {
+                this.game.promptForSelect(this.controller, {
+                    activePromptTitle: 'Select a character to kill',
+                    waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
+                    cardCondition: card => card.location === 'play area' && this.game.currentChallenge.isDefending(card),
+                    onSelect: (p, card) => this.onCardSelected(p, card)
+                });                
+            }
+        });
+    }
+
+    onCardSelected(player, card) {
+        player.sacrificeCard(this);
+
+        card.controller.killCharacter(card);
+
+        this.game.addMessage('{0} sacrifices {1} to kill {2}', player, this, card);
+
+        return true;
+    }
+
+    canAttach(player, card) {
+        if(card.getType() !== 'character' || !card.hasTrait('ironborn')) {
+            return false;
+        }
+
+        return super.canAttach(player, card);
+    }
+}
+
+ThrowingAxe.code = '01077';
+
+module.exports = ThrowingAxe;
