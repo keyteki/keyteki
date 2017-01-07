0271704ab4fe69897e47e1fb5282cc2dcf524f49
Stuart Walsh <stu@ipng.org.uk>
2017-01-07 19:26:46 +0000

    implement lady

--- server/game/cards/attachments/02/lady.js
+++ server/game/cards/attachments/02/lady.js
@@ -1,12 +1,50 @@
 const DrawCard = require('../../../drawcard.js');
 
 class Lady extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Pay 1 gold to attach Lady to another character',
+            method: 'reAttach'
+        });
+    }
+
+    reAttach(player) {
+        if(!this.parent || this.controller.gold < 1) {
+            return false;
+        }
+
+        player.removeAttachment(this, false);
+        player.promptForAttachment(this);
+
+        player.gold -= 1;
+
+        this.triggered = true;
+
+        return true;
+    }
+
     canAttach(player, card) {
         if(card.getFaction() !== this.getFaction()) {
             return false;
         }
 
         return super.canAttach(player, card);
+    }
+
+    attach(player, card) {
+        card.strengthModifier += 2;
+
+        if(this.triggered) {
+            if(card.name === 'Sansa Stark' && card.kneeled) {
+                player.standCard(card);
+            }
+
+            this.triggered = false;
+        }
+    }
+
+    leavesPlay() {
+        this.parent.strengthModifier -= 2;
     }
 }
 
