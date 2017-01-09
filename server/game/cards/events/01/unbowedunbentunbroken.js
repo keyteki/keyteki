f6cf94cac81e63b6f08219304ef7e906be27301e
Stuart Walsh <stu@ipng.org.uk>
2017-01-09 13:37:35 +0000

    implement unbowed, unbent, unbroken

---
+++ server/game/cards/events/01/unbowedunbentunbroken.js
@@ -0,0 +1,57 @@
+const DrawCard = require('../../../drawcard.js');
+
+class UnbowedUnbentUnbroken extends DrawCard {
+    canPlay(player, card) {
+        if(player !== this.controller || this !== card || player.firstPlayer) {
+            return false;
+        }
+
+        if(!this.game.currentChallenge || this.game.currentChallenge.loser !== this.controller) {
+            return false;
+        }
+
+        return true;
+    }
+
+    play(player) {
+        this.game.promptWithMenu(player, this, {
+            activePrompt: {
+                menuTitle: 'Select a challenge type',
+                buttons: [
+                    { text: 'Military', method: 'trigger', arg: 'military' },
+                    { text: 'Intrigue', method: 'trigger', arg: 'intrigue' },
+                    { text: 'Power', method: 'trigger', arg: 'power' },
+                    { text: 'Cancel', method: 'cancel' }
+                ]
+            },
+            waitingPromptTitle: 'Waiting for opponent to use ' + this.name
+        });
+    }
+
+    trigger(player, challengeType) {
+        var otherPlayer = this.game.getOtherPlayer(player);
+
+        if(!otherPlayer) {
+            return true;
+        }
+
+        otherPlayer.challenges[challengeType].max = 0;
+
+        this.game.addMessage('{0} uses {1} to make {2} unable to initiate {3} challenges until the end of the phase', player, this, otherPlayer, challengeType);
+
+        return true;
+    }
+
+    cancel(player) {
+        this.game.addMessage('{0} cancel the resolution of {1}', player, this);
+
+        player.gold += this.getCost();
+        player.moveCard(this, 'hand');
+
+        return true;
+    }
+}
+
+UnbowedUnbentUnbroken.code = '01120';
+
+module.exports = UnbowedUnbentUnbroken;
