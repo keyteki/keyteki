bc101f43a253d5f8155cf12f372e306ede4bfaf1
Stuart Walsh <stu@ipng.org.uk>
2017-01-09 11:54:56 +0000

    implement we do not sow

---
+++ server/game/cards/events/01/wedonotsow.js
@@ -0,0 +1,38 @@
+const _ = require('underscore');
+
+const DrawCard = require('../../../drawcard.js');
+
+class WeDoNotSow extends DrawCard {
+    canPlay(player, card) {
+        if(player !== this.controller || this !== card) {
+            return false;
+        }
+
+        if(!this.game.currentChallenge || this.game.currentChallenge.winner !== this.controller || !this.game.currentChallenge.isUnopposed()) {
+            return false;
+        }
+
+        return super.canPlay();
+    }
+
+    play(player) {
+        this.game.promptForSelect(player, {
+            activePromptTitle: 'Select attachment or location',
+            waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
+            cardCondition: card => card.location === 'play area' && card.controller === this.game.currentChallenge.loser && (card.getType() === 'attachment' || card.getType() === 'location'),
+            onSelect: (player, cards) => this.onCardSelected(player, cards)
+        });
+    }
+
+    onCardSelected(player, card) {
+        card.controller.discardCard(card);
+
+        this.game.addMessage('{0} uses {1} to discard {2} from play', player, this, card);
+
+        return true;
+    }
+}
+
+WeDoNotSow.code = '01083';
+
+module.exports = WeDoNotSow;
