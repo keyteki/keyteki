0b114a8bcc90a4ce5e86bffba7a371e5166f2f0c
Stuart Walsh <stu@ipng.org.uk>
2017-01-09 13:43:00 +0000

    until we implenent cancellable callbacks, do not let maester aemon trigger twice when multiple characters are killed

--- server/game/cards/characters/01/maesteraemon.js
+++ server/game/cards/characters/01/maesteraemon.js
@@ -5,11 +5,12 @@ class MaesterAemon extends DrawCard {
         this.interrupt({
             when: {
                 onKillingCharacter: (event, player, card, allowSaves) => {
-                    if(!allowSaves || card.getFaction() !== 'thenightswatch' || player !== card.controller || this.kneeled) {
+                    if(!allowSaves || card.getFaction() !== 'thenightswatch' || player !== card.controller || this.kneeled || this.triggered) {
                         return false;
                     }
 
                     this.toKill = card;
+                    this.triggered = true;
 
                     event.cancel = true;
 
@@ -20,9 +21,13 @@ class MaesterAemon extends DrawCard {
                 this.game.addMessage('{0} kneels {1} to save {2}', this.controller, this, this.toKill);
 
                 this.controller.kneelCard(this);
+
+                this.triggered = false;
             },
             onCancel: () => {
                 this.controller.killCharacter(this.toKill, false);
+
+                this.triggered = false;
 
                 return true;
             }
