3d727197d7ddf7a6a91e035cbb96e56828e98b83
Stuart Walsh <stu@ipng.org.uk>
2017-01-09 17:28:30 +0000

    implement harrenhal

---
+++ server/game/cards/locations/04/harrenhal.js
@@ -0,0 +1,31 @@
+const DrawCard = require('../../../drawcard.js');
+
+class Harrenhal extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: (event, card) => {
+                    if(this.controller === card.controller || this.controller.faction.kneeled || this.controller.phase === 'setup') {
+                        return false;
+                    }
+
+                    this.pendingCard = card;
+
+                    return true;
+                }
+            },
+            handler: () => {
+                this.controller.faction.kneeled = true;
+                this.controller.sacrificeCard(this);
+
+                this.pendingCard.controller.killCharacter(this.pendingCard);
+
+                this.game.addMessage('{0} sacrifices {1} to kill {2}', this.controller, this, this.pendingCard);
+            }
+        });
+    }
+}
+
+Harrenhal.code = '04082';
+
+module.exports = Harrenhal;
