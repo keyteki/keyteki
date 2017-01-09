238c45652dbc508e8db183ae8ac55298cc20b1b9
Stuart Walsh <stu@ipng.org.uk>
2017-01-09 19:50:26 +0000

    implement winter festival

---
+++ server/game/cards/plots/04/winterfestival.js
@@ -0,0 +1,34 @@
+const _ = require('underscore');
+
+const PlotCard = require('../../../plotcard.js');
+
+class WinterFestival extends PlotCard {
+    setupCardAbilities() {
+        this.interrupt({
+            when: {
+                onPhaseEnded: (event, phase) => {
+                    if(phase !== 'challenge') {
+                        return false;
+                    }
+
+                    if(_.any(this.game.getPlayers(), player => {
+                        return player.activePlot.hasTrait('Summer');
+                    })) {
+                        return false;
+                    }
+
+                    return true;
+                }
+            },
+            handler: () => {
+                this.game.addPower(this.controller, 2);
+
+                this.game.addMessage('{0} gains 2 power from {1}', this.controller, this, this.pendingCard);
+            }
+        });
+    }
+}
+
+WinterFestival.code = '04040';
+
+module.exports = WinterFestival;
