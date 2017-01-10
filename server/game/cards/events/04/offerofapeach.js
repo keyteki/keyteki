d26f1e4c06e8862e7b2c6f33fd508c086a6d5e2e
Stuart Walsh <stu@ipng.org.uk>
2017-01-10 12:24:58 +0000

    implement offer of a peach

---
+++ server/game/cards/events/04/offerofapeach.js
@@ -0,0 +1,45 @@
+const DrawCard = require('../../../drawcard.js');
+
+class OfferOfAPeach extends DrawCard {
+    canPlay(player, card) {
+        if(this !== card || player.phase !== 'challenge' || !this.game.currentChallenge) {
+            return false;
+        }
+
+        if(!player.cardsInPlay.any(card => {
+            return card.hasTrait('Lady') || card.name === 'Renly Baratheon';
+        })) {
+            return false;
+        }
+
+        return super.canPlay(player, card);
+    }
+
+    play(player) {
+        this.game.promptForSelect(player, {
+            cardCondition: card => this.cardCondition(card),
+            activePromptTitle: 'Select a character',
+            waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
+            onSelect: (player, card) => this.onCardSelected(player, card)
+        });
+
+        return true;
+    }
+
+    cardCondition(card) {
+        return card.location === 'play area' && this.game.currentChallenge.isAttacking(card);
+    }
+
+    onCardSelected(player, card) {
+        card.controller.standCard(card);
+        this.game.currentChallenge.removeFromChallenge(card);
+
+        this.game.addMessage('{0} uses {1} to stand {2} and remove them from the challenge', player, this, card);
+
+        return true;
+    }
+}
+
+OfferOfAPeach.code = '04084';
+
+module.exports = OfferOfAPeach;
