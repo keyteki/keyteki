71f72d0844cd1a2b9dc807c8647c04fcbe9d6911
Stuart Walsh <stu@ipng.org.uk>
2017-01-11 08:39:58 +0000

    Add chat messages for claim.  Fixes #204

--- server/game/gamesteps/challenge/challengeflow.js
+++ server/game/gamesteps/challenge/challengeflow.js
@@ -169,12 +169,20 @@ class ChallengeFlow extends BaseStep {
 
         switch(this.challenge.challengeType) {
             case 'military':
+                this.game.addMessage('{0} claim is applied.  {1} must kill {2} character{3}', this.challenge.challengeType, this.challenge.loser, this.challenge.claim, 
+                    this.challenge.claim > 1 ? 's' : '');
                 this.game.queueStep(new FulfillMilitaryClaim(this.game, this.challenge.loser, this.challenge.claim));
                 break;
             case 'intrigue':
+                this.game.addMessage('{0} claim is applied.  {1} must discard {2} card{3} at random', this.challenge.challengeType, this.challenge.loser, this.challenge.claim, 
+                    this.challenge.claim > 1 ? 's' : '');
                 this.challenge.loser.discardAtRandom(this.challenge.claim);
                 break;
             case 'power':
+                if(this.challenge.loser.faction.power > 0) {
+                    this.game.addMessage('{0} claim is applied.  {1} removes {2} power and {3} gains {2} power', this.challenge.challengeType, this.challenge.loser, this.challenge.claim, 
+                        this.challenge.winner);
+                }
                 this.game.transferPower(this.challenge.winner, this.challenge.loser, this.challenge.claim);
                 break;
         }
