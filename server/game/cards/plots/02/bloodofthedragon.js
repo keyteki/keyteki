c332fcace9b95ebe59f3f9b87a24389ee8a6220c
Stuart Walsh <stu@ipng.org.uk>
2017-01-13 18:15:19 +0000

    implement blood of the dragon

---
+++ server/game/cards/plots/02/bloodofthedragon.js
@@ -0,0 +1,20 @@
+const _ = require('underscore');
+
+const PlotCard = require('../../../plotcard.js');
+
+class BloodOfTheDragon extends PlotCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: card => card.getType() === 'character' && !card.hasTrait('Dragon'),
+            targetController: 'any',
+            effect: [
+                ability.effects.modifyStrength(-1),
+                ability.effects.killByStrength
+            ]
+        });
+    }
+}
+
+BloodOfTheDragon.code = '02075';
+
+module.exports = BloodOfTheDragon;
