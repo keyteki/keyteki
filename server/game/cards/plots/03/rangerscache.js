f6ed60c6d4488d0389b5255de98b0316b4b408e8
Stuart Walsh <stu@ipng.org.uk>
2017-01-14 13:54:07 +0000

    implement ranger's cache

---
+++ server/game/cards/plots/03/rangerscache.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../../drawcard.js');
+
+const PlotCard = require('../../../plotcard.js');
+
+class RangersCache extends PlotCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onPhaseEnded: (event, phase) => phase === 'taxation'
+            },
+            choices: {
+                'Gain 3 gold': () => {
+                    this.game.addGold(this.controller, 3);
+                    this.game.addMessage('{0} uses {1} to gain 3 gold', this.controller, this);
+                },
+                'Draw 2 cards': () => {
+                    this.controller.drawCardsToHand(2);
+                    this.game.addMessage('{0} uses {1} to draw 2 card', this.controller, this);
+                }
+            }
+        });
+    }
+}
+
+RangersCache.code = '03052';
+
+module.exports = RangersCache;
