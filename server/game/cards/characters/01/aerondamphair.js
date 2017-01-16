8bcc4809b0284330e1b230d6ae8abc8ddb7064c8
Stuart Walsh <stu@ipng.org.uk>
2017-01-16 19:49:53 +0000

    implement aeron damphair

---
+++ server/game/cards/characters/01/aerondamphair.js
@@ -0,0 +1,49 @@
+const DrawCard = require('../../../drawcard.js');
+
+class AeronDamphair extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onDominanceDetermined: (event, winner) => this.controller === winner
+            },
+            handler: () => {
+                this.game.promptForSelect(this.controller, {
+                    cardCondition: card => this.cardCondition(card),
+                    activePromptTitle: 'Select character',
+                    waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
+                    onSelect: (player, card) => this.onCardSelected(player, card)
+                });
+            }
+        });
+    }
+
+    cardCondition(card) {
+        var mainCondition = card.getType() === 'character' && card.location === 'dead pile' && card.hasTrait('Ironborn');
+
+        if(!mainCondition || !card.isUnique()) {
+            return mainCondition;
+        }
+
+        var sameCardsInDeadPile = this.controller.deadPile.filter(c => {
+            return c.name === card.name;
+        });
+
+        if(sameCardsInDeadPile.length > 1) {
+            return false;
+        }
+
+        return mainCondition;
+    }
+
+    onCardSelected(player, card) {
+        player.moveCard(card, 'play area');
+
+        this.game.addMessage('{0} uses {1} to put {2} into play from their dead pile', player, this, card);
+
+        return true;
+    } 
+}
+
+AeronDamphair.code = '01065';
+
+module.exports = AeronDamphair;
