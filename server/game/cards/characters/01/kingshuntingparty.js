b858d07eeb9e5242420c1668d8f7156a79de4d81
Stuart Walsh <stu@ipng.org.uk>
2017-01-16 18:56:44 +0000

    implement king's hunting party

---
+++ server/game/cards/characters/01/kingshuntingparty.js
@@ -0,0 +1,30 @@
+const DrawCard = require('../../../drawcard.js');
+
+class KingsHuntingParty extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: this.effectCondition.bind(this),
+            match: this,
+            effect: ability.effects.addIcon('intrigue')
+        });
+    }
+
+    effectCondition() {
+        var otherPlayer = this.game.getOtherPlayer(this.controller);
+        if(!otherPlayer || this.controller.phase === 'setup') {
+            return false;
+        }
+
+        if(otherPlayer.cardsInPlay.any(card => {
+            return card.getType() === 'character' && card.hasTrait('King');
+        })) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+KingsHuntingParty.code = '01055';
+
+module.exports = KingsHuntingParty;
