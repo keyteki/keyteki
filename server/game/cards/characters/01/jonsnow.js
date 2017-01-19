81ffa3bc177ad54747ea6cfda5740b528db8a45d
Stuart Walsh <stu@ipng.org.uk>
2017-01-19 12:44:05 +0000

    implement jon snow

---
+++ server/game/cards/characters/01/jonsnow.js
@@ -0,0 +1,32 @@
+const _ = require('underscore');
+
+const DrawCard = require('../../../drawcard.js');
+
+class JonSnow extends DrawCard {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+
+        this.registerEvents(['onAttackersDeclared']);
+    }
+
+    onAttackersDeclared(event, challenge) {
+        if(this.kneeled || this.controller !== challenge.attackingPlayer) {
+            return;
+        }
+
+        if(!_.any(challenge.attackers, card => {
+            return card.getFaction() === 'thenightswatch';
+        })) {
+            return;
+        }
+
+        challenge.addAttacker(this);
+        this.kneeled = false;
+
+        this.game.addMessage('{0} uses {1} to add {1} to the challenge as an attacker', this.controller, this);
+    }
+}
+
+JonSnow.code = '01124';
+
+module.exports = JonSnow;
