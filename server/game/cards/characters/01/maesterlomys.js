9470ecdebf1eda47390d8799c0aa69e39ae707d3
Stuart Walsh <stu@ipng.org.uk>
2017-01-21 16:42:26 +0000

    implement maester lomys

---
+++ server/game/cards/characters/01/maesterlomys.js
@@ -0,0 +1,20 @@
+const DrawCard = require('../../../drawcard.js');
+
+class MaesterLomys extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: (event, challenge) => challenge.winner === this.controller && challenge.defendingPlayer === this.controller
+            },
+            handler: () => {
+                this.game.currentChallenge.loser.discardAtRandom(1);
+
+                this.game.addMessage('{0} uses {1} to discard 1 card at random from {2}\'s hand', this.controller, this, this.game.currentChallenge.loser);
+            }
+        });
+    }
+}
+
+MaesterLomys.code = '01180';
+
+module.exports = MaesterLomys;
