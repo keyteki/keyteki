bd9bd5f9506a6b140851ce36b88d3040ea88aa15
Stuart Walsh <stu@ipng.org.uk>
2017-01-21 17:12:12 +0000

    typo

--- server/game/cards/characters/01/thequeenofthorns.js
+++ server/game/cards/characters/01/thequeenofthorns.js
@@ -15,14 +15,14 @@ class TheQueenOfThorns extends DrawCard {
                 });                 
             }
         });
+    }
 
-        onCardSelected(player, card) {
-            player.moveCard(card, 'play area');
+    onCardSelected(player, card) {
+        player.moveCard(card, 'play area');
 
-            this.game.addMessage('{0} uses {1} to put {2} in play from their hand', player, this, card);
+        this.game.addMessage('{0} uses {1} to put {2} in play from their hand', player, this, card);
 
-            return true;
-        }
+        return true;
     }
 }
 
