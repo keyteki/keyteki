720458767550f9bccaee705136bd574a88f7a10c
Brian Upton <ystros@gmail.com>
2017-01-23 22:53:08 -0800

    Fix tests for Kings of Summer / Winter. (#260)

--- test/server/cards/agendas/04037 - kingsofsummer.spec.js
+++ test/server/cards/agendas/04037 - kingsofsummer.spec.js
@@ -10,7 +10,6 @@ describe('Kings Of Summer', function() {
         this.plot1 = jasmine.createSpyObj('plot1', ['hasTrait']);
         this.plot2 = jasmine.createSpyObj('plot2', ['hasTrait']);
         this.plot3 = jasmine.createSpyObj('plot3', ['hasTrait']);
-        this.plot4 = jasmine.createSpyObj('plot4', ['hasTrait']);
 
         this.plot1.hasTrait.and.callFake(trait => {
             return trait === 'Summer';
@@ -19,86 +18,91 @@ describe('Kings Of Summer', function() {
 
         this.player1Fake = {};
         this.player1Fake.game = this.gameSpy;
-        this.player1Fake.selectedPlot = this.plot1;
-        this.player1Fake.selectedPlot.reserveModifier = 0;
-        this.player1Fake.selectedPlot.goldModifier = 0;
+        this.player1Fake.activePlot = this.plot1;
+        this.player1Fake.activePlot.reserveModifier = 0;
+        this.player1Fake.activePlot.goldModifier = 0;
+        this.plot1.controller = this.player1Fake;
+        this.plot3.controller = this.player1Fake;
 
         this.player2Fake = {};
         this.player2Fake.game = this.gameSpy;
-        this.player2Fake.selectedPlot = this.plot2;
-        this.player2Fake.selectedPlot.reserveModifier = 0;
-        this.player2Fake.selectedPlot.goldModifier = 0;
+        this.player2Fake.activePlot = this.plot2;
+        this.player2Fake.activePlot.reserveModifier = 0;
+        this.player2Fake.activePlot.goldModifier = 0;
+        this.plot2.controller = this.player2Fake;
 
         this.gameSpy.getPlayers.and.returnValue([this.player1Fake, this.player2Fake]);
 
         this.agenda = new KingsOfSummer(this.player1Fake, {});
     });
 
-    describe('When flipped faceup', function() {
+    describe('reserve persistent effect', function() {
         beforeEach(function() {
-            this.agenda.onPlotFlip();
+            this.reserveEffect = this.agenda.abilities.persistentEffects[0];
         });
 
-        it('should increase the reserve modifier of both players plot cards', function() {
-            expect(this.player1Fake.selectedPlot.reserveModifier).toBe(1);
-            expect(this.player2Fake.selectedPlot.reserveModifier).toBe(1);
+        it('should target all players', function() {
+            expect(this.reserveEffect.targetController).toBe('any');
         });
 
-        describe('and there is a winter plot revealed', function() {
+        it('should match with active plots', function() {
+            expect(this.reserveEffect.match(this.plot1)).toBe(true);
+            // Belongs to player 1 but is not active.
+            expect(this.reserveEffect.match(this.plot3)).toBe(false);
+        });
+
+        it('should increase the plots reserve', function() {
+            this.reserveEffect.effect.apply(this.plot1);
+            expect(this.plot1.reserveModifier).toBe(1);
+        });
+    });
+
+    describe('gold persistent effect', function() {
+        beforeEach(function() {
+            this.goldEffect = this.agenda.abilities.persistentEffects[1];
+        });
+
+        it('should increase the gold on the plot', function() {
+            this.goldEffect.effect.apply(this.plot1);
+            expect(this.plot1.goldModifier).toBe(1);
+        });
+
+        describe('when a Winter plot is revealed', function() {
             beforeEach(function() {
-                this.player1Fake.selectedPlot.reserveModifier = 0;
-                this.player1Fake.selectedPlot.goldModifier = 0;
-                this.player2Fake.selectedPlot.reserveModifier = 0;
-                this.player2Fake.selectedPlot.goldModifier = 0;
-
-                this.plot2.hasTrait.and.returnValue(true);
-
-                this.agenda.onPlotFlip();
+                this.plot2.hasTrait.and.callFake(trait => trait === 'Winter');
             });
 
-            it('should not increase the owning player\'s plot gold modifier', function() {
-                expect(this.player1Fake.selectedPlot.goldModifier).toBe(0);
-                expect(this.player2Fake.selectedPlot.goldModifier).toBe(0);
-            });
-
-            describe('and then a new plot is flipped faceup', function() {
-                beforeEach(function() {
-                    this.player1Fake.activePlot = this.player1Fake.selectedPlot;
-                    this.player2Fake.activePlot = this.player2Fake.selectedPlot;
-                    this.player1Fake.selectedPlot = this.plot3;
-                    this.player2Fake.selectedPlot = this.plot4;
-
-                    this.agenda.onPlotFlip();
-                });
-
-                it('should restore the reserve modifier of both players plots', function() {
-                    expect(this.plot1.reserveModifier).toBe(0);
-                    expect(this.plot2.reserveModifier).toBe(0);
-                });
+            it('should not pass the activation condition', function() {
+                expect(this.goldEffect.condition()).toBe(false);
             });
         });
 
-        describe('and there are no winter plots revealed', function() {
-            it('should increase the owning player\'s plot gold modifier', function() {
-                expect(this.player1Fake.selectedPlot.goldModifier).toBe(1);
-                expect(this.player2Fake.selectedPlot.goldModifier).toBe(0);
-            });
-
-            describe('and then a new plot is flipped faceup', function() {
+        describe('when no Winter plot is revealed', function() {
+            describe('and the current player has a Summer plot', function() {
                 beforeEach(function() {
-                    this.player1Fake.activePlot = this.player1Fake.selectedPlot;
-                    this.player2Fake.activePlot = this.player2Fake.selectedPlot;
-                    this.player1Fake.selectedPlot = this.plot3;
-                    this.player2Fake.selectedPlot = this.plot4;
-
-                    this.agenda.onPlotFlip();
+                    this.plot1.hasTrait.and.callFake(trait => trait === 'Summer');
                 });
 
-                it('should reset the player\'s gold and reserve modifiers', function() {
-                    expect(this.plot1.goldModifier).toBe(0);
-                    expect(this.plot1.goldModifier).toBe(0);
-                    expect(this.plot1.reserveModifier).toBe(0);
-                    expect(this.plot1.reserveModifier).toBe(0);
+                it('should pass the activation condition', function() {
+                    expect(this.goldEffect.condition()).toBe(true);
+                });
+
+                it('should match the plot', function() {
+                    expect(this.goldEffect.match(this.plot1)).toBe(true);
+                });
+            });
+
+            describe('and the current player does not have a Summer plot', function() {
+                beforeEach(function() {
+                    this.plot1.hasTrait.and.returnValue(false);
+                });
+
+                it('should pass the activation condition', function() {
+                    expect(this.goldEffect.condition()).toBe(true);
+                });
+
+                it('should not match the plot', function() {
+                    expect(this.goldEffect.match(this.plot1)).toBe(false);
                 });
             });
         });
--- test/server/cards/agendas/04038 - kingsofwinter.spec.js
+++ test/server/cards/agendas/04038 - kingsofwinter.spec.js
@@ -5,111 +5,108 @@ const KingsOfWinter = require('../../../../server/game/cards/agendas/kingsofwint
 
 describe('Kings Of Winter', function() {
     beforeEach(function() {
-        this.gameSpy = jasmine.createSpyObj('game', ['addMessage', 'getPlayers', 'on', 'getOtherPlayer']);
+        this.gameSpy = jasmine.createSpyObj('game', ['addMessage', 'getPlayers', 'on']);
 
         this.plot1 = jasmine.createSpyObj('plot1', ['hasTrait']);
         this.plot2 = jasmine.createSpyObj('plot2', ['hasTrait']);
         this.plot3 = jasmine.createSpyObj('plot3', ['hasTrait']);
-        this.plot4 = jasmine.createSpyObj('plot4', ['hasTrait']);
 
-        this.plot1.hasTrait.and.returnValue(false);
+        this.plot1.hasTrait.and.callFake(trait => {
+            return trait === 'Summer';
+        });
         this.plot2.hasTrait.and.returnValue(false);
 
         this.player1Fake = {};
         this.player1Fake.game = this.gameSpy;
-        this.player1Fake.selectedPlot = this.plot1;
-        this.player1Fake.selectedPlot.reserveModifier = 5;
-        this.player1Fake.selectedPlot.goldModifier = 0;
+        this.player1Fake.activePlot = this.plot1;
+        this.player1Fake.activePlot.reserveModifier = 0;
+        this.player1Fake.activePlot.goldModifier = 0;
+        this.plot1.controller = this.player1Fake;
+        this.plot3.controller = this.player1Fake;
 
         this.player2Fake = {};
         this.player2Fake.game = this.gameSpy;
-        this.player2Fake.selectedPlot = this.plot2;
-        this.player2Fake.selectedPlot.reserveModifier = 5;
-        this.player2Fake.selectedPlot.goldModifier = 0;
+        this.player2Fake.activePlot = this.plot2;
+        this.player2Fake.activePlot.reserveModifier = 0;
+        this.player2Fake.activePlot.goldModifier = 0;
+        this.plot2.controller = this.player2Fake;
 
         this.gameSpy.getPlayers.and.returnValue([this.player1Fake, this.player2Fake]);
-        this.gameSpy.getOtherPlayer.and.returnValue(this.player2Fake);
 
         this.agenda = new KingsOfWinter(this.player1Fake, {});
     });
 
-    describe('When flipped faceup', function() {
+    describe('reserve persistent effect', function() {
         beforeEach(function() {
-            this.agenda.onPlotFlip();
+            this.reserveEffect = this.agenda.abilities.persistentEffects[0];
         });
 
-        it('should reduce the reserve modifier of both players plot cards', function() {
-            expect(this.player1Fake.selectedPlot.reserveModifier).toBe(4);
-            expect(this.player2Fake.selectedPlot.reserveModifier).toBe(4);
+        it('should target all players', function() {
+            expect(this.reserveEffect.targetController).toBe('any');
         });
 
-        describe('and I have a winter plot revealed', function() {
+        it('should match with active plots', function() {
+            expect(this.reserveEffect.match(this.plot1)).toBe(true);
+            // Belongs to player 1 but is not active.
+            expect(this.reserveEffect.match(this.plot3)).toBe(false);
+        });
+
+        it('should reduce the plots reserve', function() {
+            this.reserveEffect.effect.apply(this.plot1);
+            expect(this.plot1.reserveModifier).toBe(-1);
+        });
+    });
+
+    describe('gold persistent effect', function() {
+        beforeEach(function() {
+            this.goldEffect = this.agenda.abilities.persistentEffects[1];
+        });
+
+        it('should reduce the gold on the plot', function() {
+            this.goldEffect.effect.apply(this.plot1);
+            expect(this.plot1.goldModifier).toBe(-1);
+        });
+
+        describe('when the current player does not have a Winter', function() {
             beforeEach(function() {
-                this.player1Fake.selectedPlot.reserveModifier = 0;
-                this.player1Fake.selectedPlot.goldModifier = 0;
-                this.player2Fake.selectedPlot.reserveModifier = 0;
-                this.player2Fake.selectedPlot.goldModifier = 0;
-
-                this.plot1.hasTrait.and.returnValue(true);
-
-                this.agenda.onPlotFlip();
+                this.plot1.hasTrait.and.returnValue(false);
             });
 
-            describe('and my opponent has a summer plot revealed', function() {
+            it('should not pass the activation condition', function() {
+                expect(this.goldEffect.condition()).toBe(false);
+            });
+        });
+
+        describe('when the current player has a Winter plot', function() {
+            beforeEach(function() {
+                this.plot1.hasTrait.and.callFake(trait => trait === 'Winter');
+            });
+
+            describe('and opponent has a Summer plot', function() {
                 beforeEach(function() {
-                    this.player1Fake.selectedPlot.reserveModifier = 0;
-                    this.player1Fake.selectedPlot.goldModifier = 0;
-                    this.player2Fake.selectedPlot.reserveModifier = 0;
-                    this.player2Fake.selectedPlot.goldModifier = 0;
-                    this.plot2.hasTrait.and.returnValue(true);
-
-                    this.agenda.onPlotFlip();
+                    this.plot2.hasTrait.and.callFake(trait => trait === 'Summer');
                 });
 
-                it('should not reduce the opponent\'s gold modifier', function() {
-                    expect(this.player1Fake.selectedPlot.goldModifier).toBe(0);
-                    expect(this.player2Fake.selectedPlot.goldModifier).toBe(0);
+                it('should pass the activation condition', function() {
+                    expect(this.goldEffect.condition()).toBe(true);
                 });
 
-                describe('and then a new plot is flipped faceup', function() {
-                    beforeEach(function() {
-                        this.player1Fake.activePlot = this.player1Fake.selectedPlot;
-                        this.player2Fake.activePlot = this.player2Fake.selectedPlot;
-                        this.player1Fake.selectedPlot = this.plot3;
-                        this.player2Fake.selectedPlot = this.plot4;
-
-                        this.agenda.onPlotFlip();
-                    });
-
-                    it('should restore the reserve modifier of both players plots', function() {
-                        expect(this.plot1.reserveModifier).toBe(0);
-                        expect(this.plot2.reserveModifier).toBe(0);
-                    });
+                it('should not match the plot', function() {
+                    expect(this.goldEffect.match(this.plot2)).toBe(false);
                 });
             });
 
-            describe('and my opponent does not have a summer plot revealed', function() {
-                it('should reduce the opponent\'s gold modifier by 1', function() {
-                    expect(this.player1Fake.selectedPlot.goldModifier).toBe(0);
-                    expect(this.player2Fake.selectedPlot.goldModifier).toBe(-1);
+            describe('and opponent does not have a Summer plot', function() {
+                beforeEach(function() {
+                    this.plot2.hasTrait.and.returnValue(false);
                 });
 
-                describe('and then a new plot is flipped faceup', function() {
-                    beforeEach(function() {
-                        this.player1Fake.activePlot = this.player1Fake.selectedPlot;
-                        this.player2Fake.activePlot = this.player2Fake.selectedPlot;
-                        this.player1Fake.selectedPlot = this.plot3;
-                        this.player2Fake.selectedPlot = this.plot4;
+                it('should pass the activation condition', function() {
+                    expect(this.goldEffect.condition()).toBe(true);
+                });
 
-                        this.agenda.onPlotFlip();
-                    });
-
-                    it('should restore the reserve and gold modifier of both players plots', function() {
-                        expect(this.plot1.reserveModifier).toBe(0);
-                        expect(this.plot1.goldModifier).toBe(0);
-                        expect(this.plot2.reserveModifier).toBe(0);
-                        expect(this.plot1.goldModifier).toBe(0);
-                    });
+                it('should match the plot', function() {
+                    expect(this.goldEffect.match(this.plot2)).toBe(true);
                 });
             });
         });
