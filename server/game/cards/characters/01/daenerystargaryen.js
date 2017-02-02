579b8de6325b42157d4b755f34a15edd747268a5
Brian Upton <ystros@gmail.com>
2017-02-02 10:26:10 -0800

    Convert Daenyrs Targaryen to effects API (#288)
    Fixes #287

--- server/game/cards/characters/01/daenerystargaryen.js
+++ server/game/cards/characters/01/daenerystargaryen.js
@@ -3,51 +3,13 @@ const _ = require('underscore');
 const DrawCard = require('../../../drawcard.js');
 
 class DaenerysTargaryen extends DrawCard {
-    constructor(owner, cardData) {
-        super(owner, cardData);
-
-        this.registerEvents(['onAttackersDeclared', 'onDefendersDeclared', 'afterChallenge']);
-    }
-
-    modifyStrength(challenge) {
-        var opponentCards = challenge.getOpponentCards(this.controller);
-
-        _.each(opponentCards, card => {
-            card.strengthModifier--;
-        });
-
-        challenge.calculateStrength();
-    
-        this.game.addMessage('{0} uses {1} to add -1 to the strength of each opponent\'s participating character for this challenge', this.controller, this);
-
-        this.strengthModified = true;
-    }
-
-    onAttackersDeclared(event, challenge) {
-        if(this.kneeled || challenge.attackingPlayer === this.controller || this.isBlank()) {
-            return;
-        }
-
-        this.modifyStrength(challenge);
-    }
-
-    onDefendersDeclared(event, challenge) {
-        if(this.kneeled || challenge.defendingPlayer === this.controller || this.isBlank()) {
-            return;
-        }
-
-        this.modifyStrength(challenge);
-    }    
-
-    afterChallenge(event, challenge) {
-        if(!this.strengthModified || this.isBlank()) {
-            return;
-        }
-
-        var opponentCards = challenge.getOpponentCards(this.controller);
-
-        _.each(opponentCards, card => {
-            card.strengthModifier++;
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => !this.kneeled && this.game.currentChallenge,
+            match: card => this.game.currentChallenge.isParticipating(card) && card.getType() === 'character',
+            targetController: 'opponent',
+            effect: ability.effects.modifyStrength(-1),
+            recalculateWhen: ['onCardStood', 'onCardKneeled', 'onAttackersDeclared', 'onDefendersDeclared']
         });
     }
 }
