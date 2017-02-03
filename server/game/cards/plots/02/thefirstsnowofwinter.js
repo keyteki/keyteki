455e597b9d2680ed40dadeff4c23c2ddc14985f8
Stuart Walsh <stu@ipng.org.uk>
2017-02-03 19:16:46 +0000

    fix first snow text

--- server/game/cards/plots/02/thefirstsnowofwinter.js
+++ server/game/cards/plots/02/thefirstsnowofwinter.js
@@ -11,7 +11,7 @@ class TheFirstSnowOfWinter extends PlotCard {
             handler: () => {
                 _.each(this.game.getPlayers(), player => this.returnCardsToHand(player));
 
-                this.game.addMessage('{0} uses {1} to force both players to return each card with printed cost 3 or lower to their hand', this.controller);
+                this.game.addMessage('{0} uses {1} to force both players to return each card with printed cost 3 or lower to their hand', this.controller, this);
             }
         });
     }
