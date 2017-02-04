0836e6cbf7c8694acfb816d14c3f99f5c64fb0cd
DukeTax <DukeTax@users.noreply.github.com>
2017-02-04 19:53:10 +0100

    Create appointed.js (#304)
    Implement appointed

---
+++ server/game/cards/attachments/05/appointed.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../../drawcard.js');
+
+class Appointed extends DrawCard {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: [
+            ability.effects.addIcon('intrigue'),
+            ability.effects.addTrait('Small Council')
+            ]
+        });
+    }
+    
+    canAttach(player, card) {
+        if(card.getType() !== 'character' || !card.isUnique()) {
+            return false;
+        }
+
+        return super.canAttach(player, card);
+    }
+}
+
+Appointed.code = '05043';
+
+module.exports = Appointed;
