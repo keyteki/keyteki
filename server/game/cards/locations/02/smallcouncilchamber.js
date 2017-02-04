2a3ce4939201b1ecbadacc2dc7fbb9245c98d4b5
Brian Upton <ystros@gmail.com>
2017-02-04 01:44:56 -0800

    Implement Small Council Chamber. (#295)

---
+++ server/game/cards/locations/02/smallcouncilchamber.js
@@ -0,0 +1,20 @@
+const DrawCard = require('../../../drawcard.js');
+
+class SmallCouncilChamber extends DrawCard {
+    setupCardAbilities() {
+        // TODO: Immune to effects.
+        this.reaction({
+            when: {
+                afterChallenge: (e, challenge) => challenge.winner === this.controller && challenge.challengeType === 'intrigue'
+            },
+            handler: () => {
+                this.game.addMessage('{0} gains 1 power on {1}', this.controller, this);
+                this.modifyPower(1);
+            }
+        });
+    }
+}
+
+SmallCouncilChamber.code = '02110';
+
+module.exports = SmallCouncilChamber;
