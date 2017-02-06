e749dd8337b6b12c7fa880072f63755a74dcab90
DukeTax <DukeTax@users.noreply.github.com>
2017-02-06 19:39:03 +0100

    Implement Bear Island (#309)
    Not a 100% perfect implementation, since I believe it would trigger when you would for example put a Grey Wind into play with Arianne during marshalling, but that's such a fringe case and I believe Stark Harrenhal has a similar limitation.

---
+++ server/game/cards/locations/04/bearisland.js
@@ -0,0 +1,20 @@
+const DrawCard = require('../../../drawcard.js');
+
+class BearIsland extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: (event, card) => card.controller === this.controller && card.isLoyal() && this.game.currentPhase === 'marshal'
+            },
+            limit: ability.limit.perPhase(2),
+            handler: () => {
+                this.game.addGold(this.controller, 1);
+                this.game.addMessage('{0} uses {1} to gain 1 gold', this.controller, this);
+            }
+        });
+    }
+}
+
+BearIsland.code = '04042';
+
+module.exports = BearIsland;
