94e96ea72036dcfff78b7bbffd98f14eff3e390f
DukeTax <DukeTax@users.noreply.github.com>
2017-02-07 08:05:17 +0100

    Implement Greatjon's Vanguard (#312)
    Is there a way to implement 'no attachments' and the like? Didn't see it on other cards.

---
+++ server/game/cards/characters/02/greatjonsvanguard.js
@@ -0,0 +1,18 @@
+const DrawCard = require('../../../drawcard.js');
+
+class GreatjonsVanguard extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.controller.plotDiscard.size() <= 2,
+            match: this,
+            effect: [
+              ability.effects.modifyStrength(2),
+              ability.effects.addKeyword('Renown')
+            ]
+        });
+    }
+}
+
+GreatjonsVanguard.code = '02081';
+
+module.exports = GreatjonsVanguard;
