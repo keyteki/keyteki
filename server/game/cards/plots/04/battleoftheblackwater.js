51a737d4c8f4547af7f6c9386bb13bd0481a9183
Kevin Cavnar-Johnson <cavnak@gmail.com>
2017-02-08 01:11:41 -0600

    Feature/implement blackwater (#318)
    * Battle of the Blackwater

---
+++ server/game/cards/plots/04/battleoftheblackwater.js
@@ -0,0 +1,28 @@
+const _ = require('underscore');
+
+const PlotCard = require('../../../plotcard.js');
+
+class BattleOfTheBlackwater extends PlotCard {
+    setupCardAbilities() {
+        this.whenRevealed({
+            handler: () => {
+                _.each(this.game.getPlayersInFirstPlayerOrder(), player => {
+                    this.removeAllDupes(player);
+                });
+            }
+        });
+    }
+
+    removeAllDupes(player) {
+        var characters = player.cardsInPlay.filter(card => card.dupes.size() > 0);
+        _.each(characters, character => {
+            while(character.dupes.size() > 0) {
+                player.removeDuplicate(character);
+            }
+        });
+    }
+}
+
+BattleOfTheBlackwater.code = '04120';
+
+module.exports = BattleOfTheBlackwater;
