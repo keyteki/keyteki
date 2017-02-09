45595c92a5bf73623630c64078578c9c012d8ed2
DukeTax <DukeTax@users.noreply.github.com>
2017-02-09 08:11:27 +0100

    Implement Lannisport Guard (#324)
    * Implement Lannisport Guard

---
+++ server/game/cards/characters/05/lannisportguard.js
@@ -0,0 +1,22 @@
+const _ = require('underscore');
+const DrawCard = require('../../../drawcard.js');
+
+class LannisportGuard extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: (e, card) => card === this && this.game.currentPhase === 'marshal'
+            },
+            handler: () => {
+                _.each(this.game.getPlayers(), player => {
+                player.drawCardsToHand(1);
+                });
+                this.game.addMessage('{0} uses {1} to have each player draw a card', this.controller, this);
+            }
+        });
+    }
+}
+
+LannisportGuard.code = '05016';
+
+module.exports = LannisportGuard;
