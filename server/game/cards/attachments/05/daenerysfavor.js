dc796bb081e0921712a642eb560ac66d518ef7fd
Kevin Cavnar-Johnson <cavnak@gmail.com>
2017-02-10 00:59:24 -0600

    implement danys favor (#328)

---
+++ server/game/cards/attachments/05/daenerysfavor.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../../drawcard.js');
+
+class DaenerysFavor extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => (
+                this.game.currentChallenge &&
+                this.game.currentChallenge.isParticipating(this.parent)
+            ),
+            match: card => this.game.currentChallenge.isParticipating(card) && card.getType() === 'character' && card !== this.parent,
+            targetController: 'any',
+            effect: ability.effects.modifyStrength(-1),
+            recalculateWhen: ['onAttackersDeclared', 'onDefendersDeclared']
+        });
+    }
+    canAttach(player, card) {
+        if(card.getType() !== 'character' || card.getFaction() !== this.getFaction()) {
+            return false;
+        }
+        return super.canAttach(player, card);
+    }
+}
+
+DaenerysFavor.code = '05036';
+
+module.exports = DaenerysFavor;
