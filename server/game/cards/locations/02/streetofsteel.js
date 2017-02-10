cc916bd19ded923aef3d5c1d4fc3790e534708e1
DukeTax <DukeTax@users.noreply.github.com>
2017-02-10 07:58:26 +0100

    Implement Street of Steel (#326)
    * Implement Street of Steel

---
+++ server/game/cards/locations/02/streetofsteel.js
@@ -0,0 +1,56 @@
+const DrawCard = require('../../../drawcard.js');
+
+class StreetOfSteel extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: (e, challenge) => challenge.winner === this.controller && challenge.challengeType === 'military' && !this.controller.faction.kneeled
+            },
+            handler: () => {
+                var events = this.controller.searchDrawDeck(10, card => {
+                    return card.getType() === 'attachment' && (card.hasTrait('Weapon') || card.hasTrait('Item'));
+                });
+
+                var buttons = _.map(events, card => {
+                    return { text: card.name, method: 'cardSelected', arg: card.uuid, card: card.getSummary(true) };
+                });
+                buttons.push({ text: 'Done', method: 'doneSelecting' });
+
+                this.game.promptWithMenu(this.controller, this, {
+                    activePrompt: {
+                        menuTitle: 'Select a card to add to your hand',
+                        buttons: buttons
+                    },
+                    waitingPromptTitle: 'Waiting for opponent to use ' + this.name
+                });
+            }
+        });
+    }
+
+    cardSelected(player, cardId) {
+        var card = player.findCardByUuid(player.drawDeck, cardId);
+
+        if(!card) {
+            return false;
+        }
+
+        this.controller.faction.kneeled = true;
+        player.moveCard(card, 'hand');
+        player.shuffleDrawDeck();
+        this.game.addMessage('{0} uses {1} to reveal {2} and add it to their hand', player, this, card);
+
+        return true;
+    }
+
+    doneSelecting(player) {
+        this.controller.faction.kneeled = true;
+        player.shuffleDrawDeck();
+        this.game.addMessage('{0} does not use {1} to add a card to their hand', player, this);
+
+        return true;
+    }
+}
+
+StreetOfSteel.code = '02097';
+
+module.exports = StreetOfSteel;
