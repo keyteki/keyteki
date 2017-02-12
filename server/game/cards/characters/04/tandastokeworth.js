0c6064e4acb12845647d794b1cae465cad80748a
DukeTax <DukeTax@users.noreply.github.com>
2017-02-12 10:56:24 +0100

    Implement Tanda Stokeworth (#338)

---
+++ server/game/cards/characters/04/tandastokeworth.js
@@ -0,0 +1,23 @@
+const _ = require('underscore');
+const DrawCard = require('../../../drawcard.js');
+
+class TandaStokeworth extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: (e, card) => card === this && this.game.currentPhase === 'marshal'
+            },
+            handler: () => {
+                _.each(this.game.getPlayers(), player => {
+                    player.gold += 3;
+                });
+
+                this.game.addMessage('{0} uses {1} to have each player gain three gold', this.controller, this);
+            }
+        });
+    }
+}
+
+TandaStokeworth.code = '04069';
+
+module.exports = TandaStokeworth;
