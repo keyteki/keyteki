c2e9e5ca69dbcf561c13d221ecad49b527df06a4
DukeTax <DukeTax@users.noreply.github.com>
2017-02-12 10:57:33 +0100

    Allow Daenerys to be knelt for Dracarys! (#340)

--- server/game/cards/events/01/dracarys.js
+++ server/game/cards/events/01/dracarys.js
@@ -17,7 +17,7 @@ class Dracarys extends DrawCard {
         this.game.promptForSelect(player, {
             activePromptTitle: 'Select a character to kneel',
             waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
-            cardCondition: card => card.location === 'play area' && card.hasTrait('Dragon') && !card.kneeled,
+            cardCondition: card => card.location === 'play area' && (card.hasTrait('Dragon') || card.name === 'Daenerys Targaryen') && !card.kneeled,
             onSelect: (player, card) => this.onCardSelected(player, card)
         });
     }
