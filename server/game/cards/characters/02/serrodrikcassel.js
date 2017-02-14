5715c89e09641d74960b3073e5cff95de198d440
DukeTax <DukeTax@users.noreply.github.com>
2017-02-14 08:16:13 +0100

    Implement Ser Rodrik Cassel (#361)

---
+++ server/game/cards/characters/02/serrodrikcassel.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../../drawcard.js');
+
+class SerRodrikCassel extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition:(
+                this.game.currentChallenge &&
+                this.game.currentChallenge.challengeType === 'military' &&
+                this.game.currentChallenge.isAttacking(this)
+            ),
+            match: card => card.isUnique() && card.getFaction() === this.getFaction(),
+            effect: ability.effects.addKeyword('Insight')
+        });
+    }
+}
+
+SerRodrikCassel.code = '02041';
+
+module.exports = SerRodrikCassel;
