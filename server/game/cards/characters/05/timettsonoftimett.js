412497be032b3d405fab71045ea7d1ad0e5eca63
DukeTax <DukeTax@users.noreply.github.com>
2017-02-16 23:12:58 +0100

    Implement Timett Son of Timett (#378)
    * Implement Timett Son of Timett

---
+++ server/game/cards/characters/05/timettsonoftimett.js
@@ -0,0 +1,38 @@
+const DrawCard = require('../../../drawcard.js');
+
+class TimettSonOfTimett extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: (event, challenge) => (
+                    challenge.winner === this.controller && 
+                    challenge.isAttacking(this))
+            },
+            handler: () => {
+                this.game.promptForSelect(this.controller, {
+                    cardCondition: card => (
+                        card.location === 'play area' && 
+                        card.getType() === 'character' && 
+                        card.getCost() <= this.getNumberOfClansmen()),
+                    activePromptTitle: 'Select character to kill',
+                    waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
+                    onSelect: (player, card) => (
+                        card.controller.killCard(card),
+                        this.game.addMessage('{0} uses {1} to kill {2}', this.controller, this, card))
+                });
+            }
+        });
+    }
+
+    getNumberOfClansmen() {
+        var cards = this.controller.cardsInPlay.filter(card => {
+            return card.hasTrait('Clansman') && card.getType() === 'character';
+        });
+
+        return cards.length;
+    }
+}
+
+TimettSonOfTimett.code = '05004';
+
+module.exports = TimettSonOfTimett;
