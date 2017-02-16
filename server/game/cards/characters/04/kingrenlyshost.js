f3b39474df584d20d0bc455d56b8f12a76db40a0
Stefano Zacchiroli <zack@upsilon.cc>
2017-02-16 13:33:39 +0100

    Implement King Renly's Host (#390)

---
+++ server/game/cards/characters/04/kingrenlyshost.js
@@ -0,0 +1,30 @@
+const _ = require('underscore');
+
+const DrawCard = require('../../../drawcard.js');
+
+class KingRenlySHost extends DrawCard {
+
+    setupCardAbilities(ability) {
+        this.persistentEffect({  // STR increase while Summer
+            condition: () => this.anyPlotHasTrait('Summer'),
+            match: this,
+            effect: ability.effects.modifyStrength(4),
+        });
+        this.persistentEffect({  // cannot attack while Winter
+            condition: () => this.anyPlotHasTrait('Winter'),
+            match: this,
+            effect: ability.effects.allowAsAttacker(false),
+        });
+    }
+
+    anyPlotHasTrait(trait) {
+        return _.any(this.game.getPlayers(), player =>
+                     player.activePlot
+                     && player.activePlot.hasTrait(trait));
+    }
+
+}
+
+KingRenlySHost.code = '04063';
+
+module.exports = KingRenlySHost;
