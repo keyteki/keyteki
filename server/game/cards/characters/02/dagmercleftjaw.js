05a1974feef9153a8ad45d498883026084d318d5
DukeTax <DukeTax@users.noreply.github.com>
2017-02-16 13:30:51 +0100

    Implement Dagmer Cleftjaw (#380)

---
+++ server/game/cards/characters/02/dagmercleftjaw.js
@@ -0,0 +1,39 @@
+const DrawCard = require('../../../drawcard.js');
+
+class DagmerCleftjaw extends DrawCard {
+    setupCardAbilities() {
+        this.interrupt({
+            when: {
+                onClaimApplied: (event, challenge) => (
+                    challenge.winner === this.controller &&
+                    challenge.isAttacking(this) &&
+                    challenge.attackers.length === 1)
+            },
+            handler: context => {
+                context.skipHandler();
+                this.game.promptForSelect(this.controller, {
+                    activePromptTitle: 'Select a location',
+                    waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
+                    cardCondition: card => (
+                        card.location === 'play area' &&
+                        card.getType() === 'location' &&
+                        card.getCost() <= 3 &&
+                        !card.isLimited() &&
+                        card.controller !== this.controller),
+                    onSelect: (p, card) => this.onCardSelected(p, card)
+                });
+            }
+        });
+    }
+
+    onCardSelected(player, card) {
+        this.game.takeControl(player, card);
+        this.game.addMessage('{0} uses {1} to take control of {2} instead of normal claim effects', player, this, card);
+
+        return true;
+    }
+}
+
+DagmerCleftjaw.code = '02111';
+
+module.exports = DagmerCleftjaw;
