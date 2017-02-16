3bbacddf465c61e732ca0075a03324e0a4b961b5
Brian Upton <ystros@gmail.com>
2017-02-16 04:31:15 -0800

    Cersei should only raise claim when attacking (#386)

--- server/game/cards/characters/01/cerseilannister.js
+++ server/game/cards/characters/01/cerseilannister.js
@@ -1,22 +1,16 @@
 const DrawCard = require('../../../drawcard.js');
 
 class CerseiLannister extends DrawCard {
-    constructor(owner, cardData) {
-        super(owner, cardData);
-
-        this.registerEvents(['onAttackersDeclared']);
-    }
-
-    onAttackersDeclared(event, challenge) {
-        var player = challenge.attackingPlayer;
-        if(challenge.challengeType !== 'intrigue' || player !== this.controller) {
-            return;
-        }
-
-        this.untilEndOfChallenge(ability => ({
-            match: this.controller.activePlot,
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => (
+                this.game.currentChallenge &&
+                this.game.currentChallenge.challengeType === 'intrigue' &&
+                this.game.currentChallenge.isAttacking(this)
+            ),
+            match: card => card === this.controller.activePlot,
             effect: ability.effects.modifyClaim(1)
-        }));
+        });
     }
 }
 
