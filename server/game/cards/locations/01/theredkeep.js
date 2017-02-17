9644ac66e17f678bb784ced1f65fc0a3fe65abb7
Stuart Walsh <stu@ipng.org.uk>
2017-02-17 19:12:31 +0000

    partially move the red keep to the effects engine

--- server/game/cards/locations/01/theredkeep.js
+++ server/game/cards/locations/01/theredkeep.js
@@ -4,7 +4,21 @@ class TheRedKeep extends DrawCard {
     constructor(owner, cardData) {
         super(owner, cardData);
 
-        this.registerEvents(['onAttackersDeclared', 'onDefendersDeclared', 'onPhaseEnded']);
+        this.registerEvents(['onAttackersDeclared', 'onDefendersDeclared']);
+    }
+
+    setupCardAbilities() {
+        this.interrupt({
+            when: {
+                onPhaseEnded: (event, phase) => phase === 'challenge' && !this.kneeled &&
+                    this.controller.getNumberOfChallengesLost('power') === 0
+            },
+            handler: () => {
+                this.game.addMessage('{0} kneels {1} to draw 2 cards', this.controller, this);
+
+                this.controller.drawCardsToHand(2);
+            }
+        });
     }
 
     onAttackersDeclared(event, challenge) {
@@ -23,46 +37,6 @@ class TheRedKeep extends DrawCard {
 
         challenge.modifyDefenderStrength(2);
         this.game.addMessage('{0} uses {1} to add 2 to the strength of this {2} challenge', this.controller, this, challenge.challengeType);
-    }
-
-    onPhaseEnded(event, phase) {
-        if(phase !== 'challenge' || this.kneeled || this.isBlank()) {
-            return;
-        }
-
-        if(this.controller.getNumberOfChallengesLost('power') === 0) {
-            this.game.promptWithMenu(this.controller, this, {
-                activePrompt: {
-                    menuTitle: 'Kneel ' + this.name + '?',
-                    buttons: [
-                        { text: 'Yes', method: 'drawTwo' },
-                        { text: 'No', method: 'cancel' }
-                    ]
-                },
-                waitingPromptTitle: 'Waiting for opponent to use ' + this.name
-            });
-        }
-    }
-
-    drawTwo(player) {
-        if(this.isBlank() || this.controller !== player) {
-            return false;
-        }
-
-        this.game.addMessage('{0} kneels {1} to draw 2 cards', player, this);
-
-        player.drawCardsToHand(2);
-
-        return true;
-    }
-
-    cancel(player) {
-        if(this.isBlank() || this.controller !== player) {
-            return false;
-        }
-
-        this.game.addMessage('{0} declines to trigger {1}', player, this);
-        return true;
     }
 }
 
