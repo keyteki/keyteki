fa65162a5c024187501f37b1684a226ce0694869
Kevin Cavnar-Johnson <cavnak@gmail.com>
2017-02-17 12:50:31 -0600

    Feature/unsworn apprentice (#372)
    * implement unsworn apprentice

---
+++ server/game/cards/characters/02/unswornapprentice.js
@@ -0,0 +1,43 @@
+const _ = require('underscore');
+const DrawCard = require('../../../drawcard.js');
+
+class UnswornApprentice extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Gain Icon',
+            method: 'gainIcon',
+            phase: 'challenge',
+            limit: ability.limit.perPhase(1)
+        });
+    }
+
+    gainIcon() {
+        if(this.location !== 'play area') {
+            return false;
+        }
+        var icons = ['Military', 'Intrigue', 'Power'];
+        var buttons = _.map(icons, icon => {
+            return { text: icon, method: 'iconSelected', arg: icon.toLowerCase() };
+        });
+        this.game.promptWithMenu(this.controller, this, {
+            activePrompt: {
+                menuTitle: 'Select an icon to gain',
+                buttons: buttons
+            },
+            waitingPromptTitle: 'Waiting for opponent to use ' + this.name
+        });
+        return true;
+    }
+
+    iconSelected(player, icon) {
+        this.untilEndOfPhase(ability => ({
+            match: this,
+            effect: ability.effects.addIcon(icon)
+        }));
+        return true;
+    }
+}
+
+UnswornApprentice.code = '02025';
+
+module.exports = UnswornApprentice;
