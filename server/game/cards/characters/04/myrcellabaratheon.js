ab753435662730e22a4409d10ca92b91d43ddd8e
DukeTax <DukeTax@users.noreply.github.com>
2017-02-19 10:16:54 +0100

    Implement Myrcella Baratheon (#400)
    * Implement Myrcella Baratheon

---
+++ server/game/cards/characters/04/myrcellabaratheon.js
@@ -0,0 +1,31 @@
+const _ = require('underscore');
+const DrawCard = require('../../../drawcard.js');
+
+class MyrcellaBaratheon extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => (
+                this.areNoKingsInPlay() &&
+                this.game.currentChallenge &&
+                this.game.currentChallenge.challengeType === 'power'),
+            match: this,
+            effect: ability.effects.doesNotKneelAsDefender()
+        });
+
+        this.persistentEffect({
+            condition: () => this.areNoKingsInPlay(),
+            match: this,
+            effect: ability.effects.addKeyword('Renown')
+        });
+    }
+
+    areNoKingsInPlay() {
+        return !_.any(this.game.getPlayers(), player => {
+            return player.cardsInPlay.any(card => card.getType() === 'character' && card.hasTrait('King'));
+        });
+    }
+}
+
+MyrcellaBaratheon.code = '04095';
+
+module.exports = MyrcellaBaratheon;
