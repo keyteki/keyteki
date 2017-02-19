c107e98b04d4ff17f3b2389040f6a11a67ca329a
Brian Upton <ystros@gmail.com>
2017-02-19 01:28:36 -0800

    Implement The Greenblood (#421)

---
+++ server/game/cards/locations/04/thegreenblood.js
@@ -0,0 +1,22 @@
+const _ = require('underscore');
+
+const DrawCard = require('../../../drawcard.js');
+
+class TheGreenblood extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: card => card.getType() === 'character' && card.getFaction() === this.getFaction(),
+            effect: ability.effects.dynamicStrength(() => this.numOfSummerPlotsRevealed())
+        });
+    }
+
+    numOfSummerPlotsRevealed() {
+        var plots = _.filter(this.game.getPlayers(), player => player.activePlot && player.activePlot.hasTrait('Summer'));
+
+        return plots.length;
+    }
+}
+
+TheGreenblood.code = '04116';
+
+module.exports = TheGreenblood;
