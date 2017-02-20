2ea887d63142cc4fcf6f7688d5e9dad2d84e26ff
DukeTax <DukeTax@users.noreply.github.com>
2017-02-20 09:47:58 +0100

    Implement Arya Stark [WotN] (#426)
    * Implement Arya Stark

---
+++ server/game/cards/characters/03/aryastark.js
@@ -0,0 +1,36 @@
+const DrawCard = require('../../../drawcard.js');
+
+class AryaStark extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCharacterKilled: (event, player, card) => (
+                    this.controller === card.controller &&
+                    card.getFaction() === this.getFaction())
+            },
+            cost: ability.costs.sacrificeSelf(),
+            handler: () => {
+                this.game.promptForSelect(this.controller, {
+                    cardCondition: card => (
+                        card.location === 'play area' && 
+                        card.getType() === 'character' &&
+                        card.getStrength() <= 3),
+                    activePromptTitle: 'Select a character',
+                    waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
+                    onSelect: (player, card) => this.onCardSelected(player, card)
+                });
+            }
+        });
+    }
+
+    onCardSelected(player, card) {
+        card.controller.killCharacter(card);
+        this.game.addMessage('{0} sacrifices {1} to kill {2}', player, this, card);
+
+        return true;
+    }
+}
+
+AryaStark.code = '03007';
+
+module.exports = AryaStark;
