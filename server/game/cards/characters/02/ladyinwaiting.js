b5b7977f8f80a25a962d45d7ad745def4852bbaf
Kevin Cavnar-Johnson <cavnak@gmail.com>
2017-02-20 02:46:43 -0600

    (partial) Implement lady in waiting as reaction (#408)
    * Implement lady in waiting as reaction

---
+++ server/game/cards/characters/02/ladyinwaiting.js
@@ -0,0 +1,34 @@
+const DrawCard = require('../../../drawcard.js');
+
+class LadyInWaiting extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: (event, card) => card === this && this.game.currentPhase === 'marshal' && this.controller.gold >= 2
+            },
+            handler: () => {
+                this.marshalAsDupe();
+            }
+        });
+    }
+    marshalAsDupe() {
+        this.controller.game.promptForSelect(this.controller, {
+            activePromptTitle: 'Select a character',
+            waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
+            cardCondition: card => card.location === 'play area' && card.getType() === 'character' && card.hasTrait('Lady'),
+            onSelect: (p, card) => this.onCardSelected(p, card)
+        });
+        return true;
+    }
+    onCardSelected(player, card) {
+        this.controller.removeCardFromPile(this);
+        card.addDuplicate(this);
+        this.controller.gold += 2;
+        this.game.addMessage('{0} places {1} on {2} as a duplicate', this.controller, this, card);
+        return true;
+    }
+}
+
+LadyInWaiting.code = '02023';
+
+module.exports = LadyInWaiting;
