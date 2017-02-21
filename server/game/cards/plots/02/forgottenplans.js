548be89bfecdc4e0cdd077049d8ada135ee2c14d
Stuart Walsh <stu@ipng.org.uk>
2017-02-21 14:54:59 +0000

    implement forgotten plans

---
+++ server/game/cards/plots/02/forgottenplans.js
@@ -0,0 +1,16 @@
+const PlotCard = require('../../../plotcard.js');
+
+class ForgottenPlans extends PlotCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.game.phase !== 'plot',
+            match: card => !card.hasTrait('Scheme'),
+            targetController: 'any',
+            effect: ability.effects.blank
+        });
+    }
+}
+
+ForgottenPlans.code = '02119';
+
+module.exports = ForgottenPlans;
