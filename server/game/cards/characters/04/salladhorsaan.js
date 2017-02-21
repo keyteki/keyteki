67dc5eea9eeda82eac88635098aa7451f1337361
DukeTax <DukeTax@users.noreply.github.com>
2017-02-21 10:12:16 +0100

    Implement Salladhor Saan (#434)
    * Implement Salladhor Saan

---
+++ server/game/cards/characters/04/salladhorsaan.js
@@ -0,0 +1,34 @@
+const DrawCard = require('../../../drawcard.js');
+
+class SalladhorSaan extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: (event, challenge) => (
+                    challenge.winner === this.controller &&
+                    challenge.isParticipating(this))
+            },
+            handler: () => {
+                this.game.promptForSelect(this.controller, {
+                    activePromptTitle: 'Select a card to put into play',
+                    waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
+                    cardCondition: card => (
+                        card.location === 'hand' &&
+                        card.controller === this.controller &&
+                        ((card.hasTrait('Warship') && card.getType() === 'location') ||
+                        (card.hasTrait('Weapon') && card.getType() === 'attachment'))),
+                    onSelect: (player, card) => {
+                        player.playCard(card, true);
+                        this.game.addMessage('{0} uses {1} to put {2} into play', this.controller, this, card);
+                        
+                        return true;
+                    }
+                });
+            }
+        });
+    }
+}
+
+SalladhorSaan.code = '04107';
+
+module.exports = SalladhorSaan;
