de1b8f01c8aecf163ae37666f7914ee3ccff9639
Stuart Walsh <stu@ipng.org.uk>
2017-02-24 08:58:32 +0000

    fix tests

--- test/server/card/drawcard.modifyPower.spec.js
+++ test/server/card/drawcard.modifyPower.spec.js
@@ -6,7 +6,7 @@ const DrawCard = require('../../../server/game/drawcard.js');
 describe('DrawCard', function () {
     beforeEach(function () {
         this.testCard = { code: '111', label: 'test 1(some pack)', name: 'test 1' };
-        this.gameSpy = jasmine.createSpyObj('game', ['raiseEvent']);
+        this.gameSpy = jasmine.createSpyObj('game', ['raiseEvent', 'checkWinCondition']);
         this.card = new DrawCard({ game: this.gameSpy }, this.testCard);
     });
 
