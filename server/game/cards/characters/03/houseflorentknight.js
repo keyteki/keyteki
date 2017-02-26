93a3e9fd6fac146f0c21dc1ec4bf6d1e1cc8d874
Kevin Cavnar-Johnson <cavnak@gmail.com>
2017-02-26 06:47:41 -0600

    House florent knight (#469)
    * House florent knight
    
    * Fix spacing eslint missed. this time it really was eslints fault!

---
+++ server/game/cards/characters/03/houseflorentknight.js
@@ -0,0 +1,43 @@
+const DrawCard = require('../../../drawcard.js');
+const _ = require('underscore');
+
+class HouseFlorentKnight extends DrawCard {
+    setupCardAbilities() {
+        this.forcedReaction({
+            when: {
+                onCardEntersPlay: (event, card) => card === this
+            },
+            handler: () => {
+                this.game.promptForSelect(this.controller, {
+                    activePromptTitle: 'Select a character with the lowest strength in play',
+                    waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
+                    cardCondition: card => {
+                        return card.getStrength() === this.getLowestStrInPlay() && card.location === 'play area';
+                    },
+                    onSelect: (player, card) => {
+                        player.discardCard(card);
+                        return true;
+                    }
+                });
+            }
+        });
+    }
+
+    getLowestStrInPlay() {
+        var currentMin;
+        _.each(this.game.getPlayers(), (player) => {
+            var playerMin = player.cardsInPlay.min(card => {
+                if(card.getType() === 'character') {
+                    return card.getStrength();
+                }          
+            }).getStrength();
+            if(!currentMin || playerMin < currentMin) {
+                currentMin = playerMin;
+            }
+        });
+        return currentMin;
+    }
+}
+HouseFlorentKnight.code = '03037';
+
+module.exports = HouseFlorentKnight;
