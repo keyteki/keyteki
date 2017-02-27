f8e0c1aa317ddd9df73d147161f8f7d3d753fe26
Stuart Walsh <stu@ipng.org.uk>
2017-02-27 08:21:22 +0000

    Update release and news

--- client/index.prod.jsx
+++ client/index.prod.jsx
@@ -7,7 +7,7 @@ import configureStore from './configureStore';
 import {navigate, login} from './actions';
 import 'bootstrap/dist/js/bootstrap';
 
-Raven.config('https://f5286cd580bf46898e7180c7a46de2f6@sentry.io/123019', { release: '2017-02-24-2'}).install();
+Raven.config('https://f5286cd580bf46898e7180c7a46de2f6@sentry.io/123019', { release: '2017-02-27'}).install();
 
 const store = configureStore();
 
