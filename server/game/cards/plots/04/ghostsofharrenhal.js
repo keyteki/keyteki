a9951fc115b8b8c221951b94e94cebe7646f2719
Stefano Zacchiroli <zack@upsilon.cc>
2017-03-03 08:20:37 +0100

    implement Ghosts of Harrenhal (#518)

---
+++ server/game/cards/plots/04/ghostsofharrenhal.js
@@ -0,0 +1,28 @@
+const _ = require('underscore');
+
+const PlotCard = require('../../../plotcard.js');
+
+class GhostsOfHarrenhal extends PlotCard {
+
+    setupCardAbilities() {
+        this.whenRevealed({
+            handler: () => {
+                _.each(this.game.getPlayersInFirstPlayerOrder(), player => {
+                    var deadCharacters = player.findCards(player.deadPile, card => card.getType() === 'character');
+                    if(deadCharacters) {
+                        var lastDeadCharacter = _.last(deadCharacters);
+                        player.playCard(lastDeadCharacter, true);
+
+                        this.game.addMessage('{0} uses {1} to put {2} into play from {3}\'s dead pile',
+                                             this.controller, this, lastDeadCharacter, lastDeadCharacter.controller);
+                    }
+                });
+            }
+        });
+    }
+
+}
+
+GhostsOfHarrenhal.code = '04100';
+
+module.exports = GhostsOfHarrenhal;
