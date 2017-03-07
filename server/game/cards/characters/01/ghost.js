286f20a9675a4ff604514620f02f33d14d75ba18
DukeTax <DukeTax@users.noreply.github.com>
2017-03-07 13:37:03 +0100

    Implement Ghost (#538)

---
+++ server/game/cards/characters/01/ghost.js
@@ -0,0 +1,28 @@
+const DrawCard = require('../../../drawcard.js');
+
+class Ghost extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onBypassedByStealth: (event, challenge, source, target) => {
+                    if(source !== this) {
+                        return false;
+                    }
+                    this.bypassed = target;
+                    return true;
+                }
+            },
+            handler: () => {
+                this.game.addMessage('{0} uses {1} to make {2} unable to be declared as a defender', this.controller, this, this.bypassed);
+                this.untilEndOfPhase(ability => ({
+                    match: this.bypassed,
+                    effect: ability.effects.allowAsDefender(false)
+                }));
+            }
+        });
+    }
+}
+
+Ghost.code = '01123';
+
+module.exports = Ghost;
