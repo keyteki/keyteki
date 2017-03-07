de8ae2184e490819ed1b5880eca96cc7c4995e1f
DukeTax <DukeTax@users.noreply.github.com>
2017-03-07 13:43:16 +0100

    Implement Eastwatch-by-the-sea (#548)

---
+++ server/game/cards/locations/06/eastwatch-by-the-sea.js
@@ -0,0 +1,33 @@
+const DrawCard = require('../../../drawcard.js');
+
+class EastwatchByTheSea extends DrawCard {
+    setupCardAbilities() {
+        this.plotModifiers({
+            reserve: 1
+        });
+
+        this.reaction({
+            when: {
+                onPhaseStarted: (event, phase) => phase === 'dominance'
+            },
+            condition: () => this.hasHigherReserveThanOpponent(),
+            handler: () => {
+                this.controller.drawCardsToHand(1);
+                this.game.addMessage('{0} uses {1} to draw 1 card', this.controller, this);
+            }
+        });
+    }
+
+    hasHigherReserveThanOpponent() {
+        var otherPlayer = this.game.getOtherPlayer(this.controller);
+        if(!otherPlayer) {
+            return false;
+        }
+
+        return this.controller.getTotalReserve() > otherPlayer.getTotalReserve();
+    }
+}
+
+EastwatchByTheSea.code = '06006';
+
+module.exports = EastwatchByTheSea;
