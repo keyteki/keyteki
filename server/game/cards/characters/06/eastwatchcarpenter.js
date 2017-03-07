3224739c2ffcb063c4b08afb2095f3b7f2affabd
DukeTax <DukeTax@users.noreply.github.com>
2017-03-07 13:42:22 +0100

    Implement Eastwatch Carpenter (#547)

---
+++ server/game/cards/characters/06/eastwatchcarpenter.js
@@ -0,0 +1,29 @@
+const DrawCard = require('../../../drawcard.js');
+
+class EastwatchCarpenter extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onIncomeCollected: (event, player) => player === this.controller
+            },
+            handler: () => {
+                var gold = Math.floor(this.numberOfNWLocations() / 2);
+                this.game.addGold(this.controller, gold);
+
+                this.game.addMessage('{0} uses {1} to gain {2} gold', this.controller, this, gold);
+            }
+        });
+    }
+
+    numberOfNWLocations() {
+        var cards = this.controller.cardsInPlay.filter(card => {
+            return card.isFaction('thenightswatch') && card.getType() === 'location';
+        });
+
+        return cards.length;
+    }
+}
+
+EastwatchCarpenter.code = '06005';
+
+module.exports = EastwatchCarpenter;
