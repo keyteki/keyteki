9a979ee02a9dcfa061c3752d12079dea8ecd2536
Brian Upton <ystros@gmail.com>
2017-03-15 03:53:14 -0700

    Apply Wraiths in their Midst to Rains plots. (#592)
    Previously, Wraiths in their Midst was not being applied to plots
    revealed by The Rains of Castamere. This has been resolved by converting
    it into a persistent effect.

--- server/game/cards/plots/02/wraithsintheirmidst.js
+++ server/game/cards/plots/02/wraithsintheirmidst.js
@@ -1,31 +1,17 @@
 const PlotCard = require('../../../plotcard.js');
 
 class WraithsInTheirMidst extends PlotCard {
-    constructor(owner, cardData) {
-        super(owner, cardData);
-
-        this.registerEvents(['onPhaseEnded']);
-    }
-
-    onPhaseEnded(event, phase) {
-        if(phase !== 'plot') {
-            return;
-        }
-
-        var otherPlayer = this.game.getOtherPlayer(this.controller);
-        if(!otherPlayer) {
-            return;
-        }
-
-        this.untilEndOfRound(ability => ({
-            match: otherPlayer.activePlot,
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            targetController: 'opponent',
+            match: card => card === card.controller.activePlot,
             effect: ability.effects.modifyReserve(-2)
-        }));
-        this.untilEndOfRound(ability => ({
+        });
+        this.persistentEffect({
             targetType: 'player',
             targetController: 'opponent',
             effect: ability.effects.setMinReserve(2)
-        }));
+        });
     }
 }
 
