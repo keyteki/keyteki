feed41b77a31bf3565f6ed93ecf453812646fef5
Brian Upton <ystros@gmail.com>
2017-03-26 01:06:42 -0700

    Convert King Balon's Solar to ability API. (#680)

--- server/game/cards/locations/04/kingbalonssolar.js
+++ server/game/cards/locations/04/kingbalonssolar.js
@@ -1,41 +1,16 @@
 const DrawCard = require('../../../drawcard.js');
 
 class KingBalonsSolar extends DrawCard {
-    constructor(owner, cardData) {
-        super(owner, cardData);
-
-        this.registerEvents(['onInitiativeDetermined']);
-    }
-
-    onInitiativeDetermined(event, player) {
-        if(this.controller !== player || this.isBlank()) {
-            return;
-        }
-
-        this.game.promptWithMenu(player, this, {
-            activePrompt: {
-                menuTitle: 'Trigger ' + this.name + '?',
-                buttons: [
-                    { text: 'Gain 1 gold', method: 'gainGold' },
-                    { text: 'Cancel', method: 'cancel' }
-                ]
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onInitiativeDetermined: (event, player) => player === this.controller
             },
-            source: this
+            handler: context => {
+                this.game.addGold(context.player, 1);
+                this.game.addMessage('{0} uses {1} to gain 1 gold', context.player, this);
+            }
         });
-    }
-
-    gainGold(player) {
-        this.game.addGold(player, 1);
-
-        this.game.addMessage('{0} uses {1} to gain 1 gold', player, this);
-
-        return true;
-    }
-
-    cancel(player) {
-        this.game.addMessage('{0} declines to trigger {1}', player, this);
-
-        return true;
     }
 }
 
