6783947e06ceadb1f65a42be390fab425f21d725
DukeTax <DukeTax@users.noreply.github.com>
2017-03-26 10:01:46 +0200

    Implement Salt Wife (#673)

---
+++ server/game/cards/characters/07/saltwife.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../../drawcard.js');
+
+class SaltWife extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Sacrifice Salt Wife',
+            phase: 'challenge',
+            cost: ability.costs.sacrificeSelf(),
+            target: {
+                activePromptTitle: 'Select a character',
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character'
+            },
+            handler: context => {
+                this.untilEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.allowAsDefender(false)
+                }));
+                this.game.addMessage('{0} sacrifices {1} to make {2} unable to be declared as a defender', 
+                                      context.player, this, context.target);
+            }
+        });
+    }
+}
+
+SaltWife.code = '07027';
+
+module.exports = SaltWife;
