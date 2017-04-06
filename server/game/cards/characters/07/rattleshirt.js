ce9086cbf7974f0eeda72e47124713b05594c8bf
Stefano Zacchiroli <zack@upsilon.cc>
2017-04-06 21:08:38 +0200

    fix Rattleshirt check for "no attachments" (#736)
    It should match only characters, controlled by the opponent. The previous check
    ended up on cards that don't have the .attachments property, making Rattleshirt
    backtrace as crazy during challenges.

--- server/game/cards/characters/07/rattleshirt.js
+++ server/game/cards/characters/07/rattleshirt.js
@@ -3,11 +3,14 @@ const DrawCard = require('../../../drawcard.js');
 class Rattleshirt extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
-            condition: () => (
-                this.game.currentChallenge &&
-                this.game.currentChallenge.isAttacking(this) &&
-                this.game.currentChallenge.attackers.length === 1),
-            match: card => card.attachments.size() === 0,
+            condition: () =>
+                this.game.currentChallenge
+                && this.game.currentChallenge.isAttacking(this)
+                && this.game.currentChallenge.attackers.length === 1,
+            match: card =>
+                card.getType() === 'character'
+                && card.attachments.size() === 0,
+            targetController: 'opponent',
             effect: ability.effects.allowAsDefender(false)
         });
     }
