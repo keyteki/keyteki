e165269233d13362b7594099f4df68161b80efdd
Brian Upton <ystros@gmail.com>
2017-04-06 13:36:08 -0700

    Implement Summoned to Court. (#739)

---
+++ server/game/cards/plots/05/summonedtocourt.js
@@ -0,0 +1,102 @@
+const _ = require('underscore');
+
+const PlotCard = require('../../../plotcard.js');
+
+class SummonedToCourt extends PlotCard {
+    setupCardAbilities() {
+        this.forcedInterrupt({
+            when: {
+                onPhaseEnded: (event, phase) => phase === 'draw'
+            },
+            handler: () => {
+                this.remainingPlayers = this.game.getPlayersInFirstPlayerOrder();
+                this.playerChoices = [];
+                this.promptNextPlayerForChoice();
+            }
+        });
+    }
+
+    promptNextPlayerForChoice() {
+        while(this.remainingPlayers.length !== 0) {
+            let currentPlayer = this.remainingPlayers.shift();
+            if(!currentPlayer.hand.isEmpty()) {
+                this.promptPlayerForChoice(currentPlayer);
+                return;
+            }
+
+            this.game.addMessage('{0} does not have any cards to be revealed by {1}', currentPlayer, this);
+        }
+
+        this.revealPlayerChoices();
+        this.validChoices = this.getLowestCostChoices();
+        this.promptNextPlayerToPutIntoPlay();
+    }
+
+    promptPlayerForChoice(player) {
+        this.game.promptForSelect(player, {
+            activePromptTitle: 'Choose a card to reveal',
+            source: this,
+            cardCondition: card => card.controller === player && card.location === 'hand',
+            onSelect: (player, card) => this.chooseCard(player, card)
+        });
+    }
+
+    chooseCard(player, card) {
+        this.playerChoices.push({ player: player, card: card, cost: card.getCost() });
+        this.promptNextPlayerForChoice();
+        return true;
+    }
+
+    revealPlayerChoices() {
+        _.each(this.playerChoices, choice => {
+            this.game.addMessage('{0} reveals {1} as their choice for {2}', choice.player, choice.card, this);
+        });
+    }
+
+    getLowestCostChoices() {
+        let characterChoices = _.filter(this.playerChoices, choice => choice.card.getType() === 'character');
+        let minCost = _.min(_.pluck(characterChoices, 'cost'));
+        return _.filter(characterChoices, choice => choice.cost === minCost);
+    }
+
+    promptNextPlayerToPutIntoPlay() {
+        if(this.validChoices.length === 0) {
+            return;
+        }
+
+        let choice = this.validChoices.shift();
+        this.promptPlayerToPutIntoPlay(choice.player, choice.card);
+    }
+
+    promptPlayerToPutIntoPlay(player, card) {
+        this.game.promptWithMenu(player, this, {
+            activePrompt: {
+                menuTitle: 'Put ' + card.name + ' into play?',
+                buttons: [
+                    { text: 'Yes', method: 'putChoiceIntoPlay', arg: card.uuid, card: card.getSummary(true) },
+                    { text: 'No', method: 'declinePutIntoPlay', arg: card.uuid }
+                ]
+            },
+            source: this
+        });
+    }
+
+    putChoiceIntoPlay(player, cardId) {
+        let card = player.findCardByUuidInAnyList(cardId);
+        player.putIntoPlay(card);
+        this.game.addMessage('{0} chooses to put {1} into play using {2}', player, card, this);
+        this.promptNextPlayerToPutIntoPlay();
+        return true;
+    }
+
+    declinePutIntoPlay(player, cardId) {
+        let card = player.findCardByUuidInAnyList(cardId);
+        this.game.addMessage('{0} declines to put {1} into play using {2}', player, card, this);
+        this.promptNextPlayerToPutIntoPlay();
+        return true;
+    }
+}
+
+SummonedToCourt.code = '05048';
+
+module.exports = SummonedToCourt;
---
+++ test/server/cards/plots/05/05048-summonedtocourt.spec.js
@@ -0,0 +1,120 @@
+/* global describe, it, expect, beforeEach, integration */
+/* eslint camelcase: 0, no-invalid-this: 0 */
+
+describe('Summoned to Court', function() {
+    integration(function() {
+        beforeEach(function() {
+            const deck = this.buildDeck('greyjoy', [
+                'Summoned to Court', 'A Noble Cause',
+                'Aeron Damphair (Core)', 'Balon Greyjoy (Core)', 'The Roseroad'
+            ]);
+
+            this.player1.selectDeck(deck);
+            this.player2.selectDeck(deck);
+            this.startGame();
+            this.skipSetupPhase();
+        });
+
+        describe('when one player chooses a character card with lower cost', function() {
+            beforeEach(function() {
+                this.player1.selectPlot('Summoned to Court');
+                this.player2.selectPlot('A Noble Cause');
+                this.selectFirstPlayer(this.player2);
+
+                this.player2.clickCard('Aeron Damphair', 'hand');
+                this.player1.clickCard('Balon Greyjoy', 'hand');
+            });
+
+            it('should prompt only that player to put it into play', function() {
+                expect(this.player2).toHavePrompt('Put Aeron Damphair into play?');
+
+                this.player2.clickPrompt('Yes');
+
+                expect(this.player2.findCardByName('Aeron Damphair').location).toBe('play area');
+
+                expect(this.player1).not.toHavePrompt('Put Balon Greyjoy into play?');
+            });
+        });
+
+        describe('when both players choose a character card with the same cost', function() {
+            beforeEach(function() {
+                this.player1.selectPlot('Summoned to Court');
+                this.player2.selectPlot('A Noble Cause');
+                this.selectFirstPlayer(this.player2);
+
+                this.player2.clickCard('Balon Greyjoy', 'hand');
+                this.player1.clickCard('Balon Greyjoy', 'hand');
+            });
+
+            it('should prompt both players to put it into play', function() {
+                expect(this.player2).toHavePrompt('Put Balon Greyjoy into play?');
+                this.player2.clickPrompt('Yes');
+                expect(this.player2.findCardByName('Balon Greyjoy').location).toBe('play area');
+
+                expect(this.player1).toHavePrompt('Put Balon Greyjoy into play?');
+                this.player1.clickPrompt('Yes');
+                expect(this.player1.findCardByName('Balon Greyjoy').location).toBe('play area');
+            });
+        });
+
+        describe('when one player chooses a character and another a non-character', function() {
+            beforeEach(function() {
+                this.player1.selectPlot('Summoned to Court');
+                this.player2.selectPlot('A Noble Cause');
+                this.selectFirstPlayer(this.player2);
+
+                this.player2.clickCard('The Roseroad', 'hand');
+                this.player1.clickCard('Balon Greyjoy', 'hand');
+            });
+
+            it('should only prompt the player that revealed a character', function() {
+                expect(this.player1).toHavePrompt('Put Balon Greyjoy into play?');
+                this.player1.clickPrompt('Yes');
+                expect(this.player1.findCardByName('Balon Greyjoy').location).toBe('play area');
+            });
+        });
+
+        describe('when one player does not have any cards', function() {
+            beforeEach(function() {
+                this.player2Object.hand.each(card => {
+                    this.player2Object.moveCard(card, 'discard pile');
+                });
+
+                this.player1.selectPlot('Summoned to Court');
+                this.player2.selectPlot('A Noble Cause');
+                this.selectFirstPlayer(this.player2);
+            });
+
+            it('should only prompt the player with cards', function() {
+                expect(this.player1).toHavePrompt('Choose a card to reveal');
+                expect(this.player2).not.toHavePrompt('Choose a card to reveal');
+
+                this.player1.clickCard('Balon Greyjoy', 'hand');
+
+                expect(this.player1).toHavePrompt('Put Balon Greyjoy into play?');
+                this.player1.clickPrompt('Yes');
+                expect(this.player1.findCardByName('Balon Greyjoy').location).toBe('play area');
+            });
+        });
+
+        describe('when neither player has cards', function() {
+            beforeEach(function() {
+                this.player1Object.hand.each(card => {
+                    this.player1Object.moveCard(card, 'discard pile');
+                });
+                this.player2Object.hand.each(card => {
+                    this.player2Object.moveCard(card, 'discard pile');
+                });
+
+                this.player1.selectPlot('Summoned to Court');
+                this.player2.selectPlot('A Noble Cause');
+                this.selectFirstPlayer(this.player2);
+            });
+
+            it('should not prompt either player', function() {
+                expect(this.player1).not.toHavePrompt('Choose a card to reveal');
+                expect(this.player2).not.toHavePrompt('Choose a card to reveal');
+            });
+        });
+    });
+});
