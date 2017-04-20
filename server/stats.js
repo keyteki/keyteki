c7a9d9300f9c8d37cf05a51eee964406e40f26d6
Stuart Walsh <stu@ipng.org.uk>
2017-04-20 12:20:47 +0100

    add debugging to stats

--- server/stats.js
+++ server/stats.js
@@ -20,22 +20,33 @@ console.info('Running stats...');
 gameRepository.getAllGames(args[0], args[1], (err, games) => {
     let winners = {};
     //let factionWinners = {};
+    let rejected = { singlePlayer: 0, noWinner: 0 };
+
+    console.info(_.size(games), 'total games');
 
     _.each(games, game => {
         if(_.size(game.players) !== 2) {
+            rejected.singlePlayer++;
+            console.info(game.startedAt, _.size(game.players));
             return;
         }
 
         if(game.winner) {
             if(winners[game.winner]) {
-                winners[game.winner]++;
+                winners[game.winner].count++;
             } else {
-                winners[game.winner] = 1;
+                winners[game.winner] = { name: game.winner, count: 1 };
             }
+        } else {
+            rejected.noWinner++;
+            console.info(game);
         }
     });
 
+    winners = _.sortBy(winners, item => {
+        return item.count;
+    }).reverse();
+
     console.info(winners);
+    console.info(rejected);
 });
-
-
