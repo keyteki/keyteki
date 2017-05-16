1ba103000370fd0004dfb51206925d30c7ee9f2e
Stuart Walsh <stu@ipng.org.uk>
2017-05-16 10:15:46 +0100

    finish ser alliser thorne

--- server/game/cards/characters/02/seralliserthorne.js
+++ server/game/cards/characters/02/seralliserthorne.js
@@ -7,7 +7,22 @@ class SerAlliserThorne extends DrawCard {
             match: card => card.getType() === 'character' && card.isFaction('thenightswatch'),
             effect: ability.effects.addIcon('military')
         });
-        // TODO: Military ambush ability
+
+        this.reaction({
+            location: 'hand',
+            when: {
+                onChallenge: (event, challenge) => challenge.challengeType === 'military'
+            },
+            cost: [
+                ability.costs.kneelFactionCard(),
+                ability.costs.payGold(4)
+            ],
+            handler: () => {
+                this.controller.putIntoPlay(this);
+
+                this.game.addMessage('{0} uses {1} to put {1} into play from their hand', this.controller, this);
+            }
+        });
     }
 }
 
