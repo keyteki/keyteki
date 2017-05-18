44ef6cb8ed5cad9a32d498c42b795451eb228cdb
Gryffon <ryan@karetas.com>
2017-05-18 10:34:07 -0400

    Update card types

--- server/game/cards/index.js
+++ server/game/cards/index.js
@@ -2,8 +2,9 @@ const _ = require('underscore');
 const path = require('path');
 const fs = require('fs');
 
-const plots = require('./plots');
-const locations = require('./locations');
+const strongholds = require('./strongholds');
+const provinces = require('./provinces');
+const holdings = require('./holdings');
 const characters = require('./characters');
 const attachments = require('./attachments');
 const events = require('./events');
@@ -16,6 +17,6 @@ _.each(fs.readdirSync(path.join(__dirname, 'agendas')), file => {
     cards[card.code] = card;
 });
 
-cards = _.extend(cards, plots, locations, characters, attachments, events);
+cards = _.extend(cards, strongholds, provinces, holdings, characters, attachments, events);
 
 module.exports = cards;
