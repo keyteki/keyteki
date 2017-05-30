15fde9b29e686c342b968c80dba8f39ba07e68fc
Gryffon <ryan@karetas.com>
2017-05-30 17:14:47 -0400

    Update setupphase

--- server/game/gamesteps/setupphase.js
+++ server/game/gamesteps/setupphase.js
@@ -2,32 +2,23 @@ const _ = require('underscore');
 const Phase = require('./phase.js');
 const SimpleStep = require('./simplestep.js');
 const KeepOrMulliganPrompt = require('./setup/keepormulliganprompt.js');
-const SetupCardsPrompt = require('./setup/setupcardsprompt.js');
-const CheckAttachmentsPrompt = require('./setup/checkattachmentsprompt.js');
+const SetupProvincesPrompt = require('./setup/setupprovincesprompt.js');
 
 class SetupPhase extends Phase {
     constructor(game) {
         super(game, 'setup');
         this.initialise([
+            new SetupProvincesPrompt(game),
             new SimpleStep(game, () => this.prepareDecks()),
-            //Setup province prompt
-            //Set out provinces
             //Attach stronghold
             new KeepOrMulliganPrompt(game),
             new SimpleStep(game, () => this.startGame()),
-            new SetupCardsPrompt(game),
             new SimpleStep(game, () => this.setupDone()),
-            new CheckAttachmentsPrompt(game)
         ]);
     }
 
     prepareDecks() {
         this.game.raiseEvent('onDecksPrepared');
-        _.each(this.game.getPlayers(), player => {
-            if(player.agenda) {
-                player.agenda.play();
-            }
-        });
     }
 
     startGame() {
