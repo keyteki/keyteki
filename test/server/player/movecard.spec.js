776530774269c7a3c84abe6e508a8dbdf9a1a9a9
Gryffon <ryan@karetas.com>
2017-05-30 10:28:43 -0400

    update unit tests

--- test/server/player/movecard.spec.js
+++ test/server/player/movecard.spec.js
@@ -11,7 +11,7 @@ describe('Player', function() {
             this.gameSpy = jasmine.createSpyObj('game', ['raiseEvent', 'raiseMergedEvent', 'getOtherPlayer', 'playerDecked']);
             this.player = new Player('1', 'Player 1', true, this.gameSpy);
             this.player.initialise();
-            this.player.phase = 'marshal';
+            this.player.phase = 'dynasty';
 
             this.gameSpy.raiseMergedEvent.and.callFake((name, params, handler) => {
                 if(handler) {
@@ -33,16 +33,16 @@ describe('Player', function() {
                 expect(this.card.location).toBe('hand');
             });
 
-            it('should add the card to the player discard pile', function() {
-                this.player.moveCard(this.card, 'discard pile');
-                expect(this.player.discardPile).toContain(this.card);
-                expect(this.card.location).toBe('discard pile');
+            it('should add the card to the player conflict discard pile', function() {
+                this.player.moveCard(this.card, 'conflict discard pile');
+                expect(this.player.conflictDiscardPile).toContain(this.card);
+                expect(this.card.location).toBe('conflict discard pile');
             });
 
-            it('should add the card to the player dead pile', function() {
-                this.player.moveCard(this.card, 'dead pile');
-                expect(this.player.deadPile).toContain(this.card);
-                expect(this.card.location).toBe('dead pile');
+            it('should add the card to the player dynasty discard pile', function() {
+                this.player.moveCard(this.card, 'dynasty discard pile');
+                expect(this.player.dynastyDiscardPile).toContain(this.card);
+                expect(this.card.location).toBe('dynasty discard pile');
             });
 
             it('should add the card to the player play area', function() {
@@ -54,8 +54,8 @@ describe('Player', function() {
 
         describe('when the card is in a non-play-area pile', function() {
             beforeEach(function() {
-                this.player.discardPile.push(this.card);
-                this.card.location = 'discard pile';
+                this.player.conflictDiscardPile.push(this.card);
+                this.card.location = 'conflict discard pile';
 
                 this.player.moveCard(this.card, 'hand');
             });
@@ -65,7 +65,7 @@ describe('Player', function() {
             });
 
             it('should remove it from the original pile', function() {
-                expect(this.player.discardPile).not.toContain(this.card);
+                expect(this.player.conflictDiscardPile).not.toContain(this.card);
             });
 
             it('should not make the card leave play', function() {
@@ -84,12 +84,12 @@ describe('Player', function() {
             });
 
             it('should make the card leave play', function() {
-                this.player.moveCard(this.card, 'dead pile');
+                this.player.moveCard(this.card, 'conflict discard pile');
                 expect(this.card.leavesPlay).toHaveBeenCalled();
             });
 
             it('should raise the left play event', function() {
-                this.player.moveCard(this.card, 'dead pile');
+                this.player.moveCard(this.card, 'conflict discard pile');
                 expect(this.gameSpy.raiseMergedEvent).toHaveBeenCalled();
             });
 
@@ -130,42 +130,29 @@ describe('Player', function() {
                 });
             });
 
-            describe('when the card has duplicates', function() {
-                beforeEach(function() {
-                    this.dupe = new DrawCard(this.player, {});
-                    this.card.addDuplicate(this.dupe);
-
-                    this.player.moveCard(this.card, 'hand');
-                });
-
-                it('should discard the dupes', function() {
-                    expect(this.player.discardPile).toContain(this.dupe);
-                    expect(this.dupe.location).toBe('discard pile');
-                });
-            });
         });
 
-        describe('when the target location is the draw deck', function() {
+        describe('when the target location is the conflict deck', function() {
             beforeEach(function() {
-                this.player.drawDeck = _([{}, {}, {}]);
+                this.player.conflictDeck = _([{}, {}, {}]);
             });
 
             it('should add the card to the top of the deck', function() {
-                this.player.moveCard(this.card, 'draw deck');
-                expect(this.player.drawDeck.first()).toBe(this.card);
+                this.player.moveCard(this.card, 'conflict deck');
+                expect(this.player.conflictDeck.first()).toBe(this.card);
             });
 
             it('should add the card to the bottom of the deck when the option is passed', function() {
-                this.player.moveCard(this.card, 'draw deck', { bottom: true });
-                expect(this.player.drawDeck.last()).toBe(this.card);
+                this.player.moveCard(this.card, 'conflict deck', { bottom: true });
+                expect(this.player.conflictDeck.last()).toBe(this.card);
             });
 
             it('should be able to move a card from top to bottom of the deck', function() {
-                this.player.drawDeck = _([this.card, {}, {}, {}]);
-                this.card.location = 'draw deck';
-                this.player.moveCard(this.card, 'draw deck', { bottom: true });
-                expect(this.player.drawDeck.size()).toBe(4);
-                expect(this.player.drawDeck.last()).toBe(this.card);
+                this.player.conflictDeck = _([this.card, {}, {}, {}]);
+                this.card.location = 'conflict deck';
+                this.player.moveCard(this.card, 'conflict deck', { bottom: true });
+                expect(this.player.conflictDeck.size()).toBe(4);
+                expect(this.player.conflictDeck.last()).toBe(this.card);
             });
         });
 
@@ -176,7 +163,7 @@ describe('Player', function() {
             // challenges and dominance.
             beforeEach(function() {
                 // Put into play with the wrong location.
-                this.card.location = 'discard pile';
+                this.card.location = 'conflict discard pile';
                 this.player.cardsInPlay = _([this.card]);
 
                 this.player.moveCard(this.card, 'play area');
@@ -188,11 +175,5 @@ describe('Player', function() {
             });
         });
 
-        describe('when the target location is the active plot', function() {
-            it('should set the card as the active plot', function() {
-                this.player.moveCard(this.card, 'active plot');
-                expect(this.player.activePlot).toBe(this.card);
-            });
-        });
     });
 });
