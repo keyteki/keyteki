9fb8e4ccebe98e12c7de568dfe6fa6b84cbb66b6
Gryffon <ryan@karetas.com>
2017-06-02 16:42:16 -0400

    Add new components

---
+++ client/GameComponents/HonorFan.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import _ from 'underscore';
+
+class HonorFan extends React.Component {
+    constructor() {
+        super();
+
+        this.onButtonClick = this.onButtonClick.bind(this);
+    }
+
+    onButtonClick(event, command, arg, method) {
+        event.preventDefault();
+
+        if(this.props.onButtonClick) {
+            this.props.onButtonClick(command, arg, method);
+        }
+    }
+
+    onMouseOver(event, card) {
+        if(card && this.props.onMouseOver) {
+            this.props.onMouseOver(card);
+        }
+    }
+
+    onMouseOut(event, card) {
+        if(card && this.props.onMouseOut) {
+            this.props.onMouseOut(card);
+        }
+    }
+
+    getButtons() {
+        var buttonIndex = 0;
+
+        var buttons = _.map(this.props.buttons, button => {
+            var option = (
+                <button key={ button.command + buttonIndex.toString() } className='btn btn-primary'
+                    onClick={ event => this.onButtonClick(event, button.command, button.arg, button.method) }
+                    onMouseOver={ event => this.onMouseOver(event, button.card) } onMouseOut={ event => this.onMouseOut(event, button.card) }
+                    disabled={ button.disabled }>{ button.text }</button>);
+
+            buttonIndex++;
+
+            return option;
+        });
+
+        return buttons;
+    }
+
+    render() {
+
+        return (<div>
+            <div className='honor-fan'>
+                <img className='honor-fan-value' src={'/img/honorfan-' + this.props.value +'.png'} />
+            </div>
+        </div>);
+    }
+}
+
+HonorFan.displayName = 'HonorFan';
+HonorFan.propTypes = {
+    buttons: React.PropTypes.array,
+    onButtonClick: React.PropTypes.func,
+    onMouseOut: React.PropTypes.func,
+    onMouseOver: React.PropTypes.func,
+    socket: React.PropTypes.object,
+    value: React.PropTypes.number
+};
+
+export default HonorFan;
