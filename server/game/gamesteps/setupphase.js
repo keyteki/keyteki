fb2fb6fd6d814438218305f7ce34ec575936d0b0
Gryffon <ryan@karetas.com>
2017-06-06 19:52:56 -0400

    add setup phase steps

--- server/game/gamesteps/setupphase.js
+++ server/game/gamesteps/setupphase.js
@@ -10,11 +10,24 @@ class SetupPhase extends Phase {
         this.initialise([
             new SimpleStep(game, () => this.prepareDecks()),
             new SetupProvincesPrompt(game),
-                        //Attach stronghold
+            new SimpleStep(game, () => this.attachStronghold()),
+            //new SimpleStep(game, () => this.fillProvinces()),
             new KeepOrMulliganPrompt(game),
             new SimpleStep(game, () => this.startGame()),
             new SimpleStep(game, () => this.setupDone()),
         ]);
+    }
+
+    attachStronghold() {
+        _.each(this.game.getPlayers(), player => {
+            player.attachStronghold();
+        });
+    }
+
+    fillProvinces() {
+        _.each(this.game.getPlayers(), player => {
+            player.fillProvinces();
+        });
     }
 
     prepareDecks() {
