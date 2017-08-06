ba651277e0674043bbfb3d3908107d3db3cb7ab1
gryffon <ryan@karetas.com>
2017-08-06 16:00:24 -0400

    Update require

--- server/api/account.js
+++ server/api/account.js
@@ -1,5 +1,5 @@
 const logger = require('../log.js');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const config = require('../config.js');
 const jwt = require('jsonwebtoken');
