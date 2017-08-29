dbcbbdf85032f88300e4d82e588c8437ff86efe0
Gryffon <ryan@karetas.com>
2017-08-29 14:02:51 -0400

    Fix server logname

--- server/log.js
+++ server/log.js
@@ -3,7 +3,7 @@ const winston = require('winston');
 const logger = new winston.Logger({
     transports: [
         new winston.transports.Console({ json: false, timestamp: true }),
-        new winston.transports.File({ filename: __dirname + '/logs/eggteki.log', json: false, timestamp: true })
+        new winston.transports.File({ filename: __dirname + '/logs/ringteki.log', json: false, timestamp: true })
     ]
 });
 
