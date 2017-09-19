7664f199e94cae0ac18d75fbc5e584a4fcae914e
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-09-19 15:49:20 +0100

    need a participant to claim

--- server/game/conflict.js
+++ server/game/conflict.js
@@ -111,9 +111,9 @@ class Conflict {
         this.attackerSkill = this.calculateSkillFor(this.attackers) + this.attackerSkillModifier;
         this.defenderSkill = this.calculateSkillFor(this.defenders) + this.defenderSkillModifier;
         
-        if(this.attackingPlayer.imperialFavor === this.conflictType) {
+        if(this.attackingPlayer.imperialFavor === this.conflictType && this.attackers !== []) {
             this.attackerSkill++;
-        } else if(this.defendingPlayer.imperialFavor === this.conflictType) {
+        } else if(this.defendingPlayer.imperialFavor === this.conflictType && this.defenders !== []) {
             this.defenderSkill++;
         }
     }
