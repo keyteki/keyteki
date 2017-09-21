throneteki/throneteki

Fix spectating Don't store messages twice Add log rotating Change username field name to not upset autocorrect

server/log.js
@@ -1,9 +1,18 @@
 const winston = require('winston');
+require('winston-daily-rotate-file');
+
+let rotate = new (winston.transports.DailyRotateFile)({
+    filename: __dirname + '/logs/throneteki',
+    datePattern: '-yyyy-MM-dd.log',
+    timestamp: true,
+    json: false,
+    zippedArchive: true
+});
 
 const logger = new winston.Logger({
     transports: [
         new winston.transports.Console({ json: false, timestamp: true }),
-        new winston.transports.File({ filename: __dirname + '/logs/throneteki.log', json: false, timestamp: true })
+        rotate
     ]
 });
 

keyteki/keyteki

server/log.js
@@ -1,9 +1,18 @@
 const winston = require('winston');
+require('winston-daily-rotate-file');
+
+let rotate = new (winston.transports.DailyRotateFile)({
+    filename: __dirname + '/logs/ringteki',
+    datePattern: '-yyyy-MM-dd.log',
+    timestamp: true,
+    json: false,
+    zippedArchive: true
+});
 
 const logger = new winston.Logger({
     transports: [
         new winston.transports.Console({ json: false, timestamp: true }),
-        new winston.transports.File({ filename: __dirname + '/logs/ringteki.log', json: false, timestamp: true })
+        rotate
     ]
 });
 