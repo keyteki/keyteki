a3044279dd2ab4876e20c8622ad430e6dc64f464
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-09-26 14:14:59 +0100

    miya mystic

--- server/game/cards/characters/01 - Core/MiyaMystic.js
+++ server/game/cards/characters/01 - Core/MiyaMystic.js
@@ -1,7 +1,26 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+const DrawCard = require('../../../drawcard.js');
+
+class MiyaMystic extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Sacrifice to discard an attachment',
+            cost: ability.costs.sacrificeSelf(),
+            phase: 'conflict',
+            target: {
+                activePromptTitle: 'Select an attachment',
+                cardType: 'attachment',
+                cardCondition: card => card.location === 'play area'
+            },
+            handler: context => {
+                this.game.addMessage({0} sacrifices {1} to discard {2}, this.controller, this, context.target);
+                this.controller.removeAttachment(context.target);
+            }
+        })
+    }
+}
+
+MiyaMystic.code = '01125';
+
+module.exports = MiyaMystic;
 
 
