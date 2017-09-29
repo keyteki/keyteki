ced27751b7ff041d89a25e2df7a586e72481282b
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-09-29 14:06:21 +0100

    added message

--- server/game/cards/01 - Core/VengefulOathKeeper.js
+++ server/game/cards/01 - Core/VengefulOathKeeper.js
@@ -7,7 +7,10 @@ class VengefulOathkeeper extends DrawCard {
                 afterConflict: (event, params) => params.conflict.loser === this.controller && params.conflict.conflictType === 'military'
             },
             location: 'hand',
-            handler: () => this.controller.putIntoPlay(this)
+            handler: () => {
+                this.controller.putIntoPlay(this);
+                this.game.addMessage('{0} puts {1} into play from his hand', this.controller, this);
+            }
         });
     }
 }
