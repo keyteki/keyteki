0d1e3a92211e4806e6f8cab77888eddcf86eaae9
John-Waltz <Nothaft.John@gmail.com>
2017-10-01 04:34:20 -0400

    Let Go Implementation and Miya Mystic fix
    Implemented Let Go. Fixed Text output on Miya Mystic

--- server/game/cards/01 - Core/LetGo.js
+++ server/game/cards/01 - Core/LetGo.js
@@ -1,26 +1,24 @@
 const DrawCard = require('../../drawcard.js');
 
-class MiyaMystic extends DrawCard {
+class LetGo extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
-            title: 'Sacrifice to discard an attachment',
-            cost: ability.costs.sacrificeSelf(),
-            phase: 'conflict',
+            title: 'Choose an attachment – discard it.',
             target: {
                 activePromptTitle: 'Select an attachment',
                 cardType: 'attachment',
                 cardCondition: card => card.location === 'play area'
             },
             handler: context => {
-                this.game.addMessage('{0} sacrifices {1} to discard {2}', this.controller, this, context.target);
+                this.game.addMessage('{0} Was discarded', context.target);
                 this.controller.removeAttachment(context.target);
             }
         });
     }
 }
 
-MiyaMystic.id = 'miya-mystic';
+LetGo.id = 'let-go';
 
-module.exports = MiyaMystic;
+module.exports = LetGo;
 
 
