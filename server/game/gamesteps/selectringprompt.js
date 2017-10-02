f94faa6abbb167248eafa0edae73e14965825dea
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-02 07:26:47 +0100

    implement select ring

--- server/game/gamesteps/selectringprompt.js
+++ server/game/gamesteps/selectringprompt.js
@@ -1,7 +1,125 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+const _ = require('underscore');
+const UiPrompt = require('./uiprompt.js');
 
+/**
+ * General purpose prompt that asks the user to select 1 or more cards.
+ *
+ * The properties option object has the following properties:
+ * numCards           - an integer specifying the number of cards the player
+ *                      must select. Set to 0 if there is no limit on the num
+ *                      of cards that can be selected.
+ * multiSelect        - boolean that ensures that the selected cards are sent as
+ *                      an array, even if the numCards limit is 1.
+ * additionalButtons  - array of additional buttons for the prompt.
+ * activePromptTitle  - the title that should be used in the prompt for the
+ *                      choosing player.
+ * waitingPromptTitle - the title that should be used in the prompt for the
+ *                      opponent players.
+ * maxStat            - a function that returns the maximum value that cards
+ *                      selected by the prompt cannot exceed. If not specified,
+ *                      then no stat limiting is done on the prompt.
+ * cardStat           - a function that takes a card and returns a stat value.
+ *                      Used for prompts that have a maximum stat value.
+ * cardCondition      - a function that takes a card and should return a boolean
+ *                      on whether that card is elligible to be selected.
+ * cardType           - a string or array of strings listing which types of
+ *                      cards can be selected. Defaults to the list of draw
+ *                      card types.
+ * onSelect           - a callback that is called once all cards have been
+ *                      selected. On single card prompts this is called as soon
+ *                      as an elligible card is clicked. On multi-select prompts
+ *                      it is called when the done button is clicked. If the
+ *                      callback does not return true, the prompt is not marked
+ *                      as complete.
+ * onMenuCommand      - a callback that is called when one of the additional
+ *                      buttons is clicked.
+ * onCancel           - a callback that is called when the player clicks the
+ *                      done button without selecting any cards.
+ * source             - what is at the origin of the user prompt, usually a card;
+ *                      used to provide a default waitingPromptTitle, if missing
+ * gameAction         - a string representing the game action to be checked on
+ *                      target cards.
+ * ordered            - an optional boolean indicating whether or not to display
+ *                      the order of the selection during the prompt.
+ */
+class SelectRingPrompt extends UiPrompt {
+    constructor(game, choosingPlayer, properties) {
+        super(game);
+
+        this.choosingPlayer = choosingPlayer;
+        if(properties.source && !properties.waitingPromptTitle) {
+            properties.waitingPromptTitle = 'Waiting for opponent to use ' + properties.source.name;
+        }
+
+        this.properties = properties;
+        _.defaults(this.properties, this.defaultProperties());
+        this.selectedRing = null;
+    }
+
+    defaultProperties() {
+        return {
+            additionalButtons: [],
+            ringCondition: () => true,
+            onSelect: () => true,
+            onMenuCommand: () => true,
+            onCancel: () => true
+        };
+    }
+
+    activeCondition(player) {
+        return player === this.choosingPlayer;
+    }
+
+    activePrompt() {
+        return {
+            selectCard: true,
+            selectOrder: this.properties.ordered,
+            menuTitle: this.properties.activePromptTitle || this.defaultActivePromptTitle(),
+            buttons: this.properties.additionalButtons.concat([
+                { text: 'Done', arg: 'done' }
+            ]),
+            promptTitle: this.properties.source ? this.properties.source.name : undefined
+        };
+    }
+
+    defaultActivePromptTitle() {
+        return 'Choose a card';
+    }
+
+    waitingPrompt() {
+        return { menuTitle: this.properties.waitingPromptTitle || 'Waiting for opponent' };
+    }
+
+    onRingClicked(player, ring) {
+        if(player !== this.choosingPlayer) {
+            return false;
+        }
+
+        if(!this.ringCondition(ring)) {
+            return false;
+        }
+
+        if(this.properties.onSelect(player, ring)) {
+            this.complete();
+        }
+    }
+
+    onMenuCommand(player, arg) {
+        if(player !== this.choosingPlayer) {
+            return false;
+        }
+
+        if(arg !== 'done') {
+            if(this.properties.onMenuCommand(player, arg)) {
+                this.complete();
+            }
+            return;
+        }
+
+        this.properties.onCancel(player);
+        this.complete();
+    }
+}
+
+module.exports = SelectRingPrompt;
 
