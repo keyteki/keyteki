92d1d8e8612f910ab8ccbd9079e5b998813ebc30
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-05 14:29:52 +0100

    swordsmith bugfix

--- server/game/cards/01 - Core/AgashaSwordsmith.js
+++ server/game/cards/01 - Core/AgashaSwordsmith.js
@@ -1,3 +1,4 @@
+const _ = require('underscore');
 const DrawCard = require('../../drawcard.js');
 
 class AgashaSwordsmith extends DrawCard {
@@ -6,23 +7,29 @@ class AgashaSwordsmith extends DrawCard {
             title: 'Search top 5 card for attachment',
             limit: ability.limit.perRound(1),
             handler: (context) => {
-                let properties = {
-                    numCards: 5,
+                this.game.addMessage('{0} uses {1} to look at the top five cards of their deck.', this.controller, this);
+                let attachments = _.filter(this.controller.conflictDeck.first(5), card => card.type === 'attachment');
+                let choices = _.map(attachments, card => card.name);
+                let handlers = _.map(attachments, card => {
+                    return () => {
+                        this.controller.moveCard(card, 'hand');
+                        this.game.addMessage('{0} takes {1} and adds it to their hand', this.controller, card);
+                        this.controller.shuffleConflictDeck();
+                        return true;
+                    };
+                });
+                choices.push('Take nothing');
+                handlers.push(() => {
+                    this.game.addMessage('{0} takes nothing', this.controller);                    
+                    this.controller.shuffleConflictDeck();
+                    return true;
+                });
+                this.game.promptWithHandlerMenu(this.controller, {
                     activePromptTitle: 'Choose a card',
-                    waitingPromptTitle: ('Waiting for opponent to use {0}', context.source),
-                    cardCondition: card => card.getType() === 'attachment',
-                    onSelect: (player, card) => {
-                        this.game.addMessage('{0} takes {1} and adds it to their hand', player, card);
-                        player.moveCard(card, 'hand');
-                        return true;
-                    },
-                    onCancel: player => {
-                        this.game.addMessage('{0} takes nothing', player);
-                        return true;
-                    },
-                    source: context.source
-                };
-                this.game.promptForDeckSearch(context.player, properties);
+                    source: context.source,
+                    choices: choices,
+                    handlers: handlers
+                });
             }
         });
     }
