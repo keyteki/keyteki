506658e9c8b8ff5c616864e64bc0243fbc67ad39
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-06 20:39:38 +0100

    Bayushi Manipulator

---
+++ server/game/cards/01 - Core/BayushiManipulator.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../drawcard.js');
+
+class BayushiManipulator extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onHonorDialsRevealed: () => true
+            },
+            handler: () => {
+                this.game.addMessage('{0} uses {1} to increase their bid by 1', this.controller, this);
+                this.controller.honorBid++;
+            }
+        });
+    }
+}
+
+BayushiManipulator.id = 'bayushi-manipulator';
+
+module.exports = BayushiManipulator;
