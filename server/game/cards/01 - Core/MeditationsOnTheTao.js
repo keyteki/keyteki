eef144116d42bb87d82754a8da691b3d8b445bf4
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-07 20:03:47 +0100

    Meditations on the Tao

---
+++ server/game/cards/01 - Core/MeditationsOnTheTao.js
@@ -0,0 +1,23 @@
+const ProvinceCard = require('../../provincecard.js');
+
+class MeditationsOnTheTao extends ProvinceCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Remove a fate from a character',
+            clickToActivate: true,
+            condition: () => this.game.currentConflict && this.game.currentConflict.conflictProvince === this,
+            target: {
+                cardType: 'character',
+                cardCondition: card => card.isAttacking() && card.fate > 0
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to remove a fate from {2}', this.controller, this, context.target);
+                context.target.modifyFate(-1);
+            }
+        });
+    }
+}
+
+MeditationsOnTheTao.id = 'meditations-on-the-tao';
+
+module.exports = MeditationsOnTheTao;
