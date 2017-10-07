c873b20ce23835b6baaef06bf79df5f9654221c7
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-07 20:02:49 +0100

    Manicured Gardens

---
+++ server/game/cards/01 - Core/ManicuredGardens.js
@@ -0,0 +1,19 @@
+const ProvinceCard = require('../../provincecard.js');
+
+class ManicuredGardens extends ProvinceCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Gain 1 fate',
+            clickToActivate: true,
+            condition: () => this.game.currentConflict && this.game.currentConflict.conflictProvince === this,
+            handler: () => {
+                this.game.addMessage('{0} uses {1} to gain 1 fate', this.controller, this);
+                this.game.addFate(this.controller, 1);
+            }
+        });
+    }
+}
+
+ManicuredGardens.id = 'manicured-gardens';
+
+module.exports = ManicuredGardens;
