7d1091894563ae2787784b0ab029c4b5a8ede8b2
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-07 19:55:31 +0100

    fixes for fortifications

--- server/game/cards/01 - Core/BorderlandsFortifications.js
+++ server/game/cards/01 - Core/BorderlandsFortifications.js
@@ -4,15 +4,19 @@ class BorderlandsFortifications extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Switch this card with another',
-            location: ['province 1', 'province 2', 'province 3', 'province 4'],
+            clickToActivate: true,
             target: {
                 cardCondition: card => card.isDynasty && ['province 1', 'province 2', 'province 3', 'province 4'].includes(card.location)
             },
             handler: context => {
                 this.game.addMessage('{0} uses {1} to swap it with {2}', this.controller, this, context.target.facedown ? 'a facedown card' : context.target);
                 let location = this.location;
+                this.controller.removeCardFromPile(this);
+                this.controller.removeCardFromPile(context.target);
                 this.moveTo(context.target.location);
                 context.target.moveTo(location);
+                this.controller.getSourceList(location).push(context.target);
+                this.controller.getSourceList(this.location).push(this);
             }
         });
     }
