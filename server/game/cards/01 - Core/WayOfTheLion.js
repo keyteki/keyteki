7c7dfdd506f0b9d0d3134a82fcbfbdf929809878
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-08 17:13:50 +0100

    Way of the Lion

---
+++ server/game/cards/01 - Core/WayOfTheLion.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class WayOfTheLion extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            clickToActivate: true,
+            condition: () => this.game.currentConflict,
+            target: {
+                cardType: 'character',
+                cardCondition: card => card.location === 'play area' && card.isFaction('lion')
+            },
+            handler: context => {
+                let baseSkill = context.target.baseMilitarySkill;
+                this.game.addMessage('{0} uses {1} to double {2}\'s base military skill to {3}', this.controller, this, context.target, baseSkill * 2);
+                this.untilEndOfConflict(ability => ({
+                    match: context.target,
+                    effect: ability.effects.modifyBaseMilitarySkill(baseSkill)
+                }));
+            }
+        });
+    }
+}
+
+WayOfTheLion.id = 'way-of-the-lion';
+
+module.exports = WayOfTheLion;
