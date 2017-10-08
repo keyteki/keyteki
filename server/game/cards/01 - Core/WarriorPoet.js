33d947525e105971e853d2648269c6747b87a31a
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-08 21:27:09 +0100

    added condition

--- server/game/cards/01 - Core/WarriorPoet.js
+++ server/game/cards/01 - Core/WarriorPoet.js
@@ -3,10 +3,10 @@ const DrawCard = require('../../drawcard.js');
 class WarriorPoet extends DrawCard {
     setupCardAbilities() {
         this.action({
-            title: 'Reduced skill of opponent\'s characters',
+            title: 'Reduce skill of opponent\'s characters',
+            condition: () => this.isParticipating(),
             handler: () => {
                 this.game.addMessage('{0} uses {1} to reduce the skill of all {2}\'s characters', this.controller, this, this.game.getOtherPlayer(this.controller));
-
                 this.untilEndOfConflict(ability => ({
                     match: card => (
                         card.getType() === 'character' && this.game.currentConflict.isParticipating(card)
