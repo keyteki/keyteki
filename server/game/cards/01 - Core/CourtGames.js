6e41e4fd5ac3145afc2a2b0a36bf13a8aadd651b
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-08 07:22:09 +0100

    Court Games

---
+++ server/game/cards/01 - Core/CourtGames.js
@@ -0,0 +1,53 @@
+const DrawCard = require('../../drawcard.js');
+
+class CourtGames extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            condition: () => this.game.currentConflict && this.game.currentConflict.conflictType === 'political',
+            target: {
+                player: 'self',
+                mode: 'select',
+                choices: {
+                    'Honor a character you control': () => this.controller.cardsInPlay.any(card => {
+                        return this.game.currentConflict.isParticipating(card) && !card.isHonored;
+                    }),
+                    'Dishonor an opposing character': () => this.controller.opponent && this.controller.opponent.cardsInPlay.any(card => {
+                        return this.game.currentConflict.isParticipating(card) && card.allowGameAction('dishonor');
+                    })
+                }
+            },
+            clickToActivate: true,
+            handler: context => {
+                if(context.target === 'Honor a character you control') {
+                    this.game.promptForSelect(this.controller, {
+                        cardType: 'character',
+                        cardCondition: card => this.game.currentConflict.isParticipating(card) && card.controller === this.controller && !card.isHonored,
+                        source: this,
+                        onSelect: (player, card) => {
+                            this.game.addMessage('{0} uses {1} to honor {2}', this.controller, this, card);
+                            player.honorCard(card);
+                            return true;
+                        }
+                    });
+                } else {
+                    let otherPlayer = this.game.getOtherPlayer(this.controller);
+                    this.game.promptForSelect(otherPlayer, {
+                        cardType: 'character',
+                        cardCondition: card => this.game.currentConflict.isParticipating(card) && card.controller === otherPlayer && card.allowGameAction('dishonor'),
+                        source: this,
+                        onSelect: (player, card) => {
+                            this.game.addMessage('{0} uses {1} to dishonor {2}', this.controller, this, card);
+                            player.dishonorCard(card);
+                            return true;
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
+
+CourtGames.id = 'court-games';
+
+module.exports = CourtGames;
+
