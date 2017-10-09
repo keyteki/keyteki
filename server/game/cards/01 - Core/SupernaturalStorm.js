aae7aed83a59f1190432bdb84ab8a679185c6497
Tim Josefsson <jozefusan.chimu@gmail.com>
2017-10-09 18:48:51 +0200

    Supernatural Storm

---
+++ server/game/cards/01 - Core/SupernaturalStorm.js
@@ -0,0 +1,32 @@
+const DrawCard = require('../../drawcard.js');
+
+class SupernaturalStorm extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Increase the skill of one character',
+            clickToActivate: true,
+            condition: () => this.game.currentConflict,
+            target: {
+                activePromptTitle: 'Choose a character',
+                cardType: 'character',
+                cardCondition: card => card.location === 'play area' && this.game.currentConflict.isParticipating(card)
+            },
+            handler: context => {
+                let shugenja = this.controller.filterCardsInPlay(card => card.hasTrait('shugenja') && card.getType() === 'character');
+                let numOfShugenja = shugenja.length;
+                this.untilEndOfConflict(ability => ({
+                    match: context.target,
+                    effect: [
+                        ability.effects.modifyMilitarySkill(numOfShugenja),
+                        ability.effects.modifyPoliticalSkill(numOfShugenja)
+                    ]
+                }));
+                this.game.addMessage('{0} uses {1} to increase the political and military skill of {2} with {3}', this.controller, this, context.target,numOfShugenja);
+            }
+        });
+    }
+}
+
+SupernaturalStorm.id = 'supernatural-storm';
+
+module.exports = SupernaturalStorm;
