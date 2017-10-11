1a92c6463674b2700d9462ed2f3c5046c2ecde20
Tim Josefsson <jozefusan.chimu@gmail.com>
2017-10-11 16:54:35 +0200

    Stand Your Ground

---
+++ server/game/cards/01 - Core/StandYourGround.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard.js');
+
+class StandYourGround extends DrawCard {
+    setupCardAbilities() {
+        this.interrupt({
+            when: {
+                onCardLeavesPlay: event => event.card.controller === this.controller && event.card.getType === 'character' && event.card.isHonored
+            },
+            canCancel: true,
+            handler: context => {
+                context.cancel();
+                context.event.source.card.isHonored = false;
+                this.game.addMessage('{0} uses {1} to stop {2} from leaving play', this.controller, this, context.event.source);
+            }
+        });
+    }
+}
+
+StandYourGround.id = 'stand-your-ground';
+
+module.exports = StandYourGround;
