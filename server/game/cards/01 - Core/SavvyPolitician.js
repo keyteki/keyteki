ddf999a3f33fbbe226095e7297ed47f2f9316c77
Tim Josefsson <jozefusan.chimu@gmail.com>
2017-10-11 12:32:34 +0200

    Removed honored characters from target selection.

--- server/game/cards/01 - Core/SavvyPolitician.js
+++ server/game/cards/01 - Core/SavvyPolitician.js
@@ -7,7 +7,7 @@ class SavvyPolitician extends DrawCard {
                 'onCardHonored': event => event.card === this
             },
             target: {
-                cardCondition: card => card.location === 'play area' && card.getType() === 'character'
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' && !card.isHonored
             },
             handler: context => {
                 this.game.addMessage('{0} uses {1}\'s ability to honor {2}', this.controller, this, context.target);
