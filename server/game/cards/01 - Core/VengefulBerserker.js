e824e1fdd1d08efd07d73398f0e26a4fdc1b3b26
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-11 12:59:47 +0100

    Vengeful Berserker

---
+++ server/game/cards/01 - Core/VengefulBerserker.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard.js');
+
+class VengefulBerserker extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardLeavesPlay: event => event.card.type === 'character' && event.card.controller === this.controller && this.game.currentConflict
+            },
+            handler: () => {
+                this.game.addMessage('{0} uses {1}\'s ability to double his military skill until the end of the conflict', this.controller, this);
+                this.untilEndOfConflict(ability => ({
+                    match: this,
+                    effect: ability.effects.modifyMilitarySkillMultiplier(2)
+                }));
+            }
+        });
+    }
+}
+
+VengefulBerserker.id = 'vengeful-berserker';
+
+module.exports = VengefulBerserker;
