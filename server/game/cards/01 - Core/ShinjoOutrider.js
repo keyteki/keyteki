92b782fe6692764adf33bd2b99590bd1c50de4e9
Tim Josefsson <jozefusan.chimu@gmail.com>
2017-10-11 14:59:12 +0200

    Cleaned up move to conflict wording.

--- server/game/cards/01 - Core/ShinjoOutrider.js
+++ server/game/cards/01 - Core/ShinjoOutrider.js
@@ -4,10 +4,10 @@ class ShinjoOutrider extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Move this character to conflict',
-            condition: () => this.game.currentConflict,
+            condition: () => this.game.currentConflict && this.allowGameAction('moveToConflict'),
             handler: () => {
                 this.game.addMessage('{0} moves {1} to the conflict by using its ability', this.controller, this);
-                this.game.currentConflict.moveToConflict(this);
+                this.game.currentConflict.moveToConflict(this,this.game.currentConflict.attackingPlayer === this.controller);
             }
         });
     }
