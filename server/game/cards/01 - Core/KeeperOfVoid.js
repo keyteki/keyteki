d90c0830c8493061a7e2e42e7b6dcd2be22c7e91
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-12 12:44:22 +0100

    remove debugging

--- server/game/cards/01 - Core/KeeperOfVoid.js
+++ server/game/cards/01 - Core/KeeperOfVoid.js
@@ -5,11 +5,7 @@ class KeeperOfVoid extends RoleCard {
     setupCardAbilities() {
         this.reaction({
             when: {
-                afterConflict: event => {
-                    console.log('checking conditions of', this.name);
-                    console.log(this.hasTrait('void'), event.conflict.elements, event.conflict.winner === this.controller, event.conflict.defendingPlayer === this.controller);
-                    return _.any(event.conflict.elements, element => this.hasTrait(element)) && event.conflict.winner === this.controller && event.conflict.defendingPlayer === this.controller;
-                }
+                afterConflict: event => _.any(event.conflict.elements, element => this.hasTrait(element)) && event.conflict.winner === this.controller && event.conflict.defendingPlayer === this.controller
             },
             handler: () => {
                 this.game.addMessage('{0} uses {1} to gain 1 fate', this.controller, this);
