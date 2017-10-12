478ffeb9f9d69f25ba1e8aba41262216be2aeb9c
Tim Josefsson <jozefusan.chimu@gmail.com>
2017-10-12 20:48:59 +0200

    Matsu Beiona

---
+++ server/game/cards/01 - Core/MatsuBeiona.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../drawcard.js');
+
+class MatsuBeiona extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                'onCardEntersPlay': event => event.card === this && this.controller.filterCardsInPlay(card => card.hasTrait('bushi') && card.getType() === 'character' && card !== this).length >= 3
+            },
+            handler: () => {
+                this.game.addMessage('{0} uses {1}\'s ability to put 2 fate on {1}', this.controller, this);
+                this.fate += 2;
+            }
+        });
+    }
+}
+
+MatsuBeiona.id = 'matsu-beiona';
+
+module.exports = MatsuBeiona;
