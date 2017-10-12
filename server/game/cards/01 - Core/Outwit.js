3b63f24378ad2bb5940e7a7de9e0dc35d5f35a1b
Tim Josefsson <jozefusan.chimu@gmail.com>
2017-10-12 11:13:44 +0200

    Fixes to outwit

--- server/game/cards/01 - Core/Outwit.js
+++ server/game/cards/01 - Core/Outwit.js
@@ -6,17 +6,50 @@ class Outwit extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Choose a character – send it home.',
-            condition: () => this.game.currentConflict && this.controller.anyCardsInPlay(card => card.isParticipating() && card.hasTrait('courtier')),
+            condition: () => this.game.currentConflict && this.hasCourtierPresent(),
             target: {
                 activePromptTitle: 'Select a character',
                 cardType: 'character',
-                cardCondition: card => card.isParticipating() && card.controller !== this.controller && card.getMilitarySkill() < _.max(this.controller.filterCardsInPlay(card => card.isParticipating() && card.hasTrait('courtier')), card => card.getMilitarySkill())
+                cardCondition: card => card.location === 'play area' && this.game.currentConflict.isParticipating(card) && card.controller !== this.controller && card.getPoliticalSkill() < this.getStrongestCourtier()
             },
             handler: context => {
                 this.game.addMessage('{0} uses {1} to send {2} home', this.controller, this, context.target);
                 this.game.currentConflict.sendHome(context.target);
             }
         });
+    }
+
+    hasCourtierPresent() {
+        if(this.game.currentConflict.attackingPlayer === this.controller) {
+            let present = _.size(_.filter(this.game.currentConflict.attackers, card => card.hasTrait('courtier')));
+            if(present > 0) {
+                return true;
+            }
+        }
+        if(this.game.currentConflict.defendingPlayer === this.controller) {
+            let present = _.size(_.filter(this.game.currentConflict.attackers, card => card.hasTrait('courtier')));
+            if(present > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    getStrongestCourtier() {
+        if(this.game.currentConflict.attackingPlayer === this.controller) {
+            let characters = _.filter(this.game.currentConflict.attackers, card => card.hasTrait('courtier'));
+            let highestSkilledCharacter = _.max(characters, function(character) {
+                return character.getPoliticalSkill();
+            });
+            return highestSkilledCharacter.getPoliticalSkill();
+        }
+        if(this.game.currentConflict.defendingPlayer === this.controller) {
+            let characters = _.filter(this.game.currentConflict.attackers, card => card.hasTrait('courtier'));
+            let highestSkilledCharacter = _.max(characters, function(character) {
+                return character.getPoliticalSkill();
+            });
+            return highestSkilledCharacter.getPoliticalSkill();
+        }
     }
 }
 
