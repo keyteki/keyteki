d9504b73f739b2d650c29aadd5dfc58cb51c46e1
Gryffon <ryan@karetas.com>
2017-10-13 10:57:30 -0400

    Update Raven config

--- client/index.prod.jsx
+++ client/index.prod.jsx
@@ -10,10 +10,52 @@ import ReduxToastr from 'react-redux-toastr';
 
 import version from '../version.js';
 
-Raven.config('https://6338b02af9114903bb47e5b4dee79d0e@sentry.io/169735', {
-    ignoreErrors: ['/recaptcha/api2'],
+const ravenOptions = {
+    ignoreErrors: [
+        // Random plugins/extensions
+        'top.GLOBALS',
+        // See: http://blog.errorception.com/2012/03/tale-of-unfindable-js-error. html
+        'originalCreateNotification',
+        'canvas.contentDocument',
+        'MyApp_RemoveAllHighlights',
+        'http://tt.epicplay.com',
+        'Can\'t find variable: ZiteReader',
+        'jigsaw is not defined',
+        'ComboSearch is not defined',
+        'http://loading.retry.widdit.com/',
+        'atomicFindClose',
+        // Facebook borked
+        'fb_xd_fragment',
+        // ISP "optimizing" proxy - `Cache-Control: no-transform` seems to
+        // reduce this. (thanks @acdha)
+        // See http://stackoverflow.com/questions/4113268
+        'bmi_SafeAddOnload',
+        'EBCallBackMessageReceived',
+        // See http://toolbar.conduit.com/Developer/HtmlAndGadget/Methods/JSInjection.aspx
+        'conduitPage',
+        'YoukuAntiAds.eval'
+    ],
+    ignoreUrls: [
+        // Facebook flakiness
+        /graph\.facebook\.com/i,
+        // Facebook blocked
+        /connect\.facebook\.net\/en_US\/all\.js/i,
+        // Woopra flakiness
+        /eatdifferent\.com\.woopra-ns\.com/i,
+        /static\.woopra\.com\/js\/woopra\.js/i,
+        // Chrome extensions
+        /extensions\//i,
+        /^chrome:\/\//i,
+        // Other plugins
+        /127\.0\.0\.1:4001\/isrunning/i, // Cacaoweb
+        /webappstoolbarba\.texthelp\.com\//i,
+        /metrics\.itunes\.apple\.com\.edgesuite\.net\//i
+    ],
     release: version
-}).install();
+};
+
+Raven.config('https://6338b02af9114903bb47e5b4dee79d0e@sentry.io/169735', ravenOptions)
+    .install();
 
 const store = configureStore();
 
