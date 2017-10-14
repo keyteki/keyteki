02b7e52078b5e14f6c2448c1a8f0325baffcf12c
Tim Josefsson <jozefusan.chimu@gmail.com>
2017-10-14 20:27:50 +0200

    More fixing

---
+++ server/game/cards/01 - Core/LionsPrideBrawler.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard.js');
+
+class LionsPrideBrawler extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Bow a character',
+            condition: () => this.game.currentConflict && this.game.currentConflict.isAttacking(this),
+            target: {
+                activePromptTitle: 'Choose a character',
+                cardType: 'character',
+                gameAction: 'bow',
+                cardCondition: card => !card.bowed && card.getMilitarySkill() <= this.getMilitarySkill()
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to bow {2}', this.controller, this, context.target);
+                this.controller.bowCard(context.target);
+            }
+        });
+    }
+}
+
+LionsPrideBrawler.id = 'lion-s-pride-brawler';
+
+module.exports = LionsPrideBrawler;
