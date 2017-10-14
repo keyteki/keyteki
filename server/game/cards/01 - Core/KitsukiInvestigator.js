8a1b9d66ce63e43e858159b4372e4f571c025a07
Tim Josefsson <jozefusan.chimu@gmail.com>
2017-10-14 23:25:34 +0200

    removed additional method

--- server/game/cards/01 - Core/KitsukiInvestigator.js
+++ server/game/cards/01 - Core/KitsukiInvestigator.js
@@ -6,36 +6,19 @@ class KitsukiInvestigator extends DrawCard {
             title: 'Look at opponent\'s hand',
             condition: () => this.game.currentConflict && this.game.currentConflict.isParticipating(this) && this.game.currentConflict.conflictType === 'political',
             handler: () => {
-                let opponent = this.controller.opponent;
-
-                let buttons = opponent.hand.map(card => {
-                    return { method: 'cardSelected', card: card };
-                });
-
-                this.game.promptWithMenu(this.controller, this, {
-                    activePrompt: {
-                        menuTitle: 'Select a card',
-                        buttons: buttons
-                    },
+                this.game.promptWithHandlerMenu(this.controller, {
+                    activePromptTitle: 'Choose card to discard',
+                    choices: this.controller.opponent.hand.map(card => card.name),
+                    handlers: this.controller.opponent.hand.map(card => {
+                        return () => {
+                            this.game.addMessage('{0} uses {1} to discard {2} from {3}\'s hand', this.controller, this, card, this.controller.opponent);
+                            this.controller.opponent.discardCardFromHand(card);
+                        };
+                    }),
                     source: this
                 });
             }
         });
-    }
-
-    cardSelected(player, cardId) {
-        let opponent = this.controller.opponent;
-
-        var card = opponent.findCardByUuid(opponent.hand, cardId);
-        if(!card) {
-            return false;
-        }
-
-        opponent.discardCard(card);
-
-        this.game.addMessage('{0} uses {1} to discard {2} from {3}\'s hand', player, this, card, opponent);
-
-        return true;
     }
 }
 
