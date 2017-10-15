5c22ae8cf13686fc5b8e5a3822009199533425e9
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-15 09:32:58 +0100

    Shosuro Miyako

---
+++ server/game/cards/01 - Core/ShosuroMiyako.js
@@ -0,0 +1,45 @@
+const DrawCard = require('../../drawcard.js');
+
+class ShosuroMiyako extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardPlayed: event => {
+                    return (event.player === this.controller && 
+                            event.card.type === 'character' && 
+                            event.card.location === 'hand' && 
+                            this.controller.opponent);
+                }
+            },
+            target: {
+                mode: 'select',
+                player: 'opponent',
+                choices: {
+                    'Discard at random': () => this.controller.opponent.hand.size() > 0,
+                    'Dishonor a character': () => this.controller.opponent.anyCardsInPlay(card => card.allowGameAction('dishonor'))
+                }
+            },
+            handler: context => {
+                if(context.target === 'Discard at random') {
+                    this.game.addMessage('{0} uses {1} - {2} chose to discard a card at random', this.controller, this, this.controller.opponent);
+                    this.controller.opponent.discardAtRandom(1);
+                } else {
+                    this.game.promptForSelect(this.controller.opponent, {
+                        source: this,
+                        cardType: 'character',
+                        cardCondition: card => card.controller === this.controller.opponent && card.location === 'play area' && card.allowGameAction('dishonor'),
+                        onSelect: (player, card) => {
+                            this.game.addMessage('{0} uses {1} - {2} chose to dishonor {3}', this.controller, this, player, card);
+                            player.dishonorCard(card);
+                            return true;
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
+
+ShosuroMiyako.id = 'shosuro-miyako';
+
+module.exports = ShosuroMiyako;
