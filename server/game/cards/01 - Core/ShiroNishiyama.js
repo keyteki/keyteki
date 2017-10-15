a6569cc5b7cc6248d7780b5e76659d7b0b056f6a
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-15 09:12:47 +0100

    Shiro Nishiyama

---
+++ server/game/cards/01 - Core/ShiroNishiyama.js
@@ -0,0 +1,26 @@
+const StrongholdCard = require('../../strongholdcard.js');
+
+class ShiroNishiyama extends StrongholdCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Give defending characters +1/+1',
+            clickToActivate: true,
+            cost: ability.costs.bowSelf(),
+            condition: () => this.game.currentConflict && this.controller.anyCardsInPlay(card => this.game.currentConflict.isDefending(card)),
+            handler: () => {
+                this.game.addMessage('{0} bows {1} to add +1/+1 to all defenders they control', this.controller, this);
+                this.untilEndOfConflict(ability => ({
+                    match: card => card.isDefending() && card.controller === this.controller,
+                    effect: [
+                        ability.effects.modifyMilitarySkill(1),
+                        ability.effects.modifyPoliticalSkill(1)
+                    ]
+                }));
+            }
+        });
+    }
+}
+
+ShiroNishiyama.id = 'shiro-nishiyama';
+
+module.exports = ShiroNishiyama;
