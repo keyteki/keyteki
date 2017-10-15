8d430fb8122dabff441d32e9d1fff0dbfae263b7
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-15 18:47:36 +0100

    remove debugging

--- server/game/cards/01 - Core/Spyglass.js
+++ server/game/cards/01 - Core/Spyglass.js
@@ -5,17 +5,9 @@ class Spyglass extends DrawCard {
         this.reaction({
             //title: 'Draw a card when commiting to conflict',
             when: {
-                onConflictDeclared: event => {
-                    console.log(this.name, event.name, event.conflict.isAttacking(this.parent))
-                    return event.conflict.isAttacking(this.parent);
-                },
-                onDefendersDeclared: event => {
-                    return event.conflict.isDefending(this.parent);
-                },
-                onMoveToConflict: event => {
-                    console.log(this.name, event.name, event.card === this.parent)
-                    return event.card === this.parent;
-                }
+                onConflictDeclared: event => event.conflict.isAttacking(this.parent),
+                onDefendersDeclared: event => event.conflict.isDefending(this.parent),
+                onMoveToConflict: event => event.card === this.parent
             },
             limit: ability.limit.perRound(2),
             hander: () => {
