7c35ea10c8e6f42d8c30b2ebdcf205748b1c2a65
Peter Henderson <p.henderson139@outlook.com>
2017-10-16 20:10:57 +0100

    Implement Good Omen

---
+++ server/game/cards/01 - Core/GoodOmen.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard.js');
+
+class GoodOmen extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            condition: () => this.controller.honorBid < this.game.getOtherPlayer(this.controller).honorBid,
+            clickToActivate: true,
+            target: {
+                cardType: 'character',
+                cardCondition: card => card.location === 'play area' && card.getCost() >= 3
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to add 1 fate to {2}', this.controller, this, context.target);
+                context.target.modifyFate(1);
+            }
+        });
+    }
+}
+
+GoodOmen.id = 'good-omen';
+
+module.exports = GoodOmen;
