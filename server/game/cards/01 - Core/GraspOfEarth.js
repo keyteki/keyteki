27b76a89bc286906e37a9bad84adb69494369144
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-17 18:39:57 +0100

    Update GraspOfEarth.js
    Lasting effect should be applied to all characters, not just those outside the conflict
    Fixed it to a card cannot effect, rather than a player one

--- server/game/cards/01 - Core/GraspOfEarth.js
+++ server/game/cards/01 - Core/GraspOfEarth.js
@@ -4,17 +4,17 @@ class GraspOfEarth extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
             title: 'Opponent\'s cards cannot join this conflict',
-            condition: () => this.game.currentConflict && this.bowed === false,
+            condition: () => this.game.currentConflict,
             cost: ability.costs.bowSelf(),
             handler: () => {
                 this.game.addMessage('{0} bows {1} to prevent the opponent from bringing characters to the conflict',this.controller,this);
 
                 //Cannot move characters into the conflict
                 this.controller.opponent.cardsInPlay.each(card => {
-                    if(card.type === 'character' && !card.isParticipating()) {
+                    if(card.type === 'character') {
                         card.untilEndOfConflict(ability => ({
                             match: card,
-                            effect: ability.effects.cannotMoveCharactersIntoConflict()
+                            effect: ability.effects.cannotBeMovedIntoConflict()
                         }));
                     }
                 });
