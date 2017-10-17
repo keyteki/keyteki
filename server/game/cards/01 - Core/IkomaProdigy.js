17a0c297837f343f83d27e114996096be0917f66
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-17 15:03:10 +0100

    Ikoma Prodigy

---
+++ server/game/cards/01 - Core/IkomaProdigy.js
@@ -0,0 +1,20 @@
+const DrawCard = require('../../drawcard.js');
+
+class IkomaProdigy extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => event.card === this && this.fate > 0,
+                onCardFateChanged: event => event.card === this && event.fate > 0
+            },
+            handler: () => {
+                this.game.addHonor(this.controller, 1);
+                this.game.addMessage('{0} uses {1} to gain 1 honor', this.controller, this);
+            }
+        });
+    }
+}
+
+IkomaProdigy.id = 'ikoma-prodigy';
+
+module.exports = IkomaProdigy;
