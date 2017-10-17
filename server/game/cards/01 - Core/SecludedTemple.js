786b26ce77208afb13ad625e55ba9d744a2b518c
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-17 11:36:15 +0100

    Secluded Temple

---
+++ server/game/cards/01 - Core/SecludedTemple.js
@@ -0,0 +1,27 @@
+const _ = require('underscore');
+const DrawCard = require('../../drawcard.js');
+
+class SecludedTemple extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onPhaseStarted: event => event.phase === 'conflict' && this.controller.cardsInPlay.size() < this.controller.opponent.cardsInPlay.size() && this.controller.opponent.cardsInPlay.any(card => card.fate > 0 && card.allowGameAction('removeFate'))
+            },
+            target: {
+                player: 'opponent',
+                activePromptTitle: 'Choose a character to remove a fate from',
+                cardType: 'character',
+                gameAction: 'removeFate',
+                cardCondition: card => card.location === 'play area' && card.controller !== this.controller && card.fate > 0
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to force {2} to remove a fate from {3}', this.controller, this, this.controller.opponent, context.target);
+                context.target.modifyFate(-1);
+            }
+        });
+    }
+}
+
+SecludedTemple.id = 'secluded-temple';
+
+module.exports = SecludedTemple;
