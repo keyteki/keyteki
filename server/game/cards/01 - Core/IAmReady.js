7e1a9274747ded9f25590b6e4fd6846d2a9f69e6
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-17 07:52:22 +0100

    Update IAmReady.js
    My previous change won't work - adding a TODO

--- server/game/cards/01 - Core/IAmReady.js
+++ server/game/cards/01 - Core/IAmReady.js
@@ -4,7 +4,6 @@ class IAmReady extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
             title: 'Ready a character',
-            cost: ability.costs.discardFate(1),
             target: {
                 activePromptTitle: 'Choose a character',
                 cardType: 'character',
@@ -14,8 +13,10 @@ class IAmReady extends DrawCard {
                     card.controller === this.controller
                 )
             },
+            //TODO: Fate payment should be a cost here, but we can't pay costs until we have chosen a target
             handler: context => {
                 this.game.addMessage('{0} uses {1} to ready {2}', this.controller, this, context.target);
+                context.target.modifyFate(-1)
                 this.controller.readyCard(context.target);
             }
         });
