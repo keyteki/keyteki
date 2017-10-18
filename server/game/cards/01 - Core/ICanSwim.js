ed9fc23847b091bfeb40bb3f48867d532f333628
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-18 22:36:15 +0100

    I Can Swim

---
+++ server/game/cards/01 - Core/ICanSwim.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard.js');
+
+class ICanSwim extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            condition: () => this.game.currentConflict && this.controller.opponent && this.controller.showBid > this.controller.opponent.showBid,
+            target: {
+                cardType: 'character',
+                gameAction: 'discardCardFromPlay',
+                cardCondition: card => card.isParticipating() && card.isDishonored && card.controller !== this.controller 
+            },
+            handler: context => {
+                this.game.addMessage('{0} plays {1} to discard {2}', this.controller, this, context.target);
+                this.controller.discardCardFromPlay(context.target);
+            }
+        });
+    }
+}
+
+ICanSwim.id = 'i-can-swim';
+
+module.exports = ICanSwim;
