5e2b44c28b5b91588c8007a6ea0e0a82eb820b05
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-19 12:58:16 +0100

    Rout rewrite

--- server/game/cards/01 - Core/Rout.js
+++ server/game/cards/01 - Core/Rout.js
@@ -5,52 +5,24 @@ const DrawCard = require('../../drawcard.js');
 class Rout extends DrawCard {
     setupCardAbilities() {
         this.action({
-            title: 'Choose a character – send it home.',
-            condition: () => this.game.currentConflict && this.hasBushiPresent(),
+            title: 'Send a character home.',
+            condition: () => this.player.anyCardsInPlay(card => card.isParticipating() && card.hasTrait('bushi')),
             target: {
                 activePromptTitle: 'Select a character',
                 cardType: 'character',
                 gameAction: 'sendHome',
-                cardCondition: card => card.location === 'play area' && this.game.currentConflict.isParticipating(card) && card.controller !== this.controller && card.getMilitarySkill() < this.getStrongestBushi()
+                cardCondition: card => card.isParticipating() && card.controller !== this.controller && card.getMilitarySkill() < this.controller.cardsInPlay.max(card => {
+                    if(card.hasTrait('bushi') && card.isParticipating) {
+                        return card.getMilitarySkill();
+                    }
+                    return 0;
+                })
             },
             handler: context => {
                 this.game.addMessage('{0} uses {1} to send {2} home', this.controller, this, context.target);
                 this.game.currentConflict.sendHome(context.target);
             }
         });
-    }
-
-    hasBushiPresent() {
-        if(this.game.currentConflict.attackingPlayer === this.controller) {
-            let present = _.size(_.filter(this.game.currentConflict.attackers, card => card.hasTrait('bushi')));
-            if(present > 0) {
-                return true;
-            }
-        }
-        if(this.game.currentConflict.defendingPlayer === this.controller) {
-            let present = _.size(_.filter(this.game.currentConflict.attackers, card => card.hasTrait('bushi')));
-            if(present > 0) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    getStrongestBushi() {
-        if(this.game.currentConflict.attackingPlayer === this.controller) {
-            let characters = _.filter(this.game.currentConflict.attackers, card => card.hasTrait('bushi'));
-            let highestSkilledCharacter = _.max(characters, function(character) {
-                return character.getMilitarySkill();
-            });
-            return highestSkilledCharacter.getMilitarySkill();
-        }
-        if(this.game.currentConflict.defendingPlayer === this.controller) {
-            let characters = _.filter(this.game.currentConflict.attackers, card => card.hasTrait('bushi'));
-            let highestSkilledCharacter = _.max(characters, function(character) {
-                return character.getMilitarySkill();
-            });
-            return highestSkilledCharacter.getMilitarySkill();
-        }
     }
 }
 
