6f714bad4542648299277ba9f9611aaa73352a77
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-30 07:04:40 +0000

    academy v1

--- server/game/cards/01 - Core/ArtisanAcademy.js
+++ server/game/cards/01 - Core/ArtisanAcademy.js
@@ -1,11 +1,72 @@
+const _ = require('underscore');
+
 const DrawCard = require('../../drawcard.js');
+const PlayAttachmentAction = require('../../playattachmentaction.js');
+const PlayCharacterAction = require('../../playcharacteraction.js');
+
+class AcademyPlayAttachmentAction extends PlayAttachmentAction {
+    constructor() {
+        super();
+        this.location = ['conflict deck'];
+    }
+}
+
+class AcademyPlayCharacterAction extends PlayCharacterAction {
+    constructor() {
+        super();
+        this.location = ['conflict deck'];
+    }
+}
 
 class ArtisanAcademy extends DrawCard {
     setupCardAbilities() {
+        this.eventsRegistrar = new EventsRegistrar
         this.action({
             title: 'Draw a card',
             phase: 'conflict',
-            handler: () => this.game.addMessage('{0} uses {1} to reveal the top card of their conflict deck: {2}. If you want to play this card, drag the top card of your deck to your hand and play it normally', this.controller, this, this.controller.conflictDeck.first())
+            handler: () => {
+                let topCard = this.controller.conflictDeck.first();
+                this.game.addMessage('{0} uses {1} to reveal the top card of their conflict deck: {2}', this.controller, this, topCard);
+                this.untilEndOfPhase(ability => ({
+                    targetType: 'player',
+                    effect: ability.effects.revealTopCardOfConflictDeck()
+                }));
+                _.each(topCard.abilities.playActions, action => {
+                    if(action.location && action.location.includes('hand')) {
+                        let clonedAction = _.clone(action);
+                        clonedAction.location = ['conflict deck'];
+                        console.log('playAction', action.title)
+                        this.untilEndOfPhase(ability => ({
+                            match: topCard,
+                            targetLocation: 'conflict deck',
+                            effect: ability.effects.gainPlayAction(clonedAction)
+                        }));
+                    }
+                });
+                if(topCard.type === 'event') {
+                    console.log('event')
+                    this.untilEndOfPhase(ability => ({
+                        match: topCard,
+                        targetLocation: 'conflict deck',
+                        effect: ability.effects.enableEventToBeUsedInCurrentLocaton()
+                    }));
+                } else if(topCard.type === 'attachment') {
+                    console.log('attachment')
+                    this.untilEndOfPhase(ability => ({
+                        match: topCard,
+                        targetLocation: 'conflict deck',
+                        effect: ability.effects.gainPlayAction(new AcademyPlayAttachmentAction())
+                    }));
+                    
+                } else if(topCard.type === 'character') {
+                    console.log('character')
+                    this.untilEndOfPhase(ability => ({
+                        match: topCard,
+                        targetLocation: 'conflict deck',
+                        effect: ability.effects.gainPlayAction(new AcademyPlayCharacterAction())
+                    }));
+                }
+            }
         });
     }
 }
