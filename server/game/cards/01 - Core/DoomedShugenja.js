9d84457871ea9d6de0ad660eae946e2e48c60514
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-10-30 14:50:29 +0000

    Doomed Shugenja ver 2

--- server/game/cards/01 - Core/DoomedShugenja.js
+++ server/game/cards/01 - Core/DoomedShugenja.js
@@ -2,24 +2,11 @@ const DrawCard = require('../../drawcard.js');
 
 class DoomedShugenja extends DrawCard {
     setupCardAbilities(ability) {
-        this.action({
-            title: 'Play Doomed Shugenja',
-            cost: [ability.costs.playLimited(), ability.costs.chooseFate(0), ability.costs.payReduceableFateCost('play')],
-            condition: () => !this.facedown && this.controller.canPutIntoPlay(this),
-            phase: 'dynasty',
-            location: 'province',
-            printedAbility: false,
-            cannotBeCopied: true,
-            cannotBeCancelled: true,
-            handler: () => {
-                this.controller.putIntoPlay(this);
-                this.game.addMessage('{0} plays {1} with {2} additional fate', this.controller, this, this.fate);
-            }
+        this.persistentEffect({
+            location: 'any',
+            targetType: 'player',
+            effect: ability.effects.playerCannotPlaceFate(card => card === this)
         });
-    }
-    
-    canPlay() {
-        return false;
     }
 }
 
