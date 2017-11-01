dedd79fb669215370cc008348d869df5f9e9b52e
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-11-01 20:29:41 +0000

    discard fix

--- server/game/player.js
+++ server/game/player.js
@@ -1486,22 +1486,22 @@ class Player extends Spectator {
         return true;
     }
 
-    discardCharactersWithNoFate() {
-        if(!this.anyCardsInPlay(card => card.type === 'character' && card.fate === 0 && card.allowGameAction('discardCardFromPlay'))) {
+    discardCharactersWithNoFate(cardsToDiscard) {
+        if(cardsToDiscard.length === 0) {
             return;
         }
         this.game.promptForSelect(this, {
             activePromptTitle: 'Choose character to discard\n(or click Done to discard all characters with no fate)',
             waitingPromptTitle: 'Waiting for opponent to discard characters with no fate',
-            cardCondition: card => card.fate === 0 && card.allowGameAction('discardCardFromPlay') && card.location === 'play area' && card.controller === this,
+            cardCondition: card => cardsToDiscard.includes(card),
             cardType: 'character',
             onSelect: (player, card) => {
                 player.discardCardFromPlay(card);
-                this.game.queueSimpleStep(() => player.discardCharactersWithNoFate());
+                this.game.queueSimpleStep(() => player.discardCharactersWithNoFate(_.reject(cardsToDiscard, c => c === card)));
                 return true;
             },
             onCancel: () => {
-                _.each(this.filterCardsInPlay(card => card.type === 'character' && card.fate === 0), character => {
+                _.each(cardsToDiscard, character => {
                     this.discardCardFromPlay(character);
                 });
                 return true;
