9f8e41b0eef3eb232dbb42363e5c4a362d797758
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-11-02 17:23:39 +0000

    refactor onCardAbilityInitiated

--- server/game/gamesteps/abilityresolver.js
+++ server/game/gamesteps/abilityresolver.js
@@ -23,6 +23,7 @@ class AbilityResolver extends BaseStepWithPipeline {
             new SimpleStep(game, () => this.waitForTargetResolution()),
             new SimpleStep(game, () => this.markActionAsTaken()),
             new SimpleStep(game, () => this.initiateAbility()),
+            new SimpleStep(game, () => this.raiseCardPlayedIfAbility()),
             new SimpleStep(game, () => this.executeHandler()),
             new SimpleStep(game, () => this.raiseCardPlayedIfNotAbility()),
             new SimpleStep(game, () => this.game.popAbilityContext())
@@ -116,12 +117,23 @@ class AbilityResolver extends BaseStepWithPipeline {
         if(this.context.ability.isCardAbility()) {
             let targets = _.flatten(_.values(this.context.targets));
             targets = _.filter(targets, target => target instanceof BaseCard);
-            this.game.raiseInitiateAbilityEvent({ player: this.context.player, source: this.context.source, resolver: this, targets: targets });
+            this.cancelledByAbility = true
+            this.game.raiseInitiateAbilityEvent(this.context, () => this.cancelledByAbility = false);
+        }
+    }
+
+    raiseCardPlayedIfAbility() {
+        if(this.cancelled) {
+            return;
+        }
+        
+        if(this.context.ability.isCardAbility() && this.context.ability.isCardPlayed()) {
+            this.game.raiseEvent('onCardPlayed', { player: this.context.player, card: this.context.source, originalLocation: this.context.ability.originalLocation });
         }
     }
 
     executeHandler() {
-        if(this.cancelled) {
+        if(this.cancelled || this.cancelledByAbility) {
             return;
         }
 
@@ -129,6 +141,10 @@ class AbilityResolver extends BaseStepWithPipeline {
     }
 
     raiseCardPlayedIfNotAbility() {
+        if(this.cancelled) {
+            return;
+        }
+        
         if(!this.context.ability.isCardAbility() && this.context.ability.isCardPlayed()) {
             this.game.raiseEvent('onCardPlayed', { player: this.context.player, card: this.context.source, originalLocation: this.context.ability.originalLocation });
         }
