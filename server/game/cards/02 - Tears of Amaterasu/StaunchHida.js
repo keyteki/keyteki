4fc3524ddf0cf64d7d7d5810bed7899d772d85ed
Cody Duncan <cody1024d@gmail.com>
2017-11-02 16:21:57 -0500

    StaunchHida implemented.  Needs to be tested.  Lint working

--- server/game/cards/02 - Tears of Amaterasu/StaunchHida.js
+++ server/game/cards/02 - Tears of Amaterasu/StaunchHida.js
@@ -1,15 +1,12 @@
-const _ = require('underscore');
 const DrawCard = require('../../drawcard.js');
-const EventRegistrar = require('../../eventregistrar.js');
 
 class StaunchHida extends DrawCard {
     setupCardAbilities() {
         this.reaction({
-            limit: ability.limit.perConflictPhase(1),
             when: {
-                afterConflict: event => event.conflict.winner === this.controller
+                afterConflict: event => event.conflict.winner === this.controller && event.conflict.isParticipating(this)
             },
-            handler: () => {
+            handler: context => {
                 this.game.addMessage('{0} uses {1} to resolve the ring effect', this.controller, this);
                 context.event.conflict.resolveConflictRing(this.controller);
             }
