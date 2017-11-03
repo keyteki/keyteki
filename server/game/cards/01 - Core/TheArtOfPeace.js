8c15db2a2556b1c24360e69a6a08ae6c1a074925
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-11-03 13:12:29 +0000

    Change multievents too

--- server/game/cards/01 - Core/TheArtOfPeace.js
+++ server/game/cards/01 - Core/TheArtOfPeace.js
@@ -11,12 +11,12 @@ class TheArtOfPeace extends ProvinceCard {
                 let events = [];
                 _.each(context.event.conflict.attackers, card => events.push({
                     name: 'onCardDishonored',
-                    params: { player: this.controller, card: card },
+                    params: { player: this.controller, card: card, source: this },
                     handler: () => card.dishonor()
                 }));
                 _.each(context.event.conflict.defenders, card => events.push({
                     name: 'onCardHonored',
-                    params: { player: this.controller, card: card },
+                    params: { player: this.controller, card: card, source: this },
                     handler: () => card.honor()
                 }));
                 this.game.addMessage('{0} uses {1} to dishonor all attackers and honor all defenders in this conflict', this.controller, this);
