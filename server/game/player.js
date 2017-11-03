ab78e8f37cffe407bf47fcc593bb5001b3bae400
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-11-03 16:20:20 +0000

    Add chat announcements for regroup discards

--- server/game/player.js
+++ server/game/player.js
@@ -213,20 +213,6 @@ class Player extends Spectator {
         return province.find(card => card.isProvince);
     }
 
-    discardFromBrokenProvinces() {
-        _.each(['province 1', 'province 2', 'province 3', 'province 4'], location => {
-            let provinceCard = this.getProvinceCardInProvince(location);
-            let dynastyCard = this.getDynastyCardInProvince(location);
-            if(dynastyCard) {
-                if(provinceCard && provinceCard.isBroken && !dynastyCard.facedown) {
-                    this.moveCard(dynastyCard,'dynasty discard pile');
-                }
-            } else {
-                this.replaceDynastyCard(location);
-            }
-        });
-    }
-
     anyCardsInPlay(predicate) {
         return this.game.allCards.any(card => card.controller === this && card.location === 'play area' && predicate(card));
     }
@@ -1201,7 +1187,7 @@ class Player extends Spectator {
         }
 
         if(['province 1', 'province 2', 'province 3', 'province 4', 'stronghold province'].includes(targetLocation)) {
-            if(!card.isStronghold) {
+            if(['dynasty deck', 'province deck'].includes(location)) {
                 card.facedown = true;
             }
             if(!this.takenDynastyMulligan && card.isDynasty) {
