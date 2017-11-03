4d65dea4a6d16ef4f041f0bd213622466388c755
Cody Duncan <cody1024d@gmail.com>
2017-11-03 17:21:25 -0500

    Left in some bad code, oops!

--- server/game/cards/02.1-ToA/GoblinSneak.js
+++ server/game/cards/02.1-ToA/GoblinSneak.js
@@ -8,7 +8,6 @@ class GoblinSneak extends DrawCard {
                 onCardEntersPlay: event => event.card === this && this.controller.opponent && this.controller.opponent.fate > 0
             },
             handler: () => {
-                if ()
                 this.game.addMessage('{0} uses {1}\'s ability to take a fate from {2}', this.controller, this, this.controller.opponent);
                 this.game.addFate(this.controller.opponent, -1);
                 this.modifyFate(1);
