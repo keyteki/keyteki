0a29d88881f9e6b20a5eca3620e8efdf014964fa
Cody Duncan <cody1024d@gmail.com>
2017-11-03 16:47:40 -0500

    Adding additional guards to make sure goblin sneak player has an opponent, and they ahve fate, before calling handler

--- server/game/cards/02.1-ToA/GoblinSneak.js
+++ server/game/cards/02.1-ToA/GoblinSneak.js
@@ -5,9 +5,10 @@ class GoblinSneak extends DrawCard {
         this.reaction({
             title: 'Steal a fate',
             when: {
-                onCardEntersPlay: event => event.card === this
+                onCardEntersPlay: event => event.card === this && this.controller.opponent && this.controller.opponent.fate > 0
             },
             handler: () => {
+                if ()
                 this.game.addMessage('{0} uses {1}\'s ability to take a fate from {2}', this.controller, this, this.controller.opponent);
                 this.game.addFate(this.controller.opponent, -1);
                 this.modifyFate(1);
