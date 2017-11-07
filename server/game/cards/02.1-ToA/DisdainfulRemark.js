3ad381fa820d62109c57706fed6c316022215274
Cody Duncan <cody1024d@gmail.com>
2017-11-07 13:48:04 -0600

    Adding condition to check for cards in opponent hand

--- server/game/cards/02.1-ToA/DisdainfulRemark.js
+++ server/game/cards/02.1-ToA/DisdainfulRemark.js
@@ -4,7 +4,7 @@ class DisdainfulRemark extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Add Province Strength',
-            condition: () => this.controller.anyCardsInPlay(card => card.isParticipating() && card.hasTrait('courtier')),
+            condition: () => this.controller.anyCardsInPlay(card => card.isParticipating() && card.hasTrait('courtier')) && this.controller.opponent.hand.length > 0,
             handler: () => {
                 let opponentHandSize = this.controller.opponent.hand.length;
                 this.game.addMessage('{0} uses {1}\'s ability to add {2} to the province strength.', this.controller, this, opponentHandSize);
