ab59a75d9d05595126b1e6939694c505a1d26f1f
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-11-07 07:42:47 +0000

    chat bugfix for Keeper Initiate

--- server/game/cards/01-Core/KeeperInitiate.js
+++ server/game/cards/01-Core/KeeperInitiate.js
@@ -11,7 +11,7 @@ class KeeperInitiate extends DrawCard {
             },
             location: ['province 1', 'province 2', 'province 3', 'province 4', 'dynasty discard pile'],
             handler: () => {
-                this.game.addMessage('{0} puts {1} into play from their {2}', this.controller, this, this.location === 'dynasty dicard pile' ? 'discard pile' : 'province');
+                this.game.addMessage('{0} puts {1} into play from their {2}', this.controller, this, this.location === 'dynasty discard pile' ? 'discard pile' : 'province');
                 this.controller.putIntoPlay(this);
                 this.modifyFate(1);
             }
