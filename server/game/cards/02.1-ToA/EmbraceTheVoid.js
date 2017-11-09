4adfd6f6f17f4bcceb176185e571a22367d11c4c
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-11-09 00:22:26 +0000

    remove debugging on Embrace The Void

--- server/game/cards/02.1-ToA/EmbraceTheVoid.js
+++ server/game/cards/02.1-ToA/EmbraceTheVoid.js
@@ -5,10 +5,7 @@ class EmbraceTheVoid extends DrawCard {
         this.interrupt({
             title: 'Take Fate',
             when: {
-                onCardRemoveFate: event => {
-                    console.log(event.card.name, event.fate)
-                    return event.card === this.parent && event.fate > 0;
-                }
+                onCardRemoveFate: event => event.card === this.parent && event.fate > 0
             },
             handler: context => {
                 this.game.addMessage('{0} uses {1} to take the {2} fate being removed from {3}', this.controller, this, context.event.fate, this.parent);
