841590927996b93db5cafdb21d6dd41616054bc3
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-11-12 20:30:50 +0000

    Northern Wall Sensei

---
+++ server/game/cards/02.2-FHaG/NorthernWallSensei.js
@@ -0,0 +1,25 @@
+const DrawCard = require('../../drawcard.js');
+
+class NorthernWallSensei extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Grant immunity to events',
+            condition: () => this.isParticipating(),
+            target: {
+                cardType: 'character',
+                cardCondition: card => card.isParticipating() && card.controller === this.controller && card.attachments.size() > 0,
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to grant immunity to events to {2}', this.controller, this, context.target);
+                context.target.untilEndOfConflict(ability => ({
+                    match: context.target,
+                    effect: ability.effects.immuneTo(context => context && context.source.type === 'event')
+                }));
+            }
+        })
+    }
+}
+
+NorthernWallSensei.id = 'northern-wall-sensei';
+
+module.exports = NorthernWallSensei;
