1e41ff69508c3e2e4ec7ca2b26041ab15f5f6347
Cody Duncan <cody1024d@gmail.com>
2017-11-11 20:27:57 -0600

    Implementing Riot In The Streets

---
+++ server/game/cards/02.2-FGaH/RiotInTheStreets.js
@@ -0,0 +1,26 @@
+const ProvinceCard = require('../../provincecard.js');
+const _ = require('underscore');
+
+class RiotInTheStreets extends ProvinceCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Bow character if you have 3 participating bushi',
+            condition: () => this.game.currentConflict && this.game.currentConflict.conflictProvince === this && 
+                this.controller.getNumberOfCardsInPlay(card => card.hasTrait('bushi') && card.isParticipating()) >= 3,
+            target: {
+                activePromptTitle: 'Choose a character',
+                cardType: 'character',
+                gameAction: 'bow',
+                cardCondition: card => card.isParticipating()
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to bow {2}', this.controller, this, context.target);
+                this.controller.bowCard(context.target, context.source);
+            }
+        });
+    }
+}
+
+RiotInTheStreets.id = 'riot-in-the-streets';
+
+module.exports = RiotInTheStreets;
