0b512dff5a7e64fcacd48b7cae7e7582016c5c2c
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-11-13 04:56:27 +0000

    Disdainful Remark bugfix

--- server/game/cards/02.1-ToA/DisdainfulRemark.js
+++ server/game/cards/02.1-ToA/DisdainfulRemark.js
@@ -4,11 +4,13 @@ class DisdainfulRemark extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Add Province Strength',
-            condition: () => this.controller.anyCardsInPlay(card => card.isParticipating() && card.hasTrait('courtier')) && this.controller.opponent.hand.length > 0,
+            condition: () => this.controller.anyCardsInPlay(card => card.isParticipating() && card.hasTrait('courtier')) && this.controller.opponent.hand.size() > 0,
             handler: () => {
-                let opponentHandSize = this.controller.opponent.hand.length;
+                let opponentHandSize = this.controller.opponent.hand.size();
                 this.game.addMessage('{0} uses {1}\'s ability to add {2} to the province strength.', this.controller, this, opponentHandSize);
                 this.untilEndOfConflict(ability => ({
+                    match: this.game.currentConflict.conflictProvince,
+                    targetLocation: 'province',
                     effect: ability.effects.modifyProvinceStrength(opponentHandSize)
                 }));
             }
