503385b38a1624c758eabd81fc54360f02142fcb
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-11-18 17:44:38 +0000

    Refactor Raise the Alarm

--- server/game/cards/02.2-FHaG/RaiseTheAlarm.js
+++ server/game/cards/02.2-FHaG/RaiseTheAlarm.js
@@ -4,21 +4,22 @@ class RaiseTheAlarm extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Flip a dynasty card',
-            condition: () => this.game.currentConflict && this.game.currentConflict.defendingPlayer === this.controller && this.game.currentConflict.conflictType === 'military',
-            target: {
-                cardCondition: card => {
-                    return (['province 1', 'province 2', 'province 3', 'province 4'].includes(card.location) &&
-                        card.isDynasty && card.facedown && card.controller === this.controller);
+            condition: () => {
+                if(!this.controller.isDefendingPlayer() || !this.game.currentConflict.conflictProvince || this.game.currentConflict.conflictType !== 'military') {
+                    return false;
                 }
+                let dynastyCard = this.controller.getDynastyCardInProvince(this.game.currentConflict.conflictProvince.location);
+                return dynastyCard && dynastyCard.facedown;
             },
-            handler: context => {
-                context.target.facedown = false;
-                if(context.target.type === 'character' && this.controller.canPutIntoPlay(context.target, true)) {
-                    this.game.addMessage('{0} uses {1} to bring {2} into the conflict!', this.controller, this, context.target);
-                    this.controller.putIntoPlay(context.target, true);
+            handler: () => {
+                let card = this.controller.getDynastyCardInProvince(this.game.currentConflict.conflictProvince.location);
+                card.facedown = false;
+                if(card.type === 'character' && this.controller.canPutIntoPlay(card, true)) {
+                    this.game.addMessage('{0} uses {1} to bring {2} into the conflict!', this.controller, this, card);
+                    this.controller.putIntoPlay(card, true);
                 }
                 else {
-                    this.game.addMessage('{0} uses {1} but cannot bring {2} into the conflict!', this.controller, this, context.target);
+                    this.game.addMessage('{0} uses {1} but cannot bring {2} into the conflict!', this.controller, this, card);
                 }
             }
         });
