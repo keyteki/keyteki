1167cf78e0c9f77fb9651882b8f0204f4fb12d78
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-11-18 12:01:35 +0000

    Kakita Yoshi

--- server/game/cards/02.3-ItFC/KakitaYoshi.js
+++ server/game/cards/02.3-ItFC/KakitaYoshi.js
@@ -2,6 +2,22 @@ const DrawCard = require('../../drawcard.js');
 
 class KakitaYoshi extends DrawCard {
     setupCardAbilities(ability) {
+        this.action({
+            title: 'Draw 3 cards',
+            condition: () => this.isParticipating(),
+            cost: ability.costs.discardImperialFavor(),
+            handler: () => {
+                this.game.addMessage('{0} uses {1}, discarding the favor to draw 3 cards, and reduce the cost of events this conflict', this.controller, this);
+                this.controller.drawCardsToHand(3);
+                this.untilEndOfConflict(ability => ({
+                    targetType: 'player',
+                    effect: ability.effects.reduceCost({
+                        amount: 2,
+                        match: card => card.type === 'event'
+                    })
+                }));
+            }
+        });
     }
 }
 
