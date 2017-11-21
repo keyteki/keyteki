f0480e88f286143a9d0208c6b152accf850105c8
cdmayberry1 <gmodman12@gmail.com>
2017-11-21 11:29:17 -0500

    Updated to check for Phoenix clan and added 'honor' as a gameAction to target.

--- server/game/cards/02.3-ItFC/BentensTouch.js
+++ server/game/cards/02.3-ItFC/BentensTouch.js
@@ -5,11 +5,12 @@ class BentensTouch extends DrawCard {
         this.action({
             title: 'Bow and Honor a character',
             condition: () => this.game.currentConflict,
-            cost: ability.costs.bow(card => card.location === 'play area' && card.hasTrait('shugenja')),
+            cost: ability.costs.bow(card => card.isFaction('phoenix') && card.hasTrait('shugenja')),
             target: {
                 cardType: 'character',
                 activePromptTitle: 'Choose a character to honor',
-                cardCondition: card => card.isParticipating() && !card.isHonored && card.controller === this.controller
+                gameAction: 'honor',
+                cardCondition: card => card.isParticipating() && card.controller === this.controller
             },
             handler: context => {
                 this.game.addMessage('{0} uses {1} to bow {2} and honor {3}', this.controller, this, context.costs.bow, context.target);
