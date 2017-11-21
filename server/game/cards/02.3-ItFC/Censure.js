35f79d89407428e7364b2eda5c43bdf2f5ab046f
cdmayberry1 <gmodman12@gmail.com>
2017-11-21 13:36:53 -0500

    Added Censure

--- server/game/cards/02.3-ItFC/Censure.js
+++ server/game/cards/02.3-ItFC/Censure.js
@@ -1,7 +1,17 @@
 const DrawCard = require('../../drawcard.js');
 
 class Censure extends DrawCard {
-    setupCardAbilities(ability) {
+    setupCardAbilities() {        
+        this.interrupt({
+            when: {
+                onCardAbilityInitiated: event => event.card.type === 'event' && this.controller.opponent && this.controller.imperialFavor !== ''
+            },
+            canCancel: true,
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to cancel {2}', this.controller, this, context.event.card);
+                context.cancel();
+            }
+        });
     }
 }
 
