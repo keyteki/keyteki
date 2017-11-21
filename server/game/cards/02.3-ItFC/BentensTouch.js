6f27082a07be64b936ae1ebbf30d15406e0aef85
cdmayberry1 <gmodman12@gmail.com>
2017-11-21 10:37:18 -0500

    Added Benten's Touch

--- server/game/cards/02.3-ItFC/BentensTouch.js
+++ server/game/cards/02.3-ItFC/BentensTouch.js
@@ -2,9 +2,23 @@ const DrawCard = require('../../drawcard.js');
 
 class BentensTouch extends DrawCard {
     setupCardAbilities(ability) {
+        this.action({
+            title: 'Bow and Honor a character',
+            condition: () => this.game.currentConflict,
+            cost: ability.costs.bow(card => card.location === 'play area' && card.hasTrait('shugenja')),
+            target: {
+                cardType: 'character',
+                activePromptTitle: 'Choose a character to honor',
+                cardCondition: card => card.isParticipating() && !card.isHonored && card.controller === this.controller
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to bow {2} and honor {3}', this.controller, this, context.costs.bow, context.target);
+                this.controller.honorCard(context.target, this);
+            }
+        });
     }
 }
 
-BentensTouch.id = 'benten-s-touch'; // This is a guess at what the id might be - please check it!!!
+BentensTouch.id = 'benten-s-touch';
 
 module.exports = BentensTouch;
