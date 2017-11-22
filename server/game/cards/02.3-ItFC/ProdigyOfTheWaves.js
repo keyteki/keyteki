9950fc3f648fcc03f3c1ce5393ef935a4956baf5
Patrick McQuay <patrick.mcquay@gmail.com>
2017-11-21 21:05:50 -0500

    Implemented Prodigy of the Waves

--- server/game/cards/02.3-ItFC/ProdigyOfTheWaves.js
+++ server/game/cards/02.3-ItFC/ProdigyOfTheWaves.js
@@ -2,9 +2,17 @@ const DrawCard = require('../../drawcard.js');
 
 class ProdigyOfTheWaves extends DrawCard {
     setupCardAbilities(ability) {
+        this.action({
+            title: 'Ready this character',
+            condition: () => this.bowed && this.game.rings['water'].claimed,
+            handler: () => {
+                this.game.addMessage('{0} readies {1} using its ability', this.controller, this);
+                this.controller.readyCard(this);
+            }
+        });
     }
 }
 
-ProdigyOfTheWaves.id = 'prodigy-of-the-waves'; // This is a guess at what the id might be - please check it!!!
+ProdigyOfTheWaves.id = 'prodigy-of-the-waves';
 
 module.exports = ProdigyOfTheWaves;
