be4a4d46737e4c7ad139f5a8d7bd76f79bfd16e3
Patrick McQuay <patrick.mcquay@gmail.com>
2017-11-21 20:53:40 -0500

    Implmenting Hiruma Skirmisher

--- server/game/cards/02.3-ItFC/HirumaSkirmisher.js
+++ server/game/cards/02.3-ItFC/HirumaSkirmisher.js
@@ -2,9 +2,22 @@ const DrawCard = require('../../drawcard.js');
 
 class HirumaSkirmisher extends DrawCard {
     setupCardAbilities(ability) {
+        this.reaction({
+            title: 'Gain covert until end of phase',
+            when: {
+                'onCardEntersPlay': event => event.card === this
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1}\'s ability to give {1} covert until the end of the phase', this.controller, this);
+                this.untilEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.addKeyword('covert')
+                }));
+            }
+        });
     }
 }
 
-HirumaSkirmisher.id = 'hiruma-skirmisher'; // This is a guess at what the id might be - please check it!!!
+HirumaSkirmisher.id = 'hiruma-skirmisher';
 
 module.exports = HirumaSkirmisher;
