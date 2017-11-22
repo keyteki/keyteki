3a2f43694e9d68967c161b0e6f987cc6e3e8ad98
Patrick McQuay <patrick.mcquay@gmail.com>
2017-11-21 20:59:32 -0500

    Implemented Chisei District

--- server/game/cards/02.3-ItFC/ChiseiDistrict.js
+++ server/game/cards/02.3-ItFC/ChiseiDistrict.js
@@ -2,6 +2,13 @@ const DrawCard = require('../../drawcard.js');
 
 class ChiseiDistrict extends DrawCard {
     setupCardAbilities(ability) {
+        this.persistentEffect({
+            location: 'any',
+            targetLocation: 'province',
+            condition: () => ['province 1', 'province 2', 'province 3', 'province 4'].includes(this.location),
+            match: card => card.isProvince && card.location === this.location && card.controller === this.controller,
+            effect: ability.effects.cardCannotInitiateConflict(() => this.game.currentConflict.conflictType === 'military')
+        });
     }
 }
 
