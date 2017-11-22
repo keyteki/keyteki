c74c0dfa5845f2c1e16b713f18657ac347ec07b2
Patrick McQuay <patrick.mcquay@gmail.com>
2017-11-22 17:33:04 -0500

    Adding match back in

--- server/game/cards/02.3-ItFC/HirumaSkirmisher.js
+++ server/game/cards/02.3-ItFC/HirumaSkirmisher.js
@@ -10,6 +10,7 @@ class HirumaSkirmisher extends DrawCard {
             handler: context => {
                 this.game.addMessage('{0} uses {1}\'s ability to give {1} covert until the end of the phase', this.controller, this);
                 this.untilEndOfPhase(ability => ({
+                    match: this,
                     effect: ability.effects.addKeyword('covert')
                 }));
             }
