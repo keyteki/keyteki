4de4bf80b5db37c52035aa7a7b6ed10ffa259cfb
Patrick McQuay <patrick.mcquay@gmail.com>
2017-11-21 20:57:54 -0500

    Implemented Mantra of Water

--- server/game/cards/02.3-ItFC/MantraOfWater.js
+++ server/game/cards/02.3-ItFC/MantraOfWater.js
@@ -2,9 +2,23 @@ const DrawCard = require('../../drawcard.js');
 
 class MantraOfWater extends DrawCard {
     setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onConflictDeclared: event => event.conflictRing === 'water' && event.conflict.attackingPlayer !== this.controller
+            },
+            target: {
+                cardType: 'character',
+                cardCondition: card => card.location === 'play area' && (card.hasTrait('monk') || card.attachments.any(card => card.hasTrait('monk'))) && card.bowed
+            },
+            handler: context => {
+                this.game.addMessage('{0} plays {1} to ready {2} and draw a card', this.controller, this, context.target);
+                this.controller.readyCard(context.target);
+                this.controller.drawCardsToHand(1);
+            }
+        });
     }
 }
 
-MantraOfWater.id = 'mantra-of-water'; // This is a guess at what the id might be - please check it!!!
+MantraOfWater.id = 'mantra-of-water';
 
 module.exports = MantraOfWater;
