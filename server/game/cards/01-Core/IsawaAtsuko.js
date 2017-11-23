a8d71a356c0a0ef1b0f2379f9984a9d091fd26da
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-11-23 08:18:57 +0000

    Isawa Atsuko bugfix

--- server/game/cards/01-Core/IsawaAtsuko.js
+++ server/game/cards/01-Core/IsawaAtsuko.js
@@ -7,21 +7,30 @@ class IsawaAtsuko extends DrawCard {
             condition: () => this.game.currentConflict && this.game.currentConflict.elements.includes('void'),
             handler: () => {
                 this.game.addMessage('{0} uses {1} to give friendly characters +1/+1 and opposing characters -1/-1', this.controller, this);
-                this.untilEndOfConflict(ability => ({
-                    match: card => card.isParticipating(),
-                    targetController: 'opponent',
-                    effect: [
-                        ability.effects.modifyMilitarySkill(-1),
-                        ability.effects.modifyPoliticalSkill(-1)
-                    ]
-                }));
-                this.untilEndOfConflict(ability => ({
-                    match: card => card.isParticipating(),
-                    effect: [
-                        ability.effects.modifyMilitarySkill(1),
-                        ability.effects.modifyPoliticalSkill(1)
-                    ]
-                }));
+                this.controller.cardsInPlay.each(card => {
+                    if(card.isParticipating()) {
+                        this.untilEndOfConflict(ability => ({
+                            match: card,
+                            effect: [
+                                ability.effects.modifyMilitarySkill(1),
+                                ability.effects.modifyPoliticalSkill(1)
+                            ]
+                        }));
+                    }
+                });
+                if(this.controller.opponent) {
+                    this.controller.opponent.cardsInPlay.each(card => {
+                        if(card.isParticipating()) {
+                            this.untilEndOfConflict(ability => ({
+                                match: card,
+                                effect: [
+                                    ability.effects.modifyMilitarySkill(-1),
+                                    ability.effects.modifyPoliticalSkill(-1)
+                                ]
+                            }));
+                        }
+                    });
+                }
             }
         });
     }
