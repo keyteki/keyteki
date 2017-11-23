1cc7c5558b3313e8c969339c398987083917dd87
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-11-23 20:20:55 +0000

    Use Token code

--- server/game/cards/02.3-ItFC/PublicForum.js
+++ server/game/cards/02.3-ItFC/PublicForum.js
@@ -4,14 +4,14 @@ class PublicForum extends ProvinceCard {
     setupCardAbilities(ability) {
         this.interrupt({
             when: {
-                onBreakProvince: event => event.province === this && !event.province.hasHonorToken,
+                onBreakProvince: event => event.province === this && !event.province.hasToken('honor'),
             },
             canCancel: true,
             handler: context => {
-                context.cancel();
-                context.event.province.hasHonorToken = true;
                 this.game.addMessage('{0} adds an honor token to {1} instead of breaking it', this.controller, this);
-            }
+                context.cancel();
+                context.event.province.addToken('honor');
+             }
         });
     }
 }
