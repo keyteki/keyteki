abd57f3ba78cc8b25c9a972c561710f914c66faf
Patrick McQuay <patrick.mcquay@gmail.com>
2017-11-23 23:30:11 -0500

    Before the Throne

--- server/game/cards/02.4-TCT/BeforeTheThrone.js
+++ server/game/cards/02.4-TCT/BeforeTheThrone.js
@@ -2,9 +2,19 @@ const ProvinceCard = require('../../provincecard.js');
 
 class BeforeTheThrone extends ProvinceCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+        this.interrupt({
+            title: 'Take 2 honor',
+            when: {
+                onBreakProvince: event => event.province === this
+            },
+            handler: () => {
+                this.game.addMessage('{0} uses {1} to take 2 honor', this.controller, this);
+                this.game.transferHonor(this.game.getOtherPlayer(this.controller), this.controller, 2);
+            }
+        });
     }
 }
 
-BeforeTheThrone.id = 'before-the-throne'; // This is a guess at what the id might be - please check it!!!
+BeforeTheThrone.id = 'before-the-throne'
 
 module.exports = BeforeTheThrone;
