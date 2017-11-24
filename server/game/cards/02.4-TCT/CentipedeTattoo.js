eb83a902555eb5a5364dd6d23e287fa1656efcd6
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-11-24 12:01:04 +0000

    Placeholders for TCT

---
+++ server/game/cards/02.4-TCT/CentipedeTattoo.js
@@ -0,0 +1,10 @@
+const DrawCard = require('../../drawcard.js');
+
+class CentipedeTattoo extends DrawCard {
+    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars - Remove this when the card is implemented
+    }
+}
+
+CentipedeTattoo.id = 'centipede-tattoo'; // This is a guess at what the id might be - please check it!!!
+
+module.exports = CentipedeTattoo;
---
+++ server/game/cards/02.4-TCT/UtakuMediatior.js
@@ -0,0 +1,10 @@
+const DrawCard = require('../../drawcard.js');
+
+class UtakuMediatior extends DrawCard {
+    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars - Remove this when the card is implemented
+    }
+}
+
+UtakuMediatior.id = 'utaku-mediatior'; // This is a guess at what the id might be - please check it!!!
+
+module.exports = UtakuMediatior;
