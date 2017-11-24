ff70bdecc361e35315ecefb7b0f59a9225bed73c
Cody Duncan <cody1024d@gmail.com>
2017-11-24 07:50:39 -0600

    Gaijin Customs

--- server/game/cards/02.4-TCT/GaijinCustoms.js
+++ server/game/cards/02.4-TCT/GaijinCustoms.js
@@ -1,10 +1,22 @@
 const DrawCard = require('../../drawcard.js');
 
 class GaijinCustoms extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities() {
+        this.action({
+            title: 'Choose unicorn card, unbow non unicorn card',
+            condition: () => this.controller.anyCardsInPlay(card => card.isFaction('unicorn')),
+            target: {
+                cardType: 'character',
+                cardCondition: (card) => card.location === 'play area' && !card.isFaction('unicorn') && card.bowed
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to ready {3}', this.controller, this, context.target);
+                this.controller.readyCard(context.target);
+            }
+        });
     }
 }
 
-GaijinCustoms.id = 'gaijin-customs'; // This is a guess at what the id might be - please check it!!!
+GaijinCustoms.id = 'gaijin-customs';    
 
 module.exports = GaijinCustoms;
