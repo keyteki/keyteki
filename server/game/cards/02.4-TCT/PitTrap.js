df73391196f6ecc1834f249302380c2b68db84d5
Patrick McQuay <patrick.mcquay@gmail.com>
2017-11-24 21:50:11 -0500

    Fixed eslint issues

--- server/game/cards/02.4-TCT/PitTrap.js
+++ server/game/cards/02.4-TCT/PitTrap.js
@@ -1,21 +1,21 @@
 const DrawCard = require('../../drawcard.js');
 
 class PitTrap extends DrawCard {
-  setupCardAbilities(ability) {
-      this.whileAttached({
-          condition: () => (
-              this.game.currentPhase === 'regroup'
-          ),
-          effect: ability.effects.cannotBeReadied()
-      });
-  }
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            condition: () => (
+                this.game.currentPhase === 'regroup'
+            ),
+            effect: ability.effects.cannotBeReadied()
+        });
+    }
 
-  canAttach(card) {
-      if(this.game.currentConflict && this.game.currentConflict.isAttacking(card)) {
-          return super.canAttach(card);
-      }
-      return false;
-  }
+    canAttach(card) {
+        if(this.game.currentConflict && this.game.currentConflict.isAttacking(card)) {
+            return super.canAttach(card);
+        }
+        return false;
+    }
 }
 
 PitTrap.id = 'pit-trap';
