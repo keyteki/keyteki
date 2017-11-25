e9a5e96dd23f6da3bc217b084ebff949a221208e
Patrick McQuay <patrick.mcquay@gmail.com>
2017-11-25 11:42:54 -0500

    Implmented Curry Favor

--- server/game/cards/02.4-TCT/CurryFavor.js
+++ server/game/cards/02.4-TCT/CurryFavor.js
@@ -2,9 +2,24 @@ const DrawCard = require('../../drawcard.js');
 
 class CurryFavor extends DrawCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+        this.reaction({
+            title: 'Ready a character'
+            when: {
+                onParticipantsReturnHome: this.controller.conflicts && this.controller.conflicts.conflictOpportunities === 0 && this.controller.isAttackingPlayer(),
+            },
+            target: {
+                cardType: 'character',
+                gameAction: 'discardCardFromPlay',
+                cardCondition: card => this.game.currentConflict.isParticipating(card) && this.isAttacking()
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to ready {2}', this.controller, this, context.target);
+                this.controller.readyCard(context.target);
+            }
+        });
     }
 }
 
-CurryFavor.id = 'curry-favor'; // This is a guess at what the id might be - please check it!!!
+CurryFavor.id = 'curry-favor';
 
 module.exports = CurryFavor;
