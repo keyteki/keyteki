680002a544f6b944e4d49909f38ff1ce39790600
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-11-25 17:05:52 +0000

    Mountaintop Statuary
    TODO: Add event to dynasty flip

--- server/game/cards/02.4-TCT/MountaintopStatuary.js
+++ server/game/cards/02.4-TCT/MountaintopStatuary.js
@@ -1,10 +1,34 @@
 const DrawCard = require('../../drawcard.js');
 
 class MountaintopStatuary extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.reaction({
+            title: 'Move this to stronghold province',
+            when: {
+                onDynastyCardTurnedFaceup: event => event.card === this
+            },
+            handler: () => {
+                this.game.addMessage('{0} uses {1} to move {1} to their stronghold province', this.controller, this);
+                this.controller.moveCard(this, 'stronghold province');
+            }
+        });
+        this.action({
+            title: 'Send a 2 or lower cost character home',
+            cost: ability.costs.sacrificeSelf(),
+            condition: () => this.game.currentConflict && this.game.currentConflict.conflictProvince === this,
+            target: {
+                cardType: 'character',
+                gameAction: 'sendHome',
+                cardCondition: card => card.isAttacking() && card.getCost() < 3
+            },
+            handler: context => {
+                this.game.addMessage('{0} sacrifices {1} to send {2} home', this.controller, this, context.target);
+                this.game.currentConflict.sendHome(context.target, this);
+            }
+        });
     }
 }
 
-MountaintopStatuary.id = 'mountaintop-statuary'; // This is a guess at what the id might be - please check it!!!
+MountaintopStatuary.id = 'mountaintop-statuary'; 
 
 module.exports = MountaintopStatuary;
