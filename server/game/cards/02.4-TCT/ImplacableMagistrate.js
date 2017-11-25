d528681194f3f8b7a075b35d7c28cc33d4e1c7db
Patrick McQuay <patrick.mcquay@gmail.com>
2017-11-24 20:49:56 -0500

    Implacable Magistrate

--- server/game/cards/02.4-TCT/ImplacableMagistrate.js
+++ server/game/cards/02.4-TCT/ImplacableMagistrate.js
@@ -1,10 +1,16 @@
 const DrawCard = require('../../drawcard.js');
 
 class ImplacableMagistrate extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+          condition: () => this.isAttacking(),
+          match: card => card.isParticipating() && !card.isHonored && card !== this,
+          targetController: 'any',
+          effect: ability.effects.cannotCountForResolution()
+        });
     }
 }
 
-ImplacableMagistrate.id = 'implacable-magistrate'; // This is a guess at what the id might be - please check it!!!
+ImplacableMagistrate.id = 'implacable-magistrate';
 
 module.exports = ImplacableMagistrate;
