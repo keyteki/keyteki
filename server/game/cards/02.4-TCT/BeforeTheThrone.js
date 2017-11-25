22f21ab1112931dcf859477d70ca4dae5a4d503c
Patrick McQuay <patrick.mcquay@gmail.com>
2017-11-25 10:57:38 -0500

    Added back eslint comment (ability parameter never used)

--- server/game/cards/02.4-TCT/BeforeTheThrone.js
+++ server/game/cards/02.4-TCT/BeforeTheThrone.js
@@ -1,7 +1,7 @@
 const ProvinceCard = require('../../provincecard.js');
 
 class BeforeTheThrone extends ProvinceCard {
-    setupCardAbilities(ability) {
+    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
         this.interrupt({
             title: 'Take 2 honor',
             when: {
@@ -9,9 +9,9 @@ class BeforeTheThrone extends ProvinceCard {
             },
             handler: () => {
                 this.game.addMessage('{0} uses {1} to take 2 honor', this.controller, this);
-                if(this.controller.opponent){
+                if(this.controller.opponent) {
                     this.game.transferHonor(this.game.getOtherPlayer(this.controller), this.controller, 2);
-                }else{
+                } else {
                     this.game.addHonor(2);
                 }
             }
