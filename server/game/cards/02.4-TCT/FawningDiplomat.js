934344d03768602891df916d66427a7b61ec3fc2
Patrick McQuay <patrick.mcquay@gmail.com>
2017-11-27 21:02:27 -0500

    Removed unnecessary functionality

--- server/game/cards/02.4-TCT/FawningDiplomat.js
+++ server/game/cards/02.4-TCT/FawningDiplomat.js
@@ -8,27 +8,9 @@ class FawningDiplomat extends DrawCard {
                 onCardLeavesPlay: event => event.card === this
             },
             handler: () => {
-                this.game.promptWithMenu(this.controller, this, {
-                    activePrompt: {
-                        menuTitle: 'Which side of the Imperial Favor would you like to claim?',
-                        buttons: [
-                            { text: 'Military', method: 'giveImperialFavorToPlayer', arg: 'military' },
-                            { text: 'Political', method: 'giveImperialFavorToPlayer', arg: 'political' }
-                        ]
-                    }
-                });
+                this.controller.claimImperialFavor()
             }
         });
-    }
-
-    giveImperialFavorToPlayer(arg) {
-        let otherPlayer = this.game.getOtherPlayer(this.controller);
-        this.controller.claimImperialFavor(arg);
-        if(otherPlayer) {
-            otherPlayer.loseImperialFavor();
-            this.game.addMessage('{0} succesfully claims the Emperor\'s {1} favor with total glory of {2} vs {3}', this.controller, arg, this.controller.totalGloryForFavor, otherPlayer.totalGloryForFavor);
-        }
-        return true;
     }
 }
 
