a4b2d7321651386d6c3f7c483521331fd56897b7
Patrick McQuay <patrick.mcquay@gmail.com>
2017-11-29 22:29:08 -0500

    Fixed bugs after testing

--- server/game/cards/02.4-TCT/CurryFavor.js
+++ server/game/cards/02.4-TCT/CurryFavor.js
@@ -3,14 +3,14 @@ const DrawCard = require('../../drawcard.js');
 class CurryFavor extends DrawCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
         this.reaction({
-            title: 'Ready a character'
+            title: 'Ready a character',
             when: {
-                onParticipantsReturnHome: this.controller.conflicts && this.controller.conflicts.conflictOpportunities === 0 && this.controller.isAttackingPlayer(),
+                onParticipantsReturnHome: event => event.conflict.attackingPlayer && event.conflict.attackingPlayer === this.controller && event.conflict.attackingPlayer.conflicts.complete === 2,
             },
             target: {
                 cardType: 'character',
-                gameAction: 'discardCardFromPlay',
-                cardCondition: card => this.game.currentConflict.isParticipating(card) && this.isAttacking()
+                gameAction: 'ready',
+                cardCondition: card => this.game.currentConflict.isParticipating(card) && card.controller === this.controller
             },
             handler: context => {
                 this.game.addMessage('{0} uses {1} to ready {2}', this.controller, this, context.target);
