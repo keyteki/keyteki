a331a90894b1245e1ad06d6d3490de6b21b1916a
cdmayberry1 <gmodman12@gmail.com>
2017-12-01 10:34:23 -0500

    Setup card

---
+++ server/game/cards/02.5-FHNS/HenshinDisciple.js
@@ -0,0 +1,11 @@
+const DrawCard = require('../../drawcard.js');
+
+class HenshinDisciple extends DrawCard {
+    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+        
+    }
+}
+
+HenshinDisciple.id = 'henshin-disciple'; // This is a guess at what the id might be - please check it!!!
+
+module.exports = HenshinDisciple;
