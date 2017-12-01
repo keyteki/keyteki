d566c1751e7017b76f6d10bf1c8a55f6a686706b
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-12-01 22:32:07 +0000

    stop facedown holdings from creating effects

--- server/game/cards/02.4-TCT/TheImperialPalace.js
+++ server/game/cards/02.4-TCT/TheImperialPalace.js
@@ -4,7 +4,7 @@ class TheImperialPalace extends DrawCard {
     setupCardAbilities(ability) { 
         this.persistentEffect({
             location: 'any',
-            condition: () => ['province 1', 'province 2', 'province 3', 'province 4', 'stronghold province'].includes(this.location),
+            condition: () => ['province 1', 'province 2', 'province 3', 'province 4', 'stronghold province'].includes(this.location) && !this.facedown,
             targetType: 'player',
             effect: ability.effects.changePlayerGloryModifier(3)
         });
