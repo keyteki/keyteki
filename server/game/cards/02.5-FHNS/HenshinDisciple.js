2f55ee3845ce26fdaf74b415e091b7da35e62dac
cdmayberry1 <gmodman12@gmail.com>
2017-12-05 10:52:17 -0500

    Implemented Henshin Disciple

--- server/game/cards/02.5-FHNS/HenshinDisciple.js
+++ server/game/cards/02.5-FHNS/HenshinDisciple.js
@@ -1,11 +1,25 @@
 const DrawCard = require('../../drawcard.js');
 
 class HenshinDisciple extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
-        
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => (this.game.rings['air'].claimed && this.game.rings['air'].claimedBy === this.controller.name) || this.game.rings['air'].contested,
+            match: this,
+            effect: ability.effects.modifyPoliticalSkill(2)
+        });
+        this.persistentEffect({
+            condition: () => (this.game.rings['earth'].claimed && this.game.rings['earth'].claimedBy === this.controller.name) || this.game.rings['earth'].contested,
+            match: this,
+            effect: ability.effects.modifyMilitarySkill(2)
+        });
+        this.persistentEffect({
+            condition: () => (this.game.rings['fire'].claimed && this.game.rings['fire'].claimedBy === this.controller.name) || this.game.rings['fire'].contested,
+            match: this,
+            effect: ability.effects.addKeyword('pride')
+        });
     }
 }
 
-HenshinDisciple.id = 'henshin-disciple'; // This is a guess at what the id might be - please check it!!!
+HenshinDisciple.id = 'henshin-disciple';
 
 module.exports = HenshinDisciple;
