dbb8c3f6830920ad09ec1b03ee1535adf47895d6
Patrick McQuay <patrick.mcquay@gmail.com>
2017-12-05 22:24:18 -0500

    Implmented Young Harrier

--- server/game/cards/02.5-FHNS/YoungHarrier.js
+++ server/game/cards/02.5-FHNS/YoungHarrier.js
@@ -1,10 +1,21 @@
 const DrawCard = require('../../drawcard.js');
 
 class YoungHarrier extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Prevent other characters from being dishonored',
+            cost: ability.costs.dishonorSelf(),
+            handler: () => {
+                this.game.addMessage('{0} dishonors {1} to prevent Crane characters from being dishonored this phase', this.controller, this);
+                this.untilEndOfPhase(ability => ({
+                    match: card => card.factions['crane'] >= 0 && card.controller === this.controller && card.getType() === 'character',
+                    effect: ability.effects.cannotBecomeDishonored()
+                }));
+            }
+        });
     }
 }
 
-YoungHarrier.id = 'young-harrier'; // This is a guess at what the id might be - please check it!!!
+YoungHarrier.id = 'young-harrier';
 
 module.exports = YoungHarrier;
