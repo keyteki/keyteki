21d1e6af11889e669e8ccf21561b345f3afd0bbd
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-12-07 05:33:41 +0000

    Add conflictProvince to condition

--- server/game/cards/02.5-FHNS/RideThemDown.js
+++ server/game/cards/02.5-FHNS/RideThemDown.js
@@ -5,7 +5,7 @@ class RideThemDown extends DrawCard {
         this.action({
             title: 'Reduce province strength',
             cost: ability.costs.discardImperialFavor(),
-            condition: () => this.game.currentConflict,
+            condition: () => this.game.currentConflict && this.game.currentConflict.conflictProvince,
             handler: () => {
                 let strengthToReduce = (this.game.currentConflict.conflictProvince.getBaseStrength() - 1) * -1;
                 this.game.addMessage('{0} uses {1} to reduce the strength of {2} by {3}', this.controller, this, this.game.currentConflict.conflictProvince, -strengthToReduce);
