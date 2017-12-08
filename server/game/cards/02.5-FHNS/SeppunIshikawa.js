3c69e28deef6040917673cdc073879ed5f75b0dd
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-12-08 20:40:55 +0000

    remove recalculateWhen and add holdings to imperial card calculation

--- server/game/cards/02.5-FHNS/SeppunIshikawa.js
+++ server/game/cards/02.5-FHNS/SeppunIshikawa.js
@@ -5,7 +5,6 @@ class SeppunIshikawa extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
             match: this,
-            recalculateWhen: ['onMove', 'onPlayIntoConflict', 'onCardTraitChanged', 'onCardEntersPlay', 'onProvinceRevealed', 'onCardTakenControl', 'onCardLeavesPlay', 'onCardSacrificed', 'onCardAttached', 'onBreakProvince'],
             effect: [
                 ability.effects.dynamicMilitarySkill(() => this.getImperialCardsInPlay()),
                 ability.effects.dynamicPoliticalSkill(() => this.getImperialCardsInPlay())
@@ -14,24 +13,15 @@ class SeppunIshikawa extends DrawCard {
     }
 
     getImperialCardsInPlay() {
-        if(this.controller && this.controller.cardsInPlay) {
-            return _.reduce(_.flatten([this.controller.cardsInPlay._wrapped, this.getProvinces()]), (sum, card) => {
-                if(card.hasTrait('imperial')) {
-                    return sum + 1;
-                }
-                return sum;
-            }, -1);
-        }
-        return 0;
-    }
-
-    getProvinces() {
-        let provinces = _.map(['province 1', 'province 2', 'province 3', 'province 4'], location => {
-            return this.controller.getProvinceCardInProvince(location);
-        });
-        return _.filter(provinces, province => {
-            return !province.isBroken && !province.facedown;
-        });
+        return this.game.allCards.reduce((sum, card) => {
+            if(card.controller === this.controller && card.hasTrait('imperial') && !card.facedown &&
+                (card.location === 'play area' || (card.isProvince && !card.isBroken && ) ||
+                (['province 1', 'province 2', 'province 3', 'province 4', 'stronghold province'].includes(card.location) && 
+                 card.type === 'holding'))) {
+                return sum + 1;
+            }
+            return sum;
+        }, 0);
     }
 }
 
