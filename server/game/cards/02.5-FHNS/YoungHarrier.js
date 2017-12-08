bc18c99fea36e44f14d3c87248175d1ccb6bb7cc
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-12-08 20:12:49 +0000

    Add a lasting effect to each eligible card

--- server/game/cards/02.5-FHNS/YoungHarrier.js
+++ server/game/cards/02.5-FHNS/YoungHarrier.js
@@ -7,10 +7,14 @@ class YoungHarrier extends DrawCard {
             cost: ability.costs.dishonorSelf(),
             handler: () => {
                 this.game.addMessage('{0} dishonors {1} to prevent Crane characters from being dishonored this phase', this.controller, this);
-                this.untilEndOfPhase(ability => ({
-                    match: card => card.factions['crane'] >= 0 && card.controller === this.controller && card.getType() === 'character',
-                    effect: ability.effects.cannotBecomeDishonored()
-                }));
+                this.controller.cardsInPlay.each(card => {
+                    if(card.isFaction('crane')) {
+                        this.untilEndOfPhase(ability => ({
+                            match: card,
+                            effect: ability.effects.cannotBeDishonored()
+                        }));
+                    }
+                });
             }
         });
     }
