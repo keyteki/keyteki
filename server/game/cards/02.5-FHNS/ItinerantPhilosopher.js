60ebfc1e3a278c95b9c97ce0754bf337118b4d96
Patrick McQuay <patrick.mcquay@gmail.com>
2017-12-08 20:22:08 -0500

    Implemented Itinerant Philosopher

--- server/game/cards/02.5-FHNS/ItinerantPhilosopher.js
+++ server/game/cards/02.5-FHNS/ItinerantPhilosopher.js
@@ -1,10 +1,25 @@
 const DrawCard = require('../../drawcard.js');
 
 class ItinerantPhilosopher extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Bow a character',
+            condition: () => this.game.currentConflict && this.game.currentConflict.isParticipating(this),
+            cost: ability.costs.discardImperialFavor(),
+            target: {
+                activePromptTitle: 'Choose an opposing character',
+                cardType: 'character',
+                cardCondition: card => this.game.currentConflict.isParticipating(card) &&
+                  card.controller === this.controller.opponent && card.attachments._wrapped.length > 0
+            },
+            handler: context => {
+                this.game.addMessage('{0} discards the Imperial Favor to use {1}, bowing {2}', this.controller, this, context.target);
+                this.controller.bowCard(context.target, this);
+            }
+        });
     }
 }
 
-ItinerantPhilosopher.id = 'itinerant-philosopher'; // This is a guess at what the id might be - please check it!!!
+ItinerantPhilosopher.id = 'itinerant-philosopher';
 
 module.exports = ItinerantPhilosopher;
