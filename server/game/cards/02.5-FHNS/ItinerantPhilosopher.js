a50d10d23518af8dfcdbf0b21953d0e95bf7c692
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-12-09 06:48:20 +0000

    Prettify code a little

--- server/game/cards/02.5-FHNS/ItinerantPhilosopher.js
+++ server/game/cards/02.5-FHNS/ItinerantPhilosopher.js
@@ -4,13 +4,12 @@ class ItinerantPhilosopher extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
             title: 'Bow a character',
-            condition: () => this.game.currentConflict && this.game.currentConflict.isParticipating(this),
+            condition: () => this.isParticipating(),
             cost: ability.costs.discardImperialFavor(),
             target: {
-                activePromptTitle: 'Choose an opposing character',
                 cardType: 'character',
-                cardCondition: card => this.game.currentConflict.isParticipating(card) &&
-                  card.controller === this.controller.opponent && card.attachments._wrapped.length > 0
+                cardCondition: card => card.isParticipating() && card.controller === this.controller.opponent && 
+                                       card.attachments.size() > 0
             },
             handler: context => {
                 this.game.addMessage('{0} discards the Imperial Favor to use {1}, bowing {2}', this.controller, this, context.target);
