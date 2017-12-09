a266a7936b74c305f127241b9c7887e184dd46a4
Patrick McQuay <patrick.mcquay@gmail.com>
2017-12-08 21:27:31 -0500

    Implemented Yasuki Taka

--- server/game/cards/02.5-FHNS/YasukiTaka.js
+++ server/game/cards/02.5-FHNS/YasukiTaka.js
@@ -1,10 +1,22 @@
 const DrawCard = require('../../drawcard.js');
 
 class YasukiTaka extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.reaction({
+            title: 'Gain fate',
+            when: {
+                onCardLeavesPlay: (event, params) => params.card.controller === this.controller && params.card.factions['crab'] > 0 && params.card.type === 'character'
+            },
+            phase: 'conflict',
+            limit: ability.limit.perPhase(Infinity),
+            handler: () => {
+                this.game.addMessage('{0} uses {1} to gain 1 fate', this.controller, this);
+                this.game.addFate(this.controller, 1);
+            }
+        });
     }
 }
 
-YasukiTaka.id = 'yasuki-taka'; // This is a guess at what the id might be - please check it!!!
+YasukiTaka.id = 'yasuki-taka';
 
 module.exports = YasukiTaka;
