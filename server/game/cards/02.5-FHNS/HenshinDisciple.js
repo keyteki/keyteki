15a23891500e62d8c17c78fc904578e6907a6578
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-12-12 17:21:25 +0000

    lint fix

--- server/game/cards/02.5-FHNS/HenshinDisciple.js
+++ server/game/cards/02.5-FHNS/HenshinDisciple.js
@@ -5,24 +5,21 @@ class HenshinDisciple extends DrawCard {
         this.persistentEffect({
             condition: () => (
                 this.game.rings['air'].claimed && this.game.rings['air'].claimedBy === this.controller.name) || 
-                (this.game.currentConflict && this.game.currentConflict.hasElement('air')
-            ),
+                (this.game.currentConflict && this.game.currentConflict.hasElement('air')),
             match: this,
             effect: ability.effects.modifyPoliticalSkill(2)
         });
         this.persistentEffect({
             condition: () => (
                 this.game.rings['earth'].claimed && this.game.rings['earth'].claimedBy === this.controller.name) || 
-                (this.game.currentConflict && this.game.currentConflict.hasElement('earth')
-            ),
+                (this.game.currentConflict && this.game.currentConflict.hasElement('earth')),
             match: this,
             effect: ability.effects.modifyMilitarySkill(2)
         });
         this.persistentEffect({
             condition: () => (
                 this.game.rings['fire'].claimed && this.game.rings['fire'].claimedBy === this.controller.name) || 
-                (this.game.currentConflict && this.game.currentConflict.hasElement('fire')
-            ),
+                (this.game.currentConflict && this.game.currentConflict.hasElement('fire')),
             match: this,
             effect: ability.effects.addKeyword('pride')
         });
