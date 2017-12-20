1a4b0b5d60bb322e2a426438a0f496857c5fad10
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2017-12-20 11:02:41 +0000

    more test fixes

--- test/server/card/cardforcedreaction.spec.js
+++ test/server/card/cardforcedreaction.spec.js
@@ -1,5 +1,5 @@
 const CardForcedReaction = require('../../../server/game/cardforcedreaction.js');
-const Event = require('../../../server/game/event.js');
+const Event = require('../../../server/game/events/Event');
 
 describe('CardForcedReaction', function () {
     beforeEach(function () {
