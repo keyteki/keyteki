93a23e825d468e78a5536b67939ad8dcb88fc1cb
pwitko <piotr@landingi.com>
2018-01-09 21:17:14 +0100

    Add UUIDs for messages in chat to recognize keys

--- client/GameComponents/Messages.jsx
+++ client/GameComponents/Messages.jsx
@@ -6,6 +6,8 @@ import _ from 'underscore';
 import Avatar from '../Avatar.jsx';
 import * as actions from '../actions';
 
+const uuid = require('uuid')
+
 class InnerMessages extends React.Component {
     constructor() {
         super();
@@ -41,9 +43,8 @@ class InnerMessages extends React.Component {
     }
 
     getMessage() {
-        var index = 0;
         var messages = _.map(this.props.messages, message => {
-            return <div key={ 'message' + index++ } className='message'>{ this.formatMessageText(message.message) }</div>;
+            return <div key={ 'message' + uuid() } className='message'>{ this.formatMessageText(message.message) }</div>;
         });
 
         return messages;
