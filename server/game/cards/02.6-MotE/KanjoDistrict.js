defe437948013f4b9f595faf1b9d424a4b02d4ce
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-01-10 17:54:06 +0000

    Kanjo District bugfix

--- server/game/cards/02.6-MotE/KanjoDistrict.js
+++ server/game/cards/02.6-MotE/KanjoDistrict.js
@@ -13,7 +13,7 @@ class KanjoDistrict extends DrawCard {
                 let message = '';
                 if(context.target.allowGameAction('bow')) {
                     message = '{0} uses {1} to bow {2}';
-                    this.controller.bowCard(context.target);
+                    this.controller.bowCard(context.target, this);
                 }
                 if(context.target.allowGameAction('sendHome')) {
                     message = message ? '{0} uses {1} to bow and send {2} home' : '{0} uses {1} to send {2} home';
