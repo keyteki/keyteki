33ae944e26f6645f9bdbddebb9782896e1fc76ec
artemmikhalitsin <artemmikhalitsin@gmail.com>
2018-01-15 10:29:03 -0500

    Ambush bugfix

--- test/helpers/integrationhelper.js
+++ test/helpers/integrationhelper.js
@@ -173,7 +173,7 @@ global.integration = function(definitions) {
                     return;
                 }
                 this.noMoreActions();
-            }
+            };
         });
 
         definitions();
