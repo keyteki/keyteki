d4d381e3b3bcbb3fc5a378030bff2a073ad00112
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-02-21 02:41:57 +0000

    shosuro actress bugfix

--- server/game/cards/01-Core/ShosuroActress.js
+++ server/game/cards/01-Core/ShosuroActress.js
@@ -9,13 +9,13 @@ class ShosuroActress extends DrawCard {
             target: {
                 cardType: 'character',
                 gameAction: 'putIntoConflict',
-                cardCondition: card => {
-                    return (card.owner !== this.controller && card.location.includes('discard pile') && 
+                cardCondition: (card, context) => {
+                    return (card.owner !== context.player && card.location.includes('discard pile') && 
                             card.getCost() < 4 && !card.hasTrait('shinobi'));
                 }
             },
             handler: context => {
-                this.game.addMessage('{0} sacrifices {1} to put {2} into play under their control', context.cardStateWhenInitiated.controller, this, context.target);
+                this.game.addMessage('{0} sacrifices {1} to put {2} into play under their control', context.player, this, context.target);
                 this.game.applyGameAction(context, { putIntoConflict: context.target });
             }
         });
