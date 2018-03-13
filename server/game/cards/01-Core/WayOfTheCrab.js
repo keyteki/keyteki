027cc8b34c68ef571bc815593333b684a095ec14
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-03-13 02:11:22 +0000

    WotCrab bugfix (#1696)

--- server/game/cards/01-Core/WayOfTheCrab.js
+++ server/game/cards/01-Core/WayOfTheCrab.js
@@ -7,18 +7,24 @@ class WayOfTheCrab extends DrawCard {
             condition: context => context.player.opponent && context.player.opponent.cardsInPlay.any(card => card.allowGameAction('sacrifice', context)),
             cost: ability.costs.sacrifice(card => card.type === 'character' && card.isFaction('crab')),
             max: ability.limit.perRound(1),
-            handler: context => this.game.promptForSelect(this.controller.opponent, {
-                activePromptTitle: 'Choose a character to sacrifice',
-                source: this,
-                gameAction: 'sacrifice',
-                cardCondition: card => card.location === 'play area' && card.controller === this.controller.opponent,
-                cardType: 'character',
-                onSelect: (player, card) => {
-                    this.game.addMessage('{0} sacrifices {1} using {2} to force {3} to sacrifice {4}', this.controller, context.costs.sacrifice, this, player, card);
-                    this.game.applyGameAction(context, { sacrifice: card });
-                    return true;
+            handler: context => {
+                if(context.player.opponent.cardsInPlay.any(card => card.allowGameAction('sacrifice', context))) {
+                    this.game.promptForSelect(context.player.opponent, {
+                        activePromptTitle: 'Choose a character to sacrifice',
+                        source: this,
+                        gameAction: 'sacrifice',
+                        cardCondition: card => card.location === 'play area' && card.controller === context.player.opponent,
+                        cardType: 'character',
+                        onSelect: (player, card) => {
+                            this.game.addMessage('{0} sacrifices {1} using {2} to force {3} to sacrifice {4}', this.controller, context.costs.sacrifice, this, player, card);
+                            this.game.applyGameAction(context, { sacrifice: card });
+                            return true;
+                        }
+                    });
+                } else {
+                    this.game.addMessage('{0} sacrifices {1} using {2}, but {3} has nothing to sacrifice', this.controller, context.costs.sacrifice, this, this.controller.opponent);
                 }
-            })
+            }
         });
     }
 }
