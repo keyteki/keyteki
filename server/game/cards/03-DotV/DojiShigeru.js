f7b7a68ca2f65d867113b524531a9b2c7510b556
Travis McDaniel <Travis959200@gmail.com>
2018-04-04 21:12:51 -0400

    Doji Shigeru (#1778)
    * Add files via upload
    
    * Update DojiShigeru.js
    
    * Update DojiShigeru.js
    
    Use a single card prompt, not an exactly one

---
+++ server/game/cards/03-DotV/DojiShigeru.js
@@ -0,0 +1,34 @@
+const DrawCard = require('../../drawcard.js');
+
+class DojiShigeru extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            title: 'Opponent discards a card',
+            limit: ability.limit.unlimitedPerConflict(),
+            when: {
+                onCardPlayed: event => {
+                    return (event.player === this.controller.opponent && 
+                            event.card.type === 'event' && 
+                            this.game.currentConflict.isParticipating());
+                }
+            },
+
+            handler: () => {
+                this.game.addMessage('{0} uses {1} to force {2} to choose and discard a card', this.controller, this, this.controller.opponent);
+                this.game.promptForSelect(this.controller.opponent, {
+                    activePromptTitle: 'Choose a card to discard',
+                    source: this,
+                    cardCondition: card => card.location === 'hand',
+                    onSelect: (player, card) => {
+                        player.discardCardsFromHand(card);
+                        return true;
+                    }
+                });
+            }
+        });
+    }
+}
+
+DojiShigeru.id = 'doji-shigeru';
+
+module.exports = DojiShigeru;
