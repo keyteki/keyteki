fa064e542fc46a0d6f47ca4b4b3c53c5c7128bc0
Caelan Mayberry <gmodman12@gmail.com>
2018-04-04 22:39:02 -0400

    Added clarity of purpose (#1750)
    * Clarity of Purpose added. Cannot be bowed effect still being worked on.
    
    * wording
    
    * Now prevents bowing from opponents cards.
    
    * Not bowing in political resolutions now re-checks condition.
    
    * Update ClarityOfPurpose.js

--- server/game/cards/03-DotV/ClarityOfPurpose.js
+++ server/game/cards/03-DotV/ClarityOfPurpose.js
@@ -1,10 +1,34 @@
 const DrawCard = require('../../drawcard.js');
 
 class ClarityOfPurpose extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities() {
+        this.action({
+            title: 'Opponent\'s cards cannot bow the target character and it does not bow as a result of political conflicts',
+            condition: () => this.game.currentConflict,
+            target: {
+                activePromptTitle: 'Choose a character',
+                cardType: 'character',
+                cardCondition: card => card.controller === this.controller && card.location === 'play area'
+            },
+            handler: context => {
+                this.game.addMessage('{0} plays {1} on {2} to prevent opponents\' actions from bowing it and stop it bowing at the end of a political conflict', this.controller, this, context.target);
+                this.untilEndOfConflict(ability => ({
+                    match: context.target,
+                    effect: ability.effects.cannotBeBowed(context => context && context.source.type !== 'ring' && context.source.controller === this.controller.opponent)
+                }));
+                this.untilEndOfConflict(ability => ({
+                    match: context.target,
+                    condition: () => this.game.currentConflict && this.game.currentConflict.conflictType === 'political',
+                    effect: [
+                        ability.effects.doesNotBowAsAttacker(),
+                        ability.effects.doesNotBowAsDefender()
+                    ]
+                }));
+            }
+        });
     }
 }
 
-ClarityOfPurpose.id = 'clarity-of-purpose'; // This is a guess at what the id might be - please check it!!!
+ClarityOfPurpose.id = 'clarity-of-purpose';
 
 module.exports = ClarityOfPurpose;
