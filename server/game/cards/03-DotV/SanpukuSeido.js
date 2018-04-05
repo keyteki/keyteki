6ffaa26a24c003be88bdd72fc492c41f1a549875
Travis McDaniel <Travis959200@gmail.com>
2018-04-04 22:42:04 -0400

    Sanpuku Seido Implementation (#1720)
    * Add files via upload
    
    * Update SanpukuSeido.js

---
+++ server/game/cards/03-DotV/SanpukuSeido.js
@@ -0,0 +1,20 @@
+const ProvinceCard = require('../../provincecard.js');
+
+class SanpukuSeido extends ProvinceCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: this,
+            targetLocation: 'province',
+            condition: () => this.game.currentConflict && this.game.currentConflict.conflictProvince === this && !this.facedown,
+            effect: ability.effects.changeConflictSkillFunction(card => card.getGlory())
+        });
+    }
+
+    cannotBeStrongholdProvince() {
+        return true;
+    }
+}
+
+SanpukuSeido.id = 'sanpuku-seido';
+
+module.exports = SanpukuSeido;
