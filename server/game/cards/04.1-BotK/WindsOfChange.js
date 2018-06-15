71d1e8e769ecad7bd3d40d896bf6fd6e6e5f9f91
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-06-15 06:00:07 +0100

    placeholders for BotK (#1902)

---
+++ server/game/cards/04.1-BotK/WindsOfChange.js
@@ -0,0 +1,10 @@
+const DrawCard = require('../../drawcard.js');
+
+class WindsOfChange extends DrawCard {
+    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    }
+}
+
+WindsOfChange.id = 'winds-of-change'; // This is a guess at what the id might be - please check it!!!
+
+module.exports = WindsOfChange;
