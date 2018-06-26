4c48d31befc4d33ce4868ed60c0ff05795dea505
Kwaice <kwaice@gmail.com>
2018-06-26 19:54:47 +0200

    BotK: Sacred Sanctuary (#2063)
    * BotK: Sacred Sanctuary
    
    Closes #1977
    
    * BotK: Sacred Sanctuary
    
    * Update SacredSanctuary.js
    
    * Update SacredSanctuary.js
    
    * Update SacredSanctuary.js
    
    * Update SacredSanctuary.js
    
    Formatting
    
    * ESLint Fix

--- server/game/cards/04.1-BotK/SacredSanctuary.js
+++ server/game/cards/04.1-BotK/SacredSanctuary.js
@@ -1,10 +1,34 @@
 const ProvinceCard = require('../../provincecard.js');
 
 class SacredSanctuary extends ProvinceCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.reaction({
+            title: 'Choose a monk character',
+            when: {
+                onConflictDeclared: (event, context) => event.conflict.conflictProvince === context.source
+            },
+            target: {
+                controller: 'self',
+                cardCondition: card => card.hasTrait('monk'),
+                gameAction: [
+                    ability.actions.ready(),
+                    ability.actions.cardLastingEffect({
+                        condition: () => this.game.isDuringConflict(),
+                        effect: ability.effects.doesNotBow()
+                    }),
+                    ability.actions.cardLastingEffect(context => ({
+                        effect: ability.effects.cardCannot('bow', abilityContext => (
+                            abilityContext.source.type !== 'ring' && context.player.opponent &&
+                            abilityContext.source.controller === context.player.opponent
+                        ))
+                    }))
+                ]
+            },
+            effect: 'prevent opponents\' actions from bowing {0} and stop it bowing at the end of the conflict'
+        });
     }
 }
 
-SacredSanctuary.id = 'sacred-sanctuary'; // This is a guess at what the id might be - please check it!!!
+SacredSanctuary.id = 'sacred-sanctuary';
 
 module.exports = SacredSanctuary;
