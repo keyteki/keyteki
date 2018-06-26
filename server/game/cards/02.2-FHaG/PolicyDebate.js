f7abba7292a220b2942eb230bd56ac0fea3c0be1
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-06-26 18:54:02 +0100

    Policy debate bugfix (#2065)

--- server/game/cards/02.2-FHaG/PolicyDebate.js
+++ server/game/cards/02.2-FHaG/PolicyDebate.js
@@ -35,7 +35,10 @@ class PolicyDebate extends DrawCard {
                 activePromptTitle: 'Choose card to discard',
                 context: context,
                 cards: loser.controller.hand.sortBy(card => card.name),
-                cardHandler: card => this.game.applyGameAction(context, { discardCard: card })
+                cardHandler: card => {
+                    this.game.addMessage('{0} chooses to discard {1}', winner.controller, card);
+                    this.game.applyGameAction(context, { discardCard: card });
+                }
             });
         }
     }
