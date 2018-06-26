134adab51b704521d4a5fd3a5703c4246f4d6aeb
Martin Schröder <martinschroeder13@yahoo.de>
2018-06-26 19:56:34 +0200

    Implemented Ancient Master (#2036)

--- server/game/cards/04.1-BotK/AncientMaster.js
+++ server/game/cards/04.1-BotK/AncientMaster.js
@@ -1,10 +1,47 @@
 const DrawCard = require('../../drawcard.js');
+const PlayAttachmentAction = require('../../playattachmentaction.js');
 
-class AncientMaster extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+class PlayAncientMasterAsAttachment extends PlayAttachmentAction {
+    constructor(card) {
+        super(card);
+        this.title = 'Play Ancient Master as an attachment';
+    }
+
+    executeHandler(context) {
+        context.source.type = 'attachment';
+        super.executeHandler(context);
     }
 }
 
-AncientMaster.id = 'ancient-master'; // This is a guess at what the id might be - please check it!!!
+class AncientMaster extends DrawCard {
+    setupCardAbilities(ability) {
+        this.abilities.playActions.push(new PlayAncientMasterAsAttachment(this));
+        this.reaction({
+            title: 'Search top 5 card for kiho or tattoo',
+            when: {
+                onConflictDeclared: (event, context) => context.source.type === 'attachment' && context.source.parent.isAttacking(),
+                onDefendersDeclared: (event, context) => context.source.type === 'attachment' && context.source.parent.isDefending()
+            },
+            printedAbility: false,
+            effect: 'look at the top five cards of their deck',
+            gameAction: ability.actions.deckSearch({
+                amount: 5,
+                cardCondition: card => card.hasTrait('kiho') || card.hasTrait('tattoo')
+            })
+        });
+    }
+
+    // Remove the check on being an attachment when checking whether this can be played as one
+    canAttach(card) {
+        return card && card.getType() === 'character';
+    }
+
+    leavesPlay() {
+        this.type = 'character';
+        super.leavesPlay();
+    }
+}
+
+AncientMaster.id = 'ancient-master';
 
 module.exports = AncientMaster;
