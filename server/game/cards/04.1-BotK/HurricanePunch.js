5d024890663184bf4d684025deff78a48f175070
Noah <nbtheduke@gmail.com>
2018-06-26 13:58:37 -0400

    BotK: Hurricane Punch (#1910)
    * Implements Hurricane Punch
    
    * Fixes test and ability

--- server/game/cards/04.1-BotK/HurricanePunch.js
+++ server/game/cards/04.1-BotK/HurricanePunch.js
@@ -1,10 +1,23 @@
 const DrawCard = require('../../drawcard.js');
 
 class HurricanePunch extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Increase a monk\'s military skill and draw 1 card',
+            effect: 'grant 2 military skill to {0} and draw a card',
+            condition: () => this.game.isDuringConflict(),
+            target: {
+                cardType: 'character',
+                cardCondition: card => card.isParticipating() && card.hasTrait('monk'),
+                gameAction: ability.actions.cardLastingEffect(() => ({
+                    effect: ability.effects.modifyMilitarySkill(2)
+                }))
+            },
+            gameAction: ability.actions.draw()
+        });
     }
 }
 
-HurricanePunch.id = 'hurricane-punch'; // This is a guess at what the id might be - please check it!!!
+HurricanePunch.id = 'hurricane-punch';
 
 module.exports = HurricanePunch;
