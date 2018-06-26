142d09f746c5a1ca83c70a6d5a3b9a0f6558c101
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-06-26 18:53:51 +0100

    BotK: Hiruma Outpost (#2066)
    * BotK: Hiruma Outpost
    
    * test

--- server/game/cards/04.1-BotK/HirumaOutpost.js
+++ server/game/cards/04.1-BotK/HirumaOutpost.js
@@ -1,10 +1,33 @@
 const DrawCard = require('../../drawcard.js');
 
 class HirumaOutpost extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.grantedAbilityLimits = {};
+        this.persistentEffect({
+            condition: () => !this.controller.getProvinceCardInProvince(this.location).isBroken,
+            match: this,
+            effect: ability.effects.gainAbility('reaction', {
+                title: 'Make opponent lose an honor',
+                when: {
+                    onConflictDeclared: (event, context) => {
+                        if(event.conflict.attackingPlayer === context.player) {
+                            return false;
+                        }
+                        let card = context.player.getDynastyCardInProvince(event.conflict.conflictProvince.location);
+                        return !card || card.facedown || card.type !== 'holding';
+                    }
+                },
+                gameAction: ability.actions.loseHonor()
+            })
+        });
+    }
+
+    leavesPlay() {
+        this.grantedAbilityLimits = {};
+        super.leavesPlay();
     }
 }
 
-HirumaOutpost.id = 'hiruma-outpost'; // This is a guess at what the id might be - please check it!!!
+HirumaOutpost.id = 'hiruma-outpost';
 
 module.exports = HirumaOutpost;
---
+++ test/server/cards/04.1-BotK/HirumaOutpost.spec.js
@@ -0,0 +1,89 @@
+fdescribe('Hiruma Outpose', function() {
+    integration(function() {
+        describe('Hiruma Outpose\'s ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        honor: 12,
+                        inPlay: ['matsu-berserker']
+                    },
+                    player2: {
+                        dynastyDeck: ['hiruma-outpost', 'imperial-storehouse']
+                    }
+                });
+                this.hirumaOutpost = this.player2.placeCardInProvince('hiruma-outpost');
+                this.imperialStorehouse = this.player2.placeCardInProvince('imperial-storehouse', 'province 2');
+                this.shamefulDisplay = this.player2.findCardByName('shameful-display', 'province 2');
+            });
+
+            fit('should trigger when an attack is declared against a province without a holding', function() {
+                this.player1.pass();
+                this.player2.clickCard(this.imperialStorehouse);
+                this.noMoreActions();
+                this.initiateConflict({
+                    province: this.shamefulDisplay,
+                    attackers: ['matsu-berserker']
+                });
+                console.log(this.hirumaOutpost.abilities.reactions);
+                console.log(this.game.effectEngine.getDebugInfo());
+                expect(this.player2).toHavePrompt('Triggered Abilities');
+                expect(this.player2).toBeAbleToSelect(this.hirumaOutpost);
+            });
+
+            it('should trigger when an attack is declared against a province with a facedown holding', function() {
+                this.imperialStorehouse.facedown = true;
+                this.noMoreActions();
+                this.initiateConflict({
+                    province: this.shamefulDisplay,
+                    attackers: ['matsu-berserker']
+                });
+                expect(this.player2).toHavePrompt('Triggered Abilities');
+                expect(this.player2).toBeAbleToSelect(this.hirumaOutpost);
+            });
+
+            it('should not trigger when an attack is declared against a province with a holding', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    province: this.shamefulDisplay,
+                    attackers: ['matsu-berserker']
+                });
+                expect(this.player2).toHavePrompt('Choose defenders');
+            });
+
+            it('shouldn\'t trigger when Hiruma Outpost is facedown', function() {
+                this.hirumaOutpost.facedown = true;
+                this.game.checkGameState(true);
+                this.noMoreActions();
+                this.initiateConflict({
+                    province: this.shamefulDisplay,
+                    attackers: ['matsu-berserker']
+                });
+                expect(this.player2).toHavePrompt('Choose defenders');
+            });
+
+            it('shouldn\'t trigger when Hiruma Outpost is on a broken province', function() {
+                this.player2.findCardByName('shameful-display', 'province 1').isBroken = true;
+                this.game.checkGameState(true);
+                this.noMoreActions();
+                this.initiateConflict({
+                    province: this.shamefulDisplay,
+                    attackers: ['matsu-berserker']
+                });
+                expect(this.player2).toHavePrompt('Choose defenders');
+            });
+
+            it('should make the opponent lose one honor', function() {
+                this.player1.pass();
+                this.player2.clickCard(this.imperialStorehouse);
+                this.noMoreActions();
+                this.initiateConflict({
+                    province: this.shamefulDisplay,
+                    attackers: ['matsu-berserker']
+                });
+                this.player2.clickCard(this.hirumaOutpost);
+                expect(this.player1.honor).toBe(11);
+            });
+        });
+    });
+});
