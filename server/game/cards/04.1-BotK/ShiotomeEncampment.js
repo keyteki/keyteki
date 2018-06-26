a99cef6a7247ea956a7f2239d2c7515bac12da4c
Noah <nbtheduke@gmail.com>
2018-06-26 13:53:03 -0400

    BotK: Shiotome Encampment (#2070)
    * Implements Shiotome Encampment
    
    * Removed debugging

--- server/game/cards/04.1-BotK/ShiotomeEncampment.js
+++ server/game/cards/04.1-BotK/ShiotomeEncampment.js
@@ -1,10 +1,25 @@
 const DrawCard = require('../../drawcard.js');
 
 class ShiotomeEncampment extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Ready a Cavalry character',
+            effect: 'ready a Cavalry character',
+            condition: context =>
+                Object.values(this.game.rings).some(
+                    ring =>
+                        ring.isConsideredClaimed(context.player) &&
+                        ring.isConflictType('military')
+                ),
+            target: {
+                cardType: 'character',
+                cardCondition: card => card.hasTrait('cavalry'),
+                gameAction: ability.actions.ready()
+            }
+        });
     }
 }
 
-ShiotomeEncampment.id = 'shiotome-encampment'; // This is a guess at what the id might be - please check it!!!
+ShiotomeEncampment.id = 'shiotome-encampment';
 
 module.exports = ShiotomeEncampment;
