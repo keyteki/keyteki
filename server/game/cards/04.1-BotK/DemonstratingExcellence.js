4abcfa0e459ed5e62f0b4413f2f844a7cc87c3a0
Noah <nbtheduke@gmail.com>
2018-06-26 13:58:22 -0400

    BotK: Demonstrating Excellence (#1924)
    * Implemented with basic test
    
    * Revert fdescribe

--- server/game/cards/04.1-BotK/DemonstratingExcellence.js
+++ server/game/cards/04.1-BotK/DemonstratingExcellence.js
@@ -1,10 +1,27 @@
 const ProvinceCard = require('../../provincecard.js');
 
 class DemonstratingExcellence extends ProvinceCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: this,
+            targetLocation: 'province',
+            condition: () => this.controller.role.hasTrait('air'),
+            effect: ability.effects.modifyProvinceStrength(2)
+        });
+
+        this.interrupt({
+            title: 'Gain 1 fate and draw 1 card',
+            when: {
+                onBreakProvince: (event, context) => event.card === context.source
+            },
+            gameAction: [
+                ability.actions.gainFate(),
+                ability.actions.draw()
+            ]
+        });
     }
 }
 
-DemonstratingExcellence.id = 'demonstrating-excellence'; // This is a guess at what the id might be - please check it!!!
+DemonstratingExcellence.id = 'demonstrating-excellence';
 
 module.exports = DemonstratingExcellence;
