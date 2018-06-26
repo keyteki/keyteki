b4639aaefcf7aeba4030ddd751a512ef6933d672
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-06-26 18:52:49 +0100

    Illustrious Plagiarist bugfix (#2071)
    * Illustrious Plagiarist bugfix
    
    * remove debugging

--- test/server/cards/02.6-MotE/IllustriousPlagiarist.spec.js
+++ test/server/cards/02.6-MotE/IllustriousPlagiarist.spec.js
@@ -38,5 +38,50 @@ describe('Illustrious Plagiarist', function() {
                 expect(this.dojiChallenger.bowed).toBe(true);
             });
         });
+
+        describe('When copying Assassinate', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        fate: 1,
+                        honor: 10,
+                        inPlay: ['bayushi-liar', 'illustrious-plagiarist'],
+                        hand: ['cloud-the-mind']
+                    },
+                    player2: {
+                        inPlay: ['doji-whisperer'],
+                        hand: ['assassination', 'banzai']
+                    }
+                });
+                this.bayushiLiar = this.player1.findCardByName('bayushi-liar');
+                this.dojiWhisperer = this.player2.findCardByName('doji-whisperer');
+                this.noMoreActions();
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: [this.bayushiLiar],
+                    defenders: [this.dojiWhisperer]
+                });
+                this.assassination = this.player2.clickCard('assassination');
+                this.player2.clickCard(this.bayushiLiar);
+            });
+
+            it('should allow plagiarist to copy Assassinate', function() {
+                expect(this.bayushiLiar.location).toBe('dynasty discard pile');
+                this.player1.playAttachment('cloud-the-mind', this.dojiWhisperer);
+                this.player2.pass();
+                this.illustriousPlagiarist = this.player1.clickCard('illustrious-plagiarist');
+                this.player1.clickCard(this.assassination);
+                this.player2.clickCard('banzai');
+                this.player2.clickCard(this.dojiWhisperer);
+                this.player2.clickPrompt('Done');
+                this.player1.clickCard(this.illustriousPlagiarist);
+                expect(this.player1).toHavePrompt('Illustrious Plagiarist');
+                expect(this.player1).toBeAbleToSelect(this.dojiWhisperer);
+                this.player1.clickCard(this.dojiWhisperer);
+                expect(this.dojiWhisperer.location).toBe('dynasty discard pile');
+                expect(this.player1.honor).toBe(7);
+            });
+        });
     });
 });
