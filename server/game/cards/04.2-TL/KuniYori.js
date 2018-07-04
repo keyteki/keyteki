5cf9cccb2e84a1273490dad54b45ae8c8b71564a
Kwaice <kwaice@gmail.com>
2018-07-04 04:50:26 +0200

    TL: Kuni Yori (#2098)
    * TL: Kuni Yori
    
    * Prompt fix
    
    * Fix
    
    * Title fix
    
    * Directory change

--- server/game/cards/04.2-TL/KuniYori.js
+++ server/game/cards/04.2-TL/KuniYori.js
@@ -1,10 +1,28 @@
 const DrawCard = require('../../drawcard.js');
 
 class KuniYori extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.game.isDuringConflict('earth'),
+            effect: ability.effects.modifyBothSkills(1)
+        });
+
+        this.action({
+            title: 'Select a player to discard a card at random',
+            condition: () => this.game.isDuringConflict(),
+            cost: ability.costs.payHonor(1),
+            target: {
+                mode: 'select',
+                activePromptTitle:'Select a player to discard a random card from his/her hand',
+                choices: {
+                    'Me': ability.actions.discardAtRandom(context => ({ target: context.player })),
+                    'My Opponent': ability.actions.discardAtRandom()
+                }
+            }
+        });
     }
 }
 
-KuniYori.id = 'kuni-yori'; // This is a guess at what the id might be - please check it!!!
+KuniYori.id = 'kuni-yori';
 
 module.exports = KuniYori;
