b6ce8604a98e67832d02445634f8907129dee9aa
Caelan Mayberry <gmodman12@gmail.com>
2018-07-03 22:53:26 -0400

    TL: Feral Ningyo (#2028)
    * Looking at feral ningyo
    
    * Feral Ningyo and tests added
    
    * Simplified condition, added shuffle test in preparation
    
    * eslint

--- server/game/cards/04.2-TL/FeralNingyo.js
+++ server/game/cards/04.2-TL/FeralNingyo.js
@@ -1,10 +1,25 @@
 const DrawCard = require('../../drawcard.js');
 
 class FeralNingyo extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Put into play',
+            condition: () => this.game.isDuringConflict('water'),
+            location: 'hand',
+            gameAction: ability.actions.putIntoConflict(),
+            then: () => ({
+                gameAction: ability.actions.delayedEffect({
+                    when: {
+                        onConflictFinished: () => true
+                    },
+                    message: '{0} returns to the deck and shuffles due to it\'s effect',
+                    gameAction: ability.actions.returnToDeck({ shuffle: true })
+                })
+            })
+        });
     }
 }
 
-FeralNingyo.id = 'feral-ningyo'; // This is a guess at what the id might be - please check it!!!
+FeralNingyo.id = 'feral-ningyo';
 
 module.exports = FeralNingyo;
