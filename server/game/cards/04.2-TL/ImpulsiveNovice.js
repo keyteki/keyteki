b7df29e4d42284fc7c29fba6f56403ae401e96e2
Martin Schröder <martinschroeder13@yahoo.de>
2018-07-04 04:42:57 +0200

    TL: Impulsive Novice (#2127)
    * Implemented Impulsive Novice
    
    * Fixed Impulsive Novice and Tests

--- server/game/cards/04.2-TL/ImpulsiveNovice.js
+++ server/game/cards/04.2-TL/ImpulsiveNovice.js
@@ -1,10 +1,17 @@
 const DrawCard = require('../../drawcard.js');
 
 class ImpulsiveNovice extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => (
+                this.game.rings.fire.isConsideredClaimed(this.controller) ||
+                this.game.rings.void.isConsideredClaimed(this.controller)),
+            match: this,
+            effect: ability.effects.modifyBothSkills(1)
+        });
     }
 }
 
-ImpulsiveNovice.id = 'impulsive-novice'; // This is a guess at what the id might be - please check it!!!
+ImpulsiveNovice.id = 'impulsive-novice';
 
 module.exports = ImpulsiveNovice;
--- test/server/cards/04.2-TL/ImpulsiveNovice.spec.js
+++ test/server/cards/04.2-TL/ImpulsiveNovice.spec.js
@@ -11,19 +11,39 @@ describe('Impulsive Novice', function() {
                         inPlay: []
                     }
                 });
+                this.novice = this.player1.findCardByName('impulsive-novice');
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('should give +1 to both skills if the fire ring is claimed', function() {
+                this.game.rings.fire.claimRing(this.player1.player);
+                this.game.checkGameState(true);
+                expect(this.novice.getMilitarySkill()).toBe(3);
+                expect(this.novice.getPoliticalSkill()).toBe(3);
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
+            it('should give +1 to both skills if the void ring is claimed', function() {
+                this.game.rings.void.claimRing(this.player1.player);
+                this.game.checkGameState(true);
+                expect(this.novice.getMilitarySkill()).toBe(3);
+                expect(this.novice.getPoliticalSkill()).toBe(3);
             });
 
-            it('should have DEF effect on GHI', function() {
+            it('not give +1 to both skills if the opponent claimed the void or fire ring', function() {
+                this.game.rings.void.claimRing(this.player2.player);
+                this.game.rings.fire.claimRing(this.player2.player);
+                this.game.checkGameState(true);
+                expect(this.novice.getMilitarySkill()).toBe(2);
+                expect(this.novice.getPoliticalSkill()).toBe(2);
+            });
 
+            it('not give +1 to both skills if any other ring is claimed', function() {
+                this.game.rings.water.claimRing(this.player1.player);
+                this.game.rings.air.claimRing(this.player1.player);
+                this.game.rings.earth.claimRing(this.player1.player);
+                this.game.checkGameState(true);
+                expect(this.novice.getMilitarySkill()).toBe(2);
+                expect(this.novice.getPoliticalSkill()).toBe(2);
             });
         });
     });
