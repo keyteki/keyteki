b98501c95b105c071162fa019af45a47d2a13671
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-07-04 11:38:37 +0900

    TL: Spreading the Darkness (#2143)
    * TL: Spreading the Darkness
    
    * test and bugfix

--- server/game/cards/04.2-TL/SpreadingTheDarkness.js
+++ server/game/cards/04.2-TL/SpreadingTheDarkness.js
@@ -1,7 +1,26 @@
 const DrawCard = require('../../drawcard.js');
 
 class SpreadingTheDarkness extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Give a character +4/+0',
+            condition: () => this.game.isDuringConflict(),
+            cost: ability.costs.payHonor(2),
+            target: {
+                cardType: 'character',
+                controller: 'self',
+                cardCondition: card => card.isParticipating(),
+                gameAction: ability.actions.cardLastingEffect(context => ({
+                    effect: [
+                        ability.effects.modifyMilitarySkill(4),
+                        ability.effects.cardCannot('target', abilityContext => (
+                            abilityContext.player === context.player.opponent &&
+                            context.source.type && context.source.type !== 'ring'
+                        ))
+                    ]
+                }))
+            }
+        });
     }
 }
 
--- test/server/cards/04.2-TL/SpreadingTheDarkness.spec.js
+++ test/server/cards/04.2-TL/SpreadingTheDarkness.spec.js
@@ -5,25 +5,51 @@ describe('Spreading the Darkness', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        honor: 10,
+                        inPlay: ['child-of-the-plains'],
+                        hand: ['spreading-the-darkness']
                     },
                     player2: {
-                        inPlay: []
+                        hand: ['assassination']
                     }
                 });
                 this.noMoreActions();
+                this.initiateConflict({
+                    attackers: ['child-of-the-plains']
+                });
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('should cost 2 honor', function() {
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                this.childOfThePlains = this.player1.clickCard('child-of-the-plains');
+                this.player2.clickPrompt('Done');
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard('spreading-the-darkness');
+                this.player1.clickCard(this.childOfThePlains);
+                expect(this.player1.honor).toBe(8);
+                expect(this.player2).toHavePrompt('Conflict Action Window');
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
+            it('should increase mil by 4', function() {
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                this.childOfThePlains = this.player1.clickCard('child-of-the-plains');
+                this.player2.clickPrompt('Done');
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard('spreading-the-darkness');
+                this.player1.clickCard(this.childOfThePlains);
+                expect(this.childOfThePlains.militarySkill).toBe(5);
+                expect(this.player2).toHavePrompt('Conflict Action Window');
             });
 
-            it('should have DEF effect on GHI', function() {
-
+            it('should prevent targeting by opponent\'s events', function() {
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                this.childOfThePlains = this.player1.clickCard('child-of-the-plains');
+                this.player2.clickPrompt('Done');
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard('spreading-the-darkness');
+                this.player1.clickCard(this.childOfThePlains);
+                this.player2.clickCard('assassination');
+                expect(this.player2).toHavePrompt('Conflict Action Window');
             });
         });
     });
