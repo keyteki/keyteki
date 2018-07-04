41679d8372bda2f5d3b25362539bbb6bfadddae3
Kwaice <kwaice@gmail.com>
2018-07-04 04:40:57 +0200

    TL: Apprentice EarthCaller (#2135)
    * TL: Apprentice EarthCaller
    
    Closes #1915
    
    * Effect fix
    
    * Fix
    
    * Update
    
    * Added Test

--- server/game/cards/04.2-TL/ApprenticeEarthcaller.js
+++ server/game/cards/04.2-TL/ApprenticeEarthcaller.js
@@ -1,10 +1,25 @@
 const DrawCard = require('../../drawcard.js');
 
 class ApprenticeEarthcaller extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Set skill values to printed values',
+            condition: () => this.game.isDuringConflict(),
+            target: {
+                cardType: 'character',
+                cardCondition: card => card.isAttacking() && card.attachments.size() === 0,
+                gameAction: ability.actions.cardLastingEffect(context => ({
+                    effect: [
+                        ability.effects.setMilitarySkill(context.target.printedMilitarySkill),
+                        ability.effects.setPoliticalSkill(context.target.printedPoliticalSkill)
+                    ]
+                }))
+            },
+            effect: 'set {0}\'s skill values to their printed values until the end of the conflict'
+        });
     }
 }
 
-ApprenticeEarthcaller.id = 'apprentice-earthcaller'; // This is a guess at what the id might be - please check it!!!
+ApprenticeEarthcaller.id = 'apprentice-earthcaller';
 
 module.exports = ApprenticeEarthcaller;
