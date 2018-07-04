0e2f0a08f6471f16dd70363bc5b717c1a7155b5c
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-07-04 11:56:49 +0900

    TL: Oracle of Stone (#2169)

--- server/game/cards/04.2-TL/OracleOfStone.js
+++ server/game/cards/04.2-TL/OracleOfStone.js
@@ -1,10 +1,23 @@
 const DrawCard = require('../../drawcard.js');
 
 class OracleOfStone extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Draw 2 cards, then discard 2 cards',
+            gameAction: ability.actions.draw(context => ({
+                target: context.game.getPlayers(),
+                amount: 2
+            })),
+            then: context => ({
+                gameAction: ability.actions.chosenDiscard({
+                    target: context.game.getPlayers(),
+                    amount: 2
+                })
+            })
+        });
     }
 }
 
-OracleOfStone.id = 'oracle-of-stone'; // This is a guess at what the id might be - please check it!!!
+OracleOfStone.id = 'oracle-of-stone';
 
 module.exports = OracleOfStone;
