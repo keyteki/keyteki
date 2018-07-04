3411363270b43b5d06802bc114775121d0c5ee93
Kwaice <kwaice@gmail.com>
2018-07-04 04:40:39 +0200

    TL: Sabotage (#2136)
    * TL: Sabotage
    
    Closes #2124
    
    * Add cardType
    
    * Update

--- server/game/cards/04.2-TL/Sabotage.js
+++ server/game/cards/04.2-TL/Sabotage.js
@@ -1,10 +1,19 @@
 const DrawCard = require('../../drawcard.js');
 
 class Sabotage extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            condition: () => this.game.isDuringConflict('military'),
+            title: 'Discard a card in a province',
+            target: {
+                location: 'province',
+                controller: 'opponent',
+                cardType: ['character','holding'],
+                gameAction: ability.actions.discardCard()
+            }
+        });
     }
 }
-
-Sabotage.id = 'sabotage'; // This is a guess at what the id might be - please check it!!!
+Sabotage.id = 'sabotage';
 
 module.exports = Sabotage;
