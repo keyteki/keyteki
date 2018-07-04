6411cb727919a88c04221ab69a0aac571cb9ecde
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-07-04 11:36:30 +0900

    TL: Upholding Authority (#2090)
    * TE: Upholding Authority
    
    * backend changes
    
    * test and bug fixes

--- server/game/GameActions/CardGameAction.js
+++ server/game/GameActions/CardGameAction.js
@@ -36,8 +36,8 @@ class CardGameAction extends GameAction {
         super.preEventHandler(context);
         if(this.promptForSelect) {
             let selector = this.getSelector();
+            this.target = [];
             if(!selector.hasEnoughTargets(context)) {
-                this.target = [];
                 return;
             }
             let defaultProperties = {
@@ -57,12 +57,15 @@ class CardGameAction extends GameAction {
         } else if(this.promptWithHandlerMenu) {
             let properties = this.promptWithHandlerMenu;
             properties.cards = properties.cards.filter(card => this.canAffect(card, context));
+            this.target = [];
             if(properties.cards.length === 0) {
-                this.target = [];
                 return;
             }
             if(!properties.player) {
                 properties.player = context.player;
+            }
+            if(properties.customHandler) {
+                properties.cardHandler = card => properties.customHandler(card, this);
             }
             let defaultProperties = {
                 context: context,
--- server/game/cards/04.2-TL/UpholdingAuthority.js
+++ server/game/cards/04.2-TL/UpholdingAuthority.js
@@ -1,7 +1,53 @@
 const ProvinceCard = require('../../provincecard.js');
 
 class UpholdingAuthority extends ProvinceCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: this,
+            condition: () => this.controller.role.hasTrait('earth'),
+            effect: ability.effects.modifyProvinceStrength(2)
+        });
+
+        this.interrupt({
+            title: 'Look at opponent\'s hand and discard all copies of a card',
+            when: {
+                onBreakProvince: (event, context) => event.card === context.source && context.player.opponent
+            },
+            gameAction: ability.actions.discardCard(context => ({
+                promptWithHandlerMenu: {
+                    activePromptTitle: 'Choose a card to discard',
+                    cards: context.player.opponent.hand.sortBy(card => card.name),
+                    choices: ['Don\'t discard anything'],
+                    handlers: [() => context.game.addMessage('{0} chooses not to discard anything', context.player)],
+                    customHandler: (card, gameAction) => this.cardSelected(context, card, gameAction)
+                }
+            }))
+        });
+    }
+
+    cardSelected(context, card, gameAction) {
+        this.game.addMessage('{0} reveals their hand: {1}', context.player.opponent, context.player.opponent.hand.sortBy(card => card.name));
+        let cards = context.player.opponent.hand.filter(c => c.id === card.id);
+        if(cards.length === 1) {
+            gameAction.setTarget(cards);
+            context.game.addMessage('{0} chooses to discard {1}', context.player, card);
+            return;
+        }
+        let choices = Array.from(Array(cards.length + 1), (x, i) => i);
+        context.game.promptWithHandlerMenu(context.player, {
+            activePromptTitle: 'Choose how many cards to discard',
+            choices: choices,
+            choiceHandler: choice => this.numberToDiscardSelected(context, cards, gameAction, choice)
+        });
+    }
+
+    numberToDiscardSelected(context, cards, gameAction, choice) {
+        if(choice === 0) {
+            context.game.addMessage('{0} chooses not to discard anything', context.player);
+            return;
+        }
+        gameAction.setTarget(cards.splice(0, choice));
+        this.game.addMessage('{0} chooses to discard {1} cop{2} of {3}', context.player, choice, choice === 1 ? 'y' : 'ies', cards[0]);
     }
 }
 
