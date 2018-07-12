00c0e97dbc27f2d86689ce4a8a836d0d80df7d43
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-07-12 16:44:41 +0900

    Lasting effects should check potential to change game state (#2207)

--- server/game/GameActions/LastingEffectCardAction.js
+++ server/game/GameActions/LastingEffectCardAction.js
@@ -5,7 +5,7 @@ class LastingEffectCardAction extends CardGameAction {
         this.duration = 'untilEndOfConflict';
         this.condition = null;
         this.until = null;
-        this.effect = null;
+        this.effect = [];
         this.targetLocation = null;
     }
 
@@ -15,17 +15,36 @@ class LastingEffectCardAction extends CardGameAction {
         this.effectMsg = 'apply a lasting effect to {0}';
     }
 
+    update(context) {
+        super.update(context);
+        let effect = this.effect;
+        if(!Array.isArray(effect)) {
+            effect = [effect];
+        }
+        let effectProperties = {
+            condition: this.condition,
+            duration: this.duration,
+            targetLocation: this.targetLocation,
+            until: this.until
+        };
+        this.effect = effect.map(factory => factory(context.game, context.source, effectProperties));
+    }
+
     canAffect(card, context) {
         if(card.location !== 'play area' && this.targetLocation !== 'province') {
+            return false;
+        }
+        if(!this.effect.some(effect => effect.effect.canBeApplied(card))) {
             return false;
         }
         return super.canAffect(card, context);
     }
 
     getEvent(card, context) {
+        let { effect } = this.propertyFactory(context);
         let properties = {
             condition: this.condition,
-            effect: this.effect,
+            effect: effect,
             match: card,
             targetLocation: this.targetLocation,
             until: this.until
