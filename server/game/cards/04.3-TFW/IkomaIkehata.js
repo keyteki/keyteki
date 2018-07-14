ca00fed96654a55841eb481f9af8e7a232cc64a3
Kwaice <kwaice@gmail.com>
2018-07-14 14:16:43 +0200

    TFW: Ikoma Ikehata (#2236)
    * TFW: Ikoma Ikehata
    
    * Test
    
    * Fix
    
    * Draw outside target

--- server/game/cards/04.3-TFW/IkomaIkehata.js
+++ server/game/cards/04.3-TFW/IkomaIkehata.js
@@ -1,10 +1,24 @@
 const DrawCard = require('../../drawcard.js');
 
 class IkomaIkehata extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.reaction({
+            title: 'Honor a character and draw a card',
+            when: {
+                afterConflict: (event, context) => event.conflict.winner === context.player && context.source.isParticipating() && event.conflict.conflictType === 'political'
+            },
+            target: {
+                activePromptTitle: 'Choose a character to honor',
+                cardType: 'character',
+                controller: 'self',
+                cardcondition: card => !card.isHonored,
+                gameAction: ability.actions.honor()
+            },
+            gameAction: ability.actions.draw()
+        });
     }
 }
 
-IkomaIkehata.id = 'ikoma-ikehata'; // This is a guess at what the id might be - please check it!!!
+IkomaIkehata.id = 'ikoma-ikehata';
 
 module.exports = IkomaIkehata;
--- test/server/cards/04.3-TFW/IkomaIkehata.spec.js
+++ test/server/cards/04.3-TFW/IkomaIkehata.spec.js
@@ -5,26 +5,103 @@ describe('Ikoma Ikehata', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        inPlay: ['ikoma-ikehata','akodo-toturi'],
+                        hand: ['fine-katana','ornate-fan','way-of-the-crane']
                     },
                     player2: {
-                        inPlay: []
+                        inPlay: ['miya-mystic']
                     }
                 });
+                this.ikehata = this.player1.findCardByName('ikoma-ikehata');
+                this.toturi = this.player1.findCardByName('akodo-toturi');
+                this.crane = this.player1.findCardByName('way-of-the-crane');
+
+                this.mystic = this.player2.findCardByName('miya-mystic');
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('should trigger only after winning a political conflict', function() {
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: 'ikoma-ikehata',
+                    defenders: 'miya-mystic',
+                    jumpTo: 'afterConflict'
+                });
+                expect(this.player1).not.toBeAbletoSelect(this.ikehata);
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
+            it('should trigger after winning a political conflict', function() {
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: 'ikoma-ikehata',
+                    defenders: 'miya-mystic',
+                    jumpTo: 'afterConflict'
+                });
+                expect(this.player1).toBeAbletoSelect(this.ikehata);
             });
 
-            it('should have DEF effect on GHI', function() {
-
+            it('should only target characters that are not honored', function() {
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: 'ikoma-ikehata',
+                    defenders: 'miya-mystic'
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.crane);
+                this.player1.clickCard(this.toturi);
+                expect(this.toturi.isHonored).toBe(true);
+                this.player2.pass();
+                this.player1.pass();
+                expect(this.player1).toBeAbletoSelect(this.ikehata);
+                this.player1.clickCard(this.ikehata);
+                expect(this.player1).not.toBeAbletoSelect(this.toturi);
+                expect(this.player1).toBeAbletoSelect(this.ikehata);
+                expect(this.player1).not.toBeAbletoSelect(this.mystic);
             });
+
+            it('should correctly honor targeted character', function() {
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: 'ikoma-ikehata',
+                    defenders: 'miya-mystic'
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.crane);
+                this.player1.clickCard(this.toturi);
+                expect(this.toturi.isHonored).toBe(true);
+                this.player2.pass();
+                this.player1.pass();
+                expect(this.player1).toBeAbletoSelect(this.ikehata);
+                this.player1.clickCard(this.ikehata);
+                expect(this.player1).not.toBeAbletoSelect(this.toturi);
+                expect(this.player1).toBeAbletoSelect(this.ikehata);
+                expect(this.player1).not.toBeAbletoSelect(this.mystic);
+                this.player1.clickCard(this.ikehata);
+                expect(this.ikehata.isHonored).toBe(true);
+            });
+
+            it('should correctly draw a card after honoring', function() {
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: 'ikoma-ikehata',
+                    defenders: 'miya-mystic'
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.crane);
+                this.player1.clickCard(this.toturi);
+                expect(this.toturi.isHonored).toBe(true);
+                expect(this.player1.hand.length).toBe(2);
+                this.player2.pass();
+                this.player1.pass();
+                this.player1.clickCard(this.ikehata);
+                expect(this.player1).not.toBeAbletoSelect(this.toturi);
+                expect(this.player1).toBeAbletoSelect(this.ikehata);
+                expect(this.player1).not.toBeAbletoSelect(this.mystic);
+                this.player1.clickCard(this.ikehata);
+                expect(this.ikehata.isHonored).toBe(true);
+                expect(this.player1.hand.length).toBe(3);
+            });
+
         });
     });
 });
