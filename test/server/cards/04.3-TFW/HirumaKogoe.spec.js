60590f758f60ec543089c03e032b99538bf9071f
Kwaice <kwaice@gmail.com>
2018-07-14 14:22:52 +0200

    TFW: Hiruma Kogoe (#2151)
    * TFW: Hiruma Kogoe
    
    Closes #1982
    
    * Added Test
    
    * Test fix

--- test/server/cards/04.3-TFW/HirumaKogoe.spec.js
+++ test/server/cards/04.3-TFW/HirumaKogoe.spec.js
@@ -1,29 +1,58 @@
-describe('Hiruma Kogoe', function() {
-    integration(function() {
-        describe('Hiruma Kogoe\'s ability', function() {
-            beforeEach(function() {
+describe('Hiruma Kogoe', function () {
+    integration(function () {
+        describe('Hiruma Kogoe\'s ability', function () {
+            beforeEach(function () {
                 this.setupTest({
-                    phase: 'conflict',
+                    phase: 'dynasty',
                     player1: {
-                        inPlay: []
+                        inPlay: ['hiruma-kogoe'],
+                        honor: 5,
+                        hand: ['reprieve','fine-katana','ornate-fan']
                     },
                     player2: {
-                        inPlay: []
+                        honor:5
                     }
                 });
-                this.noMoreActions();
+                this.kogoe = this.player1.findCardByName('hiruma-kogoe');
+                this.katana = this.player1.findCardByName('fine-katana');
+                this.fan = this.player1.findCardByName('ornate-fan');
+                this.reprieve = this.player1.findCardByName('reprieve');
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('should not trigger if no lower honor', function () {
+                this.player2.player.honor = 3;
+                this.nextPhase();
+                expect(this.player1).not.toBeAbleToSelect(this.kogoe);
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
+            it('should trigger if lower honor', function () {
+                this.player2.player.honor = 11;
+                this.nextPhase();
+                expect(this.player1).toBeAbleToSelect(this.kogoe);
             });
 
-            it('should have DEF effect on GHI', function() {
+            it('should not trigger if even honor', function () {
+                this.player2.player.honor = 5;
+                this.nextPhase();
+                expect(this.player1).not.toBeAbleToSelect(this.kogoe);
+            });
 
+            it('should correctly rearrange cards', function () {
+                this.player2.player.honor = 11;
+                this.player1.moveCard(this.reprieve, 'conflict deck');
+                this.player1.moveCard(this.fan, 'conflict deck');
+                this.player1.moveCard(this.katana, 'conflict deck');
+                this.nextPhase();
+                this.player1.clickCard(this.kogoe);
+                expect(this.player1).toHavePrompt('Which card do you want to be on top?');
+                this.player1.clickPrompt('Ornate Fan');
+                expect(this.player1).toHavePrompt('Which card do you want to be the second card?');
+                expect(this.player1).toHavePromptButton('Reprieve');
+                expect(this.player1).toHavePromptButton('Fine Katana');
+                this.player1.clickPrompt('Reprieve');
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('3');
+                expect(this.fan.location).toBe('hand');
             });
         });
     });
