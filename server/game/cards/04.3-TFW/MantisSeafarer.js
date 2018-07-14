0ab3e4fa21c0d20d6aa470ebedbe11e6ecaa82bc
Caelan Mayberry <gmodman12@gmail.com>
2018-07-14 08:20:20 -0400

    Added Mantis Seafarer (#2213)

--- server/game/cards/04.3-TFW/MantisSeafarer.js
+++ server/game/cards/04.3-TFW/MantisSeafarer.js
@@ -2,6 +2,15 @@ const DrawCard = require('../../drawcard.js');
 
 class MantisSeafarer extends DrawCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+        this.reaction({
+            title: 'Gain a fate',
+            when: {
+                afterConflict: (event, context) => context.source.isParticipating() && event.conflict.winner === context.player
+            },
+            cost: ability.costs.payHonor(1),
+            gameAction: ability.actions.gainFate(),
+            limit: ability.limit.unlimitedPerConflict()
+        });
     }
 }
 
