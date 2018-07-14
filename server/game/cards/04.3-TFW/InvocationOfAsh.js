f19c16eff3e6f2c3112a686d27807254ca8e7099
Caelan Mayberry <gmodman12@gmail.com>
2018-07-14 08:22:04 -0400

    TFW: Invocation of Ash (#2159)
    * IoA added with test
    
    * Check references not names

--- server/game/cards/04.3-TFW/InvocationOfAsh.js
+++ server/game/cards/04.3-TFW/InvocationOfAsh.js
@@ -1,10 +1,24 @@
 const DrawCard = require('../../drawcard.js');
 
 class InvocationOfAsh extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Move to another character',
+            cost: ability.costs.payHonor(1),
+            target: {
+                cardType: 'character',
+                controller: 'self',
+                gameAction: ability.actions.attach(context => ({ attachment: context.source }))
+            },
+            then: context => ({
+                message: '{1} removes one fate from {3}',
+                messageArgs: context.target,
+                gameAction: ability.actions.removeFate({ target: context.target })
+            })
+        });
     }
 }
 
-InvocationOfAsh.id = 'invocation-of-ash'; // This is a guess at what the id might be - please check it!!!
+InvocationOfAsh.id = 'invocation-of-ash';
 
 module.exports = InvocationOfAsh;
