c365519186028dcb161aa66cb62620b415efe685
Mark Byrne <markjbyrne81@gmail.com>
2018-07-14 13:20:07 +0100

    Shiksha scout (#2216)
    * Implementing Shiksha Scout (#1990)
    
    Implemented Shiksha Scout including test.
    
    * Fixing linting errors..

--- test/server/cards/04.3-TFW/ShikshaScout.spec.js
+++ test/server/cards/04.3-TFW/ShikshaScout.spec.js
@@ -1,29 +1,29 @@
-describe('Shiksha Scout', function() {
-    integration(function() {
-        describe('Shiksha Scout\'s ability', function() {
-            beforeEach(function() {
+describe('Shiksha Scout', function () {
+    integration(function () {
+        describe('Shiksha Scout\'s ability', function () {
+            beforeEach(function () {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
-                    },
-                    player2: {
-                        inPlay: []
+                        fate: 2,
+                        inPlay: ['shiksha-scout'],
+                        hand: [],
+                        conflictDeck: []
                     }
                 });
+                this.shikshaScout = this.player1.findCardByName('shiksha-scout', 'play area');
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
-            });
-
-            it('should not trigger under ABC circumstances', function() {
-
-            });
-
-            it('should have DEF effect on GHI', function() {
-
+            it('should increment the number of participants in the conflict by 1 if participating in the conflict.', function () {
+                this.initiateConflict({
+                    type: 'military',
+                    ring: 'air',
+                    attackers: ['shiksha-scout'],
+                    defenders: []
+                });
+                this.noMoreActions();
+                expect(this.game.currentConflict.getNumberOfParticipantsFor(this.player1.player)).toBe(2);
             });
         });
     });
