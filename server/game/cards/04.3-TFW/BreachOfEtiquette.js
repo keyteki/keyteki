1eb1200e2eb8aed87d824d4e544b8ad0b61685fe
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-07-14 21:20:31 +0900

    TFW: Breach of Etiquette (#2211)
    * TFW: Breach of Protocol
    
    * name change
    
    * test

---
+++ server/game/cards/04.3-TFW/BreachOfEtiquette.js
@@ -0,0 +1,32 @@
+const DrawCard = require('../../drawcard.js');
+
+class BreachOfEtiquette extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Force honor loss on players when their non-courtier characters use abilities',
+            condition: () => this.game.isDuringConflict('political'),
+            effect: 'force honor loss on players when their non-courtier characters use abilities during this conflict',
+            gameAction: ability.actions.playerLastingEffect(context => ({
+                targetController: 'any',
+                effect: ability.effects.customDetachedPlayer({
+                    apply: player => context.source.delayedEffect({
+                        target: player,
+                        source: context.source,
+                        context: context,
+                        when: {
+                            onCardAbilityTriggered: (event, context) => context.player === player && !event.card.hasTrait('courtier')
+                        },
+                        gameAction: ability.actions.loseHonor(),
+                        message: '{1} loses 1 honor due to {0}',
+                        multipleTrigger: true
+                    }),
+                    unapply: (player, context, effect) => context.game.effectEngine.removeDelayedEffect(effect)
+                })
+            }))
+        });
+    }
+}
+
+BreachOfEtiquette.id = 'breach-of-etiquette';
+
+module.exports = BreachOfEtiquette;
