886be67c0802d8c1b48fafd0a7a31ac92846c166
Noah <nbtheduke@gmail.com>
2018-07-14 08:19:40 -0400

    Icon of Favor (#2230)

--- test/server/cards/04.3-TFW/IconOfFavor.spec.js
+++ test/server/cards/04.3-TFW/IconOfFavor.spec.js
@@ -5,26 +5,62 @@ describe('Icon of Favor', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        inPlay: ['moto-youth'],
+                        hand: ['icon-of-favor']
                     },
                     player2: {
                         inPlay: []
                     }
                 });
+                this.moto = this.player1.findCardByName('moto-youth');
+                this.icon = this.player1.findCardByName('icon-of-favor');
+                this.player1.playAttachment(this.icon, this.moto);
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('should give attached character +1 glory when you have Imperial Favor', function() {
+                let glory = this.moto.glory;
+                this.player1.player.imperialFavor = 'political';
+                this.game.checkGameState(true);
+                expect(this.moto.glory).toBe(glory + 1);
+                this.player1.player.imperialFavor = '';
+                this.game.checkGameState(true);
+                expect(this.moto.glory).toBe(glory);
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
+            it('should honor after winning Fire conflict', function() {
+                expect(this.moto.isHonored).toBe(false);
+                this.initiateConflict({
+                    ring: 'fire',
+                    attackers: [this.moto]
+                });
+                this.player2.clickPrompt('Done');
+                this.player2.pass();
+                this.player1.pass();
+                this.player1.clickCard(this.icon);
+                expect(this.moto.isHonored).toBe(true);
             });
 
-            it('should have DEF effect on GHI', function() {
+            function testNonFireConflicts(conflictType) {
+                it('should not honor after winning a ' + conflictType + ' conflict',
+                    function() {
+                        expect(this.moto.isHonored).toBe(false);
+                        this.initiateConflict({
+                            ring: conflictType,
+                            attackers: [this.moto]
+                        });
+                        this.player2.clickPrompt('Done');
+                        this.player2.pass();
+                        this.player1.pass();
+                        this.player1.clickCard(this.icon);
+                        expect(this.moto.isHonored).toBe(false);
+                    }
+                );
+            }
 
-            });
+            for(let conflictType of ['air', 'earth', 'water', 'void']) {
+                testNonFireConflicts(conflictType);
+            }
         });
     });
 });
