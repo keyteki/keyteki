752553318b875fd29474961b0e9a723ca949446b
Mark Byrne <markjbyrne81@gmail.com>
2018-07-14 13:16:54 +0100

    Ancestral Armory (#2234)
    Implementing Ancestral Armory and test.

--- test/server/cards/04.3-TFW/AncestralArmory.spec.js
+++ test/server/cards/04.3-TFW/AncestralArmory.spec.js
@@ -5,25 +5,32 @@ describe('Ancestral Armory', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        inPlay: ['seppun-guardsman'],
+                        hand: [],
+                        conflictDiscard: ['fine-katana', 'ornate-fan']
                     },
                     player2: {
-                        inPlay: []
+                        inPlay: ['moto-nergui']
                     }
                 });
-                this.noMoreActions();
+                this.ancestralArmory = this.player1.placeCardInProvince('ancestral-armory', 'province 1');
+                this.player2.pass();
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('should add a weapon attachment back to your hand', function() {
+                this.player1.clickCard('ancestral-armory');
+                expect(this.player1).toHavePrompt('Ancestral Armory');
+                this.fineKatana = this.player1.clickCard('fine-katana');
+                expect(this.fineKatana.location).toBe('play area');
+                expect(this.ancestralArmory.location).toBe('dynasty discard pile');
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
-            });
-
-            it('should have DEF effect on GHI', function() {
-
+            it('should not add a non weapon attachment back to your hand', function() {
+                this.player1.clickCard('ancestral-armory');
+                expect(this.player1).toHavePrompt('Ancestral Armory');
+                this.oranteFan = this.player1.clickCard('ornate-fan');
+                expect(this.oranteFan.location).toBe('conflict discard pile');
+                expect(this.ancestralArmory.location).toBe('play area');
             });
         });
     });
