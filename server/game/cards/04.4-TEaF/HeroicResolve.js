d9adf73635564e79dc7b44d9abccbf126c8bc380
Mark Byrne <markjbyrne81@gmail.com>
2018-07-18 02:00:09 +0100

    TEaF: Heroic resolve (#2217)
    * Implement Heroic Resolve.
    
    * Implementing Heroic Resolve
    
    Implemented Heroic Resolve with test.

--- server/game/cards/04.4-TEaF/HeroicResolve.js
+++ server/game/cards/04.4-TEaF/HeroicResolve.js
@@ -2,9 +2,14 @@ const DrawCard = require('../../drawcard.js');
 
 class HeroicResolve extends DrawCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+        this.action({
+            title: 'Ready attached character',
+            condition: context => context.player.getClaimedRings().length >= 2,
+            gameAction: ability.actions.ready(context => ({ target: context.source.parent }))
+        });
     }
 }
 
-HeroicResolve.id = 'heroic-resolve'; // This is a guess at what the id might be - please check it!!!
+HeroicResolve.id = 'heroic-resolve';
 
 module.exports = HeroicResolve;
