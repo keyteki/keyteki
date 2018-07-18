f7fe745071965a392141219346c866570f13bbf5
Nathan <nathaniel.j.mith@gmail.com>
2018-07-17 19:00:23 -0600

    TEaF: Deceptive Offer (#2154)
    * Update DeceptiveOffer.js
    
    * Update DeceptiveOffer.js
    
    * Update DeceptiveOffer.js
    
    * Update DeceptiveOffer.js
    
    * Create DeceptiveOffer.spec.js
    
    * Update DeceptiveOffer.js
    
    * Update DeceptiveOffer.js
    
    * Update DeceptiveOffer.spec.js
    
    * Update DeceptiveOffer.spec.js
    
    * Update DeceptiveOffer.spec.js
    
    Just fixing indentation.
    
    * Update DeceptiveOffer.js
    
    Just trying something to see if I can make it work. I'm just a little lost on how to make sure the originally selected character is targeted for the +2/+2 boost.
    
    * Update DeceptiveOffer.spec.js
    
    * Update DeceptiveOffer.js
    
    * Update DeceptiveOffer.js

--- server/game/cards/04.4-TEaF/DeceptiveOffer.js
+++ server/game/cards/04.4-TEaF/DeceptiveOffer.js
@@ -1,10 +1,33 @@
 const DrawCard = require('../../drawcard.js');
 
 class DeceptiveOffer extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Increase a character\'s military and political skill or take an honor from your opponent',
+            targets: {
+                character: {
+                    cardType: 'character',
+                    controller: 'self',
+                    cardCondition: card => card.isParticipating()
+                },
+                select: {
+                    mode: 'select',
+                    dependsOn: 'character',
+                    player: 'opponent',
+                    choices: {
+                        'Allow your opponent\'s character to gain military and political skill': ability.actions.cardLastingEffect(context => ({
+                            target: context.targets.character,
+                            duration: 'untilEndOfConflict',
+                            effect: ability.effects.modifyBothSkills(2)
+                        })),
+                        'Give your opponent 1 honor': ability.actions.takeHonor()
+                    }
+                }
+            }
+        });
     }
 }
 
-DeceptiveOffer.id = 'deceptive-offer'; // This is a guess at what the id might be - please check it!!!
+DeceptiveOffer.id = 'deceptive-offer';
 
 module.exports = DeceptiveOffer;
