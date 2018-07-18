be9dce693330b0a3953d0d3f25333116500c270b
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-07-18 13:33:50 +0900

    lint fixes (#2294)

--- server/game/cards/04.4-TEaF/FloodedWaste.js
+++ server/game/cards/04.4-TEaF/FloodedWaste.js
@@ -2,15 +2,15 @@ const ProvinceCard = require('../../provincecard.js');
 
 class FloodedWaste extends ProvinceCard {
     setupCardAbilities(ability) {
-            this.reaction({
-                title: 'Bow each attacking character',
-                when: {
-                    onProvinceRevealed: (event, context) => event.card === context.source
-                },
-                gameAction: ability.actions.bow(() => ({
-                    target: this.game.findAnyCardsInPlay(card => card.getType() === 'character' && card.isAttacking() )
-                }))
-            });
+        this.reaction({
+            title: 'Bow each attacking character',
+            when: {
+                onProvinceRevealed: (event, context) => event.card === context.source
+            },
+            gameAction: ability.actions.bow(() => ({
+                target: this.game.findAnyCardsInPlay(card => card.getType() === 'character' && card.isAttacking())
+            }))
+        });
     }
 }
 
