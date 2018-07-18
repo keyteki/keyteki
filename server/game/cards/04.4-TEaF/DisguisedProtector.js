433a63d5cc702c4d3fec09b1db37ccb68bef1b8a
Mark Byrne <markjbyrne81@gmail.com>
2018-07-18 01:59:53 +0100

    TEaF - Disguised protector - card & test (#2255)
    * card placeholders (#2208)
    
    * Test placeholders for TFW (#2209)
    
    * card placeholders
    
    * test placeholders
    
    * test fix
    
    * Added court games test (#2193)
    
    * Test: Feral Ningyo and Hida Kisada (#2194)
    
    * Added Hida Kisada test
    
    * simplification
    
    * and just a little more verification
    
    * Implementing Disguised Protector
    
    Includes card & test.

--- server/game/cards/04.4-TEaF/DisguisedProtector.js
+++ server/game/cards/04.4-TEaF/DisguisedProtector.js
@@ -2,9 +2,27 @@ const DrawCard = require('../../drawcard.js');
 
 class DisguisedProtector extends DrawCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+        this.action({
+            title: 'Add each players honor bid to their skill total',
+            condition: context => context.source.isParticipating(),
+            effect: 'add the bid on each players dial to their skill total',
+            gameAction: [
+                ability.actions.playerLastingEffect(context => ({
+                    duration: 'untilEndOfConflict',
+                    //target: context.player,
+                    effect: ability.effects.changePlayerSkillModifier(context.player.showBid)
+                })),
+                ability.actions.playerLastingEffect(context => ({
+                    duration: 'untilEndOfConflict',
+                    //target: context.player.opponent,
+                    targetController: 'opponent',
+                    effect: ability.effects.changePlayerSkillModifier(context.player.opponent.showBid)
+                }))
+            ]
+        });
     }
 }
 
-DisguisedProtector.id = 'disguised-protector'; // This is a guess at what the id might be - please check it!!!
+DisguisedProtector.id = 'disguised-protector';
 
 module.exports = DisguisedProtector;
