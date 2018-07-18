6036f94c6435b5344a805857c903abe66883fb18
Kwaice <kwaice@gmail.com>
2018-07-18 03:59:52 +0200

    TEaF: Master of the Swift Waves (#2258)
    * TEaF: Master of the Swift Waves
    
    * Joint action
    
    * Test fix
    
    * Prompt Fix
    
    * Update MasterOfTheSwiftWaves.spec.js
    
    * Test fix
    
    * fiveringsdb-data

--- server/game/cards/04.4-TEaF/MasterOfTheSwiftWaves.js
+++ server/game/cards/04.4-TEaF/MasterOfTheSwiftWaves.js
@@ -1,10 +1,34 @@
 const DrawCard = require('../../drawcard.js');
 
 class MasterOfTheSwiftWaves extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title:'Switch 2 characters you control',
+            condition: () => this.game.isDuringConflict(),
+            gameAction: ability.actions.jointAction([
+                ability.actions.sendHome(context => ({
+                    promptForSelect: {
+                        activePromptTitle: 'Choose a participating character to send home',
+                        cardType: 'character',
+                        cardCondition: (card,context) => card.isParticipating() && card.controller === context.player,
+                        message: '{0} moves {1} back home',
+                        messageArgs: card => [context.player, card]
+                    }
+                })),
+                ability.actions.moveToConflict(context => ({
+                    promptForSelect: {
+                        activePromptTitle: 'Choose a character to move to the conflict',
+                        cardType: 'character',
+                        cardCondition: (card,context) => !card.isParticipating() && card.controller === context.player,
+                        message: '{0} moves {1} to the conflict',
+                        messageArgs: card => [context.player, card]
+                    }
+                }))
+            ])
+        });
     }
 }
 
-MasterOfTheSwiftWaves.id = 'master-of-the-swift-waves'; // This is a guess at what the id might be - please check it!!!
+MasterOfTheSwiftWaves.id = 'master-of-the-swift-waves';
 
 module.exports = MasterOfTheSwiftWaves;
