2313157eb78131ce4e4b719f505eb9e4c06da93a
Mark Byrne <markjbyrne81@gmail.com>
2018-07-24 05:59:56 +0100

    AaN: Raging battleground (#2322)
    * update submodule (#2259)
    
    * fix and tests (#2256)
    
    * TFW: Soshi Shiori (#2237)
    
    * TFW: Ikoma Ikehata (#2236)
    
    * TFW: Ikoma Ikehata
    
    * Test
    
    * Fix
    
    * Draw outside target
    
    * Ancestral Armory (#2234)
    
    Implementing Ancestral Armory and test.
    
    * TFW: Chikai Order Protector (#2233)
    
    * card placeholders (#2208)
    
    * Test placeholders for TFW (#2209)
    
    * card placeholders
    
    * test placeholders
    
    * test fix
    
    * Added court games test (#2193)
    
    * Test: Feral Ningyo and Hida Kisada (#2194)
    
    * Added Hida Kisada test
    
    * simplification
    
    * and just a little more verification
    
    * Implementing Chikai Order Protector
    
    Implemented Chikai Order Protector.  Test to follow.
    
    * Adding test for Chikai Order Protector
    
    * TFW: Insult to Injury (#2223)
    
    * Revert "update submodule" (#2260)
    
    * Revert "TFW: Insult to Injury (#2223)"
    
    This reverts commit 4d61567098039328a254d1030c51d1849811457c.
    
    * Revert "TFW: Chikai Order Protector (#2233)"
    
    This reverts commit eac3bab64b75984584d01f6c5f01ee0bc0704450.
    
    * Revert "Ancestral Armory (#2234)"
    
    This reverts commit 752553318b875fd29474961b0e9a723ca949446b.
    
    * Revert "TFW: Ikoma Ikehata (#2236)"
    
    This reverts commit ca00fed96654a55841eb481f9af8e7a232cc64a3.
    
    * Revert "TFW: Soshi Shiori (#2237)"
    
    This reverts commit d1a6c2d0a6148304200abb05bbc08872dbba0db7.
    
    * Revert "fix and tests (#2256)"
    
    This reverts commit 362d9a35d52b29105af89c6951953feff776dfe6.
    
    * Revert "update submodule (#2259)"
    
    This reverts commit 762dc25e3d735713cc2e30b2c1037ea62dcccfb3.
    
    * TFW: Sadane Student (#2232)
    
    * Sadane Student
    
    * Fixed test
    
    * TFW: Battle Maiden Recruit (#2231)
    
    * Battle Maiden Recruit
    
    * Fixed tests, moved checkGameState into claimRing
    
    * Icon of Favor (#2230)
    
    * Shiksha scout (#2216)
    
    * Implementing Shiksha Scout (#1990)
    
    Implemented Shiksha Scout including test.
    
    * Fixing linting errors..
    
    * Added Mantis Seafarer (#2213)
    
    * TFW: Breach of Etiquette (#2211)
    
    * TFW: Breach of Protocol
    
    * name change
    
    * test
    
    *  TFW: Jurojin's Curse (#2210)
    
    * TFW: Jurojin's Curse
    
    * test
    
    * fix when: eventNames
    
    * TFW: Wicked Tetsubo (#2192)
    
    * TFW: Wicked-Tetsubo
    
    * cha
    
    * Ok
    
    * Handleeeeeeeeer?
    
    * Changes
    
    * Added test
    
    * Test fix
    
    * Test changes
    
    * Effect fix
    
    * TFW: Invocation of Ash (#2159)
    
    * IoA added with test
    
    * Check references not names
    
    * TFW: Hiruma Kogoe (#2151)
    
    * TFW: Hiruma Kogoe
    
    Closes #1982
    
    * Added Test
    
    * Test fix
    
    * TFW: Smoke (#2145)
    
    * Smoke and tests
    
    * One more test
    
    * Master Alchemist (#2187)
    
    * TFW: Abandoning Honor (#2073)
    
    * TFW: AbandoningHonor
    
    Closes #1981
    
    * TFW: Abandoning Honor
    
    Formatting
    
    * ESLint formatting
    
    * Added test
    
    * TFW: The Wrath of the Kami (#2052)
    
    * Added The Wrath of the Kami with test
    
    * Common denominator
    
    * Test works with any province
    
    * eslint
    
    * TFW: Infiltrator (#2132)
    
    * TFW: Infiltrator
    
    * add message to Infiltrator
    
    * test
    
    * test complete
    
    * New fiveringsdb-data commits.
    
    * Revert "New fiveringsdb-data commits."
    
    This reverts commit ce2e2a8bf925e4eb55d50f42e390278598102d4f.
    
    * Implementing Raging Battleground
    
    Test included.
    
    * Update IkomaIkehata.spec.js

--- server/game/cards/04.5-AaN/RagingBattleground.js
+++ server/game/cards/04.5-AaN/RagingBattleground.js
@@ -2,6 +2,19 @@ const ProvinceCard = require('../../provincecard.js');
 
 class RagingBattleground extends ProvinceCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+        this.reaction({
+            title: 'Choose a character to discard',
+            when: {
+                onProvinceRevealed: (event, context) => event.card === context.source
+            },
+            target: {
+                cardType: 'character',
+                location: 'play area',
+                controller: 'any',
+                cardCondition: card => !card.isUnique() && card.getFate() < 1,
+                gameAction: ability.actions.discardFromPlay()
+            }
+        });
     }
 }
 
--- test/server/cards/04.5-AaN/RagingBattleground.spec.js
+++ test/server/cards/04.5-AaN/RagingBattleground.spec.js
@@ -5,25 +5,47 @@ describe('Raging Battleground', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        inPlay: ['hida-tomonatsu', 'crisis-breaker', 'borderlands-defender']
                     },
                     player2: {
-                        inPlay: []
+                        provinces: ['raging-battleground']
                     }
                 });
+
+                this.hidaTomonatsu = this.player1.findCardByName('hida-tomonatsu');
+                this.crisisBreaker = this.player1.findCardByName('crisis-breaker');
+                this.borderlandsDefender = this.player1.findCardByName('borderlands-defender');
+                this.borderlandsDefender.modifyFate(1);
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('should trigger when the province is revealed', function() {
+                this.initiateConflict({
+                    attackers: [this.crisisBreaker]
+                });
+                expect(this.player2).toHavePrompt('Triggered Abilities');
+                expect(this.player2).toBeAbleToSelect('raging-battleground');
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
+            it('should allow you to discard a non-unique character without fate', function() {
+                this.initiateConflict({
+                    attackers: [this.crisisBreaker]
+                });
+                this.player2.clickCard('raging-battleground');
+                expect(this.player2).toHavePrompt('Choose a character');
+                expect(this.player2).toBeAbleToSelect(this.crisisBreaker);
+                this.player2.clickCard(this.crisisBreaker);
+                expect(this.crisisBreaker.location).toBe('dynasty discard pile');
             });
 
-            it('should have DEF effect on GHI', function() {
-
+            it('should not allow you to discard a unique character or a non-unique character with fate', function() {
+                this.initiateConflict({
+                    attackers: [this.borderlandsDefender, this.hidaTomonatsu]
+                });
+                this.player2.clickCard('raging-battleground');
+                expect(this.player2).toHavePrompt('Choose a character');
+                expect(this.player2).not.toBeAbleToSelect(this.borderlandsDefender);
+                expect(this.player2).not.toBeAbleToSelect(this.hidaTomonatsu);
             });
         });
     });
