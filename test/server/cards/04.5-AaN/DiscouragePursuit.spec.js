2b37c78d9cd27c09ca4a2b1398eb8cd6e8558d1f
Kwaice <kwaice@gmail.com>
2018-07-24 06:49:41 +0200

    AaN: Discourage Pursuit (#2336)

--- test/server/cards/04.5-AaN/DiscouragePursuit.spec.js
+++ test/server/cards/04.5-AaN/DiscouragePursuit.spec.js
@@ -5,21 +5,45 @@ describe('Discourage Pursuit', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        inPlay: ['shosuro-miyako','bayushi-manipulator'],
+                        hand: ['discourage-pursuit']
                     },
                     player2: {
-                        inPlay: []
+                        inPlay: ['akodo-toturi']
                     }
                 });
+                this.toturi = this.player2.findCardByName('akodo-toturi');
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
+            it('should correctly trigger with a shinobi in play', function() {
+                this.initiateConflict({
+                    attackers: ['bayushi-manipulator'],
+                    defenders: [this.toturi]
+                });
+                this.player2.pass();
+                this.player1.clickCard('discourage-pursuit');
+                expect(this.player1).toHavePrompt('Choose a character');
+                expect(this.player1).toBeAbleToSelect('bayushi-manipulator');
+                expect(this.player1).toBeAbleToSelect(this.toturi);
+                this.player1.clickCard(this.toturi);
+                expect(this.player1).toBeAbleToSelect('shosuro-miyako');
+                this.player1.clickCard('shosuro-miyako');
+
+
+                expect(this.toturi.getMilitarySkill()).toBe(2);
 
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
+            it('should not trigger without a shinobi in play', function() {
+                this.initiateConflict({
+                    attackers: ['bayushi-manipulator'],
+                    defenders: [this.toturi]
+                });
+                this.player2.pass();
+                this.player1.moveCard('shosuro-miyako', 'dynasty discard pile');
+                this.player1.clickCard('discourage-pursuit');
+                expect(this.player1).not.toHavePrompt('Choose a character');
             });
 
             it('should have DEF effect on GHI', function() {
