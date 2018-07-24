4092891d77b979fd0676b5c6af4db17cbe873f63
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-07-24 14:00:05 +0900

    AaN: Void Fist (#2320)
    * AaN: Void Fist
    
    * test and bugfix

--- server/game/cards/04.5-AaN/VoidFist.js
+++ server/game/cards/04.5-AaN/VoidFist.js
@@ -1,10 +1,41 @@
 const DrawCard = require('../../drawcard.js');
+const EventRegistrar = require('../../eventregistrar.js');
 
 class VoidFist extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.cardsPlayedThisConflict = {};
+        this.eventRegistrar = new EventRegistrar(this.game, this);
+        this.eventRegistrar.register(['onConflictFinished', 'onCardPlayed']);
+        this.action({
+            title: 'Bow and send a character home',
+            condition: context => this.cardsPlayedThisConflict[context.player.uuid] >= 2,
+            target: {
+                cardType: 'character',
+                cardCondtion: (card, context) =>
+                    card.isParticipating() && this.game.currentConflict.getCharacters(context.player).some(myCard =>
+                        myCard.hasTrait('monk') && myCard.militarySkill > card.militarySkill
+                    ),
+                gameAction: [ability.actions.bow(), ability.actions.sendHome()]
+            },
+            effect: 'bow {0} and send them home'
+        });
+    }
+
+    onConflictFinished() {
+        this.cardsPlayedThisConflict = {};
+    }
+
+    onCardPlayed(event) {
+        if(this.game.isDuringConflict()) {
+            if(this.cardsPlayedThisConflict[event.player.uuid]) {
+                this.cardsPlayedThisConflict[event.player.uuid] += 1;
+            } else {
+                this.cardsPlayedThisConflict[event.player.uuid] = 1;
+            }
+        }
     }
 }
 
-VoidFist.id = 'void-fist'; // This is a guess at what the id might be - please check it!!!
+VoidFist.id = 'void-fist';
 
 module.exports = VoidFist;
