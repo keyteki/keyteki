a206e2dd5998f044d514e4163b71231335d9b4f3
Mark Byrne <markjbyrne81@gmail.com>
2018-07-24 22:58:36 +0100

    AaN: Tainted hero (#2343)
    * Implementing Tainted Hero
    
    test to follow.
    
    * Adding test for Tainted Hero
    
    Also fixed some linting errors.
    
    * Removing unnecessary code
    
    * Update TaintedHero.spec.js

--- server/game/cards/04.5-AaN/TaintedHero.js
+++ server/game/cards/04.5-AaN/TaintedHero.js
@@ -2,9 +2,26 @@ const DrawCard = require('../../drawcard.js');
 
 class TaintedHero extends DrawCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+        this.persistentEffect({
+            match: this,
+            effect: [
+                ability.effects.cardCannot('declareAsAttacker'),
+                ability.effects.cardCannot('declareAsDefender')
+            ]
+        });
+
+        this.action({
+            title: 'Make text box blank',
+            cost: ability.costs.sacrifice(card => card.getType() === 'character'),
+            gameAction: ability.actions.cardLastingEffect({
+                match: this,
+                duration: 'untilEndOfPhase',
+                effect: ability.effects.blank()
+            })
+        });
     }
 }
 
-TaintedHero.id = 'tainted-hero'; // This is a guess at what the id might be - please check it!!!
+TaintedHero.id = 'tainted-hero';
 
 module.exports = TaintedHero;
