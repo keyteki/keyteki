79669b8c49961a5039c0a733433d54b134dfc3f2
Kwaice <kwaice@gmail.com>
2018-07-24 06:50:04 +0200

    AaN: Bustling Academy (#2332)
    * AaN: Bustling Academy
    
    * Test fix
    
    * Simpler test
    
    * Test fix

--- server/game/cards/04.5-AaN/BustlingAcademy.js
+++ server/game/cards/04.5-AaN/BustlingAcademy.js
@@ -1,10 +1,27 @@
 const DrawCard = require('../../drawcard.js');
 
 class BustlingAcademy extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Discard a card in a province and refill it faceup',
+            condition: context => context.player.cardsInPlay.any(card => card.hasTrait('scholar')) && context.player.opponent,
+            target: {
+                location: 'province',
+                cardType: ['character','holding'],
+                gameAction: ability.actions.discardCard()
+            },
+            then: context => ({
+                handler: () => {
+                    let card = context.player.getDynastyCardInProvince(context.cardStateWhenInitiated.location);
+                    if(card) {
+                        card.facedown = false;
+                    }
+                }
+            })
+        });
     }
 }
 
-BustlingAcademy.id = 'bustling-academy'; // This is a guess at what the id might be - please check it!!!
+BustlingAcademy.id = 'bustling-academy';
 
 module.exports = BustlingAcademy;
--- test/server/cards/04.5-AaN/BustlingAcademy.spec.js
+++ test/server/cards/04.5-AaN/BustlingAcademy.spec.js
@@ -5,25 +5,59 @@ describe('Bustling Academy', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        inPlay: ['naive-student'],
+                        dynastyDiscard: ['bustling-academy','kanjo-district']
                     },
                     player2: {
-                        inPlay: []
+                        inPlay: ['moto-youth','shinjo-scout'],
+                        dynastyDiscard:['shiotome-encampment'],
+                        hand: []
                     }
                 });
-                this.noMoreActions();
+
+                this.ba = this.player1.placeCardInProvince('bustling-academy','province 1');
+                this.kd = this.player1.placeCardInProvince('kanjo-district','province 2');
+                this.naive = this.player1.findCardByName('naive-student');
+
+                this.shio = this.player2.placeCardInProvince('shiotome-encampment','province 1');
+                this.youth = this.player2.placeCardInProvince('moto-youth','province 2');
+                this.scout = this.player2.findCardByName('shinjo-scout');
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('should correctly target cards in provinces', function() {
+                this.player1.clickCard(this.ba);
+                expect(this.player1).toHavePrompt('Choose a card');
+                expect(this.player1).toBeAbleToSelect(this.ba);
+                expect(this.player1).toBeAbleToSelect(this.kd);
+                expect(this.player1).toBeAbleToSelect(this.shio);
+                expect(this.player1).toBeAbleToSelect(this.youth);
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
+            it('should correctly discard the targeted card', function() {
+                this.player1.clickCard(this.ba);
+                this.player1.clickCard(this.shio);
+                expect(this.shio.location).toBe('dynasty discard pile');
             });
 
-            it('should have DEF effect on GHI', function() {
+            it('should correctly refill the province faceup', function() {
+                this.player2.moveCard('shinjo-scout', 'dynasty deck');
+                this.player1.clickCard(this.ba);
+                this.player1.clickCard(this.shio);
+                expect(this.shio.location).toBe('dynasty discard pile');
+                expect(this.scout.location).toBe('province 1');
+            });
 
+            it('should correctly discard itself', function() {
+                this.player1.clickCard(this.ba);
+                this.player1.clickCard(this.ba);
+                expect(this.ba.location).toBe('dynasty discard pile');
+            });
+
+            it('should not work if a scholar is not in play', function() {
+                this.player1.moveCard('naive-student', 'dynasty discard pile');
+                expect(this.naive.location).toBe('dynasty discard pile');
+                this.player1.clickCard(this.ba);
+                expect(this.player1).toHavePrompt('Initiate an action');
             });
         });
     });
