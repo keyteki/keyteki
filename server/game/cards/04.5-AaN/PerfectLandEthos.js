4ece494bc83f0056eb31d6cb20b5d69a2cc2456e
Ben Szymkow <simcof@gmail.com>
2018-07-28 02:55:16 +0000

    Perfect Land Ethos (#2367)
    Perfect Land Ethos implementation plus unit test

--- server/game/cards/04.5-AaN/PerfectLandEthos.js
+++ server/game/cards/04.5-AaN/PerfectLandEthos.js
@@ -1,10 +1,22 @@
 const DrawCard = require('../../drawcard.js');
 
 class PerfectLandEthos extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Discard each status token',
+            effect: 'discard each status token',
+            gameAction: [
+                ability.actions.dishonor(() => ({
+                    target: this.game.findAnyCardsInPlay(card => card.isHonored)
+                })),
+                ability.actions.honor(() => ({
+                    target: this.game.findAnyCardsInPlay(card => card.isDishonored)
+                }))
+            ]
+        });
     }
 }
 
-PerfectLandEthos.id = 'perfect-land-ethos'; // This is a guess at what the id might be - please check it!!!
+PerfectLandEthos.id = 'perfect-land-ethos';
 
 module.exports = PerfectLandEthos;
