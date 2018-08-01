4ca65dadb342bc6c59df074bdc7e380316606f7c
Ben Wootten <benmwootten@gmail.com>
2018-08-01 06:29:42 -0500

    EU:Restored Heirloom (#2378)
    * adds Restored Heirloom and tests
    
    * linting

--- server/game/cards/04.6-EU/RestoredHeirloom.js
+++ server/game/cards/04.6-EU/RestoredHeirloom.js
@@ -1,10 +1,27 @@
 const DrawCard = require('../../drawcard.js');
 
 class RestoredHeirloom extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.wouldInterrupt({
+            title: 'Put into play',
+            when: {
+                onResolveRingElement: (event, context) => event.ring.element === 'water' && event.player === context.player
+            },
+            effect: 'replace the water ring with putting Restored Heirloom into play',
+            location: ['hand','conflict discard pile'],
+            target: {
+                cardType: 'character',
+                controller: 'self'
+            },
+            handler: context => {
+                context.cancel();
+                let event = ability.actions.attach({ attachment: context.source }).getEvent(context.target, context);
+                context.event.window.addEvent(event);
+            }
+        });
     }
 }
 
-RestoredHeirloom.id = 'restored-heirloom'; // This is a guess at what the id might be - please check it!!!
+RestoredHeirloom.id = 'restored-heirloom';
 
 module.exports = RestoredHeirloom;
--- test/server/cards/04.6-EU/RestoredHeirloom.spec.js
+++ test/server/cards/04.6-EU/RestoredHeirloom.spec.js
@@ -5,25 +5,45 @@ describe('Restored Heirloom', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
-                    },
-                    player2: {
-                        inPlay: []
+                        fate: 2,
+                        inPlay: ['matsu-berserker'],
+                        conflictDiscard: ['restored-heirloom']
                     }
                 });
+                this.berserker = this.player1.findCardByName('matsu-berserker');
+                this.heirloom = this.player1.findCardByName('restored-heirloom');
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
+            it('should trigger when the water ring is resolved', function() {
+                this.initiateConflict({
+                    ring: 'water',
+                    attackers: ['matsu-berserker'],
+                    defenders: []
+                });
+                this.noMoreActions();
+                //break province prompt
+                this.player1.clickPrompt('no');
 
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.heirloom);
             });
 
-            it('should not trigger under ABC circumstances', function() {
+            it('should put the heirloom into play for free', function() {
+                this.initiateConflict({
+                    ring: 'water',
+                    attackers: ['matsu-berserker'],
+                    defenders: []
+                });
+                this.noMoreActions();
+                //break province prompt
+                this.player1.clickPrompt('no');
 
-            });
-
-            it('should have DEF effect on GHI', function() {
-
+                this.player1.clickCard(this.heirloom);
+                expect(this.player1).toBeAbleToSelect(this.berserker);
+                this.player1.clickCard(this.berserker);
+                expect(this.heirloom.location).toBe('play area');
+                expect(this.player1.fate).toBe(2);
             });
         });
     });
