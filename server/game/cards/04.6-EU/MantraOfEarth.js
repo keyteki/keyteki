95bce8c96b430476f984b77dcffe8c6bfbb69b26
Caelan Mayberry <gmodman12@gmail.com>
2018-08-01 07:28:37 -0400

    EU: Mantra of Earth (#2386)
    * Initial pass
    
    * Updated test
    
    * Update MantraOfEarth.js

--- server/game/cards/04.6-EU/MantraOfEarth.js
+++ server/game/cards/04.6-EU/MantraOfEarth.js
@@ -1,10 +1,29 @@
 const DrawCard = require('../../drawcard.js');
 
 class MantraOfEarth extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.reaction({
+            title: 'Make a monk untargetable by opponents\' card effects and draw a card',
+            when: {
+                onConflictDeclared: (event, context) => event.ring.hasElement('earth') && event.conflict.attackingPlayer === context.player.opponent
+            },
+            target: {
+                cardType: 'character',
+                cardCondition: card => card.hasTrait('monk') || card.attachments.any(card => card.hasTrait('monk')),
+                gameAction: ability.actions.cardLastingEffect(context => ({
+                    effect: ability.effects.cardCannot({
+                        cannot: 'target',
+                        restricts: 'opponentsCardEffects',
+                        player: context.player
+                    })
+                }))
+            },
+            effect: 'make {0} untargetable by opponents\' card effects and draw a card',
+            gameAction: ability.actions.draw()
+        });
     }
 }
 
-MantraOfEarth.id = 'mantra-of-earth'; // This is a guess at what the id might be - please check it!!!
+MantraOfEarth.id = 'mantra-of-earth';
 
 module.exports = MantraOfEarth;
--- test/server/cards/04.6-EU/MantraOfEarth.spec.js
+++ test/server/cards/04.6-EU/MantraOfEarth.spec.js
@@ -1,29 +1,79 @@
-describe('Mantra of Earth', function() {
-    integration(function() {
-        describe('Mantra of Earth\'s ability', function() {
-            beforeEach(function() {
+describe('Mantra of Earth', function () {
+    integration(function () {
+        describe('Mantra of Earth\'s ability', function () {
+            beforeEach(function () {
                 this.setupTest({
                     phase: 'conflict',
-                    player1: {
-                        inPlay: []
-                    },
                     player2: {
-                        inPlay: []
+                        provinces: ['kuroi-mori'],
+                        inPlay: ['child-of-the-plains', 'togashi-initiate'],
+                        hand: ['spreading-the-darkness', 'mantra-of-earth']
+                    },
+                    player1: {
+                        inPlay: ['solemn-scholar'],
+                        hand: ['assassination', 'cloud-the-mind', 'spreading-the-darkness', 'mantra-of-earth']
                     }
                 });
+                this.mantra = this.player2.findCardByName('mantra-of-earth', 'hand');
+                this.togashi = this.player2.findCardByName('togashi-initiate');
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('it should trigger when the earth ring is contested', function () {
+                this.initiateConflict({
+                    ring: 'earth',
+                    attackers: ['solemn-scholar']
+                });
+                expect(this.player2).toHavePrompt('Triggered Abilities');
+                expect(this.player2).toBeAbleToSelect(this.mantra);
+                this.player2.clickCard(this.mantra);
+                expect(this.player2).toBeAbleToSelect(this.togashi);
+                this.player2.clickCard(this.togashi);
+                this.player2.clickPrompt('Done');
+                expect(this.player2).toHavePrompt('Conflict Action Window');
             });
 
-            it('should not trigger under ABC circumstances', function() {
+            describe('when the ability is triggered', function () {
+                beforeEach(function () {
+                    this.initHand = this.player2.hand.length;
+                    this.initiateConflict({
+                        ring: 'earth',
+                        province: 'kuroi-mori',
+                        attackers: ['solemn-scholar']
+                    });
+                    this.player2.clickCard(this.mantra);
+                    this.player2.clickCard(this.togashi);
+                    this.player2.clickPrompt('Done');
+                });
 
-            });
+                it('should prevent targeting by opponent\'s events', function () {
+                    this.player2.pass();
+                    this.player1.clickCard('assassination');
+                    expect(this.player1).toHavePrompt('Assassination');
+                    expect(this.player1).toBeAbleToSelect('solemn-scholar');
+                    expect(this.player1).not.toBeAbleToSelect(this.togashi);
+                });
 
-            it('should have DEF effect on GHI', function() {
+                it('should draw 1 card', function () {
+                    expect(this.player2.hand.length).toBe(this.initHand);
+                });
 
+                it('should not prevent attaching attachments to that character', function () {
+                    this.player2.pass();
+                    this.player1.clickCard('cloud-the-mind');
+                    expect(this.player1).toHavePrompt('Cloud the Mind');
+                    expect(this.player1).toBeAbleToSelect('solemn-scholar');
+                    expect(this.player1).toBeAbleToSelect(this.togashi);
+                });
+
+                it('should not prevent targeting that character with ring effects', function () {
+                    this.player2.clickCard('kuroi-mori');
+                    this.player2.clickPrompt('Switch the contested ring');
+                    this.player2.clickRing('water');
+                    this.noMoreActions();
+                    expect(this.player1).toHavePrompt('Water Ring');
+                    expect(this.player1).toBeAbleToSelect(this.togashi);
+                });
             });
         });
     });
