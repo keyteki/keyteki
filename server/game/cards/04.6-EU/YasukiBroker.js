b2a0382c728650c49c119579f2b21ca3048a0825
Mark Byrne <markjbyrne81@gmail.com>
2018-08-01 12:28:17 +0100

    EU: Yasuki Broker (#2388)
    * Implementing Yasuki Broker
    
    Including test.
    
    * Fixing bug in test.

--- server/game/cards/04.6-EU/YasukiBroker.js
+++ server/game/cards/04.6-EU/YasukiBroker.js
@@ -2,6 +2,14 @@ const DrawCard = require('../../drawcard.js');
 
 class YasukiBroker extends DrawCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+        this.persistentEffect({
+            condition: () => this.isParticipating(),
+            match: card => card.getType() === 'character',
+            effect: [
+                ability.effects.addKeyword('courtesy'),
+                ability.effects.addKeyword('sincerity')
+            ]
+        });
     }
 }
 
