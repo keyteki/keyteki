bbda2d05080d4f549ede08bc0724270757925914
Mark Byrne <markjbyrne81@gmail.com>
2018-08-01 12:30:20 +0100

    EU: Unleash the Djinn (#2365)
    * Adding test and effect message to card implementation.
    
    * Update UnleashTheDjinn.js

--- server/game/cards/04.6-EU/UnleashTheDjinn.js
+++ server/game/cards/04.6-EU/UnleashTheDjinn.js
@@ -1,10 +1,24 @@
 const DrawCard = require('../../drawcard.js');
 
 class UnleashTheDjinn extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Make all participating characters 3/3',
+            condition: () => this.game.isDuringConflict(),
+            cost: ability.costs.payHonor(3),
+            gameAction: ability.actions.cardLastingEffect(context => ({
+                target: context.game.currentConflict.getParticipants(),
+                effect: [
+                    ability.effects.setMilitarySkill(3),
+                    ability.effects.setPoliticalSkill(3)
+                ]
+            })),
+            effect: 'make all participating characters 3{1}/3{2}',
+            effectArgs: () => ['military', 'political']
+        });
     }
 }
 
-UnleashTheDjinn.id = 'unleash-the-djinn'; // This is a guess at what the id might be - please check it!!!
+UnleashTheDjinn.id = 'unleash-the-djinn';
 
 module.exports = UnleashTheDjinn;
