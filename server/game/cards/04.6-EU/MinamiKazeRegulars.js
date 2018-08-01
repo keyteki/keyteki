e470a3971ab3b738f6842ead7967238701727042
Mark Byrne <markjbyrne81@gmail.com>
2018-08-01 12:30:11 +0100

    EU: Minami Kaze Regulars (#2366)
    * Implementing Minami Kaze Regulars
    
    Including test
    
    * Update MinamiKazeRegulars.js

--- server/game/cards/04.6-EU/MinamiKazeRegulars.js
+++ server/game/cards/04.6-EU/MinamiKazeRegulars.js
@@ -1,10 +1,24 @@
 const DrawCard = require('../../drawcard.js');
 
 class MinamiKazeRegulars extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.reaction({
+            title: 'Gain a fate and draw a card',
+            when: {
+                afterConflict: (event, context) => 
+                    event.conflict.winner === context.player &&
+                    context.source.isParticipating() &&
+                    context.game.currentConflict.hasMoreParticipants(context.player)
+            },
+            gameAction: [
+                ability.actions.gainFate(),
+                ability.actions.draw()
+            ],
+            effect: 'gain a fate and draw a card'
+        });
     }
 }
 
-MinamiKazeRegulars.id = 'minami-kaze-regulars'; // This is a guess at what the id might be - please check it!!!
+MinamiKazeRegulars.id = 'minami-kaze-regulars';
 
 module.exports = MinamiKazeRegulars;
--- test/server/cards/04.6-EU/MinamiKazeRegulars.spec.js
+++ test/server/cards/04.6-EU/MinamiKazeRegulars.spec.js
@@ -5,25 +5,63 @@ describe('Minami Kaze Regulars', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        fate: 3,
+                        inPlay: ['minami-kaze-regulars'],
+                        hand:[]
                     },
                     player2: {
-                        inPlay: []
+                        inPlay: ['borderlands-defender', 'steadfast-witch-hunter']
                     }
                 });
+
+                this.minamiKazeRegulars = this.player1.findCardByName('minami-kaze-regulars');
+
+                this.borderlandsDefender = this.player2.findCardByName('borderlands-defender');
+                this.steadfastWitchHunter = this.player2.findCardByName('steadfast-witch-hunter');
+
+                this.shamefulDisplay = this.player2.findCardByName('shameful-display', 'province 1');
+
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('should trigger after winning a conflict where controller has more participants than the opponent', function() {
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: [this.minamiKazeRegulars],
+                    defenders: [],
+                    province: this.shamefulDisplay
+                });
+                this.noMoreActions();
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                this.player1.clickCard(this.minamiKazeRegulars);
+                expect(this.player1.hand.length).toBe(1);
+                expect(this.player1.fate).toBe(4);
+                expect(this.player1).toHavePrompt('Break Shameful Display');
+                this.player1.clickPrompt('Yes');
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
+            it('should not trigger after winning a conflict where controller has less or equal participants than the opponent', function() {
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: [this.minamiKazeRegulars],
+                    defenders: [this.steadfastWitchHunter]
+                });
+                this.noMoreActions();
+                expect(this.player1.hand.length).toBe(0);
+                expect(this.player1.fate).toBe(3);
             });
 
-            it('should have DEF effect on GHI', function() {
-
+            it('should not trigger after losing a conflict', function() {
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: [this.minamiKazeRegulars],
+                    defenders: [this.borderlandsDefender, this.steadfastWitchHunter]
+                });
+                this.noMoreActions();
+                expect(this.game.currentConflict).toBeNull();
+                expect(this.player1).toHavePrompt('Action Window');
+                expect(this.player1.hand.length).toBe(0);
+                expect(this.player1.fate).toBe(3);
             });
         });
     });
