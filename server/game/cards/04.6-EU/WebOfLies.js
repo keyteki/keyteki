4355143c00148822add4a50b61b059fb7050e06f
Mark Byrne <markjbyrne81@gmail.com>
2018-08-01 12:27:38 +0100

    EU: Web of Lies (#2392)
    * Implementing Web Of Lies
    
    Including test.
    
    * Adding Test for Web Of Lies
    
    * Fix bug in how the strength modification is cancelled.

--- server/game/cards/04.6-EU/WebOfLies.js
+++ server/game/cards/04.6-EU/WebOfLies.js
@@ -2,9 +2,13 @@ const ProvinceCard = require('../../provincecard.js');
 
 class WebOfLies extends ProvinceCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+        this.persistentEffect({
+            match: this,
+            effect: ability.effects.modifyProvinceStrength(() => this.controller.showBid * 2)
+        });
     }
 }
 
-WebOfLies.id = 'web-of-lies'; // This is a guess at what the id might be - please check it!!!
+WebOfLies.id = 'web-of-lies';
 
 module.exports = WebOfLies;
--- test/server/cards/04.6-EU/WebOfLies.spec.js
+++ test/server/cards/04.6-EU/WebOfLies.spec.js
@@ -3,27 +3,53 @@ describe('Web of Lies', function() {
         describe('Web of Lies\'s ability', function() {
             beforeEach(function() {
                 this.setupTest({
-                    phase: 'conflict',
+                    phase: 'draw',
                     player1: {
-                        inPlay: []
+                        inPlay: ['borderlands-defender']
                     },
                     player2: {
-                        inPlay: []
+                        inPlay: [],
+                        hand: ['maze-of-illusion'],
+                        provinces: ['web-of-lies']
                     }
                 });
+                this.borderlandsDefender = this.player1.findCardByName('borderlands-defender');
+
+                this.webOfLies = this.player2.findCardByName('web-of-lies');
+
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('3');
+
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('should have a strength of 6', function() {
+                expect(this.game.currentPhase).toBe('conflict');
+                this.noMoreActions();
+                this.initiateConflict({
+                    type: 'military',
+                    province: 'web-of-lies',
+                    attackers: [this.borderlandsDefender],
+                    defenders: []
+                });
+                expect(this.webOfLies.getStrength()).toBe(6);
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
-            });
-
-            it('should have DEF effect on GHI', function() {
-
+            it('provice should update if the players bid changes', function() {
+                expect(this.game.currentPhase).toBe('conflict');
+                this.noMoreActions();
+                this.initiateConflict({
+                    type: 'military',
+                    province: 'web-of-lies',
+                    attackers: [this.borderlandsDefender],
+                    defenders: []
+                });
+                expect(this.webOfLies.getStrength()).toBe(6);
+                this.player2.clickCard('maze-of-illusion');
+                this.player2.clickCard(this.borderlandsDefender);
+                this.player2.clickPrompt('1');
+                this.player1.clickPrompt('Odd');
+                expect(this.webOfLies.getStrength()).toBe(2);
             });
         });
     });
