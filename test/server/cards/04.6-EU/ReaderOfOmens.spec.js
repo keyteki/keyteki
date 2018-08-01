e4ac59087a1e93f58cc35e853ffbdbf73ecde704
Noah <nbtheduke@gmail.com>
2018-08-01 07:26:19 -0400

    Reader of Omens (#2396)

--- test/server/cards/04.6-EU/ReaderOfOmens.spec.js
+++ test/server/cards/04.6-EU/ReaderOfOmens.spec.js
@@ -1,30 +1,56 @@
-describe('Reader of Omens', function() {
+describe('Reader Of Omens', function() {
     integration(function() {
-        describe('Reader of Omens\'s ability', function() {
+        describe('Reader Of Omens\'s ability', function() {
             beforeEach(function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        inPlay: ['reader-of-omens']
                     },
                     player2: {
                         inPlay: []
                     }
                 });
+                this.reader = this.player1.findCardByName('reader-of-omens');
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
+            function worksWithRing(ringType) {
+                it('should give +3 to political skill if the ' + ringType + ' ring is claimed',
+                    function() {
+                        let political = this.reader.getPoliticalSkill();
+                        this.player1.claimRing(ringType);
+                        expect(this.reader.getPoliticalSkill()).toBe(political + 3);
+                    }
+                );
+            }
 
-            });
+            for(let ringType of ['air', 'void']) {
+                worksWithRing(ringType);
+            }
 
-            it('should not trigger under ABC circumstances', function() {
+            function doesntWorkWithRing(ringType) {
+                it('should not give +3 to political skill if the ' + ringType + ' ring is claimed',
+                    function() {
+                        let political = this.reader.getPoliticalSkill();
+                        this.player1.claimRing(ringType);
+                        expect(this.reader.getPoliticalSkill()).toBe(political);
+                    }
+                );
+            }
 
-            });
+            for(let ringType of ['earth', 'fire', 'water']) {
+                doesntWorkWithRing(ringType);
+            }
 
-            it('should have DEF effect on GHI', function() {
-
-            });
+            it('should not give +3 to political skill if the opponent claimed the air or void ring',
+                function() {
+                    let political = this.reader.getPoliticalSkill();
+                    this.player2.claimRing('air');
+                    this.player2.claimRing('void');
+                    expect(this.reader.getPoliticalSkill()).toBe(political);
+                }
+            );
         });
     });
 });
