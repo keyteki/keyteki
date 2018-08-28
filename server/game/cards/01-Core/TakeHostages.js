cac7e1e49c83a0a5c9f42a6025cd41a6c85b2613
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-08-28 06:53:01 +0100

    Take Hostages bugfix

--- server/game/cards/01-Core/TakeHostages.js
+++ server/game/cards/01-Core/TakeHostages.js
@@ -4,12 +4,12 @@ class TakeHostages extends Card {
     setupCardAbilities(ability) {
         this.play({
             effect: 'capture amber after fighting with a creature until the end of the turn',
-            gameAction: ability.actions.forRemainderOfTurn({
+            gameAction: ability.actions.forRemainderOfTurn(context => ({
                 when: {
-                    onFight: (event, context) => event.attacker.controller === context.player && event.attacker.location === 'play area'
+                    onFight: event => event.attacker.controller === context.player && event.attacker.location === 'play area'
                 },
                 gameAction: ability.actions.capture()
-            })
+            }))
         });
     }
 }
