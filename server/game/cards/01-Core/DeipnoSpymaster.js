cf02bb4469f0b7c10c4f0a9d1582bf85003c342a
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-10-17 17:55:24 +0100

    0.11.6

--- server/game/cards/01-Core/DeipnoSpymaster.js
+++ server/game/cards/01-Core/DeipnoSpymaster.js
@@ -6,8 +6,11 @@ class DeipnoSpymaster extends Card {
             target: {
                 cardType: 'creature',
                 controller: 'self',
-                gameAction: ability.actions.use()
-            }            
+                gameAction: ability.actions.forRemainderOfTurn(context => ({
+                    effect: ability.effects.canUse(card => card === context.target)
+                }))
+            },
+            effect: 'allow {0} to be used for the remainder of the turn'
         });
     }
 }
