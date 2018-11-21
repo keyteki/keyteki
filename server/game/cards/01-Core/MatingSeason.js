eb7944436a19cb2ee5da8b41a931aad047873836
jeremylarner <jeremyaroundtheworld@gmail.com>
2018-11-21 16:09:21 +0000

    0.13.10

--- server/game/cards/01-Core/MatingSeason.js
+++ server/game/cards/01-Core/MatingSeason.js
@@ -5,15 +5,15 @@ class MatingSeason extends Card {
         this.play({
             effect: 'shuffle each Mars creature into their owner\'s deck, and each player gains 1 amber for each creature shuffled into their deck',
             gameAction: [
-                ability.actions.returnToDeck(context => ({
-                    target: context => context.game.creaturesInPlay.filter(card => card.hasHouse('mars'))
-                })),
                 ability.actions.gainAmber(context => ({
                     amount: context.player.creaturesInPlay.filter(card => card.hasHouse('mars'))
                 })),
                 ability.actions.gainAmber(context => ({
                     target: context.player.opponent,
                     amount: context.player.opponent.creaturesInPlay.filter(card => card.hasHouse('mars'))
+                })),
+                ability.actions.returnToDeck(context => ({
+                    target: context.game.creaturesInPlay.filter(card => card.hasHouse('mars'))
                 }))
             ]
         });
--- server/game/cards/01-Core/OathOfPoverty.js
+++ server/game/cards/01-Core/OathOfPoverty.js
@@ -5,9 +5,14 @@ class OathOfPoverty extends Card {
         this.play({
             effect: 'destroy all their artifacts and gain {1} amber',
             effectArgs: context => context.player.cardsInPlay.filter(card => card.type === 'artifact').length * 2,
-            gameActions: ability.actions.gainAmber(context => ({
-                amount: context.player.cardsInPlay.filter(card => card.type === 'artifact').length * 2
-            }))
+            gameActions: [
+                ability.actions.gainAmber(context => ({
+                    amount: context.player.cardsInPlay.filter(card => card.type === 'artifact').length * 2
+                })),
+                ability.actions.destroy(context => ({
+                    target: context.player.cardsInPlay.filter(card => card.type === 'artifact')
+                }))
+            ]
         });
     }
 }
