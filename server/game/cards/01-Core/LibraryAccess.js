d63890325cfccefe0525a0ddb3233daf8afc5be0
jeremylarner <jeremyaroundtheworld@gmail.com>
2019-05-30 07:29:59 +0100

    Update LibraryAccess.js

--- server/game/cards/01-Core/LibraryAccess.js
+++ server/game/cards/01-Core/LibraryAccess.js
@@ -3,7 +3,7 @@ const Card = require('../../Card.js');
 class LibraryAccess extends Card {
     setupCardAbilities(ability) {
         this.play({
-            effect: 'draw a card after playing a card for the remainder of the turn',
+            effect: 'draw a card after playing a card for the remainder of the turn, and purge {0}',
             gameAction: [
                 ability.actions.forRemainderOfTurn(context => ({
                     when: {
