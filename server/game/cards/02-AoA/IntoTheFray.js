2dc88e44d51109c230377350dc9de655041c6f85
Stuart Walsh <stu@ipng.org.uk>
2019-06-30 08:18:23 +0100

    implement into the fray

---
+++ server/game/cards/02-AoA/IntoTheFray.js
@@ -0,0 +1,22 @@
+const Card = require('../../Card.js');
+
+class IntoTheFray extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            target: {
+                cardType: 'creature',
+                cardCondition: card => card.hasHouse('brobnar'),
+                gameAction: ability.actions.cardLastingEffect({
+                    effect: ability.effects.gainAbility('fight', {
+                        gameAction: ability.actions.ready()
+                    })
+                })
+            },
+            effect: 'give {0} a fight ability'
+        });
+    }
+}
+
+IntoTheFray.id = 'into-the-fray';
+
+module.exports = IntoTheFray;
