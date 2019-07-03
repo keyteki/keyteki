82173d306259492daa65581c5d26e68eafc04e9a
Stuart Walsh <stu@ipng.org.uk>
2019-07-03 21:06:07 +0100

    Implement song of spring

---
+++ server/game/cards/02-AoA/SongOfSpring.js
@@ -0,0 +1,19 @@
+const Card = require('../../Card.js');
+
+class SongOfSpring extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            target: {
+                mode: 'unlimited',
+                location: ['play area', 'discard', 'hand'],
+                cardCondition: card => card.type === 'creature' && card.hasHouse('untamed'),
+                controller: 'self',
+                gameAction: ability.actions.returnToDeck({ shuffle: true })
+            }
+        });
+    }
+}
+
+SongOfSpring.id = 'song-of-spring';
+
+module.exports = SongOfSpring;
