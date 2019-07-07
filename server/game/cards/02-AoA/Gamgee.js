8dab6859fc8a14c9f85a72a804d21cfc2f05154a
Stuart Walsh <stu@ipng.org.uk>
2019-07-07 20:03:17 +0100

    Implement Gamgee

---
+++ server/game/cards/02-AoA/Gamgee.js
@@ -0,0 +1,14 @@
+const Card = require('../../Card.js');
+
+class Gamgee extends Card {
+    setupCardAbilities(ability) {
+        this.reap({
+            condition: context => context.player.opponent && context.player.amber < context.player.opponent.amber,
+            gameAction: ability.actions.steal()
+        });
+    }
+}
+
+Gamgee.id = 'gamgee';
+
+module.exports = Gamgee;
