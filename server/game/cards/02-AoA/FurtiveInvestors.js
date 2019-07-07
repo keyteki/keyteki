a42322732f50c0e0e51978c81428260a61a02e41
Stuart Walsh <stu@ipng.org.uk>
2019-07-07 20:01:26 +0100

    Implement furtive investors

---
+++ server/game/cards/02-AoA/FurtiveInvestors.js
@@ -0,0 +1,16 @@
+const Card = require('../../Card.js');
+
+class FurtiveInvestors extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            condition: context => context.player.opponent && context.player.amber < context.player.opponent.amber,
+            gameAction: ability.actions.gainAmber(context => ({
+                amount: context.player.opponent.keys
+            }))
+        });
+    }
+}
+
+FurtiveInvestors.id = 'furtive-investors';
+
+module.exports = FurtiveInvestors;
