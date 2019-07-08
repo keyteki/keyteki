11caab5967b280680dd31683fbbfaa966888718d
Stuart Walsh <stu@ipng.org.uk>
2019-07-08 20:16:42 +0100

    Implement unlocked gateway

---
+++ server/game/cards/02-AoA/UnlockedGateway.js
@@ -0,0 +1,13 @@
+const Card = require('../../Card.js');
+
+class UnlockedGateway extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            gameAction: ability.actions.destroy(context => ({ target: context.game.creaturesInPlay }))
+        });
+    }
+}
+
+UnlockedGateway.id = 'unlocked-gateway';
+
+module.exports = UnlockedGateway;
