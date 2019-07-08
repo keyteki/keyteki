16e53162b0728ded85087b5b64e1892db1847b65
Stuart Walsh <stu@ipng.org.uk>
2019-07-08 19:56:32 +0100

    Implement Not Finished With You

---
+++ server/game/cards/02-AoA/NotFinishedWithYou.js
@@ -0,0 +1,21 @@
+const Card = require('../../Card.js');
+
+class NotFinishedWithYou extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            target: {
+                activePromptTitle: 'Choose which creatres to return',
+                mode: 'unlimited',
+                controller: 'self',
+                cardType: 'creature',
+                location: 'discard',
+                gameAction: [
+                    ability.actions.returnToDeck({ shuffle: true })
+                ]}
+        });
+    }
+}
+
+NotFinishedWithYou.id = 'not-finished-with-you';
+
+module.exports = NotFinishedWithYou;
