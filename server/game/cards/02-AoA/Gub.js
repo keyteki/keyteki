5cd87d7e1552294aa54dfe5576530e952e6c6f79
Stuart Walsh <stu@ipng.org.uk>
2019-07-08 19:39:34 +0100

    Implement gub

---
+++ server/game/cards/02-AoA/Gub.js
@@ -0,0 +1,18 @@
+const Card = require('../../Card.js');
+
+class Gub extends Card {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: this,
+            condition: () => !this.isOnFlank(),
+            effect: [
+                ability.effects.modifyPower(5),
+                ability.effects.addKeyword({ taunt: 1 })
+            ]
+        });
+    }
+}
+
+Gub.id = 'gub';
+
+module.exports = Gub;
