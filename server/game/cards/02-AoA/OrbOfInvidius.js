b7750f251b855921788080e2dc736f4352324255
Stuart Walsh <stu@ipng.org.uk>
2019-07-14 16:44:21 +0100

    implement orb of invidius

---
+++ server/game/cards/02-AoA/OrbOfInvidius.js
@@ -0,0 +1,18 @@
+const Card = require('../../Card.js');
+
+class OrbOfInvidius extends Card {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onReap: event => event.card.type === 'creature'
+            },
+            gameAction: ability.actions.stun(context => ({
+                target: context.event.card
+            }))
+        });
+    }
+}
+
+OrbOfInvidius.id = 'orb-of-invidius';
+
+module.exports = OrbOfInvidius;
