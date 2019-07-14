37f0fe8f0ac3b750a6d9d9be4b4e271dc33738b4
Stuart Walsh <stu@ipng.org.uk>
2019-07-14 15:19:36 +0100

    implement soulkeeper

---
+++ server/game/cards/02-AoA/Soulkeeper.js
@@ -0,0 +1,21 @@
+const Card = require('../../Card.js');
+
+class Soulkeeper extends Card {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.gainAbility('destroyed', {
+                target: {
+                    mode: 'mostStat',
+                    cardType: 'creature',
+                    numCards: 1,
+                    cardStat: card => card.power,
+                    gameAction: ability.actions.destroy()
+                }
+            })
+        });
+    }
+}
+
+Soulkeeper.id = 'soulkeeper';
+
+module.exports = Soulkeeper;
