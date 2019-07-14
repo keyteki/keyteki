1ee9a23c7c3f659139ac4dcef04f04de90b11047
Stuart Walsh <stu@ipng.org.uk>
2019-07-14 15:56:47 +0100

    Implement spyyyder

---
+++ server/game/cards/02-AoA/Spyyyder.js
@@ -0,0 +1,19 @@
+const Card = require('../../Card.js');
+
+class Spyyyder extends Card {
+    setupCardAbilities(ability) {
+        this.interrupt({
+            when: {
+                onFight: (event, context) => event.attacker === context.source && event.attackerTarget.isOnFlank()
+            },
+            gameAction: ability.actions.cardLastingEffect({
+                effect: ability.effects.addKeyword({ poison: 1 })
+            }),
+            effect: 'add poison when attacking a flank creature'
+        });
+    }
+}
+
+Spyyyder.id = 'spyyyder';
+
+module.exports = Spyyyder;
