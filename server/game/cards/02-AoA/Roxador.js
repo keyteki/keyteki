00a07e2e6dc02376ab2bf81736f0a9ae7c8cd8bf
arthur <arthur@eligeo.com>
2019-07-22 17:53:42 -0600

    Added Roxador and StormCrawler to take advantage of newly added limitFightDamage effect.

---
+++ server/game/cards/02-AoA/Roxador.js
@@ -0,0 +1,17 @@
+const Card = require('../../Card.js');
+
+class Roxador extends Card {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: this,
+            effect: ability.effects.limitFightDamage(2)
+        });
+        this.fight({
+            gameAction: ability.actions.stun(context => ({ target: context.event.card }))
+        });
+    }
+}
+
+Roxador.id = 'roxador';
+
+module.exports = Roxador;
