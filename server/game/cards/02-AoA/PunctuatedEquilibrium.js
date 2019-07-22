19c335868e59b8caea591d52b1e5fe136ae2de42
arthur <arthur@eligeo.com>
2019-07-21 21:53:13 -0600

    Added some Untamed Cards. Believe to be 100% accurate.

---
+++ server/game/cards/02-AoA/PunctuatedEquilibrium.js
@@ -0,0 +1,26 @@
+const Card = require('../../Card.js');
+
+class PunctuatedEquilibrium extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            effect: 'discard all cards from both players\' hands and refill them as if it were the end of the turn.',
+            gameAction: [
+                ability.actions.discard(context => ({
+                    target: context.player.hand
+                })),
+                ability.actions.draw(context => ({ amount: context.player.maxHandSize })),
+                ability.actions.discard(context => ({
+                    target: context.player.opponent.hand
+                })),
+                ability.actions.draw(context => ({
+                    target: context.player.opponent,
+                    amount: context.player.opponent.maxHandSize
+                }))
+            ]
+        });
+    }
+}
+
+PunctuatedEquilibrium.id = 'punctuated-equilibrium';
+
+module.exports = PunctuatedEquilibrium;
