d691db11bdcdfe27210fc9d0f491ac61eead6762
issa <issa@luniki.de>
2019-07-23 11:54:31 +0200

    add missing Shards, refs #5, #6, #8

---
+++ server/game/cards/02-AoA/ShardOfHate.js
@@ -0,0 +1,22 @@
+const Card = require('../../Card.js');
+
+class ShardOfHate extends Card {
+    setupCardAbilities(ability) {
+        this.action({
+            gameAction: ability.actions.sequentialForEach(context => ({
+                num: context.player.cardsInPlay.filter(card => card.hasTrait('shard')).length,
+                action: ability.actions.stun({
+                    promptForSelect: {
+                        activePromptTitle: 'Choose a creature to stun',
+                        cardType: 'creature',
+                        controller: 'opponent'
+                    }
+                })
+            }))
+        });
+    }
+}
+
+ShardOfHate.id = 'shard-of-hate';
+
+module.exports = ShardOfHate;
