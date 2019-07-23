6fec76390755b193da9bf166e375708f4f7a5035
Tristan Greaves <tristan@extricate.org>
2019-07-23 15:54:52 +0100

    Implement 'Plague Rat'.

---
+++ server/game/cards/02-AoA/PlagueRat.js
@@ -0,0 +1,17 @@
+const Card = require('../../Card.js');
+
+class PlagueRat extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            effect: 'Each non-Rat creature is dealt 1 damage for each Rat creature in play.',
+            gameAction: ability.actions.dealDamage(context => ({
+                amount: context.game.creaturesInPlay.filter(card => card.hasTrait('rat')).length,
+                target: context.game.creaturesInPlay.filter(card => !card.hasTrait('rat'))
+            }))
+        });
+    }
+}
+
+PlagueRat.id = 'plague-rat';
+
+module.exports = PlagueRat;
