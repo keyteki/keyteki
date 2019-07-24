02f35c1f3e89442858b5257437683bdf320b179d
小林一茶 <issa@luniki.de>
2019-07-24 08:34:28 +0200

    add missing Shards, refs #5, #6, #8 (#35)

---
+++ server/game/cards/02-AoA/ShardOfKnowledge.js
@@ -0,0 +1,15 @@
+const Card = require('../../Card.js');
+
+class ShardOfKnowledge extends Card {
+    setupCardAbilities(ability) {
+        this.action({
+            gameAction: ability.actions.draw(context => ({
+                amount: context.player.cardsInPlay.filter(card => card.hasTrait('shard')).length
+            }))
+        });
+    }
+}
+
+ShardOfKnowledge.id = 'shard-of-knowledge';
+
+module.exports = ShardOfKnowledge;
