fa9335e8d708741dd90a463ff86f6d1dcd2f4698
jeremylarner <jeremyaroundtheworld@gmail.com>
2019-07-25 11:34:17 +0100

    Camouflage

---
+++ server/game/cards/02-AoA/Camouflage.js
@@ -0,0 +1,13 @@
+const Card = require('../../Card.js');
+
+class Camouflage extends Card {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.cardCannot('attack', context => !context.source.isOnFlank())
+        });
+    }
+}
+
+Camouflage.id = 'camouflage';
+
+module.exports = Camouflage;
