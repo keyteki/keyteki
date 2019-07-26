5c798ebf9485cc2725bc760ca20effdcd2cef36b
EmbraCraig <embracraig.gmail.com>
2019-07-26 23:04:00 +0100

    Added first draft for Tyxl Beambucker and MoveToFlank action for feedback (not currently working)

---
+++ server/game/GameActions/MoveToFlank.js
@@ -0,0 +1,54 @@
+const CardGameAction = require('./CardGameAction');
+
+class MoveToFlankAction extends CardGameAction {
+    setDefaultProperties() {
+        this.left = false;
+        this.myControl = false;
+    }
+
+    
+    update(context) {
+        this.applyProperties(Object.assign({ target: this.getDefaultTargets(context), origin: context.source }, this.propertyFactory(context)));
+    }
+
+    setup() {
+        this.name = 'moveToFlank';
+        this.targetType = ['creature'];
+        this.effectMsg = 'moved {0} to flank';
+    }
+
+    canAffect(card, context) {
+        return card.location === 'play area' && super.canAffect(card, context);
+    }
+
+    preEventHandler(context) {
+        super.preEventHandler(context);
+        let card = this.target.length > 0 ? this.target[0] : context.source;
+        let player = this.myControl ? context.player : card.controller;
+        if(player.cardsInPlay.some(card => card.type === 'creature')) {
+            context.game.promptWithHandlerMenu(context.player, {
+                activePromptTitle: 'Which flank do you want to move this creature to?',
+                context: context,
+                source: this.target.length > 0 ? this.target[0] : context.source,
+                choices: ['Left', 'Right'],
+                choiceHandler: choice => this.left = choice === 'Left'
+            });
+        }
+    }
+
+    getEvent(card, context) {
+        return super.createEvent('onMoveToFlank', { card: card, context: context }, () => {
+            let cardIndex = card.controller.cardsInPlay.indexOf(card);
+            if(this.Left) {
+                this.origin.controller.cardsInPlay.splice(cardIndex, 1);
+                this.origin.controller.cardsInPlay.unshift(card);
+            }
+            else{
+                this.origin.controller.cardsInPlay.splice(cardIndex, 1);
+                this.origin.controller.cardsInPlay.push(card);
+            }
+        });
+    }
+}
+
+module.exports = MoveToFlankAction;
---
+++ server/game/cards/02-AoA/TyxlBeambuckler.js
@@ -0,0 +1,21 @@
+const Card = require('../../Card.js');
+
+class TyxlBeambuckler extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            effect: 'Deal 2 damage to a creature and move it to either flank of its controller’s battleline.',
+            target: {
+                cardType: 'creature',
+            	gameAction: [ ability.actions.dealDamage(context => ({
+                	amount: 2
+            		})),
+            		ability.actions.moveToFlank()
+            	]
+            }
+        });
+    }
+}
+
+TyxlBeambuckler.id = 'tyxl-beambuckler';
+
+module.exports = TyxlBeambuckler;
