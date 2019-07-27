496f41752910c533479d43904afd0636e06c93ec
jeremylarner <jeremyaroundtheworld@gmail.com>
2019-07-27 18:29:31 +0100

    Add effect to Exterminate

--- server/game/cards/02-AoA/ExterminateExterminate.js
+++ server/game/cards/02-AoA/ExterminateExterminate.js
@@ -3,6 +3,7 @@ const Card = require('../../Card.js');
 class ExterminateExterminate extends Card {
     setupCardAbilities(ability) {
         this.play({
+            effect: 'destroy a non-Mars creature for each Mars creature they control',
             gameAction: ability.actions.sequentialForEach(context => ({
                 forEach: context.player.creaturesInPlay.filter(card => card.hasHouse('mars')),
                 action: card => ability.actions.destroy({
