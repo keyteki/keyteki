37ad112f55e5e5c64b7114686d0bd2257b939605
Stuart Walsh <stu@ipng.org.uk>
2019-07-27 10:28:47 +0100

    Implement anahite the trader

---
+++ server/game/cards/02-AoA/AnahitaTheTrader.js
@@ -0,0 +1,26 @@
+const Card = require('../../Card.js');
+
+class AnahitaTheTrader extends Card {
+    setupCardAbilities(ability) {
+        this.reap({
+            condition: context => context.player.opponent,
+            target: {
+                cardType: 'artifact',
+                controller: 'self',
+                gameAction: ability.actions.cardLastingEffect(context => ({
+                    duration: 'lastingEffect',
+                    effect: ability.effects.takeControl(context.player.opponent)
+                })),
+            },
+            then: {
+                gameAction: ability.actions.steal({ amount: 2 })
+            },
+            effect: 'give control of {0} to {1} and take 2 amber from them',
+            effectArgs: context => context.player.opponent
+        });
+    }
+}
+
+AnahitaTheTrader.id = 'anahita-the-trader';
+
+module.exports = AnahitaTheTrader;
