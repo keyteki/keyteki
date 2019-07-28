e29f7e79d64da119bd9fb6ceafd7f84249765008
Stuart Walsh <stu@ipng.org.uk>
2019-07-28 14:12:57 +0100

    Fix a couple more sentry issues (#118)
    * Make freemarket single player friendly
    
    * Make murkens single player friendly

--- server/game/cards/02-AoA/FreeMarkets.js
+++ server/game/cards/02-AoA/FreeMarkets.js
@@ -3,7 +3,8 @@ const Card = require('../../Card.js');
 class FreeMarkets extends Card {
     setupCardAbilities(ability) {
         this.play({
-            effect: 'gain 1 amber for each house represented amongst cards in play (except Sanctum).',
+            effect: 'gain 1 amber for each house represented amongst cards in play (except Sanctum)',
+            condition: context => !!context.player.opponent,
             gameAction: ability.actions.gainAmber(context => ({
                 amount:
                 ((context.player.cardsInPlay.filter(card => card.hasHouse('brobnar')).length || context.player.opponent.cardsInPlay.filter(card => card.hasHouse('brobnar')).length) > 0 ? 1 : 0) +
