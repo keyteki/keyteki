4f21ce09628d76d3bf47019f3e253fe39ecec31e
arthur <arthur@eligeo.com>
2019-07-28 17:14:12 -0600

    Fixed Forgemaster Og - possibly not the most efficient way to do so but it works.

--- server/game/cards/02-AoA/ForgemasterOg.js
+++ server/game/cards/02-AoA/ForgemasterOg.js
@@ -2,13 +2,23 @@ const Card = require('../../Card.js');
 
 class ForgemasterOg extends Card {
     setupCardAbilities(ability) {
+
         this.reaction({
             when: {
-                onForgeKey: () => true
+                onForgeKey: (event, context) => event.player === context.player
             },
-            gameAction: ability.actions.loseAmber(context => ({
+            gameAction: ability.actions.loseAmber((context) => ({
                 target: context.player,
                 amount: context.player.amber
+            }))
+        });
+        this.reaction({
+            when: {
+                onForgeKey: (event, context) => event.player === context.player.opponent
+            },
+            gameAction: ability.actions.loseAmber((context) => ({
+                target: context.player.opponent,
+                amount: context.player.opponent.amber
             }))
         });
     }
