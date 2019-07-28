ab40636f5ac05c2b3759e766b53b5f868f860d67
Stuart Walsh <stu@ipng.org.uk>
2019-07-28 10:29:00 +0100

    Fix binate rupture's first effect
    Fixes #87

---
+++ test/server/cards/02-AoA/BinateRupture.spec.js
@@ -0,0 +1,28 @@
+describe('Binate Rupture', function() {
+    integration(function() {
+        describe('Binate Rupture\'s ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    player1: {
+                        amber: 2,
+                        house: 'logos',
+                        hand: ['binate-rupture'],
+                        inPlay: ['troll']
+                    },
+                    player2: {
+                        amber: 2,
+                        hand: [],
+                        inPlay: ['bad-penny', 'helper-bot', 'mother', 'troll']
+                    }
+                });
+            });
+
+            it('should double both player\'s amber', function() {
+                this.player1.play(this.binateRupture);
+
+                expect(this.player1.amber).toBe(4);
+                expect(this.player2.amber).toBe(4);
+            });
+        });
+    });
+});
