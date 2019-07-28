c60805d29fd46dfb20d13586046492c438bd6e3f
jeremylarner <jeremyaroundtheworld@gmail.com>
2019-07-28 06:32:55 +0100

    Rework Equalize to correctly do friendly then enemy creatures

--- server/game/cards/02-AoA/Equalize.js
+++ server/game/cards/02-AoA/Equalize.js
@@ -3,36 +3,44 @@ const Card = require('../../Card.js');
 class Equalize extends Card {
     setupCardAbilities(ability) {
         this.play({
-            condition: context => !!context.player.opponent,
             effect: 'redistribute the amber on both player\'s creatures',
             gameAction: ability.actions.removeAmber(context => ({
-                target: context.game.creaturesInPlay,
+                target: context.player.creaturesInPlay,
                 noGameStateCheck: true,
                 all: true
             })),
             then: {
-                gameAction: [
-                    ability.actions.sequentialForEach(context => ({
-                        num: context.preThenEvents.filter(event => !event.cancelled && event.card.controller === context.player.opponent && event.amount > 0).reduce((total, event) => total + event.amount, 0),
-                        action: ability.actions.placeAmber({
-                            noGameStateCheck: true,
-                            promptForSelect: {
-                                cardType: 'creature',
-                                controller: 'opponent'
-                            }
-                        })
+                alwaysTriggers: true,
+                gameAction: ability.actions.sequentialForEach(context => ({
+                    num: context.preThenEvents.filter(event => !event.cancelled).reduce((total, event) => total + event.amount, 0),
+                    action: ability.actions.placeAmber({
+                        noGameStateCheck: true,
+                        promptForSelect: {
+                            cardType: 'creature',
+                            controller: 'self'
+                        }
+                    })
+                })),
+                then: {
+                    alwaysTriggers: true,
+                    gameAction: ability.actions.removeAmber(context => ({
+                        target: context.player.opponent && context.player.opponent.creaturesInPlay,
+                        noGameStateCheck: true,
+                        all: true
                     })),
-                    ability.actions.sequentialForEach(context => ({
-                        num: context.preThenEvents.filter(event => !event.cancelled && event.card.controller === context.player && event.amount > 0).reduce((total, event) => total + event.amount, 0),
-                        action: ability.actions.placeAmber({
-                            noGameStateCheck: true,
-                            promptForSelect: {
-                                cardType: 'creature',
-                                controller: 'self'
-                            }
-                        })
-                    }))
-                ]
+                    then: {
+                        gameAction: ability.actions.sequentialForEach(context => ({
+                            num: context.preThenEvents.filter(event => !event.cancelled).reduce((total, event) => total + event.amount, 0),
+                            action: ability.actions.placeAmber({
+                                noGameStateCheck: true,
+                                promptForSelect: {
+                                    cardType: 'creature',
+                                    controller: 'opponent'
+                                }
+                            })
+                        }))
+                    }
+                }
             }
         });
     }
