1a3a553aa9ef10e038a4251d9c30034b2cdcb77c
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2019-07-28 19:58:52 +0100

    Dharna bugfix (#140)
    * Dharna bugfix
    
    * Prince Derric bugfix
    
    * Update LittleRapscal.js

--- server/game/cards/02-AoA/LittleRapscal.js
+++ server/game/cards/02-AoA/LittleRapscal.js
@@ -3,6 +3,7 @@ const Card = require('../../Card.js');
 class LittleRapscal extends Card {
     setupCardAbilities(ability) {
         this.persistentEffect({
+            targetController: 'both',
             match: card => card.type === 'creature',
             effect: [
                 ability.effects.cardCannot('useAction', context => context.source.type === 'creature' && context.ability.title !== 'Fight with this creature'),
