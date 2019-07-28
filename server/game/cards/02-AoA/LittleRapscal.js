634b3176cca49e76ca068b0a69c0cd710f4db4c4
jeremylarner <jeremyaroundtheworld@gmail.com>
2019-07-28 22:22:31 +0100

    More Misc bugfixes

--- server/game/cards/02-AoA/LittleRapscal.js
+++ server/game/cards/02-AoA/LittleRapscal.js
@@ -3,7 +3,8 @@ const Card = require('../../Card.js');
 class LittleRapscal extends Card {
     setupCardAbilities(ability) {
         this.persistentEffect({
-            match: card => card.type === 'creature',
+            targetController: 'both',
+            match: (card, context) => card.type === 'creature' && ability.actions.fight().canAffect(card, context),
             effect: [
                 ability.effects.cardCannot('useAction', context => context.source.type === 'creature' && context.ability.title !== 'Fight with this creature'),
                 ability.effects.cardCannot('reap')
