ed64987989dad18c94239105b3035b8e187f3d2e
arthur <arthur@eligeo.com>
2019-07-29 11:21:44 -0600

    Ogopogo fix + test - should now only fire when Ogopogo is attacker.

--- server/game/cards/02-AoA/Ogopogo.js
+++ server/game/cards/02-AoA/Ogopogo.js
@@ -3,8 +3,9 @@ const Card = require('../../Card.js');
 class Ogopogo extends Card {
     setupCardAbilities(ability) {
         this.reaction({
+            condition: () => this.controller.opponent.creaturesInPlay.length > 0,
             when: {
-                onDamageDealt: (event, context) => event.damageSource === context.source && event.destroyed
+                onDamageDealt: (event, context) => event.damageSource === context.source && event.fightEvent.attacker === this && event.destroyed && context.player.opponent.creaturesInPlay.length > 0
             },
             optional: true,
             target: {
