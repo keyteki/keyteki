7c45f651246a0a402dc6d9a387a541813521c538
arthur <arthur@eligeo.com>
2019-07-28 18:07:00 -0600

    Added Effect text to make the log correct re: aember gain

--- server/game/cards/01-Core/SoulSnatcher.js
+++ server/game/cards/01-Core/SoulSnatcher.js
@@ -6,6 +6,8 @@ class SoulSnatcher extends Card {
             when: {
                 onCardDestroyed: event => event.card.type === 'creature'
             },
+            effect: 'make {1} gain 1 amber due to {2} being destroyed',
+            effectArgs: context => [context.event.card.owner, context.event.card],
             gameAction: ability.actions.gainAmber(context => ({
                 target: context.event.card.owner
             }))
