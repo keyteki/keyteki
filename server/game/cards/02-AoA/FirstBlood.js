034b14d78403f650cdecc34625c6ec2df74bedd4
arthur <arthur@eligeo.com>
2019-08-01 14:18:26 -0600

    First Blood fix to allow single damage dealt however player prefers + test.

--- server/game/cards/02-AoA/FirstBlood.js
+++ server/game/cards/02-AoA/FirstBlood.js
@@ -3,17 +3,18 @@ const Card = require('../../Card.js');
 class FirstBlood extends Card {
     setupCardAbilities(ability) {
         this.play({
-            effect: 'deal 2 damage to a creature for each friendly brobnar creature',
             gameAction: ability.actions.sequentialForEach(context => ({
-                num: context.player.cardsInPlay.filter(card => card.type === 'creature' && card.hasHouse('brobnar')).length,
+                num: (context.player.cardsInPlay.filter(card => card.type === 'creature' && card.hasHouse('brobnar')).length) * 2,
                 action: ability.actions.dealDamage({
-                    amount: 2,
+                    amount: 1,
                     promptForSelect: {
-                        activePromptTitle: 'Choose a creature to deal 2 damage to',
+                        activePromptTitle: 'Choose a creature to deal 1 damage to',
                         cardType: 'creature'
                     }
                 })
-            }))
+            })),
+            effect: 'deal 2 damage for each friendly brobnar creature, totalling {1}.',
+            effectArgs: context => (context.player.cardsInPlay.filter(card => card.type === 'creature' && card.hasHouse('brobnar')).length) * 2
         });
     }
 }
