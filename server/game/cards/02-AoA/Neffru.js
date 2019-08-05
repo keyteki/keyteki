787ed86d912ac5859f429e50545efc8ebda454eb
arthur <arthur@eligeo.com>
2019-08-05 16:41:51 -0600

    Fixed Neffru awarding opponent an aember if he was attacked and both creatures died. Added Neffru Test and Krump Test.

--- server/game/cards/02-AoA/Neffru.js
+++ server/game/cards/02-AoA/Neffru.js
@@ -4,9 +4,11 @@ class Neffru extends Card {
     setupCardAbilities(ability) {
         this.reaction({
             when: {
-                onCardDestroyed: (event) => event.clone.type === 'creature'
+                onCardDestroyed: (event) => event.clone.type === 'creature' && !(this.tokens.damage >= this.getPower())
             },
-            gameAction: ability.actions.gainAmber(context => ({ target: context.event.card.controller }))
+            gameAction: ability.actions.gainAmber(context => ({ target: context.event.card.controller })),
+            effect: 'make {1} gain 1 amber.',
+            effectArgs:  context=> context.event.card.controller
         });
     }
 }
