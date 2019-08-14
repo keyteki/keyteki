cd2b9e6cbf29b222c78a8790a02a6398ea268ed2
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2019-08-14 15:42:24 +0100

    Fix for Bonerot Venom and Containment Field (#328)
    * Delete ShadowOfDis.spec.js
    
    * Fix for Bonerot Venom
    
    * Revert "Delete ShadowOfDis.spec.js"
    
    This reverts commit fcad5d74a8ef4c1b7bcd1eb6e99b846a441bc122.
    
    * Fix Bonerot Venom and Containment field to match their card wording

--- server/game/cards/02-AoA/ContainmentField.js
+++ server/game/cards/02-AoA/ContainmentField.js
@@ -2,12 +2,11 @@ const Card = require('../../Card.js');
 
 class ContainmentField extends Card {
     setupCardAbilities(ability) {
-        this.whileAttached({
-            effect: [
-                ability.effects.gainAbility('action', { gameAction: ability.actions.destroy() }),
-                ability.effects.gainAbility('reap', { gameAction: ability.actions.destroy() }),
-                ability.effects.gainAbility('fight', { gameAction: ability.actions.destroy() })
-            ]
+        this.reaction({
+            when: {
+                onUseCard: (event, context) => event.card === context.source.parent
+            },
+            gameAction: ability.actions.destroy(context => ({ target: context.source.parent }))
         });
     }
 }
