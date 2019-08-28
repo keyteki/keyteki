431d0ea1157a8272349eeefff8c4b1c6ea6dbaeb
Arthur Chocholacek <arthur.chocholacek@gmail.com>
2019-08-28 03:26:22 -0600

    add fix for shard of greed to work with remote access. add tests. (#312)

--- server/game/cards/02-AoA/ShardOfGreed.js
+++ server/game/cards/02-AoA/ShardOfGreed.js
@@ -4,7 +4,7 @@ class ShardOfGreed extends Card {
     setupCardAbilities(ability) {
         this.action({
             gameAction: ability.actions.gainAmber(context => ({
-                amount: context.player.cardsInPlay.filter(card => card.hasTrait('shard')).length
+                amount: context.player === this.owner ? context.player.cardsInPlay.filter(card => card.hasTrait('shard')).length : context.player.cardsInPlay.filter(card => card.hasTrait('shard')).length + 1
             }))
         });
     }
