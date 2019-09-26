e0d47e4aef634dd1ba18c5d3aec8cea9dfdb253f
Patrick <saluk64007@gmail.com>
2019-09-26 03:54:54 -0700

    Worlds collide (#443)
    * Add the grim reaper, test case, and isHaunted property to player.js
    
    * Add mogghunter
    
    * Add alaka test
    
    * Add Alaka
    
    * Add Berserker Slam
    
    * Implementing irestaff: Added effect mustFightIfAble and enrage/unenrage abilities. Updated LittleRapscal to use the mustFightIfAble effect. Tests for irestaff and littlerapscal
    
    * Early exit unenrage/enrage functions, fixing lint issues, move unenrage logic to resolvefight action
    
    * include changes from json-data
    
    * Trailing comma in json data
    
    * keyteki-json-data
    
    * Sync up with new data

---
+++ test/server/cards/03-WC/TheGrimReaper.spec.js
@@ -0,0 +1,34 @@
+describe('The Grim Reaper', function() {
+    integration(function() {
+        describe('Playing the Grim Reaper:', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    player1: {
+                        house: 'brobnar',
+                        hand: ['the-grim-reaper','anger'],
+                        discard: ['poke','poke','poke','poke','poke','poke','poke','poke','poke']
+                    },
+                    player2: {
+                        house: 'brobnar',
+                        hand: ['poke'],
+                        discard: ['poke','poke','poke','poke','poke','poke','poke','poke','poke','poke']
+                    }
+                });
+            });
+
+            it('with 9 cards in discard, grim reaper should enter exhausted', function() {
+                expect(this.player1.player.isHaunted()).toBe(false);
+                expect(this.player2.player.isHaunted()).toBe(true);
+                this.player1.play(this.theGrimReaper);
+                expect(this.theGrimReaper.exhausted).toBe(true);
+            });
+
+            it('with 10 cards in discard, grim reaper should enter ready', function() {
+                this.player1.moveCard(this.anger, 'discard');
+                expect(this.player1.player.isHaunted()).toBe(true);
+                this.player1.play(this.theGrimReaper);
+                expect(this.theGrimReaper.exhausted).toBe(false);
+            });
+        });
+    });
+});
