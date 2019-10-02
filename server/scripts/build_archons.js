d732fdae6b9549f675b255e47404f7bb566bc99c
SkyJedi <skyjedi@gmail.com>
2019-10-02 03:40:46 -0500

    spruced up the build_archon.js script (#491)

--- server/scripts/build_archons.js
+++ server/scripts/build_archons.js
@@ -1,6 +1,16 @@
+//build buildAllFiles will build every permutation of the supplied houses and language provided
+//buildArchon needs [house1, house2, house3, lang, 1] to build a specific archon cardback
+//the file name is btoa(card.join()).png
+// `node server/scripts/build_archons.js`  to only build only en languages
+// `node server/scripts/build_archons.js -l en de`  to only build specific languages
+// `node server/scripts/build_archons.js -l all`  to build all languages
+// the script will drop all the files into public/img/idbacks/archons and no further moving is necessary
+
 const {createCanvas, loadImage, registerFont} = require('canvas');
 const fs = require('fs');
 const Path = require('path');
+const commandLineArgs = require('command-line-args');
+
 const lanugageJSON = {
     brobnar: {
         en: 'Brobnar',
@@ -112,14 +122,20 @@ const lanugageJSON = {
     }
 };
 const houses = ['brobnar', 'dis', 'logos', 'mars', 'sanctum', 'shadows', 'saurian', 'staralliance', 'untamed'];
-const languages = ['en', 'zhhant', 'zhhans', 'pt', 'fr', 'es', 'de', 'it', 'pl', 'th'];
-
-//build buildAllFiles will build every permutation of the supplied houses and language provided
-//buildArchon needs [house1, house2, house3, lang, 1] to build a specific archon cardback
-//the file name is btoa(card.join()).png
+const allLanguages = ['en', 'zhhant', 'zhhans', 'pt', 'fr', 'es', 'de', 'it', 'pl', 'th'];
+const optionDefinitions = [
+    { name: 'language', alias: 'l', type: String, multiple: true }
+];
 
 const buildArchon = (card) => new Promise(cardResolve => {
     const number = card[4];
+    const path = Path.resolve(__dirname, '../../public/img/idbacks/archons', `${ Buffer.from(card.join()).toString('base64') }.png`);
+    if(fs.existsSync(path)) {
+        // eslint-disable-next-line no-console
+        console.log('File Exists, moving on!');
+        cardResolve();
+        return;
+    }
     const houseNames = [{x: -305, y: 720}, {x: -125, y: 770}, {x: 55, y: 720}];
     const canvas = createCanvas(600, 840);
     const ctx = canvas.getContext('2d');
@@ -137,11 +153,10 @@ const buildArchon = (card) => new Promise(cardResolve => {
         card.slice(0, 3).forEach((house, index) => {
             ctx.drawImage((getCircularText(lanugageJSON[house][card[3]], 850, 0)), houseNames[index].x, houseNames[index].y);
         });
-        const path = Path.resolve(__dirname, './images', `${ Buffer.from(card.join()).toString('base64') }.png`);
         fs.writeFile(path, canvas.toBuffer(), () => cardResolve());
     });
 });
-const permute = () => {
+const permute = (languages) => {
     let final = [];
     for(let a = 0; a < houses.length; a++) {
         for(let b = 0; b < houses.length; b++) {
@@ -160,17 +175,26 @@ const permute = () => {
     }
     return final;
 };
+
 const buildAllFiles = async () => {
-    new registerFont('./public/fonts/Oswald-Regular.ttf', {family: 'Keyforge'});
-    new registerFont('./public/fonts/ZCOOL-Regular.ttf', {family: 'Keyforge'});
-    new registerFont('./public/fonts/Kanit-Regular.ttf', {family: 'Keyforge'});
-    const cards = permute();
+    const options = commandLineArgs(optionDefinitions);
+    if(!options.language) {
+        options.language = ['en'];
+    }
+    if(options.language[0] === 'all') {
+        options.language = allLanguages;
+    }
+    new registerFont(Path.join(__dirname, '../../public/fonts/Oswald-Regular.ttf'), {family: 'Keyforge'});
+    new registerFont(Path.join(__dirname, '../../public/fonts/ZCOOL-Regular.ttf'), {family: 'Keyforge'});
+    new registerFont(Path.join(__dirname, '../../public/fonts/Kanit-Regular.ttf'), {family: 'Keyforge'});
+    const cards = permute(options.language);
     for(let i = 0; i < cards.length; i++) {
         // eslint-disable-next-line no-console
         console.log(`Building ${ i } ${ cards[i] }.  ${cards.length - i} cards to go!`);
         await buildArchon(cards[i]);
     }
 };
+
 const getCircularText = (text = '', diameter, kerning) => {
     let canvas = createCanvas(0, 0);
     let ctx = canvas.getContext('2d');
