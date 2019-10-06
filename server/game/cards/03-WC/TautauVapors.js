191d387f647127f25ffbe7cdd1a353112d688770
Stuart Walsh <stu@ipng.org.uk>
2019-10-06 09:02:45 +0100

    Update TautauVapors.js

--- server/game/cards/03-WC/TautauVapors.js
+++ server/game/cards/03-WC/TautauVapors.js
@@ -3,7 +3,7 @@ const Card = require('../../Card.js');
 class TautauVapors extends Card {
     setupCardAbilities(ability) {
         this.play({
-            gameAction: ability.actions.draw({ amount: 2}),
+            gameAction: ability.actions.draw({ amount: 2 }),
             then: {
                 target: {
                     controller: 'self',
