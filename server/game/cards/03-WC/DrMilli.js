288a04c41d076ce248cd0ee36254deaae4efcbc9
Arthur Chocholacek <arthur.chocholacek@gmail.com>
2019-10-06 02:05:18 -0600

    Added WC Logos cards/tests - updated test for WC Berserker Slam (#527)
    * Added WC Logos cards/tests - updated test for WC Berserker Slam to reflect missing aember pip in original json. This is gonna fail without the updated WC.json the PR has been submitted for.
    
    * fix linting

---
+++ server/game/cards/03-WC/DrMilli.js
@@ -0,0 +1,26 @@
+const Card = require('../../Card.js');
+
+class DrMilli extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            condition: context => !!context.player.opponent && context.player.opponent.creaturesInPlay.length > context.player.creaturesInPlay.length - 1,
+            target: {
+                mode: 'exactly',
+                controller: 'self',
+                location: 'hand',
+                numCards: context => Math.min(
+                    context.player.hand.length, context.player.opponent.creaturesInPlay.length - (context.player.creaturesInPlay.length - 1)
+                ),
+                gameAction: ability.actions.archive()
+            },
+            effect: 'archive {1} cards',
+            effectArgs: context => [
+                context.player.opponent.creaturesInPlay.length - context.player.creaturesInPlay.length - 1
+            ]
+        });
+    }
+}
+
+DrMilli.id = 'dr-milli';
+
+module.exports = DrMilli;
