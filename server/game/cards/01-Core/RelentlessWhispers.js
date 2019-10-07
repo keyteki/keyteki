bd387e094e14d9a151a35e809efa681bf71fc637
LifeIsPizza <pizzamarinara@protonmail.com>
2019-10-07 23:52:33 +0200

    Fixed a small bug on RW that wasn't stealing properly after destroying

--- server/game/cards/01-Core/RelentlessWhispers.js
+++ server/game/cards/01-Core/RelentlessWhispers.js
@@ -8,7 +8,7 @@ class RelentlessWhispers extends Card {
                 gameAction: ability.actions.dealDamage({ amount: 2 })
             },
             then: {
-                condition: context => context.preThenEvent.destroyed && !context.preThenEvent,
+                condition: context => context.preThenEvent.destroyed,
                 gameAction: ability.actions.steal()
             }
         });
