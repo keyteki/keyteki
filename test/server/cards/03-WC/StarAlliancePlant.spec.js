7749333da96d76055dcccf5c9fa835d319d8c423
Romano Silva <romano.magacho@gmail.com>
2019-10-09 03:15:42 -0300

    Shadows' plants (depend on keyteki-json-data) (#532)
    * Shadows' plants (depend on keyteki-json-data)
    
    * Fixed a CTRL+C/V error in Imperium
    
    * Typo!

---
+++ test/server/cards/03-WC/StarAlliancePlant.spec.js
@@ -0,0 +1,97 @@
+describe('Star Alliance Plant', function() {
+    integration(function() {
+        describe('Star Alliance Plant\'s constant effect', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    player1: {
+                        house: 'shadows',
+                        inPlay: ['troll'],
+                        hand: ['star-alliance-plant', 'force-field']
+                    },
+                    player2: {
+                        inPlay: ['lamindra'],
+                        hand: ['medic-ingram', 'collar-of-subordination']
+                    }
+                });
+            });
+
+            it('Should get amber when selecting staralliance', function() {
+                expect(this.player1.amber).toBe(0);
+                expect(this.player2.amber).toBe(0);
+
+                this.player1.playCreature(this.starAlliancePlant);
+
+                this.player1.endTurn();
+
+                this.player2.clickPrompt('staralliance');
+
+                expect(this.player1.amber).toBe(1);
+                expect(this.player2.amber).toBe(0);
+
+                this.player2.endTurn();
+
+                this.player1.clickPrompt('staralliance');
+
+                expect(this.player1.amber).toBe(2);
+                expect(this.player2.amber).toBe(0);
+
+                this.player1.endTurn();
+            });
+
+            it('Should not get amber when not selecting staralliance', function() {
+                expect(this.player1.amber).toBe(0);
+                expect(this.player2.amber).toBe(0);
+
+                this.player1.playCreature(this.starAlliancePlant);
+
+                this.player1.endTurn();
+
+                this.player2.clickPrompt('dis');
+
+                expect(this.player1.amber).toBe(0);
+                expect(this.player2.amber).toBe(0);
+
+                this.player2.endTurn();
+
+                this.player1.clickPrompt('shadows');
+
+                expect(this.player1.amber).toBe(0);
+                expect(this.player2.amber).toBe(0);
+
+                this.player1.endTurn();
+            });
+
+            it('Controller get amber after taking control', function() {
+                expect(this.player1.amber).toBe(0);
+                expect(this.player2.amber).toBe(0);
+
+                this.player1.playCreature(this.starAlliancePlant);
+
+                this.player1.endTurn();
+
+                this.player2.clickPrompt('dis');
+                this.player2.playUpgrade(this.collarOfSubordination, this.starAlliancePlant);
+                this.player2.clickPrompt('Left');
+
+                expect(this.player1.amber).toBe(0);
+                expect(this.player2.amber).toBe(0);
+
+                this.player2.endTurn();
+
+                this.player1.clickPrompt('staralliance');
+
+                expect(this.player1.amber).toBe(0);
+                expect(this.player2.amber).toBe(1);
+
+                this.player1.endTurn();
+
+                this.player2.clickPrompt('staralliance');
+
+                expect(this.player1.amber).toBe(0);
+                expect(this.player2.amber).toBe(2);
+
+                this.player2.endTurn();
+            });
+        });
+    });
+});
