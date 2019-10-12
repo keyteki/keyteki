a9b964c61c63858cc05ae02dd6576c102a51d765
romano-silva <romano.silva@hpe.com>
2019-10-12 18:32:09 -0300

    Review changes

--- server/game/GameActions/RemoveTokenAction.js
+++ server/game/GameActions/RemoveTokenAction.js
@@ -14,23 +14,24 @@ class RemoveTokenAction extends CardGameAction {
     setup() {
         this.name = 'removeToken';
         this.targetType = ['artifact', 'creature'];
-        let token = '+1 power counters';
-        if(this.amount === 1) {
-            token = '+1 power counter';
+
+        let type = (this.type === 'power') ? 'power counter' : this.type;
+        if(this.all || this.amount > 1) {
+            type += 's';
         }
-        this.effectMsg = 'remove ' + (this.all ? 'all' : this.amount) + ' ' + (this.type === 'power' ? token : this.type) + ' from {0}';
+        this.effectMsg = `remove ${this.all ? 'all' : this.amount} ${type} from {0}`;
     }
 
-    computeAmount(card) {
+    getAmount(card) {
         return this.all ? card.tokens[this.type] || 0 : this.amount;
     }
 
     canAffect(card, context) {
-        return this.computeAmount(card) > 0 && card.location === 'play area' && super.canAffect(card, context);
+        return this.getAmount(card) > 0 && card.location === 'play area' && super.canAffect(card, context);
     }
 
     getEvent(card, context) {
-        return super.createEvent('onRemoveToken', { type: this.type, card: card, context: context, amount: this.computeAmount(card) }, event => {
+        return super.createEvent('onRemoveToken', { type: this.type, card: card, context: context, amount: this.getAmount(card) }, event => {
             card.removeToken(event.type, event.amount);
         });
     }
--- server/game/cards/03-WC/CenturionStenopius.js
+++ server/game/cards/03-WC/CenturionStenopius.js
@@ -4,7 +4,7 @@ class CenturionStenopius extends Card {
     setupCardAbilities(ability) {
         this.persistentEffect({
             match: this,
-            effect: ability.effects.modifyPower(() => 3 * (this.tokens['amber'] || 0))
+            effect: ability.effects.modifyPower(() => 3 * (this.tokens.amber || 0))
         });
         this.play({
             fight: true,
