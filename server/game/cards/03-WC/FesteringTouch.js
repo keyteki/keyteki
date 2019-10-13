676409f684943e41975bce4883b320620569680a
Stuart Walsh <stu@ipng.org.uk>
2019-10-13 12:27:15 +0100

    implement Festering touch

---
+++ server/game/cards/03-WC/FesteringTouch.js
@@ -0,0 +1,26 @@
+const Card = require('../../Card.js');
+
+class FesteringTouch extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            target: {
+                mode: 'upTo',
+                numCards: 2,
+                cardType: 'creature',
+                gameAction: [
+                    ability.actions.dealDamage(context => ({
+                        amount: 3,
+                        target: context.target.filter(card => card.hasToken('damage'))
+                    })),
+                    ability.actions.dealDamage(context => ({
+                        amount: 1,
+                        target: context.target.filter(card => !card.hasToken('damage'))
+                    }))
+                ] }
+        });
+    }
+}
+
+FesteringTouch.id = 'festering-touch';
+
+module.exports = FesteringTouch;
---
+++ test/server/cards/03-WC/FesteringTouch.spec.js
@@ -0,0 +1,35 @@
+describe('Festering Touch', function() {
+    integration(function() {
+        describe('its ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    player1: {
+                        house: 'dis',
+                        inPlay: ['troll', 'dextre'],
+                        hand: ['festering-touch', 'tremor']
+                    },
+                    player2: {
+                        inPlay: ['troll', 'bulwark', 'sequis']
+                    }
+                });
+            });
+
+            it('deal 1 damage to a creature that is undamaged', function() {
+                this.player1.play(this.festeringTouch);
+                this.player1.clickCard(this.troll);
+                this.player1.clickPrompt('Done');
+
+                expect(this.troll.tokens.damage).toBe(1);
+            });
+
+            it('deal 3 damage to a creature that is damaged', function() {
+                this.troll.tokens.damage = 1;
+                this.player1.play(this.festeringTouch);
+                this.player1.clickCard(this.troll);
+                this.player1.clickPrompt('Done');
+
+                expect(this.troll.tokens.damage).toBe(4);
+            });
+        });
+    });
+});
