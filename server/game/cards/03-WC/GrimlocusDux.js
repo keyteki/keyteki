075294254d26410eec02e9c1f231b86870de8b58
romano-silva <romano.silva@hpe.com>
2019-10-13 21:54:04 -0300

    5 new Saurian cards

---
+++ server/game/cards/03-WC/GrimlocusDux.js
@@ -0,0 +1,17 @@
+const Card = require('../../Card.js');
+
+class GrimlocusDux extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            effect: 'exalt {0} twice',
+            gameAction: [
+                ability.actions.exalt(),
+                ability.actions.exalt()
+            ]
+        });
+    }
+}
+
+GrimlocusDux.id = 'grimlocus-dux';
+
+module.exports = GrimlocusDux;
