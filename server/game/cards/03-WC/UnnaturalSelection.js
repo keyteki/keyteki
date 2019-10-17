20dfddfd63bb407c3c1bbc7807117600ab0c9c7f
romano-silva <romano.silva@hpe.com>
2019-10-16 23:15:43 -0300

    UnnaturalSelection.js: Remove unnecessary condition and custom effect message ForceField.spec.js: Review test message

--- server/game/cards/03-WC/UnnaturalSelection.js
+++ server/game/cards/03-WC/UnnaturalSelection.js
@@ -3,7 +3,6 @@ const Card = require('../../Card.js');
 class UnnaturalSelection extends Card {
     setupCardAbilities(ability) {
         this.play({
-            condition: context => context.player.creaturesInPlay.length > 3 || (context.player.opponent && context.player.opponent.creaturesInPlay.length > 3),
             targets: {
                 friendly: {
                     activePromptTitle: 'Choose 3 friendly creatures',
@@ -20,9 +19,6 @@ class UnnaturalSelection extends Card {
                     controller: 'opponent'
                 }
             },
-            effects: 'destroy {1}',
-            effectsArg: context => context.game.creaturesInPlay.filter(card => (!context.targets.enemy || !context.targets.enemy.includes(card)) &&
-                           (!context.targets.friendly || !context.targets.friendly.includes(card))),
             gameAction: ability.actions.destroy(context => ({
                 target: context.game.creaturesInPlay.filter(card => (!context.targets.enemy || !context.targets.enemy.includes(card)) &&
                            (!context.targets.friendly || !context.targets.friendly.includes(card)))
