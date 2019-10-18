7ac58a01d15cb27ffaf7c00557a36182ddda972c
Stuart Walsh <stu@ipng.org.uk>
2019-10-18 18:12:23 +0100

    implement Praefectus Ludo

---
+++ server/game/cards/03-WC/PraefectusLudo.js
@@ -0,0 +1,16 @@
+const Card = require('../../Card.js');
+
+class PraefectusLudo extends Card {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: card => card !== this && card.type === 'creature',
+            effect: ability.effects.gainAbility('destroyed', {
+                gameAction: ability.actions.removeAmber({ all: true })
+            })
+        });
+    }
+}
+
+PraefectusLudo.id = 'praefectus-ludo';
+
+module.exports = PraefectusLudo;
