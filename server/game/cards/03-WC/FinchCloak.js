b1a6ae480e4fca6e090c2f534a0f6a0c9a489efa
arthur <arthur@eligeo.com>
2019-10-17 22:21:00 -0600

    Added more Shadows Cards/Tests.

---
+++ server/game/cards/03-WC/FinchCloak.js
@@ -0,0 +1,34 @@
+const Card = require('../../Card.js');
+
+class FinchCloak extends Card {
+    setupCardAbilities(ability) {
+        this.triggerSteal = false;
+        this.fight({
+            reap: true,
+            gameAction: [
+                ability.actions.steal(context => {
+                    if(context.player.opponent && context.player.amber < context.player.opponent.amber) {
+                        this.triggerSteal = true;
+                        return { amount: 1 };
+                    }
+                    return { amount: 0 };
+                }),
+                ability.actions.gainAmber(context => {
+                    if(!this.triggerSteal) {
+                        return { target: context.player, amount: 1 };
+                    }
+                    return { target: context.player, amount: 0 };
+                }),
+                ability.actions.gainAmber(context => {
+                    if(!this.triggerSteal) {
+                        return { target: context.player.opponent, amount: 1 };
+                    }
+                    return { target: context.player.opponent, amount: 0 };
+                })
+            ]
+        });
+    }
+}
+FinchCloak.id = 'finch-cloak';
+
+module.exports = FinchCloak;
