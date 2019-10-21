dcdeb686a9015181b0a7a72e38fa75e019866157
romanogit <romano.magacho@gmail.com>
2019-10-21 11:55:46 -0300

    copycard.js merge

--- server/scripts/copycard.js
+++ server/scripts/copycard.js
@@ -30,7 +30,8 @@ function findFile(dirPath, targetFile) {
 
     return null;
 }
-function verifyLocale(cardsDir, source, target, tid, tpack) {
+
+function copyCard(cardsDir, source, target, tid, tpack) {
     let sourceName = source + '.js';
     let card = findFile(cardsDir, sourceName);
 
@@ -51,4 +52,4 @@ function findFile(dirPath, targetFile) {
 }
 // example usage
 // node server/scripts/copycard.js --source=Rustgnawer --target=Hock --tid=hock
-verifyLocale(options['cards-dir'], options['source'], options['target'], options['tid'], options['tpack']);
+copyCard(options['cards-dir'], options['source'], options['target'], options['tid'], options['tpack']);
