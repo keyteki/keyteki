a81721e024be4e9fe8ac6dc6c387224e9232f48b
Stuart Walsh <stu@ipng.org.uk>
2019-10-22 14:24:00 +0100

    newlines

--- test/server/cards/03-WC/Calv1N.spec.js
+++ test/server/cards/03-WC/Calv1N.spec.js
@@ -18,11 +18,13 @@ describe('Calv-1N', function() {
                 this.player1.reap(this.calv1n);
                 expect(this.player1.hand.length).toBe(2);
             });
+            
             it('should draw one card after figth', function() {
                 this.player1.fightWith(this.calv1n, this.lamindra);
                 expect(this.player1.hand.length).toBe(2);
             });
         });
+        
         describe('Calv-1N\'s upgrade ability', function() {
             beforeEach(function() {
                 this.setupTest({
@@ -41,11 +43,13 @@ describe('Calv-1N', function() {
                 this.player1.playUpgrade(this.calv1n, this.captainValJericho);
                 expect(this.calv1n.parent).toBe(this.captainValJericho);
             });
+            
             it('should draw one card after reap', function() {
                 this.player1.playUpgrade(this.calv1n, this.captainValJericho);
                 this.player1.reap(this.captainValJericho);
                 expect(this.player1.hand.length).toBe(2);
             });
+            
             it('should draw one card after figth', function() {
                 this.player1.playUpgrade(this.calv1n, this.captainValJericho);
                 this.player1.fightWith(this.captainValJericho, this.lamindra);
