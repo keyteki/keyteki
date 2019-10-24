4294fb24f633db122fe0ff772c627e04b92a08ef
Stuart Walsh <stu@ipng.org.uk>
2019-10-24 14:49:25 +0100

    Implement lesser oxtet

---
+++ server/game/cards/03-WC/LesserOxtet.js
@@ -0,0 +1,24 @@
+const Card = require('../../Card.js');
+
+class LesserOxtet extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            gameAction: ability.actions.purge(context => ({
+                target: context.player.hand
+            }))
+        });
+
+        this.reap({
+            effect: 'increase {1}\'s key cost by 3 until the end of their next turn',
+            effectArgs: context => context.player.opponent,
+            gameAction: ability.actions.lastingEffect({
+                targetController: 'opponent',
+                effect: ability.effects.modifyKeyCost(3)
+            })
+        });
+    }
+}
+
+LesserOxtet.id = 'lesser-oxtet';
+
+module.exports = LesserOxtet;
