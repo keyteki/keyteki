00c2834082ec8e4451b91181c4892c9140097125
romano-silva <romano.silva@hpe.com>
2019-10-25 20:35:11 -0300

    Fix RitualOfBalance condition Fixes #617

--- server/game/cards/01-Core/RitualOfBalance.js
+++ server/game/cards/01-Core/RitualOfBalance.js
@@ -3,8 +3,8 @@ const Card = require('../../Card.js');
 class RitualOfBalance extends Card {
     setupCardAbilities(ability) {
         this.action({
-            condition: context => context.player.opponent && context.player.opponent.amber >= 6,
-            gameAction: ability.actions.steal()
+            condition: context => !!context.player.opponent,
+            gameAction: ability.actions.steal(context => ({ amount : context.player.opponent.amber >= 6 ? 1 : 0}))
         });
     }
 }
