664b2a96d8cb590d3b5d07f0f17c319e69df66fd
Stuart Walsh <stu@ipng.org.uk>
2019-10-25 18:50:59 +0100

    Add test, implement properly

--- server/game/cards/03-WC/CodeMonkey.js
+++ server/game/cards/03-WC/CodeMonkey.js
@@ -4,21 +4,20 @@ class CodeMonkey extends Card {
     setupCardAbilities(ability) {
         this.play({
             effect: 'archive each neighboring creature and gain 2 amber for each house they share with {0}',
-            gameAction: ability.actions.archive(context => ({
-                target: context.source.neighbors
-            })),
-            then: {
-                gameAction: ability.actions.gainAmber(context => {
+            gameAction: [
+                ability.actions.archive(context => ({
+                    target: context.source.neighbors
+                })),
+                ability.actions.gainAmber(context => {
                     let card = context.source;
                     let houses = card.getHouses();
 
                     let neighborsSharingHouses = card.neighbors.filter(c => c.getHouses().some(h => houses.includes(h)));
                     return {
-                        target: card,
-                        amount: neighborsSharingHouses.length
+                        amount: neighborsSharingHouses.length === 2 ? 2 : 0
                     };
                 })
-            }
+            ]
         });
     }
 }
