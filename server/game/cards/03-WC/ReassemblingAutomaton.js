ff02e77353433a36f214cccdcd8af9e17b9ab76f
Stuart Walsh <stu@ipng.org.uk>
2019-10-25 19:53:49 +0100

    Implement reassembling automaton

---
+++ server/game/cards/03-WC/ReassemblingAutomaton.js
@@ -0,0 +1,24 @@
+const Card = require('../../Card.js');
+
+class ReassemblingAutomaton extends Card {
+    setupCardAbilities(ability) {
+        this.destroyed({
+            effect: 'heal all damage from {0}, exhaust it and move it to a flank',
+            effectArgs: () => this,
+            gameAction: [
+                ability.actions.heal({ fully: true }),
+                ability.actions.exhaust(),
+                ability.actions.moveToFlank(),
+                ability.actions.changeEvent(context => ({
+                    event: context.event,
+                    cancel: true,
+                    postHandler: context => context.source.moribund = false
+                }))
+            ]
+        });
+    }
+}
+
+ReassemblingAutomaton.id = 'reassembling-automaton';
+
+module.exports = ReassemblingAutomaton;
