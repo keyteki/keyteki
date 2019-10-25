36991fb8c459ba0c8f093cb2e8f1aeb2e740129d
romano-silva <romano.silva@hpe.com>
2019-10-25 20:41:10 -0300

    Fixes #617

--- test/server/cards/01-Core/RitualOfBalance.spec.js
+++ test/server/cards/01-Core/RitualOfBalance.spec.js
@@ -63,6 +63,30 @@ describe('Ritual of Balance', function() {
             beforeEach(function() {
                 this.setupTest({
                     player1: {
+                        house: 'shadows',
+                        hand: ['remote-access'],
+                        inPlay: ['nexus', 'gorm-of-omm', 'library-of-babble']
+                    },
+                    player2: {
+                        inPlay: ['gauntlet-of-command', 'ritual-of-balance']
+                    }
+                });
+            });
+
+            it('should be exhausted if used by an opponent even if the condition is not met', function() {
+                this.player1.reap(this.nexus);
+                expect(this.player1).toHavePrompt('Nexus');
+                expect(this.player1).toBeAbleToSelect(this.ritualOfBalance);
+                this.player1.clickCard(this.ritualOfBalance);
+                expect(this.ritualOfBalance.exhausted).toBe(true);
+                expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            });
+        });
+
+        describe('Ritual of Balance\'s ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    player1: {
                         house: 'logos',
                         hand: ['remote-access'],
                         inPlay: ['nexus', 'gorm-of-omm', 'library-of-babble']
