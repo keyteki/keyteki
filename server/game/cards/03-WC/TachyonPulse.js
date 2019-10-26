c92977170515025be232937be0a6e1e890818127
romano-silva <romano.silva@hpe.com>
2019-10-26 20:38:05 -0300

    Implemented Tachyon Pulse (Star Alliance)

--- server/game/cards/03-WC/TachyonPulse.js
+++ server/game/cards/03-WC/TachyonPulse.js
@@ -3,17 +3,13 @@ const Card = require('../../Card.js');
 class TachyonPulse extends Card {
     setupCardAbilities(ability) {
         this.play({
-            effect: 'destroy each artifact and exhaust each creature with an upgrade',
+            effectStyle: 'all',
             gameAction: [
                 ability.actions.destroy(context => ({
                     target: context.game.cardsInPlay.filter(card => card.type === 'artifact')
                 })),
-                ability.actions.gainAmber(context => ({
-                    amount: context.player.creaturesInPlay.filter(card => card.hasHouse('dis')).length
-                })),
-                ability.actions.gainAmber(context => ({
-                    target: context.player.opponent,
-                    amount: context.player.opponent ? context.player.opponent.creaturesInPlay.filter(card => card.hasHouse('dis')).length : 0
+                ability.actions.exhaust(context => ({
+                    target: context.game.creaturesInPlay.filter(card => card.upgrades.length > 0)
                 }))
             ]
         });
---
+++ test/server/cards/03-WC/TachyonPulse.spec.js
@@ -0,0 +1,38 @@
+fdescribe('Tachyon Pulse', function() {
+    integration(function() {
+        describe('Tachyon Pulse\'s ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    player1: {
+                        house: 'staralliance',
+                        inPlay: ['nexus', 'techivore-pulpate', 'uncharted-lands'],
+                        hand: ['tachyon-pulse', 'force-field', 'disruption-field', 'explo-rover']
+                    },
+                    player2: {
+                        inPlay: ['the-sting', 'lifeward', 'urchin', 'lamindra']
+                    }
+                });
+            });
+
+            it('destroy all artifacts and exhaust creatures with upgrade', function() {
+                this.player1.playUpgrade(this.forceField, this.nexus);
+                this.player1.playUpgrade(this.exploRover, this.nexus);
+                this.player1.playUpgrade(this.disruptionField, this.lamindra);
+                this.player1.play(this.tachyonPulse);
+                expect(this.lifeward.location).toBe('discard');
+                expect(this.theSting.location).toBe('discard');
+                expect(this.unchartedLands.location).toBe('discard');
+
+                expect(this.nexus.location).toBe('play area');
+                expect(this.techivorePulpate.location).toBe('play area');
+                expect(this.lamindra.location).toBe('play area');
+                expect(this.urchin.location).toBe('play area');
+
+                expect(this.nexus.exhausted).toBe(true);
+                expect(this.techivorePulpate.exhausted).toBe(false);
+                expect(this.lamindra.exhausted).toBe(true);
+                expect(this.urchin.exhausted).toBe(false);
+            });
+        });
+    });
+});
