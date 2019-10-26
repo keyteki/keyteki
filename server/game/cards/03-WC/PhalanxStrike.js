b9c3c8e8ba92bc92406f3ff97652ab38598d7dde
arthur <arthur@eligeo.com>
2019-10-25 23:47:22 -0600

    Added Phalanx Strike & Stomp

---
+++ server/game/cards/03-WC/PhalanxStrike.js
@@ -0,0 +1,36 @@
+const Card = require('../../Card.js');
+
+class PhalanxStrike extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            target: {
+                cardType: 'creature',
+                gameAction: ability.actions.dealDamage(context => ({ amount: context.player.creaturesInPlay.length }))
+            },
+            effect: 'deal {1} damage to {2}',
+            effectArgs: context => [context.player.creaturesInPlay.length,context.target],
+            then: {
+                target: {
+                    optional: true,
+                    cardType: 'creature',
+                    controller: 'self',
+                    gameAction: ability.actions.exalt()
+                },
+                message: '{0} exalts {2} to repeat Phalanx Strike\'s effect',
+                messageArgs: context => [context.target],
+                then: {
+                    target: {
+                        cardType: 'creature',
+                        gameAction: ability.actions.dealDamage(context => ({ amount: context.player.creaturesInPlay.length }))
+                    },
+                    message: '{0} uses {1} to deal {3} damage to {2}',
+                    messageArgs: context => [context.player.creaturesInPlay.length,context.target]
+                }
+            }
+        });
+    }
+}
+
+PhalanxStrike.id = 'phalanx-strike';
+
+module.exports = PhalanxStrike;
