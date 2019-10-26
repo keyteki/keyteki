d8ee64d5ed6314f93a74bdd406ebe0a5dc0ad872
romano-silva <romano.silva@hpe.com>
2019-10-26 20:43:04 -0300

    Fixed lint and added one more test

--- test/server/cards/03-WC/TachyonPulse.spec.js
+++ test/server/cards/03-WC/TachyonPulse.spec.js
@@ -1,4 +1,4 @@
-fdescribe('Tachyon Pulse', function() {
+describe('Tachyon Pulse', function() {
     integration(function() {
         describe('Tachyon Pulse\'s ability', function() {
             beforeEach(function() {
@@ -14,7 +14,7 @@ fdescribe('Tachyon Pulse', function() {
                 });
             });
 
-            it('destroy all artifacts and exhaust creatures with upgrade', function() {
+            it('should destroy all artifacts and exhaust creatures with upgrade', function() {
                 this.player1.playUpgrade(this.forceField, this.nexus);
                 this.player1.playUpgrade(this.exploRover, this.nexus);
                 this.player1.playUpgrade(this.disruptionField, this.lamindra);
@@ -22,6 +22,27 @@ fdescribe('Tachyon Pulse', function() {
                 expect(this.lifeward.location).toBe('discard');
                 expect(this.theSting.location).toBe('discard');
                 expect(this.unchartedLands.location).toBe('discard');
+
+                expect(this.nexus.location).toBe('play area');
+                expect(this.techivorePulpate.location).toBe('play area');
+                expect(this.lamindra.location).toBe('play area');
+                expect(this.urchin.location).toBe('play area');
+
+                expect(this.nexus.exhausted).toBe(true);
+                expect(this.techivorePulpate.exhausted).toBe(false);
+                expect(this.lamindra.exhausted).toBe(true);
+                expect(this.urchin.exhausted).toBe(false);
+            });
+
+            it('should exhaust creatures with upgrade even if there are no artifacts', function() {
+                this.player1.moveCard(this.unchartedLands, 'discard');
+                this.player1.moveCard(this.lifeward, 'discard');
+                this.player1.moveCard(this.unchartedLands, 'discard');
+
+                this.player1.playUpgrade(this.forceField, this.nexus);
+                this.player1.playUpgrade(this.exploRover, this.nexus);
+                this.player1.playUpgrade(this.disruptionField, this.lamindra);
+                this.player1.play(this.tachyonPulse);
 
                 expect(this.nexus.location).toBe('play area');
                 expect(this.techivorePulpate.location).toBe('play area');
