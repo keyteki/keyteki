ad6fb268d7078441c8c7582490ebaa10880b16de
Stuart Walsh <stu@ipng.org.uk>
2019-10-27 17:00:13 +0000

    Implement captain val jericho

--- server/game/Effects/StaticEffect.js
+++ server/game/Effects/StaticEffect.js
@@ -12,36 +12,6 @@ const binaryCardEffects = [
     'showTopConflictCard'
 ];
 
-const hasDash = {
-    modifyBaseMilitarySkill: card => card.hasDash('military'),
-    modifyBasePoliticalSkill: card => card.hasDash('political'),
-    modifyBothSkills: card => card.hasDash('military') && card.hasDash('political'),
-    modifyMilitarySkill: card => card.hasDash('military'),
-    modifyMilitarySkillMultiplier: card => card.hasDash('military'),
-    modifyPoliticalSkill: card => card.hasDash('political'),
-    modifyPoliticalSkillMultiplier: card => card.hasDash('political'),
-    setBaseMilitarySkill: card => card.hasDash('military'),
-    setBasePoliticalSkill: card => card.hasDash('political'),
-    setDash: (card, type) => card.hasDash(type),
-    setMilitarySkill: card => card.hasDash('military'),
-    setPoliticalSkill: card => card.hasDash('political')
-};
-
-const conflictingEffects = {
-    modifyBaseMilitarySkill: card => card.effects.filter(effect => effect.type === 'setBaseMilitarySkill'),
-    modifyBasePoliticalSkill: card => card.effects.filter(effect => effect.type === 'setBasePoliticalSkill'),
-    modifyMilitarySkill: card => card.effects.filter(effect => effect.type === 'setMilitarySkill'),
-    modifyMilitarySkillMultiplier: card => card.effects.filter(effect => effect.type === 'setMilitarySkill'),
-    modifyPoliticalSkill: card => card.effects.filter(effect => effect.type === 'setPoliticalSkill'),
-    modifyPoliticalSkillMultiplier: card => card.effects.filter(effect => effect.type === 'setPoliticalSkill'),
-    setBaseMilitarySkill: card => card.effects.filter(effect => effect.type === 'setMilitarySkill'),
-    setBasePoliticalSkill: card => card.effects.filter(effect => effect.type === 'setPoliticalSkill'),
-    setMaxConflicts: (player, value) =>
-        player.mostRecentEffect('setMaxConflicts') === value ? [_.last(player.effects.filter(effect => effect.type === 'setMaxConflicts'))] : [],
-    takeControl: (card, player) =>
-        card.mostRecentEffect('takeControl') === player ? [_.last(card.effects.filter(effect => effect.type === 'takeControl'))] : []
-};
-
 class StaticEffect {
     constructor(type = '', value = true) {
         this.type = type;
@@ -74,10 +44,6 @@ class StaticEffect {
     }
 
     canBeApplied(target) {
-        if(hasDash[this.type] && hasDash[this.type](target, this.value)) {
-            return false;
-        }
-
         return this.checkConflictingEffects(this.type, target);
     }
 
@@ -85,15 +51,6 @@ class StaticEffect {
         if(binaryCardEffects.includes(type)) {
             let matchingEffects = target.effects.filter(effect => effect.type === type);
             return matchingEffects.every(effect => this.hasLongerDuration(effect) || effect.isConditional);
-        }
-
-        if(conflictingEffects[type]) {
-            let matchingEffects = conflictingEffects[type](target, this.value);
-            return matchingEffects.every(effect => this.hasLongerDuration(effect) || effect.isConditional);
-        }
-
-        if(type === 'modifyBothSkills') {
-            return this.checkConflictingEffects('modifyMilitarySkill', target) || this.checkConflictingEffects('modifyPoliticalSkill', target);
         }
 
         return true;
