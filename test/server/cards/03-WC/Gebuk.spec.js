c576d3c467fb64bdacf8f77469373f231bd26178
Stuart Walsh <stu@ipng.org.uk>
2019-10-28 13:15:50 +0000

    Implement gebuk (#642)
    * Implement Gebuk
    This will break if the player has no cards in their deck when gebuk is destroyed
    
    * Changes based on feedback
    
    * Fix test focus
    
    * Fix linting
    
    * More linting fixes

---
+++ test/server/cards/03-WC/Gebuk.spec.js
@@ -0,0 +1,46 @@
+describe('Gebuk', function() {
+    integration(function() {
+        describe('when destroyed', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    player1: {
+                        house: 'untamed',
+                        inPlay: ['bad-penny', 'gebuk', 'umbra'],
+                        hand: ['regrowth', 'niffle-ape']
+                    },
+                    player2: {
+                        inPlay: ['valdr', 'troll', 'nexus']
+                    }
+                });
+            });
+
+            describe('and the top card of the deck is not a creature', function() {
+                beforeEach(function() {
+                    this.player1.moveCard(this.regrowth, 'deck');
+
+                    this.player1.fightWith(this.gebuk, this.troll);
+                });
+
+                it('should discard the top card of the deck', function() {
+                    expect(this.player1.discard).toContain(this.regrowth);
+                });
+
+                it('should not put anything into the battle line', function() {
+                    expect(this.player1.inPlay.length).toBe(2);
+                });
+            });
+
+            describe('and the top card of the deck is a creature', function() {
+                beforeEach(function() {
+                    this.player1.moveCard(this.niffleApe, 'deck');
+
+                    this.player1.fightWith(this.gebuk, this.troll);
+                });
+
+                it('should put that creature into play in the same position as gebuk was', function() {
+                    expect(this.player1.inPlay[1]).toBe(this.niffleApe);
+                });
+            });
+        });
+    });
+});
