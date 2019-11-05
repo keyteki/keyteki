07c017f3c353a21a10244ca02250469e5dab9782
Romano Silva <romano.magacho@gmail.com>
2019-11-05 00:26:05 -0700

    Fixes WC Harmonia not granting amber when played herself (#689)
    * Fixes WC Harmonia not granting amber when played herself
    Fixes #675
    
    * Update Harmonia.spec.js

--- server/game/cards/03-WC/Harmonia.js
+++ server/game/cards/03-WC/Harmonia.js
@@ -4,7 +4,7 @@ class Harmonia extends Card {
     setupCardAbilities(ability) {
         this.constantReaction({
             when: {
-                onCardPlayed: (event, context) => event.card.type === 'creature' && event.player === context.player && event.card !== context.source
+                onCardPlayed: (event, context) => event.card.type === 'creature' && event.player === context.player
             },
             gameAction: ability.actions.gainAmber(context => ({
                 amount: context.player.creaturesInPlay.length < context.player.opponent.creaturesInPlay.length ? 1 : 0
