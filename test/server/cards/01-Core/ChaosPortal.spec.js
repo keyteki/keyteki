ff3c68c3f1e52dd5327f2e1f01ce430d5bb7976a
jtrussell <jus.russell@gmail.com>
2019-11-05 23:13:33 -0500

    Add tests for Chaos Portal
    Includes tests for basic functionality and interaction with Omega cards.
    
    See #690

---
+++ test/server/cards/01-Core/ChaosPortal.spec.js
@@ -0,0 +1,48 @@
+describe('Chaos Portal', function() {
+    integration(function() {
+        describe('Chaos Portal\'s Ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    player1: {
+                        house: 'logos',
+                        inPlay: ['chaos-portal'],
+                        discard: ['duskwitch', 'bad-penny']
+                    }
+                });
+            });
+
+            it('should prompt to choose a house', function() {
+                this.player1.useAction(this.chaosPortal);
+                expect(this.player1).toHavePrompt('Chaos Portal');
+                expect(this.player1.currentButtons).toContain('logos');
+                expect(this.player1.currentButtons).toContain('shadows');
+                expect(this.player1.currentButtons).toContain('untamed');
+            });
+
+            it('should play the card if it is of the chosen house', function() {
+                this.player1.moveCard(this.badPenny, 'deck');
+                expect(this.badPenny.location).toBe('deck');
+                this.player1.useAction(this.chaosPortal);
+                this.player1.clickPrompt('shadows');
+                expect(this.badPenny.location).toBe('play area');
+            });
+
+            it('should not play the card if it is of a different house', function() {
+                this.player1.moveCard(this.badPenny, 'deck');
+                expect(this.badPenny.location).toBe('deck');
+                this.player1.useAction(this.chaosPortal);
+                this.player1.clickPrompt('untamed');
+                expect(this.badPenny.location).toBe('deck');
+            });
+
+            it('should end the turn when an omega card is played', function() {
+                this.player1.moveCard(this.duskwitch, 'deck');
+                expect(this.duskwitch.location).toBe('deck');
+                this.player1.useAction(this.chaosPortal);
+                this.player1.clickPrompt('untamed');
+                expect(this.duskwitch.location).toBe('play area');
+                expect(this.player2).toHavePrompt('House Choice');
+            });
+        });
+    });
+});
