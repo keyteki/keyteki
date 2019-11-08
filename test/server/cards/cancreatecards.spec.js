ed8b3daa8ec2c39348b4e7a43b6564ad0796850c
romano-silva <romano.silva@hpe.com>
2019-11-08 12:47:29 -0700

    Fixed test error

--- test/server/cards/cancreatecards.spec.js
+++ test/server/cards/cancreatecards.spec.js
@@ -16,6 +16,7 @@ const mockContext = {
     game: {
         cardsUsed: [],
         cardsPlayed: [],
+        creaturesInPlay: [],
         activePlayer: player,
         getFrameworkContext: () => {}
     },
