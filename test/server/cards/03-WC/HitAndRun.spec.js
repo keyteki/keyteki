b3b5e341325ed16b4015037d05f0405828f32d8a
skyjedi <50d2037077b7240576851289a37ce14e5c911642>
2019-11-08 11:43:24 -0600

    https://github.com/keyteki/keyteki/issues/713

--- test/server/cards/03-WC/HitAndRun.spec.js
+++ test/server/cards/03-WC/HitAndRun.spec.js
@@ -26,7 +26,6 @@ describe('Hit and Run', function() {
 
                 this.player1.clickCard(this.flaxia);
 
-                expect(this.flaxia.tokens.damage).toBe(2);
 
                 expect(this.player1).toHavePrompt('Hit and Run');
                 expect(this.player1).toBeAbleToSelect(this.flaxia);
@@ -39,6 +38,7 @@ describe('Hit and Run', function() {
                 expect(this.flaxia.location).toBe('play area');
                 expect(this.lamindra.location).toBe('play area');
                 expect(this.troll.location).toBe('play area');
+                expect(this.flaxia.tokens.damage).toBe(2);
             });
 
             it('Hit an enemy creature and return a friendly creature to hand', function() {
@@ -52,7 +52,31 @@ describe('Hit and Run', function() {
 
                 this.player1.clickCard(this.troll);
 
+                expect(this.player1).toHavePrompt('Hit and Run');
+                expect(this.player1).toBeAbleToSelect(this.flaxia);
+                expect(this.player1).toBeAbleToSelect(this.knoxx);
+                expect(this.player1).not.toBeAbleToSelect(this.lamindra);
+                expect(this.player1).not.toBeAbleToSelect(this.troll);
+
+                this.player1.clickCard(this.knoxx);
+                expect(this.knoxx.location).toBe('hand');
+                expect(this.flaxia.location).toBe('play area');
+                expect(this.lamindra.location).toBe('play area');
+                expect(this.troll.location).toBe('play area');
                 expect(this.troll.tokens.damage).toBe(2);
+            });
+            it('Hit an enemy creature warded and return a friendly creature to hand', function() {
+                this.troll.tokens.ward = 1;
+                expect(this.troll.warded).toBe(true);
+                this.player1.play(this.hitAndRun);
+
+                expect(this.player1).toHavePrompt('Hit and Run');
+                expect(this.player1).toBeAbleToSelect(this.flaxia);
+                expect(this.player1).toBeAbleToSelect(this.knoxx);
+                expect(this.player1).toBeAbleToSelect(this.lamindra);
+                expect(this.player1).toBeAbleToSelect(this.troll);
+
+                this.player1.clickCard(this.troll);
 
                 expect(this.player1).toHavePrompt('Hit and Run');
                 expect(this.player1).toBeAbleToSelect(this.flaxia);
@@ -65,6 +89,8 @@ describe('Hit and Run', function() {
                 expect(this.flaxia.location).toBe('play area');
                 expect(this.lamindra.location).toBe('play area');
                 expect(this.troll.location).toBe('play area');
+                expect(this.troll.hasToken('damage')).toBe(false);
+                expect(this.troll.hasToken('ward')).toBe(false);
             });
         });
     });
