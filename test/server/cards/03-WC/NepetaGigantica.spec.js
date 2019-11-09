efa1eabf0124e4a4b537c245333c45399a8a486c
Romano Silva <romano.magacho@gmail.com>
2019-11-09 12:37:06 -0700

    Fix "Cannot read property 'choice' of undefined" on NepetaGigantica. (#786)
    Simplified the card and remove unused locales

--- test/server/cards/03-WC/NepetaGigantica.spec.js
+++ test/server/cards/03-WC/NepetaGigantica.spec.js
@@ -5,7 +5,7 @@ describe('Nepeta Gigantica', function() {
                 this.setupTest({
                     player1: {
                         house: 'untamed',
-                        inPlay: ['nepeta-gigantica', 'tantadlin', 'dextre', 'moor-wolf']
+                        inPlay: ['nepeta-gigantica', 'tantadlin', 'dextre', 'moor-wolf', 'brammo']
                     },
                     player2: {
                         inPlay: ['drummernaut']
@@ -15,34 +15,13 @@ describe('Nepeta Gigantica', function() {
 
             it('should give the option to choose between stunning P>5 or Giant', function() {
                 this.player1.useAction(this.nepetaGigantica);
-                expect(this.player1).toHavePromptButton('5 or higher power creature');
-                expect(this.player1).toHavePromptButton('Giant creature');
-            });
-
-            it('should give the appropriate choices when choosing P>5', function() {
-                this.player1.useAction(this.nepetaGigantica);
-                expect(this.player1).toHavePromptButton('5 or higher power creature');
-                expect(this.player1).toHavePromptButton('Giant creature');
-                this.player1.clickPrompt('5 or higher power creature');
                 expect(this.player1).toBeAbleToSelect(this.tantadlin);
                 expect(this.player1).toBeAbleToSelect(this.drummernaut);
+                expect(this.player1).toBeAbleToSelect(this.brammo);
                 expect(this.player1).not.toBeAbleToSelect(this.moorWolf);
                 expect(this.player1).not.toBeAbleToSelect(this.dextre);
                 this.player1.clickCard(this.tantadlin);
                 expect(this.tantadlin.stunned).toBe(true);
-            });
-
-            it('should give the appropriate choices when choosing Giant', function() {
-                this.player1.useAction(this.nepetaGigantica);
-                expect(this.player1).toHavePromptButton('5 or higher power creature');
-                expect(this.player1).toHavePromptButton('Giant creature');
-                this.player1.clickPrompt('Giant creature');
-                expect(this.player1).not.toBeAbleToSelect(this.tantadlin);
-                expect(this.player1).toBeAbleToSelect(this.drummernaut);
-                expect(this.player1).not.toBeAbleToSelect(this.moorWolf);
-                expect(this.player1).not.toBeAbleToSelect(this.dextre);
-                this.player1.clickCard(this.drummernaut);
-                expect(this.drummernaut.stunned).toBe(true);
             });
         });
     });
