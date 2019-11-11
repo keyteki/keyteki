a603544e274c6306449eb33348e57ddf5cbe607a
Romano Silva <romano.magacho@gmail.com>
2019-11-11 00:48:38 -0700

    Fixes GeneralOrder24 not destroying itself (#842)
    * Fixes GeneralOrder24 not destroying itself
    Fixes #724
    
    * REMOVED CONDITION

--- test/server/cards/03-WC/GeneralOrder24.spec.js
+++ test/server/cards/03-WC/GeneralOrder24.spec.js
@@ -17,18 +17,23 @@ describe('General Order 24', function() {
             });
 
             it('general order 24 is activated when owner has no creatures, it gets destroyed', function() {
+                this.player2.moveCard(this.badPenny, 'play area');
+                this.player1.play(this.generalOrder24, 'play area');
                 this.player1.endTurn();
-                this.player1.moveCard(this.generalOrder24, 'play area');
+                this.player2.clickCard(this.badPenny);
                 this.player2.clickPrompt('shadows');
-                expect(this.player1.inPlay.length).toBe(1);
+                this.player2.play(this.badPenny);
                 this.player2.endTurn();
-                expect(this.player1.inPlay.length).toBe(0);
+                this.player1.clickPrompt('staralliance');
+                expect(this.generalOrder24.location).toBe('discard');
             });
 
             it('general order 24 is activated when opponent has no creatures, it gets destroyed', function() {
+                this.player1.moveCard(this.navigatorAli, 'play area');
                 this.player1.play(this.generalOrder24);
                 this.player1.endTurn();
-                expect(this.player1.inPlay.length).toBe(0);
+                this.player2.clickPrompt('shadows');
+                expect(this.generalOrder24.location).toBe('discard');
             });
 
             it('general order 24 is played when opponent has creatures, it forces the opponent to choose a creature', function() {
@@ -54,14 +59,15 @@ describe('General Order 24', function() {
                 expect(this.player2.inPlay).toContain(this.eyegor);
             });
 
-            it('general order 24 is played when opponent has creatures, it destroys the creature the opponent chooses and other cards in the house', function() {
+            it('general order 24 is played when owner has creatures, it destroys the creature the owner chooses and other cards in the house', function() {
                 this.player2.moveCard(this.badPenny, 'play area');
                 this.player2.moveCard(this.umbra, 'play area');
                 this.player2.moveCard(this.eyegor, 'play area');
-                this.player1.play(this.captainValJericho);
+                this.player1.moveCard(this.captainValJericho, 'play area');
+                this.player1.moveCard(this.navigatorAli, 'play area');
                 this.player1.play(this.generalOrder24);
                 this.player1.endTurn();
-                expect(this.player1.inPlay.length).toBe(2);
+                expect(this.player1.inPlay.length).toBe(3);
                 expect(this.player2).toHavePrompt('Choose a creature to destroy');
                 this.player2.clickCard(this.badPenny);
                 expect(this.player2.inPlay.length).toBe(1);
