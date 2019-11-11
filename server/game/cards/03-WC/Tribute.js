655aae7cbcab9bbe07fc223260da402e4b4a2047
Mateusz Borycki <mateuszborycki@gmail.com>
2019-11-11 01:49:40 +0100

    Changed tribute to allow exalting even if no amber was captured.

--- server/game/cards/03-WC/Tribute.js
+++ server/game/cards/03-WC/Tribute.js
@@ -2,27 +2,33 @@ const Card = require('../../Card.js');
 
 class Tribute extends Card {
     setupCardAbilities(ability) {
+        let askForExalt = context => ({
+            condition: () => !context.secondResolution,
+            alwaysTriggers: true,
+            may: 'exhalt the creature',
+            gameAction: ability.actions.exalt({ target: context.target }),
+            then: {
+                gameAction: ability.actions.resolveAbility({
+                    ability: context.ability,
+                    secondResolution: true
+                }),
+                message: '{0} exalts {3} to repeat the effect of {1}',
+                messageArgs: context.target
+            }
+        });
+
         this.play({
             target: {
                 mode: 'mostStat',
                 cardType: 'creature',
                 controller: 'self',
                 numCards: 1,
-                cardStat: card => card.power,
-                gameAction: ability.actions.capture({ amount: 2 })
+                cardStat: card => card.power
             },
             then: context => ({
-                condition: () => !context.secondResolution,
-                may: 'exhalt the creature',
-                gameAction: ability.actions.exalt({ target: context.target }),
-                then: {
-                    gameAction: ability.actions.resolveAbility({
-                        ability: context.ability,
-                        secondResolution: true
-                    }),
-                    message: '{0} exalts {3} to repeat the effect of {1}',
-                    messageArgs: context.target
-                }
+                alwaysTriggers: true,
+                gameAction: ability.actions.capture({ target: context.target, amount: 2 }),
+                then: askForExalt(context)
             })
         });
     }
