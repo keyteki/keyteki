276acd31864ca4804dbe44cbe3bc4f8dd0e6f644
Romano Silva <romano.magacho@gmail.com>
2019-11-13 10:53:31 -0700

    Fixed Gebuk being replaced by Artifact (#895)

--- test/server/cards/03-WC/Gebuk.spec.js
+++ test/server/cards/03-WC/Gebuk.spec.js
@@ -6,7 +6,7 @@ describe('Gebuk', function() {
                     player1: {
                         house: 'untamed',
                         inPlay: ['bad-penny', 'gebuk', 'umbra'],
-                        hand: ['regrowth', 'niffle-ape']
+                        hand: ['regrowth', 'niffle-ape', 'bear-flute']
                     },
                     player2: {
                         inPlay: ['valdr', 'troll', 'nexus']
@@ -14,7 +14,7 @@ describe('Gebuk', function() {
                 });
             });
 
-            describe('and the top card of the deck is not a creature', function() {
+            describe('and the top card of the deck is an action', function() {
                 beforeEach(function() {
                     this.player1.moveCard(this.regrowth, 'deck');
 
@@ -30,6 +30,22 @@ describe('Gebuk', function() {
                 });
             });
 
+            describe('and the top card of the deck is an artifact', function() {
+                beforeEach(function() {
+                    this.player1.moveCard(this.bearFlute, 'deck');
+
+                    this.player1.fightWith(this.gebuk, this.troll);
+                });
+
+                it('should discard the top card of the deck', function() {
+                    expect(this.player1.discard).toContain(this.bearFlute);
+                });
+
+                it('should not put anything into the battle line', function() {
+                    expect(this.player1.inPlay.length).toBe(2);
+                });
+            });
+
             describe('and the top card of the deck is a creature', function() {
                 beforeEach(function() {
                     this.player1.moveCard(this.niffleApe, 'deck');
@@ -38,6 +54,7 @@ describe('Gebuk', function() {
                 });
 
                 it('should put that creature into play in the same position as gebuk was', function() {
+                    expect(this.player1.inPlay.length).toBe(3);
                     expect(this.player1.inPlay[1]).toBe(this.niffleApe);
                 });
             });
