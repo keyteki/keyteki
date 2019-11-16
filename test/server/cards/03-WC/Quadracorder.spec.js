bd2650d3a63d61af3139a145a7653e55e5ce51ed
skyjedi <50d2037077b7240576851289a37ce14e5c911642>
2019-11-15 21:26:36 -0600

    add quadacorder test to try an replicate https://github.com/keyteki/keyteki/issues/875
    unable to replicate

--- test/server/cards/03-WC/Quadracorder.spec.js
+++ test/server/cards/03-WC/Quadracorder.spec.js
@@ -6,12 +6,13 @@ describe('Quadracorder', function() {
                     player1: {
                         amber: 6,
                         house: 'staralliance',
-                        hand: ['quadracorder'],
+                        hand: ['quadracorder', 'mother', 'rustgnawer'],
                         inPlay: ['lieutenant-khrkhar']
                     },
                     player2: {
                         amber: 6,
-                        hand: ['remote-access']
+                        hand: ['remote-access'],
+                        inPlay: ['bad-penny']
                     }
                 });
             });
@@ -22,51 +23,53 @@ describe('Quadracorder', function() {
                 expect(this.player2.player.getForgedKeys()).toBe(0);
                 expect(this.player2.player.amber).toBe(6);
             });
-        });
-        describe('Quadracorder\'s ability', function() {
-            beforeEach(function() {
-                this.setupTest({
-                    player1: {
-                        amber: 6,
-                        house: 'staralliance',
-                        hand: ['quadracorder'],
-                        inPlay: ['lieutenant-khrkhar','mother']
-                    },
-                    player2: {
-                        amber: 7,
-                        hand: ['remote-access']
-                    }
-                });
-            });
 
             it('should increase key cost by [2] when it is attached to a creature', function() {
+                this.player1.moveCard(this.mother, 'play area');
+                this.player2.amber = 7;
                 this.player1.playUpgrade(this.quadracorder, this.lieutenantKhrkhar);
                 this.player1.endTurn();
                 expect(this.player2.player.getForgedKeys()).toBe(0);
                 expect(this.player2.player.amber).toBe(7);
             });
-        });
-        describe('Quadracorder\'s ability', function() {
-            beforeEach(function() {
-                this.setupTest({
-                    player1: {
-                        amber: 6,
-                        house: 'staralliance',
-                        hand: ['quadracorder'],
-                        inPlay: ['lieutenant-khrkhar', 'mother', 'rustgnawer']
-                    },
-                    player2: {
-                        amber: 8,
-                        hand: ['remote-access']
-                    }
-                });
-            });
 
             it('should increase key cost by [3] when it is attached to a creature', function() {
+                this.player1.moveCard(this.mother, 'play area');
+                this.player1.moveCard(this.rustgnawer, 'play area');
+                this.player2.amber = 8;
                 this.player1.playUpgrade(this.quadracorder, this.lieutenantKhrkhar);
                 this.player1.endTurn();
                 expect(this.player2.player.getForgedKeys()).toBe(0);
                 expect(this.player2.player.amber).toBe(8);
+            });
+
+            it('should increase key cost by [3] when it is attached to an enemy creature', function() {
+                this.player1.moveCard(this.mother, 'play area');
+                this.player1.moveCard(this.rustgnawer, 'play area');
+                this.player2.moveCard(this.badPenny, 'play area');
+                this.player2.amber = 8;
+                this.player1.playUpgrade(this.quadracorder, this.badPenny);
+                this.player1.endTurn();
+                expect(this.player2.player.getForgedKeys()).toBe(0);
+                expect(this.player2.player.amber).toBe(8);
+            });
+            it('should increase key cost by [3] when it is attached to an enemy creature, but not to controller', function() {
+                this.player1.moveCard(this.mother, 'play area');
+                this.player1.moveCard(this.rustgnawer, 'play area');
+                this.player2.moveCard(this.badPenny, 'play area');
+                this.player1.amber = 6;
+                this.player2.amber = 8;
+                this.player1.playUpgrade(this.quadracorder, this.badPenny);
+                this.player1.endTurn();
+                expect(this.player2.player.getForgedKeys()).toBe(0);
+                expect(this.player2.player.amber).toBe(8);
+                this.player2.clickPrompt('shadows');
+                this.player2.endTurn();
+                this.player1.forgeKey('Red');
+                expect(this.player1.player.keys.red).toBe(true);
+                expect(this.player1.player.keys.blue).toBe(false);
+                expect(this.player1.player.keys.yellow).toBe(false);
+                expect(this.player1.amber).toBe(0);
             });
         });
     });
