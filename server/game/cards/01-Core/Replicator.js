33af5243e91ab3fe3163928f80c4c5174cad41dc
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2019-11-20 07:27:22 +0000

    Bugfix for Replicator (#950)

--- server/game/cards/01-Core/Replicator.js
+++ server/game/cards/01-Core/Replicator.js
@@ -6,13 +6,9 @@ class Replicator extends Card {
             target: {
                 cardType: 'creature',
                 cardCondition: (card, context) =>
-                    card !== context.source && card.abilities.reactions.some(ability =>
-                        Object.keys(ability.when).some(key => key === 'onReap')
-                    ),
+                    card !== context.source && card.abilities.reactions.some(ability => ability.properties.reap),
                 gameAction: ability.actions.resolveAbility(context => ({
-                    ability: context.target.abilities.reactions.find(ability =>
-                        Object.keys(ability.when).some(key => key === 'onReap')
-                    )
+                    ability: context.target.abilities.reactions.find(ability => ability.properties.reap)
                 }))
             },
             // TODO: add a menu to choose which ability
