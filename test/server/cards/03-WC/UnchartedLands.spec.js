df7e91e71ec5042ff346029442aec9042c336757
romano-silva <romano.silva@hpe.com>
2019-12-05 09:57:54 -0300

    Should allow only if has amber > 0 Fixes #1009

--- test/server/cards/03-WC/UnchartedLands.spec.js
+++ test/server/cards/03-WC/UnchartedLands.spec.js
@@ -5,50 +5,69 @@ describe('Uncharted Lands', function() {
                 this.setupTest({
                     player1: {
                         house: 'staralliance',
-                        inPlay: ['lieutenant-khrkhar', 'mother'],
+                        inPlay: ['lieutenant-khrkhar', 'explo-rover', 'mother'],
                         hand: ['uncharted-lands']
                     },
                     player2: {
-                        inPlay: ['urchin','crash-muldoon'],
+                        inPlay: ['urchin', 'crash-muldoon'],
                         amber: 3
                     }
                 });
             });
+
             it('should put 6A on itself when played.', function() {
                 this.player1.play(this.unchartedLands);
-                expect(this.unchartedLands.tokens.amber).toBe(6);
+                expect(this.unchartedLands.amber).toBe(6);
             });
-            it('should transfer one from itself to the players pool when a gsa creature reaps.', function() {
+
+            it('should transfer one from itself to the players pool when an SA creature reaps.', function() {
                 this.player1.play(this.unchartedLands);
-                expect(this.unchartedLands.tokens.amber).toBe(6);
+                expect(this.unchartedLands.amber).toBe(6);
                 this.player1.reap(this.lieutenantKhrkhar);
                 expect(this.player1.amber).toBe(2);
-                expect(this.unchartedLands.tokens.amber).toBe(5);
+                expect(this.unchartedLands.amber).toBe(5);
             });
-            it('should NOT transfer one from itself to the players pool when a non-gsa creature reaps.', function() {
+
+            it('should transfer a maximum of 6 when an SA creature reaps.', function() {
                 this.player1.play(this.unchartedLands);
-                expect(this.unchartedLands.tokens.amber).toBe(6);
+                expect(this.unchartedLands.amber).toBe(6);
+                for(let i = 0; i < 6; ++i) {
+                    this.player1.reap(this.lieutenantKhrkhar);
+                    expect(this.player1.amber).toBe((i + 1) * 2);
+                    this.lieutenantKhrkhar.exhausted = false;
+                    expect(this.unchartedLands.amber).toBe(5 - i);
+                }
+
+                this.player1.reap(this.exploRover);
+                expect(this.player1.amber).toBe(13);
+                expect(this.unchartedLands.amber).toBe(0);
+            });
+
+            it('should not transfer one from itself to the players pool when a non-SA creature reaps.', function() {
+                this.player1.play(this.unchartedLands);
+                expect(this.unchartedLands.amber).toBe(6);
                 this.player1.reap(this.lieutenantKhrkhar);
                 expect(this.player1.amber).toBe(2);
-                expect(this.unchartedLands.tokens.amber).toBe(5);
+                expect(this.unchartedLands.amber).toBe(5);
                 this.player1.endTurn();
                 this.player2.clickPrompt('shadows');
                 this.player2.reap(this.urchin);
-                expect(this.unchartedLands.tokens.amber).toBe(5);
+                expect(this.unchartedLands.amber).toBe(5);
                 expect(this.player2.amber).toBe(4);
             });
-            it('should transfer one from itself to the opponents pool when an opposing gsa creature reaps.', function() {
+
+            it('should transfer one from itself to the opponents pool when an opposing SA creature reaps.', function() {
                 this.player1.play(this.unchartedLands);
-                expect(this.unchartedLands.tokens.amber).toBe(6);
+                expect(this.unchartedLands.amber).toBe(6);
                 this.player1.reap(this.lieutenantKhrkhar);
                 expect(this.player1.amber).toBe(2);
-                expect(this.unchartedLands.tokens.amber).toBe(5);
+                expect(this.unchartedLands.amber).toBe(5);
                 this.player1.endTurn();
                 this.player2.clickPrompt('staralliance');
                 this.player2.reap(this.crashMuldoon);
                 expect(this.player2.amber).toBe(5);
                 expect(this.player1.amber).toBe(2);
-                expect(this.unchartedLands.tokens.amber).toBe(4);
+                expect(this.unchartedLands.amber).toBe(4);
             });
         });
     });
