55b9a252676e506e47598053194b719f9001c1ca
LifeIsPizza <pizzamarinara@protonmail.com>
2019-12-26 13:50:51 +0100

    Fix for City State Interest

--- server/game/cards/03-WC/CityStateInterest.js
+++ server/game/cards/03-WC/CityStateInterest.js
@@ -3,10 +3,21 @@ const Card = require('../../Card.js');
 class CityStateInterest extends Card {
     setupCardAbilities(ability) {
         this.play({
-            gameAction: ability.actions.capture(context => ({
-                amount: 1,
-                target: context.player.creaturesInPlay
-            })),
+            gameAction: ability.actions.capture(context => {
+                if(context.player.opponent.amber >= context.player.creaturesInPlay.length) {
+                    return { target: context.player.creaturesInPlay };
+                }
+
+                return {
+                    promptForSelect: {
+                        activePromptTitle: 'Choose a creature to capture 1 amber',
+                        cardType: 'creature',
+                        controller: 'self',
+                        mode: 'exactly',
+                        numCards: context.player.opponent.amber
+                    }
+                };
+            }),
             effect: 'make each friendly creature capture 1 amber'
         });
     }
--- test/server/cards/03-WC/CityStateInterest.spec.js
+++ test/server/cards/03-WC/CityStateInterest.spec.js
@@ -24,5 +24,37 @@ describe('City-State Interest', function() {
                 expect(this.lamindra.hasToken('amber')).toBe(false);
             });
         });
+        describe('City-State Interest\'s play ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    player1: {
+                        house: 'saurian',
+                        inPlay: ['troll', 'valdr', 'krump'],
+                        hand: ['city-state-interest']
+                    },
+                    player2: {
+                        amber: 2,
+                        inPlay: ['lamindra']
+                    }
+                });
+            });
+
+            it('will let player choose which creatures capture if there are more creatures than opponent aember', function() {
+                this.player1.play(this.cityStateInterest);
+                expect(this.player1).toHavePrompt('City-State Interest');
+                expect(this.player1).not.toBeAbleToSelect(this.lamindra);
+                expect(this.player1).toBeAbleToSelect(this.troll);
+                expect(this.player1).toBeAbleToSelect(this.valdr);
+                expect(this.player1).toBeAbleToSelect(this.krump);
+                this.player1.clickCard(this.troll);
+                this.player1.clickCard(this.valdr);
+                this.player1.clickPrompt('Done');
+                expect(this.player2.player.amber).toBe(0);
+                expect(this.troll.tokens.amber).toBe(1);
+                expect(this.valdr.tokens.amber).toBe(1);
+                expect(this.krump.hasToken('amber')).toBe(false);
+                expect(this.lamindra.hasToken('amber')).toBe(false);
+            });
+        });
     });
 });
