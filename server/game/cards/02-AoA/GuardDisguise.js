2b3c99710658ea297e8c518dd9712ca6d6a9cf29
E <5575513+LifeIsPizza@users.noreply.github.com>
2019-12-31 13:18:15 +0100

    Fix to Guard Disguise (#1058)
    * Fix to Guard Disguise
    
    * Silly test
    
    * Silly test part 2

--- server/game/cards/02-AoA/GuardDisguise.js
+++ server/game/cards/02-AoA/GuardDisguise.js
@@ -3,9 +3,9 @@ const Card = require('../../Card.js');
 class GuardDisguise extends Card {
     setupCardAbilities(ability) {
         this.action({
-            condition: context => context.player.opponent && context.player.opponent.amber <= 3,
+            condition: context => context.player.opponent,
             gameAction: [
-                ability.actions.steal({ amount: 3 }),
+                ability.actions.steal(context => ({ amount: context.player.opponent && context.player.opponent.amber <= 3 ? 3 : 0 })),
                 ability.actions.sacrifice()
             ]
         });
