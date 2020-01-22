fd039f23cc656d503e50d6635d22bfb1858f15a7
jtrussell <jus.russell@gmail.com>
2020-01-21 22:12:10 -0500

    Remove fdescribe :facepalm:

--- test/server/cards/01-Core/NumquidTheFair.spec.js
+++ test/server/cards/01-Core/NumquidTheFair.spec.js
@@ -1,4 +1,4 @@
-fdescribe('Numquid the Fair', function() {
+describe('Numquid the Fair', function() {
     integration(function() {
         describe('Numquid the Fair\'s ability', function() {
             beforeEach(function() {
@@ -14,7 +14,7 @@ fdescribe('Numquid the Fair', function() {
             });
 
             it('should destroy enemy creatures until both players have the same number of creatures', function() {
-                this.player1.play(this.numquidTheFair)
+                this.player1.play(this.numquidTheFair);
                 expect(this.player1).toHavePrompt('Numquid the Fair');
                 expect(this.player1).toBeAbleToSelect(this.urchin);
                 expect(this.player1).toBeAbleToSelect(this.silvertooth);
@@ -30,8 +30,8 @@ fdescribe('Numquid the Fair', function() {
             });
 
             it('continue to destroy through warded creatures', function() {
-                this.urchin.ward()
-                this.player1.play(this.numquidTheFair)
+                this.urchin.ward();
+                this.player1.play(this.numquidTheFair);
                 this.player1.clickCard(this.urchin);
                 expect(this.player1).toBeAbleToSelect(this.urchin);
                 this.player1.clickCard(this.urchin);
