7836bcb0b51b4f1506065f7b486953e554e79b71
zraul <zavaczki.raul@gmail.com>
2020-03-15 23:48:00 +0200

    added deckbuilder class

---
+++ client/pages/Deckbuilder.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class GameBoard extends React.Component {
+render() {
+    return ( 
+        <Panel title={ 'Deckbuilder' }>
+ 
+        </Panel>
+        ); 
+    }
+}
