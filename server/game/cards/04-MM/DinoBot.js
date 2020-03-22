892fa98e1c3a02206ad07a8cb25b250cdcfe5d71
skyjedi <50d2037077b7240576851289a37ce14e5c911642>
2020-03-22 13:46:54 -0500

    implementing Dino-Bot, Dæmo-Bot, Lyco-Bot, and Unbra-Bot

---
+++ server/game/cards/04-MM/DinoBot.js
@@ -0,0 +1,33 @@
+const Card = require('../../Card.js');
+
+class DinoBot extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            optional: true,
+            gameAction: ability.actions.exalt(context => ({
+                target: context.source
+            })),
+            then: {
+                target: {
+                    cardType: 'creature',
+                    gameAction: ability.actions.dealDamage({ amount: 3 })
+                }
+            }
+        });
+
+        this.reap({
+            target: {
+                controller: 'self',
+                location: 'hand',
+                gameAction: ability.actions.discard()
+            },
+            then: {
+                gameAction: ability.actions.draw()
+            }
+        });
+    }
+}
+
+DinoBot.id = 'dino-bot';
+
+module.exports = DinoBot;
