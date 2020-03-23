25422749f61437d620b8abde3e50ea11b717810f
skyjedi <50d2037077b7240576851289a37ce14e5c911642>
2020-03-22 20:58:24 -0500

    implementing DiametricCharge, Lethologica, Mind over Matter, Munchling, Q-Mechs

---
+++ server/game/cards/04-MM/Munchling.js
@@ -0,0 +1,30 @@
+const Card = require('../../Card.js');
+
+class Munchling extends Card {
+    setupCardAbilities (ability) {
+        this.fight({
+            optional: true,
+            select: {
+                mode: 'select',
+                activePromptTitle: 'Choose which to discard from hand or archives',
+                choices: {
+                    'discard': context => context.player.discard.length > 0,
+                    'archives\'s': context => context.player.archives.length > 0
+                }
+            },
+            cards: {
+                dependsOn: 'select',
+                mode: 'upTo',
+                numCards: 1,
+                gameAction: ability.actions.discard()
+            },
+            then: {
+                gameAction: ability.actions.gainAmber()
+            }
+        });
+    }
+}
+
+Munchling.id = 'munchling';
+
+module.exports = Munchling;
