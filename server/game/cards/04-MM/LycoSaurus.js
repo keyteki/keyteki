97c7c2cb59bd77cd0b199473e7f200c543854da4
SkyJedi <skyjedi@gmail.com>
2020-03-25 08:42:09 -0500

    MM: Saurian Dæmo-Saurus, Ides of March, Lyco-Saurus, Techno-Saurus, Umbra-Saurus (#1207)
    * MM Saurian Dæmo-Saurus, Faust the Great, Ides of March, Lyco-Saurus, Techno-Saurus, Unbra-Saurus,
    
    * MM Saurian Dæmo-Saurus, Faust the Great, Ides of March, Lyco-Saurus, Techno-Saurus, Unbra-Saurus,

---
+++ server/game/cards/04-MM/LycoSaurus.js
@@ -0,0 +1,22 @@
+const Card = require('../../Card.js');
+
+class LycoSaurus extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            optional: true,
+            gameAction: ability.actions.exalt(context => ({
+                target: context.source
+            })),
+            then: {
+                target: {
+                    cardType: 'creature',
+                    gameAction: ability.actions.dealDamage({ amount: 3 })
+                }
+            }
+        });
+    }
+}
+
+LycoSaurus.id = 'lyco-saurus';
+
+module.exports = LycoSaurus;
