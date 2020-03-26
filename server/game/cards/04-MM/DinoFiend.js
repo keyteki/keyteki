874b3a72894bad7f6052ca6ec258afe1982c08b3
skyjedi <50d2037077b7240576851289a37ce14e5c911642>
2020-03-26 18:48:42 -0500

    MM: Dis- Dino-Fiend, Lyco-Fiend, Techno-Fiend

---
+++ server/game/cards/04-MM/DinoFiend.js
@@ -0,0 +1,26 @@
+const Card = require('../../Card.js');
+
+class DinoFiend extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            optional: true,
+            gameAction: ability.actions.exalt(context => ({
+                target: context.source
+            })),
+            then: {
+                target: {
+                    cardType: 'creature',
+                    gameAction: ability.actions.dealDamage({ amount: 3 })
+                }
+            }
+        });
+
+        this.destroyed({
+            gameAction: ability.actions.steal({ amount: 1 })
+        });
+    }
+}
+
+DinoFiend.id = 'dino-fiend';
+
+module.exports = DinoFiend;
