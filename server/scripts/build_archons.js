7ee670595c5f52b037eedff5157f83acb0f934f0
skyjedi <50d2037077b7240576851289a37ce14e5c911642>
2020-03-31 15:55:41 -0500

    SPACESESES!!!!

--- server/scripts/build_archons.js
+++ server/scripts/build_archons.js
@@ -129,7 +129,7 @@ const optionDefinitions = [
 
 const buildArchon = (card) => new Promise(cardResolve => {
     const number = card[4];
-    const path = Path.resolve(__dirname, '../../public/img/idbacks/archons', `${Buffer.from(card.join()).toString('base64')}.png`);
+    const path = Path.resolve(__dirname, '../../public/img/idbacks/archons', `${ Buffer.from(card.join()).toString('base64') }.png`);
     if(fs.existsSync(path)) {
         // eslint-disable-next-line no-console
         console.log('File Exists, moving on!');
@@ -142,10 +142,10 @@ const buildArchon = (card) => new Promise(cardResolve => {
     const ctx = canvas.getContext('2d');
     let promises = [];
 
-    promises.push(loadImage(Path.join(__dirname, `./archon_blanks/archon_${number}.png`)));
-    promises.push(loadImage(Path.join(__dirname, `./archon_houses/${card[0]}.png`)));
-    promises.push(loadImage(Path.join(__dirname, `./archon_houses/${card[1]}.png`)));
-    promises.push(loadImage(Path.join(__dirname, `./archon_houses/${card[2]}.png`)));
+    promises.push(loadImage(Path.join(__dirname, `./archon_blanks/archon_${ number }.png`)));
+    promises.push(loadImage(Path.join(__dirname, `./archon_houses/${ card[0] }.png`)));
+    promises.push(loadImage(Path.join(__dirname, `./archon_houses/${ card[1] }.png`)));
+    promises.push(loadImage(Path.join(__dirname, `./archon_houses/${ card[2] }.png`)));
     Promise.all(promises).then(([cardBack, house1, house2, house3]) => {
         ctx.drawImage(cardBack, 0, 0);
         ctx.drawImage(house1, 45, 590, 150, 150);
@@ -199,7 +199,7 @@ const buildAllFiles = async () => {
 
     for(let i = 0; i < cards.length; i++) {
         // eslint-disable-next-line no-console
-        console.log(`Building ${i} ${cards[i]}.  ${cards.length - i} cards to go!`);
+        console.log(`Building ${ i } ${ cards[i] }.  ${ cards.length - i } cards to go!`);
         await buildArchon(cards[i]);
     }
 };
