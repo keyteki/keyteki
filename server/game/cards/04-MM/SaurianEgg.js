57f1ce9f888c90b9135d3200a41f39306c37c816
Rhiannon Christianson <rhiannonchristianson@rhiannons-mbp.lan>
2020-04-02 21:33:34 -0700

    fixed lint

--- server/game/cards/04-MM/SaurianEgg.js
+++ server/game/cards/04-MM/SaurianEgg.js
@@ -1,11 +1,7 @@
 const Card = require('../../Card.js');
 
-
-
 class SaurianEgg extends Card {
-	
     setupCardAbilities(ability) {
-		
         this.persistentEffect({
             match: this,
             effect: ability.effects.cardCannot('reap')
@@ -14,88 +10,45 @@ class SaurianEgg extends Card {
             match: this,
             effect: ability.effects.cardCannot('fight')
         });
-		
 
         this.omni({
 
-            gameAction: ability.actions.discard( context => ({ target: context.player.deck.slice(0,2) } )),
-            
+            gameAction: ability.actions.discard(context => ({ target: context.player.deck.slice(0,2) })),
+
             then: context => ({
-                condition: context.preThenEvents && 
-					context.preThenEvents.filter( 
-	                    event => !event.cancelled && 
-	                    event.name === 'onCardDiscarded'  && 
-	                    event.card.type === 'creature' && 
-	                    event.card.hasHouse('saurian') 
-	                ).length > 0,
-                gameAction: ability.actions.sequentialForEach( context => ({
-					
-					
-                    forEach: context.preThenEvents.filter( 
-						event => !event.cancelled && 
-                        event.name === 'onCardDiscarded'  && 
-                        event.card.type === 'creature' && 
-                        event.card.hasHouse('saurian') 
-                    ).map( event => event.card ),
-                    
-				    action: ability.actions.sequential([
-						ability.actions.putIntoPlay(),
-						ability.actions.ready(),
-						ability.actions.addPowerCounter({ amount: 3 })
-						
-					]) 
-				
+                condition: context.preThenEvents &&
+                    context.preThenEvents.filter(
+                        event => !event.cancelled &&
+                        event.name === 'onCardDiscarded' &&
+                        event.card.type === 'creature' &&
+                        event.card.hasHouse('saurian')
+                    ).length > 0,
+                gameAction: ability.actions.sequentialForEach(context => ({
+
+                    forEach: context.preThenEvents.filter(
+                        event => !event.cancelled &&
+                        event.name === 'onCardDiscarded' &&
+                        event.card.type === 'creature' &&
+                        event.card.hasHouse('saurian')
+                    ).map(event => event.card),
+
+                    action: ability.actions.sequential([
+                        ability.actions.putIntoPlay(),
+                        ability.actions.ready(),
+                        ability.actions.addPowerCounter({ amount: 3 })
+
+                    ])
                 })),
-				
-				then: context => ({
-					gameAction: ability.actions.destroy(context => ({
-						target: context.source
-					}))
-				})
+
+                then: {
+                    gameAction: ability.actions.destroy(context => ({
+                        target: context.source
+                    }))
+                }
             })
         });
-		
-
     }
 }
 
 SaurianEgg.id = 'saurian-egg';
-
 module.exports = SaurianEgg;
-
-/*
-
-        this.omni({
-
-            gameAction: ability.actions.discard( context => ({ target: context.player.deck.slice(0,2) } )),
-            
-            then: context => ({
-                condition: context.preThenEvents && 
-					context.preThenEvents.filter( 
-	                    event => !event.cancelled && 
-	                    event.name === 'onCardDiscarded'  && 
-	                    event.card.type === 'creature' && 
-	                    event.card.hasHouse('saurian') 
-	                ).length > 0,
-                gameAction: ability.actions.sequentialForEach( context => ({
-					
-					
-                    forEach: context.preThenEvents.filter( 
-						event => !event.cancelled && 
-                        event.name === 'onCardDiscarded'  && 
-                        event.card.type === 'creature' && 
-                        event.card.hasHouse('saurian') 
-                    ).map( event => event.card ),
-                    
-				    action: ability.actions.sequential([
-						ability.actions.putIntoPlay(),
-						ability.actions.ready(),
-						ability.actions.addPowerCounter({ amount: 3 })
-						
-					]) 
-				
-                }))
-            })
-        });
-
-*/
--- test/server/cards/04-MM/SaurianEgg.spec.js
+++ test/server/cards/04-MM/SaurianEgg.spec.js
@@ -5,7 +5,7 @@ describe('saurian-egg', function() {
                 this.setupTest({
                     player1: {
                         house: 'saurian',
-                        inPlay: ['saurian-egg','troll','umbra','gargantodon','paraguardian'],
+                        inPlay: ['saurian-egg','troll','umbra','gargantodon','paraguardian']
                     },
                     player2: {
                         amber: 1,
@@ -14,111 +14,108 @@ describe('saurian-egg', function() {
                 });
             });
 
-
             it('should discard 2 cards that are not saurian creatures and not be destoryed.', function() {
-				this.player1.player.deck = [];
-				this.player1.moveCard(this.troll, 'deck');
-				this.player1.moveCard(this.umbra, 'deck');
+                this.player1.player.deck = [];
+                this.player1.moveCard(this.troll, 'deck');
+                this.player1.moveCard(this.umbra, 'deck');
                 this.player1.clickCard(this.saurianEgg);
-				this.player1.clickPrompt('Use this card\'s Omni ability');
-				expect(this.umbra.location).toBe('discard');
-				expect(this.troll.location).toBe('discard');
-				expect(this.saurianEgg.location).toBe('play area');
+                this.player1.clickPrompt('Use this card\'s Omni ability');
+                expect(this.umbra.location).toBe('discard');
+                expect(this.troll.location).toBe('discard');
+                expect(this.saurianEgg.location).toBe('play area');
             });
-			
-            it('should discard 2 cards where the first is saurian and it goes into play', function() {
-				this.player1.player.deck = [];
-				this.player1.moveCard(this.umbra, 'deck');
-				this.player1.moveCard(this.gargantodon, 'deck');
-                this.player1.clickCard(this.saurianEgg);
-				this.player1.clickPrompt('Use this card\'s Omni ability');
-				
-				expect(this.player1).toHavePromptButton('Left');
-				this.player1.clickPrompt('Left');
-				expect(this.gargantodon.location).toBe('play area');
 
-				expect(this.umbra.location).toBe('discard');
-				expect(this.gargantodon.location).toBe('play area');
-				expect(this.player1.player.cardsInPlay[0]).toBe(this.gargantodon);
-				expect(this.gargantodon.exhausted).toBe(false);
-				expect(this.gargantodon.power).toBe(19);
-				
-				expect(this.saurianEgg.location).toBe('discard');
+            it('should discard 2 cards where the first is saurian and it goes into play', function() {
+                this.player1.player.deck = [];
+                this.player1.moveCard(this.umbra, 'deck');
+                this.player1.moveCard(this.gargantodon, 'deck');
+                this.player1.clickCard(this.saurianEgg);
+                this.player1.clickPrompt('Use this card\'s Omni ability');
+
+                expect(this.player1).toHavePromptButton('Left');
+                this.player1.clickPrompt('Left');
+                expect(this.gargantodon.location).toBe('play area');
+
+                expect(this.umbra.location).toBe('discard');
+                expect(this.gargantodon.location).toBe('play area');
+                expect(this.player1.player.cardsInPlay[0]).toBe(this.gargantodon);
+                expect(this.gargantodon.exhausted).toBe(false);
+                expect(this.gargantodon.power).toBe(19);
+
+                expect(this.saurianEgg.location).toBe('discard');
             });
-			
+
             it('should have a omni menu, but not reap or fight', function() {
                 this.player1.clickCard(this.saurianEgg);
                 expect(this.player1).not.toHavePromptButton('Fight with this creature');
-				expect(this.player1).not.toHavePromptButton('Reap with this creature');
-				expect(this.player1).toHavePromptButton('Use this card\'s Omni ability');
+                expect(this.player1).not.toHavePromptButton('Reap with this creature');
+                expect(this.player1).toHavePromptButton('Use this card\'s Omni ability');
             });
-			
+
             it('should discard only 1 cards that and not be destoryed, when only 1 non-saurian card in the deck.', function() {
-				this.player1.player.deck = [];
-				this.player1.moveCard(this.troll, 'deck');
+                this.player1.player.deck = [];
+                this.player1.moveCard(this.troll, 'deck');
                 this.player1.clickCard(this.saurianEgg);
-				this.player1.clickPrompt('Use this card\'s Omni ability');
-				expect(this.troll.location).toBe('discard');
-				expect(this.saurianEgg.location).toBe('play area');
+                this.player1.clickPrompt('Use this card\'s Omni ability');
+                expect(this.troll.location).toBe('discard');
+                expect(this.saurianEgg.location).toBe('play area');
             });
-			
+
             it('should do nothing when no cards in deck', function() {
-				this.player1.player.deck = [];
+                this.player1.player.deck = [];
                 this.player1.clickCard(this.saurianEgg);
-				this.player1.clickPrompt('Use this card\'s Omni ability');
-				expect(this.player1).not.toHavePromptButton('Left');
-				expect(this.saurianEgg.location).toBe('play area');
+                this.player1.clickPrompt('Use this card\'s Omni ability');
+                expect(this.player1).not.toHavePromptButton('Left');
+                expect(this.saurianEgg.location).toBe('play area');
             });
-			
-			
+
             it('when only 1 saurian creature in deck that only has 1 card it should be destroyed', function() {
-				this.player1.player.deck = [];
-				this.player1.moveCard(this.gargantodon, 'deck');
+                this.player1.player.deck = [];
+                this.player1.moveCard(this.gargantodon, 'deck');
                 this.player1.clickCard(this.saurianEgg);
-				this.player1.clickPrompt('Use this card\'s Omni ability');
-				expect(this.player1).toHavePromptButton('Left');
-				this.player1.clickPrompt('Left');
-				expect(this.saurianEgg.location).toBe('discard');
+                this.player1.clickPrompt('Use this card\'s Omni ability');
+                expect(this.player1).toHavePromptButton('Left');
+                this.player1.clickPrompt('Left');
+                expect(this.saurianEgg.location).toBe('discard');
             });
-			
+
             it('when 2 saurian creature in deck it should put both into play', function() {
-				this.player1.player.deck = [];
-				this.player1.moveCard(this.gargantodon, 'deck');
-				this.player1.moveCard(this.paraguardian, 'deck');				
-				
+                this.player1.player.deck = [];
+                this.player1.moveCard(this.gargantodon, 'deck');
+                this.player1.moveCard(this.paraguardian, 'deck');
+
                 this.player1.clickCard(this.saurianEgg);
-				this.player1.clickPrompt('Use this card\'s Omni ability');
-				expect(this.player1).toHavePromptButton('Left');
-				this.player1.clickPrompt('Left');
-				expect(this.player1).toHavePromptButton('Left');
-				this.player1.clickPrompt('Left');
-				
-				expect(this.gargantodon.location).toBe('play area');
-				expect(this.gargantodon.exhausted).toBe(false);
-				expect(this.gargantodon.power).toBe(19);
-				
-				expect(this.paraguardian.location).toBe('play area');
-				expect(this.paraguardian.exhausted).toBe(false);
-				expect(this.paraguardian.power).toBe(9);
-				
-				expect(this.saurianEgg.location).toBe('discard');
+                this.player1.clickPrompt('Use this card\'s Omni ability');
+                expect(this.player1).toHavePromptButton('Left');
+                this.player1.clickPrompt('Left');
+                expect(this.player1).toHavePromptButton('Left');
+                this.player1.clickPrompt('Left');
+
+                expect(this.gargantodon.location).toBe('play area');
+                expect(this.gargantodon.exhausted).toBe(false);
+                expect(this.gargantodon.power).toBe(19);
+
+                expect(this.paraguardian.location).toBe('play area');
+                expect(this.paraguardian.exhausted).toBe(false);
+                expect(this.paraguardian.power).toBe(9);
+
+                expect(this.saurianEgg.location).toBe('discard');
             });
-		
+
             it('when 2 saurian creature in deck it should not be destroyed if it was warded', function() {
-				this.player1.player.deck = [];
-				this.player1.moveCard(this.gargantodon, 'deck');
-				this.player1.moveCard(this.paraguardian, 'deck');				
-				this.saurianEgg.tokens.ward = 1;
-				expect(this.saurianEgg.warded).toBe(true);
+                this.player1.player.deck = [];
+                this.player1.moveCard(this.gargantodon, 'deck');
+                this.player1.moveCard(this.paraguardian, 'deck');
+                this.saurianEgg.tokens.ward = 1;
+                expect(this.saurianEgg.warded).toBe(true);
                 this.player1.clickCard(this.saurianEgg);
-				this.player1.clickPrompt('Use this card\'s Omni ability');
-				expect(this.player1).toHavePromptButton('Left');
-				this.player1.clickPrompt('Left');
-				expect(this.player1).toHavePromptButton('Left');
-				this.player1.clickPrompt('Left');
-				expect(this.saurianEgg.location).toBe('play area');
+                this.player1.clickPrompt('Use this card\'s Omni ability');
+                expect(this.player1).toHavePromptButton('Left');
+                this.player1.clickPrompt('Left');
+                expect(this.player1).toHavePromptButton('Left');
+                this.player1.clickPrompt('Left');
+                expect(this.saurianEgg.location).toBe('play area');
             });
-			
         });
     });
 });
