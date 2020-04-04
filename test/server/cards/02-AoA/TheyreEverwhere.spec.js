421a306a836e1cfbcb574f9f80ab2a4e8594627f
skyjedi <50d2037077b7240576851289a37ce14e5c911642>
2020-04-04 13:35:38 -0500

    thery're everywhere sequential

---
+++ test/server/cards/02-AoA/TheyreEverwhere.spec.js
@@ -0,0 +1,26 @@
+describe('They\'re Everywhere', function() {
+    integration(function() {
+        describe('They\'re Everywhere\'s ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    player1: {
+                        house: 'untamed',
+                        hand: ['they-re-everywhere']
+                    },
+                    player2: {
+                        inPlay: ['helper-bot', 'mooncurser', 'urchin', 'mindworm', 'symon']
+                    }
+                });
+            });
+
+            it('should cause a creature to fight, and then deal 2 damage to its neighbors', function() {
+                this.player1.play(this.theyReEverywhere);
+                expect(this.helperBot.location).toBe('discard');
+                expect(this.mooncurser.location).toBe('play area');
+                expect(this.urchin.location).toBe('discard');
+                expect(this.mindworm.location).toBe('play area');
+                expect(this.symon.location).toBe('discard');
+            });
+        });
+    });
+});
