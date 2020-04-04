215f7bd3d48b8926ca901ae1147f7ad83bd788db
romano-silva <romano.silva@hpe.com>
2020-04-04 02:54:18 -0300

    Suggested file and card names changes for consistency

---
+++ server/game/cards/04-MM/DaemoSaurus.js
@@ -0,0 +1,26 @@
+const Card = require('../../Card.js');
+
+class DaemoSaurus extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            optional: true,
+            gameAction: ability.actions.exalt(context => ({
+                target: context.source
+            })),
+            then: {
+                target: {
+                    cardType: 'creature',
+                    gameAction: ability.actions.dealDamage({ amount: 3 })
+                }
+            }
+        });
+
+        this.destroyed({
+            gameAction: ability.actions.steal({ amount: 1 })
+        });
+    }
+}
+
+DaemoSaurus.id = 'dæmo-saurus';
+
+module.exports = DaemoSaurus;
