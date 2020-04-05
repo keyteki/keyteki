4648a5796d0909737618da73552122e4e7a1c05d
Romano Silva <romano.magacho@gmail.com>
2020-04-05 03:18:32 -0300

    Fixes UnitedAction not considering upgrades (#1234)
    * Fixes UnitedAction not considering upgrades
    Fixes #1150
    This was a bit trickier as upgrades are only counted on cardsInPlay, so I cannot pass player.cardsInPlay to getHousesInPlay. I added a new player filter to the game's getHousesInPlay function
    
    * Replaced 'player' by a filter, which I believe will be more beneficial in the long run
    
    Co-authored-by: romano-silva <romano.silva@hpe.com>

--- server/game/game.js
+++ server/game/game.js
@@ -1026,10 +1026,11 @@ class Game extends EventEmitter {
      *
      * @param {Array} cards - which cards to consider. Default are all cards.
      * @param {boolean} upgrade - if upgrades should be counted. Default is false.
+     * @param {filter} filter - an extra filter to apply to the card.
      */
-    getHousesInPlay(cards = this.cardsInPlay, upgrade = false) {
-        return Constants.Houses.filter(house => cards.some(card => card.hasHouse(house)
-            || (upgrade && card.upgrades && card.upgrades.some(upgrade => upgrade.hasHouse(house)))));
+    getHousesInPlay(cards = this.cardsInPlay, upgrade = false, filter = null) {
+        return Constants.Houses.filter(house => cards.some(card => ((!filter || filter(card)) && card.hasHouse(house))
+            || (upgrade && card.upgrades && card.upgrades.some(upgrade => (!filter || filter(upgrade)) && upgrade.hasHouse(house)))));
     }
 
     firstThingThisTurn() {
