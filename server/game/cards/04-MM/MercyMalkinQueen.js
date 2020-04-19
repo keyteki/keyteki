2917ddf9572209dc180a455cabd00fb8577fdcb8
romano-silva <romano.silva@hpe.com>
2020-04-19 15:44:22 -0300

    And MercyMalkinQueen.js

---
+++ server/game/cards/04-MM/MercyMalkinQueen.js
@@ -0,0 +1,25 @@
+const Card = require('../../Card.js');
+
+class MercyMalkinQueen extends Card {
+    setupCardAbilities(ability) {
+        this.constantReaction({
+            when: {
+                onCardPlayed: (event, context) => context.player === event.player && event.card.type === 'creature' && event.card.hasTrait('cat')
+            },
+            gameAction: ability.actions.ward(context => ({ target: context.event.card }))
+        });
+
+        this.fight({
+            effect: 'ready a friendly Beast creature.',
+            target: {
+                controller: 'self',
+                cardCondition: card => card.hasTrait('beast'),
+                gameAction: ability.actions.ready()
+            }
+        });
+    }
+}
+
+MercyMalkinQueen.id = 'mercy-malkin-queen';
+
+module.exports = MercyMalkinQueen;
