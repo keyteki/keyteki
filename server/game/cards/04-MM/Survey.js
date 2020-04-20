a7c1c1063321d027b96232854c2a01e04bc8bf7e
ryanch <ryanch@gmail.com>
2020-04-20 00:45:24 -0700

    MM: Star Alliance - Survey (#1221)
    * added survey
    
    * fixed lint and removed extra message
    
    * added 1 card test
    
    * fixed message
    
    Co-authored-by: Rhiannon Christianson <rhiannonchristianson@rhiannons-mbp.lan>

---
+++ server/game/cards/04-MM/Survey.js
@@ -0,0 +1,19 @@
+const Card = require('../../Card.js');
+
+class Survey extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            condition: context => context.player.deck.length > 0,
+            gameAction: ability.actions.discard(context => ({
+                promptWithHandlerMenu: {
+                    activePromptTitle: 'Choose a card to discard',
+                    cards: context.player.deck.slice(0, 2)
+                }
+            }))
+        });
+    }
+}
+
+Survey.id = 'survey';
+
+module.exports = Survey;
