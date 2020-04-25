2293668b759823c1924209687414d4e85d9d5a52
romano-silva <romano.silva@hpe.com>
2020-04-24 22:52:51 -0300

    Consider deck size, instead of cards in play to count the difference. This will be useful if this card ever interacts with a giant creature in play

--- server/game/cards/03-WC/Timequake.js
+++ server/game/cards/03-WC/Timequake.js
@@ -4,7 +4,7 @@ class Timequake extends Card {
     setupCardAbilities(ability) {
         this.play({
             gameAction: ability.actions.returnToDeck(context => {
-                context.event.amountCardsInPlay = context.player.cardsInPlay.length;
+                context.event.deckLength = context.player.deck.length;
                 return {
                     shuffle: true,
                     target: context.player.cardsInPlay
@@ -13,9 +13,9 @@ class Timequake extends Card {
             then: preThenContext => ({
                 alwaysTriggers: true,
                 message: '{0} uses {1} to draw {3} card',
-                messageArgs: context => [preThenContext.event ? preThenContext.event.amountCardsInPlay - context.player.cardsInPlay.length : 0],
+                messageArgs: context => [preThenContext.event ? context.player.deck.length - preThenContext.event.deckLength : 0],
                 gameAction: ability.actions.draw(context => ({
-                    amount: preThenContext.event ? preThenContext.event.amountCardsInPlay - context.player.cardsInPlay.length : 0
+                    amount: preThenContext.event ? context.player.deck.length - preThenContext.event.deckLength : 0
                 }))
             })
         });
