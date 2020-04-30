239b208f99777b2407151ae8200c540ffa748441
Romano Silva <romano.magacho@gmail.com>
2020-04-30 04:26:22 -0300

    Fix Timequake not working with warded cards (#1344)
    * Fix Timequake not working with warded cards
    Fixes #1298
    
    * Consider deck size, instead of cards in play to count the difference.
    This will be useful if this card ever interacts with a giant creature in play
    
    Co-authored-by: romano-silva <romano.silva@hpe.com>

--- server/game/cards/03-WC/Timequake.js
+++ server/game/cards/03-WC/Timequake.js
@@ -4,18 +4,19 @@ class Timequake extends Card {
     setupCardAbilities(ability) {
         this.play({
             gameAction: ability.actions.returnToDeck(context => {
-                context.event.amountCardsReturned = context.player.cardsInPlay.length;
+                context.event.deckLength = context.player.deck.length;
                 return {
                     shuffle: true,
                     target: context.player.cardsInPlay
                 };
             }),
             then: preThenContext => ({
+                alwaysTriggers: true,
                 message: '{0} uses {1} to draw {3} card',
-                messageArgs: [preThenContext.event.amountCardsReturned],
-                gameAction: ability.actions.draw({
-                    amount: preThenContext.event && preThenContext.event.amountCardsReturned
-                })
+                messageArgs: context => [preThenContext.event ? context.player.deck.length - preThenContext.event.deckLength : 0],
+                gameAction: ability.actions.draw(context => ({
+                    amount: preThenContext.event ? context.player.deck.length - preThenContext.event.deckLength : 0
+                }))
             })
         });
     }
--- test/server/cards/03-WC/Timequake.spec.js
+++ test/server/cards/03-WC/Timequake.spec.js
@@ -1,6 +1,6 @@
-describe('Lateral Shift', function() {
+describe('Timequake', function() {
     integration(function() {
-        describe('Lateral Shift\'s omni ability', function() {
+        describe('Timequake\'s action ability', function() {
             beforeEach(function() {
                 this.setupTest({
                     player1: {
@@ -24,19 +24,39 @@ describe('Lateral Shift', function() {
 
                 expect(this.player1.player.cardsInPlay.length).toBe(0);
                 expect(this.player1.player.hand.length).toBe(5);
+                expect(this.lamindra.location).not.toBe('play area');
+                expect(this.murkens.location).not.toBe('play area');
+                expect(this.orbOfWonder.location).not.toBe('play area');
             });
 
-            it('should not count cards under other cards', function() {
-                expect(this.player1.player.cardsInPlay.length).toBe(3);
+            it('should return not draw any card if nothing is in play', function() {
+                this.player1.moveCard(this.lamindra, 'discard');
+                this.player1.moveCard(this.orbOfWonder, 'discard');
+                this.player1.moveCard(this.murkens, 'discard');
+                expect(this.player1.player.cardsInPlay.length).toBe(0);
                 expect(this.player1.player.hand.length).toBe(3);
 
                 this.player1.play(this.timequake);
 
                 expect(this.player1.player.cardsInPlay.length).toBe(0);
-                expect(this.player1.player.hand.length).toBe(5);
+                expect(this.player1.player.hand.length).toBe(2);
             });
 
-            it('should return not draw any card if nothing is in play', function() {
+            it('should consider warded cards', function() {
+                this.lamindra.tokens.ward = 1;
+                expect(this.player1.player.cardsInPlay.length).toBe(3);
+                expect(this.player1.player.hand.length).toBe(3);
+
+                this.player1.play(this.timequake);
+
+                expect(this.player1.player.cardsInPlay.length).toBe(1);
+                expect(this.player1.player.hand.length).toBe(4);
+                expect(this.lamindra.location).toBe('play area');
+                expect(this.murkens.location).not.toBe('play area');
+                expect(this.orbOfWonder.location).not.toBe('play area');
+            });
+
+            it('should not count cards under other cards', function() {
                 this.player1.player.removeCardFromPile(this.lamindra);
                 this.player1.player.removeCardFromPile(this.murkens);
 
@@ -55,6 +75,10 @@ describe('Lateral Shift', function() {
 
                 expect(this.player1.player.cardsInPlay.length).toBe(0);
                 expect(this.player1.player.hand.length).toBe(3);
+
+                expect(this.lamindra.location).toBe('discard');
+                expect(this.murkens.location).toBe('discard');
+                expect(this.orbOfWonder.location).not.toBe('play area');
             });
         });
     });
