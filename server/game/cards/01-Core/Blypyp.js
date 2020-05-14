86c8d23308cae6bdff8a8298a9d38938bc0baeca
Romano Silva <romano.magacho@gmail.com>
2020-05-14 05:04:25 -0300

    Fixed Blypyp self-ready (#1456)
    Fixes #1454
    
    Co-authored-by: romano-silva <romano.silva@hpe.com>

--- server/game/cards/01-Core/Blypyp.js
+++ server/game/cards/01-Core/Blypyp.js
@@ -9,7 +9,7 @@ class Blypyp extends Card {
                     onCardPlayed: event => event.card.type === 'creature' && context.player === event.player && event.card.hasHouse('mars')
                 },
                 multipleTrigger: false,
-                gameAction: ability.actions.ready()
+                gameAction: ability.actions.ready(context => ({ target: context.event.card }))
             }))
         });
     }
