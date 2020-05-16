29b1840c064bd8843ee6c162060760c38237713c
romano-silva <romano.silva@hpe.com>
2020-05-16 15:03:00 -0300

    New Untamed and fixed The Shadowsmith

--- server/game/cards/04-MM/TheShadowsmith.js
+++ server/game/cards/04-MM/TheShadowsmith.js
@@ -3,6 +3,7 @@ const Card = require('../../Card.js');
 class TheShadowsmith extends Card {
     setupCardAbilities(ability) {
         this.persistentEffect({
+            targetController: 'any',
             match: card => card.hasTrait('mutant'),
             effect: ability.effects.addKeyword({ elusive: 1 })
         });
--- test/server/cards/03-WC/OdoacThePatrician.spec.js
+++ test/server/cards/03-WC/OdoacThePatrician.spec.js
@@ -25,24 +25,9 @@ describe('Odoac The Patrician', function() {
                 expect(this.player1.amber).toBe(1);
                 expect(this.player2.amber).toBe(3);
             });
-        });
 
-        describe('Odoac The Patrician\'s ability', function() {
-            beforeEach(function() {
-                this.setupTest({
-                    player1: {
-                        amber: 1,
-                        house: 'logos',
-                        hand: ['phase-shift', 'bumpsy', 'dextre', 'urchin']
-                    },
-                    player2: {
-                        amber: 4,
-                        hand: ['odoac-the-patrician']
-                    }
-                });
-            });
-
-            it('should not prevent amber being stolen when it has no amber on it', function() {
+            it('should prevent amber being stolen when it has amber on it', function() {
+                this.player1.player.amber = 1;
                 this.player1.endTurn();
                 this.player2.clickPrompt('saurian');
                 this.player2.play(this.odoacThePatrician);
