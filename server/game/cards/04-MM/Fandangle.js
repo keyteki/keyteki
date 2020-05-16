7d2d2c5b4170594260503561431af6efe7377df2
romano-silva <romano.silva@hpe.com>
2020-05-16 17:24:53 -0300

    Fandangle

---
+++ server/game/cards/04-MM/Fandangle.js
@@ -0,0 +1,16 @@
+const Card = require('../../Card.js');
+
+class Fandangle extends Card {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            targetLocation: 'any',
+            condition: (context) => context.source.controller.amber >= 4,
+            match: card => !card.hasHouse('untamed'),
+            effect: ability.effects.entersPlayReady()
+        });
+    }
+}
+
+Fandangle.id = 'fandangle';
+
+module.exports = Fandangle;
