d5742f366f867e01f7f0f677a884a855acb91af2
romano-silva <romano.silva@hpe.com>
2020-05-18 09:21:41 -0300

    Added an extra test case to Dark Centurion (to confirm functionality after English translation)

--- test/server/cards/04-MM/DarkCenturion.spec.js
+++ test/server/cards/04-MM/DarkCenturion.spec.js
@@ -1,11 +1,11 @@
-describe('dark-centurion', function() {
+fdescribe('Dark Centurion', function() {
     integration(function() {
         describe('Dark Centurion\'s ability', function() {
             beforeEach(function() {
                 this.setupTest({
                     player1: {
                         house: 'saurian',
-                        inPlay: ['umbra','dark-centurion'],
+                        inPlay: ['umbra', 'dark-centurion'],
                         hand: ['access-denied']
                     },
                     player2: {
@@ -15,16 +15,24 @@ describe('dark-centurion', function() {
                 });
             });
 
-            it('should be able to remove one aember from a creature that this player controls, and ward it', function() {
-                this.umbra.tokens.amber = 1;
-                expect(this.umbra.hasToken('amber')).toBe(true);
-
-                expect(this.darkCenturion.location).toBe('play area');
-                this.player1.clickCard(this.darkCenturion);
-                expect(this.player1).toHavePromptButton('Use this card\'s Action ability');
-                this.player1.clickPrompt('Use this card\'s Action ability');
+            it('should be able to select a creature without amber and not ward it', function() {
+                this.player1.useAction(this.darkCenturion);
 
                 expect(this.player1).toBeAbleToSelect(this.umbra);
+                expect(this.player1).toBeAbleToSelect(this.troll);
+                this.player1.clickCard(this.umbra);
+
+                expect(this.umbra.hasToken('amber')).toBe(false);
+                expect(this.umbra.hasToken('ward')).toBe(false);
+                expect(this.darkCenturion.hasToken('ward')).toBe(false);
+            });
+
+            it('should be able to remove one aember from a creature that this player controls, and ward it', function() {
+                this.umbra.tokens.amber = 1;
+                this.player1.useAction(this.darkCenturion);
+
+                expect(this.player1).toBeAbleToSelect(this.umbra);
+                expect(this.player1).toBeAbleToSelect(this.troll);
                 this.player1.clickCard(this.umbra);
 
                 expect(this.umbra.hasToken('amber')).toBe(false);
@@ -36,13 +44,7 @@ describe('dark-centurion', function() {
                 this.umbra.tokens.amber = 1;
                 this.troll.tokens.amber = 1;
 
-                expect(this.umbra.hasToken('amber')).toBe(true);
-                expect(this.troll.hasToken('amber')).toBe(true);
-
-                expect(this.darkCenturion.location).toBe('play area');
-                this.player1.clickCard(this.darkCenturion);
-                expect(this.player1).toHavePromptButton('Use this card\'s Action ability');
-                this.player1.clickPrompt('Use this card\'s Action ability');
+                this.player1.useAction(this.darkCenturion);
 
                 expect(this.player1).toBeAbleToSelect(this.umbra);
                 expect(this.player1).toBeAbleToSelect(this.troll);
