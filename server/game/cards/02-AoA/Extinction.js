44e729f5aaad8ec6dda210fb421e294cbb138326
romano-silva <romano.silva@hpe.com>
2020-05-27 01:47:25 -0300

    Move the context.target check outside the filter call

--- server/game/cards/02-AoA/Extinction.js
+++ server/game/cards/02-AoA/Extinction.js
@@ -7,11 +7,11 @@ class Extinction extends Card {
                 cardType: 'creature',
                 location: 'play area',
                 gameAction: ability.actions.destroy((context) => ({
-                    target: context.game.creaturesInPlay.filter(
-                        (card) =>
-                            context.target &&
-                            card.getTraits().some((trait) => context.target.hasTrait(trait))
-                    )
+                    target: context.target
+                        ? context.game.creaturesInPlay.filter((card) =>
+                              card.getTraits().some((trait) => context.target.hasTrait(trait))
+                          )
+                        : []
                 }))
             },
             effect: 'destroy {0} and each creature that shares a trait with it',
