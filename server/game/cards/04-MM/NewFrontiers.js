c17de37a53b21e6a7c35e0ef392893cb2c49a076
romano-silva <romano.silva@hpe.com>
2020-05-30 18:24:35 -0300

    MM Star Alliance - New Frontiers

---
+++ server/game/cards/04-MM/NewFrontiers.js
@@ -0,0 +1,30 @@
+const Card = require('../../Card.js');
+
+class NewFrontiers extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            target: {
+                mode: 'house'
+            },
+            condition: (context) => context.player.deck.length > 0,
+            effect: 'choose {1} and reveal the top 3 cards of their deck: {2}',
+            effectArgs: (context) => [context.house, context.player.deck.slice(0, 3)],
+            gameAction: [
+                ability.actions.archive((context) => ({
+                    target: context.player.deck
+                        .slice(0, 3)
+                        .filter((card) => card.hasHouse(context.house))
+                })),
+                ability.actions.discard((context) => ({
+                    target: context.player.deck
+                        .slice(0, 3)
+                        .filter((card) => !card.hasHouse(context.house))
+                }))
+            ]
+        });
+    }
+}
+
+NewFrontiers.id = 'new-frontiers';
+
+module.exports = NewFrontiers;
