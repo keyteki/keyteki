587c7d166156bc48ab04b55be31eac6167f48d3a
romano-silva <romano.silva@hpe.com>
2020-05-31 20:33:12 -0300

    pretty

--- server/game/cards/04-MM/BaldricTheBold.js
+++ server/game/cards/04-MM/BaldricTheBold.js
@@ -4,7 +4,14 @@ class BaldricTheBold extends Card {
     setupCardAbilities(ability) {
         this.beforeFight({
             gameAction: ability.actions.gainAmber((context) => ({
-                amount: context.player.opponent && context.event.card.power === Math.max(... context.player.opponent.creaturesInPlay.map(card => card.power)) ? 2 : 0,
+                amount:
+                    context.player.opponent &&
+                    context.event.card.power ===
+                        Math.max(
+                            ...context.player.opponent.creaturesInPlay.map((card) => card.power)
+                        )
+                        ? 2
+                        : 0,
                 target: context.player
             }))
         });
