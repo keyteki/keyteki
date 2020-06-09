35fadb7eae2b5738bcb9c67b11aa6b40c615edd2
Luke Olson <skyjedi@gmail.com>
2020-06-09 05:56:13 -0500

    reverting sequential damage ruling (#1560)
    Co-authored-by: skyjedi@gmail.com <4tX7wP7kYh*8>

--- test/server/cards/02-AoA/TheyreEverwhere.spec.js
+++ test/server/cards/02-AoA/TheyreEverwhere.spec.js
@@ -16,9 +16,9 @@ describe("They're Everywhere", function () {
             it('should cause a creature to fight, and then deal 2 damage to its neighbors', function () {
                 this.player1.play(this.theyReEverywhere);
                 expect(this.helperBot.location).toBe('discard');
-                expect(this.mooncurser.location).toBe('play area');
+                expect(this.mooncurser.location).toBe('discard');
                 expect(this.urchin.location).toBe('discard');
-                expect(this.mindworm.location).toBe('play area');
+                expect(this.mindworm.location).toBe('discard');
                 expect(this.symon.location).toBe('discard');
             });
         });
