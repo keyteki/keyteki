313c34a2cd9b4f4394a83e6a786defb4390a149c
Stuart Walsh <stu@ipng.org.uk>
2020-06-10 21:20:45 +0100

    Implement Animator

---
+++ server/game/cards/04-MM/Animator.js
@@ -0,0 +1,24 @@
+const Card = require('../../Card.js');
+
+class Animator extends Card {
+    setupCardAbilities(ability) {
+        this.action({
+            target: {
+                cardType: 'artifact',
+                gameAction: ability.actions.sequential([
+                    ability.actions.cardLastingEffect((context) => ({
+                        effect: [
+                            ability.effects.changeType('creature'),
+                            ability.effects.changeHouse(context.player.activeHouse)
+                        ].concat(context.source.power ? [] : ability.effects.modifyPower(3))
+                    })),
+                    ability.actions.moveToFlank()
+                ])
+            }
+        });
+    }
+}
+
+Animator.id = 'animator';
+
+module.exports = Animator;
