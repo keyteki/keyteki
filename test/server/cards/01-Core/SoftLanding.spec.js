ca01a644e1c3f87c34c9deb0887b0601e03837a4
LifeIsPizza <pizzamarinara@protonmail.com>
2020-06-14 10:44:10 +0200

    Tests for Blypyp/SL interactions with creatures entering the field

--- test/server/cards/01-Core/SoftLanding.spec.js
+++ test/server/cards/01-Core/SoftLanding.spec.js
@@ -39,5 +39,39 @@ describe('Soft Landing', function () {
                 expect(this.mindwarper.exhausted).toBe(true);
             });
         });
+
+        describe("Soft Landing's ability", function () {
+            beforeEach(function () {
+                this.setupTest({
+                    player1: {
+                        house: 'mars',
+                        hand: ['zorg', 'mindwarper', 'soft-landing', 'john-smyth'],
+                        inPlay: ['tunk']
+                    },
+                    player2: {
+                        inPlay: ['batdrone']
+                    }
+                });
+            });
+
+            it('should ready the next card played after you click the prompt when you have another simultaneous ability', function () {
+                this.player1.play(this.softLanding);
+                this.player1.play(this.zorg);
+                this.player1.clickPrompt('Soft Landing');
+                expect(this.zorg.exhausted).toBe(false);
+                this.player1.play(this.mindwarper);
+                expect(this.mindwarper.exhausted).toBe(true);
+            });
+
+            it('should ready the next card played even if you have a prompt and choose to resolve the other trigger first', function () {
+                this.player1.fightWith(this.tunk, this.batdrone);
+                expect(this.tunk.tokens.damage).toBe(1);
+                this.player1.play(this.softLanding);
+                this.player1.play(this.mindwarper);
+                this.player1.clickCard(this.tunk);
+                expect(this.mindwarper.exhausted).toBe(false);
+                expect(this.tunk.tokens.damage).toBe(undefined);
+            });
+        });
     });
 });
