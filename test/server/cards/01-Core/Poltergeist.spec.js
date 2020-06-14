5a14d21ef80354e3420f481242bd979f60bb1e1a
jeremylarner <jeremyaroundtheworld@gmail.com>
2020-06-14 12:05:28 +0100

    Update Poltergeist.spec.js

--- test/server/cards/01-Core/Poltergeist.spec.js
+++ test/server/cards/01-Core/Poltergeist.spec.js
@@ -137,7 +137,7 @@ describe("Poltergeist's", function () {
             });
         });
 
-        fdescribe('Poltergeist/The Golden Spiral interaction', function () {
+        describe('Poltergeist/The Golden Spiral interaction', function () {
             beforeEach(function () {
                 this.setupTest({
                     player1: {
@@ -160,7 +160,7 @@ describe("Poltergeist's", function () {
                 expect(this.theGoldenSpiral.location).toBe('discard');
             });
 
-            fit('should cause Spiral to target their creature, then get destroyed', function () {
+            it('should cause Spiral to target their creature, then get destroyed', function () {
                 this.player1.play(this.dustImp);
                 expect(this.dustImp.exhausted).toBe(true);
                 this.player1.play(this.poltergeist);
@@ -170,6 +170,7 @@ describe("Poltergeist's", function () {
                 expect(this.theGoldenSpiral.location).toBe('play area');
                 expect(this.player1).toHavePrompt('The Golden Spiral');
                 expect(this.player1).toBeAbleToSelect(this.dustImp);
+                expect(this.player1).not.toBeAbleToSelect(this.questorJarta);
                 this.player1.clickCard(this.dustImp);
                 expect(this.dustImp.exhausted).toBe(false);
                 expect(this.dustImp.amber).toBe(1);
@@ -178,6 +179,58 @@ describe("Poltergeist's", function () {
                 expect(this.player1.amber).toBe(2);
                 expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
                 expect(this.theGoldenSpiral.location).toBe('discard');
+            });
+        });
+
+        describe('Poltergeist/Sneklifter/Domination Bauble interaction', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    player1: {
+                        house: 'shadows',
+                        hand: ['sneklifter'],
+                        inPlay: ['the-golden-spiral', 'questor-jarta']
+                    },
+                    player2: {
+                        hand: ['poltergeist', 'dust-imp'],
+                        inPlay: ['dominator-bauble']
+                    }
+                });
+
+                this.player1.play(this.sneklifter);
+                this.player1.clickCard(this.dominatorBauble);
+                this.player1.endTurn();
+                this.player2.clickPrompt('dis');
+            });
+
+            it('should cause Domination Bauble to fizzle, then get destroyed', function () {
+                expect(this.player2).toHavePrompt('Choose a card to play, discard or use');
+                expect(this.dominatorBauble.controller).toBe(this.player1.player);
+                this.player2.play(this.poltergeist);
+                expect(this.player2).toHavePrompt('Poltergeist');
+                expect(this.player2).toBeAbleToSelect(this.dominatorBauble);
+                this.player2.clickCard(this.dominatorBauble);
+                expect(this.player2).toHavePrompt('Choose a card to play, discard or use');
+                expect(this.dominatorBauble.location).toBe('discard');
+            });
+
+            it('should cause Bauble to target their creature, then get destroyed', function () {
+                this.player2.play(this.dustImp);
+                this.dustImp.ready();
+                expect(this.dustImp.exhausted).toBe(false);
+                this.player2.play(this.poltergeist);
+                expect(this.player2).toHavePrompt('Poltergeist');
+                expect(this.player2).toBeAbleToSelect(this.dominatorBauble);
+                this.player2.clickCard(this.dominatorBauble);
+                expect(this.dominatorBauble.location).toBe('play area');
+                expect(this.player2).toHavePrompt('Dominator Bauble');
+                expect(this.player2).toBeAbleToSelect(this.dustImp);
+                expect(this.player2).not.toBeAbleToSelect(this.questorJarta);
+                this.player2.clickCard(this.dustImp);
+                expect(this.player2).toHavePrompt('Dust Imp');
+                this.player2.clickPrompt('Reap with this creature');
+                expect(this.player2.amber).toBe(2);
+                expect(this.player2).toHavePrompt('Choose a card to play, discard or use');
+                expect(this.dominatorBauble.location).toBe('discard');
             });
         });
     });
