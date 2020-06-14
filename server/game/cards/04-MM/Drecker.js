d1d207d965401492bdc792bcd54c662f7e368cd8
jeremylarner <jeremyaroundtheworld@gmail.com>
2020-06-14 10:57:59 +0100

    MM - Drecker

---
+++ server/game/cards/04-MM/Drecker.js
@@ -0,0 +1,33 @@
+const Card = require('../../Card.js');
+
+class Drecker extends Card {
+    setupCardAbilities(ability) {
+        this.interrupt({
+            when: {
+                onDamageDealt: (event, context) =>
+                    event.fightEvent && context.source.neighbors.includes(event.card)
+            },
+            effect: 'duplicate the damage dealt to {1}',
+            effectArgs: (context) => context.event.card,
+            gameAction: ability.actions.addEventToWindow((context) => ({
+                targetEvent: context.event,
+                eventToAdd: ability.actions
+                    .dealDamage({
+                        amount: context.event.amount,
+                        damageSource: context.event.damageSource,
+                        ignoreArmor: context.event.ignoreArmor,
+                        fightEvent: context.event.fightEvent
+                    })
+                    .getEvent(context.source, context.event.context)
+            }))
+        });
+
+        this.reap({
+            gameAction: ability.actions.steal()
+        });
+    }
+}
+
+Drecker.id = 'drecker';
+
+module.exports = Drecker;
