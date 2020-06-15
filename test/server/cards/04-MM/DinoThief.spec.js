61dbe5fce14ea9dab06a1906290126db83500f12
romano-silva <romano.silva@hpe.com>
2020-06-15 20:01:43 -0300

    Rollback change in DinoThief.spec.js

--- test/server/cards/04-MM/DinoThief.spec.js
+++ test/server/cards/04-MM/DinoThief.spec.js
@@ -1,37 +1,54 @@
 describe('Dino-Thief', function () {
     integration(function () {
-        describe("Dino-Thief's ability", function () {
+        describe("Dino-Thief's Play ability", function () {
             beforeEach(function () {
                 this.setupTest({
                     player1: {
+                        amber: 5,
                         house: 'shadows',
-                        hand: ['dino-thief', 'eyegor', 'titan-mechanic', 'archimedes'],
-                        amber: 4
+                        hand: ['dino-thief']
                     },
                     player2: {
-                        amber: 1,
-                        inPlay: ['lamindra', 'shooler', 'troll']
+                        amber: 4,
+                        inPlay: ['troll']
                     }
+                });
+
+                this.player1.play(this.dinoThief);
+            });
+
+            it('should allow dino-thief to be exalted', function () {
+                expect(this.player1).toHavePrompt('Any reactions to dino-thief being played?');
+            });
+
+            describe('and the ability is triggered', function () {
+                beforeEach(function () {
+                    this.player1.clickCard(this.dinoThief);
+                });
+
+                it('should exalt dino-thief', function () {
+                    expect(this.dinoThief.tokens.amber).toBe(1);
+                });
+
+                describe('should prompt to to do damage', function () {
+                    it('damage troll', function () {
+                        expect(this.player1).toHavePrompt('Choose a creature');
+                        expect(this.player1).toBeAbleToSelect(this.troll);
+                        expect(this.player1).toBeAbleToSelect(this.dinoThief);
+                        this.player1.clickCard(this.troll);
+                        expect(this.troll.tokens.damage).toBe(3);
+                    });
                 });
             });
 
-            it('should choose to not exalt after play', function () {
-                this.player1.play(this.dinoThief);
-                this.player1.clickPrompt('Done');
-                expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
-            });
+            describe('and the ability is not triggered', function () {
+                beforeEach(function () {
+                    this.player1.clickPrompt('Done');
+                });
 
-            it('should choose to exalt after play', function () {
-                this.player1.play(this.dinoThief);
-                this.player1.clickCard(this.dinoThief);
-                expect(this.player1).toBeAbleToSelect(this.dinoThief);
-                expect(this.player1).toBeAbleToSelect(this.troll);
-                expect(this.player1).toBeAbleToSelect(this.shooler);
-                expect(this.player1).toBeAbleToSelect(this.lamindra);
-                this.player1.clickCard(this.shooler);
-                expect(this.dinoThief.amber).toBe(1);
-                expect(this.shooler.tokens.damage).toBe(3);
-                expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+                it('should not exalt dino-thief', function () {
+                    expect(this.dinoThief.tokens.amber).toBe(undefined);
+                });
             });
         });
     });
