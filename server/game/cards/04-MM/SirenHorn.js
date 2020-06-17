f915a8f55c65fad446c109312e413c0da20a4891
Stuart Walsh <stu@ipng.org.uk>
2020-06-17 21:22:55 +0100

    Implement siren horn

---
+++ server/game/cards/04-MM/SirenHorn.js
@@ -0,0 +1,24 @@
+const Card = require('../../Card.js');
+
+class SirenHorn extends Card {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.gainAbility('beforeFight', {
+                effect: 'move 1 amber from {0} to {1}',
+                effectArgs: (context) => context.event.card,
+                gameAction: [
+                    ability.actions.removeAmber((context) => ({
+                        target: context.source
+                    })),
+                    ability.actions.placeAmber((context) => ({
+                        target: context.event.card
+                    }))
+                ]
+            })
+        });
+    }
+}
+
+SirenHorn.id = 'siren-horn';
+
+module.exports = SirenHorn;
