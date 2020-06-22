a9634e845b9860f5737ea34ad2c04157a23f4a2a
Romano Silva <romano.magacho@gmail.com>
2020-06-22 10:34:36 -0300

    Minimize issue with Overrun and Lifeweb. (#1465)
    * Minimize issue with Overrun and Lifeweb.
    If array is undefined, length would cause an exception. Storing a number allow, at least, to return false in the inequality
    Fixes #1443
    Fixes KEYTEKI-16J
    Fixes KEYTEKI-HK
    Fixes KEYTEKI-FN
    Fixes KEYTEKI-SH
    
    * Fixed card name in test
    
    * Added a "setup" phase test to make sure the change worked
    
    * Initializing opponent data
    
    * Initializing at setup
    
    Co-authored-by: romano-silva <romano.silva@hpe.com>

--- test/server/cards/03-WC/Overrun.spec.js
+++ test/server/cards/03-WC/Overrun.spec.js
@@ -3,6 +3,7 @@ describe('Overrun', function () {
         describe('Overruns Ability', function () {
             beforeEach(function () {
                 this.setupTest({
+                    phase: 'setup',
                     player1: {
                         amber: 3,
                         house: 'brobnar',
@@ -15,6 +16,30 @@ describe('Overrun', function () {
                     }
                 });
             });
+
+            it('should NOT cause the opponent to lose amber on first play.', function () {
+                this.player1.clickPrompt('brobnar');
+                this.player1.play(this.overrun);
+                expect(this.player2.amber).toBe(3);
+            });
+        });
+
+        describe('Overruns Ability', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    player1: {
+                        amber: 3,
+                        house: 'brobnar',
+                        hand: ['overrun'],
+                        inPlay: ['brammo', 'ganger-chieftain', 'foozle', 'culf-the-quiet']
+                    },
+                    player2: {
+                        amber: 3,
+                        inPlay: ['silvertooth', 'bad-penny', 'umbra', 'redlock']
+                    }
+                });
+            });
+
             it('should cause the opponent to lose 2A if 3 or more enemy creatures die in a fight this round.', function () {
                 this.player1.fightWith(this.brammo, this.silvertooth);
                 this.player1.fightWith(this.gangerChieftain, this.badPenny);
@@ -22,6 +47,7 @@ describe('Overrun', function () {
                 this.player1.play(this.overrun);
                 expect(this.player2.amber).toBe(1);
             });
+
             it('should NOT cause the opponent to lose 2A if less than 3 enemy creatures die in a fight this round.', function () {
                 this.player1.fightWith(this.brammo, this.silvertooth);
                 this.player1.fightWith(this.gangerChieftain, this.badPenny);
