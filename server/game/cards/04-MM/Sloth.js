704cacc9381f835b712eaf00c7f0dd8b5f4ef27c
romano-silva <romano.silva@hpe.com>
2020-06-26 20:34:53 -0300

    Add Sloth

---
+++ server/game/cards/04-MM/Sloth.js
@@ -0,0 +1,22 @@
+const Card = require('../../Card.js');
+
+class Sloth extends Card {
+    setupCardAbilities(ability) {
+        this.interrupt({
+            when: {
+                onRoundEnded: (event, context) =>
+                    context.player === this.game.activePlayer &&
+                    context.game.cardsUsed.filter((card) => card.type === 'creature').length === 0
+            },
+            gameAction: ability.actions.gainAmber((context) => ({
+                amount: context.player.creaturesInPlay.filter(
+                    (card) => card.type === 'creature' && card.hasTrait('sin')
+                ).length
+            }))
+        });
+    }
+}
+
+Sloth.id = 'sloth';
+
+module.exports = Sloth;
