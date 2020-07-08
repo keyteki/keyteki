7f018e416a34b1b733f364974cf3000cb4354553
romano-silva <romano.silva@hpe.com>
2020-07-08 10:32:01 -0300

    Xit'ed test cases to show Exhume and Exlpo-rover still fails in some situations

--- test/server/cards/02-AoA/Exhume.spec.js
+++ test/server/cards/02-AoA/Exhume.spec.js
@@ -5,13 +5,13 @@ describe('Exhume', function () {
                 player1: {
                     house: 'dis',
                     inPlay: ['flaxia'],
-                    hand: ['exhume'],
-                    discard: ['glimmer', 'ancient-bear', 'bumblebird', 'shooler'],
+                    hand: ['exhume', 'gub', 'spyyyder'],
+                    discard: ['glimmer', 'ancient-bear', 'bumblebird', 'shooler', 'explo-rover'],
                     amber: 4
                 },
                 player2: {
-                    amber: 8,
-                    inPlay: ['bumpsy'],
+                    amber: 1,
+                    inPlay: ['bumpsy', 'lifeward', 'quixxle-stone'],
                     discard: ['troll']
                 }
             });
@@ -29,6 +29,61 @@ describe('Exhume', function () {
             this.player1.clickCard(this.shooler);
             this.player1.clickPrompt('Left');
             expect(this.shooler.location).toBe('play area');
+        });
+
+        it('should allow selecting explo-rover and playing it as an upgrade', function () {
+            this.player1.play(this.exhume);
+            this.player1.clickCard(this.exploRover);
+            expect(this.player1).toHavePromptButton('Play this creature');
+            expect(this.player1).toHavePromptButton('Play this upgrade');
+            this.player1.clickPrompt('Play this upgrade');
+            this.player1.clickCard(this.flaxia);
+            expect(this.exploRover.parent).toBe(this.flaxia);
+        });
+
+        it('should allow selecting explo-rover and playing it as an upgrade', function () {
+            this.player1.play(this.exhume);
+            this.player1.clickCard(this.exploRover);
+            expect(this.player1).toHavePromptButton('Play this creature');
+            expect(this.player1).toHavePromptButton('Play this upgrade');
+            this.player1.clickPrompt('Play this upgrade');
+            this.player1.clickCard(this.flaxia);
+            expect(this.exploRover.parent).toBe(this.flaxia);
+        });
+
+        xit('should allow selecting explo-rover and playing it as an upgrade, if Lifeward was used', function () {
+            this.player1.endTurn();
+            this.player2.clickPrompt('dis');
+            this.player2.useAction(this.lifeward, true);
+            expect(this.lifeward.location).toBe('discard');
+            this.player2.endTurn();
+            this.player1.clickPrompt('dis');
+            this.player1.clickCard(this.gub);
+            expect(this.player1).not.toHavePromptButton('Play this creature');
+            expect(this.player1).toHavePromptButton('Discard this card');
+            this.player1.clickPrompt('Discard this card');
+            this.player1.play(this.exhume);
+            this.player1.clickCard(this.exploRover);
+            expect(this.player1).not.toHavePromptButton('Play this creature');
+            expect(this.player1).toHavePromptButton('Play this upgrade');
+            this.player1.clickPrompt('Play this upgrade');
+            this.player1.clickCard(this.flaxia);
+            expect(this.exploRover.parent).toBe(this.flaxia);
+        });
+
+        xit('should allow selecting explo-rover and playing it as an upgrade, even when Quixxle Stone in play', function () {
+            this.player1.play(this.gub);
+            this.player1.clickCard(this.spyyyder);
+            expect(this.player1).not.toHavePromptButton('Play this creature');
+            expect(this.player1).toHavePromptButton('Discard this card');
+            this.player1.clickPrompt('Discard this card');
+            this.player1.play(this.exhume);
+            this.player1.clickCard(this.exploRover);
+            expect(this.player1).not.toHavePromptButton('Play this creature');
+            expect(this.player1).toHavePromptButton('Play this upgrade');
+            this.player1.clickPrompt('Play this upgrade');
+            this.player1.clickCard(this.flaxia);
+            expect(this.exploRover.parent).toBe(this.flaxia);
         });
     });
 });
