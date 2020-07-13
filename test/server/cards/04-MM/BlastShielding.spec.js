5b1ec0a79c13c120ea05b7cd52ccb345ae040079
Romano Silva <romano.magacho@gmail.com>
2020-07-13 03:10:34 -0300

    Fixes BlastShielding (#1681)
    Co-authored-by: romano-silva <romano.silva@hpe.com>

--- test/server/cards/04-MM/BlastShielding.spec.js
+++ test/server/cards/04-MM/BlastShielding.spec.js
@@ -19,7 +19,7 @@ describe('blast-shielding', function () {
             });
         });
 
-        it('should should apply to a creature', function () {
+        it('should apply to a creature', function () {
             this.player1.playUpgrade(this.blastShielding, this.cxoTaber);
             expect(this.cxoTaber.location).toBe('play area');
             expect(this.cxoTaber.upgrades).toContain(this.blastShielding);
@@ -38,7 +38,7 @@ describe('blast-shielding', function () {
             expect(this.cxoTaber.upgrades).toContain(this.blastShielding);
         });
 
-        it('should get prompt to move blast shield when creature is used, and be able to move it', function () {
+        it('should get prompt to move blast shield when creature is used, and be able to move it right', function () {
             this.player1.playUpgrade(this.blastShielding, this.cxoTaber);
             expect(this.cxoTaber.location).toBe('play area');
             expect(this.cxoTaber.upgrades).toContain(this.blastShielding);
@@ -52,6 +52,22 @@ describe('blast-shielding', function () {
             expect(this.cxoTaber.upgrades).not.toContain(this.blastShielding);
             expect(this.sciOfficerQincan.upgrades).toContain(this.blastShielding);
             expect(this.sciOfficerQincan.tokens.armor).toBe(2);
+        });
+
+        it('should get prompt to move blast shield when creature is used, and be able to move it left', function () {
+            this.player1.playUpgrade(this.blastShielding, this.cxoTaber);
+            expect(this.cxoTaber.location).toBe('play area');
+            expect(this.cxoTaber.upgrades).toContain(this.blastShielding);
+            this.player1.clickCard(this.cxoTaber);
+            expect(this.player1).toHavePromptButton('Reap with this creature');
+            this.player1.clickPrompt('Reap with this creature');
+            expect(this.player1).toHavePromptButton('Done');
+            expect(this.player1).toBeAbleToSelect(this.chiefEngineerWalls);
+            expect(this.player1).toBeAbleToSelect(this.sciOfficerQincan);
+            this.player1.clickCard(this.chiefEngineerWalls);
+            expect(this.cxoTaber.upgrades).not.toContain(this.blastShielding);
+            expect(this.chiefEngineerWalls.upgrades).toContain(this.blastShielding);
+            expect(this.chiefEngineerWalls.tokens.armor).toBe(2);
         });
     });
 });
