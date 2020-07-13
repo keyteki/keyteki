a931ee123e5c5577757f11935c2c793c52c88754
Romano Silva <romano.magacho@gmail.com>
2020-07-13 18:58:09 -0300

    MM Untamed card fixes (#1706)
    Co-authored-by: romano-silva <romano.silva@hpe.com>

--- test/server/cards/04-MM/Fandangle.spec.js
+++ test/server/cards/04-MM/Fandangle.spec.js
@@ -6,7 +6,7 @@ describe('Fandangle', function () {
                     amber: 2,
                     house: 'logos',
                     inPlay: ['fandangle'],
-                    hand: ['phase-shift', 'hunting-witch', 'dextre']
+                    hand: ['phase-shift', 'hunting-witch', 'dextre', 'anomaly-exploiter']
                 },
                 player2: {
                     amber: 2,
@@ -39,6 +39,12 @@ describe('Fandangle', function () {
             this.player1.play(this.huntingWitch);
             expect(this.dextre.exhausted).toBe(false);
             expect(this.huntingWitch.exhausted).toBe(true);
+        });
+
+        it('should ready non-Untamed artifacts if more than 4A', function () {
+            this.player1.player.amber = 5;
+            this.player1.play(this.anomalyExploiter);
+            expect(this.anomalyExploiter.exhausted).toBe(true);
         });
     });
 
