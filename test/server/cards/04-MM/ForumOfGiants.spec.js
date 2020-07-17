af59ef87932c9fa583055764dd14cb4cedf83ba4
romano-silva <romano.silva@hpe.com>
2020-07-17 00:19:58 -0300

    Fixes and removes TODO from Forum of Giants

--- test/server/cards/04-MM/ForumOfGiants.spec.js
+++ test/server/cards/04-MM/ForumOfGiants.spec.js
@@ -9,12 +9,12 @@ describe('Forum Of Giants', function () {
                 player2: {
                     amber: 4,
                     inPlay: ['batdrone'],
-                    hand: ['troll']
+                    hand: ['troll', 'earthshaker', 'culf-the-quiet']
                 }
             });
         });
 
-        it('should give player 1 1 amber at the start of their turn if they have the most powerful creature', function () {
+        it('should give player1 1 amber at the start of their turn if they have the most powerful creature', function () {
             this.player1.endTurn();
 
             this.player2.clickPrompt('brobnar');
@@ -25,11 +25,12 @@ describe('Forum Of Giants', function () {
             expect(this.player2.amber).toBe(4);
         });
 
-        it('should give player 2 1 amber at the start of their turn if they have the most powerful creature', function () {
+        it('should give player2 1 amber at the start of their turn if they have the most powerful creature', function () {
             this.player1.endTurn();
 
             this.player2.clickPrompt('brobnar');
             this.player2.play(this.troll);
+            this.player2.play(this.earthshaker);
             this.player2.endTurn();
 
             this.player1.clickPrompt('saurian');
@@ -37,7 +38,7 @@ describe('Forum Of Giants', function () {
             expect(this.player2.amber).toBe(5);
         });
 
-        it('if there are not creatures, active player gets 1A', function () {
+        it('if there are no creatures, no player gets 1A', function () {
             this.player2.player.moveCard(this.batdrone, 'discard');
             this.player1.player.moveCard(this.stealerOfSouls, 'discard');
 
@@ -47,8 +48,20 @@ describe('Forum Of Giants', function () {
             this.player2.endTurn();
 
             this.player1.clickPrompt('saurian');
-            expect(this.player1.amber).toBe(1);
+            expect(this.player1.amber).toBe(0);
             expect(this.player2.amber).toBe(4);
+        });
+
+        it('if there is a tie, both players get 1A', function () {
+            this.player1.endTurn();
+
+            this.player2.clickPrompt('brobnar');
+            this.player2.play(this.culfTheQuiet);
+            this.player2.endTurn();
+
+            this.player1.clickPrompt('saurian');
+            expect(this.player1.amber).toBe(1);
+            expect(this.player2.amber).toBe(5);
         });
     });
 });
