bad4504fd5e6b334f6e632d6051b188e5b4b6d23
Romano Silva <romano.magacho@gmail.com>
2020-07-20 06:25:00 -0300

    Fix for Gladiodontus and GH (#1801)
    * Fix for Gladiodontus and GH
    
    * Removed redundant prompt title
    
    Co-authored-by: romano-silva <romano.silva@hpe.com>

--- server/game/cards/03-WC/Ghosthawk.js
+++ server/game/cards/03-WC/Ghosthawk.js
@@ -4,19 +4,18 @@ class Ghosthawk extends Card {
     setupCardAbilities(ability) {
         this.play({
             effect: 'reap with each of its neighbors in turn',
-            gameAction: ability.actions.sequentialForEach((context) => ({
-                forEach: context.source.neighbors,
-                action: () =>
-                    ability.actions.reap({
-                        promptForSelect: {
-                            activePromptTitle: 'Choose a creature to reap with',
-                            optional: true,
-                            cardType: 'creature',
-                            cardCondition: (c) => context.source.neighbors.includes(c),
-                            context: context
-                        }
-                    })
-            }))
+            target: {
+                cardCondition: (card, context) =>
+                    !card.exhausted && context.source.neighbors.includes(card),
+                gameAction: ability.actions.sequential([
+                    ability.actions.reap(),
+                    ability.actions.reap((context) => ({
+                        target: context.target
+                            ? context.source.neighbors.filter((c) => c !== context.target)
+                            : []
+                    }))
+                ])
+            }
         });
     }
 }
