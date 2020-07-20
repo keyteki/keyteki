b1dbc9930a853159a7c10209bca3b6c2ebd0f34a
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2020-07-20 10:30:23 +0100

    Yxilo Bolter and Special Delivery bugfix (#1784)
    * Yxilo Bolter and Special Delivery bugfix
    
    * Update YxiloBolter.spec.js
    
    Co-authored-by: Stuart Walsh <stu@ipng.org.uk>

--- server/game/cards/01-Core/SpecialDelivery.js
+++ server/game/cards/01-Core/SpecialDelivery.js
@@ -6,11 +6,15 @@ class SpecialDelivery extends Card {
             target: {
                 cardType: 'creature',
                 cardCondition: (card) => card.isOnFlank(),
-                gameAction: ability.actions.dealDamage({ amount: 3, purge: true })
+                gameAction: ability.actions.dealDamage({ amount: 3 })
             },
             effect: 'sacrifice {1} and deal 3 damage to {0}',
             effectArgs: (context) => context.source,
-            gameAction: ability.actions.sacrifice()
+            gameAction: ability.actions.sacrifice(),
+            then: (context) => ({
+                condition: () => context.target.location === 'discard',
+                gameAction: ability.actions.purge({ target: context.target })
+            })
         });
     }
 }
