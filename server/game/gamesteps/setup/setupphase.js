acd6357f47a13d908313c104c7ea26b2d1aa69dc
Luke Olson <skyjedi@gmail.com>
2020-08-08 07:39:11 -0500

    fixes #1859 (#1865)
    Co-authored-by: skyjedi@gmail.com <4tX7wP7kYh*8>

--- server/game/gamesteps/setup/setupphase.js
+++ server/game/gamesteps/setup/setupphase.js
@@ -27,12 +27,6 @@ class SetupPhase extends Phase {
         for (let step of this.steps) {
             this.game.queueStep(step);
         }
-    }
-
-    setupBegin() {
-        for (let card of this.game.allCards) {
-            card.applyAnyLocationPersistentEffects();
-        }
 
         for (const player of this.game.getPlayers()) {
             let link = {
@@ -42,12 +36,18 @@ class SetupPhase extends Phase {
             };
             if (this.game.gameFormat !== 'sealed' && !this.game.hideDecklists) {
                 this.game.addMessage(
-                    '{0} is playing as the Archon: {1}{2}',
+                    '{0} brings {1}{2} to The Crucible',
                     player,
                     link,
                     player.chains > 0 ? ` with ${player.chains} chains` : ''
                 );
             }
+        }
+    }
+
+    setupBegin() {
+        for (let card of this.game.allCards) {
+            card.applyAnyLocationPersistentEffects();
         }
     }
 
