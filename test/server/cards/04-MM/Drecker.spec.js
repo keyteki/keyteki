21abf2dcb20b64dac17ee5dfa616d370f3206a7d
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2020-08-08 13:37:58 +0100

    Change Drecker to an interrupt (#1862)
    * Drecker tests
    
    * Change Drecker to interrupt
    
    * Make damage simultaneous
    
    * Make prettier happy

--- test/server/cards/04-MM/Drecker.spec.js
+++ test/server/cards/04-MM/Drecker.spec.js
@@ -3,14 +3,15 @@ describe('Drecker', function () {
         this.setupTest({
             player1: {
                 house: 'dis',
-                inPlay: ['snarette', 'drecker', 'umbra'],
-                hand: ['ortannu-s-binding']
+                inPlay: ['snarette', 'drecker'],
+                hand: ['ortannu-s-binding', 'drecker']
             },
             player2: {
                 amber: 3,
                 inPlay: ['troll', 'bad-penny']
             }
         });
+        this.drecker2 = this.player1.findCardByName('drecker', 'hand');
     });
 
     describe("when drecker's neighbor is damaged during a fight", function () {
@@ -45,6 +46,19 @@ describe('Drecker', function () {
         it('should steal 1', function () {
             expect(this.player1.amber).toBe(2);
             expect(this.player2.amber).toBe(2);
+        });
+    });
+
+    describe('when two dreckers are next to each other', function () {
+        beforeEach(function () {
+            this.player1.play(this.drecker2);
+            this.player1.fightWith(this.drecker, this.badPenny);
+        });
+
+        it('should duplicate damage once', function () {
+            expect(this.drecker.neighbors).toContain(this.drecker2);
+            expect(this.drecker.tokens.damage).toBe(1);
+            expect(this.drecker2.tokens.damage).toBe(1);
         });
     });
 });
