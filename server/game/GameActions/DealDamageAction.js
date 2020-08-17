bc5e10ea3eb53f08eced33f816b4b2278b1ee891
jeremylarner <jeremyaroundtheworld@gmail.com>
2020-08-17 15:32:19 +0100

    Add log text for wards preventing damage

--- server/game/GameActions/DealDamageAction.js
+++ server/game/GameActions/DealDamageAction.js
@@ -64,14 +64,18 @@ class DealDamageAction extends CardGameAction {
         };
 
         return super.createEvent('onDamageDealt', params, (damageDealtEvent) => {
-            if (card.warded) {
+            if (damageDealtEvent.card.warded) {
                 for (let event of damageDealtEvent
                     .getSimultaneousEvents()
-                    .filter((event) => event.name === 'onDamageDealt' && event.card === card)) {
+                    .filter((event) => event.name === 'onDamageDealt' && event.card === damageDealtEvent.card)) {
                     event.cancel();
                 }
-
-                card.unward();
+                context.game.addMessage(
+                    '{0}\'s ward token prevents the damage dealt by {1} and is discarded',
+                    damageDealtEvent.card,
+                    damageDealtEvent.damageSource
+                );
+                damageDealtEvent.card.unward();
                 return;
             }
 
