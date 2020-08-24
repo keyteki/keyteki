0d33993d38fb0c4a02dd031bd4ef5f742a0999ae
Romano Silva <romano.magacho@gmail.com>
2020-08-24 15:56:09 -0300

    Turnkey - Add context to forgeKey action to apply a more recent modifier to forge at no cost (#1904)
    * Add an 'inifinite' modifier to forge at no cost
    
    * Rolled back to a better current key cost implementation
    
    * Use forgeContext in target
    
    * Adding atNoCost flag
    
    * Backport atNoCost to other cards
    
    * Redacted test case
    
    Co-authored-by: romano-silva <romano.silva@hpe.com>

---
+++ test/server/cards/02-AoA/Redacted.spec.js
@@ -0,0 +1,60 @@
+describe('[Redacted]', function () {
+    describe("[Redacted]'s ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 5,
+                    house: 'logos',
+                    inPlay: ['dextre'],
+                    hand: ['[redacted]', 'flaxia']
+                },
+                player2: {
+                    amber: 1,
+                    inPlay: ['troll', 'gauntlet-of-command']
+                }
+            });
+        });
+
+        it('should not have amber when played', function () {
+            this.player1.play(this['[redacted]']);
+            expect(this['[redacted]'].amber).toBe(0);
+        });
+
+        it('should not add amber when select a non-logos house', function () {
+            this.player1.play(this['[redacted]']);
+            this.player1.endTurn();
+            this.player2.clickPrompt('brobnar');
+            this.player2.endTurn();
+            this.player1.clickPrompt('untamed');
+            expect(this['[redacted]'].amber).toBe(0);
+        });
+
+        it('should add amber when select logos house', function () {
+            this.player1.play(this['[redacted]']);
+            this.player1.endTurn();
+            this.player2.clickPrompt('brobnar');
+            this.player2.endTurn();
+            this.player1.clickPrompt('logos');
+            expect(this['[redacted]'].amber).toBe(1);
+        });
+
+        it('should add 4th amber, forge and sacrifice when select logos house', function () {
+            this.player1.play(this['[redacted]']);
+            this['[redacted]'].tokens.amber = 2;
+            this.player1.endTurn();
+            this.player2.clickPrompt('brobnar');
+            this.player2.endTurn();
+            this.player1.clickPrompt('logos');
+            expect(this['[redacted]'].amber).toBe(3);
+            this.player1.endTurn();
+            this.player2.clickPrompt('brobnar');
+            this.player2.endTurn();
+            this.player1.clickPrompt('logos');
+            this.player1.forgeKey('red');
+            expect(this['[redacted]'].location).toBe('discard');
+            expect(this.player1.amber).toBe(5);
+            expect(this.player1.player.getForgedKeys()).toBe(1);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
