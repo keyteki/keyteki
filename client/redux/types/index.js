106ed62faad34dff0f8bba6923fdea273a65f323
Stuart Walsh <stu@ipng.org.uk>
2020-09-05 11:47:29 +0100

    remove textarea control (#1948)
    refactor newsadmin control

--- client/redux/types/index.js
+++ client/redux/types/index.js
@@ -57,3 +57,14 @@ export const Admin = Object.freeze({
     SaveUser: 'SAVE_USER',
     UserSaved: 'USER_SAVED'
 });
+
+export const News = Object.freeze({
+    RequestNews: 'REQUEST_NEWS',
+    NewsReceived: 'RECEIVE_NEWS',
+    AddNews: 'ADD_NEWS',
+    NewsAdded: 'NEWS_ADDED',
+    SaveNews: 'SAVE_NEWS',
+    NewsSaved: 'NEWS_SAVED',
+    DeleteNews: 'DELETE_NEWS',
+    NewsDeleted: 'NEWS_DELETED'
+});
