58b876b174d17405fcb25889c32a2fca8f18dca6
Romano Silva <romano.magacho@gmail.com>
2020-09-11 16:25:29 -0300

    Fixes Harvest Time and One Last Job not considering warded creatures (#1954)
    Co-authored-by: romano-silva <romano.silva@hpe.com>

--- server/game/cards/01-Core/OneLastJob.js
+++ server/game/cards/01-Core/OneLastJob.js
@@ -5,18 +5,15 @@ class OneLastJob extends Card {
         this.play({
             effect: 'purge each friendly shadows creature and steal that much amber from {1}',
             effectArgs: (context) => context.player.opponent,
-            gameAction: [
-                ability.actions.purge((context) => ({
-                    target: context.player.creaturesInPlay.filter((card) =>
-                        card.hasHouse('shadows')
-                    )
-                })),
-                ability.actions.steal((context) => ({
-                    amount: context.player.creaturesInPlay.filter((card) =>
-                        card.hasHouse('shadows')
-                    ).length
+            gameAction: ability.actions.purge((context) => ({
+                target: context.player.creaturesInPlay.filter((card) => card.hasHouse('shadows'))
+            })),
+            then: {
+                alwaysTriggers: true,
+                gameAction: ability.actions.steal((context) => ({
+                    amount: context.preThenEvents.filter((event) => !event.cancelled).length
                 }))
-            ]
+            }
         });
     }
 }
