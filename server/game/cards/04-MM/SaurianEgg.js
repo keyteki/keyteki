8897a075f0d86a763665977e394c688f659de9a7
romano-silva <romano.silva@hpe.com>
2020-11-25 20:04:18 -0300

    Fixes Saurian not being destroyed hen half gigantic is discarded

--- server/game/cards/04-MM/SaurianEgg.js
+++ server/game/cards/04-MM/SaurianEgg.js
@@ -11,47 +11,56 @@ class SaurianEgg extends Card {
                 target: context.player.deck.slice(0, 2)
             })),
             then: {
-                gameAction: ability.actions.sequentialForEach((context) => {
-                    let forEachCards = context.preThenEvents
-                        ? context.preThenEvents
-                              .filter(
-                                  (event) =>
-                                      event.card.type === 'creature' &&
-                                      event.card.hasHouse('saurian')
-                              )
-                              .map((event) => event.card)
-                        : [];
+                gameAction: [
+                    ability.actions.sequentialForEach((context) => {
+                        let forEachCards = context.preThenEvents
+                            ? context.preThenEvents
+                                  .filter(
+                                      (event) =>
+                                          event.card.type === 'creature' &&
+                                          event.card.hasHouse('saurian')
+                                  )
+                                  .map((event) => event.card)
+                            : [];
 
-                    if (forEachCards.length === 1) {
-                        if (forEachCards[0].gigantic) {
-                            forEachCards = [];
-                        }
-                    } else if (forEachCards.length === 2) {
-                        if (forEachCards[0].gigantic) {
-                            if (!forEachCards[1].gigantic) {
-                                forEachCards = [forEachCards[1]];
-                            } else if (forEachCards[1].id === forEachCards[0].compositeId) {
-                                forEachCards = [forEachCards[0]];
-                            } else {
+                        if (forEachCards.length === 1) {
+                            if (forEachCards[0].gigantic) {
                                 forEachCards = [];
                             }
-                        } else if (forEachCards[1].gigantic) {
-                            forEachCards = [forEachCards[0]];
+                        } else if (forEachCards.length === 2) {
+                            if (forEachCards[0].gigantic) {
+                                if (!forEachCards[1].gigantic) {
+                                    forEachCards = [forEachCards[1]];
+                                } else if (forEachCards[1].id === forEachCards[0].compositeId) {
+                                    forEachCards = [forEachCards[0]];
+                                } else {
+                                    forEachCards = [];
+                                }
+                            } else if (forEachCards[1].gigantic) {
+                                forEachCards = [forEachCards[0]];
+                            }
                         }
-                    }
 
-                    return {
-                        forEach: forEachCards,
-                        action: ability.actions.sequential([
-                            ability.actions.putIntoPlay(),
-                            ability.actions.ready(),
-                            ability.actions.addPowerCounter({ amount: 3 })
-                        ])
-                    };
-                }),
-                then: {
-                    gameAction: ability.actions.destroy()
-                }
+                        return {
+                            forEach: forEachCards,
+                            action: ability.actions.sequential([
+                                ability.actions.putIntoPlay(),
+                                ability.actions.ready(),
+                                ability.actions.addPowerCounter({ amount: 3 })
+                            ])
+                        };
+                    }),
+                    ability.actions.destroy((context) => ({
+                        target:
+                            context.preThenEvents &&
+                            context.preThenEvents.filter(
+                                (event) =>
+                                    event.card.type === 'creature' && event.card.hasHouse('saurian')
+                            ).length > 0
+                                ? context.source
+                                : null
+                    }))
+                ]
             }
         });
     }
