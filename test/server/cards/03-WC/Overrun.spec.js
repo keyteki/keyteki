83ff94851a938c6437f93d383337eb0cd43c74e4
Romano Silva <romano.magacho@gmail.com>
2020-12-02 06:55:46 -0300

    Added more test cases for overrun (#2092)
    Co-authored-by: romano-silva <romano.silva@hpe.com>

--- test/server/cards/03-WC/Overrun.spec.js
+++ test/server/cards/03-WC/Overrun.spec.js
@@ -5,27 +5,63 @@ describe('Overrun', function () {
                 player1: {
                     amber: 3,
                     house: 'brobnar',
-                    hand: ['overrun'],
+                    hand: ['overrun', 'ballcano'],
                     inPlay: ['brammo', 'ganger-chieftain', 'foozle', 'culf-the-quiet']
                 },
                 player2: {
-                    amber: 3,
-                    inPlay: ['silvertooth', 'bad-penny', 'umbra', 'redlock']
+                    amber: 5,
+                    inPlay: ['silvertooth', 'bad-penny', 'umbra'],
+                    hand: ['lamindra']
                 }
             });
         });
+
         it('should cause the opponent to lose 2A if 3 or more enemy creatures die in a fight this round.', function () {
             this.player1.fightWith(this.brammo, this.silvertooth);
             this.player1.fightWith(this.gangerChieftain, this.badPenny);
             this.player1.fightWith(this.foozle, this.umbra);
             this.player1.play(this.overrun);
-            expect(this.player2.amber).toBe(1);
+            expect(this.player2.amber).toBe(3);
         });
+
+        it('should only count creatures destroyed in the current round.', function () {
+            this.player1.fightWith(this.brammo, this.silvertooth);
+            this.player1.fightWith(this.gangerChieftain, this.badPenny);
+            this.player1.fightWith(this.foozle, this.umbra);
+            this.player1.play(this.overrun);
+            expect(this.player2.amber).toBe(3);
+            this.player1.endTurn();
+            this.player2.clickPrompt('shadows');
+            this.player2.play(this.lamindra);
+            this.player2.endTurn();
+            this.player1.clickPrompt('brobnar');
+            this.player1.moveCard(this.overrun, 'hand');
+            this.player1.play(this.overrun);
+            this.player1.play(this.ballcano);
+            expect(this.lamindra.location).toBe('discard');
+            expect(this.player2.amber).toBe(3);
+        });
+
         it('should NOT cause the opponent to lose 2A if less than 3 enemy creatures die in a fight this round.', function () {
             this.player1.fightWith(this.brammo, this.silvertooth);
             this.player1.fightWith(this.gangerChieftain, this.badPenny);
             this.player1.play(this.overrun);
-            expect(this.player2.amber).toBe(3);
+            expect(this.player2.amber).toBe(5);
+        });
+
+        it('should NOT cause the opponent to lose 2A if creatures are warded.', function () {
+            this.silvertooth.ward();
+            this.umbra.ward();
+            this.badPenny.ward();
+            this.player1.play(this.ballcano);
+            expect(this.silvertooth.location).toBe('play area');
+            expect(this.umbra.location).toBe('play area');
+            expect(this.badPenny.location).toBe('play area');
+            expect(this.silvertooth.tokens.ward).toBeUndefined();
+            expect(this.badPenny.tokens.ward).toBeUndefined();
+            expect(this.umbra.tokens.ward).toBeUndefined();
+            this.player1.play(this.overrun);
+            expect(this.player2.amber).toBe(5);
         });
     });
 });
