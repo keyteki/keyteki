f90592998239d2db797a179d7ab3d9279de0ce35
romano-silva <romano.silva@hpe.com>
2020-12-03 17:09:30 -0300

    Handling reveal at PlayCardACtion instead of Cards

--- server/game/GameActions/PlayCardAction.js
+++ server/game/GameActions/PlayCardAction.js
@@ -3,6 +3,8 @@ const CardGameAction = require('./CardGameAction');
 class PlayCardAction extends CardGameAction {
     setDefaultProperties() {
         this.location = 'hand';
+        this.revealOnIllegalTarget = false;
+        this.revealOnIllegalTargetMessage = null;
     }
 
     setup() {
@@ -16,10 +18,14 @@ class PlayCardAction extends CardGameAction {
             return false;
         }
 
+        if (this.revealOnIllegalTarget) {
+            return true;
+        }
+
         let actions = card
             .getActions(this.location)
             .filter((action) => action.title.includes('Play'));
-        return !!actions.find((action) => {
+        return actions.some((action) => {
             let actionContext = action.createContext(context.player);
             actionContext.ignoreHouse = true;
             return !action.meetsRequirements(actionContext, ['location']);
@@ -27,17 +33,17 @@ class PlayCardAction extends CardGameAction {
     }
 
     getEvent(card, context) {
-        return super.createEvent('unnamedEvent', { card: card, context: context }, () => {
-            let playActions = card.getActions(this.location).filter((action) => {
-                if (action.title.includes('Play')) {
-                    let newContext = action.createContext(context.player);
-                    newContext.ignoreHouse = true;
-                    return !action.meetsRequirements(newContext, ['location']);
-                } else {
-                    return false;
-                }
-            });
+        let playActions = card.getActions(this.location).filter((action) => {
+            if (action.title.includes('Play')) {
+                let newContext = action.createContext(context.player);
+                newContext.ignoreHouse = true;
+                return !action.meetsRequirements(newContext, ['location']);
+            } else {
+                return false;
+            }
+        });
 
+        let playEvent = super.createEvent('unnamedEvent', { card: card, context: context }, () => {
             let action;
             if (playActions.length > 1) {
                 context.game.promptWithHandlerMenu(context.player, {
@@ -58,6 +64,19 @@ class PlayCardAction extends CardGameAction {
                 context.game.resolveAbility(actionContext);
             });
         });
+
+        playEvent.addChildEvent(
+            context.game.getEvent('unamedEvent', { card, context }, () => {
+                if (playActions.length === 0 && this.revealOnIllegalTarget) {
+                    const message =
+                        this.revealOnIllegalTargetMessage ||
+                        '{0} uses {1} to only reveal and not play {2}';
+                    context.game.addMessage(message, context.player, context.source, card);
+                }
+            })
+        );
+
+        return playEvent;
     }
 }
 
