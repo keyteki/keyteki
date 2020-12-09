dd9815d6e99b544e328c7ff35abcd19f1ab0ae56
romano-silva <romano.silva@hpe.com>
2020-12-09 20:11:55 -0300

    Grafted cards should be visible and Memolith should allow selecting not-controlled cards

--- test/server/cards/03-WC/Memolith.spec.js
+++ test/server/cards/03-WC/Memolith.spec.js
@@ -44,7 +44,12 @@ describe('Memolith', function () {
             expect(this.player1).not.toBeAbleToSelect(this.infomancer);
             expect(this.player1).not.toBeAbleToSelect(this.autocannon);
             this.player1.clickCard(this.pound);
-            expect(this.pound.facedown).toBe(false);
+            expect(this.player1.player.game.isCardVisible(this.pound, this.player1.player)).toBe(
+                true
+            );
+            expect(this.player1.player.game.isCardVisible(this.pound, this.player2.player)).toBe(
+                true
+            );
             expect(this.pound.parent).toBe(this.memolith);
             expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
         });
@@ -52,7 +57,12 @@ describe('Memolith', function () {
         it('should activate the play effect of a grafted card', function () {
             this.player1.useAction(this.memolith);
             this.player1.clickCard(this.pound);
-            expect(this.pound.facedown).toBe(false);
+            expect(this.player1.player.game.isCardVisible(this.pound, this.player1.player)).toBe(
+                true
+            );
+            expect(this.player1.player.game.isCardVisible(this.pound, this.player2.player)).toBe(
+                true
+            );
             expect(this.pound.parent).toBe(this.memolith);
             this.memolith.exhausted = false;
             this.player1.useAction(this.memolith);
@@ -72,7 +82,12 @@ describe('Memolith', function () {
             this.player1.moveCard(this.anger, 'discard');
             this.player1.useAction(this.memolith);
             this.player1.clickCard(this.pound);
-            expect(this.pound.facedown).toBe(false);
+            expect(this.player1.player.game.isCardVisible(this.pound, this.player1.player)).toBe(
+                true
+            );
+            expect(this.player1.player.game.isCardVisible(this.pound, this.player2.player)).toBe(
+                true
+            );
             expect(this.pound.parent).toBe(this.memolith);
             this.memolith.exhausted = false;
             this.player1.useAction(this.memolith);
@@ -91,7 +106,12 @@ describe('Memolith', function () {
             this.player1.clickCard(this.anger);
             this.player1.useAction(this.memolith);
             this.player1.clickCard(this.pound);
-            expect(this.pound.facedown).toBe(false);
+            expect(this.player1.player.game.isCardVisible(this.pound, this.player1.player)).toBe(
+                true
+            );
+            expect(this.player1.player.game.isCardVisible(this.pound, this.player2.player)).toBe(
+                true
+            );
             expect(this.pound.parent).toBe(this.memolith);
             this.memolith.exhausted = false;
             this.player1.useAction(this.memolith);
@@ -104,6 +124,84 @@ describe('Memolith', function () {
             expect(this.tantadlin.tokens.damage).toBe(2);
             expect(this.bigtwig.tokens.damage).toBe(1);
             expect(this.flaxia.tokens.damage).toBe(1);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+
+    describe("Memolith's and Remote Access ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'logos',
+                    hand: ['remote-access', 'wild-wormhole', 'labwork', 'dextre']
+                },
+                player2: {
+                    inPlay: ['memolith', 'troll'],
+                    hand: ['anger', 'pound']
+                }
+            });
+        });
+
+        it('should allow grafting an action card', function () {
+            this.player1.play(this.remoteAccess);
+            this.player1.clickCard(this.memolith);
+            expect(this.player1).toBeAbleToSelect(this.wildWormhole);
+            expect(this.player1).toBeAbleToSelect(this.labwork);
+            expect(this.player1).not.toBeAbleToSelect(this.dextre);
+            expect(this.player1).not.toBeAbleToSelect(this.pound);
+            expect(this.player1).not.toBeAbleToSelect(this.anger);
+            this.player1.clickCard(this.labwork);
+            expect(this.player1.player.game.isCardVisible(this.labwork, this.player1.player)).toBe(
+                true
+            );
+            expect(this.player1.player.game.isCardVisible(this.labwork, this.player2.player)).toBe(
+                true
+            );
+            expect(this.labwork.parent).toBe(this.memolith);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it("should allow using opponent's grafted card", function () {
+            this.player1.play(this.remoteAccess);
+            this.player1.clickCard(this.memolith);
+            this.player1.clickCard(this.labwork);
+            this.player1.endTurn();
+            this.player2.clickPrompt('brobnar');
+            this.memolith.exhausted = false;
+            this.player2.useAction(this.memolith);
+            expect(this.player2).not.toBeAbleToSelect(this.wildWormhole);
+            expect(this.player2).toBeAbleToSelect(this.labwork);
+            expect(this.player2).not.toBeAbleToSelect(this.dextre);
+            expect(this.player2).toBeAbleToSelect(this.pound);
+            expect(this.player2).toBeAbleToSelect(this.anger);
+            this.player2.clickCard(this.pound);
+            expect(this.player1.player.game.isCardVisible(this.labwork, this.player1.player)).toBe(
+                true
+            );
+            expect(this.player1.player.game.isCardVisible(this.labwork, this.player2.player)).toBe(
+                true
+            );
+            expect(this.player1.player.game.isCardVisible(this.pound, this.player1.player)).toBe(
+                true
+            );
+            expect(this.player1.player.game.isCardVisible(this.pound, this.player2.player)).toBe(
+                true
+            );
+            expect(this.labwork.parent).toBe(this.memolith);
+            expect(this.pound.parent).toBe(this.memolith);
+            this.player2.endTurn();
+            this.player1.clickPrompt('logos');
+            this.player1.moveCard(this.remoteAccess, 'hand');
+            this.player1.play(this.remoteAccess);
+            this.player1.clickCard(this.memolith);
+            expect(this.player1).toBeAbleToSelect(this.wildWormhole);
+            expect(this.player1).toBeAbleToSelect(this.labwork);
+            expect(this.player1).not.toBeAbleToSelect(this.dextre);
+            expect(this.player1).toBeAbleToSelect(this.pound);
+            expect(this.player1).not.toBeAbleToSelect(this.anger);
+            this.player1.clickCard(this.pound);
+            this.player1.clickCard(this.troll);
+            expect(this.troll.tokens.damage).toBe(2);
             expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
         });
     });
