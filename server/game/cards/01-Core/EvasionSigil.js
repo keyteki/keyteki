ba0db9fe130b40725cf6030d290c8eaeabbb4660
romano-silva <romano.silva@hpe.com>
2020-12-09 13:11:00 -0300

    Fixes 2 Evasion Sigil and Hazardous/Assault interaction

--- server/game/cards/01-Core/EvasionSigil.js
+++ server/game/cards/01-Core/EvasionSigil.js
@@ -36,9 +36,10 @@ class EvasionSigil extends Card {
                     let topCard = player.deck.length ? player.deck[0] : '';
                     let cancelFight =
                         topCard && topCard.hasHouse(context.game.activePlayer.activeHouse);
+
                     return {
                         event: context.event,
-                        cancel: cancelFight
+                        condition: cancelFight ? () => false : context.event.condition
                     };
                 }),
                 ability.actions.discard((context) => ({
