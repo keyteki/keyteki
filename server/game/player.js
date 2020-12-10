5363bb7b76c155f4f94f898cfb1f833e7294f017
romano-silva <romano.silva@hpe.com>
2020-12-10 13:22:16 -0300

    Avoid adding the message when choice is zero

--- server/game/player.js
+++ server/game/player.js
@@ -621,11 +621,13 @@ class Player extends GameObject {
                             modifiedCost -= choice;
                             totalAvailable -= choice;
                             source.removeToken('amber', choice);
-                            this.game.addMessage(
-                                `{0} uses ${choice} amber from {1} to forge a key`,
-                                this.game.activePlayer,
-                                source
-                            );
+                            if (choice) {
+                                this.game.addMessage(
+                                    `{0} uses ${choice} amber from {1} to forge a key`,
+                                    this.game.activePlayer,
+                                    source
+                                );
+                            }
                         }
                     });
                 });
