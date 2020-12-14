30a13c937b3b771afe0fe3c368d9adb17566a886
romano-silva <romano.silva@hpe.com>
2020-12-14 12:31:23 -0300

    Test case for Zorg

---
+++ test/server/cards/01-Core/Zorg.spec.js
@@ -0,0 +1,58 @@
+describe('Zorg', function () {
+    describe("Zorg's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'mars',
+                    hand: ['zorg', 'exhume', 'three-fates']
+                },
+                player2: {
+                    inPlay: ['ancient-bear', 'niffle-ape'],
+                    hand: ['gateway-to-dis', 'hypnobeam']
+                }
+            });
+        });
+
+        it('should enter play stunned', function () {
+            this.player1.play(this.zorg);
+            expect(this.zorg.location).toBe('play area');
+            expect(this.zorg.stunned).toBe(true);
+        });
+
+        it('should enter play after being destroyed and played with Exhume', function () {
+            this.player1.play(this.zorg);
+            this.player1.endTurn();
+            this.player2.clickPrompt('dis');
+            this.player2.play(this.gatewayToDis);
+            this.player2.endTurn();
+            this.player1.clickPrompt('dis');
+            this.player1.play(this.exhume);
+            this.player1.clickCard(this.zorg);
+            expect(this.zorg.location).toBe('play area');
+            expect(this.zorg.stunned).toBe(true);
+        });
+
+        it('should enter play after taken control, being destroyed and played with Exhume', function () {
+            this.player1.play(this.zorg);
+            this.player1.endTurn();
+            this.player2.clickPrompt('mars');
+            this.player2.play(this.hypnobeam);
+            this.player2.clickCard(this.zorg);
+            this.player2.clickPrompt('Right');
+            this.player2.clickCard(this.zorg);
+            this.player2.clickPrompt("Remove this creature's stun");
+            this.player2.endTurn();
+            this.player1.clickPrompt('dis');
+            this.player1.play(this.threeFates);
+            this.player1.clickCard(this.niffleApe);
+            this.player1.clickCard(this.ancientBear);
+            this.player1.clickCard(this.zorg);
+            this.player1.clickPrompt('Done');
+            expect(this.zorg.location).toBe('discard');
+            this.player1.play(this.exhume);
+            this.player1.clickCard(this.zorg);
+            expect(this.zorg.location).toBe('play area');
+            expect(this.zorg.stunned).toBe(true);
+        });
+    });
+});
