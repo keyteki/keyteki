d9bff98177fe964eab82231d94d1d3d130921f68
Romano Silva <romano.magacho@gmail.com>
2020-12-28 10:56:00 -0300

    Fixed takeControl to be a flexible effect (#2148)
    Co-authored-by: romano-silva <romano.silva@hpe.com>

--- server/game/cards/01-Core/CollarOfSubordination.js
+++ server/game/cards/01-Core/CollarOfSubordination.js
@@ -3,7 +3,7 @@ const Card = require('../../Card.js');
 class CollarOfSubordination extends Card {
     setupCardAbilities(ability) {
         this.whileAttached({
-            effect: ability.effects.takeControl(this.controller)
+            effect: ability.effects.takeControl(() => this.controller)
         });
     }
 }
