9ac9c33ce91920e7d628d5ff1344db1c561047a0
romano-silva <romano.silva@hpe.com>
2021-01-13 15:42:37 -0300

    Hard Simpson twins

---
+++ test/server/cards/05-DT/HardSimpsonEvilTwin.spec.js
@@ -0,0 +1,65 @@
+describe("Hard Simpson Evil Twin's ability", function () {
+    describe('when tide is neutral', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 4,
+                    house: 'shadows',
+                    inPlay: ['hard-simpson-evil-twin', 'shooler']
+                },
+                player2: {
+                    amber: 3,
+                    inPlay: ['murkens', 'lamindra']
+                }
+            });
+            this.shooler.tokens.damage = 1;
+            this.murkens.tokens.damage = 1;
+        });
+
+        it('should not capture amber', function () {
+            this.player1.reap(this.hardSimpsonEvilTwin);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        describe('when tide is low', function () {
+            beforeEach(function () {
+                this.player1.lowerTide();
+            });
+
+            it('should not capture amber', function () {
+                this.player1.reap(this.hardSimpsonEvilTwin);
+                expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            });
+        });
+
+        describe('when tide is high', function () {
+            beforeEach(function () {
+                this.player1.raiseTide();
+            });
+
+            it('should capture amber on enemy creature', function () {
+                this.player1.reap(this.hardSimpsonEvilTwin);
+                expect(this.player1).not.toBeAbleToSelect(this.lamindra);
+                expect(this.player1).toBeAbleToSelect(this.murkens);
+                this.player1.clickCard(this.murkens);
+                expect(this.murkens.amber).toBe(1);
+                expect(this.lamindra.amber).toBe(0);
+                expect(this.player1.amber).toBe(5);
+                expect(this.player2.amber).toBe(2);
+                expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            });
+
+            it('should capture amber on friendly creature', function () {
+                this.player1.reap(this.hardSimpsonEvilTwin);
+                expect(this.player1).not.toBeAbleToSelect(this.hardSimpsonEvilTwin);
+                expect(this.player1).toBeAbleToSelect(this.shooler);
+                this.player1.clickCard(this.shooler);
+                expect(this.hardSimpsonEvilTwin.amber).toBe(0);
+                expect(this.shooler.amber).toBe(1);
+                expect(this.player1.amber).toBe(4);
+                expect(this.player2.amber).toBe(3);
+                expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            });
+        });
+    });
+});
