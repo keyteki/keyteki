a8307dda52d26ad1954e78692242fddcea4a6f09
romano-silva <romano.silva@hpe.com>
2021-01-14 09:09:36 -0300

    Fixed tests in the wrong folder. Added Kaupe and Sea Urchin

---
+++ server/game/cards/05-DT/Kaupe.js
@@ -0,0 +1,50 @@
+const Card = require('../../Card.js');
+
+class Kaupe extends Card {
+    //Your opponent cannot play more than 1 card of each card type (action, artifact, creature, upgrade) each turn.
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () =>
+                this.game.cardsPlayed.filter((card) => card.type === 'action').length > 0,
+            targetController: 'opponent',
+            effect: ability.effects.playerCannot(
+                'play',
+                (context) => context.source.type === 'action'
+            )
+        });
+
+        this.persistentEffect({
+            condition: () =>
+                this.game.cardsPlayed.filter((card) => card.type === 'artifact').length > 0,
+            targetController: 'opponent',
+            effect: ability.effects.playerCannot(
+                'play',
+                (context) => context.source.type === 'artifact'
+            )
+        });
+
+        this.persistentEffect({
+            condition: () =>
+                this.game.cardsPlayed.filter((card) => card.type === 'creature').length > 0,
+            targetController: 'opponent',
+            effect: ability.effects.playerCannot(
+                'play',
+                (context) => context.source.type === 'creature'
+            )
+        });
+
+        this.persistentEffect({
+            condition: () =>
+                this.game.cardsPlayed.filter((card) => card.type === 'upgrade').length > 0,
+            targetController: 'opponent',
+            effect: ability.effects.playerCannot(
+                'play',
+                (context) => context.source.type === 'upgrade'
+            )
+        });
+    }
+}
+
+Kaupe.id = 'kaupe';
+
+module.exports = Kaupe;
