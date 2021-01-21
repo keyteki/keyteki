ebb0d9d47127e6e8e26b3d057bef02b9c373435e
Romano Silva <romano.magacho@gmail.com>
2021-01-21 08:50:10 -0300

    Remove 'may' from Vezyme and Stunner (#2190)
    * Remove 'may' from Vezyme and Stunner
    Standardize the optional prompt of these two cards
    
    * Fixed Deusillus test case
    
    Co-authored-by: romano-silva <romano.silva@hpe.com>

--- test/server/cards/01-Core/VezymaThinkdrone.spec.js
+++ test/server/cards/01-Core/VezymaThinkdrone.spec.js
@@ -14,21 +14,32 @@ describe('Vezyma Thinkdrone', function () {
             });
         });
 
+        it('should be optional', function () {
+            this.player1.play(this.phloxemSpike);
+            expect(this.player1.amber).toBe(1);
+            this.player1.playUpgrade(this.brainStemAntenna, this.gamgee);
+            expect(this.gamgee.upgrades).toContain(this.brainStemAntenna);
+            this.player1.reap(this.vezymaThinkdrone);
+            expect(this.player1).toBeAbleToSelect(this.vezymaThinkdrone);
+            expect(this.player1).toBeAbleToSelect(this.ronnieWristclocks);
+            expect(this.player1).toBeAbleToSelect(this.gamgee);
+            this.player1.clickPrompt('Done');
+            expect(this.ronnieWristclocks.location).toBe('play area');
+            expect(this.vezymaThinkdrone.location).toBe('play area');
+            expect(this.gamgee.location).toBe('play area');
+            expect(this.player1.amber).toBe(2);
+        });
+
         it('should archive a card after reap', function () {
             this.player1.play(this.phloxemSpike);
             expect(this.player1.amber).toBe(1);
             this.player1.playUpgrade(this.brainStemAntenna, this.gamgee);
             expect(this.gamgee.upgrades).toContain(this.brainStemAntenna);
             this.player1.reap(this.vezymaThinkdrone);
-            expect(this.player1).toHavePrompt('Choose a card');
             expect(this.player1).toBeAbleToSelect(this.vezymaThinkdrone);
             expect(this.player1).toBeAbleToSelect(this.ronnieWristclocks);
             expect(this.player1).toBeAbleToSelect(this.gamgee);
             this.player1.clickCard(this.ronnieWristclocks);
-            expect(this.player1).toHavePrompt(
-                'Do you wish to archive a friendly creature or artifact?'
-            );
-            this.player1.clickPrompt('Yes');
             expect(this.ronnieWristclocks.location).toBe('archives');
             expect(this.vezymaThinkdrone.location).toBe('play area');
             expect(this.gamgee.location).toBe('play area');
@@ -49,10 +60,6 @@ describe('Vezyma Thinkdrone', function () {
             expect(this.player1).toBeAbleToSelect(this.gamgee);
             expect(this.player1).toBeAbleToSelect(this.scowlyCaper);
             this.player1.clickCard(this.scowlyCaper);
-            expect(this.player1).toHavePrompt(
-                'Do you wish to archive a friendly creature or artifact?'
-            );
-            this.player1.clickPrompt('Yes');
             expect(this.scowlyCaper.location).toBe('archives');
             expect(this.vezymaThinkdrone.location).toBe('play area');
             expect(this.gamgee.location).toBe('play area');
