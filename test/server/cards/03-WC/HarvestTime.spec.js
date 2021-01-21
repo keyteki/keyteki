279fa1ba7b62f05b45c6f2284872afdede7748a7
Romano Silva <romano.magacho@gmail.com>
2021-01-21 08:49:55 -0300

    Fixes Harvest Time prompt (#2189)
    * Fixes Harvest Time prompt
    It fixes a few things:
    - Prompt closing when no selection
    - preThenEvents not properly set in case of no events and alwaysTriggers: true
    
    Fixes #1919
    Fixes #2064
    
    * Fixes Harvest Time looking for owner and not controller
    
    Co-authored-by: romano-silva <romano.silva@hpe.com>

--- test/server/cards/03-WC/HarvestTime.spec.js
+++ test/server/cards/03-WC/HarvestTime.spec.js
@@ -5,14 +5,17 @@ describe('Harvest Time', function () {
                 player1: {
                     amber: 2,
                     house: 'dis',
-                    hand: ['harvest-time'],
+                    hand: ['harvest-time', 'collar-of-subordination'],
                     inPlay: ['gamgee', 'bad-penny', 'troll']
                 },
                 player2: {
                     amber: 2,
-                    inPlay: ['knuckles-bolton', 'pip-pip']
+                    inPlay: ['knuckles-bolton', 'pip-pip', 'lamindra']
                 }
             });
+
+            this.player1.playUpgrade(this.collarOfSubordination, this.lamindra);
+            this.player1.clickPrompt('left');
         });
 
         describe('when the card is played', function () {
@@ -32,13 +35,14 @@ describe('Harvest Time', function () {
                 it('should purge all creatures with that trait', function () {
                     expect(this.gamgee.location).toBe('purged');
                     expect(this.badPenny.location).toBe('purged');
+                    expect(this.lamindra.location).toBe('purged');
                     expect(this.knucklesBolton.location).toBe('purged');
                     expect(this.troll.location).not.toBe('purged');
                     expect(this.pipPip.location).not.toBe('purged');
                 });
 
                 it('should give each player 1 amber for every card purged', function () {
-                    expect(this.player1.amber).toBe(4);
+                    expect(this.player1.amber).toBe(5);
                     expect(this.player2.amber).toBe(3);
                 });
             });
@@ -51,6 +55,7 @@ describe('Harvest Time', function () {
 
                 it('should purge all creatures with that trait', function () {
                     expect(this.gamgee.location).toBe('play area');
+                    expect(this.lamindra.location).toBe('purged');
                     expect(this.badPenny.location).toBe('purged');
                     expect(this.knucklesBolton.location).toBe('purged');
                     expect(this.troll.location).not.toBe('purged');
@@ -59,7 +64,7 @@ describe('Harvest Time', function () {
 
                 it('should not give 1A for warded creatures', function () {
                     expect(this.gamgee.tokens.ward).toBeUndefined();
-                    expect(this.player1.amber).toBe(3);
+                    expect(this.player1.amber).toBe(4);
                     expect(this.player2.amber).toBe(3);
                 });
             });
