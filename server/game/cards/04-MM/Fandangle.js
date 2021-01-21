580c457919d6d8fdc4aa2bdbda68ebaf2570ef62
Romano Silva <romano.magacho@gmail.com>
2021-01-21 08:47:34 -0300

    fixes maverick fandangle entering ready (#2172)

--- server/game/cards/04-MM/Fandangle.js
+++ server/game/cards/04-MM/Fandangle.js
@@ -8,6 +8,13 @@ class Fandangle extends Card {
             match: (card) => card.type === 'creature' && !card.hasHouse('untamed'),
             effect: ability.effects.entersPlayReady()
         });
+
+        this.persistentEffect({
+            location: 'any',
+            condition: (context) =>
+                !context.source.hasHouse('untamed') && context.source.controller.amber >= 4,
+            effect: ability.effects.entersPlayReady()
+        });
     }
 }
 
