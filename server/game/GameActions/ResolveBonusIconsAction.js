179e1695a5596566a1e96fcd57bdf9368a34f77a
romano-silva <romano.silva@hpe.com>
2021-01-25 16:27:40 -0300

    Fixes "after next card is played" bugs Fixes #2235 Fixes #2227 Fixes #2137

--- server/game/GameActions/ResolveBonusIconsAction.js
+++ server/game/GameActions/ResolveBonusIconsAction.js
@@ -7,13 +7,96 @@ class ResolveBonusIconsAction extends CardGameAction {
         this.effectMsg = "resolve {0}'s bonus icons";
     }
 
+    resolveIcon(card, context, event, icon) {
+        if (icon === 'amber') {
+            context.game.actions
+                .gainAmber({ bonus: true })
+                .resolve(context.player, context.game.getFrameworkContext(context.player));
+            context.game.addMessage(
+                "{0} gains an amber due to {1}'s bonus icon",
+                context.player,
+                event.card
+            );
+        } else if (icon === 'draw') {
+            context.game.actions
+                .draw({ bonus: true })
+                .resolve(context.player, context.game.getFrameworkContext(context.player));
+            context.game.addMessage(
+                "{0} draws a card due to {1}'s bonus icon",
+                context.player,
+                event.card
+            );
+        } else if (icon === 'steal') {
+            if (context.player.opponent && context.player.opponent.amber > 0) {
+                context.game.actions
+                    .steal()
+                    .resolve(
+                        context.player.opponent,
+                        context.game.getFrameworkContext(context.player)
+                    );
+                context.game.addMessage(
+                    "{0} steals an amber due to {1}'s bonus icon",
+                    context.player,
+                    event.card
+                );
+            }
+        } else if (icon === 'capture') {
+            if (
+                context.player.opponent &&
+                context.player.opponent.amber > 0 &&
+                context.player.creaturesInPlay.length > 0
+            ) {
+                context.game.promptForSelect(context.player, {
+                    activePromptTitle: 'Choose a creature to capture amber due to bonus icon',
+                    cardType: 'creature',
+                    controller: 'self',
+                    source: card,
+                    onSelect: (player, card) => {
+                        context.game.actions
+                            .capture({ bonus: true })
+                            .resolve(card, context.game.getFrameworkContext(player));
+                        context.game.addMessage(
+                            "{0} captures an amber on {1} due to {2}'s bonus icon",
+                            player,
+                            card,
+                            event.card
+                        );
+                        return true;
+                    }
+                });
+            }
+        } else if (icon === 'damage') {
+            if (context.game.creaturesInPlay.length > 0) {
+                context.game.promptForSelect(context.player, {
+                    activePromptTitle: 'Choose a creature to damage due to bonus icon',
+                    source: card,
+                    cardType: 'creature',
+                    onSelect: (player, card) => {
+                        context.game.actions
+                            .dealDamage({ bonus: true })
+                            .resolve(card, context.game.getFrameworkContext(player));
+                        context.game.addMessage(
+                            "{0} deals 1 damage to {1} due to {2}'s bonus icon",
+                            player,
+                            card,
+                            event.card
+                        );
+                        return true;
+                    }
+                });
+            }
+        }
+    }
+
     getEvent(card, context) {
         return super.createEvent(
             'onResolveBonusIcons',
             { card: card, context: context },
             (event) => {
                 for (let icon of event.card.bonusIcons) {
-                    context.game.queueSimpleStep(() => {
+                    const resolveCount = card.sumEffects('resolveBonusIconsAdditionalTime') + 1;
+
+                    for (let rc = 0; rc < resolveCount; ++rc) {
                         let choices = [icon];
                         let mayResolveBonusIconsAsEffects = context.player.getEffects(
                             'mayResolveBonusIconsAs'
@@ -44,107 +127,18 @@ class ResolveBonusIconsAction extends CardGameAction {
                                 activePromptTitle:
                                     'How do you wish to resolve this ' + icon + ' icon?',
                                 choices: choices,
-                                handlers: choices.map((choice) => () => (icon = choice)),
+                                handlers: choices.map((choice) => () =>
+                                    this.resolveIcon(card, context, event, choice)
+                                ),
                                 context: context,
                                 source: card
                             });
+                        } else {
+                            context.game.queueSimpleStep(() => {
+                                this.resolveIcon(card, context, event, icon);
+                            });
                         }
-                    });
-
-                    context.game.queueSimpleStep(() => {
-                        if (icon === 'amber') {
-                            context.game.actions
-                                .gainAmber({ bonus: true })
-                                .resolve(
-                                    context.player,
-                                    context.game.getFrameworkContext(context.player)
-                                );
-                            context.game.addMessage(
-                                "{0} gains an amber due to {1}'s bonus icon",
-                                context.player,
-                                event.card
-                            );
-                        } else if (icon === 'draw') {
-                            context.game.actions
-                                .draw({ bonus: true })
-                                .resolve(
-                                    context.player,
-                                    context.game.getFrameworkContext(context.player)
-                                );
-                            context.game.addMessage(
-                                "{0} draws a card due to {1}'s bonus icon",
-                                context.player,
-                                event.card
-                            );
-                        } else if (icon === 'steal') {
-                            if (context.player.opponent && context.player.opponent.amber > 0) {
-                                context.game.actions
-                                    .steal()
-                                    .resolve(
-                                        context.player.opponent,
-                                        context.game.getFrameworkContext(context.player)
-                                    );
-                                context.game.addMessage(
-                                    "{0} steals an amber due to {1}'s bonus icon",
-                                    context.player,
-                                    event.card
-                                );
-                            }
-                        } else if (icon === 'capture') {
-                            if (
-                                context.player.opponent &&
-                                context.player.opponent.amber > 0 &&
-                                context.player.creaturesInPlay.length > 0
-                            ) {
-                                context.game.promptForSelect(context.player, {
-                                    activePromptTitle:
-                                        'Choose a creature to capture amber due to bonus icon',
-                                    cardType: 'creature',
-                                    controller: 'self',
-                                    source: card,
-                                    onSelect: (player, card) => {
-                                        context.game.actions
-                                            .capture({ bonus: true })
-                                            .resolve(
-                                                card,
-                                                context.game.getFrameworkContext(player)
-                                            );
-                                        context.game.addMessage(
-                                            "{0} captures an amber on {1} due to {2}'s bonus icon",
-                                            player,
-                                            card,
-                                            event.card
-                                        );
-                                        return true;
-                                    }
-                                });
-                            }
-                        } else if (icon === 'damage') {
-                            if (context.game.creaturesInPlay.length > 0) {
-                                context.game.promptForSelect(context.player, {
-                                    activePromptTitle:
-                                        'Choose a creature to damage due to bonus icon',
-                                    source: card,
-                                    cardType: 'creature',
-                                    onSelect: (player, card) => {
-                                        context.game.actions
-                                            .dealDamage({ bonus: true })
-                                            .resolve(
-                                                card,
-                                                context.game.getFrameworkContext(player)
-                                            );
-                                        context.game.addMessage(
-                                            "{0} deals 1 damage to {1} due to {2}'s bonus icon",
-                                            player,
-                                            card,
-                                            event.card
-                                        );
-                                        return true;
-                                    }
-                                });
-                            }
-                        }
-                    });
+                    }
                 }
             }
         );
--- test/server/cards/01-Core/SoftLanding.spec.js
+++ test/server/cards/01-Core/SoftLanding.spec.js
@@ -4,47 +4,14 @@ describe('Soft Landing', function () {
             this.setupTest({
                 player1: {
                     house: 'mars',
-                    hand: ['zorg', 'mindwarper', 'soft-landing']
-                },
-                player2: {
-                    inPlay: ['troll']
-                }
-            });
-            this.player1.play(this.softLanding);
-        });
-
-        it('should ready the next card played', function () {
-            this.player1.play(this.zorg);
-            expect(this.zorg.exhausted).toBe(false);
-            this.player1.play(this.mindwarper);
-            expect(this.mindwarper.exhausted).toBe(true);
-        });
-        /*
-            it('should stack', function() {
-                this.player1.moveCard(this.softLanding, 'hand');
-                this.player1.play(this.softLanding);
-                this.player1.play(this.zorg);
-                expect(this.zorg.exhausted).toBe(false);
-                this.player1.play(this.mindwarper);
-                expect(this.mindwarper.exhausted).toBe(false);
-            });
-            */
-        it('should not carry over to the following turn', function () {
-            this.player1.endTurn();
-            this.player2.clickPrompt('brobnar');
-            this.player2.endTurn();
-            this.player1.clickPrompt('mars');
-            this.player1.play(this.mindwarper);
-            expect(this.mindwarper.exhausted).toBe(true);
-        });
-    });
-
-    describe("Soft Landing's ability", function () {
-        beforeEach(function () {
-            this.setupTest({
-                player1: {
-                    house: 'mars',
-                    hand: ['zorg', 'mindwarper', 'soft-landing', 'john-smyth'],
+                    hand: [
+                        'zorg',
+                        'mindwarper',
+                        'soft-landing',
+                        'john-smyth',
+                        'commpod',
+                        'custom-virus'
+                    ],
                     inPlay: ['tunk']
                 },
                 player2: {
@@ -53,23 +20,54 @@ describe('Soft Landing', function () {
             });
         });
 
-        it('should ready the next card played after you click the prompt when you have another simultaneous ability', function () {
+        it('should ready the next creature played when it has another simultaneous ability', function () {
             this.player1.play(this.softLanding);
             this.player1.play(this.zorg);
-            this.player1.clickPrompt('Soft Landing');
+            expect(this.zorg.stunned).toBe(true);
             expect(this.zorg.exhausted).toBe(false);
             this.player1.play(this.mindwarper);
             expect(this.mindwarper.exhausted).toBe(true);
         });
 
-        it('should ready the next card played even if you have a prompt and choose to resolve the other trigger first', function () {
+        it('should ready the next creature played even when there is another trigger to resolve', function () {
             this.player1.fightWith(this.tunk, this.batdrone);
             expect(this.tunk.tokens.damage).toBe(1);
             this.player1.play(this.softLanding);
             this.player1.play(this.mindwarper);
-            this.player1.clickCard(this.tunk);
             expect(this.mindwarper.exhausted).toBe(false);
             expect(this.tunk.tokens.damage).toBe(undefined);
+        });
+
+        it('should ready the next artifact played', function () {
+            this.player1.play(this.softLanding);
+            this.player1.play(this.commpod);
+            expect(this.commpod.exhausted).toBe(false);
+            this.player1.play(this.customVirus);
+            expect(this.customVirus.exhausted).toBe(true);
+        });
+    });
+
+    describe("Soft Landing's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'shadows',
+                    hand: ['lamindra', 'murkens']
+                },
+                player2: {
+                    hand: ['troll', 'soft-landing']
+                }
+            });
+            this.player2.moveCard(this.softLanding, 'deck');
+        });
+
+        it('should not ready the creature that played it', function () {
+            this.player1.play(this.murkens);
+            this.player1.clickPrompt('Top of deck');
+            expect(this.softLanding.location).toBe('discard');
+            expect(this.murkens.exhausted).toBe(true);
+            this.player1.play(this.lamindra);
+            expect(this.lamindra.exhausted).toBe(false);
         });
     });
 });
