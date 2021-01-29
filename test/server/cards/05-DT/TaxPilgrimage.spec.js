aab0ec960793e2f5ee00a4658f6337b69b8c9fd8
romano-silva <romano.silva@hpe.com>
2021-01-29 11:20:00 -0300

    DT: Tax Pilgrimage

---
+++ test/server/cards/05-DT/TaxPilgrimage.spec.js
@@ -0,0 +1,183 @@
+describe('Tax Pilgrimage', function () {
+    describe("Tax Pilgrimage's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 1,
+                    house: 'sanctum',
+                    inPlay: ['lamindra', 'angry-mob'],
+                    hand: ['tax-pilgrimage', 'barrister-joya', 'challe-the-safeguard']
+                },
+                player2: {
+                    inPlay: ['murkens']
+                }
+            });
+        });
+
+        describe('when there are not creatures in play', function () {
+            beforeEach(function () {
+                this.player1.moveCard(this.lamindra, 'discard');
+                this.player1.moveCard(this.barristerJoya, 'discard');
+            });
+
+            it('should not capture any amber', function () {
+                expect(this.angryMob.amber).toBe(0);
+                expect(this.angryMob.amber).toBe(0);
+                expect(this.player2.amber).toBe(0);
+                expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            });
+        });
+
+        describe('when opponent has no amber', function () {
+            beforeEach(function () {
+                this.player1.play(this.barristerJoya);
+                this.player1.play(this.taxPilgrimage);
+            });
+
+            it('should not capture any amber', function () {
+                expect(this.angryMob.amber).toBe(0);
+                expect(this.angryMob.amber).toBe(0);
+                expect(this.player2.amber).toBe(0);
+                expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            });
+        });
+
+        describe('when opponent has 1 amber', function () {
+            beforeEach(function () {
+                this.player2.amber = 1;
+                this.player1.play(this.barristerJoya);
+                this.player1.play(this.taxPilgrimage);
+            });
+
+            describe('and a friendly creature is selected', function () {
+                beforeEach(function () {
+                    this.player1.clickCard(this.angryMob);
+                });
+
+                it('should capture 1 amber the selected creature', function () {
+                    expect(this.angryMob.amber).toBe(1);
+                    expect(this.player2.amber).toBe(0);
+                });
+
+                it('should not prompt to capture more ambers', function () {
+                    expect(this.barristerJoya.amber).toBe(0);
+                    expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+                });
+            });
+        });
+
+        describe('when opponent has 2 ambers', function () {
+            beforeEach(function () {
+                this.player2.amber = 2;
+            });
+
+            describe('and only 1 neighbor shares a house', function () {
+                beforeEach(function () {
+                    this.player1.play(this.barristerJoya);
+                    this.player1.play(this.taxPilgrimage);
+                });
+
+                describe('and a friendly creature is selected', function () {
+                    beforeEach(function () {
+                        this.player1.clickCard(this.angryMob);
+                    });
+
+                    it('should capture 1 amber the selected creature', function () {
+                        expect(this.angryMob.amber).toBe(1);
+                    });
+
+                    it('should not prompt to capture more ambers', function () {
+                        expect(this.barristerJoya.amber).toBe(1);
+                        expect(this.player2.amber).toBe(0);
+                        expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+                    });
+                });
+            });
+
+            describe('and 2 neighbors shares a house', function () {
+                beforeEach(function () {
+                    this.player1.play(this.barristerJoya);
+                    this.player1.play(this.challeTheSafeguard, true, true);
+                    this.player1.clickCard(this.angryMob);
+                    this.player1.play(this.taxPilgrimage);
+                });
+
+                describe('and a friendly creature is selected', function () {
+                    beforeEach(function () {
+                        this.player1.clickCard(this.angryMob);
+                    });
+
+                    it('should capture 1 amber on the selected creature', function () {
+                        expect(this.angryMob.amber).toBe(1);
+                        expect(this.player2.amber).toBe(1);
+                    });
+
+                    it('should prompt to capture amber on one of the neighbors', function () {
+                        expect(this.player1).toBeAbleToSelect(this.barristerJoya);
+                        expect(this.player1).toBeAbleToSelect(this.challeTheSafeguard);
+                        this.player1.clickCard(this.barristerJoya);
+                        expect(this.barristerJoya.amber).toBe(1);
+                        expect(this.challeTheSafeguard.amber).toBe(0);
+                        expect(this.player2.amber).toBe(0);
+                        expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+                    });
+                });
+            });
+        });
+
+        describe('when opponent has 3 ambers', function () {
+            beforeEach(function () {
+                this.player2.amber = 3;
+            });
+
+            describe('and only 1 neighbor shares a house', function () {
+                beforeEach(function () {
+                    this.player1.play(this.barristerJoya);
+                    this.player1.play(this.taxPilgrimage);
+                });
+
+                describe('and a friendly creature is selected', function () {
+                    beforeEach(function () {
+                        this.player1.clickCard(this.angryMob);
+                    });
+
+                    it('should capture 1 amber the selected creature', function () {
+                        expect(this.angryMob.amber).toBe(1);
+                    });
+
+                    it('should not prompt to capture more ambers', function () {
+                        expect(this.barristerJoya.amber).toBe(1);
+                        expect(this.player2.amber).toBe(1);
+                        expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+                    });
+                });
+            });
+
+            describe('and 2 neighbors shares a house', function () {
+                beforeEach(function () {
+                    this.player1.play(this.barristerJoya);
+                    this.player1.play(this.challeTheSafeguard, true, true);
+                    this.player1.clickCard(this.angryMob);
+                    this.player1.play(this.taxPilgrimage);
+                });
+
+                describe('and a friendly creature is selected', function () {
+                    beforeEach(function () {
+                        this.player1.clickCard(this.angryMob);
+                    });
+
+                    it('should capture 1 amber on the selected creature', function () {
+                        expect(this.angryMob.amber).toBe(1);
+                    });
+
+                    it('should capture ambers on both  neighbors', function () {
+                        expect(this.barristerJoya.amber).toBe(1);
+                        expect(this.challeTheSafeguard.amber).toBe(1);
+                        expect(this.player2.amber).toBe(0);
+                        expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+                    });
+                });
+            });
+        });
+    });
+});
