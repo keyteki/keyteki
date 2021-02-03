f897416227fc1bb5442d04cbacd3c7cf359205c6
romano-silva <romano.silva@hpe.com>
2021-02-02 22:55:08 -0300

    using switch/case

--- server/game/GameActions/ResolveBonusIconsAction.js
+++ server/game/GameActions/ResolveBonusIconsAction.js
@@ -7,84 +7,90 @@ class ResolveBonusIconsAction extends CardGameAction {
         this.effectMsg = "resolve {0}'s bonus icons";
     }
 
-    resolveIcon(card, context, event, icon) {
-        if (icon === 'amber') {
-            context.game.actions
-                .gainAmber({ bonus: true })
-                .resolve(context.player, context.game.getFrameworkContext(context.player));
-            context.game.addMessage(
-                "{0} gains an amber due to {1}'s bonus icon",
-                context.player,
-                event.card
-            );
-        } else if (icon === 'draw') {
-            context.game.actions
-                .draw({ bonus: true })
-                .resolve(context.player, context.game.getFrameworkContext(context.player));
-            context.game.addMessage(
-                "{0} draws a card due to {1}'s bonus icon",
-                context.player,
-                event.card
-            );
-        } else if (icon === 'steal') {
-            if (context.player.opponent && context.player.opponent.amber > 0) {
+    resolveIcon(context, event, icon) {
+        switch (icon) {
+            case 'amber':
                 context.game.actions
-                    .steal()
-                    .resolve(
-                        context.player.opponent,
-                        context.game.getFrameworkContext(context.player)
-                    );
+                    .gainAmber({ bonus: true })
+                    .resolve(context.player, context.game.getFrameworkContext(context.player));
                 context.game.addMessage(
-                    "{0} steals an amber due to {1}'s bonus icon",
+                    "{0} gains an amber due to {1}'s bonus icon",
                     context.player,
                     event.card
                 );
-            }
-        } else if (icon === 'capture') {
-            if (
-                context.player.opponent &&
-                context.player.opponent.amber > 0 &&
-                context.player.creaturesInPlay.length > 0
-            ) {
-                context.game.promptForSelect(context.player, {
-                    activePromptTitle: 'Choose a creature to capture amber due to bonus icon',
-                    cardType: 'creature',
-                    controller: 'self',
-                    source: card,
-                    onSelect: (player, card) => {
-                        context.game.actions
-                            .capture({ bonus: true })
-                            .resolve(card, context.game.getFrameworkContext(player));
-                        context.game.addMessage(
-                            "{0} captures an amber on {1} due to {2}'s bonus icon",
-                            player,
-                            card,
-                            event.card
+                break;
+            case 'draw':
+                context.game.actions
+                    .draw({ bonus: true })
+                    .resolve(context.player, context.game.getFrameworkContext(context.player));
+                context.game.addMessage(
+                    "{0} draws a card due to {1}'s bonus icon",
+                    context.player,
+                    event.card
+                );
+                break;
+            case 'steal':
+                if (context.player.opponent && context.player.opponent.amber > 0) {
+                    context.game.actions
+                        .steal()
+                        .resolve(
+                            context.player.opponent,
+                            context.game.getFrameworkContext(context.player)
                         );
-                        return true;
-                    }
-                });
-            }
-        } else if (icon === 'damage') {
-            if (context.game.creaturesInPlay.length > 0) {
-                context.game.promptForSelect(context.player, {
-                    activePromptTitle: 'Choose a creature to damage due to bonus icon',
-                    source: card,
-                    cardType: 'creature',
-                    onSelect: (player, card) => {
-                        context.game.actions
-                            .dealDamage({ bonus: true })
-                            .resolve(card, context.game.getFrameworkContext(player));
-                        context.game.addMessage(
-                            "{0} deals 1 damage to {1} due to {2}'s bonus icon",
-                            player,
-                            card,
-                            event.card
-                        );
-                        return true;
-                    }
-                });
-            }
+                    context.game.addMessage(
+                        "{0} steals an amber due to {1}'s bonus icon",
+                        context.player,
+                        event.card
+                    );
+                }
+                break;
+            case 'capture':
+                if (
+                    context.player.opponent &&
+                    context.player.opponent.amber > 0 &&
+                    context.player.creaturesInPlay.length > 0
+                ) {
+                    context.game.promptForSelect(context.player, {
+                        activePromptTitle: 'Choose a creature to capture amber due to bonus icon',
+                        cardType: 'creature',
+                        controller: 'self',
+                        source: event.card,
+                        onSelect: (player, card) => {
+                            context.game.actions
+                                .capture({ bonus: true })
+                                .resolve(card, context.game.getFrameworkContext(player));
+                            context.game.addMessage(
+                                "{0} captures an amber on {1} due to {2}'s bonus icon",
+                                player,
+                                card,
+                                event.card
+                            );
+                            return true;
+                        }
+                    });
+                }
+                break;
+            case 'damage':
+                if (context.game.creaturesInPlay.length > 0) {
+                    context.game.promptForSelect(context.player, {
+                        activePromptTitle: 'Choose a creature to damage due to bonus icon',
+                        source: event.card,
+                        cardType: 'creature',
+                        onSelect: (player, card) => {
+                            context.game.actions
+                                .dealDamage({ bonus: true })
+                                .resolve(card, context.game.getFrameworkContext(player));
+                            context.game.addMessage(
+                                "{0} deals 1 damage to {1} due to {2}'s bonus icon",
+                                player,
+                                card,
+                                event.card
+                            );
+                            return true;
+                        }
+                    });
+                }
+                break;
         }
     }
 
@@ -128,14 +134,14 @@ class ResolveBonusIconsAction extends CardGameAction {
                                     'How do you wish to resolve this ' + icon + ' icon?',
                                 choices: choices,
                                 handlers: choices.map((choice) => () =>
-                                    this.resolveIcon(card, context, event, choice)
+                                    this.resolveIcon(context, event, choice)
                                 ),
                                 context: context,
                                 source: card
                             });
                         } else {
                             context.game.queueSimpleStep(() => {
-                                this.resolveIcon(card, context, event, icon);
+                                this.resolveIcon(context, event, icon);
                             });
                         }
                     }
