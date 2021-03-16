fc34e117578977bdcea6acd99da6d3f58d3ef6ca
romano-silva <romano.silva@hpe.com>
2021-03-16 18:29:11 -0300

    checking for creatures

--- server/game/cards/05-DT/Submersible.js
+++ server/game/cards/05-DT/Submersible.js
@@ -4,9 +4,11 @@ class Submersible extends Card {
     //While the tide is high, each friendly Thief creature gains elusive.
     setupCardAbilities(ability) {
         this.persistentEffect({
-            condition: (context) => context.source.controller.isTideHigh(),
-            match: (card) => card.hasTrait('thief'),
-            effect: ability.effects.addKeyword({ elusive: 1 })
+            condition: (context) => context.player.isTideHigh(),
+            match: (card) => card.type === 'creature' && card.hasTrait('thief'),
+            effect: ability.effects.addKeyword({
+                elusive: 1
+            })
         });
     }
 }
