44cc3daf4bc789112f60145be758f49f2782578c
romano-silva <romano.silva@hpe.com>
2021-03-17 15:00:09 -0300

    Differentiate useCondition from action condition which makes card development much, much more intuitive

--- server/game/cards/05-DT/Hobnobber.js
+++ server/game/cards/05-DT/Hobnobber.js
@@ -4,9 +4,8 @@ class Hobnobber extends Card {
     //Action: If your opponent has 6A or more, steal 2A.
     setupCardAbilities(ability) {
         this.action({
-            gameAction: ability.actions.steal((context) => ({
-                amount: context.player.opponent && context.player.opponent.amber >= 6 ? 2 : 0
-            }))
+            condition: (context) => context.player.opponent && context.player.opponent.amber >= 6,
+            gameAction: ability.actions.steal({ amount: 2 })
         });
     }
 }
--- server/game/cards/05-DT/HobnobberEvilTwin.js
+++ server/game/cards/05-DT/HobnobberEvilTwin.js
@@ -4,9 +4,8 @@ class HobnobberEvilTwin extends Card {
     //Omni: If your opponent has exactly 1A, steal it.
     setupCardAbilities(ability) {
         this.omni({
-            gameAction: ability.actions.steal((context) => ({
-                amount: context.player.opponent && context.player.opponent.amber === 1 ? 1 : 0
-            }))
+            condition: (context) => context.player.opponent && context.player.opponent.amber === 1,
+            gameAction: ability.actions.steal()
         });
     }
 }
