ab8290e092fe84fb817ca761cd48817f6ac11dbf
romano-silva <romano.silva@hpe.com>
2021-03-17 23:40:12 -0300

    Ghosthawk should be optional. Fixes #2306

--- server/game/cards/03-WC/Ghosthawk.js
+++ server/game/cards/03-WC/Ghosthawk.js
@@ -3,6 +3,7 @@ const Card = require('../../Card.js');
 class Ghosthawk extends Card {
     setupCardAbilities(ability) {
         this.play({
+            optional: true,
             effect: 'reap with each of its neighbors in turn',
             target: {
                 cardCondition: (card, context) => context.source.neighbors.includes(card),
