79aa9617a9fd09a88e26d32e28dc468c3df45556
romano-silva <romano.silva@hpe.com>
2021-03-18 18:33:53 -0300

    more tests

---
+++ test/server/cards/05-DT/MagistraVita.spec.js
@@ -0,0 +1,60 @@
+describe('Magistra Vita', function () {
+    describe("Magistra Vita's play ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 4,
+                    house: 'saurian',
+                    inPlay: ['shooler', 'senator-shrix'],
+                    hand: ['magistra-vita']
+                },
+                player2: {
+                    amber: 4,
+                    inPlay: ['murkens']
+                }
+            });
+
+            this.player1.play(this.magistraVita);
+        });
+
+        it('should be able to exalt and fight a non-saurian friendly creature', function () {
+            expect(this.player1).toBeAbleToSelect(this.shooler);
+            expect(this.player1).not.toBeAbleToSelect(this.magistraVita);
+            expect(this.player1).not.toBeAbleToSelect(this.senatorShrix);
+            expect(this.player1).not.toBeAbleToSelect(this.murkens);
+            this.player1.clickCard(this.shooler);
+            expect(this.player1.amber).toBe(5);
+            expect(this.shooler.tokens.amber).toBe(1);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+
+    describe("Magistra Vita's reap ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 4,
+                    house: 'saurian',
+                    inPlay: ['magistra-vita', 'shooler', 'senator-shrix']
+                },
+                player2: {
+                    amber: 4,
+                    inPlay: ['murkens', 'lamindra']
+                }
+            });
+
+            this.player1.reap(this.magistraVita);
+        });
+
+        it('should be able to exalt and fight a non-saurian friendly creature', function () {
+            expect(this.player1).toBeAbleToSelect(this.shooler);
+            expect(this.player1).not.toBeAbleToSelect(this.magistraVita);
+            expect(this.player1).not.toBeAbleToSelect(this.senatorShrix);
+            expect(this.player1).not.toBeAbleToSelect(this.murkens);
+            this.player1.clickCard(this.shooler);
+            expect(this.player1.amber).toBe(6);
+            expect(this.shooler.tokens.amber).toBe(1);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
