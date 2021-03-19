867c5dcc8d16b737a1732e4166f3136df6ea84b4
romano-silva <romano.silva@hpe.com>
2021-03-19 13:51:13 -0300

    Finished tests

--- server/game/cards/05-DT/AssertDominance.js
+++ server/game/cards/05-DT/AssertDominance.js
@@ -4,14 +4,19 @@ class AssertDominance extends Card {
     //Play: For the remainder of the turn, a friendly creature gains Skirmish. Ready and fight with that creature.
     setupCardAbilities(ability) {
         this.play({
-            gameAction: ability.actions.sequential([
-                ability.actions.cardLastingEffect({
-                    effect: ability.effects.addKeyword({
-                        skirmish: 1
-                    })
-                }),
-                ability.actions.sequential([ability.actions.ready(), ability.actions.fight()])
-            ])
+            target: {
+                controller: 'self',
+                cardType: 'creature',
+                gameAction: ability.actions.sequential([
+                    ability.actions.cardLastingEffect({
+                        effect: ability.effects.addKeyword({
+                            skirmish: 1
+                        })
+                    }),
+                    ability.actions.sequential([ability.actions.ready(), ability.actions.fight()])
+                ]),
+                effect: 'give skirmish to {0}, then ready and fight with it'
+            }
         });
     }
 }
---
+++ test/server/cards/05-DT/EasyMarks.spec.js
@@ -0,0 +1,32 @@
+describe('Easy Marks', function () {
+    describe("Easy Marks's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 1,
+                    house: 'shadows',
+                    inPlay: ['ancient-bear', 'snufflegator', 'bumblebird'],
+                    hand: ['easy-marks']
+                },
+                player2: {
+                    amber: 4,
+                    inPlay: ['murkens', 'faygin', 'shooler']
+                }
+            });
+
+            this.murkens.tokens.damage = 1;
+            this.shooler.tokens.damage = 1;
+            this.ancientBear.tokens.damage = 2;
+        });
+
+        it('should exalt each damaged enemy creature', function () {
+            this.player1.play(this.easyMarks);
+            expect(this.ancientBear.amber).toBe(0);
+            expect(this.snufflegator.amber).toBe(0);
+            expect(this.bumblebird.amber).toBe(0);
+            expect(this.murkens.amber).toBe(1);
+            expect(this.faygin.amber).toBe(0);
+            expect(this.shooler.amber).toBe(1);
+        });
+    });
+});
