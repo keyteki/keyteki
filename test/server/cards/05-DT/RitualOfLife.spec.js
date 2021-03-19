a13174fa7e844f339bd52280f254c8e82f979f1c
romano-silva <romano.silva@hpe.com>
2021-03-18 23:29:22 -0300

    final test cases

---
+++ test/server/cards/05-DT/RitualOfLife.spec.js
@@ -0,0 +1,72 @@
+describe('Ritual of Life', function () {
+    describe("Ritual of Life's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 2,
+                    house: 'untamed',
+                    inPlay: ['ritual-of-life'],
+                    hand: ['ancient-bear', 'tantadlin'],
+                    discard: ['dextre', 'archimedes', 'animator']
+                },
+                player2: {
+                    amber: 2,
+                    inPlay: ['murkens']
+                }
+            });
+        });
+
+        describe('when there are no friendly creatures in play', function () {
+            beforeEach(function () {
+                this.player1.useAction(this.ritualOfLife);
+            });
+
+            it('should not do anything', function () {
+                expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            });
+        });
+
+        describe('when there are friendly creatures in play', function () {
+            beforeEach(function () {
+                this.player1.play(this.ancientBear);
+                this.player1.play(this.tantadlin);
+                this.player1.useAction(this.ritualOfLife);
+            });
+
+            it('should be able to select friendly creatures', function () {
+                expect(this.player1).toBeAbleToSelect(this.ancientBear);
+                expect(this.player1).toBeAbleToSelect(this.tantadlin);
+                expect(this.player1).not.toBeAbleToSelect(this.ritualOfLife);
+                expect(this.player1).not.toBeAbleToSelect(this.murkens);
+            });
+
+            describe('and a creature is selected', function () {
+                beforeEach(function () {
+                    this.player1.clickCard(this.tantadlin);
+                });
+
+                it('the creature should be destroyed', function () {
+                    expect(this.tantadlin.location).toBe('discard');
+                });
+
+                it('should be able to select a different creature from discard', function () {
+                    expect(this.player1).not.toBeAbleToSelect(this.ancientBear);
+                    expect(this.player1).not.toBeAbleToSelect(this.tantadlin);
+                    expect(this.player1).not.toBeAbleToSelect(this.animator);
+                    expect(this.player1).toBeAbleToSelect(this.dextre);
+                    expect(this.player1).toBeAbleToSelect(this.archimedes);
+                });
+
+                describe('and a creature from discard is selected', function () {
+                    beforeEach(function () {
+                        this.player1.clickCard(this.archimedes);
+                    });
+
+                    it('the creature should be returned to hand', function () {
+                        expect(this.archimedes.location).toBe('hand');
+                    });
+                });
+            });
+        });
+    });
+});
---
+++ test/server/cards/05-DT/Scooped.spec.js
@@ -0,0 +1,64 @@
+describe('Scooped', function () {
+    describe("Scooped's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 2,
+                    house: 'shadows',
+                    inPlay: ['armsmaster-molina', 'tantadlin', 'ancient-bear', 'dew-faerie'],
+                    hand: ['scooped']
+                },
+                player2: {
+                    amber: 2,
+                    inPlay: ['murkens', 'troll', 'lamindra'],
+                    hand: ['wipe-clear']
+                }
+            });
+        });
+
+        it('should deal 2D to a friendly creature and not capture if destroyed', function () {
+            this.player1.play(this.scooped);
+            this.player1.clickCard(this.dewFaerie);
+            expect(this.player1.amber).toBe(3);
+            expect(this.player2.amber).toBe(2);
+            expect(this.dewFaerie.location).toBe('discard');
+        });
+
+        it('should deal 2D to a friendly creature and capture from own side if not destroyed', function () {
+            this.player1.play(this.scooped);
+            this.player1.clickCard(this.tantadlin);
+            expect(this.player1.amber).toBe(2);
+            expect(this.player2.amber).toBe(2);
+            expect(this.tantadlin.tokens.damage).toBe(2);
+            expect(this.tantadlin.tokens.amber).toBe(1);
+        });
+
+        it('should deal 2D to an enemy creature and not capture if destroyed', function () {
+            this.player1.play(this.scooped);
+            this.player1.clickCard(this.lamindra);
+            expect(this.player1.amber).toBe(3);
+            expect(this.player2.amber).toBe(2);
+            expect(this.lamindra.location).toBe('discard');
+        });
+
+        it('should deal 2D to an enemy creature and capture from their side if not destroyed', function () {
+            this.player1.play(this.scooped);
+            this.player1.clickCard(this.troll);
+            expect(this.player1.amber).toBe(3);
+            expect(this.player2.amber).toBe(1);
+            expect(this.troll.tokens.damage).toBe(2);
+            expect(this.troll.tokens.amber).toBe(1);
+        });
+
+        it('should deal 2D to an enemy creature and capture from their side if not destroyed due to ward', function () {
+            this.lamindra.ward();
+            this.player1.play(this.scooped);
+            this.player1.clickCard(this.lamindra);
+            expect(this.player1.amber).toBe(3);
+            expect(this.player2.amber).toBe(1);
+            expect(this.lamindra.tokens.damage).toBeUndefined();
+            expect(this.lamindra.location).toBe('play area');
+            expect(this.lamindra.tokens.amber).toBe(1);
+        });
+    });
+});
