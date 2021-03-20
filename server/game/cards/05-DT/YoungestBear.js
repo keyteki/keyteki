caf0d957c3c918993c07ef32894c9665b8c4cb6d
Ryan Christianson <ryanch@gmail.com>
2021-03-19 17:18:30 -0700

    implemented Youngest Bear with test, cleaned up code.

--- server/game/cards/05-DT/YoungestBear.js
+++ server/game/cards/05-DT/YoungestBear.js
@@ -15,50 +15,6 @@ class YoungestBear extends Card {
     }
 }
 
-/*
-
-class YoungestBear extends Card {
-
-
-    setupCardAbilities(ability) {
-        this.reap({
-            target: {
-                cardType: 'creature',
-                controller: 'self',
-                cardCondition: (card) => card.exhausted === false,
-                gameAction: ability.actions.reap()
-            }
-        });
-    }
-
-    
-    setupCardAbilities(ability) {
-        this.reap({
-            target: {
-                cardType: 'creature',
-                controller: 'self',
-                cardCondition: (card,context) => context.source.neighbors.includes(card),
-                gameAction: ability.actions.reap()
-            }
-        });
-    }
-
-
-
-    setupCardAbilities(ability) {
-        this.reap({
-            gameAction: ability.actions.reap((context) => ({
-                target: context.player.creaturesInPlay.filter((card) =>
-                    this.neighbors.includes(card)
-                )
-            }))
-        });
-    }
-    
-}
-
-*/
-
 YoungestBear.id = 'youngest-bear';
 
 module.exports = YoungestBear;
