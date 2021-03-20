937601f82b80fcb00d14029890f2b951f6ab660d
Ryan Christianson <ryanch@gmail.com>
2021-03-20 08:53:47 -0700

    implelmented mushroom pig and evil twin

---
+++ server/game/cards/05-DT/MushroomPig.js
@@ -0,0 +1,29 @@
+const Card = require('../../Card.js');
+
+class MushroomPig extends Card {
+    //Reap: Give this creature a +1 power counter. You may remove all +1 power counters from this creature. If you do, deal 1D to each other creature for each +1 power counter removed this way.
+    setupCardAbilities(ability) {
+        this.reap({
+            gameAction: ability.actions.addPowerCounter((context) => {
+                context.source;
+            }),
+            then: {
+                may: 'Remove all power tokens',
+                alwaysTriggers: true,
+                gameAction: ability.actions.removePowerCounter({ all: true }),
+                then: {
+                    gameAction: ability.actions.dealDamage((context) => ({
+                        amount: context.preThenEvent.amount,
+                        target: context.game.creaturesInPlay.filter(
+                            (card) => card !== context.source
+                        )
+                    }))
+                }
+            }
+        });
+    }
+}
+
+MushroomPig.id = 'mushroom-pig';
+
+module.exports = MushroomPig;
