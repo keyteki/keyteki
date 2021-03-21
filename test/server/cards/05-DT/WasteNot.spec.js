9914a0981f81a2e5137a3cb9f0ba436ab5391a29
Ryan Christianson <ryanch@gmail.com>
2021-03-20 17:10:51 -0700

    added test case for gub with flank creatures

--- test/server/cards/05-DT/WasteNot.spec.js
+++ test/server/cards/05-DT/WasteNot.spec.js
@@ -6,11 +6,11 @@ describe('Waste Not', function () {
                     house: 'untamed',
                     amber: 1,
                     hand: ['waste-not'],
-                    inPlay: ['flaxia', 'urchin']
+                    inPlay: ['flaxia', 'gub', 'urchin']
                 },
                 player2: {
                     amber: 4,
-                    inPlay: ['gub', 'krump']
+                    inPlay: ['krump']
                 }
             });
         });
@@ -19,7 +19,7 @@ describe('Waste Not', function () {
             this.player1.play(this.wasteNot);
             expect(this.player1).toBeAbleToSelect(this.flaxia);
             expect(this.player1).toBeAbleToSelect(this.urchin);
-            expect(this.player1).not.toBeAbleToSelect(this.gub);
+            expect(this.player1).toBeAbleToSelect(this.gub);
             expect(this.player1).not.toBeAbleToSelect(this.krump);
         });
 
@@ -27,13 +27,14 @@ describe('Waste Not', function () {
             this.player1.play(this.wasteNot);
             expect(this.player1).toBeAbleToSelect(this.flaxia);
             expect(this.player1).toBeAbleToSelect(this.urchin);
-            expect(this.player1).not.toBeAbleToSelect(this.gub);
+            expect(this.player1).toBeAbleToSelect(this.gub);
             expect(this.player1).not.toBeAbleToSelect(this.krump);
 
             expect(this.player1.hand.length).toBe(0);
             this.player1.clickCard(this.flaxia);
             expect(this.flaxia.location).toBe('discard');
             expect(this.urchin.location).toBe('play area');
+            expect(this.gub.location).toBe('play area');
             expect(this.player1.hand.length).toBe(2);
         });
 
@@ -43,7 +44,18 @@ describe('Waste Not', function () {
             this.player1.clickCard(this.urchin);
             expect(this.urchin.location).toBe('discard');
             expect(this.flaxia.location).toBe('play area');
+            expect(this.gub.location).toBe('play area');
             expect(this.player1.hand.length).toBe(1);
+        });
+
+        it('should destroy gub with flanks and draw 3 cards', function () {
+            this.player1.play(this.wasteNot);
+            expect(this.player1.hand.length).toBe(0);
+            this.player1.clickCard(this.gub);
+            expect(this.gub.location).toBe('discard');
+            expect(this.urchin.location).toBe('play area');
+            expect(this.flaxia.location).toBe('play area');
+            expect(this.player1.hand.length).toBe(3);
         });
     });
 });
