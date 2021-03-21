3cf0b99e60050592ef372463679c64bb14c45494
skyjedi@gmail.com <4tX7wP7kYh*8>
2021-03-21 13:18:14 -0500

    DT: Brachiosaurus, ForeignLegion, HastatusRaptor

---
+++ server/game/cards/05-DT/HastatusRaptor.js
@@ -0,0 +1,26 @@
+const Card = require('../../Card.js');
+
+class HastatusRaptor extends Card {
+    //Before Fight: You may exalt $this. If you do, deal 1D to the creature $this fights for each A on friendly $thiss.
+    setupCardAbilities(ability) {
+        this.beforeFight({
+            optional: true,
+            gameAction: ability.actions.exalt(),
+            then: (preThenContext) => ({
+                gameAction: ability.actions.dealDamage((context) => {
+                    let totalAmber = context.player.creaturesInPlay
+                        .filter((card) => card.id === 'hastatus-raptor')
+                        .reduce((a, b) => a + b.tokens.amber, 0);
+                    return {
+                        amount: totalAmber,
+                        target: preThenContext.event.card
+                    };
+                })
+            })
+        });
+    }
+}
+
+HastatusRaptor.id = 'hastatus-raptor';
+
+module.exports = HastatusRaptor;
