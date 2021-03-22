8d345da87eabecaca29976409148a3125fc13150
romano-silva <romano.silva@hpe.com>
2021-03-22 10:13:19 -0300

    More test cases

--- server/game/cards/05-DT/ComOfficerGross.js
+++ server/game/cards/05-DT/ComOfficerGross.js
@@ -5,10 +5,10 @@ class ComOfficerGross extends Card {
     //Fight/Reap: Give a +1 power token to each creature between Com. Officer Gross and Com. Officer Hings.
     setupCardAbilities(ability) {
         this.play({
-            optional: true,
             effect: 'search for Com. Officer Hings and add it to their hand',
             gameAction: ability.actions.search({
                 cardName: 'Com. Officer Hings?',
+                location: ['deck'],
                 amount: 1
             })
         });
@@ -18,24 +18,22 @@ class ComOfficerGross extends Card {
             target: {
                 controller: 'self',
                 cardType: 'creature',
-                cardCondition: (card) => card.name === 'Com. Officer Hings?',
-                gameAction: ability.actions.addPowerCounter((context) => {
-                    if (!context.target) {
-                        return {
-                            target: []
-                        };
-                    }
-                    let creaturesInPlay = context.player.creaturesInPlay;
-                    let idxS = creaturesInPlay.indexOf(context.source);
-                    let idxT = creaturesInPlay.indexOf(context.target);
+                cardCondition: (card) => card.name === 'Com. Officer Hings?'
+            },
+            gameAction: ability.actions.addPowerCounter((context) => {
+                if (!context.target) {
                     return {
-                        target: creaturesInPlay.slice(
-                            Math.min(idxS, idxT) + 1,
-                            Math.max(idxS, idxT) - 1
-                        )
+                        target: []
                     };
-                })
-            }
+                }
+                let creaturesInPlay = context.player.creaturesInPlay;
+                let idxS = creaturesInPlay.indexOf(context.source);
+                let idxT = creaturesInPlay.indexOf(context.target);
+                let targets = creaturesInPlay.slice(Math.min(idxS, idxT) + 1, Math.max(idxS, idxT));
+                return {
+                    target: targets
+                };
+            })
         });
     }
 }
--- server/game/cards/05-DT/ComOfficerHings.js
+++ server/game/cards/05-DT/ComOfficerHings.js
@@ -5,31 +5,34 @@ class ComOfficerHings extends Card {
     //Fight/Reap: Draw 1 card for each creature between Com. Officer Gross and Com. Officer Hings.
     setupCardAbilities(ability) {
         this.play({
-            optional: true,
             effect: 'search for Com. Officer Hings and add it to their hand',
             gameAction: ability.actions.search({
                 cardName: 'Com. Officer Gross?',
+                location: ['deck'],
                 amount: 1
             })
         });
+
         this.fight({
             reap: true,
             target: {
                 controller: 'self',
                 cardType: 'creature',
-                cardCondition: (card) => card.name === 'Com. Officer Gross?',
-                gameAction: ability.actions.draw((context) => {
-                    if (!context.target) {
-                        return { amount: 0 };
-                    }
-                    let creaturesInPlay = context.player.creaturesInPlay;
-                    let idxS = creaturesInPlay.indexOf(context.source);
-                    let idxT = creaturesInPlay.indexOf(context.target);
-                    return {
-                        amount: Math.max(idxS, idxT) - Math.min(idxS, idxT) - 1
-                    };
-                })
-            }
+                cardCondition: (card) => card.name === 'Com. Officer Gross?'
+            },
+            gameAction: ability.actions.draw((context) => {
+                if (!context.target) {
+                    return { amount: 0 };
+                }
+                let creaturesInPlay = context.player.creaturesInPlay;
+                let idxS = creaturesInPlay.indexOf(context.source);
+                let idxT = creaturesInPlay.indexOf(context.target);
+                let amount = Math.max(idxS, idxT) - Math.min(idxS, idxT) - 1;
+                return {
+                    target: context.player,
+                    amount: amount
+                };
+            })
         });
     }
 }
