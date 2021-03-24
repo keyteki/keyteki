9893f5b39a7458506389395f2a572f947744423f
romano-silva <romano.silva@hpe.com>
2021-03-24 17:22:28 -0300

    Last test

--- server/game/cards/05-DT/WidespreadCorruption.js
+++ server/game/cards/05-DT/WidespreadCorruption.js
@@ -1,24 +1,26 @@
 const Card = require('../../Card.js');
 
 class WidespreadCorruption extends Card {
+    // After a player gains by reaping, their opponent captures that onto a creature they control.
     setupCardAbilities(ability) {
         this.reaction({
             when: {
-                onModifyAmber: (event, context) => event.reap && context.player.opponent
+                onModifyAmber: (event, context) => event.reap && context.game.activePlayer.opponent
             },
             target: {
                 cardType: 'creature',
-                cardCondition: (card, context) => card.controller === context.player.opponent,
+                cardCondition: (card, context) =>
+                    card.controller === context.game.activePlayer.opponent,
                 gameAction: ability.actions.sequential([
                     ability.actions.capture((context) => ({
                         amount: context.event.amount,
-                        player: context.player
+                        player: context.game.activePlayer
                     })),
                     ability.actions.changeEvent((context) => ({
                         event: context.event,
                         amount:
-                            context.source.controller.opponent &&
-                            context.source.controller.opponent.anyEffect('captureFromPool')
+                            context.game.activePlayer &&
+                            context.game.activePlayer.anyEffect('captureFromPool')
                                 ? 1
                                 : 0,
                         cancel: false
