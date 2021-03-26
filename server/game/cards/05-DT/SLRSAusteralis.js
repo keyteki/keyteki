7ca7a7c3a4b7e0da794c23af71c5d3165be98c4f
romano-silva <romano.silva@hpe.com>
2021-03-26 11:44:25 -0300

    last test

--- server/game/cards/05-DT/SLRSAusteralis.js
+++ server/game/cards/05-DT/SLRSAusteralis.js
@@ -3,19 +3,20 @@ const Card = require('../../Card.js');
 class SLRSAusteralis extends Card {
     setupCardAbilities(ability) {
         // Action: Exhaust up to 3 friendly Logos creatures. For each creature exhausted this way, play the top card of your deck, one at a time.
-        this.play({
+        this.action({
             target: {
                 numCards: 3,
                 mode: 'upTo',
+                controller: 'self',
                 cardType: 'creature',
                 cardCondition: (card) => card.hasHouse('logos'),
                 gameAction: ability.actions.exhaust()
             },
             then: {
                 alwaysTriggers: true,
-                condition: (context) => context.preTheEvents.some((event) => !event.cancelled),
+                condition: (context) => context.preThenEvents.some((event) => !event.cancelled),
                 gameAction: ability.actions.sequentialForEach((context) => ({
-                    num: context.preTheEvents.filter((event) => !event.cancelled).length,
+                    num: context.preThenEvents.filter((event) => !event.cancelled).length,
                     action: ability.actions.playCard((context) => ({
                         revealOnIllegalTarget: true,
                         target: context.player.deck[0]
