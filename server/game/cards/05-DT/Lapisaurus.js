196e5bae3873a371f8395c52a540c8ce7da221cc
Romano Silva <romano.magacho@gmail.com>
2021-03-27 19:25:22 -0300

    Update Lapisaurus.js

--- server/game/cards/05-DT/Lapisaurus.js
+++ server/game/cards/05-DT/Lapisaurus.js
@@ -3,13 +3,11 @@ const Card = require('../../Card.js');
 class Lapisaurus extends Card {
     //Taunt. (This creature's neighbors cannot be attacked unless they have taunt.)
     //While attacking $this, enemy creatures gain, "Before Fight: Exalt this creature."
-
     setupCardAbilities(ability) {
         this.interrupt({
             when: {
                 onFight: (event, context) => event.attackerTarget === context.source
             },
-
             gameAction: ability.actions.cardLastingEffect((context) => ({
                 target: context.event.attacker,
                 targetController: 'any',
