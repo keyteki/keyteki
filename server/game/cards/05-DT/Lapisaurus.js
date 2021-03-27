d1763329fbc4d998bab20b45772a8a76229d106d
Ryan Christianson <ryanch@gmail.com>
2021-03-27 12:40:39 -0700

    implemented Lapisaurus

---
+++ server/game/cards/05-DT/Lapisaurus.js
@@ -0,0 +1,26 @@
+const Card = require('../../Card.js');
+
+class Lapisaurus extends Card {
+    //Taunt. (This creature's neighbors cannot be attacked unless they have taunt.)
+    //While attacking $this, enemy creatures gain, "Before Fight: Exalt this creature."
+
+    setupCardAbilities(ability) {
+        this.interrupt({
+            when: {
+                onFight: (event, context) => event.attackerTarget === context.source
+            },
+
+            gameAction: ability.actions.untilNextTurn((context) => ({
+                target: context.event.attacker,
+                targetController: 'any',
+                effect: ability.effects.gainAbility('beforeFight', {
+                    gameAction: ability.actions.exalt()
+                })
+            }))
+        });
+    }
+}
+
+Lapisaurus.id = 'lapisaurus';
+
+module.exports = Lapisaurus;
---
+++ test/server/cards/05-DT/Lapisaurus.spec.js
@@ -0,0 +1,114 @@
+describe('Lapisaurus', function () {
+    describe("Lapisaurus's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'saurian',
+                    amber: 1,
+                    hand: [],
+                    inPlay: ['lapisaurus', 'consul-primus', 'rhetor-gallim']
+                },
+                player2: {
+                    amber: 1,
+                    inPlay: ['mother', 'earthshaker', 'dust-pixie', 'daughter']
+                }
+            });
+        });
+
+        it('should not exault lapisaurus when attacking', function () {
+            this.player1.amber = 1;
+            this.player2.amber = 1;
+
+            expect(this.lapisaurus.tokens.amber).toBeUndefined();
+            this.player1.fightWith(this.lapisaurus, this.dustPixie);
+            this.player1.endTurn();
+            expect(this.lapisaurus.tokens.amber).toBeUndefined();
+
+            expect(this.player1.amber).toBe(1);
+            expect(this.player2.amber).toBe(1);
+        });
+
+        it('should not exault friendly creature when attacking', function () {
+            this.player1.amber = 1;
+            this.player2.amber = 1;
+
+            expect(this.rhetorGallim.tokens.amber).toBeUndefined();
+            expect(this.lapisaurus.tokens.amber).toBeUndefined();
+            this.player1.fightWith(this.rhetorGallim, this.dustPixie);
+            this.player1.endTurn();
+
+            expect(this.rhetorGallim.tokens.amber).toBeUndefined();
+            expect(this.lapisaurus.tokens.amber).toBeUndefined();
+            expect(this.dustPixie.location).toBe('discard');
+            expect(this.player1.amber).toBe(1);
+            expect(this.player2.amber).toBe(1);
+        });
+
+        it('should not exault enemy creature when attacking other creatures', function () {
+            this.player1.amber = 1;
+            this.player2.amber = 1;
+
+            expect(this.mother.location).toBe('play area');
+            expect(this.rhetorGallim.location).toBe('play area');
+
+            this.player1.endTurn();
+            this.player2.clickPrompt('logos');
+
+            expect(this.mother.tokens.amber).toBeUndefined();
+            expect(this.lapisaurus.tokens.amber).toBeUndefined();
+
+            this.player2.fightWith(this.mother, this.rhetorGallim);
+            this.player2.endTurn();
+
+            expect(this.lapisaurus.tokens.amber).toBeUndefined();
+            expect(this.mother.tokens.amber).toBeUndefined();
+            expect(this.rhetorGallim.location).toBe('discard');
+            expect(this.player1.amber).toBe(1);
+            expect(this.player2.amber).toBe(1);
+        });
+
+        it('should exault enemy creatures attacking it', function () {
+            this.player1.amber = 1;
+            this.player2.amber = 1;
+
+            this.player1.endTurn();
+            this.player2.clickPrompt('logos');
+
+            expect(this.mother.tokens.amber).toBeUndefined();
+            expect(this.lapisaurus.tokens.amber).toBeUndefined();
+
+            this.player2.fightWith(this.mother, this.lapisaurus);
+            this.player2.endTurn();
+
+            expect(this.lapisaurus.location).toBe('play area');
+            expect(this.mother.location).toBe('play area');
+            expect(this.mother.tokens.damage).toBe(4);
+
+            expect(this.lapisaurus.tokens.amber).toBeUndefined();
+            expect(this.mother.tokens.amber).toBe(1);
+            expect(this.player1.amber).toBe(1);
+            expect(this.player2.amber).toBe(1);
+        });
+
+        it('should exalt enemy creatures attacking it and gain amber if they die', function () {
+            this.player1.amber = 1;
+            this.player2.amber = 1;
+
+            this.player1.endTurn();
+            this.player2.clickPrompt('logos');
+
+            expect(this.daughter.tokens.amber).toBeUndefined();
+            expect(this.lapisaurus.tokens.amber).toBeUndefined();
+
+            this.player2.fightWith(this.daughter, this.lapisaurus);
+            this.player2.endTurn();
+
+            expect(this.lapisaurus.location).toBe('play area');
+            expect(this.daughter.location).toBe('discard');
+
+            expect(this.lapisaurus.tokens.amber).toBeUndefined();
+            expect(this.player1.amber).toBe(2);
+            expect(this.player2.amber).toBe(1);
+        });
+    });
+});
