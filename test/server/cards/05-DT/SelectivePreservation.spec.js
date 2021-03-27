2ca70934e7735b3446f5b09761d87f94f144e2f2
Ryan Christianson <ryanch@gmail.com>
2021-03-26 19:56:33 -0700

    card impl and test

--- test/server/cards/05-DT/SelectivePreservation.spec.js
+++ test/server/cards/05-DT/SelectivePreservation.spec.js
@@ -5,7 +5,7 @@ describe('Selective Preservation', function () {
                 player1: {
                     house: 'staralliance',
                     amber: 1,
-                    hand: ['selective-preservation', 'scout-pete'],
+                    hand: ['selective-preservation', 'scout-pete', 'chief-engineer-walls'],
                     inPlay: []
                 },
                 player2: {
@@ -18,7 +18,6 @@ describe('Selective Preservation', function () {
 
         it('should play cleanly with no board', function () {
             this.player1.play(this.selectivePreservation);
-            this.player1.clickPrompt('Done');
             this.player1.endTurn();
         });
 
@@ -26,12 +25,11 @@ describe('Selective Preservation', function () {
             this.player1.moveCard(this.scoutPete, 'play area');
             expect(this.scoutPete.location).toBe('play area');
             this.player1.play(this.selectivePreservation);
-            this.player1.clickPrompt('Done');
             this.player1.endTurn();
             expect(this.scoutPete.location).toBe('play area');
         });
 
-        xit('should play with two creatures with same power and only kill 1', function () {
+        it('should play with two creatures with same power and only kill 1', function () {
             this.player1.moveCard(this.scoutPete, 'play area');
             this.player2.moveCard(this.flamewakeShaman, 'play area');
             expect(this.scoutPete.location).toBe('play area');
@@ -48,73 +46,51 @@ describe('Selective Preservation', function () {
             expect(this.flamewakeShaman.location).toBe('discard');
         });
 
-        // examples repo
-        /*
-            it('turn ending test', function () {
-                this.player1.endTurn();
-                this.player2.clickPrompt('untamed');
-                this.player2.endTurn();
-                this.player1.forgeKey('Red');
-                this.player1.clickPrompt('untamed');
-            });
+        it('should play with two sets of power and leave 1 creature from each power set alive', function () {
+            this.player1.moveCard(this.scoutPete, 'play area');
+            this.player2.moveCard(this.flamewakeShaman, 'play area');
+            this.player1.moveCard(this.chiefEngineerWalls, 'play area');
+            this.player2.moveCard(this.bingleBangbang, 'play area');
 
-            it('creature token', function () {
-                expect(this.mookling.tokens.power).toBeUndefined();
-                this.mookling.addToken('power');
-                expect(this.mookling.tokens.power).toBe(1);
-                
-                expect(this.mookling.tokens.damage).toBeUndefined();
-                this.mookling.addToken('damage');
-                expect(this.mookling.tokens.damage).toBe(1);
-                
-                expect(this.mookling.tokens.amber).toBeUndefined();
-                this.mookling.addToken('amber');
-                expect(this.mookling.tokens.amber).toBe(1);
-            });
+            expect(this.scoutPete.location).toBe('play area');
+            expect(this.flamewakeShaman.location).toBe('play area');
+            expect(this.chiefEngineerWalls.location).toBe('play area');
+            expect(this.bingleBangbang.location).toBe('play area');
 
-            it('creature amber test', function () {
-                this.urchin.tokens.amber = 1;
-            });
+            this.player1.play(this.selectivePreservation);
 
-            it('location tests', function () {
-                expect(this.mother.location).toBe('discard');
-                expect(this.mother.location).toBe('hand');
-                expect(this.mother.location).toBe('deck');
-                expect(this.mother.location).toBe('play area');
-            });
+            expect(this.player1).toBeAbleToSelect(this.scoutPete);
+            expect(this.player1).toBeAbleToSelect(this.flamewakeShaman);
+            expect(this.player1).toBeAbleToSelect(this.chiefEngineerWalls);
+            expect(this.player1).toBeAbleToSelect(this.bingleBangbang);
+            this.player1.clickCard(this.scoutPete);
+            this.player1.clickCard(this.bingleBangbang);
+            this.player1.clickPrompt('Done');
 
-            it('game interation selection', function () {
-                expect(this.player1).not.toBeAbleToSelect(this.gub);
-                expect(this.player1).toBeAbleToSelect(this.flaxia);
-                this.player1.clickPrompt('Done');
-                this.player1.clickCard(this.larva);
-                expect(this.player1).toHavePromptButton('Done');
-                expect(this.player1).not.toHavePromptButton('Done');
-            });
+            this.player1.endTurn();
+            expect(this.scoutPete.location).toBe('play area');
+            expect(this.bingleBangbang.location).toBe('play area');
+            expect(this.chiefEngineerWalls.location).toBe('discard');
+            expect(this.flamewakeShaman.location).toBe('discard');
+        });
 
-            it('basic actions tests', function () {
-                this.player1.play(this.cocoon);
-                this.player1.useAction(this.cocoon);
-                this.player1.reap(this.cocoon);
-                this.player1.fight(this.cocoon);
-            });
+        it('should play with two creatures with same power and only kill 1 - with power tokens', function () {
+            this.player1.moveCard(this.scoutPete, 'play area');
+            this.player2.moveCard(this.bingleBangbang, 'play area');
+            expect(this.scoutPete.location).toBe('play area');
+            expect(this.bingleBangbang.location).toBe('play area');
+            this.bingleBangbang.addToken('power');
+            this.bingleBangbang.addToken('power');
 
-            it('player amber test', function () {
-                this.player1.amber = 2
-                expect(this.player1.amber).toBe(2);
-            });
+            this.player1.play(this.selectivePreservation);
 
-            it('tide test', function () {
-                this.player1.lowerTide();
-                expect(this.player1.isTideHigh()).toBe(false);
-                this.player1.raiseTide();
-            });
+            expect(this.player1).toBeAbleToSelect(this.scoutPete);
+            expect(this.player1).toBeAbleToSelect(this.bingleBangbang);
+            this.player1.clickCard(this.scoutPete);
 
-            it('moving cards test', function () {
-                this.player1.moveCard(this.butterfly, 'play area');
-                this.player1.moveCard(this.butterfly, 'discard');
-                this.player1.moveCard(this.butterfly, 'hand');
-            });
-            */
+            this.player1.endTurn();
+            expect(this.scoutPete.location).toBe('play area');
+            expect(this.bingleBangbang.location).toBe('discard');
+        });
     });
 });
