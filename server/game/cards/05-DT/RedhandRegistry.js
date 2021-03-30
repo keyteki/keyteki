9de988d4d478236739d957c615770dd2555e1abf
Ryan Christianson <ryanch@gmail.com>
2021-03-29 17:43:48 -0700

    updated to use untilEndOfMyNextTurn

--- server/game/cards/05-DT/RedhandRegistry.js
+++ server/game/cards/05-DT/RedhandRegistry.js
@@ -7,19 +7,10 @@ class RedhandRegistry extends Card {
             when: {
                 onStealAmber: (event, context) => event.player === context.player
             },
-            // this looks strange, but we need to create the skip forge event
-            // at the end of the owner of this cards turn so the opponent will start
-            // their turn and skip their key forge step.
-            gameAction: ability.actions.cardLastingEffect((context) => ({
-                duration: 'lastingEffect',
-                effect: ability.effects.delayedEffect({
-                    when: { onRoundEnded: () => context.player === context.game.activePlayer },
-                    gameAction: ability.actions.lastingEffect({
-                        targetController: 'opponent',
-                        effect: ability.effects.skipStep('key')
-                    })
-                })
-            }))
+            gameAction: ability.actions.untilEndOfMyNextTurn({
+                targetController: 'opponent',
+                effect: ability.effects.skipStep('key')
+            })
         });
     }
 }
