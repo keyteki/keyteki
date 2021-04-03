5e7d6324da9bb60f405a0b600f515643871e9b1e
Steve Cooke <steve@digitallydexterous.com>
2021-04-03 11:23:41 +0100

    Properly suppress event banner. Previous removal had also gotten rid of the "TCO The Crucible Online" header.

--- client/pages/Lobby.jsx
+++ client/pages/Lobby.jsx
@@ -85,25 +85,15 @@ const Lobby = () => {
             </SideBar>
             <div>
                 <Col sm={{ span: 10, offset: 1 }}>
-                    <div className='banner'>
+                    {/*<div className='banner'>
                         <a target='_blank' rel='noreferrer' href='https://www.thefinalswindle.com/'>
                             <img width='100%' src='/banner/swindle-event-banner.png' />
                         </a>
-                    </div>
+                    </div>*/}
                     <div className='main-header' />
                 </Col>
             </div>
-            <div>
-                {/* <Col sm={{ span: 10, offset: 1 }}>
-                    <a
-                        href='https://challonge.com/tournaments/signup/T0ee0ljEUy'
-                        target='_blank'
-                        rel='noreferrer'
-                    >
-                        <div className='event-banner' />
-                    </a>
-                </Col> */}
-            </div>
+
             {motd?.message && (
                 <div>
                     <Col sm={{ span: 10, offset: 1 }} className='banner'>
