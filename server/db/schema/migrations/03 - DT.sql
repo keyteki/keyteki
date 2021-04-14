21148e848158177bedecdb3f5ff8bf2f04ef80aa
Stuart Walsh <stu@ipng.org.uk>
2021-04-14 20:42:09 +0100

    Add migration script for DT Fix deck import for DT enhanced decks

---
+++ server/db/schema/migrations/03 - DT.sql
@@ -0,0 +1,2 @@
+INSERT INTO public."Expansions" ("Id", "ExpansionId", "Code", "Name") VALUES (6, 496, 'DT', 'Dark Tidings');
+INSERT INTO public."Houses" ("Id", "Code", "Name") VALUES (10, 'unfathomable', 'Unfathomable');
