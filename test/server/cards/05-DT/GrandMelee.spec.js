da2e555a17c57264cb870579cb67707c7da2412a
romano-silva <romano.silva@hpe.com>
2021-04-15 11:25:26 -0300

    Fixes Grand Melee and no neighbors

--- test/server/cards/05-DT/GrandMelee.spec.js
+++ test/server/cards/05-DT/GrandMelee.spec.js
@@ -20,12 +20,39 @@ describe('GrandMelee', function () {
             });
 
             it('destroys card that dont share a house with neighbor', function () {
+                expect(this.badPenny.location).toBe('hand');
+                expect(this.foozle.location).toBe('discard');
                 expect(this.bumpsy.location).toBe('discard');
                 expect(this.mother.location).toBe('play area');
-                expect(this.bumpsy.location).toBe('discard');
-                expect(this.badPenny.location).toBe('hand');
                 expect(this.helperBot.location).toBe('play area');
                 expect(this.alaka.location).toBe('discard');
+            });
+        });
+    });
+
+    describe('Action test', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'sanctum',
+                    inPlay: ['bulwark'],
+                    hand: ['grand-melee']
+                },
+                player2: {
+                    inPlay: ['bumpsy'],
+                    amber: 2
+                }
+            });
+        });
+
+        describe('plays card', function () {
+            beforeEach(function () {
+                this.player1.play(this.grandMelee);
+            });
+
+            it('destroys card that dont have neighbors', function () {
+                expect(this.bulwark.location).toBe('discard');
+                expect(this.bumpsy.location).toBe('discard');
             });
         });
     });
