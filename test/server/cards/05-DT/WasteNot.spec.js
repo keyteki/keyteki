91fd8d32eb349f415294f308d14eba0ca8acfdf5
romanogit <romano.magacho@gmail.com>
2021-04-19 18:01:56 -0300

    Fixes Waste Not and Ward

--- test/server/cards/05-DT/WasteNot.spec.js
+++ test/server/cards/05-DT/WasteNot.spec.js
@@ -38,6 +38,23 @@ describe('Waste Not', function () {
             expect(this.player1.hand.length).toBe(2);
         });
 
+        it('should remove a 4 power creature ward and still 2 draw cards', function () {
+            this.flaxia.ward();
+            this.player1.play(this.wasteNot);
+            expect(this.player1).toBeAbleToSelect(this.flaxia);
+            expect(this.player1).toBeAbleToSelect(this.urchin);
+            expect(this.player1).toBeAbleToSelect(this.gub);
+            expect(this.player1).not.toBeAbleToSelect(this.krump);
+
+            expect(this.player1.hand.length).toBe(0);
+            this.player1.clickCard(this.flaxia);
+            expect(this.flaxia.location).toBe('play area');
+            expect(this.flaxia.warded).toBe(false);
+            expect(this.urchin.location).toBe('play area');
+            expect(this.gub.location).toBe('play area');
+            expect(this.player1.hand.length).toBe(2);
+        });
+
         it('should destroy 1 power creature and 1 draw cards', function () {
             this.player1.play(this.wasteNot);
             expect(this.player1.hand.length).toBe(0);
