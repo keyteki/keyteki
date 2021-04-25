016f2130776d02e3be4e3fa2bf3e0a000e20f852
romanogit <romano.magacho@gmail.com>
2021-04-25 20:47:45 -0300

    Fix this in these cards

--- server/game/cards/03-WC/TheRedBaron.js
+++ server/game/cards/03-WC/TheRedBaron.js
@@ -3,14 +3,17 @@ const Card = require('../../Card.js');
 class TheRedBaron extends Card {
     setupCardAbilities(ability) {
         this.persistentEffect({
-            match: (card) => card === this && card.controller.keys.red,
+            match: (card, context) => card === context.source && card.controller.keys.red,
             effect: ability.effects.gainAbility('reap', {
                 gameAction: ability.actions.steal()
             })
         });
+
         this.persistentEffect({
-            match: (card) =>
-                card === this && card.controller.opponent && card.controller.opponent.keys.red,
+            match: (card, context) =>
+                card === context.source &&
+                card.controller.opponent &&
+                card.controller.opponent.keys.red,
             effect: ability.effects.addKeyword({ elusive: 1 })
         });
     }
