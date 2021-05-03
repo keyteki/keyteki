1630970a4b65279e8509e61659c57b9de370e00b
romanogit <romano.magacho@gmail.com>
2021-05-03 11:11:13 -0300

    test cases for SS and Smite

--- test/server/cards/02-AoA/Smite.spec.js
+++ test/server/cards/02-AoA/Smite.spec.js
@@ -45,6 +45,7 @@ describe('Smite', function () {
             expect(this.helperBot.hasToken('damage')).toBe(false);
         });
     });
+
     describe("Smite's ability with duma", function () {
         beforeEach(function () {
             this.setupTest({
@@ -70,6 +71,52 @@ describe('Smite', function () {
             expect(this.dumaTheMartyr.location).toBe('discard');
             expect(this.murmook.tokens.damage).toBe(2);
             expect(this.troll.tokens.damage).toBe(2);
+        });
+    });
+
+    describe("Smite's ability with Shadow Self", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'sanctum',
+                    hand: ['smite'],
+                    inPlay: ['skullion', 'shooler']
+                },
+                player2: {
+                    inPlay: ['urchin', 'shadow-self', 'dodger']
+                }
+            });
+        });
+
+        it('should destroy shadow-self when fighting it with a 7-power creature and there is one neighbor', function () {
+            this.player2.moveCard(this.urchin, 'discard');
+            this.player1.play(this.smite);
+            this.player1.clickCard(this.skullion);
+            this.player1.clickCard(this.shadowSelf);
+            expect(this.shadowSelf.location).toBe('discard');
+            expect(this.dodger.location).toBe('play area');
+            expect(this.skullion.location).toBe('play area');
+            expect(this.dodger.tokens.damage).toBeUndefined();
+            expect(this.skullion.tokens.damage).toBeUndefined();
+            this.player1.endTurn();
+        });
+
+        it('should destroy shadow-self when fighting it with a 5-power creature and there are two neighbors', function () {
+            this.player1.play(this.smite);
+            this.player1.clickCard(this.shooler);
+            this.player1.clickCard(this.shadowSelf);
+            this.player1.clickCard(this.shadowSelf); // shadow self redirect prompt
+            expect(this.player1).toBeAbleToSelect(this.urchin);
+            expect(this.player1).toBeAbleToSelect(this.dodger);
+            this.player1.clickCard(this.urchin);
+            expect(this.shadowSelf.location).toBe('discard');
+            expect(this.urchin.location).toBe('play area');
+            expect(this.dodger.location).toBe('play area');
+            expect(this.shooler.location).toBe('play area');
+            expect(this.urchin.tokens.damage).toBeUndefined();
+            expect(this.dodger.tokens.damage).toBeUndefined();
+            expect(this.shooler.tokens.damage).toBeUndefined();
+            this.player1.endTurn();
         });
     });
 });
