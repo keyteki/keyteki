dba17ca8a7658fd23b51e03f3cd4bfbda4a9e64a
romanogit <romano.magacho@gmail.com>
2021-05-18 10:23:06 -0300

    Fixes BiomeDiscovery  not being optional Fixes #2469

--- server/game/cards/05-DT/BiomeDiscovery.js
+++ server/game/cards/05-DT/BiomeDiscovery.js
@@ -4,27 +4,30 @@ class BiomeDiscovery extends Card {
     // (T) Play: If the tide is high, you may look at the top 2 cards of your deck and discard 1 of them. Otherwise, raise the tide.
     setupCardAbilities(ability) {
         this.play({
-            gameAction: [
-                ability.actions.discard((context) => ({
+            gameAction: ability.actions.conditional({
+                condition: (context) => !context.player.isTideHigh(),
+                trueGameAction: ability.actions.raiseTide()
+            }),
+            effect: '{1}',
+            effectArgs: (context) =>
+                !context.player.isTideHigh()
+                    ? 'raise the tide'
+                    : 'conditionally look at top 2 deck cards',
+            then: {
+                alwaysTriggers: true,
+                condition: (context) =>
+                    (!context.preThenEvent || context.preThenEvent.length === 0) &&
+                    context.player.deck.length > 0,
+                may: 'look at the top 2 deck cards',
+                gameAction: ability.actions.discard((context) => ({
                     promptWithHandlerMenu: {
                         activePromptTitle: 'Choose a card to discard',
                         message: '{0} uses {1} to discard {2}',
                         messageArgs: (cards) => [context.player, context.source, cards[0]],
-                        // this is a hack to remove all cards from being picked
-                        // when the tide is not high. causes this action to be a no-op
-                        // this was done because ability.actions.conditional and '
-                        // promptWithHandlerMenu were not playing nice together
-                        cards:
-                            context.player.deck.length > 0 && context.player.isTideHigh()
-                                ? context.player.deck.slice(0, 2)
-                                : []
+                        cards: context.player.deck.slice(0, 2)
                     }
-                })),
-                ability.actions.conditional({
-                    condition: (context) => context.player.isTideHigh(),
-                    falseGameAction: ability.actions.raiseTide()
-                })
-            ]
+                }))
+            }
         });
     }
 }
