19ddcde3e0dc70efffac97d844ee67424fd5e836
Romano Silva <romano.magacho@gmail.com>
2021-05-25 07:52:49 -0300

    Refactor Crazy Killing Machine to destroy simultaneously (#2620)

--- test/server/cards/01-Core/CrazyKillingMachine.spec.js
+++ test/server/cards/01-Core/CrazyKillingMachine.spec.js
@@ -4,59 +4,163 @@ describe('Crazy Killing Machine', function () {
             this.setupTest({
                 player1: {
                     house: 'logos',
-                    inPlay: ['crazy-killing-machine', 'dextre', 'raiding-knight'],
+                    inPlay: [
+                        'crazy-killing-machine',
+                        'dextre',
+                        'raiding-knight',
+                        'hallowed-blaster',
+                        'helper-bot'
+                    ],
                     discard: ['inspiration', 'ancient-bear']
                 },
                 player2: {
-                    inPlay: ['macis-asp', 'sequis'],
-                    discard: ['bad-penny']
+                    inPlay: ['macis-asp', 'sequis', 'hideaway-hole'],
+                    discard: ['bad-penny', 'burning-glare', 'zorg']
                 }
             });
+        });
+
+        it('should prompt the controller to destroy creatures or artifacts', function () {
             this.player1.player.moveCard(this.inspiration, 'deck');
             this.player2.player.moveCard(this.badPenny, 'deck');
-        });
 
-        it('should discard a card from both players deck', function () {
-            expect(this.inspiration.location).toBe('deck');
-            expect(this.badPenny.location).toBe('deck');
-            this.player1.clickCard(this.crazyKillingMachine);
-            this.player1.clickPrompt("Use this card's Action ability");
+            this.player1.useAction(this.crazyKillingMachine);
             expect(this.inspiration.location).toBe('discard');
             expect(this.badPenny.location).toBe('discard');
-        });
 
-        it('should prompt the controller to destroy creatures', function () {
-            this.player1.clickCard(this.crazyKillingMachine);
-            this.player1.clickPrompt("Use this card's Action ability");
-            expect(this.player1).toHavePrompt('Choose a sanctum card to destroy');
+            expect(this.player1).toHavePrompt('Choose a card');
             expect(this.player1).toBeAbleToSelect(this.raidingKnight);
             expect(this.player1).toBeAbleToSelect(this.sequis);
+            expect(this.player1).toBeAbleToSelect(this.hallowedBlaster);
             expect(this.player1).not.toBeAbleToSelect(this.macisAsp);
+            expect(this.player1).not.toBeAbleToSelect(this.hideawayHole);
             expect(this.player1).not.toBeAbleToSelect(this.dextre);
             this.player1.clickCard(this.sequis);
-            expect(this.sequis.location).toBe('discard');
-            expect(this.player1).toHavePrompt('Choose a shadows card to destroy');
+
+            expect(this.player1).toHavePrompt('Choose a card');
             expect(this.player1).not.toBeAbleToSelect(this.raidingKnight);
+            expect(this.player1).not.toBeAbleToSelect(this.sequis);
+            expect(this.player1).not.toBeAbleToSelect(this.hallowedBlaster);
+            expect(this.player1).toBeAbleToSelect(this.hideawayHole);
             expect(this.player1).toBeAbleToSelect(this.macisAsp);
             expect(this.player1).not.toBeAbleToSelect(this.dextre);
-            this.player1.clickCard(this.macisAsp);
-            expect(this.macisAsp.location).toBe('discard');
+            this.player1.clickCard(this.hideawayHole);
+
+            expect(this.sequis.location).toBe('discard');
+            expect(this.hideawayHole.location).toBe('discard');
             expect(this.crazyKillingMachine.location).toBe('play area');
-            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            this.player1.endTurn();
+        });
+
+        it('should prompt the controller to destroy creatures or artifacts of same house', function () {
+            this.player1.player.moveCard(this.inspiration, 'deck');
+            this.player2.player.moveCard(this.burningGlare, 'deck');
+
+            this.player1.useAction(this.crazyKillingMachine);
+            expect(this.inspiration.location).toBe('discard');
+            expect(this.burningGlare.location).toBe('discard');
+
+            expect(this.player1).toHavePrompt('Choose a card');
+            expect(this.player1).toBeAbleToSelect(this.raidingKnight);
+            expect(this.player1).toBeAbleToSelect(this.sequis);
+            expect(this.player1).toBeAbleToSelect(this.hallowedBlaster);
+            expect(this.player1).not.toBeAbleToSelect(this.macisAsp);
+            expect(this.player1).not.toBeAbleToSelect(this.hideawayHole);
+            expect(this.player1).not.toBeAbleToSelect(this.dextre);
+            this.player1.clickCard(this.sequis);
+
+            expect(this.player1).toHavePrompt('Choose a card');
+            expect(this.player1).toBeAbleToSelect(this.raidingKnight);
+            expect(this.player1).not.toBeAbleToSelect(this.sequis);
+            expect(this.player1).toBeAbleToSelect(this.hallowedBlaster);
+            expect(this.player1).not.toBeAbleToSelect(this.hideawayHole);
+            expect(this.player1).not.toBeAbleToSelect(this.macisAsp);
+            expect(this.player1).not.toBeAbleToSelect(this.dextre);
+            this.player1.clickCard(this.hallowedBlaster);
+
+            expect(this.sequis.location).toBe('discard');
+            expect(this.hallowedBlaster.location).toBe('discard');
+            expect(this.crazyKillingMachine.location).toBe('play area');
+            this.player1.endTurn();
         });
 
         it('should destroy itself if there is not a legal target for one of houses', function () {
             this.player1.player.moveCard(this.ancientBear, 'deck');
-            this.player1.clickCard(this.crazyKillingMachine);
-            this.player1.clickPrompt("Use this card's Action ability");
-            expect(this.player1).toHavePrompt('Choose a shadows card to destroy');
+            this.player2.player.moveCard(this.badPenny, 'deck');
+
+            this.player1.useAction(this.crazyKillingMachine);
+            expect(this.ancientBear.location).toBe('discard');
+            expect(this.badPenny.location).toBe('discard');
+
+            expect(this.player1).toHavePrompt('Choose a card');
             expect(this.player1).not.toBeAbleToSelect(this.raidingKnight);
-            expect(this.player1).toBeAbleToSelect(this.macisAsp);
             expect(this.player1).not.toBeAbleToSelect(this.sequis);
+            expect(this.player1).not.toBeAbleToSelect(this.hallowedBlaster);
+            expect(this.player1).toBeAbleToSelect(this.hideawayHole);
+            expect(this.player1).toBeAbleToSelect(this.macisAsp);
+            expect(this.player1).not.toBeAbleToSelect(this.dextre);
             this.player1.clickCard(this.macisAsp);
+
             expect(this.macisAsp.location).toBe('discard');
             expect(this.crazyKillingMachine.location).toBe('discard');
-            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            this.player1.endTurn();
+        });
+
+        it('should be able to choose itself', function () {
+            this.player1.player.moveCard(this.dextre, 'deck');
+            this.player2.player.moveCard(this.zorg, 'deck');
+
+            this.player1.useAction(this.crazyKillingMachine);
+            expect(this.player1).toBeAbleToSelect(this.crazyKillingMachine);
+            expect(this.player1).toBeAbleToSelect(this.helperBot);
+            this.player1.clickCard(this.crazyKillingMachine);
+            expect(this.dextre.location).toBe('discard');
+            expect(this.zorg.location).toBe('discard');
+
+            expect(this.crazyKillingMachine.location).toBe('discard');
+            this.player1.endTurn();
+        });
+
+        it('should destroy itself if there is not a legal target for both houses', function () {
+            this.player1.player.moveCard(this.ancientBear, 'deck');
+            this.player2.player.moveCard(this.zorg, 'deck');
+
+            this.player1.useAction(this.crazyKillingMachine);
+            expect(this.ancientBear.location).toBe('discard');
+            expect(this.zorg.location).toBe('discard');
+
+            expect(this.crazyKillingMachine.location).toBe('discard');
+            this.player1.endTurn();
+        });
+
+        it('should destroy itself if one of the decks is empty', function () {
+            this.player1.player.moveCard(this.inspiration, 'deck');
+            this.player2.player.deck = [];
+
+            this.player1.useAction(this.crazyKillingMachine);
+            expect(this.inspiration.location).toBe('discard');
+
+            expect(this.player1).toHavePrompt('Choose a card');
+            expect(this.player1).toBeAbleToSelect(this.raidingKnight);
+            expect(this.player1).toBeAbleToSelect(this.sequis);
+            expect(this.player1).toBeAbleToSelect(this.hallowedBlaster);
+            expect(this.player1).not.toBeAbleToSelect(this.hideawayHole);
+            expect(this.player1).not.toBeAbleToSelect(this.macisAsp);
+            expect(this.player1).not.toBeAbleToSelect(this.dextre);
+            this.player1.clickCard(this.sequis);
+
+            expect(this.sequis.location).toBe('discard');
+            expect(this.crazyKillingMachine.location).toBe('discard');
+            this.player1.endTurn();
+        });
+
+        it('should destroy itself if decks are empty', function () {
+            this.player1.player.deck = [];
+            this.player2.player.deck = [];
+
+            this.player1.useAction(this.crazyKillingMachine);
+            expect(this.crazyKillingMachine.location).toBe('discard');
+            this.player1.endTurn();
         });
     });
 });
