1a825e70eb6f6cc9ce324459efe3add789370f0d
Romano Silva <romano.magacho@gmail.com>
2021-05-31 16:44:22 -0300

    Automate Bear Flute to avoid manually clicking on the ancient bears (#2642)

--- test/server/cards/01-Core/BearFlute.spec.js
+++ test/server/cards/01-Core/BearFlute.spec.js
@@ -6,7 +6,7 @@ describe('Bear Flute', function () {
                     house: 'untamed',
                     inPlay: ['bear-flute'],
                     hand: ['cooperative-hunting', 'regrowth'],
-                    discard: ['ancient-bear', 'ancient-bear', 'halacor']
+                    discard: ['ancient-bear', 'ancient-bear', 'halacor', 'niffle-ape']
                 },
                 player2: {
                     inPlay: []
@@ -17,50 +17,37 @@ describe('Bear Flute', function () {
             this.ancientBear2 = this.player1.player.discard[1];
         });
 
-        it("should search for a bear when there isn't one in play", function () {
+        it("should search discard for a bear when there isn't one in play, and shuffle discard", function () {
             this.player1.useAction(this.bearFlute);
-            expect(this.player1).toHavePrompt('Bear Flute');
-            expect(this.player1).toBeAbleToSelect(this.ancientBear1);
-            this.player1.clickCard(this.ancientBear1);
-            this.player1.clickCard(this.ancientBear2);
-            this.player1.clickPrompt('Done');
+            expect(this.halacor.location).toBe('deck');
+            expect(this.niffleApe.location).toBe('deck');
             expect(this.ancientBear1.location).toBe('hand');
             expect(this.ancientBear2.location).toBe('hand');
+            expect(this.player1.discard.length).toBe(0);
+            this.player1.endTurn();
         });
 
         it("should search for a bear when there isn't one in play, but stop when there is one", function () {
             this.player1.useAction(this.bearFlute);
-            this.player1.clickCard(this.ancientBear1);
-            this.player1.clickCard(this.ancientBear2);
-            this.player1.clickPrompt('Done');
             this.player1.play(this.ancientBear1);
             this.player1.moveCard(this.ancientBear2, 'discard');
             this.bearFlute.exhausted = false;
             this.player1.useAction(this.bearFlute);
             this.player1.clickCard(this.ancientBear1);
-            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            expect(this.ancientBear2.location).toBe('discard');
+            this.player1.endTurn();
         });
 
-        it("should search deck for a bear when there isn't one in play", function () {
+        it("should search deck for a bear when there isn't one in play and shuffle discard", function () {
+            this.player1.moveCard(this.halacor, 'deck');
             this.player1.moveCard(this.ancientBear2, 'deck');
             this.player1.useAction(this.bearFlute);
-            expect(this.player1).toHavePrompt('Bear Flute');
-            expect(this.player1).toBeAbleToSelect(this.ancientBear1);
-            this.player1.clickCard(this.ancientBear1);
-            this.player1.clickCard(this.ancientBear2);
-            this.player1.clickPrompt('Done');
             expect(this.ancientBear1.location).toBe('hand');
             expect(this.ancientBear2.location).toBe('hand');
-        });
-
-        it('should shuffle the discard pile into the deck when it searches for a bear', function () {
-            this.player1.useAction(this.bearFlute);
-            expect(this.player1).toHavePrompt('Bear Flute');
-            expect(this.player1).toBeAbleToSelect(this.ancientBear1);
-            this.player1.clickCard(this.ancientBear1);
-            this.player1.clickPrompt('Done');
-            expect(this.ancientBear1.location).toBe('hand');
             expect(this.halacor.location).toBe('deck');
+            expect(this.niffleApe.location).toBe('deck');
+            expect(this.player1.discard.length).toBe(0);
+            this.player1.endTurn();
         });
 
         it('should heal a bear when there is one in play', function () {
@@ -77,7 +64,8 @@ describe('Bear Flute', function () {
             expect(this.player1).toBeAbleToSelect(this.ancientBear1);
             this.player1.clickCard(this.ancientBear1);
             expect(this.ancientBear1.hasToken('damage')).toBe(false);
-            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            expect(this.player1.discard.length).toBe(5);
+            this.player1.endTurn();
         });
 
         it('should not be usable when there is an undamaged bear on the field', function () {
@@ -90,7 +78,8 @@ describe('Bear Flute', function () {
             expect(this.player1).toHavePrompt('Bear Flute');
             expect(this.player1).toBeAbleToSelect(this.ancientBear1);
             this.player1.clickCard(this.ancientBear1);
-            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            expect(this.player1.discard.length).toBe(4);
+            this.player1.endTurn();
         });
 
         it('should not reshuffle when there is one in play', function () {
@@ -103,8 +92,9 @@ describe('Bear Flute', function () {
             expect(this.player1).toHavePrompt('Bear Flute');
             expect(this.player1).toBeAbleToSelect(this.ancientBear1);
             this.player1.clickCard(this.ancientBear1);
-            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
             expect(this.regrowth.location).toBe('discard');
+            expect(this.player1.discard.length).toBe(4);
+            this.player1.endTurn();
         });
     });
 });
