92ae9ca09583160cecafdefb1d3a65c564543ea0
romanogit <romano.magacho@gmail.com>
2021-06-14 18:08:41 -0300

    Forgot the AutoLegionary.js files :)

--- server/game/cards/03-WC/AutoLegionary.js
+++ server/game/cards/03-WC/AutoLegionary.js
@@ -3,19 +3,23 @@ const Card = require('../../Card.js');
 class AutoLegionary extends Card {
     setupCardAbilities(ability) {
         this.action({
+            condition: (context) => context.source.controller === context.game.activePlayer,
             gameAction: ability.actions.sequential([
                 ability.actions.cardLastingEffect((context) => ({
                     target: context.source,
                     duration: 'lastingEffect',
                     effect: [
                         ability.effects.changeType('creature'),
-                        ability.effects.canUse((card) => card === context.source)
+                        ability.effects.gainAbility('persistentEffect', {
+                            effect: ability.effects.canUse((card) => card === context.source)
+                        })
                     ].concat(
                         context.source.type === 'artifact' ? ability.effects.modifyPower(5) : []
                     )
                 })),
                 ability.actions.moveToFlank()
-            ])
+            ]),
+            effect: 'move to a flank and become a 5-power creature'
         });
     }
 }
