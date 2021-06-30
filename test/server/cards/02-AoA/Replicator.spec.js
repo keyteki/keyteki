1ccd9965223ca1a6d557d305dd0c07c352beffa7
romanogit <romano.magacho@gmail.com>
2021-06-30 16:25:52 -0300

    Fixes Replicator and Quant (and opponent's creature with target) Fixes #942 Fixes #2225

--- test/server/cards/02-AoA/Replicator.spec.js
+++ test/server/cards/02-AoA/Replicator.spec.js
@@ -5,11 +5,13 @@ describe('Replicator', function () {
                 player1: {
                     house: 'logos',
                     amber: 2,
-                    inPlay: ['replicator', 'gamgee', 'titan-mechanic']
+                    inPlay: ['replicator', 'gamgee', 'titan-mechanic'],
+                    hand: ['virtuous-works']
                 },
                 player2: {
                     amber: 5,
-                    inPlay: ['foozle', 'bingle-bangbang']
+                    inPlay: ['foozle', 'bingle-bangbang', 'quant'],
+                    hand: ['anger']
                 }
             });
         });
@@ -17,23 +19,30 @@ describe('Replicator', function () {
         it("should use Gamgee's reap effect", function () {
             this.player1.reap(this.replicator);
             this.player1.clickCard(this.gamgee);
-            expect(this.player1.player.amber).toBe(4);
-            expect(this.player2.player.amber).toBe(4);
+            expect(this.player1.amber).toBe(4);
+            expect(this.player2.amber).toBe(4);
         });
 
         it("should use Foozle's reap effect", function () {
             this.player1.fightWith(this.titanMechanic, this.bingleBangbang);
             this.player1.reap(this.replicator);
             this.player1.clickCard(this.foozle);
-            expect(this.player1.player.amber).toBe(4);
-            expect(this.player2.player.amber).toBe(5);
+            expect(this.player1.amber).toBe(4);
+            expect(this.player2.amber).toBe(5);
         });
 
         it("should fail Foozle's reap effect's condition", function () {
             this.player1.reap(this.replicator);
             this.player1.clickCard(this.foozle);
-            expect(this.player1.player.amber).toBe(3);
-            expect(this.player2.player.amber).toBe(5);
+            expect(this.player1.amber).toBe(3);
+            expect(this.player2.amber).toBe(5);
+        });
+
+        it("should use Quant's reap ability", function () {
+            this.player1.reap(this.replicator);
+            this.player1.clickCard(this.quant);
+            this.player1.play(this.virtuousWorks);
+            expect(this.player1.amber).toBe(6);
         });
     });
 
