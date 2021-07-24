8434cc4744db40bc1f659163b1d99fbbdbf89623
Romano Silva <romano.magacho@gmail.com>
2021-07-24 05:13:32 -0300

    Fixes Replicator not copying Gained abilities (#2683)
    * Fixes Replicator not copying Gained abilities
    Fixes #2171
    
    * Added an xdescribe test that depends on #2690
    
    * No need to click on card anymore

--- test/server/cards/02-AoA/Replicator.spec.js
+++ test/server/cards/02-AoA/Replicator.spec.js
@@ -53,7 +53,7 @@ describe('Replicator', function () {
                     house: 'mars',
                     amber: 2,
                     hand: ['red-planet-ray-gun'],
-                    inPlay: ['replicator', 'ulyq-megamouth', 'dextre']
+                    inPlay: ['replicator', 'ulyq-megamouth', 'dextre', 'zorg', 'archimedes']
                 },
                 player2: {
                     amber: 5,
@@ -62,13 +62,11 @@ describe('Replicator', function () {
             });
         });
 
-        xit('Copy reap effects including upgrades', function () {
-            // This test specifically addresses multiple reap effects and
-            // Is xit'd until we re-factor the base replicator card code
+        it('Copy reap effects including upgrades', function () {
             this.player1.playUpgrade(this.redPlanetRayGun, this.ulyqMegamouth);
             this.player1.reap(this.ulyqMegamouth);
-            expect(this.player1).toBeAbleToSelect(this.ulyqMegamouth);
-            this.player1.clickCard(this.ulyqMegamouth);
+            expect(this.player1).toHavePromptButton('Ulyq Megamouth');
+            expect(this.player1).toHavePromptButton('Red Planet Ray Gun');
             this.player1.clickPrompt('Red Planet Ray Gun');
             this.player1.clickCard(this.foozle);
             expect(this.player1).toBeAbleToSelect(this.dextre);
@@ -77,9 +75,13 @@ describe('Replicator', function () {
             this.player1.clickPrompt('Reap with this creature');
             expect(this.player1).toBeAbleToSelect(this.ulyqMegamouth);
             this.player1.clickCard(this.ulyqMegamouth);
-            // TODO should have a prompt here
-            expect(this.player1).toHavePrompt('Ulyq Megamouth');
+            expect(this.player1).toHavePrompt('Resolve ability from:');
+            expect(this.player1).toHavePromptButton('Ulyq Megamouth');
+            expect(this.player1).toHavePromptButton('Red Planet Ray Gun');
+            this.player1.clickPrompt('Ulyq Megamouth');
+            expect(this.player1).toBeAbleToSelect(this.archimedes);
             expect(this.player1).toBeAbleToSelect(this.dextre);
+            expect(this.player1).not.toBeAbleToSelect(this.zorg);
         });
     });
 
@@ -108,6 +110,43 @@ describe('Replicator', function () {
             this.player1.clickCard(this.dewFaerie);
             this.player1.clickPrompt('Reap with this creature');
             expect(this.player1.amber).toBe(5);
+        });
+    });
+
+    xdescribe("Replicator's ability and a creature with two reap abilities", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'logos',
+                    amber: 2,
+                    inPlay: ['replicator', 'kompsos-haruspex', 'rhetor-gallim']
+                },
+                player2: {
+                    amber: 5,
+                    inPlay: ['rhetor-gallim']
+                }
+            });
+
+            this.rhetorGhallim1 = this.player1.inPlay[2];
+            this.rhetorGhallim2 = this.player2.inPlay[0];
+        });
+
+        it("should be able to choose Rethor Gallim's reap ability", function () {
+            this.player1.reap(this.replicator);
+            this.player1.clickCard(this.rhetorGhallim1);
+            this.player1.endTurn();
+        });
+
+        it("should be able to choose Rethor Gallim's play ability", function () {
+            this.player1.reap(this.replicator);
+            this.player1.clickCard(this.rhetorGhallim1);
+            this.player1.endTurn();
+        });
+
+        it("should be able to choose opponent's Rethor Gallim's reap ability", function () {
+            this.player1.reap(this.replicator);
+            this.player1.clickCard(this.rhetorGhallim2);
+            this.player1.endTurn();
         });
     });
 });
