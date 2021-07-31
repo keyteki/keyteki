e95aec4f09478f7ef7a7f73c779b68ac9ae2b0f9
romanogit <romano.magacho@gmail.com>
2021-07-31 10:45:40 -0300

    more message fix

--- server/game/cards/05-DT/Malstrom.js
+++ server/game/cards/05-DT/Malstrom.js
@@ -5,6 +5,7 @@ class Malstrom extends Card {
     // Play: Put each creature on top of its owner's deck in a random order. Gain 2 chains.
     setupCardAbilities(ability) {
         this.play({
+            effect: "randomly put each creature on top of its owner's deck and gain 2 chains",
             gameAction: ability.actions.sequential([
                 ability.actions.returnToDeck((context) => ({
                     target: _.shuffle(context.game.creaturesInPlay)
