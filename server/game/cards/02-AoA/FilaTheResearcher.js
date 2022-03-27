eb943539b44a1f9f2213aebf56e43c90ba798956
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2022-03-27 08:36:28 +0100

    Fix reference bug in Fila the Researcher

--- server/game/cards/02-AoA/FilaTheResearcher.js
+++ server/game/cards/02-AoA/FilaTheResearcher.js
@@ -4,7 +4,7 @@ class FilaTheResearcher extends Card {
     setupCardAbilities(ability) {
         this.reaction({
             when: {
-                onCardPlayed: (event) => event.card.neighbors.includes(this)
+                onCardPlayed: (event, context) => event.card.neighbors.includes(context.source)
             },
             gameAction: ability.actions.draw()
         });
