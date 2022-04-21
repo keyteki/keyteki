9260f038c4c0fda3176ed9fbbddc762816e0becb
Stuart Walsh <stu@ipng.org.uk>
2022-04-21 08:37:18 +0100

    Replace old banner while we're at it

--- client/pages/Lobby.jsx
+++ client/pages/Lobby.jsx
@@ -81,10 +81,6 @@ const Lobby = () => {
 
     const banners = [
         {
-            img: '/banner/banner-keyforge-copia1.jpg',
-            link: 'https://www.keyforgenetwork.com/championship-live/'
-        },
-        {
             img: '/banner/swindle-ste3.png',
             link: 'https://forms.gle/SQSbcxEDATbYkSU8A'
         }
