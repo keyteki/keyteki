f0c7f7d1df2e2119af8a2c707d030b09c8dbb0e4
David Brokaw <dgbrokaw2@gmail.com>
2022-08-22 00:14:32 -0600

    make hecatomb sequential so destroyed effects trigger at the same time, and also does not award amber for creatures that were not destroyed

--- server/game/cards/01-Core/Hecatomb.js
+++ server/game/cards/01-Core/Hecatomb.js
@@ -5,23 +5,28 @@ class Hecatomb extends Card {
         this.play({
             effect:
                 'destroy all dis creatures and each player gains amber equal to the number of their creatures destroyed',
-            gameAction: [
-                ability.actions.destroy((context) => ({
-                    target: context.game.creaturesInPlay.filter((card) => card.hasHouse('dis'))
-                })),
-                ability.actions.gainAmber((context) => ({
-                    amount: context.player.creaturesInPlay.filter((card) => card.hasHouse('dis'))
-                        .length
-                })),
-                ability.actions.gainAmber((context) => ({
-                    target: context.player.opponent,
-                    amount: context.player.opponent
-                        ? context.player.opponent.creaturesInPlay.filter((card) =>
-                              card.hasHouse('dis')
-                          ).length
-                        : 0
-                }))
-            ]
+            gameAction: ability.actions.destroy((context) => ({
+                target: context.game.creaturesInPlay.filter((card) => card.hasHouse('dis'))
+            })),
+            then: {
+                alwaysTriggers: true,
+                gameAction: [
+                    ability.actions.gainAmber((context) => ({
+                        amount: context.preThenEvents.filter(
+                            (event) =>
+                                !event.cancelled && event.card.owner.uuid == context.player.uuid
+                        ).length
+                    })),
+                    ability.actions.gainAmber((context) => ({
+                        target: context.player.opponent,
+                        amount: context.preThenEvents.filter(
+                            (event) =>
+                                !event.cancelled &&
+                                event.card.owner.uuid == context.player.opponent.uuid
+                        ).length
+                    }))
+                ]
+            }
         });
     }
 }
--- test/server/cards/01-Core/Hecatomb.spec.js
+++ test/server/cards/01-Core/Hecatomb.spec.js
@@ -23,7 +23,34 @@ describe('Hecatomb', function () {
             expect(this.bloodshardImp.location).toBe('discard');
         });
     });
-    describe("Hecatomb's ability", function () {
+    describe('Hecatomb & warded creatures', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'dis',
+                    hand: ['hecatomb'],
+                    amber: 0,
+                    inPlay: ['rotgrub', 'ember-imp', 'streke']
+                },
+                player2: {
+                    amber: 0,
+                    inPlay: ['bloodshard-imp']
+                }
+            });
+        });
+        it('should not award amber for warded dis creatures', function () {
+            this.emberImp.ward();
+            this.rotgrub.ward();
+            this.player1.play(this.hecatomb); // gain 1 from aember bonus > gain 1 from Streke being destroyed
+            expect(this.player1.player.amber).toBe(2);
+            expect(this.player2.player.amber).toBe(1);
+            expect(this.emberImp.location).toBe('play area');
+            expect(this.rotgrub.location).toBe('play area');
+            expect(this.streke.location).toBe('discard');
+            expect(this.bloodshardImp.location).toBe('discard');
+        });
+    });
+    describe('Hecatomb & creatures destroyed in other ways', function () {
         beforeEach(function () {
             this.setupTest({
                 player1: {
@@ -48,6 +75,43 @@ describe('Hecatomb', function () {
             expect(this.harbingerOfDoom.location).toBe('discard');
             expect(this.bumpsy.location).toBe('discard');
             expect(this.krump.location).toBe('discard');
+        });
+        it('should not award amber for warded dis creatures as an aftereffect', function () {
+            this.rotgrub.ward();
+            this.emberImp.ward();
+            this.player1.play(this.hecatomb); // gain 1 from aember bonus > gain 1 from Harbinger Of Doom being destroyed
+            expect(this.player1.player.amber).toBe(2);
+            expect(this.player2.player.amber).toBe(1);
+            expect(this.emberImp.location).toBe('discard');
+            expect(this.rotgrub.location).toBe('discard');
+            expect(this.bloodshardImp.location).toBe('discard');
+            expect(this.harbingerOfDoom.location).toBe('discard');
+            expect(this.bumpsy.location).toBe('discard');
+            expect(this.krump.location).toBe('discard');
+        });
+    });
+    describe('Hecatomb & destroyed abilities', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'dis',
+                    hand: ['hecatomb'],
+                    amber: 3,
+                    inPlay: ['rotgrub', 'ember-imp']
+                },
+                player2: {
+                    amber: 0,
+                    inPlay: ['brabble']
+                }
+            });
+        });
+        it('destroyed abilities should trigger before the aember gain', function () {
+            this.player1.play(this.hecatomb); // gain 1 from aember bonus > lose 3 from Brabble > gain 2 from friendly destroyed Dis creatures
+            expect(this.player1.player.amber).toBe(3);
+            expect(this.player2.player.amber).toBe(1);
+            expect(this.emberImp.location).toBe('discard');
+            expect(this.rotgrub.location).toBe('discard');
+            expect(this.brabble.location).toBe('discard');
         });
     });
 });
