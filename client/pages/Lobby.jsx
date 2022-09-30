417ec490fcdf0518f8618cb0e57be0f2b149d763
Steve Cooke <steve@digitallydexterous.com>
2022-09-30 12:34:10 +0100

    Remove expired Swindle Team Events Season 4 banner

--- client/pages/Lobby.jsx
+++ client/pages/Lobby.jsx
@@ -86,10 +86,6 @@ const Lobby = () => {
                 'https://docs.google.com/forms/d/e/1FAIpQLScdV0qp2V81XMbyw_ore6IyiN1nNRlpf4rpRyCpDNujFLOSZw/viewform'
         },
         {
-            img: '/banner/STE-4-TCO-banner-ny.png',
-            link: 'https://forms.gle/ryeT8ooZTakmB7jr5'
-        },
-        {
             img: '/banner/average-joes.png',
             link: 'https://discord.gg/bxSGTcSH5W'
         }
