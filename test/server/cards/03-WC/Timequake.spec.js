211617d7bb5b6010060e7b5ae1d0c3513fbb537e
Romano Silva <romano.magacho@gmail.com>
2022-09-30 11:22:04 -0300

    Timequake should consider friendly upgrades on enemy creatures (#2850)
    Fixes #2840

--- test/server/cards/03-WC/Timequake.spec.js
+++ test/server/cards/03-WC/Timequake.spec.js
@@ -5,12 +5,13 @@ describe('Timequake', function () {
                 player1: {
                     house: 'brobnar',
                     amber: 2,
-                    inPlay: ['lamindra', 'murkens', 'orb-of-wonder'],
+                    inPlay: ['lamindra', 'alaka', 'orb-of-wonder'],
                     hand: ['timequake', 'troll', 'blood-of-titans']
                 },
                 player2: {
                     amber: 1,
-                    inPlay: ['bulwark']
+                    inPlay: ['bulwark'],
+                    hand: ['bonerot-venom', 'murkens']
                 }
             });
         });
@@ -24,15 +25,16 @@ describe('Timequake', function () {
             expect(this.player1.player.cardsInPlay.length).toBe(0);
             // 3 initial cards - 1 played + 3 drawn
             expect(this.player1.player.hand.length).toBe(5);
+            expect(this.bulwark.location).toBe('play area');
             expect(this.lamindra.location).not.toBe('play area');
-            expect(this.murkens.location).not.toBe('play area');
+            expect(this.alaka.location).not.toBe('play area');
             expect(this.orbOfWonder.location).not.toBe('play area');
         });
 
         it('should not draw any card if nothing is in play', function () {
             this.player1.moveCard(this.lamindra, 'discard');
             this.player1.moveCard(this.orbOfWonder, 'discard');
-            this.player1.moveCard(this.murkens, 'discard');
+            this.player1.moveCard(this.alaka, 'discard');
             expect(this.player1.player.cardsInPlay.length).toBe(0);
             expect(this.player1.player.hand.length).toBe(3);
 
@@ -52,21 +54,21 @@ describe('Timequake', function () {
             expect(this.player1.player.cardsInPlay.length).toBe(1);
             expect(this.player1.player.hand.length).toBe(4);
             expect(this.lamindra.location).toBe('play area');
-            expect(this.murkens.location).not.toBe('play area');
+            expect(this.alaka.location).not.toBe('play area');
             expect(this.orbOfWonder.location).not.toBe('play area');
         });
 
         it('should not count cards under other cards', function () {
             this.player1.player.removeCardFromPile(this.lamindra);
-            this.player1.player.removeCardFromPile(this.murkens);
+            this.player1.player.removeCardFromPile(this.alaka);
 
             this.lamindra.parent = this.orbOfWonder;
             this.lamindra.facedown = true;
             this.orbOfWonder.childCards.push(this.lamindra);
 
-            this.murkens.parent = this.orbOfWonder;
-            this.murkens.facedown = false;
-            this.orbOfWonder.childCards.push(this.murkens);
+            this.alaka.parent = this.orbOfWonder;
+            this.alaka.facedown = false;
+            this.orbOfWonder.childCards.push(this.alaka);
 
             expect(this.player1.player.cardsInPlay.length).toBe(1);
             expect(this.player1.player.hand.length).toBe(3);
@@ -77,7 +79,7 @@ describe('Timequake', function () {
             expect(this.player1.player.hand.length).toBe(3);
 
             expect(this.lamindra.location).toBe('discard');
-            expect(this.murkens.location).toBe('discard');
+            expect(this.alaka.location).toBe('discard');
             expect(this.orbOfWonder.location).not.toBe('play area');
         });
 
@@ -85,7 +87,6 @@ describe('Timequake', function () {
             // Play the upgrade
             this.player1.playUpgrade(this.bloodOfTitans, this.lamindra);
             expect(this.player1.player.cardsInPlay.length).toBe(3);
-            expect(this.lamindra.upgrades).toContain(this.bloodOfTitans);
 
             expect(this.player1.player.hand.length).toBe(2);
 
@@ -94,6 +95,75 @@ describe('Timequake', function () {
             // Upgrade should not be in discard
             expect(this.bloodOfTitans.location).not.toBe('discard');
             expect(this.player1.player.hand.length).toBe(5);
+        });
+
+        it('should shuffle friendly upgrades attached to enemy creatures', function () {
+            // Play the upgrade
+            this.player1.playUpgrade(this.bloodOfTitans, this.bulwark);
+            expect(this.player1.player.cardsInPlay.length).toBe(3);
+
+            expect(this.player1.player.hand.length).toBe(2);
+
+            this.player1.play(this.timequake);
+
+            // Upgrade should not be in discard
+            expect(this.bulwark.upgrades.length).toBe(0);
+            expect(this.bloodOfTitans.location).not.toBe('discard');
+            expect(this.player1.player.hand.length).toBe(5);
+        });
+
+        it('opponent will attach its upgrade on my creature, timequake should not count it to draw', function () {
+            this.player1.endTurn();
+            this.player2.clickPrompt('shadows');
+            this.player2.playUpgrade(this.bonerotVenom, this.lamindra);
+            this.player2.endTurn();
+            this.player1.clickPrompt('brobnar');
+
+            expect(this.player1.player.hand.length).toBe(6);
+
+            this.player1.play(this.timequake);
+
+            // Opponent's upgrade should go to discard
+            expect(this.bonerotVenom.location).toBe('discard');
+            expect(this.player1.player.hand.length).toBe(8);
+        });
+
+        it('opponent will play timequake from my archive using murkens', function () {
+            expect(this.player2.player.hand.length).toBe(2);
+
+            this.player1.moveCard(this.timequake, 'archives');
+            this.player1.endTurn();
+            this.player2.clickPrompt('shadows');
+            this.player2.playUpgrade(this.bonerotVenom, this.lamindra);
+            this.player2.play(this.murkens);
+            this.player2.clickPrompt('Random card from archives');
+
+            expect(this.lamindra.upgrades.length).toBe(0);
+            expect(this.bonerotVenom.location).not.toBe('discard');
+            expect(this.murkens.location).not.toBe('discard');
+            expect(this.bulwark.location).not.toBe('discard');
+            expect(this.player2.player.hand.length).toBe(3);
+        });
+
+        it('opponent will murkens my upgrade and timequake should not move it to deck', function () {
+            this.player1.moveCard(this.bloodOfTitans, 'archives');
+            this.player1.endTurn();
+            this.player2.clickPrompt('shadows');
+            this.player2.play(this.murkens);
+            this.player2.clickPrompt('Random card from archives');
+            this.player2.clickCard(this.bulwark);
+            expect(this.bulwark.upgrades).toContain(this.bloodOfTitans);
+            this.player2.endTurn();
+            this.player1.clickPrompt('brobnar');
+            expect(this.player1.player.hand.length).toBe(6);
+            this.player1.play(this.timequake);
+
+            expect(this.bulwark.upgrades).toContain(this.bloodOfTitans);
+            expect(this.bulwark.location).toBe('play area');
+            expect(this.lamindra.location).not.toBe('play area');
+            expect(this.alaka.location).not.toBe('play area');
+            expect(this.orbOfWonder.location).not.toBe('play area');
+            expect(this.player1.player.hand.length).toBe(8);
         });
     });
 });
