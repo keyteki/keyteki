2573c2eef23bc7b969ebdb77546aa0e35e36b194
romanogit <romano.magacho@gmail.com>
2022-11-26 13:54:26 -0300

    Reapplied the fix to Epic Quest It was overridden when we merged DT Fixes #2920

--- server/game/cards/01-Core/EpicQuest.js
+++ server/game/cards/01-Core/EpicQuest.js
@@ -10,15 +10,17 @@ class EpicQuest extends Card {
         });
 
         this.omni({
-            condition: (context) =>
-                context.game.cardsPlayed.filter((card) => card.hasHouse('sanctum')).length > 6,
-            effect: 'sacrifice {0} and forge a key',
-            gameAction: ability.actions.sequential([
-                ability.actions.sacrifice(),
-                ability.actions.forgeKey({
-                    atNoCost: true
-                })
-            ])
+            effect: 'forge a key if 6 or more sanctum cards were played this turn',
+            gameAction: ability.actions.conditional({
+                condition: (context) =>
+                    context.game.cardsPlayed.filter((card) => card.hasHouse('sanctum')).length > 6,
+                trueGameAction: ability.actions.sequential([
+                    ability.actions.sacrifice(),
+                    ability.actions.forgeKey((context) => ({
+                        modifier: -context.player.getCurrentKeyCost()
+                    }))
+                ])
+            })
         });
     }
 }
