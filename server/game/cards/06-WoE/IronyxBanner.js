1d3201889e82054fa94bb73875cd2c196ba4c268
romanogit <romano.magacho@gmail.com>
2022-11-28 16:30:23 -0300

    Mars Ironyx cards

---
+++ server/game/cards/06-WoE/IronyxBanner.js
@@ -0,0 +1,21 @@
+const Card = require('../../Card.js');
+
+class IronyxBanner extends Card {
+    // Play: Make a token creature.
+    // Each friendly Ironyx creature gets +1 power and +1 armor.
+    setupCardAbilities(ability) {
+        this.play({
+            gameAction: ability.actions.makeTokenCreature()
+        });
+
+        this.persistentEffect({
+            targetController: 'current',
+            match: (card) => card.type === 'creature' && card.hasTrait('ironyx'),
+            effect: [ability.effects.modifyArmor(1), ability.effects.modifyPower(1)]
+        });
+    }
+}
+
+IronyxBanner.id = 'ironyx-banner';
+
+module.exports = IronyxBanner;
