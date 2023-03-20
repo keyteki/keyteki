409cbe5a021b816548336b04a2f9ff4981887271
romanogit <romano.magacho@gmail.com>
2023-03-20 11:13:53 -0300

    FlipAction should consider send to discard when controller has no token Implemented Gĕzdrutyŏ

---
+++ server/game/cards/06-WoE/GezdrutyoTheArcane.js
@@ -0,0 +1,17 @@
+const Card = require('../../Card.js');
+
+class GezdrutyoTheArcane extends Card {
+    //Action: Steal 2A. Flip Gĕzdrutyŏ the Arcane facedown (it becomes a token creature).
+    setupCardAbilities(ability) {
+        this.action({
+            gameAction: ability.actions.sequential([
+                ability.actions.steal({ amount: 2 }),
+                ability.actions.flip()
+            ])
+        });
+    }
+}
+
+GezdrutyoTheArcane.id = 'gezdrutyo-the-arcane';
+
+module.exports = GezdrutyoTheArcane;
