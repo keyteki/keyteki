78f5bf59a37b164f70d53e4599772e7d4886aed6
Jeremy Stribling <strib@alum.mit.edu>
2023-05-21 15:15:41 -0700

    WoE: implement Conductor Jărroyă

---
+++ server/game/cards/06-WoE/ConductorJarroya.js
@@ -0,0 +1,17 @@
+const Card = require('../../Card.js');
+
+class ConductorJarroya extends Card {
+    setupCardAbilities(ability) {
+        this.reap({
+            gameAction: ability.actions.ready((context) => ({
+                target: context.player.cardsInPlay.filter(
+                    (card) => card.hasTrait('buggy') && card.type === 'artifact'
+                )
+            }))
+        });
+    }
+}
+
+ConductorJarroya.id = 'conductor-jarroya';
+
+module.exports = ConductorJarroya;
