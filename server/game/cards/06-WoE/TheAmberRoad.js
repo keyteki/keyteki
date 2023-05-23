aac0905c4b2349b9c8a15d1955eb97bbd811e1f6
Jeremy Stribling <strib@alum.mit.edu>
2023-05-23 09:59:26 -0700

    WoE: fix TheAmberRoad effect message

--- server/game/cards/06-WoE/TheAmberRoad.js
+++ server/game/cards/06-WoE/TheAmberRoad.js
@@ -3,7 +3,11 @@ const Card = require('../../Card.js');
 class TheAmberRoad extends Card {
     setupCardAbilities(ability) {
         this.omni({
-            effect: 'put a token on {0}, gain {1} amber, and give control to your opponent',
+            effect: 'to put a trade counter on {0}, gain {1} amber, and give control to {2}',
+            effectArgs: (context) => [
+                context.source.tokens.trade ? context.source.tokens.trade + 1 : 1,
+                context.player.opponent
+            ],
             gameAction: ability.actions.addTradeCounter(),
             then: {
                 gameAction: ability.actions.sequential([
