6f997ed7de3cbfee22f8ffc52aeb08a40ded05fd
Jeremy Stribling <strib@alum.mit.edu>
2023-05-24 16:12:15 -0700

    WoE: implement Trade Secrets
    Issue: #2902

---
+++ server/game/cards/06-WoE/TradeSecrets.js
@@ -0,0 +1,28 @@
+const Card = require('../../Card.js');
+
+class TradeSecrets extends Card {
+    // Play: Discard any number of Ekwidon cards from your hand. Steal
+    // 1 Aember icon for each card discarded this way.
+    setupCardAbilities(ability) {
+        this.play({
+            effectStyle: 'all',
+            target: {
+                controller: 'self',
+                mode: 'unlimited',
+                location: 'hand',
+                cardCondition: (card) => card.hasHouse('ekwidon'),
+                gameAction: ability.actions.discard()
+            },
+            then: (preThenContext) => ({
+                gameAction: ability.actions.steal({
+                    target: preThenContext.player.opponent,
+                    amount: preThenContext.target.length
+                })
+            })
+        });
+    }
+}
+
+TradeSecrets.id = 'trade-secrets';
+
+module.exports = TradeSecrets;
