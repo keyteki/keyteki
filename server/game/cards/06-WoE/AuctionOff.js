00da439e347ed4ef160803c8aaa45ebcf64dc801
Jeremy Stribling <strib@alum.mit.edu>
2023-05-24 18:35:46 -0700

    WoE: implement Auction Off
    Issue: #2902

---
+++ server/game/cards/06-WoE/AuctionOff.js
@@ -0,0 +1,25 @@
+const Card = require('../../Card.js');
+
+class AuctionOff extends Card {
+    // Play: Purge an artifact. Its controller gains 1Aember.
+    setupCardAbilities(ability) {
+        this.play({
+            effect: 'purge {0} and have {1} gaining 1 amber',
+            effectArgs: (context) => [context.target.controller],
+            target: {
+                cardType: 'artifact',
+                location: 'play area',
+                gameAction: [
+                    ability.actions.purge(),
+                    ability.actions.gainAmber((context) => ({
+                        target: context.target ? context.target.controller : []
+                    }))
+                ]
+            }
+        });
+    }
+}
+
+AuctionOff.id = 'auction-off';
+
+module.exports = AuctionOff;
