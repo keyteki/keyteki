64af114a822f1ce171d5eba4fbb02f41d64ff6b6
Jeremy Stribling <strib@alum.mit.edu>
2023-05-24 19:24:08 -0700

    WoE: implement Mass Buyout
    Issue: #2902

---
+++ server/game/cards/06-WoE/MassBuyout.js
@@ -0,0 +1,42 @@
+const Card = require('../../Card.js');
+
+class MassBuyout extends Card {
+    // Play: Destroy each creature. Each player gains Aember icon
+    // equal to half the number of creatures they controlled that were
+    // destroyed this way (rounded up).
+    setupCardAbilities(ability) {
+        this.play({
+            gameAction: ability.actions.destroy((context) => ({
+                target: context.game.creaturesInPlay
+            })),
+            then: {
+                alwaysTriggers: true,
+                gameAction: [
+                    ability.actions.gainAmber((context) => ({
+                        target: context.player,
+                        amount: Math.ceil(
+                            context.preThenEvents.filter(
+                                (event) =>
+                                    !event.cancelled && event.clone.controller === context.player
+                            ).length * 0.5
+                        )
+                    })),
+                    ability.actions.gainAmber((context) => ({
+                        target: context.player.opponent,
+                        amount: Math.ceil(
+                            context.preThenEvents.filter(
+                                (event) =>
+                                    !event.cancelled &&
+                                    event.clone.controller === context.player.opponent
+                            ).length * 0.5
+                        )
+                    }))
+                ]
+            }
+        });
+    }
+}
+
+MassBuyout.id = 'mass-buyout';
+
+module.exports = MassBuyout;
