25c28155937b148439c5c6ce4d0d36de52ff9cca
Romano Silva <romano.magacho@gmail.com>
2023-05-25 21:56:24 -0300

    Update GreenAeronaut.js
    update the '==='

--- server/game/cards/06-WoE/GreenAeronaut.js
+++ server/game/cards/06-WoE/GreenAeronaut.js
@@ -8,7 +8,7 @@ class GreenAeronaut extends Card {
             effect: 'a friendly Nautilixian gains Splash-attack 3 for the remainder of the turn',
             target: {
                 controller: 'self',
-                cardCondition: (card) => card.name == 'Nautilixian',
+                cardCondition: (card) => card.name === 'Nautilixian',
                 gameAction: ability.actions.cardLastingEffect({
                     duration: 'untilEndOfRound',
                     effect: ability.effects.addKeyword({ 'splash-attack': 3 })
