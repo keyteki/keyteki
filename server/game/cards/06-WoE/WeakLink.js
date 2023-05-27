d751240af126261197fc8b8cf7afeaf12ce242ba
Romano Silva <romano.magacho@gmail.com>
2023-05-27 14:25:26 +0000

    Weak Link

--- server/game/cards/06-WoE/WeakLink.js
+++ server/game/cards/06-WoE/WeakLink.js
@@ -3,22 +3,17 @@ const Card = require('../../Card.js');
 class WeakLink extends Card {
     //This creature gains, "While this creature is exhausted, your keys cost +6A icon."
     setupCardAbilities(ability) {
-        this.play({
-            target: {
-                cardType: 'creature',
-                controller: 'opponent',
-                gameAction: ability.actions.sequential([
-                    ability.actions.enrage(),
-                    ability.actions.capture((context) => ({
-                        amount: 1,
-                        player: context.player.opponent
-                    }))
-                ])
-            }
+        this.whileAttached({
+            effect: ability.effects.gainAbility('persistentEffect', {
+                targetController: 'current',
+                effect: ability.effects.modifyKeyCost((player, context) =>
+                    context.source.exhausted ? 6 : 0
+                )
+            })
         });
     }
 }
 
-WeakLink.id = 'weaklink';
+WeakLink.id = 'weak-link';
 
 module.exports = WeakLink;
--- test/server/cards/06-WoE/WeakLink.spec.js
+++ test/server/cards/06-WoE/WeakLink.spec.js
@@ -6,7 +6,7 @@ describe('Weak Link', function () {
                     house: 'unfathomable',
                     amber: 1,
                     inPlay: ['seabringer-kekoa'],
-                    hand: ['weak-link']
+                    hand: ['weak-link', 'call-of-the-void']
                 },
                 player2: {
                     amber: 1,
@@ -17,18 +17,37 @@ describe('Weak Link', function () {
 
         describe('when played on own creature', function () {
             beforeEach(function () {
-                this.player1.playUpgrade(this.weaklink, this.seabringerKekoa);
+                this.player1.playUpgrade(this.weakLink, this.seabringerKekoa);
             });
 
-            it('should not increase cost if creature is not exhausted', function () {});
+            it('should not increase cost if creature is not exhausted', function () {
+                expect(this.player1.player.getCurrentKeyCost()).toBe(6);
+                expect(this.player2.player.getCurrentKeyCost()).toBe(6);
+            });
+
+            it('should not increase own cost if creature is exhausted', function () {
+                this.player1.reap(this.seabringerKekoa);
+                expect(this.player1.player.getCurrentKeyCost()).toBe(12);
+                expect(this.player2.player.getCurrentKeyCost()).toBe(6);
+            });
         });
 
         describe("when played on opponent's creature", function () {
             beforeEach(function () {
-                this.player1.playUpgrade(this.weaklink, this.seabringerKekoa);
+                this.player1.playUpgrade(this.weakLink, this.flaxia);
             });
 
-            it('should not increase cost if creature is not exhausted', function () {});
+            it('should not increase cost if creature is not exhausted', function () {
+                expect(this.player1.player.getCurrentKeyCost()).toBe(6);
+                expect(this.player2.player.getCurrentKeyCost()).toBe(6);
+            });
+
+            it('should not increase opponent cost if creature is exhausted', function () {
+                this.player1.play(this.callOfTheVoid);
+                this.player1.clickCard(this.flaxia);
+                expect(this.player1.player.getCurrentKeyCost()).toBe(6);
+                expect(this.player2.player.getCurrentKeyCost()).toBe(12);
+            });
         });
     });
 });
