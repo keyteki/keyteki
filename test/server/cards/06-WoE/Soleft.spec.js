6d1d7f45dfa5ca68abdbb98bfee05280dcc39a42
Romano Silva <romano.magacho@gmail.com>
2023-05-27 14:57:52 +0000

    Soleft

---
+++ test/server/cards/06-WoE/Soleft.spec.js
@@ -0,0 +1,51 @@
+describe('Soleft', function () {
+    describe("Soleft's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'unfathomable',
+                    amber: 4,
+                    hand: ['soleft'],
+                    inPlay: ['seabringer-kekoa', 'nexus']
+                },
+                player2: {
+                    amber: 2,
+                    token: 'trader',
+                    inPlay: ['trader:batdrone', 'mother', 'helper-bot']
+                }
+            });
+        });
+
+        it('should destroy the left creature', function () {
+            this.player1.play(this.soleft);
+            expect(this.seabringerKekoa.location).toBe('play area');
+            expect(this.nexus.location).toBe('play area');
+            expect(this.trader.location).toBe('discard');
+            expect(this.mother.location).toBe('play area');
+            expect(this.helperBot.location).toBe('play area');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+
+    describe("Soleft's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'unfathomable',
+                    amber: 4,
+                    hand: ['soleft'],
+                    inPlay: ['seabringer-kekoa', 'nexus']
+                },
+                player2: {
+                    amber: 6,
+                    inPlay: []
+                }
+            });
+        });
+
+        it('should do nothing with no opponent creatures', function () {
+            this.player1.play(this.soleft);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
