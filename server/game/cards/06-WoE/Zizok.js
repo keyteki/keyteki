632e87211f21e7cac0f3be29cd306043f8b02fc5
Jeremy Stribling <strib@alum.mit.edu>
2023-05-26 21:42:09 -0700

    WoE: implement Zizok
    Issue: #2903

---
+++ server/game/cards/06-WoE/Zizok.js
@@ -0,0 +1,21 @@
+const Card = require('../../Card.js');
+
+class Zizok extends Card {
+    // After an enemy creature is destroyed by Zizok's splash-attack
+    // damage, ready Zizok.
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardDestroyed: (event, context) =>
+                    event.damageEvent.damageType === 'splash-attack' &&
+                    event.damageEvent.damageSource === context.source
+            },
+            gameAction: ability.actions.ready()
+            //effect: "ready {0}"
+        });
+    }
+}
+
+Zizok.id = 'zizok';
+
+module.exports = Zizok;
