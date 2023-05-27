becb7f80fec7c8014ac12f0df1e87e87f4cac3f8
Romano Silva <romano.magacho@gmail.com>
2023-05-27 16:29:50 +0000

    two more

--- server/game/cards/06-WoE/AdultSwim.js
+++ server/game/cards/06-WoE/AdultSwim.js
@@ -1,20 +1,14 @@
 const Card = require('../../Card.js');
+const _ = require('underscore');
 
 class AdultSwim extends Card {
     //Play: Put each creature with power 3 or lower on top of its owner's deck in a random order.
     setupCardAbilities(ability) {
         this.play({
-            target: {
-                cardType: 'creature',
-                controller: 'opponent',
-                gameAction: ability.actions.sequential([
-                    ability.actions.enrage(),
-                    ability.actions.capture((context) => ({
-                        amount: 1,
-                        player: context.player.opponent
-                    }))
-                ])
-            }
+            effect: "randomly put each creature with power 3 or lower on top of its owner's deck",
+            gameAction: ability.actions.returnToDeck((context) => ({
+                target: _.shuffle(context.game.creaturesInPlay.filter((card) => card.power <= 3))
+            }))
         });
     }
 }
