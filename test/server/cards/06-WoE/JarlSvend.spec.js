8bf1d6214018da5b70bd30c5af2939f53de6aada
Jeremy Stribling <strib@alum.mit.edu>
2023-05-30 14:43:11 -0700

    WoE: implement Jarl Svend
    Issue: #2901
    Fixes #3049

---
+++ test/server/cards/06-WoE/JarlSvend.spec.js
@@ -0,0 +1,42 @@
+describe('Jarl Svend', function () {
+    describe("Jarl Svend's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 2,
+                    token: 'warrior',
+                    house: 'brobnar',
+                    inPlay: ['jarl-svend']
+                },
+                player2: {
+                    inPlay: ['mother', 'earthshaker']
+                }
+            });
+        });
+
+        it('should make a token creature when attacking and killing a creature', function () {
+            this.player1.fightWith(this.jarlSvend, this.mother);
+            expect(this.jarlSvend.tokens.damage).toBe(5);
+            expect(this.mother.location).toBe('discard');
+            this.player1.clickPrompt('Left');
+            expect(this.player1.player.creaturesInPlay.length).toBe(2);
+        });
+
+        it('should make a token creature when defended and killing a creature', function () {
+            this.player1.endTurn();
+            this.player2.clickPrompt('logos');
+            this.player2.fightWith(this.mother, this.jarlSvend);
+            expect(this.mother.location).toBe('discard');
+            this.player2.clickPrompt('Left');
+            expect(this.player1.player.creaturesInPlay.length).toBe(2);
+        });
+
+        it('should not make a token creature when Jarl Svend dies', function () {
+            this.player1.fightWith(this.jarlSvend, this.earthshaker);
+            expect(this.jarlSvend.location).toBe('discard');
+            expect(this.earthshaker.location).toBe('discard');
+            expect(this.player1.player.creaturesInPlay.length).toBe(0);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
