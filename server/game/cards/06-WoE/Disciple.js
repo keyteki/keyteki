737c2a47fcb68a2b5d99554291a3a58f46448f3b
Justin Russell <justin.russell@clublocker.com>
2023-05-30 12:45:02 -0400

    WoE: Implement Disciple
    See #2904

---
+++ server/game/cards/06-WoE/Disciple.js
@@ -0,0 +1,21 @@
+const Card = require('../../Card.js');
+
+class Disciple extends Card {
+    // Disciple gets +1 power and +1 armor for each Monk neighbor it has
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            effect: [
+                ability.effects.modifyArmor(
+                    (card) => card.neighbors.filter((c) => c.hasTrait('monk')).length
+                ),
+                ability.effects.modifyPower(
+                    (card) => card.neighbors.filter((c) => c.hasTrait('monk')).length
+                )
+            ]
+        });
+    }
+}
+
+Disciple.id = 'disciple';
+
+module.exports = Disciple;
