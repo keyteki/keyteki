0a2e141617f4d9a3aae895ff704b99e83c2db75f
Jeremy Stribling <strib@alum.mit.edu>
2023-05-31 16:21:16 -0700

    WoE: implement Diplomat
    Issue: #2902

---
+++ server/game/cards/06-WoE/Diplomat.js
@@ -0,0 +1,20 @@
+const Card = require('../../Card.js');
+
+class Diplomat extends Card {
+    // After Reap: Each player gains 1 Aember.
+    setupCardAbilities(ability) {
+        this.reap({
+            effect: 'make each player gain 1 amber',
+            gameAction: [
+                ability.actions.gainAmber(),
+                ability.actions.gainAmber((context) => ({
+                    target: context.player.opponent
+                }))
+            ]
+        });
+    }
+}
+
+Diplomat.id = 'diplomat';
+
+module.exports = Diplomat;
