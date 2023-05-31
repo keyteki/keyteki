55449784c18c0842d042b4ece46eb1258f86946a
Jeremy Stribling <strib@alum.mit.edu>
2023-05-30 19:43:31 -0700

    WoE: implement Gezrahi Blacksmith
    Issue: #2902

---
+++ server/game/cards/06-WoE/GezrahiBlacksmith.js
@@ -0,0 +1,27 @@
+const Card = require('../../Card.js');
+
+class GezrahiBlacksmith extends Card {
+    // At the start of each player's turn, that player chooses to
+    // either make a token creature or draw a card.
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onBeginRound: () => true
+            },
+            useEventPlayer: true,
+            targets: {
+                action: {
+                    mode: 'select',
+                    choices: {
+                        'Make a token creature': ability.actions.makeTokenCreature(),
+                        'Draw a card': ability.actions.draw()
+                    }
+                }
+            }
+        });
+    }
+}
+
+GezrahiBlacksmith.id = 'gezrahi-blacksmith';
+
+module.exports = GezrahiBlacksmith;
