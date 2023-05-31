910a239aa948d995b08ce7e8d002ed2db30a0778
Jeremy Stribling <strib@alum.mit.edu>
2023-05-31 11:46:00 -0700

    WoE: implement Ornate Talking Tray
    Issue: #2902

---
+++ server/game/cards/06-WoE/OrnateTalkingTray.js
@@ -0,0 +1,16 @@
+const Card = require('../../Card.js');
+
+class OrnateTalkingTray extends Card {
+    // Omni: Destroy Ornate Talking Tray. Make a token creature.
+    setupCardAbilities(ability) {
+        this.omni({
+            effect: 'destroy {1} and make a token creature',
+            effectArgs: (context) => context.source,
+            gameAction: [ability.actions.destroy(), ability.actions.makeTokenCreature()]
+        });
+    }
+}
+
+OrnateTalkingTray.id = 'ornate-talking-tray';
+
+module.exports = OrnateTalkingTray;
