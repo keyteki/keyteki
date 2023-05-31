392c32ce2d8199919e916325de31b2ad942aa76c
Jeremy Stribling <strib@alum.mit.edu>
2023-05-31 14:48:33 -0700

    WoE: implement Shizyoku Buggy
    Issue: #2902

---
+++ server/game/cards/06-WoE/ShizyokuBuggy.js
@@ -0,0 +1,29 @@
+const Card = require('../../Card.js');
+
+class ShizyokuBuggy extends Card {
+    // Action: Reveal two cards from your hand. If they share a house,
+    // discard them and make a token creature.
+    setupCardAbilities(ability) {
+        this.action({
+            target: {
+                activePromptTitle: 'Choose which cards to reveal and discard',
+                mode: 'exactly',
+                numCards: 2,
+                controller: 'self',
+                location: 'hand',
+                gameAction: [
+                    ability.actions.discard(),
+                    ability.actions.makeTokenCreature((context) => ({
+                        target: context.player
+                    }))
+                ]
+            },
+            effect: 'reveal and discard {0} to make a token creature',
+            effectArgs: (context) => context.target.length
+        });
+    }
+}
+
+ShizyokuBuggy.id = 'shizyoku-buggy';
+
+module.exports = ShizyokuBuggy;
---
+++ test/server/cards/06-WoE/ShizyokuBuggy.spec.js
@@ -0,0 +1,29 @@
+describe('Shĭzyokŭ Buggy', function () {
+    describe("Shĭzyokŭ Buggy's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 4,
+                    house: 'ekwidon',
+                    token: 'warrior',
+                    hand: ['pelf', 'antiquities-dealer', 'conductor-jarroya'],
+                    inPlay: ['shizyoku-buggy']
+                }
+            });
+        });
+
+        it('it should reveal and discard two cards', function () {
+            this.player1.useAction(this.shizyokuBuggy);
+            expect(this.player1).toBeAbleToSelect(this.pelf);
+            expect(this.player1).toBeAbleToSelect(this.antiquitiesDealer);
+            expect(this.player1).toBeAbleToSelect(this.conductorJarroya);
+            this.player1.clickCard(this.pelf);
+            this.player1.clickCard(this.antiquitiesDealer);
+            this.player1.clickPrompt('Done');
+            expect(this.pelf.location).toBe('discard');
+            expect(this.antiquitiesDealer.location).toBe('discard');
+            expect(this.player1.player.creaturesInPlay.length).toBe(1);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
