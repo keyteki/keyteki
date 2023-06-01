14efe8dc273e70bb83d3a67bf1e33f4270a8e330
Jeremy Stribling <strib@alum.mit.edu>
2023-05-31 17:24:40 -0700

    WoE: implement Aedile Tullia
    Issue: #2905

---
+++ server/game/cards/06-WoE/AedileTullia.js
@@ -0,0 +1,23 @@
+const Card = require('../../Card.js');
+
+class AedileTullia extends Card {
+    // Each time you exalt a friendly non-token creature, make a token
+    // creature.
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onExalt: (event) =>
+                    this.controller === event.context.player &&
+                    event.card.controller === this.controller &&
+                    !event.card.isToken()
+            },
+            gameAction: ability.actions.makeTokenCreature((context) => ({
+                amount: context.event.amount
+            }))
+        });
+    }
+}
+
+AedileTullia.id = 'aedile-tullia';
+
+module.exports = AedileTullia;
