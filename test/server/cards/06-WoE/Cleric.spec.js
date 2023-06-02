88b435b41ef8c68a99ee185bd1e15565e6588125
Justin Russell <justin.russell@clublocker.com>
2023-06-02 11:47:07 -0400

    WoE: Implement Cleric
    See #2904

---
+++ test/server/cards/06-WoE/Cleric.spec.js
@@ -0,0 +1,26 @@
+describe('Cleric', function () {
+    describe("Cleric's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'sanctum',
+                    token: 'cleric',
+                    amber: 1,
+                    inPlay: [],
+                    hand: ['holdfast', 'mother-northelle', 'grey-augur']
+                },
+                player2: {
+                    amber: 7,
+                    inPlay: ['gub', 'krump']
+                }
+            });
+        });
+
+        it('should capture 1 when enters play', function () {
+            this.player1.makeTokenCreature();
+            this.player1.makeTokenCreature();
+            expect(this.player1.player.creaturesInPlay[0].tokens.amber).toBe(1);
+            expect(this.player1.player.creaturesInPlay[1].tokens.amber).toBe(1);
+        });
+    });
+});
