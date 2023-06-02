7cf3d4480248e65a3c3ba4a2523bd563e49b71ae
Fabulousing <dgranger@conncoll.edu>
2023-06-02 10:33:09 -0400

    Add files via upload

---
+++ test/server/cards/06-WoE/AmbrosiaOutpost.spec.js
@@ -0,0 +1,44 @@
+describe('Embrosia Outpost', function () {
+    describe("Embrosia Outpost's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'saurian',
+                    amber: 1,
+                    inPlay: ['æmbrosia-outpost', 'galeatops'],
+                    hand: ['curse-of-vanity', 'paraguardian']
+                },
+                player2: {
+                    amber: 4,
+                    inPlay: []
+                }
+            });
+        });
+
+        it('should not prompt for any creature, since there is no friendly creature.', function () {
+            this.player1.useAction(this.æmbrosiaOutpost);
+            this.player1.clickCard(this.galeatops);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            expect(this.player1.amber).toBe(1);
+        });
+
+        it('should not prompt for any creature, since friendly creature has no.', function () {
+            this.player1.playCreature(this.paraguardian);
+            this.player1.useAction(this.æmbrosiaOutpost);
+            this.player1.clickCard(this.galeatops);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            expect(this.player1.amber).toBe(1);
+        });
+
+        it('should take amber from creature', function () {
+            this.player1.play(this.curseOfVanity);
+            this.player1.clickCard(this.galeatops);
+            this.player1.playCreature(this.paraguardian);
+            this.player1.useAction(this.æmbrosiaOutpost);
+            this.player1.clickCard(this.paraguardian);
+            this.player1.clickCard(this.galeatops);
+            expect(this.galeatops.tokens.amber).toBe(undefined);
+            expect(this.player1.amber).toBe(3);
+        });
+    });
+});
