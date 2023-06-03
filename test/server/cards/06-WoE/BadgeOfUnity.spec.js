5d8bfe0d37f07f244ad25cf2687f494e013ec6d8
Fabulousing <dgranger@conncoll.edu>
2023-06-03 13:43:28 -0400

    Update BadgeOfUnity.spec.js

--- test/server/cards/06-WoE/BadgeOfUnity.spec.js
+++ test/server/cards/06-WoE/BadgeOfUnity.spec.js
@@ -16,7 +16,7 @@ fdescribe('Badge of Unity', function () {
         it('should treat the upgraded creature as Star Alliance', function () {
             this.player1.playUpgrade(this.badgeOfUnity, this.troll);
             expect(this.troll.hasHouse('staralliance')).toBe(true);
-            expect(this.troll.hasHouse('mars')).toBe(true);
+            expect(this.troll.hasHouse('brobnar')).toBe(true);
         });
     });
 });
