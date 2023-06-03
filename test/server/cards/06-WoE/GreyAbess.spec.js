75e9851049ec828d6bf9d6b886153f504c2a1822
Justin Russell <justin.russell@clublocker.com>
2023-06-03 06:47:15 -0400

    WoE: Implement Grey Abess
    See #2904

---
+++ test/server/cards/06-WoE/GreyAbess.spec.js
@@ -0,0 +1,39 @@
+describe('Grey Abess', function () {
+    describe("Grey Abess's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'sanctum',
+                    amber: 5,
+                    token: 'cleric',
+                    hand: ['grey-abess', 'shorty'],
+                    inPlay: ['troll']
+                },
+                player2: {
+                    amber: 5,
+                    token: 'cleric',
+                    hand: ['troll'],
+                    inPlay: ['cleric:krump']
+                }
+            });
+
+            this.player1.moveCard(this.shorty, 'deck');
+        });
+
+        it('should make a token creature and give 1 armor to friendly tokens', function () {
+            this.player1.play(this.greyAbess);
+            this.player1.clickPrompt('Left');
+            expect(this.shorty.location).toBe('play area');
+            expect(this.shorty.name).toBe('Cleric');
+            expect(this.shorty.armor).toBe(1);
+            this.player1.endTurn();
+        });
+
+        it('should not boost the armor of enemy tokens', function () {
+            this.player1.play(this.greyAbess);
+            this.player1.clickPrompt('Left');
+            expect(this.player2.player.creaturesInPlay[0].armor).toBe(0);
+            this.player1.endTurn();
+        });
+    });
+});
