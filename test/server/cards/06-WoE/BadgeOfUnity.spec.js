b6c7f98542942c24c46ae28db5d1fa02e4327c03
Fabulousing <dgranger@conncoll.edu>
2023-06-03 12:32:52 -0400

    Add files via upload

---
+++ test/server/cards/06-WoE/BadgeOfUnity.spec.js
@@ -0,0 +1,22 @@
+fdescribe('Badge of Unity', function () {
+    describe("Badge of Unity's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'staralliance',
+                    inPlay: ['troll'],
+                    hand: ['badge-of-unity']
+                },
+                player2: {
+                    hand: ['dextre']
+                }
+            });
+        });
+
+        it('should treat the upgraded creature as Star Alliance', function () {
+            this.player1.playUpgrade(this.badgeOfUnity, this.troll);
+            expect(this.troll.hasHouse('staralliance')).toBe(true);
+            expect(this.troll.hasHouse('mars')).toBe(true);
+        });
+    });
+});
