ca2af2b7c58cc44c2c8d297d35a5646e357b94b5
Jeremy Stribling <strib@alum.mit.edu>
2023-06-04 16:31:51 -0700

    WoE: put real amount lost in the LoseAmber event
    Suggested by romanogit.
    
    Issue: #3098

--- server/game/cards/06-WoE/TrainingCosts.js
+++ server/game/cards/06-WoE/TrainingCosts.js
@@ -6,8 +6,6 @@ class TrainingCosts extends Card {
     // Play: Lose 2Aember. If you do not, shuffle Training Costs into
     // your deck.
     setupCardAbilities(ability) {
-        this.startingAmber = 0;
-
         this.persistentEffect({
             location: 'any',
             effect: [ability.effects.cardCannot('discard')]
@@ -15,7 +13,6 @@ class TrainingCosts extends Card {
 
         this.play({
             gameAction: ability.actions.loseAmber((context) => {
-                this.startingAmber = context.player.amber;
                 return {
                     target: context.player,
                     amount: 2
@@ -23,7 +20,7 @@ class TrainingCosts extends Card {
             }),
             then: {
                 alwaysTriggers: true,
-                condition: () => this.startingAmber < 2,
+                condition: (context) => context.preThenEvent.amount < 2,
                 gameAction: ability.actions.returnToDeck({ shuffle: true }),
                 message: '{0} uses {1} to shuffle it back into their deck'
             }
