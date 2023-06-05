527169c3547dfd394ceb9990dcae9274d9688d5d
Jeremy Stribling <strib@alum.mit.edu>
2023-06-05 12:30:26 -0700

    WoE: implement Ganymede Outpost
    Issue: #2906
    Fixes #3147

---
+++ server/game/cards/06-WoE/GanymedeOutpost.js
@@ -0,0 +1,31 @@
+const Card = require('../../Card.js');
+
+class GanymedeOutpost extends Card {
+    // Action: Put a friendly creature on the bottom of its owner's
+    // deck. If you do, archive 2 cards.
+    setupCardAbilities(ability) {
+        this.action({
+            target: {
+                cardType: 'creature',
+                controller: 'self',
+                gameAction: ability.actions.returnToDeck({ bottom: true })
+            },
+            then: {
+                targets: {
+                    cards: {
+                        mode: 'exactly',
+                        numCards: 2,
+                        controller: 'self',
+                        location: 'hand',
+                        gameAction: ability.actions.archive()
+                    }
+                },
+                message: '{0} uses {1} to archive 2 cards'
+            }
+        });
+    }
+}
+
+GanymedeOutpost.id = 'ganymede-outpost';
+
+module.exports = GanymedeOutpost;
