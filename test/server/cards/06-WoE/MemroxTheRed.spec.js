ae729346457b2eff3bd259dde3957240502d4080
Jeremy Stribling <strib@alum.mit.edu>
2023-06-04 17:21:05 -0700

    WoE: make Memrox work when moving/playing cards as well
    Suggested by romanogit.
    
    Issue: #2903

--- test/server/cards/06-WoE/MemroxTheRed.spec.js
+++ test/server/cards/06-WoE/MemroxTheRed.spec.js
@@ -5,10 +5,16 @@ describe('Memrox the Red', function () {
                 player1: {
                     amber: 1,
                     house: 'mars',
-                    inPlay: ['memrox-the-red', 'uxlyx-the-zookeeper'],
+                    inPlay: [
+                        'memrox-the-red',
+                        'uxlyx-the-zookeeper',
+                        'prof-garwynne',
+                        'project-zyx'
+                    ],
                     archives: ['pelf']
                 },
                 player2: {
+                    amber: 5,
                     inPlay: ['bumpsy', 'troll']
                 }
             });
@@ -34,6 +40,37 @@ describe('Memrox the Red', function () {
             this.player1.clickPrompt('brobnar');
             this.player1.clickPrompt('Yes');
             expect(this.bumpsy.location).toBe('hand');
+        });
+
+        it('should not return opponent cards to hand', function () {
+            this.player1.reap(this.uxlyxTheZookeeper);
+            this.player1.clickCard(this.bumpsy);
+            this.player1.endTurn();
+            this.player2.clickPrompt('brobnar');
+            this.player2.endTurn();
+            this.player1.clickPrompt('logos');
+            this.player1.clickPrompt('No');
+            this.player1.reap(this.profGarwynne);
+            this.player1.clickCard(this.bumpsy);
+            expect(this.bumpsy.location).toBe('archives');
+            expect(this.player2.archives.length).toBe(0);
+            expect(this.player1.archives.length).toBe(2);
+        });
+
+        it('should not play opponent cards from archive', function () {
+            this.player1.reap(this.uxlyxTheZookeeper);
+            this.player1.clickCard(this.bumpsy);
+            this.player1.endTurn();
+            this.player2.clickPrompt('brobnar');
+            this.player2.endTurn();
+            this.player1.clickPrompt('logos');
+            this.player1.clickPrompt('No');
+            this.player1.reap(this.projectZyx);
+            this.player1.clickCard(this.bumpsy);
+            expect(this.bumpsy.location).toBe('archives');
+            expect(this.player2.archives.length).toBe(0);
+            expect(this.player1.archives.length).toBe(2);
+            expect(this.player2.amber).toBe(5);
         });
 
         it('should gain amber for each card in their archives', function () {
