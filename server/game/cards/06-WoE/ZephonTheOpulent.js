fab952e4ee8439d7ffc5ddd29f52273ea833bb19
alifeee <alifeeerenn@hotmail.com>
2023-06-07 21:38:00 +0100

    add Zephon's skeleton

---
+++ server/game/cards/06-WoE/ZephonTheOpulent.js
@@ -0,0 +1,11 @@
+const Card = require('../../Card.js');
+
+class ZephonTheOpulent extends Card {
+    // Play: Make 2 token creatures.
+    // Zephon the Opulent cannot be used unless there are 2 or more friendly token creatures in play.
+    // After Reap: Gain 2 amber.
+}
+
+ZephonTheOpulent.id = 'zephon-the-opulent';
+
+module.exports = ZephonTheOpulent;
---
+++ test/server/cards/06-WoE/ZephonTheOpulent.spec.js
@@ -0,0 +1,10 @@
+describe('Zephon the Opulent', function () {
+    describe("'s play effect", function () {
+        it('should make 2 token creatures');
+    });
+    describe("'s reap effect", function () {
+        it('should be disabled if there are less than 2 friendly token creatures in play');
+
+        it('should gain 2 amber');
+    });
+});
