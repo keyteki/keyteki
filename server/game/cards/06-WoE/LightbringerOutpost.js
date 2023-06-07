c699ed6b35262b107128572c84e962bb2e75d742
alifeee <alifeeerenn@hotmail.com>
2023-06-07 14:37:51 +0100

    add "put on bottom of deck" functionality

--- server/game/cards/06-WoE/LightbringerOutpost.js
+++ server/game/cards/06-WoE/LightbringerOutpost.js
@@ -2,6 +2,16 @@ const Card = require('../../Card.js');
 
 class LightbringerOutpost extends Card {
     // Action: Put a friendly creature on the bottom of its owner`s deck. If you do, a friendly creature captures 3 Aember.
+    setupCardAbilities(ability) {
+        this.action({
+            effect: 'put a friendly creature on the bottom of its owner`s deck and capture 3 amber',
+            target: {
+                cardType: 'creature',
+                controller: 'self',
+                gameAction: ability.actions.returnToDeck({ bottom: true })
+            }
+        });
+    }
 }
 
 LightbringerOutpost.id = 'lightbringer-outpost';
--- test/server/cards/06-WoE/LightbringerOutpost.spec.js
+++ test/server/cards/06-WoE/LightbringerOutpost.spec.js
@@ -6,13 +6,31 @@ describe('Lightbringer Outpost', function () {
                     house: 'sanctum',
                     amber: 2,
                     inPlay: ['chelonia', 'flaxia', 'lightbringer-outpost'],
-                    hand: ['troll']
+                    hand: ['troll'],
+                    deck: ['krump', 'alaka']
                 },
                 player2: {
                     amber: 5,
                     inPlay: ['urchin', 'valoocanth', 'floomf']
                 }
             });
+        });
+
+        it('should offer friendly creatures as selections for use', function () {
+            this.player1.useAction(this.lightbringerOutpost);
+
+            expect(this.player1).toBeAbleToSelect(this.chelonia);
+            expect(this.player1).toBeAbleToSelect(this.flaxia);
+            expect(this.player1).not.toBeAbleToSelect(this.urchin);
+        });
+
+        it('should put a creature on the bottom of the deck', function () {
+            this.player1.useAction(this.lightbringerOutpost);
+            this.player1.clickCard(this.chelonia);
+
+            expect(this.player1.player.deck[this.player1.player.deck.length - 1]).toBe(
+                this.chelonia
+            );
         });
     });
 });
