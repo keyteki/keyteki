a1afc54bc851899827ec8525da12fc6a097c94a8
alifeee <alifeeerenn@hotmail.com>
2023-06-07 21:05:11 +0100

    "apply lasting effect" does not give deploy properly yet, but gives keyword

--- server/game/cards/06-WoE/Knightapult.js
+++ server/game/cards/06-WoE/Knightapult.js
@@ -2,6 +2,25 @@ const Card = require('../../Card.js');
 
 class Knightapult extends Card {
     // Action: The next time a friendly creature enters play this turn, you may have it enter anywhere in your battleline, ready.
+    setupCardAbilities(ability) {
+        this.action({
+            effect: 'have the next friendly creature enter play anywhere in your battleline, ready',
+            gameAction: ability.actions.forRemainderOfTurn((context) => ({
+                when: {
+                    onCardPlayed: (event) =>
+                        event.player === context.player && event.card.type === 'creature'
+                },
+                effect: 'have {1} enter play anywhere in their battleline, ready',
+                effectArgs: (event) => [event.card],
+                multipleTrigger: false,
+                gameAction: ability.actions.cardLastingEffect((context) => ({
+                    targetLocation: 'play area',
+                    target: context.event.card,
+                    effect: ability.effects.addKeyword('deploy')
+                }))
+            }))
+        });
+    }
 }
 
 Knightapult.id = 'knightapult';
