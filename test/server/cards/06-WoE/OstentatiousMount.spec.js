0dc7e972c8281b13ae27ef9403948829ef8a9242
alifeee <alifeeerenn@hotmail.com>
2023-06-07 13:52:18 +0100

    add selection test

--- test/server/cards/06-WoE/OstentatiousMount.spec.js
+++ test/server/cards/06-WoE/OstentatiousMount.spec.js
@@ -9,7 +9,7 @@ describe('OstentatiousMount', function () {
                     hand: ['ostentatious-mount']
                 },
                 player2: {
-                    inPlay: ['urchin', 'bad-penny', 'rad-penny']
+                    inPlay: ['urchin', 'hookmaster', 'floomf']
                 }
             });
         });
@@ -24,6 +24,8 @@ describe('OstentatiousMount', function () {
             this.player1.playUpgrade(this.ostentatiousMount, this.chelonia);
 
             expect(this.player1).toHavePrompt('Select a card to move this card next to');
+            expect(this.player1).toBeAbleToSelect(this.gub);
+            expect(this.player1).not.toBeAbleToSelect(this.urchin);
         });
 
         it('should move the creature', function () {
@@ -44,16 +46,26 @@ describe('OstentatiousMount', function () {
             expect(this.chelonia.hasKeyword('taunt')).toBe(true);
         });
 
-        it('should be playable on enemy creatures', function () {
-            this.player1.playUpgrade(this.ostentatiousMount, this.urchin);
-            this.player1.clickCard(this.radpenny);
-            this.player1.clickPrompt('Left');
+        describe('should be playable on enemy creatures', function () {
+            it('should select enemy creatures', function () {
+                this.player1.playUpgrade(this.ostentatiousMount, this.urchin);
 
-            expect(this.player1.player.amber).toBe(3);
-            expect(this.urchin.hasKeyword('taunt')).toBe(true);
-            expect(this.player2.player.cardsInPlay[0]).toBe(this.badpenny);
-            expect(this.player2.player.cardsInPlay[1]).toBe(this.urchin);
-            expect(this.player2.player.cardsInPlay[2]).toBe(this.radpenny);
+                expect(this.player1).toHavePrompt('Select a card to move this card next to');
+                expect(this.player1).toBeAbleToSelect(this.hookmaster);
+                expect(this.player1).not.toBeAbleToSelect(this.chelonia);
+            });
+
+            it('should move and give taunt to enemy', function () {
+                this.player1.playUpgrade(this.ostentatiousMount, this.urchin);
+                this.player1.clickCard(this.floomf);
+                this.player1.clickPrompt('Left');
+
+                expect(this.player1.player.amber).toBe(3);
+                expect(this.urchin.hasKeyword('taunt')).toBe(true);
+                expect(this.player2.player.cardsInPlay[0]).toBe(this.hookmaster);
+                expect(this.player2.player.cardsInPlay[1]).toBe(this.urchin);
+                expect(this.player2.player.cardsInPlay[2]).toBe(this.floomf);
+            });
         });
     });
 });
