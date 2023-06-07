98821f84d88460e8bfd95735d1812a020efd6eca
alifeee <alifeeerenn@hotmail.com>
2023-06-07 21:53:53 +0100

    make Zephon generate token creatures

--- server/game/cards/06-WoE/ZephonTheOpulent.js
+++ server/game/cards/06-WoE/ZephonTheOpulent.js
@@ -4,6 +4,14 @@ class ZephonTheOpulent extends Card {
     // Play: Make 2 token creatures.
     // Zephon the Opulent cannot be used unless there are 2 or more friendly token creatures in play.
     // After Reap: Gain 2 amber.
+    setupCardAbilities(ability) {
+        this.play({
+            effect: 'make 2 token creatures',
+            gameAction: ability.actions.makeTokenCreature({
+                amount: 2
+            })
+        });
+    }
 }
 
 ZephonTheOpulent.id = 'zephon-the-opulent';
