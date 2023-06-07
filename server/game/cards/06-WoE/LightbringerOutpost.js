48599f08a8694105248d715262050711ad8ed6c7
alifeee <alifeeerenn@hotmail.com>
2023-06-07 14:24:01 +0100

    add LightbringerOutpost

---
+++ server/game/cards/06-WoE/LightbringerOutpost.js
@@ -0,0 +1,9 @@
+const Card = require('../../Card.js');
+
+class LightbringerOutpost extends Card {
+    // Action: Put a friendly creature on the bottom of its owner`s deck. If you do, a friendly creature captures 3 Aember.
+}
+
+LightbringerOutpost.id = 'lightbringer-outpost';
+
+module.exports = LightbringerOutpost;
