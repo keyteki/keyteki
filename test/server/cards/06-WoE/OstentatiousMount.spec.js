89cac43b6c4c201c8d0f01f1ab97ac3bd8f16f3d
alifeee <alifeeerenn@hotmail.com>
2023-06-07 12:59:17 +0100

    add test OstentatiousMount.spec.js

---
+++ test/server/cards/06-WoE/OstentatiousMount.spec.js
@@ -0,0 +1,32 @@
+describe('OstentatiousMount', function () {
+    describe('Upgrade test', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'sanctum',
+                    inPlay: ['chelonia', 'flaxia', 'gub'],
+                    hand: ['ostentatious-mount']
+                },
+                player2: {
+                    inPlay: ['urchin', 'bad-penny', 'rad-penny']
+                }
+            });
+        });
+
+        it('should provide a prompt to move the creature', function () {
+            this.player1.playUpgrade(this.ostentatiousMount, this.chelonia);
+
+            expect(this.player1).toHavePrompt('Select a card to move this card next to');
+        });
+
+        it('should move the creature', function () {
+            this.player1.playUpgrade(this.ostentatiousMount, this.chelonia);
+            this.player1.clickCard(this.gub);
+            this.player1.clickPrompt('Left');
+
+            expect(this.player1.player.cardsInPlay[0]).toBe(this.flaxia);
+            expect(this.player1.player.cardsInPlay[1]).toBe(this.chelonia);
+            expect(this.player1.player.cardsInPlay[2]).toBe(this.gub);
+        });
+    });
+});
