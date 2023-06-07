5502683ff78aa82914e01fcfa07ccfb639d128c4
alifeee <alifeeerenn@hotmail.com>
2023-06-07 21:43:34 +0100

    add test board setups

--- test/server/cards/06-WoE/ZephonTheOpulent.spec.js
+++ test/server/cards/06-WoE/ZephonTheOpulent.spec.js
@@ -1,10 +1,38 @@
 describe('Zephon the Opulent', function () {
     describe("'s play effect", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'sanctum',
+                    token: 'cleric',
+                    amber: 3,
+                    hand: ['zephon-the-opulent'],
+                    deck: ['chelonia', 'toad']
+                },
+                player2: {
+                    inPlay: ['troll', 'gub']
+                }
+            });
+        });
+
         it('should make 2 token creatures');
     });
     describe("'s reap effect", function () {
-        it('should be disabled if there are less than 2 friendly token creatures in play');
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'sanctum',
+                    amber: 3,
+                    inPlay: ['zephon-the-opulent', 'cleric:chelonia', 'cleric:toad']
+                },
+                player2: {
+                    inPlay: ['troll', 'gub']
+                }
+            });
+        });
 
         it('should gain 2 amber');
+
+        it('should be disabled if there are less than 2 friendly token creatures in play');
     });
 });
