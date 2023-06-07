732779a7dfa5e893ddcf45de0f476cb8efcf2b03
alifeee <alifeeerenn@hotmail.com>
2023-06-07 20:45:03 +0100

    spec out tests

--- test/server/cards/06-WoE/Knightapult.spec.js
+++ test/server/cards/06-WoE/Knightapult.spec.js
@@ -1,17 +1,35 @@
 describe('Knightapult', function () {
-    describe("Knightapult's action", function () {
+    describe('action', function () {
         beforeEach(function () {
             this.setupTest({
                 player1: {
                     house: 'sanctum',
                     amber: 3,
                     inPlay: ['chelonia', 'knightapult'],
-                    hand: ['holdfast']
+                    hand: ['holdfast', 'berinon']
                 },
                 player2: {
                     inPlay: ['troll', 'gub']
                 }
             });
+
+            this.player1.useAction(this.knightapult);
         });
+
+        describe('should cause the next creature played', function () {
+            beforeEach(function () {
+                this.player1.play(this.holdfast);
+            });
+
+            it('to have deploy', function () {
+                expect(this.player1).toHavePrompt('Deploy Left');
+                expect(this.player1).toHavePrompt('Deploy Right');
+                expect(this.holdfast.hasKeyword('deploy')).toBe(true);
+            });
+
+            it('to be ready');
+        });
+
+        it('should only apply to the first creature played');
     });
 });
