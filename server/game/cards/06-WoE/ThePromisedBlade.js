f148a5a399a8cd60b61b0cdc2262993496a09040
David Brokaw <dgbrokaw2@gmail.com>
2023-06-08 12:50:33 -0600

    The Promised Blade WIP: taking control in select case not working

---
+++ server/game/cards/06-WoE/ThePromisedBlade.js
@@ -0,0 +1,63 @@
+const Card = require('../../Card.js');
+
+class ThePromisedBlade extends Card {
+    setupCardAbilities(ability) {
+        this.omni({
+            target: {
+                cardType: 'creature',
+                controller: 'self',
+                gameAction: ability.actions.capture(() => ({
+                    amount: 1
+                }))
+            }
+        });
+
+        // Even creature count, active player chooses
+        this.reaction({
+            when: {
+                onBeginRound: (_, context) =>
+                    !!context.source.controller.opponent &&
+                    context.source.controller.creaturesInPlay.length ==
+                        context.source.controller.opponent.creaturesInPlay.length
+            },
+            targets: {
+                select: {
+                    mode: 'select',
+                    activePromptTitle: 'Choose which player takes control of The Promised Blade',
+                    choices: {
+                        Me: () => true,
+                        Opponent: () => true
+                    }
+                },
+                then: {
+                    gameAction: ability.actions.cardLastingEffect((context) => ({
+                        duration: 'lastingEffect',
+                        effect: ability.effects.takeControl(
+                            context.selects.select.choice == 'Me'
+                                ? context.player
+                                : context.player.opponent
+                        )
+                    }))
+                }
+            }
+        });
+
+        // Uneven creature count, automatic
+        this.reaction({
+            when: {
+                onBeginRound: (_, context) =>
+                    !!context.source.controller.opponent &&
+                    context.source.controller.creaturesInPlay.length >
+                        context.source.controller.opponent.creaturesInPlay.length
+            },
+            gameAction: ability.actions.cardLastingEffect((context) => ({
+                duration: 'lastingEffect',
+                effect: ability.effects.takeControl(context.source.controller.opponent)
+            }))
+        });
+    }
+}
+
+ThePromisedBlade.id = 'the-promised-blade';
+
+module.exports = ThePromisedBlade;
--- test/server/cards/06-WoE/ThePromisedBlade.spec.js
+++ test/server/cards/06-WoE/ThePromisedBlade.spec.js
@@ -1,43 +1,69 @@
 describe('The Promised Blade', function () {
     describe("The Promised Blade's ability", function () {
-        beforeEach(function () {
-            this.setupTest({
-                player1: {
-                    house: 'sanctum',
-                    inPlay: [
-                        'baldric-the-bold',
-                        'pandulf-the-provoker',
-                        'grey-abbess',
-                        'the-promised-blade'
-                    ]
-                },
-                player2: {
-                    amber: 1,
-                    inPlay: ['blorb']
-                }
+        describe('when players have a different amount of creatures', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    player1: {
+                        house: 'sanctum',
+                        inPlay: [
+                            'baldric-the-bold',
+                            'pandulf-the-provoker',
+                            'challe-the-safeguard',
+                            'the-promised-blade'
+                        ]
+                    },
+                    player2: {
+                        amber: 1,
+                        inPlay: ['tyxl-beambuckler'],
+                        hand: ['myx-the-tallminded', 'xanthyx-harvester']
+                    }
+                });
+            });
+
+            it('should automatically move to the player with the fewest number of creatures at the start of each turn', function () {
+                this.player1.endTurn();
+                this.player2.clickPrompt('mars');
+                expect(this.thePromisedBlade.controller).toBe(this.player2.player);
+            });
+
+            it('has an omni to capture 1', function () {
+                this.player1.useAction(this.thePromisedBlade, true);
+                this.player1.clickCard(this.baldricTheBold);
+                expect(this.baldricTheBold.tokens.amber).toBe(1);
+                expect(this.player2.amber).toBe(0);
             });
         });
 
-        it("should move the player with the fewest number of creatures at the start of each player's turn", function () {
-            this.player1.endTurn();
-            this.player2.clickPrompt('mars');
-            expect(this.thePromisedBlade.controller).toBe(this.player2.player);
-        });
+        describe('when players have the same number of creatures', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    player1: {
+                        house: 'sanctum',
+                        inPlay: [
+                            'baldric-the-bold',
+                            'pandulf-the-provoker',
+                            'challe-the-safeguard',
+                            'the-promised-blade'
+                        ]
+                    },
+                    player2: {
+                        inPlay: ['tyxl-beambuckler'],
+                        hand: ['myx-the-tallminded', 'xanthyx-harvester']
+                    }
+                });
+            });
 
-        it("should give a prompt to select between players if there's a tie for fewest creatures", function () {
-            this.baldricTheBold.location = 'discard';
-            this.greyAbbess.location = 'discard';
-            this.player1.endTurn();
-            expect(this.player2).toHavePrompt('The Promised Blade');
-            this.player2.clickPrompt('Me');
-            expect(this.thePromisedBlade.controller).toBe(this.player2.player);
-        });
-
-        it('has an omni to capture 1', function () {
-            this.player1.useAction(this.thePromisedBlade, true);
-            this.player1.clickCard(this.baldricTheBold);
-            expect(this.baldricTheBold.tokens.amber).toBe(1);
-            expect(this.player2.amber).toBe(0);
+            it("should give a prompt to select between players if there's a tie for fewest creatures", function () {
+                this.player1.endTurn();
+                expect(this.thePromisedBlade.controller).toBe(this.player2.player); // player 2 has fewer creatures, they get the blade at start of turn
+                this.player2.clickPrompt('mars');
+                this.player2.play(this.myxTheTallminded);
+                this.player2.play(this.xanthyxHarvester);
+                this.player2.endTurn();
+                expect(this.player1).toHavePrompt('The Promised Blade'); // same number of creatures now, active player chooses
+                this.player1.clickPrompt('Me');
+                expect(this.thePromisedBlade.controller).toBe(this.player1.player);
+            });
         });
     });
 });
