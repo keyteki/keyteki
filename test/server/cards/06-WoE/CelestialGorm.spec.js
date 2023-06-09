59940896ba2be6890de74c669181c871d7fb972b
ryanch <ryanch@gmail.com>
2023-06-08 21:29:05 -0700

    add fix Autogenerated  cards

--- test/server/cards/06-WoE/CelestialGorm.spec.js
+++ test/server/cards/06-WoE/CelestialGorm.spec.js
@@ -6,86 +6,32 @@ describe('CelestialGorm', function () {
                     house: 'untamed',
                     amber: 1,
                     hand: ['bubbles'],
-                    inPlay: ['flaxia']
+                    inPlay: ['celestial-gorm', 'round-table']
                 },
                 player2: {
                     amber: 1,
-                    inPlay: ['gub', 'krump']
+                    inPlay: ['gub', 'krump', 'irestaff']
                 }
             });
         });
 
-        it('should have tests', function () {
-            // TODO write your code here
-        });
+        it('should move friendly and opponent artifacts to hands and destroy itself', function () {
+            this.player1.clickCard(this.celestialGorm);
+            this.player1.clickPrompt("Use this card's Omni ability");
 
-        // examples repo (clean bellow after use)
-        /*
-        it('turn ending test', function () {
+            expect(this.celestialGorm.location).toBe('discard');
+            expect(this.roundTable.location).toBe('hand');
+            expect(this.irestaff.location).toBe('hand');
+
             this.player1.endTurn();
-            this.player2.clickPrompt('untamed');
-            this.player2.endTurn();
-            this.player1.forgeKey('Red');
-            this.player1.clickPrompt('untamed');
         });
 
-        it('creature token', function () {
-            expect(this.mookling.tokens.power).toBeUndefined();
-            this.mookling.addToken('power');
-            expect(this.mookling.tokens.power).toBe(1);
-            
-            expect(this.mookling.tokens.damage).toBeUndefined();
-            this.mookling.addToken('damage');
-            expect(this.mookling.tokens.damage).toBe(1);
-            
-            expect(this.mookling.tokens.amber).toBeUndefined();
-            this.mookling.addToken('amber');
-            expect(this.mookling.tokens.amber).toBe(1);
-        });
+        it('should destroy itself if there is no artifiacts in play', function () {
+            this.player1.clickCard(this.celestialGorm);
+            this.player1.clickPrompt("Use this card's Omni ability");
+            expect(this.celestialGorm.location).toBe('discard');
 
-        it('creature amber test', function () {
-            this.urchin.tokens.amber = 1;
+            this.player1.endTurn();
         });
-
-        it('location tests', function () {
-            expect(this.mother.location).toBe('discard');
-            expect(this.mother.location).toBe('hand');
-            expect(this.mother.location).toBe('deck');
-            expect(this.mother.location).toBe('play area');
-        });
-
-        it('game interation selection', function () {
-            expect(this.player1).not.toBeAbleToSelect(this.gub);
-            expect(this.player1).toBeAbleToSelect(this.flaxia);
-            this.player1.clickPrompt('Done');
-            this.player1.clickCard(this.larva);
-            expect(this.player1).toHavePromptButton('Done');
-            expect(this.player1).not.toHavePromptButton('Done');
-        });
-
-        it('basic actions tests', function () {
-            this.player1.play(this.cocoon);
-            this.player1.useAction(this.cocoon);
-            this.player1.reap(this.cocoon);
-            this.player1.fight(this.cocoon);
-        });
-
-        it('player amber test', function () {
-            this.player1.amber = 2
-            expect(this.player1.amber).toBe(2);
-        });
-
-        it('tide test', function () {
-            this.player1.lowerTide();
-            expect(this.player1.isTideHigh()).toBe(false);
-            this.player1.raiseTide();
-        });
-
-        it('moving cards test', function () {
-            this.player1.moveCard(this.butterfly, 'play area');
-            this.player1.moveCard(this.butterfly, 'discard');
-            this.player1.moveCard(this.butterfly, 'hand');
-        });
-        */
     });
 });
--- test/server/cards/06-WoE/EchoReflector.spec.js
+++ test/server/cards/06-WoE/EchoReflector.spec.js
@@ -3,89 +3,41 @@ describe('EchoReflector', function () {
         beforeEach(function () {
             this.setupTest({
                 player1: {
-                    house: 'untamed',
+                    house: 'staralliance',
                     amber: 1,
-                    hand: ['bubbles'],
+                    hand: ['echo-reflector'],
                     inPlay: ['flaxia']
                 },
                 player2: {
-                    amber: 1,
+                    amber: 6,
                     inPlay: ['gub', 'krump']
                 }
             });
         });
 
-        it('should have tests', function () {
-            // TODO write your code here
-        });
-
-        // examples repo (clean bellow after use)
-        /*
-        it('turn ending test', function () {
+        it('should prevent forging for 6', function () {
+            this.player1.playUpgrade(this.echoReflector, this.flaxia);
             this.player1.endTurn();
-            this.player2.clickPrompt('untamed');
+
+            expect(this.player2).not.toHavePrompt('Forge a Key');
+            expect(this.player2.player.getForgedKeys()).toBe(0);
+            expect(this.player2.player.amber).toBe(6);
+            this.player2.clickPrompt('dis');
+
             this.player2.endTurn();
-            this.player1.forgeKey('Red');
-            this.player1.clickPrompt('untamed');
         });
 
-        it('creature token', function () {
-            expect(this.mookling.tokens.power).toBeUndefined();
-            this.mookling.addToken('power');
-            expect(this.mookling.tokens.power).toBe(1);
-            
-            expect(this.mookling.tokens.damage).toBeUndefined();
-            this.mookling.addToken('damage');
-            expect(this.mookling.tokens.damage).toBe(1);
-            
-            expect(this.mookling.tokens.amber).toBeUndefined();
-            this.mookling.addToken('amber');
-            expect(this.mookling.tokens.amber).toBe(1);
-        });
+        it('should allow forging for 9', function () {
+            this.player2.amber = 9;
+            this.player1.playUpgrade(this.echoReflector, this.flaxia);
+            this.player1.endTurn();
 
-        it('creature amber test', function () {
-            this.urchin.tokens.amber = 1;
-        });
+            this.player2.forgeKey('Red');
+            expect(this.player2.player.getForgedKeys()).toBe(1);
+            expect(this.player2.player.amber).toBe(0);
+            this.player2.clickPrompt('dis');
 
-        it('location tests', function () {
-            expect(this.mother.location).toBe('discard');
-            expect(this.mother.location).toBe('hand');
-            expect(this.mother.location).toBe('deck');
-            expect(this.mother.location).toBe('play area');
+            this.player2.endTurn();
         });
-
-        it('game interation selection', function () {
-            expect(this.player1).not.toBeAbleToSelect(this.gub);
-            expect(this.player1).toBeAbleToSelect(this.flaxia);
-            this.player1.clickPrompt('Done');
-            this.player1.clickCard(this.larva);
-            expect(this.player1).toHavePromptButton('Done');
-            expect(this.player1).not.toHavePromptButton('Done');
-        });
-
-        it('basic actions tests', function () {
-            this.player1.play(this.cocoon);
-            this.player1.useAction(this.cocoon);
-            this.player1.reap(this.cocoon);
-            this.player1.fight(this.cocoon);
-        });
-
-        it('player amber test', function () {
-            this.player1.amber = 2
-            expect(this.player1.amber).toBe(2);
-        });
-
-        it('tide test', function () {
-            this.player1.lowerTide();
-            expect(this.player1.isTideHigh()).toBe(false);
-            this.player1.raiseTide();
-        });
-
-        it('moving cards test', function () {
-            this.player1.moveCard(this.butterfly, 'play area');
-            this.player1.moveCard(this.butterfly, 'discard');
-            this.player1.moveCard(this.butterfly, 'hand');
-        });
-        */
     });
 });
