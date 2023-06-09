61a91f29133f4382b975c7fe40a48cd970c97bf2
Patrick Ehrenreich <31377840+pxe1833@users.noreply.github.com>
2023-06-09 04:02:58 -0400

    Nav Revamp (#3103)
    * Update navigation
    
    * add CSS comment'
    
    * do css with loop
    
    * rename link to be what it is
    
    * reinsert bootstrap components
    
    * add .item and .link controls
    
    * Fix a couple of minor non issues
    
    * Eslint should really have caught the lack of braces, but didn't
    
    ---------
    
    Co-authored-by: Stuart Walsh <stu@ipng.org.uk>

--- client/Components/Navigation/Navigation.jsx
+++ client/Components/Navigation/Navigation.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 
-import Link from './Link';
 import { RightMenu, ProfileMenu, LeftMenu } from '../../menus';
 import LanguageSelector from './LanguageSelector';
 import ProfileDropdown from './ProfileDropdown';
@@ -11,6 +9,8 @@ import ServerStatus from './ServerStatus';
 import GameContextMenu from './GameContextMenu';
 
 import './Navigation.scss';
+import Link from './Link';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 /**
  * @typedef { import('../../menus').MenuItem } MenuItem
@@ -28,32 +28,27 @@ import './Navigation.scss';
 const Navigation = (props) => {
     const { t } = useTranslation();
     const {
+        gameConnected,
+        gameConnecting,
+        gameResponse,
         games,
         currentGame,
         lobbyResponse,
         lobbySocketConnected,
         lobbySocketConnecting
-    } = useSelector((state) => ({
-        games: state.lobby.games,
-        currentGame: state.lobby.currentGame,
-        lobbyResponse: state.lobby.responseTime,
-        lobbySocketConnected: state.lobby.connected,
-        lobbySocketConnecting: state.lobby.connecting
-    }));
-    const { gameConnected, gameConnecting, gameResponse } = useSelector((state) => ({
-        gameConnected: state.games.connected,
-        gameConnecting: state.games.connecting,
-        gameResponse: state.games.responseTime
-    }));
-
-    /**
-     * @param {MenuItem} menuItem The menu item
-     * @param {User} user The logged in user
-     * @returns {boolean} Whether or not the user can see this menu item
-     */
-    const userCanSeeMenu = (menuItem, user) => {
-        return !menuItem.permission || (!!user && user.permissions[menuItem.permission]);
-    };
+    } = useSelector(
+        (state) => ({
+            gameConnected: state.games.connected,
+            gameConnecting: state.games.connecting,
+            gameResponse: state.games.responseTime,
+            games: state.lobby.games,
+            currentGame: state.lobby.currentGame,
+            lobbyResponse: state.lobby.responseTime,
+            lobbySocketConnected: state.lobby.connected,
+            lobbySocketConnecting: state.lobby.connecting
+        }),
+        null
+    );
 
     /**
      * Filter a list of menu items to what the logged in user can see
@@ -61,27 +56,14 @@ const Navigation = (props) => {
      * @param {User} user The logged in user
      * @returns {MenuItem[]} The filtered menu items
      */
-    const filterMenuItems = (menuItems, user) => {
-        const returnedItems = [];
-
-        for (const menuItem of menuItems) {
-            if (user && menuItem.showOnlyWhenLoggedOut) {
-                continue;
-            }
-
-            if (!user && menuItem.showOnlyWhenLoggedIn) {
-                continue;
-            }
-
-            if (!userCanSeeMenu(menuItem, user)) {
-                continue;
-            }
-
-            returnedItems.push(menuItem);
-        }
-
-        return returnedItems;
-    };
+    const filterMenuItems = (menuItems, user) =>
+        menuItems.filter(
+            (item) =>
+                ((user && !item.showOnlyWhenLoggedOut) ||
+                    (!user && item.showOnlyWhenLoggedIn) ||
+                    (item.permission && user && user.permissions[item.permission])) &&
+                (item.path || item.childItems)
+        );
 
     /**
      * Render a list of menu items to react components
@@ -92,77 +74,64 @@ const Navigation = (props) => {
         return filterMenuItems(menuItems, props.user).map((menuItem) => {
             const children =
                 menuItem.childItems && filterMenuItems(menuItem.childItems, props.user);
-            if (children && children.length > 0) {
-                return (
-                    <NavDropdown
-                        key={menuItem.title}
-                        title={t(menuItem.title)}
-                        id={`nav-${menuItem.title}`}
-                    >
-                        {children.map((menuItem) => {
-                            if (!menuItem.path) {
-                                return <></>;
-                            }
 
-                            return (
-                                <Link key={menuItem.path} href={menuItem.path}>
-                                    <NavDropdown.Item>{t(menuItem.title)}</NavDropdown.Item>
-                                </Link>
-                            );
-                        })}
-                    </NavDropdown>
-                );
-            }
-
-            if (!menuItem.path) {
-                return <></>;
-            }
-
-            return (
-                <Link key={menuItem.path || menuItem.title} href={menuItem.path}>
-                    <Nav.Link>{t(menuItem.title)}</Nav.Link>
+            return children && children.length > 0 ? (
+                <NavDropdown
+                    key={menuItem.title}
+                    id={`nav-${menuItem.title}`}
+                    title={t(menuItem.title)}
+                >
+                    {children.map((childItem) =>
+                        childItem.path ? (
+                            <Link key={childItem.path} href={childItem.path}>
+                                <NavDropdown.Item
+                                    className={'navbar-item interactable dropdown-child'}
+                                >
+                                    {t(childItem.title)}
+                                </NavDropdown.Item>
+                            </Link>
+                        ) : null
+                    )}
+                </NavDropdown>
+            ) : (
+                <Link key={menuItem.title} href={menuItem.path}>
+                    <Nav.Link className={'navbar-item interactable'}>{t(menuItem.title)}</Nav.Link>
                 </Link>
             );
         });
     };
 
-    let numGames = (
-        <li>
-            <span>{t('{{gameLength}} Games', { gameLength: games?.length })}</span>
-        </li>
+    const numGames = games && (
+        <div className={'navbar-item'}>{`${t(`${games.length} Games`)}`}</div>
     );
 
     return (
-        <Navbar bg='dark' variant='dark' className='navbar-sm' fixed='top'>
-            <Link className='navbar-brand' href='/'>
-                <Navbar.Brand></Navbar.Brand>
-            </Link>
-            <Navbar.Toggle aria-controls='navbar' />
+        <Navbar className='navbar-sm navbar-color' fixed='top'>
             <Nav>{renderMenuItems(LeftMenu)}</Nav>
+            <Link href={'/'}>
+                <Navbar.Brand />
+            </Link>
             <Navbar.Collapse id='navbar' className='justify-content-end'>
-                <Nav className='ml-auto pr-md-5'>
-                    <GameContextMenu />
-                    {numGames}
-                    {!currentGame && (
-                        <ServerStatus
-                            connected={lobbySocketConnected}
-                            connecting={lobbySocketConnecting}
-                            serverType='Lobby'
-                            responseTime={lobbyResponse}
-                        />
-                    )}
-                    {currentGame?.started && (
-                        <ServerStatus
-                            connected={gameConnected}
-                            connecting={gameConnecting}
-                            serverType='Game server'
-                            responseTime={gameResponse}
-                        />
-                    )}
-                    {renderMenuItems(RightMenu)}
-                    <ProfileDropdown menu={ProfileMenu} user={props.user} />
-                    <LanguageSelector />
-                </Nav>
+                <GameContextMenu />
+                {numGames}
+                {currentGame?.started ? (
+                    <ServerStatus
+                        connected={gameConnected}
+                        connecting={gameConnecting}
+                        serverType='Game server'
+                        responseTime={gameResponse}
+                    />
+                ) : (
+                    <ServerStatus
+                        connected={lobbySocketConnected}
+                        connecting={lobbySocketConnecting}
+                        serverType='Lobby'
+                        responseTime={lobbyResponse}
+                    />
+                )}
+                {renderMenuItems(RightMenu)}
+                <ProfileDropdown menu={ProfileMenu} user={props.user} />
+                <LanguageSelector />
             </Navbar.Collapse>
         </Navbar>
     );
