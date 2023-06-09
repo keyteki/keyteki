3fb0ddd748d838f987f47677358b014ebacb5b35
Jeremy Stribling <strib@alum.mit.edu>
2023-06-08 22:23:42 -0700

    WoE: simplify Catch and Release
    Suggested by romanogit.
    
    Issue: #3197

--- server/game/cards/06-WoE/CatchAndRelease.js
+++ server/game/cards/06-WoE/CatchAndRelease.js
@@ -7,7 +7,8 @@ class CatchAndRelease extends Card {
     // fewer cards in hand. Gain 2 chains.
     setupCardAbilities(ability) {
         this.play({
-            effect: "return all creatures to their owner's hand",
+            effect:
+                "return all creatures to their owner's hand and have eac player discard down to 6 cards",
             gameAction: [
                 ability.actions.returnToHand((context) => ({
                     target: context.game.creaturesInPlay
@@ -17,31 +18,19 @@ class CatchAndRelease extends Card {
             then: {
                 alwaysTriggers: true,
                 gameAction: ability.actions.sequential([
-                    ability.actions.conditional({
-                        condition: (context) => context.player.hand.length > 6,
-                        trueGameAction: ability.actions.discard((context) => ({
-                            target: _.shuffle(context.player.hand)[0]
-                        }))
-                    }),
-                    ability.actions.conditional({
-                        condition: (context) =>
-                            !!context.player.opponent && context.player.opponent.hand.length > 6,
-                        trueGameAction: ability.actions.discard((context) => ({
-                            target: _.shuffle(context.player.opponent.hand)[0]
-                        }))
-                    })
-                ]),
-                then: (preThenContext) => ({
-                    alwaysTriggers: true,
-                    gameAction: ability.actions.conditional({
-                        condition: (context) =>
-                            context.player.hand.length > 6 ||
-                            (!!context.player.opponent && context.player.opponent.hand.length > 6),
-                        trueGameAction: ability.actions.resolveAbility({
-                            ability: preThenContext.ability
-                        })
-                    })
-                })
+                    ability.actions.discard((context) => ({
+                        target:
+                            context.player.hand.length > 6
+                                ? _.shuffle(context.player.hand).slice(6)
+                                : []
+                    })),
+                    ability.actions.discard((context) => ({
+                        target:
+                            !!context.player.opponent && context.player.opponent.hand.length > 6
+                                ? _.shuffle(context.player.opponent.hand).slice(6)
+                                : []
+                    }))
+                ])
             }
         });
     }
