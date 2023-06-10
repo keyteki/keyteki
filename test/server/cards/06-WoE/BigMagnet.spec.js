50e7b59e7df7155db97a9ad38f5cc1dfeb16c330
alifeee <alifeeerenn@hotmail.com>
2023-06-10 15:10:37 +0100

    add Big Magnet test for Disruption Field

--- test/server/cards/06-WoE/BigMagnet.spec.js
+++ test/server/cards/06-WoE/BigMagnet.spec.js
@@ -210,4 +210,78 @@ describe('Big Magnet', function () {
             });
         });
     });
+
+    describe('with Disruption Field played onto friendly creature by enemy,', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'brobnar',
+                    inPlay: ['troll', 'mindworm'],
+                    hand: ['big-magnet']
+                },
+                player2: {
+                    inPlay: ['batdrone'],
+                    hand: ['reckless-experimentation', 'disruption-field']
+                }
+            });
+
+            this.player1.endTurn();
+            this.player2.clickPrompt('staralliance');
+            this.player2.playUpgrade(this.disruptionField, this.mindworm);
+            this.player2.endTurn();
+            this.player1.clickPrompt('mars');
+            this.player1.reap(this.mindworm);
+        });
+
+        describe('before big magnet played,', function () {
+            it('opponent keys should be forgeable', function () {
+                this.player2.amber = 6;
+                this.player1.endTurn();
+
+                expect(this.player2).toHavePrompt('Which key would you like to forge?');
+            });
+
+            it('friendly keys should cost 1 more', function () {
+                this.player1.amber = 6;
+                this.player1.endTurn();
+                this.player2.clickPrompt('logos');
+                this.player2.endTurn();
+
+                expect(this.player1).not.toHavePrompt('Which key would you like to forge?');
+                expect(this.player1).toHavePrompt(
+                    'Choose which house you want to activate this turn'
+                );
+            });
+        });
+
+        describe('after big magnet played,', function () {
+            beforeEach(function () {
+                this.player1.play(this.bigMagnet);
+                this.player1.clickCard(this.troll);
+            });
+
+            it('opponent keys should cost 1 more', function () {
+                this.player2.amber = 6;
+                this.player1.endTurn();
+
+                expect(this.player2).not.toHavePrompt('Which key would you like to forge?');
+                expect(this.player2).toHavePrompt(
+                    'Choose which house you want to activate this turn'
+                );
+            });
+
+            it('friendly keys should be forgeable', function () {
+                this.player1.amber = 6;
+                this.player1.endTurn();
+                this.player2.clickPrompt('logos');
+                this.player2.endTurn();
+
+                expect(this.player1).toHavePrompt('Which key would you like to forge?');
+            });
+
+            it('Disruption field counters should be moved', function () {
+                expect(this.disruptionField.tokens.disruption).toBe(1);
+            });
+        });
+    });
 });
