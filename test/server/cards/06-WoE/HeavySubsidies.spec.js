8986118fe9f2af75272866815f59c2ff8beb3805
alifeee <alifeeerenn@hotmail.com>
2023-06-11 00:05:52 +0100

    mirror tests for both players

--- test/server/cards/06-WoE/HeavySubsidies.spec.js
+++ test/server/cards/06-WoE/HeavySubsidies.spec.js
@@ -1,5 +1,5 @@
 describe('HeavySubsidies', function () {
-    describe("HeavySubsidies's ability", function () {
+    describe("'s ability", function () {
         beforeEach(function () {
             this.setupTest({
                 player1: {
@@ -15,12 +15,22 @@ describe('HeavySubsidies', function () {
             });
         });
 
-        it('should cause extra reap amber', function () {
+        it('should cause extra reap amber for player 1', function () {
             let initialAmber = this.player1.amber;
 
             this.player1.reap(this.holdfast);
 
             expect(this.player1.amber).toBe(initialAmber + 2);
+        });
+
+        it('should cause extra reap amber for player 2', function () {
+            let initialAmber = this.player2.amber;
+            this.player1.endTurn();
+            this.player2.clickPrompt('brobnar');
+
+            this.player2.reap(this.krump);
+
+            expect(this.player2.amber).toBe(initialAmber + 2);
         });
 
         it("should disable player 2's forge step for <12 amber", function () {
@@ -36,6 +46,15 @@ describe('HeavySubsidies', function () {
             this.player2.endTurn();
 
             expect(this.player1).toHavePrompt('Choose which house you want to activate this turn');
+        });
+
+        it("should not disable player 1's forge step for >=12 amber", function () {
+            this.player1.amber = 12;
+            this.player1.endTurn();
+            this.player2.clickPrompt('brobnar');
+            this.player2.endTurn();
+
+            expect(this.player1).toHavePrompt('Which key would you like to forge?');
         });
 
         it("should not disable player 2's forge step for >=12 amber", function () {
