e0a27c434cb48dd8cff5497cce8314a0bf5736c7
alifeee <alifeeerenn@hotmail.com>
2023-06-10 13:04:40 +0100

    reformat tests and add test with no upgrades

--- test/server/cards/06-WoE/BigMagnet.spec.js
+++ test/server/cards/06-WoE/BigMagnet.spec.js
@@ -12,40 +12,60 @@ describe('Big Magnet', function () {
                     hand: ['reckless-experimentation']
                 }
             });
-
-            this.player1.playUpgrade(this.jammerPack, this.zorg);
-            this.player1.playUpgrade(this.biomatrixBackup, this.troll);
-            this.player1.endTurn();
-
-            this.player2.clickPrompt('logos');
-            this.player2.playUpgrade(this.recklessExperimentation, this.batdrone);
-            this.player2.endTurn();
-
-            this.player1.clickPrompt('mars');
-            this.player1.play(this.bigMagnet);
         });
 
-        it('should offer only friendly creatures', function () {
-            expect(this.player1).toHavePrompt('Big Magnet');
-            expect(this.player1).toBeAbleToSelect(this.troll);
-            expect(this.player1).toBeAbleToSelect(this.zorg);
-            expect(this.player1).not.toBeAbleToSelect(this.batdrone);
-        });
-
-        describe('on choosing a creature', function () {
+        describe('with upgrades in play,', function () {
             beforeEach(function () {
-                this.player1.clickCard(this.troll);
+                this.player1.playUpgrade(this.jammerPack, this.zorg);
+                this.player1.playUpgrade(this.biomatrixBackup, this.troll);
+                this.player1.endTurn();
+
+                this.player2.clickPrompt('logos');
+                this.player2.playUpgrade(this.recklessExperimentation, this.batdrone);
+                this.player2.endTurn();
+
+                this.player1.clickPrompt('mars');
+
+                this.player1.play(this.bigMagnet);
             });
 
-            it('should move all upgrades to the chosen creature', function () {
-                expect(this.troll.upgrades).toContain(this.biomatrixBackup);
-                expect(this.troll.upgrades).toContain(this.jammerPack);
-                expect(this.troll.upgrades).toContain(this.recklessExperimentation);
+            it('should offer only friendly creatures', function () {
+                expect(this.player1).toHavePrompt('Big Magnet');
+                expect(this.player1).toBeAbleToSelect(this.troll);
+                expect(this.player1).toBeAbleToSelect(this.zorg);
+                expect(this.player1).not.toBeAbleToSelect(this.batdrone);
             });
 
-            it('should have removed upgrades from the other creatures', function () {
-                expect(this.zorg.upgrades).not.toContain(this.jammerPack);
-                expect(this.batdrone.upgrades).not.toContain(this.recklessExperimentation);
+            describe('on choosing a creature,', function () {
+                beforeEach(function () {
+                    this.player1.clickCard(this.troll);
+                });
+
+                it('should move all upgrades to the chosen creature', function () {
+                    expect(this.troll.upgrades).toContain(this.biomatrixBackup);
+                    expect(this.troll.upgrades).toContain(this.jammerPack);
+                    expect(this.troll.upgrades).toContain(this.recklessExperimentation);
+                });
+
+                it('should have removed upgrades from the other creatures', function () {
+                    expect(this.zorg.upgrades).not.toContain(this.jammerPack);
+                    expect(this.batdrone.upgrades).not.toContain(this.recklessExperimentation);
+                });
+            });
+        });
+
+        describe('without upgrades in play,', function () {
+            beforeEach(function () {
+                this.player1.clickCard(this.bigMagnet);
+            });
+
+            it('should still prompt', function () {
+                expect(this.player1).toHavePrompt('Big Magnet');
+            });
+
+            it('should not do anything', function () {
+                expect(this.troll.upgrades).toEqual([]);
+                expect(this.zorg.upgrades).toEqual([]);
             });
         });
     });
