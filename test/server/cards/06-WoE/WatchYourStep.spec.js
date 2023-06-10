0c43ad8a5c13411057816c07b5ae95a832380d5c
Romano Silva <romano.magacho@gmail.com>
2023-06-10 18:29:22 +0000

    added one more test case

--- test/server/cards/06-WoE/WatchYourStep.spec.js
+++ test/server/cards/06-WoE/WatchYourStep.spec.js
@@ -16,12 +16,18 @@ describe('Watch Your Step', function () {
 
         it('should not make tokens if the opponent follows instructions', function () {
             this.player1.play(this.watchYourStep);
-            expect(this.player1).toHavePromptButton('dis');
-            expect(this.player1).toHavePromptButton('logos');
-            expect(this.player1).toHavePromptButton('brobnar');
-            expect(this.player1).not.toHavePromptButton('mars');
             this.player1.clickPrompt('logos');
             this.player1.endTurn();
+            this.player2.clickPrompt('logos');
+            expect(this.player1.player.creaturesInPlay.length).toBe(0);
+            expect(this.player2).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should not make tokens if the opponent does follow instructions and deck is empty', function () {
+            this.player1.play(this.watchYourStep);
+            this.player1.clickPrompt('logos');
+            this.player1.endTurn();
+            this.player1.player.deck = [];
             this.player2.clickPrompt('logos');
             expect(this.player1.player.creaturesInPlay.length).toBe(0);
             expect(this.player2).toHavePrompt('Choose a card to play, discard or use');
