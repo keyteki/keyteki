bd7e69cdc9f4a94640008085a7524c476a0f7b86
alifeee <alifeeerenn@hotmail.com>
2023-06-10 15:00:31 +0100

    add test for Quadracorder... to test "takes control of" it fails (as expected)

--- test/server/cards/06-WoE/BigMagnet.spec.js
+++ test/server/cards/06-WoE/BigMagnet.spec.js
@@ -71,4 +71,143 @@ describe('Big Magnet', function () {
             });
         });
     });
+
+    describe('with Quadracorder played onto enemy creature,', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'staralliance',
+                    inPlay: ['troll'],
+                    hand: ['big-magnet', 'quadracorder']
+                },
+                player2: {
+                    inPlay: ['batdrone'],
+                    hand: ['reckless-experimentation']
+                }
+            });
+
+            this.player1.playUpgrade(this.quadracorder, this.batdrone);
+        });
+
+        describe('before big magnet played,', function () {
+            it('opponent keys should cost 1 more', function () {
+                this.player2.amber = 6;
+                this.player1.endTurn();
+
+                expect(this.player2).not.toHavePrompt('Which key would you like to forge?');
+                expect(this.player2).toHavePrompt(
+                    'Choose which house you want to activate this turn'
+                );
+            });
+
+            it('friendly keys should be forgeable', function () {
+                this.player1.amber = 6;
+                this.player1.endTurn();
+                this.player2.clickPrompt('logos');
+                this.player2.endTurn();
+
+                expect(this.player1).toHavePrompt('Which key would you like to forge?');
+            });
+        });
+
+        describe('after big magnet played,', function () {
+            beforeEach(function () {
+                this.player1.endTurn();
+                this.player2.clickPrompt('logos');
+                this.player2.endTurn();
+                this.player1.clickPrompt('mars');
+
+                this.player1.play(this.bigMagnet);
+                this.player1.clickCard(this.troll);
+            });
+
+            it('opponent keys should cost 1 more', function () {
+                this.player2.amber = 6;
+                this.player1.endTurn();
+
+                expect(this.player2).not.toHavePrompt('Which key would you like to forge?');
+                expect(this.player2).toHavePrompt(
+                    'Choose which house you want to activate this turn'
+                );
+            });
+
+            it('friendly key should be forgeable', function () {
+                this.player1.endTurn();
+                this.player1.amber = 6;
+                this.player2.clickPrompt('logos');
+                this.player2.endTurn();
+
+                expect(this.player1).toHavePrompt('Which key would you like to forge?');
+            });
+        });
+    });
+
+    describe('with Quadracorder played onto friendly creature by enemy,', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'brobnar',
+                    inPlay: ['troll'],
+                    hand: ['big-magnet']
+                },
+                player2: {
+                    inPlay: ['batdrone'],
+                    hand: ['reckless-experimentation', 'quadracorder']
+                }
+            });
+
+            this.player1.endTurn();
+            this.player2.clickPrompt('staralliance');
+            this.player2.playUpgrade(this.quadracorder, this.troll);
+            this.player2.endTurn();
+            this.player1.clickPrompt('mars');
+        });
+
+        describe('before big magnet played,', function () {
+            it('opponent keys should be forgeable', function () {
+                this.player2.amber = 6;
+                this.player1.endTurn();
+
+                expect(this.player2).toHavePrompt('Which key would you like to forge?');
+            });
+
+            it('friendly keys should cost 1 more', function () {
+                this.player1.amber = 6;
+                this.player1.endTurn();
+                this.player2.clickPrompt('logos');
+                this.player2.endTurn();
+
+                expect(this.player1).not.toHavePrompt('Which key would you like to forge?');
+                expect(this.player1).toHavePrompt(
+                    'Choose which house you want to activate this turn'
+                );
+            });
+        });
+
+        describe('after big magnet played,', function () {
+            beforeEach(function () {
+                this.player1.play(this.bigMagnet);
+                this.player1.clickCard(this.troll);
+            });
+
+            it('opponent keys should cost 1 more', function () {
+                this.player2.amber = 6;
+                this.player1.endTurn();
+
+                expect(this.player2).not.toHavePrompt('Which key would you like to forge?');
+                expect(this.player2).toHavePrompt(
+                    'Choose which house you want to activate this turn'
+                );
+            });
+
+            it('friendly keys should be forgeable', function () {
+                this.player1.amber = 6;
+                this.player1.endTurn();
+                this.player2.clickPrompt('logos');
+                this.player2.endTurn();
+
+                expect(this.player1).toHavePrompt('Which key would you like to forge?');
+            });
+        });
+    });
 });
