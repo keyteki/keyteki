a6e794b06e18a6b9737f230decce8d43c5725a0a
alifeee <alifeeerenn@hotmail.com>
2023-06-10 12:03:28 +0100

    add Big Magnet card

---
+++ server/game/cards/06-WoE/BigMagnet.js
@@ -0,0 +1,9 @@
+const Card = require('../../Card.js');
+
+class BigMagnet extends Card {
+    // Play: Chose a friendly creature. Take control of each upgrade in play and move it to that creature.
+}
+
+BigMagnet.id = 'big-magnet';
+
+module.exports = BigMagnet;
