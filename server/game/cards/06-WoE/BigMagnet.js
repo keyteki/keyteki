a1ef6cc1588ee605ac3c33a18905c7ed9929eec2
alifeee <alifeeerenn@hotmail.com>
2023-06-10 12:49:22 +0100

    setupCardAbilities for Big Magnet (broken - attach action only takes 1 upgrade)

--- server/game/cards/06-WoE/BigMagnet.js
+++ server/game/cards/06-WoE/BigMagnet.js
@@ -2,6 +2,21 @@ const Card = require('../../Card.js');
 
 class BigMagnet extends Card {
     // Play: Choose a friendly creature. Take control of each upgrade in play and move it to that creature.
+    setupCardAbilities(ability) {
+        this.play({
+            target: {
+                controller: 'self',
+                cardType: 'creature',
+                gameAction: ability.actions.attach((context) => ({
+                    target: context.target,
+                    upgrade: context.game.creaturesInPlay
+                        .filter((card) => card !== context.target && card.upgrades.length > 0)
+                        .flatMap((card) => card.upgrades || [])
+                }))
+            },
+            effect: 'take control of each upgrade in play and move it to {0}'
+        });
+    }
 }
 
 BigMagnet.id = 'big-magnet';
