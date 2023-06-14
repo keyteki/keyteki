99cbc8b9a688f716065d4056dbaade17c8da1b1f
alifeee <alifeeerenn@hotmail.com>
2023-06-14 14:59:11 +0100

    add test for no friendly creatures

--- test/server/cards/06-WoE/PullUpStakes.spec.js
+++ test/server/cards/06-WoE/PullUpStakes.spec.js
@@ -1,5 +1,5 @@
 describe('PullUpStakes,', function () {
-    describe('on play,', function () {
+    describe('on play with friendly creatures,', function () {
         beforeEach(function () {
             this.setupTest({
                 player1: {
@@ -56,6 +56,53 @@ describe('PullUpStakes,', function () {
             });
 
             describe('and choosing four enemy creatures,', function () {
+                beforeEach(function () {
+                    this.player1.clickCard(this.gub);
+                    this.player1.clickCard(this.krump);
+                    this.player1.clickCard(this.dendrix);
+                    this.player1.clickCard(this.infurnace);
+                    this.player1.clickPrompt('Done');
+                });
+
+                it("should return the chosen creatures to their owners' hands", function () {
+                    expect(this.gub.location).toBe('hand');
+                    expect(this.krump.location).toBe('hand');
+                    expect(this.dendrix.location).toBe('hand');
+                    expect(this.infurnace.location).toBe('hand');
+                    expect(this.player2.inPlay.length).toBe(1);
+                });
+            });
+        });
+    });
+
+    describe('on play with no friendly creatures,', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'ekwidon',
+                    amber: 1,
+                    hand: ['bubbles', 'pull-up-stakes'],
+                    inPlay: []
+                },
+                player2: {
+                    amber: 1,
+                    inPlay: ['gub', 'krump', 'dendrix', 'infurnace', 'snag']
+                }
+            });
+
+            this.player1.play(this.pullUpStakes);
+        });
+
+        describe('should still work and skip to the 2nd action, and', function () {
+            it('should offer enemy creatures,', function () {
+                expect(this.player1).toHavePrompt('Pull Up Stakes');
+                expect(this.player1).toBeAbleToSelect(this.gub);
+                expect(this.player1).toBeAbleToSelect(this.krump);
+                expect(this.player1).toBeAbleToSelect(this.dendrix);
+                expect(this.player1).toBeAbleToSelect(this.infurnace);
+            });
+
+            describe('on choosing four enemy creatures,', function () {
                 beforeEach(function () {
                     this.player1.clickCard(this.gub);
                     this.player1.clickCard(this.krump);
