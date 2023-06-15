9b26e947eb9abe784b6be2dfce25a91d06b3a35d
Jeremy Stribling <strib@alum.mit.edu>
2023-06-15 14:45:57 -0700

    WoE: give Rampaging Brutodon its play ability
    Not sure how I missed this before...

--- server/game/cards/06-WoE/RampagingBrutodon.js
+++ server/game/cards/06-WoE/RampagingBrutodon.js
@@ -1,11 +1,26 @@
 const Card = require('../../Card.js');
 
 class RampagingBrutodon extends Card {
+    // Play: Make a token creature.
+    //
+    // In order to use Rampaging Brutodon, you must destroy a friendly
+    // creature.
     setupCardAbilities(ability) {
+        this.makeTokenOnPlay = ability.actions.makeTokenCreature();
+
+        this.play({
+            gameAction: this.makeTokenOnPlay
+        });
+
         this.persistentEffect({
             targetController: 'self',
             effect: ability.effects.additionalCost((context) => {
-                if (context.source === this) {
+                if (
+                    context.source === this &&
+                    (!context.ability.gameAction ||
+                        context.ability.gameAction.length === 0 ||
+                        context.ability.gameAction[0] !== this.makeTokenOnPlay)
+                ) {
                     return ability.costs.destroyFriendlyCreature();
                 }
             })
