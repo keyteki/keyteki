5e8e11e4edf4529932e9f5e16f9871e1674d5a86
Jeremy Stribling <strib@alum.mit.edu>
2023-06-21 13:25:53 -0700

    WoE: test for Aristotlmimus playing archived card
    It should not remain in the archives.
    
    Issue: #3308

--- test/server/cards/06-WoE/Aristotlmimus.spec.js
+++ test/server/cards/06-WoE/Aristotlmimus.spec.js
@@ -5,7 +5,7 @@ describe('Aristotlmimus', function () {
                 player1: {
                     house: 'saurian',
                     hand: ['curse-of-vanity', 'tribute'],
-                    archives: ['pelf', 'bumpsy'],
+                    archives: ['pelf', 'bumpsy', 'rant-and-rive'],
                     inPlay: ['aristotlmimus', 'platopelta', 'socraterosaurus']
                 },
                 player2: {
@@ -14,12 +14,20 @@ describe('Aristotlmimus', function () {
             });
         });
 
-        it('play a card from archives when there are no wisdom counters', function () {
+        it('play a creature card from archives when there are no wisdom counters', function () {
             this.player1.reap(this.aristotlmimus);
             this.player1.clickCard(this.bumpsy);
             this.player1.clickPrompt('Right');
+            expect(this.bumpsy.location).toBe('play area');
             expect(this.player1.player.creaturesInPlay.length).toBe(4);
             expect(this.player2.amber).toBe(0);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('play an action card from archives when there are no wisdom counters', function () {
+            this.player1.reap(this.aristotlmimus);
+            this.player1.clickCard(this.rantAndRive);
+            expect(this.rantAndRive.location).toBe('discard');
             expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
         });
 
@@ -42,7 +50,7 @@ describe('Aristotlmimus', function () {
             this.player1.clickCard(this.bumpsy);
             this.player1.clickPrompt('Right');
             expect(this.player1.hand.length).toBe(3); // Draws from all three
-            expect(this.player1.archives.length).toBe(3);
+            expect(this.player1.archives.length).toBe(4);
             expect(this.player1.player.creaturesInPlay.length).toBe(4);
             expect(this.player2.amber).toBe(0);
             expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
