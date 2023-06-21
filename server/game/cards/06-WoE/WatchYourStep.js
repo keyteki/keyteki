f8e84cbd33f2e7a76919e60108ffeab3a2b2c34f
Jeremy Stribling <strib@alum.mit.edu>
2023-06-21 07:47:17 -0700

    WoE: use effect alert for Watch Your Step

--- server/game/cards/06-WoE/WatchYourStep.js
+++ server/game/cards/06-WoE/WatchYourStep.js
@@ -14,6 +14,7 @@ class WatchYourStep extends Card {
             effect:
                 'make and ready 2 token creatures if {1} does not choose {2} as their active house next turn',
             effectArgs: (context) => [context.player.opponent, context.house],
+            effectAlert: true,
             gameAction: ability.actions.nextRoundEffect((context) => ({
                 targetController: 'opponent',
                 when: {
