8bf1444adbf9f4acdda2710ab7199a18f9d96567
Jeremy Stribling <strib@alum.mit.edu>
2023-06-28 14:32:35 -0700

    WoE: don't let The Promised Blade target any artifact
    It was treating `then:` as another target.
    
    Fixes #3383

--- server/game/cards/06-WoE/ThePromisedBlade.js
+++ server/game/cards/06-WoE/ThePromisedBlade.js
@@ -30,18 +30,20 @@ class ThePromisedBlade extends Card {
                         Me: () => true,
                         Opponent: () => true
                     }
-                },
-                then: {
-                    gameAction: ability.actions.cardLastingEffect((context) => ({
-                        duration: 'lastingEffect',
-                        effect: ability.effects.takeControl(
-                            context.selects.select.choice === 'Me'
-                                ? context.game.activePlayer
-                                : context.game.activePlayer.opponent
-                        )
-                    }))
                 }
-            }
+            },
+            then: (preThenContext) => ({
+                alwaysTriggers: true,
+                gameAction: ability.actions.cardLastingEffect((context) => ({
+                    duration: 'lastingEffect',
+                    target: context.source,
+                    effect: ability.effects.takeControl(
+                        preThenContext.selects.select.choice === 'Me'
+                            ? context.game.activePlayer
+                            : context.game.activePlayer.opponent
+                    )
+                }))
+            })
         });
 
         // Uneven creature count, automatic
@@ -54,6 +56,7 @@ class ThePromisedBlade extends Card {
             },
             gameAction: ability.actions.cardLastingEffect((context) => ({
                 duration: 'lastingEffect',
+                target: context.source,
                 effect: ability.effects.takeControl(context.source.controller.opponent)
             }))
         });
