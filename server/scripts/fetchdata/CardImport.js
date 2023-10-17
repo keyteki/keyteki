8480d6072dfa516f28f30725ffbe576c0618d63c
Romano Silva <romano.magacho@gmail.com>
2023-10-17 23:55:31 +0000

    Enhance fetchdata script

--- server/scripts/fetchdata/CardImport.js
+++ server/scripts/fetchdata/CardImport.js
@@ -3,18 +3,24 @@
 const fs = require('fs');
 const mkdirp = require('mkdirp');
 const path = require('path');
-const fabric = require('fabric').fabric;
 
 const CardService = require('../../services/CardService');
 const ConfigService = require('../../services/ConfigService');
 
+const LANGUAGE_MAP = {
+    zhhans: 'zh-hans',
+    zhhant: 'zh-hant'
+};
+
 class CardImport {
-    constructor(dataSource, imageSource, imageDir, language, buildHalfSize) {
+    constructor(dataSource, imageSource, imageDir, language, force, skipReplace) {
         this.dataSource = dataSource;
         this.imageSource = imageSource;
         this.imageDir = imageDir;
         this.language = language;
-        this.buildHalfSize = buildHalfSize;
+        this.force = force;
+        this.skipReplace = skipReplace;
+        this.buildHalfSize = imageSource.getHalfSizeBuilder();
         this.cardService = new CardService(new ConfigService());
     }
 
@@ -31,9 +37,12 @@ class CardImport {
     async importCards() {
         let cards = await this.dataSource.getCards();
 
-        cards = cards.sort((a, b) => (a.expansion > b.expansion ? -1 : 1));
+        cards = cards.sort((a, b) => a.expansion - b.expansion);
 
-        await this.cardService.replaceCards(cards);
+        if (!this.skipReplace) {
+            console.log('Replacing cards');
+            await this.cardService.replaceCards(cards);
+        }
 
         console.info(cards.length + ' cards fetched');
 
@@ -41,97 +50,105 @@ class CardImport {
     }
 
     async fetchImages(cards) {
-        let imageLangDir;
-        let halfSizeImageDir;
-
-        if (this.language === 'en') {
-            // Keep english images at the current folder
-            imageLangDir = this.imageDir;
-        } else {
-            imageLangDir = path.join(this.imageDir, this.language.replace('-', ''));
-        }
-        halfSizeImageDir = imageLangDir.replace(`${path.sep}cards`, `${path.sep}halfSize`);
-        mkdirp(imageLangDir);
-        mkdirp(halfSizeImageDir);
-
+        // Add cards that were once anomaly and now are printed in a set (or sets)
         let specialCards = {
             479: { 'dark-æmber-vault': true, 'it-s-coming': true, 'orb-of-wonder': true },
             496: { 'orb-of-wonder': true, valoocanth: true }
         };
 
         const gigantic = ['deusillus', 'ultra-gravitron', 'niffle-kong'];
+        const skipMkdir = {};
 
         for (let card of cards) {
-            let imagePath = path.join(imageLangDir, card.id + '.png').replace('*', '_');
-            let halfSizePath;
+            for (let language of this.computeLanguages(card)) {
+                let imageLangDir = this.computeImagesDir(skipMkdir, language);
+                let imagePath = path.join(imageLangDir, card.id + '.png').replace('*', '_');
+                let halfSizePath;
 
-            let imageUrl = card.image
-                .replace('/en/', '/' + this.language + '/')
-                .replace('_en.', '_' + this.language + '.');
+                let imageUrl = card.image
+                    .replace('/en/', '/' + language + '/')
+                    .replace('_en.', '_' + language + '.');
 
-            if (specialCards[card.expansion] && specialCards[card.expansion][card.id]) {
-                imagePath = path.join(imageLangDir, `${card.id}-${card.house}.png`);
-            }
+                if (specialCards[card.expansion] && specialCards[card.expansion][card.id]) {
+                    imagePath = path.join(imageLangDir, `${card.id}-${card.house}.png`);
+                }
 
-            if (!fs.existsSync(imagePath)) {
-                await this.imageSource.fetchImage(card, imageUrl, imagePath);
-            }
+                if (this.force || !fs.existsSync(imagePath)) {
+                    await this.imageSource.fetchImage(card, imageUrl, imagePath);
+                }
 
-            halfSizePath = imagePath
-                .replace(`${path.sep}cards`, `${path.sep}halfSize`)
-                .replace('.png', '.jpg');
-            if (!fs.existsSync(halfSizePath) && !gigantic.some((x) => card.id.includes(x))) {
-                await this.buildHalfSize(card, card.image, halfSizePath, this.language);
+                halfSizePath = imagePath
+                    .replace(`${path.sep}cards`, `${path.sep}halfSize`)
+                    .replace('.png', '.jpg');
+                if (!gigantic.some((x) => card.id.includes(x))) {
+                    if (this.force || !fs.existsSync(halfSizePath)) {
+                        if (this.buildHalfSize) {
+                            await this.buildHalfSize(
+                                card,
+                                card.image,
+                                halfSizePath,
+                                language.replace('-', '')
+                            );
+                        }
+                    }
+                }
             }
         }
 
-        for (const card of gigantic) {
-            let imgPath = path.join(imageLangDir, card + '-complete.png');
-            let halfSizePath = imgPath
-                .replace(`${path.sep}cards`, `${path.sep}halfSize`)
-                .replace('.png', '.jpg');
-            if (!fs.existsSync(imgPath)) {
-                await this.buildGigantics(card, imageLangDir, imgPath);
-                console.log('Built gigantic image for ' + card);
-            }
-            if (!fs.existsSync(halfSizePath)) {
-                let data = cards.find((x) => x.id === card);
-                await this.buildHalfSize(
-                    { ...data, type: 'creature1' },
-                    `file://${imgPath}`,
-                    halfSizePath,
-                    this.language
-                );
+        for (const card of cards.filter((c) => gigantic.includes(c.id))) {
+            for (let language of this.computeLanguages(card)) {
+                let imageLangDir = this.computeImagesDir(skipMkdir, language);
+                let imgPath = path.join(imageLangDir, card.id + '-complete.png');
+                let halfSizePath = imgPath
+                    .replace(`${path.sep}cards`, `${path.sep}halfSize`)
+                    .replace('.png', '.jpg');
+                if (this.force || !fs.existsSync(imgPath)) {
+                    await this.imageSource.buildGigantics(card.id, language, imageLangDir, imgPath);
+                }
+                if (this.force || !fs.existsSync(halfSizePath)) {
+                    let data = cards.find((x) => x.id === card.id);
+                    if (this.buildHalfSize) {
+                        await this.buildHalfSize(
+                            { ...data, type: 'creature1' },
+                            `file://${imgPath}`,
+                            halfSizePath,
+                            language.replace('-', '')
+                        );
+                    }
+                }
             }
         }
     }
 
-    async buildGigantics(card, imageLangDir, imgPath) {
-        const canvas = new fabric.StaticCanvas();
-        canvas.setDimensions({ width: 300, height: 420 });
-        const bottom = await this.loadImage(path.join(imageLangDir, card + '.png'));
-        const top = await this.loadImage(path.join(imageLangDir, card + '2.png'));
-        top.rotate(-90).scaleToWidth(300).set({ top: 210, left: 0 });
-        bottom.rotate(-90).scaleToWidth(300).set({ top: 420, left: 0 });
-        canvas.add(top);
-        canvas.add(bottom);
-        canvas.renderAll();
-        const stream = canvas.createPNGStream();
-        const out = fs.createWriteStream(imgPath);
-        stream.on('data', (chunk) => {
-            out.write(chunk);
-        });
-        stream.on('end', () => {
-            canvas.dispose();
-        });
+    computeLanguages(card) {
+        let languages;
+        if (this.language === 'all') {
+            languages = card.locale ? Object.keys(card.locale) : ['en'];
+        } else {
+            languages = [this.language];
+        }
+        return languages.map((it) => LANGUAGE_MAP[it] ?? it);
     }
 
-    loadImage(imgPath) {
-        return new Promise((resolve) => {
-            fabric.Image.fromURL(`file://${imgPath}`, (image) => {
-                resolve(image);
-            });
-        });
+    computeImagesDir(skipMkdir, language) {
+        let imageLangDir, halfSizeImageDir;
+        if (!skipMkdir[language]) {
+            if (language === 'en') {
+                // Keep english images at the current folder
+                imageLangDir = this.imageDir;
+            } else {
+                imageLangDir = path.join(this.imageDir, language.replace('-', ''));
+            }
+            halfSizeImageDir = imageLangDir.replace(`${path.sep}cards`, `${path.sep}halfSize`);
+
+            mkdirp(imageLangDir);
+            mkdirp(halfSizeImageDir);
+            skipMkdir[language] = imageLangDir;
+        } else {
+            imageLangDir = skipMkdir[language];
+        }
+
+        return imageLangDir;
     }
 }
 
