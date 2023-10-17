c093373da799b3ba60dc612af590231d2d1fcbbe
Romano Silva <romano.magacho@gmail.com>
2023-10-17 02:49:41 +0000

    Fix tokens and entersPlayReady with Rocketeer Tryska

--- test/server/cards/05-DT/RocketeerTryska.spec.js
+++ test/server/cards/05-DT/RocketeerTryska.spec.js
@@ -4,9 +4,56 @@ describe('Rocketeer Tryska', function () {
             this.setupTest({
                 player1: {
                     amber: 2,
-                    house: 'staralliance',
+                    house: 'logos',
                     inPlay: ['rocketeer-tryska'],
-                    hand: ['armsmaster-molina', 'sensor-chief-garcia']
+                    hand: ['armsmaster-molina', 'dextre', 'wild-wormhole']
+                },
+                player2: {
+                    amber: 2,
+                    inPlay: ['murkens']
+                }
+            });
+
+            this.player1.moveCard(this.armsmasterMolina, 'deck');
+        });
+
+        describe('when the tide is not high', function () {
+            it('a neighbor should not enter play ready', function () {
+                this.player1.play(this.dextre);
+                expect(this.dextre.location).toBe('play area');
+                expect(this.dextre.exhausted).toBe(true);
+            });
+        });
+
+        describe('when the tide is high', function () {
+            beforeEach(function () {
+                this.player1.raiseTide();
+            });
+
+            it('a neighbor should enter play ready', function () {
+                this.player1.play(this.dextre);
+                expect(this.dextre.location).toBe('play area');
+                expect(this.dextre.exhausted).toBe(false);
+            });
+
+            it('a neighbor should enter play ready when played from deck', function () {
+                this.player1.play(this.wildWormhole);
+                this.player1.clickPrompt('Right');
+                expect(this.armsmasterMolina.location).toBe('play area');
+                expect(this.armsmasterMolina.exhausted).toBe(false);
+            });
+        });
+    });
+
+    describe("Rocketeer Tryska's and tokens", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 2,
+                    house: 'ekwidon',
+                    token: 'b0-t',
+                    inPlay: ['rocketeer-tryska'],
+                    hand: ['the-visible-hand']
                 },
                 player2: {
                     amber: 2,
@@ -16,10 +63,14 @@ describe('Rocketeer Tryska', function () {
         });
 
         describe('when the tide is not high', function () {
-            it('a neighbor should not enter play ready', function () {
-                this.player1.play(this.armsmasterMolina);
-                expect(this.armsmasterMolina.location).toBe('play area');
-                expect(this.armsmasterMolina.exhausted).toBe(true);
+            it('a token should not enter play ready', function () {
+                this.player1.play(this.theVisibleHand);
+                this.player1.clickPrompt('Left');
+                this.player1.clickPrompt('Right');
+                expect(this.player1.inPlay[0].name).toBe('B0-T');
+                expect(this.player1.inPlay[0].exhausted).toBe(true);
+                expect(this.player1.inPlay[2].name).toBe('B0-T');
+                expect(this.player1.inPlay[2].exhausted).toBe(true);
             });
         });
 
@@ -28,10 +79,64 @@ describe('Rocketeer Tryska', function () {
                 this.player1.raiseTide();
             });
 
-            it('a neighbor should not enter play ready', function () {
-                this.player1.play(this.armsmasterMolina);
-                expect(this.armsmasterMolina.location).toBe('play area');
-                expect(this.armsmasterMolina.exhausted).toBe(false);
+            it('a token should enter play ready', function () {
+                this.player1.play(this.theVisibleHand);
+                this.player1.clickPrompt('Left');
+                this.player1.clickPrompt('Right');
+                expect(this.player1.inPlay[0].name).toBe('B0-T');
+                expect(this.player1.inPlay[0].exhausted).toBe(false);
+                expect(this.player1.inPlay[2].name).toBe('B0-T');
+                expect(this.player1.inPlay[2].exhausted).toBe(false);
+            });
+        });
+    });
+
+    describe("Rocketeer Tryska's and take control", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 2,
+                    house: 'ekwidon',
+                    token: 'b0-t',
+                    hand: ['the-visible-hand', 'talent-scout']
+                },
+                player2: {
+                    amber: 2,
+                    hand: ['rocketeer-tryska']
+                }
+            });
+
+            this.player1.play(this.talentScout);
+            this.player1.clickCard(this.rocketeerTryska);
+            this.player1.clickPrompt('Left');
+            expect(this.rocketeerTryska.controller).toBe(this.player1.player);
+        });
+
+        describe('when the tide is not high', function () {
+            it('a token should not enter play ready', function () {
+                this.player1.play(this.theVisibleHand);
+                this.player1.clickPrompt('Left');
+                this.player1.clickPrompt('Right');
+                expect(this.player1.inPlay[0].name).toBe('B0-T');
+                expect(this.player1.inPlay[0].exhausted).toBe(true);
+                expect(this.player1.inPlay[2].name).toBe('B0-T');
+                expect(this.player1.inPlay[2].exhausted).toBe(true);
+            });
+        });
+
+        describe('when the tide is high', function () {
+            beforeEach(function () {
+                this.player1.raiseTide();
+            });
+
+            it('a token should enter play ready', function () {
+                this.player1.play(this.theVisibleHand);
+                this.player1.clickPrompt('Left');
+                this.player1.clickPrompt('Right');
+                expect(this.player1.inPlay[0].name).toBe('B0-T');
+                expect(this.player1.inPlay[0].exhausted).toBe(false);
+                expect(this.player1.inPlay[2].name).toBe('B0-T');
+                expect(this.player1.inPlay[2].exhausted).toBe(false);
             });
         });
     });
