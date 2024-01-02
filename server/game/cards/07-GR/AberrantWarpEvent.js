64c776c16508015798ba3c5a07b62bfd6934861a
David Mikalova <324548+dmikalova@users.noreply.github.com>
2024-01-01 17:56:07 -0800

    feat: work on opponent

--- server/game/cards/07-GR/AberrantWarpEvent.js
+++ server/game/cards/07-GR/AberrantWarpEvent.js
@@ -16,11 +16,13 @@ class AberrantWarpEvent extends Card {
     // on your opponent.
     setupCardAbilities(ability) {
         this.play({
+            // Self discard
             gameAction: ability.actions.discard((context) => discard(context.player.deck)),
             then: (context) => {
+                // Self play
                 const card = context.player.deck.find((card) => card.type === 'creature');
                 if (card) {
-                    context.playedCard = card;
+                    context.selfCreature = card;
                     return {
                         alwaysTriggers: true,
                         message: '{0} uses {1} to put {3} into play',
@@ -29,18 +31,90 @@ class AberrantWarpEvent extends Card {
                             target: card
                         }),
                         then: () => ({
+                            // Self destroy
                             alwaysTriggers: true,
-                            message: '{0} uses {1} to destroy 2{2}3{3}4{4}', // {3} is empty
-                            messageArgs: (ctx) => {
-                                console.log(ctx.targets);
-                                return [ctx.targets, ['this', 'that'], 'bla'];
-                            },
+                            message: '{0} uses {1} to destroy the neighboring creature',
                             gameAction: ability.actions.destroy(() => ({
                                 target: card.controller.creaturesInPlay.filter((card) =>
-                                    card.neighbors.includes(context.playedCard)
+                                    card.neighbors.includes(context.selfCreature)
                                 )
-                            }))
+                            })),
+
+                            // Opponent discard
+                            then: () => ({
+                                gameAction: ability.actions.discard((context) =>
+                                    discard(context.player.opponent.deck)
+                                ),
+                                then: (context) => {
+                                    // Opponent play
+                                    const card = context.player.opponent.deck.find(
+                                        (card) => card.type === 'creature'
+                                    );
+                                    if (card) {
+                                        context.oppCreature = card;
+                                        return {
+                                            alwaysTriggers: true,
+                                            message: '{0} uses {1} to put {3} into play',
+                                            messageArgs: card,
+                                            gameAction: ability.actions.putIntoPlay({
+                                                target: card
+                                            }),
+                                            then: () => ({
+                                                // Opponent destroy
+                                                alwaysTriggers: true,
+                                                message:
+                                                    '{0} uses {1} to destroy the neighboring creature',
+                                                gameAction: ability.actions.destroy(() => ({
+                                                    target: card.controller.creaturesInPlay.filter(
+                                                        (card) =>
+                                                            card.neighbors.includes(
+                                                                context.oppCreature
+                                                            )
+                                                    )
+                                                }))
+                                            })
+                                        };
+                                    }
+                                }
+                            })
                         })
+                    };
+                } else {
+                    return {
+                        // Opponent discard
+                        alwaysTriggers: true,
+                        message:
+                            "{0} uses {1} to discard cards from the top of their opponent's deck",
+                        gameAction: ability.actions.discard((context) =>
+                            discard(context.player.opponent.deck)
+                        ),
+                        then: (context) => {
+                            // Opponent play
+                            const card = context.player.opponent.deck.find(
+                                (card) => card.type === 'creature'
+                            );
+                            if (card) {
+                                context.oppCreature = card;
+                                return {
+                                    alwaysTriggers: true,
+                                    message: '{0} uses {1} to put {3} into play',
+                                    messageArgs: card,
+                                    gameAction: ability.actions.putIntoPlay({
+                                        target: card
+                                    }),
+                                    then: () => ({
+                                        // Opponent destroy
+                                        alwaysTriggers: true,
+                                        message: '{0} uses {1} to destroy the neighboring creature',
+                                        gameAction: ability.actions.destroy(() => ({
+                                            target: card.controller.creaturesInPlay.filter((card) =>
+                                                card.neighbors.includes(context.oppCreature)
+                                            )
+                                        }))
+                                    })
+                                };
+                            }
+                        }
                     };
                 }
             }
--- test/server/cards/07-GR/AberrantWarpEvent.spec.js
+++ test/server/cards/07-GR/AberrantWarpEvent.spec.js
@@ -6,41 +6,91 @@ describe('AberrantWarpEvent', function () {
                     house: 'mars',
                     hand: [
                         'aberrant-warp-event',
-                        'seeker-needle',
                         'the-warchest',
+                        'anger',
+                        'blood-of-titans',
                         'ganger-chieftain',
-                        'shadow-self',
-                        'urchin',
-                        'anger'
+                        'urchin'
                     ],
                     inPlay: ['dodger']
                 },
-                player2: {}
+                player2: {
+                    hand: [
+                        'causal-loop',
+                        'static-charge',
+                        'crazy-killing-machine',
+                        'daughter',
+                        'gub'
+                    ],
+                    inPlay: ['mother']
+                }
             });
+        });
 
-            this.player1.moveCard(this.shadowSelf, 'deck');
+        it('plays with creatures in both decks', function () {
             this.player1.moveCard(this.urchin, 'deck');
             this.player1.moveCard(this.gangerChieftain, 'deck');
             this.player1.moveCard(this.anger, 'deck');
+            this.player1.moveCard(this.bloodOfTitans, 'deck');
             this.player1.moveCard(this.theWarchest, 'deck');
-            this.player1.moveCard(this.seekerNeedle, 'deck');
-        });
 
-        it('plays creature', function () {
+            this.player2.moveCard(this.gub, 'deck');
+            this.player2.moveCard(this.daughter, 'deck');
+            this.player2.moveCard(this.crazyKillingMachine, 'deck');
+            this.player2.moveCard(this.staticCharge, 'deck');
+            this.player2.moveCard(this.causalLoop, 'deck');
+
+            // Self
             this.player1.play(this.aberrantWarpEvent);
-            // expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
             expect(this.player1).toHavePrompt('Which flank do you want to place this creature on?');
             this.player1.clickPrompt('Left');
-            expect(this.player1).toHavePrompt('Choose a creaturex');
-            expect(this.player1).not.toBeAbleToSelect(this.gangerChieftain);
-            expect(this.player1).not.toBeAbleToSelect(this.dodger);
-            // this.player1.clickCard(this.dodger);
-            expect(this.gangerChieftain.location).toBe('play area');
-            expect(this.shadowSelf.location).toBe('deck');
-            expect(this.seekerNeedle.location).toBe('discard');
+            expect(this.urchin.location).toBe('deck');
+            expect(this.anger.location).toBe('discard');
+            expect(this.bloodOfTitans.location).toBe('discard');
             expect(this.theWarchest.location).toBe('discard');
+            expect(this.gangerChieftain.location).toBe('play area');
             expect(this.dodger.location).toBe('discard');
-            // this.player1.clickPrompt('Left');
+
+            // Opponent
+            expect(this.player1).toHavePrompt('Which flank do you want to place this creature on?');
+            this.player1.clickPrompt('Left');
+            expect(this.gub.location).toBe('deck');
+            expect(this.causalLoop.location).toBe('discard');
+            expect(this.staticCharge.location).toBe('discard');
+            expect(this.crazyKillingMachine.location).toBe('discard');
+            expect(this.daughter.location).toBe('play area');
+            expect(this.mother.location).toBe('discard');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('plays with creatures only in opponent deck', function () {
+            this.player1.moveCard(this.anger, 'deck');
+            this.player1.moveCard(this.bloodOfTitans, 'deck');
+            this.player1.moveCard(this.theWarchest, 'deck');
+
+            this.player2.moveCard(this.gub, 'deck');
+            this.player2.moveCard(this.daughter, 'deck');
+            this.player2.moveCard(this.crazyKillingMachine, 'deck');
+            this.player2.moveCard(this.staticCharge, 'deck');
+            this.player2.moveCard(this.causalLoop, 'deck');
+
+            // Self
+            this.player1.play(this.aberrantWarpEvent);
+            expect(this.urchin.location).toBe('hand');
+            expect(this.anger.location).toBe('discard');
+            expect(this.bloodOfTitans.location).toBe('discard');
+            expect(this.theWarchest.location).toBe('discard');
+            expect(this.gangerChieftain.location).toBe('hand');
+            expect(this.dodger.location).toBe('play area');
+
+            // Opponent
+            expect(this.gub.location).toBe('deck');
+            expect(this.causalLoop.location).toBe('discard');
+            expect(this.staticCharge.location).toBe('discard');
+            expect(this.crazyKillingMachine.location).toBe('discard');
+            expect(this.daughter.location).toBe('play area');
+            expect(this.mother.location).toBe('discard');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
         });
     });
 });
