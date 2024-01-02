4653a43b8ceafd6db0d13f59dda6ca55d177d56f
David Mikalova <324548+dmikalova@users.noreply.github.com>
2024-01-02 00:46:01 -0800

    feat: implement Elite Disruptzord

--- server/game/cards/07-GR/EliteDisruptzord.js
+++ server/game/cards/07-GR/EliteDisruptzord.js
@@ -4,25 +4,11 @@ class EliteDisruptzord extends Card {
     // Creatures more powerful than Elite Disruptzord cannot be played.
     setupCardAbilities(ability) {
         this.persistentEffect({
-            // condition: (card) => card.type === 'creature' && card.power > 6,
             targetController: 'any',
-            // effect: ability.effects.playerCannot('play', () => false)
             effect: ability.effects.playerCannot(
                 'play',
-                // effectContext.source.power is ED
-                // context.source.power is creature
-                // (context, effectContext) => context.source.power === 5
-                // (context, effectContext) =>   context.source.power < effectContext.source.power
-                // (context, effectContext) =>   context.source.power < effectContext.source.power
                 (context, effectContext) => effectContext.source.power < context.source.power
             )
-
-            // effect: ability.effects.cardCannot('damage', (context, effectContext) => {
-            //     if (context.source === effectContext.source) {
-            //         return context.target && context.target.amber > 0;
-            //     }
-            //     return context.source.type === 'creature' && context.source.amber > 0;
-            // })
         });
     }
 }
