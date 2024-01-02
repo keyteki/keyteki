acb1dd322d0677c0bc04252756a182e473b4313b
David Mikalova <324548+dmikalova@users.noreply.github.com>
2024-01-01 22:30:32 -0800

    feat: implement Dr. Xyloxxzlphrex

---
+++ server/game/cards/07-GR/DrXyloxxzlphrex.js
@@ -0,0 +1,25 @@
+const Card = require('../../Card.js');
+
+class DrXyloxxzlphrex extends Card {
+    // After Reap: Play a Mars creature from your discard pile and ready it.
+    setupCardAbilities(ability) {
+        this.reap({
+            target: {
+                cardCondition: (c) => c.hasHouse('mars'),
+                cardType: 'creature',
+                controller: 'self',
+                location: 'discard',
+                gameAction: ability.actions.playCard()
+            },
+            then: {
+                gameAction: ability.actions.ready((context) => ({
+                    target: context.preThenEvents.map((event) => event.card)
+                }))
+            }
+        });
+    }
+}
+
+DrXyloxxzlphrex.id = 'dr-xyloxxzlphrex';
+
+module.exports = DrXyloxxzlphrex;
