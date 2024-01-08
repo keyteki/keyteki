68dfe7e6d99c7186c4873d62ca9f546c3e33bdb5
Jeremy Stribling <strib@alum.mit.edu>
2024-01-08 08:37:05 -0800

    GR: implement De-escalation

---
+++ server/game/cards/07-GR/DeEscalation.js
@@ -0,0 +1,34 @@
+const Card = require('../../Card.js');
+
+class DeEscalation extends Card {
+    // Play: Destroy each creature. Your opponent archives the top 3
+    // cards of their deck.
+    setupCardAbilities(ability) {
+        this.play({
+            effect: 'destroy all creatures{1}',
+            effectArgs: (context) => [
+                context.player.opponent
+                    ? ' and have ' +
+                      context.player.opponent.name +
+                      ' archive the top 3 cards of their deck'
+                    : ''
+            ],
+            gameAction: [
+                ability.actions.destroy((context) => ({ target: context.game.creaturesInPlay })),
+                ability.actions.conditional((context) => ({
+                    condition: context.player.opponent && context.player.opponent.deck.length > 0,
+                    trueGameAction: ability.actions.archive({
+                        target: context.player.opponent.deck.slice(
+                            0,
+                            Math.min(context.player.deck.length, 3)
+                        )
+                    })
+                }))
+            ]
+        });
+    }
+}
+
+DeEscalation.id = 'de-escalation';
+
+module.exports = DeEscalation;
