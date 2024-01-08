4067bb812fbcbdd7d723fcb71f858daee5b7e2ed
Jeremy Stribling <strib@users.noreply.github.com>
2024-01-08 01:10:49 -0800

    GR: implement Unsettling Ancestry (#3581)

---
+++ test/server/cards/07-GR/UnsettlingAncestry.spec.js
@@ -0,0 +1,39 @@
+describe('Unsettling Ancestry', function () {
+    describe("Unsettling Ancestry's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'unfathomable',
+                    hand: ['unsettling-ancestry', 'crushing-deep'],
+                    inPlay: ['bubbles'],
+                    discard: new Array(9).fill('poke') // not yet haunted
+                },
+                player2: {
+                    inPlay: ['umbra', 'hunting-witch']
+                }
+            });
+        });
+
+        it('stuns and exhausts, but does not archive if not haunted', function () {
+            this.player1.play(this.unsettlingAncestry);
+            expect(this.player1).not.toBeAbleToSelect(this.bubbles);
+            expect(this.player1).toBeAbleToSelect(this.umbra);
+            expect(this.player1).toBeAbleToSelect(this.huntingWitch);
+            this.player1.clickCard(this.umbra);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            expect(this.umbra.stunned).toBe(true);
+            expect(this.umbra.exhausted).toBe(true);
+            expect(this.unsettlingAncestry.location).toBe('discard');
+        });
+
+        it('stuns and exhausts, and archives if haunted', function () {
+            this.player1.play(this.crushingDeep);
+            this.player1.play(this.unsettlingAncestry);
+            this.player1.clickCard(this.umbra);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            expect(this.umbra.stunned).toBe(true);
+            expect(this.umbra.exhausted).toBe(true);
+            expect(this.unsettlingAncestry.location).toBe('archives');
+        });
+    });
+});
