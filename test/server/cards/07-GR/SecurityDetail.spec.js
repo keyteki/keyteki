e675fa361295fddb69a449172fc41c397e224484
Jeremy Stribling <strib@alum.mit.edu>
2024-01-10 18:42:17 -0800

    GR: add a 2-amber test to Security Detail
    Suggested by romanogit.

--- test/server/cards/07-GR/SecurityDetail.spec.js
+++ test/server/cards/07-GR/SecurityDetail.spec.js
@@ -30,7 +30,7 @@ describe('Security Detail', function () {
             expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
         });
 
-        it('can choose if not enough amber', function () {
+        it('can choose if only one amber', function () {
             this.player2.amber = 1;
             this.player1.play(this.securityDetail);
             this.player1.clickCard(this.theOldTinker);
@@ -44,6 +44,26 @@ describe('Security Detail', function () {
             expect(this.cpoZytar.amber).toBe(1);
             expect(this.theOldTinker.amber).toBe(0);
             expect(this.medicIngram.amber).toBe(0);
+            expect(this.gemcoatVendor.amber).toBe(0);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('can choose if only two amber', function () {
+            this.player2.amber = 2;
+            this.player1.play(this.securityDetail);
+            this.player1.clickCard(this.theOldTinker);
+            expect(this.player1).toBeAbleToSelect(this.cpoZytar);
+            expect(this.player1).toBeAbleToSelect(this.theOldTinker);
+            expect(this.player1).toBeAbleToSelect(this.medicIngram);
+            expect(this.player1).not.toBeAbleToSelect(this.gemcoatVendor);
+            this.player1.clickCard(this.cpoZytar);
+            this.player1.clickCard(this.medicIngram);
+            this.player1.clickPrompt('Done');
+            expect(this.player1.amber).toBe(2);
+            expect(this.player2.amber).toBe(0);
+            expect(this.cpoZytar.amber).toBe(1);
+            expect(this.theOldTinker.amber).toBe(0);
+            expect(this.medicIngram.amber).toBe(1);
             expect(this.gemcoatVendor.amber).toBe(0);
             expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
         });
