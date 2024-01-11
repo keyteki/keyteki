a3c98636b49b87ca4fd4e920bfd9bb65d5369ad9
Jeremy Stribling <strib@users.noreply.github.com>
2024-01-11 06:03:58 -0800

    GR: implement Skreel Swarm (#3657)
    * GR: implement Skreel Swarm
    
    * GR: Skreel Swarm is optional
    
    Suggested by romanogit.
    
    Issue: #3657

---
+++ server/game/cards/07-GR/SkreelSwarm.js
@@ -0,0 +1,29 @@
+const Card = require('../../Card.js');
+
+class SkreelSwarm extends Card {
+    // Play: Deal 1D to each enemy creature.
+    //
+    // Scrap: You may archive Skreel Swarm from your discard pile.
+    setupCardAbilities(ability) {
+        this.play({
+            effect: 'deal 1 damage to each enemy creature',
+            gameAction: ability.actions.dealDamage((context) => ({
+                amount: 1,
+                target: context.game.creaturesInPlay.filter(
+                    (card) => card.controller !== context.player
+                )
+            }))
+        });
+
+        this.scrap({
+            optional: true,
+            gameAction: ability.actions.archive((context) => ({
+                target: context.source
+            }))
+        });
+    }
+}
+
+SkreelSwarm.id = 'skreel-swarm';
+
+module.exports = SkreelSwarm;
