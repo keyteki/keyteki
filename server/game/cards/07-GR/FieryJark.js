403c56de6ebbdcae0dfc1ebc93e4f8ad65249f5f
Jeremy Stribling <strib@users.noreply.github.com>
2024-01-12 11:44:20 -0800

    GR: implement Fiery Jark (#3703)

---
+++ server/game/cards/07-GR/FieryJark.js
@@ -0,0 +1,43 @@
+const Card = require('../../Card.js');
+
+class FieryJark extends Card {
+    // Play: Destroy an enemy creature and a friendly creature. You
+    // may repeat this effect as many times as you like, as long as it
+    // is possible to repeat the entire effect.
+    setupCardAbilities(ability) {
+        this.play({
+            targets: {
+                enemy: {
+                    cardType: 'creature',
+                    controller: 'opponent',
+                    gameAction: ability.actions.destroy()
+                },
+                friendly: {
+                    cardType: 'creature',
+                    controller: 'self',
+                    gameAction: ability.actions.destroy()
+                }
+            },
+            effect: 'destroy {1}',
+            effectArgs: (context) => [Object.values(context.targets)],
+            then: (context) => ({
+                condition: () =>
+                    context.player.cardsInPlay.some(
+                        (card) => card.type === 'creature' && card.allowGameAction('destroy')
+                    ) &&
+                    context.player.opponent &&
+                    context.player.opponent.cardsInPlay.some(
+                        (card) => card.type === 'creature' && card.allowGameAction('destroy')
+                    ) &&
+                    context.source.location === 'play area',
+                alwaysTriggers: true,
+                may: 'repeat this effect',
+                gameAction: ability.actions.resolveAbility({ ability: context.ability })
+            })
+        });
+    }
+}
+
+FieryJark.id = 'fiery-jark';
+
+module.exports = FieryJark;
