41129015d96f4c050087ab45da0f0b6606102965
Jeremy Stribling <strib@alum.mit.edu>
2024-01-12 17:43:23 -0800

    GR: implement Ornar Skullface

---
+++ test/server/cards/07-GR/OrnarSkullface.spec.js
@@ -0,0 +1,27 @@
+describe('Ornar Skullface', function () {
+    describe("Ornar Skullface's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'brobnar',
+                    hand: ['ornar-skullface'],
+                    inPlay: ['troll']
+                },
+                player2: {
+                    inPlay: ['flaxia']
+                }
+            });
+        });
+
+        it('can deal 3 damage to a creature on scrap', function () {
+            this.player1.clickCard(this.ornarSkullface);
+            this.player1.clickPrompt('Discard this card');
+            expect(this.player1).toBeAbleToSelect(this.troll);
+            expect(this.player1).toBeAbleToSelect(this.flaxia);
+            this.player1.clickCard(this.flaxia);
+            expect(this.flaxia.tokens.damage).toBe(3);
+            expect(this.troll.tokens.damage).toBe(undefined);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
