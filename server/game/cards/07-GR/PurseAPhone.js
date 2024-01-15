63b41db137dca4d649b8229317dbcb6692777566
Jeremy Stribling <strib@users.noreply.github.com>
2024-01-15 14:25:03 -0800

    GR: implement Shadys and Purse-A-Phone (#3731)

---
+++ server/game/cards/07-GR/PurseAPhone.js
@@ -0,0 +1,27 @@
+const Card = require('../../Card.js');
+
+class PurseAPhone extends Card {
+    // Keys cost -2.
+    //
+    // Destroyed: If Shadys is in your discard pile, return it to your hand.
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            targetController: 'any',
+            effect: ability.effects.modifyKeyCost(-2)
+        });
+
+        this.destroyed({
+            target: {
+                cardCondition: (card) => card.name === 'Shadys',
+                location: 'discard',
+                gameAction: ability.actions.returnToHand({
+                    location: 'discard'
+                })
+            }
+        });
+    }
+}
+
+PurseAPhone.id = 'purse-a-phone';
+
+module.exports = PurseAPhone;
