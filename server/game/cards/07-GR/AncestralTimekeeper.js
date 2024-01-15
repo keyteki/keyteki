4ea1968646ed6faf026e22a56ba569e89e74dedf
Jeremy Stribling <strib@alum.mit.edu>
2024-01-15 11:36:10 -0800

    GR: implement the clock suite
    Tick-Tock, Chronometer, Ancestral Timekeeper and Synchronizer.

---
+++ server/game/cards/07-GR/AncestralTimekeeper.js
@@ -0,0 +1,34 @@
+const Card = require('../../Card.js');
+
+class AncestralTimekeeper extends Card {
+    // At the end of your turn, put a time counter on each friendly
+    // Clock creature. If there are 12 or more time counters on
+    // Ancestral Timekeeper, purge Ancestral Timekeeper. If you do,
+    // take another turn after this one.
+    setupCardAbilities(ability) {
+        this.interrupt({
+            when: {
+                onRoundEnded: (_, context) => context.player === this.game.activePlayer
+            },
+            gameAction: ability.actions.addTimeCounter((context) => ({
+                target: context.source.controller.creaturesInPlay.filter((c) => c.hasTrait('clock'))
+            })),
+            then: {
+                condition: (context) => context.source.tokens['time'] >= 12,
+                gameAction: ability.actions.purge((context) => ({
+                    target: context.source
+                })),
+                then: {
+                    message: '{0} uses {1} to take another turn after this one',
+                    gameAction: ability.actions.forRemainderOfTurn({
+                        effect: ability.effects.anotherTurn()
+                    })
+                }
+            }
+        });
+    }
+}
+
+AncestralTimekeeper.id = 'ancestral-timekeeper';
+
+module.exports = AncestralTimekeeper;
