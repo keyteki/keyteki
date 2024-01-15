ebf0df9285eefe83e10871c2034fbacdb13e2564
Jeremy Stribling <strib@users.noreply.github.com>
2024-01-15 07:37:51 -0800

    GR: implement Permanent Record (#3680)

---
+++ server/game/cards/07-GR/PermanentRecord.js
@@ -0,0 +1,23 @@
+const Card = require('../../Card.js');
+
+class PermanentRecord extends Card {
+    // Play: Exhaust a friendly creature. If you do, steal 2A.
+    setupCardAbilities(ability) {
+        this.play({
+            target: {
+                cardType: 'creature',
+                controller: 'self',
+                gameAction: ability.actions.exhaust()
+            },
+            then: {
+                condition: (context) => context.player.opponent,
+                gameAction: ability.actions.steal({ amount: 2 }),
+                message: '{0} uses {1} to steal 2 amber'
+            }
+        });
+    }
+}
+
+PermanentRecord.id = 'permanent-record';
+
+module.exports = PermanentRecord;
