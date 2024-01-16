4c397242ac942dea25167f6d771f2bfd91964030
Jeremy Stribling <strib@alum.mit.edu>
2024-01-15 18:23:39 -0800

    GR: implement Infiltrator

---
+++ test/server/cards/07-GR/Infiltrator.spec.js
@@ -0,0 +1,52 @@
+describe('Infiltrator', function () {
+    describe("Infiltrator's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'geistoid',
+                    hand: ['infiltrator'],
+                    inPlay: ['flaxia']
+                },
+                player2: {
+                    hand: ['gub'],
+                    inPlay: ['charette', 'cpo-zytar']
+                }
+            });
+            this.player1.playCreature(this.infiltrator);
+            this.infiltrator.exhausted = false;
+        });
+
+        it('enters play under opponent control', function () {
+            expect(this.player2.player.creaturesInPlay).toContain(this.infiltrator);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('can be used with any house', function () {
+            this.player1.endTurn();
+            this.player2.clickPrompt('dis');
+            this.player2.reap(this.infiltrator);
+            expect(this.player2).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('has skirmish', function () {
+            this.player1.endTurn();
+            this.player2.clickPrompt('dis');
+            this.player2.fightWith(this.infiltrator, this.flaxia);
+            expect(this.infiltrator.tokens.damage).toBe(undefined);
+            expect(this.flaxia.location).toBe('discard');
+            expect(this.player2).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('destroys its neighbors at the end of the turn', function () {
+            this.player1.endTurn();
+            this.player2.clickPrompt('dis');
+            this.player2.playCreature(this.gub);
+            this.player2.endTurn();
+            expect(this.cpoZytar.location).toBe('discard');
+            expect(this.gub.location).toBe('discard');
+            expect(this.infiltrator.location).toBe('play area');
+            expect(this.charette.location).toBe('play area');
+            this.player1.clickPrompt('geistoid');
+        });
+    });
+});
