52e497f52d82b909652c5fd3ad01f587e34d2ffb
Jeremy Stribling <strib@users.noreply.github.com>
2024-01-17 07:07:38 -0800

    GR: implement Veil of Ectoplasm (#3749)

---
+++ server/game/cards/07-GR/VeilOfEctoplasm.js
@@ -0,0 +1,30 @@
+const Card = require('../../Card.js');
+
+class VeilOfEctoplasm extends Card {
+    // Play: For each Geistoid card in your discard pile, a friendly
+    // creature captures 1 A.
+    setupCardAbilities(ability) {
+        this.play({
+            condition: (context) => !!context.player.opponent && context.player.opponent.amber > 0,
+            effect:
+                'capture 1 amber on a friendly creature for each Geistoid creature in your discard pile',
+            gameAction: ability.actions.sequentialForEach((context) => ({
+                num: Math.min(
+                    context.player.opponent.amber,
+                    context.player.discard.filter((c) => c.hasHouse('geistoid')).length
+                ),
+                action: ability.actions.capture({
+                    promptForSelect: {
+                        activePromptTitle: 'Choose a creature to capture 1 amber',
+                        cardType: 'creature',
+                        controller: 'self'
+                    }
+                })
+            }))
+        });
+    }
+}
+
+VeilOfEctoplasm.id = 'veil-of-ectoplasm';
+
+module.exports = VeilOfEctoplasm;
