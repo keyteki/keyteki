eb23560c4b5cec0b77ebbf235ce637c3581b6fbb
Jeremy Stribling <strib@alum.mit.edu>
2024-01-18 20:52:11 -0800

    GR: make Jackpot Greene a reaction instead of a reap ability

--- server/game/cards/07-GR/JackpotGreene.js
+++ server/game/cards/07-GR/JackpotGreene.js
@@ -5,24 +5,23 @@ class JackpotGreene extends Card {
     // controller's deck. If the discarded card is a Star Alliance
     // card, you gain 1.
     setupCardAbilities(ability) {
-        this.persistentEffect({
-            targetController: 'any',
-            match: (card) => card.type === 'creature',
-            effect: ability.effects.gainAbility('reap', {
-                condition: (context) => context.source.controller.deck.length > 0,
-                gameAction: ability.actions.discard((context) => ({
-                    target: context.source.controller.deck[0]
-                })),
-                then: {
-                    condition: (context) =>
-                        context.preThenEvents &&
-                        context.preThenEvents.length > 0 &&
-                        context.preThenEvents[0].card.hasHouse('staralliance'),
-                    gameAction: ability.actions.gainAmber({
-                        target: this.controller
-                    })
-                }
-            })
+        this.reaction({
+            when: {
+                onReap: () => true
+            },
+            condition: (context) => context.event.card.controller.deck.length > 0,
+            gameAction: ability.actions.discard((context) => ({
+                target: context.event.card.controller.deck[0]
+            })),
+            then: {
+                condition: (context) =>
+                    context.preThenEvents &&
+                    context.preThenEvents.length > 0 &&
+                    context.preThenEvents[0].card.hasHouse('staralliance'),
+                gameAction: ability.actions.gainAmber((context) => ({
+                    target: context.source.controller
+                }))
+            }
         });
     }
 }
