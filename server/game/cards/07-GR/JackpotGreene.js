8d8e464f7b16077d149d0b74e41d88f8607af69b
Jeremy Stribling <strib@users.noreply.github.com>
2024-01-19 12:14:03 -0800

    GR: implement Jackpot Greene (#3603)
    * GR: implement Jackpot Greene
    
    * GR: make Jackpot Greene a reaction instead of a reap ability

---
+++ server/game/cards/07-GR/JackpotGreene.js
@@ -0,0 +1,31 @@
+const Card = require('../../Card.js');
+
+class JackpotGreene extends Card {
+    // After a creature reaps, discard the top card of its
+    // controller's deck. If the discarded card is a Star Alliance
+    // card, you gain 1.
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onReap: () => true
+            },
+            condition: (context) => context.event.card.controller.deck.length > 0,
+            gameAction: ability.actions.discard((context) => ({
+                target: context.event.card.controller.deck[0]
+            })),
+            then: {
+                condition: (context) =>
+                    context.preThenEvents &&
+                    context.preThenEvents.length > 0 &&
+                    context.preThenEvents[0].card.hasHouse('staralliance'),
+                gameAction: ability.actions.gainAmber((context) => ({
+                    target: context.source.controller
+                }))
+            }
+        });
+    }
+}
+
+JackpotGreene.id = 'jackpot-greene';
+
+module.exports = JackpotGreene;
