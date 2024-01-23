4e9f9806e7fa490b122ddf8a918c2c63c509f70f
Jeremy Stribling <strib@users.noreply.github.com>
2024-01-23 12:09:21 -0800

    GR: implement Soul Exchange (#3658)
    * GR: implement Soul Exchange
    
    * GR: simplify Soul Exchange
    
    Suggested by romanogit.
    
    Issue: #3658

---
+++ server/game/cards/07-GR/SoulExchange.js
@@ -0,0 +1,56 @@
+const Card = require('../../Card.js');
+
+function firstCreatureInDiscard(context) {
+    const player =
+        !context.select || context.select === 'Mine' ? context.player : context.player.opponent;
+    return player.discard.find((c) => c.type === 'creature');
+}
+
+class SoulExchange extends Card {
+    // Play: Choose a player. Return the topmost creature from that
+    // player’s discard pile to their hand. Destroy a creature
+    // controlled by that player.
+    setupCardAbilities(ability) {
+        this.play({
+            target: {
+                mode: 'select',
+                activePromptTitle: "Which player's discard",
+                choices: {
+                    Mine: () => true,
+                    "Opponent's": (context) => !!context.player.opponent
+                }
+            },
+            gameAction: ability.actions.conditional((context) => ({
+                condition: !!firstCreatureInDiscard(context),
+                trueGameAction: ability.actions.returnToHand({
+                    location: 'discard',
+                    target: firstCreatureInDiscard(context)
+                })
+            })),
+            effect: "return {1} to {2}'s hand",
+            effectArgs: (context) => [
+                firstCreatureInDiscard(context) ? firstCreatureInDiscard(context) : 'no creature',
+                !context.select || context.select === 'Mine'
+                    ? context.player
+                    : context.player.opponent
+            ],
+            then: (preThenContext) => ({
+                alwaysTriggers: true,
+                target: {
+                    controller:
+                        !preThenContext.select || preThenContext.select === 'Mine'
+                            ? 'self'
+                            : 'opponent',
+                    cardType: 'creature',
+                    gameAction: ability.actions.destroy()
+                },
+                message: '{0} uses {1} to destroy {3}',
+                messageArgs: (context) => [context.target]
+            })
+        });
+    }
+}
+
+SoulExchange.id = 'soul-exchange';
+
+module.exports = SoulExchange;
