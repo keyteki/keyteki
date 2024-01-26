5219e4c1448f460155e2072889381e02564788c5
David Mikalova <324548+dmikalova@users.noreply.github.com>
2024-01-26 05:24:17 -0800

    GR: implement Curse of Disappearances (#3626)
    * feat: implement Curse of Disappearances
    
    * fix: remove irrelevant changes
    
    * Update CurseOfDisappearances.js
    
    Removed keyword part
    
    ---------
    
    Co-authored-by: Romano Silva <romano.magacho@gmail.com>

---
+++ server/game/cards/07-GR/CurseOfDisappearances.js
@@ -0,0 +1,27 @@
+const Card = require('../../Card.js');
+
+class CurseOfDisappearances extends Card {
+    // Treachery. (This card enters play under your opponent’s control.)
+    //
+    // At the end of your turn, put a friendly creature into your opponent’s
+    // archives. If that card leaves their archives, put it into its owner's
+    // hand instead.
+    setupCardAbilities(ability) {
+        this.interrupt({
+            when: {
+                onRoundEnded: (_, context) => context.player === this.game.activePlayer
+            },
+            gameAction: ability.actions.archive({
+                promptForSelect: {
+                    activePromptTitle: 'Choose a creature to archive',
+                    cardType: 'creature',
+                    controller: 'self'
+                }
+            })
+        });
+    }
+}
+
+CurseOfDisappearances.id = 'curse-of-disappearances';
+
+module.exports = CurseOfDisappearances;
