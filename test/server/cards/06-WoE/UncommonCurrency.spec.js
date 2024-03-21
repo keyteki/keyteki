c2c68d2338dc61f417b36de829f5b34b917b45fb
Jeremy Stribling <strib@users.noreply.github.com>
2024-03-21 05:53:54 -0700

    WoE: Uncommon Currency should do nothing with no enemy artifacts (#3914)
    Closes: #3911

--- test/server/cards/06-WoE/UncommonCurrency.spec.js
+++ test/server/cards/06-WoE/UncommonCurrency.spec.js
@@ -1,5 +1,5 @@
 describe('Uncommon Currency', function () {
-    describe("Anahita the Trader's ability", function () {
+    describe("Uncommon Currency's ability", function () {
         beforeEach(function () {
             this.setupTest({
                 player1: {
@@ -14,7 +14,7 @@ describe('Uncommon Currency', function () {
             });
         });
 
-        it('should swap control with an enemy creature', function () {
+        it('should swap control with an enemy artifact', function () {
             this.player1.useAction(this.uncommonCurrency);
             expect(this.player1).not.toBeAbleToSelect(this.potionOfInvulnerability);
             expect(this.player1).toBeAbleToSelect(this.bearFlute);
@@ -25,6 +25,14 @@ describe('Uncommon Currency', function () {
             expect(this.player2.player.cardsInPlay).toContain(this.uncommonCurrency);
             expect(this.nepentheSeed.controller).toBe(this.player1.player);
             expect(this.uncommonCurrency.controller).toBe(this.player2.player);
+        });
+
+        it('should do nothing if no enemy artifact', function () {
+            this.player2.moveCard(this.nepentheSeed, 'discard');
+            this.player2.moveCard(this.bearFlute, 'discard');
+            this.player1.useAction(this.uncommonCurrency);
+            expect(this.uncommonCurrency.controller).toBe(this.player1.player);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
         });
     });
 });
