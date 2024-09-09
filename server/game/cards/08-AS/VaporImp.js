a85c0dbe28ecb88bda5c571e72143be29298e17c
Jeremy Stribling <strib@alum.mit.edu>
2024-09-08 19:14:22 -0700

    AS: implement Vapor Imp

---
+++ server/game/cards/08-AS/VaporImp.js
@@ -0,0 +1,28 @@
+const Card = require('../../Card.js');
+
+class VaporImp extends Card {
+    // After Reap: Discard a random card from your hand. During your
+    // opponent’s next turn, they cannot play cards of the discarded
+    // card’s type.
+    setupCardAbilities(ability) {
+        this.reap({
+            gameAction: ability.actions.discardAtRandom((context) => ({
+                target: context.player
+            })),
+            then: (preThenContext) => ({
+                condition: (context) => context.player.opponent && context.preThenEvent.cards.length > 0,
+                gameAction: ability.actions.nextRoundEffect((context) => ({
+                    targetController: 'opponent',
+                    effect: ability.effects.playerCannot(
+                        'play',
+                        (innerContext) => innerContext.source.type === context.preThenEvent.cards[0].type
+                    )
+                }))
+            })
+        });
+    }
+}
+
+VaporImp.id = 'vapor-imp';
+
+module.exports = VaporImp;
