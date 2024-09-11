b53b869f5b0e9966f041c21b9e6be62f90e2efba
Jeremy Stribling <strib@users.noreply.github.com>
2024-09-11 12:09:23 -0700

    AS: implement Crepuscular Rays (#3980)

---
+++ test/server/cards/08-AS/CrepuscularRays.spec.js
@@ -0,0 +1,31 @@
+describe('Crepuscular Rays', function () {
+    describe("Crepuscular Ray's play ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 1,
+                    house: 'geistoid',
+                    hand: ['crepuscular-rays'],
+                    inPlay: ['umbra', 'lamindra']
+                },
+                player2: {
+                    inPlay: ['redlock', 'krump']
+                }
+            });
+
+            this.umbra.amber = 3;
+        });
+
+        it('should destroy a friendly creature and gain amber', function () {
+            this.player1.play(this.crepuscularRays);
+            expect(this.player1).toBeAbleToSelect(this.umbra);
+            expect(this.player1).toBeAbleToSelect(this.lamindra);
+            expect(this.player1).not.toBeAbleToSelect(this.redlock);
+            expect(this.player1).not.toBeAbleToSelect(this.krump);
+            this.player1.clickCard(this.umbra);
+            expect(this.umbra.location).toBe('discard');
+            expect(this.player1.amber).toBe(5);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
