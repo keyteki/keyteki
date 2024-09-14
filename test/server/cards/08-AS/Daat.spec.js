5c94478b89854a380bf11de78be214f9d4f5d177
Jeremy Stribling <strib@alum.mit.edu>
2024-09-14 15:11:14 -0700

    AS: implement Daat

---
+++ test/server/cards/08-AS/Daat.spec.js
@@ -0,0 +1,39 @@
+describe('Daat', function () {
+    describe("Daat's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 1,
+                    house: 'dis',
+                    hand: ['daat'],
+                    inPlay: ['ancient-bear', 'shooler']
+                },
+                player2: {
+                    inPlay: ['troll']
+                }
+            });
+        });
+
+        it('should change active house on play', function () {
+            this.player1.play(this.daat);
+            expect(this.player1).toHavePrompt('Daat');
+            expect(this.player1).toHavePromptButton('brobnar');
+            expect(this.player1).toHavePromptButton('dis');
+            expect(this.player1).toHavePromptButton('logos');
+            expect(this.player1).toHavePromptButton('mars');
+            expect(this.player1).toHavePromptButton('sanctum');
+            expect(this.player1).toHavePromptButton('shadows');
+            expect(this.player1).toHavePromptButton('untamed');
+            expect(this.player1).toHavePromptButton('staralliance');
+            expect(this.player1).toHavePromptButton('saurian');
+            expect(this.player1).toHavePromptButton('ekwidon');
+            expect(this.player1).toHavePromptButton('geistoid');
+            expect(this.player1).toHavePromptButton('skyborn');
+            this.player1.clickPrompt('untamed');
+            this.player1.reap(this.ancientBear);
+            expect(this.player1.amber).toBe(3);
+            this.player1.clickCard(this.shooler);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
