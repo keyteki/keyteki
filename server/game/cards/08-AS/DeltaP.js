c926e163c875363d5a9aa017bd746769ff8053c2
Jeremy Stribling <strib@alum.mit.edu>
2024-09-13 18:46:04 -0700

    AS: implement Delta P

---
+++ server/game/cards/08-AS/DeltaP.js
@@ -0,0 +1,27 @@
+const Card = require('../../Card.js');
+
+class DeltaP extends Card {
+    // Play: Destroy an enemy creature. If your opponent controls more
+    // creatures than you, repeat this effect.
+    setupCardAbilities(ability) {
+        this.play({
+            condition: (context) => context.player.opponent,
+            target: {
+                cardType: 'creature',
+                controller: 'opponent',
+                gameAction: ability.actions.destroy()
+            },
+            then: (preThenContext) => ({
+                condition: (context) =>
+                    context.player.creaturesInPlay.length <
+                    context.player.opponent.creaturesInPlay.length,
+                alwaysTriggers: true,
+                gameAction: ability.actions.resolveAbility({ ability: preThenContext.ability })
+            })
+        });
+    }
+}
+
+DeltaP.id = 'delta-p';
+
+module.exports = DeltaP;
---
+++ test/server/cards/08-AS/DeltaP.spec.js
@@ -0,0 +1,55 @@
+describe('Delta P', function () {
+    describe("Delta P's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 1,
+                    house: 'dis',
+                    hand: ['delta-p', 'ember-imp', 'shooler', 'charette'],
+                    inPlay: ['dew-faerie']
+                },
+                player2: {
+                    inPlay: ['rowdy-skald', 'troll', 'krump']
+                }
+            });
+        });
+
+        it('should destroy creatures while opponent has more', function () {
+            this.player1.play(this.deltaP);
+            expect(this.player1).toBeAbleToSelect(this.rowdySkald);
+            expect(this.player1).toBeAbleToSelect(this.troll);
+            expect(this.player1).toBeAbleToSelect(this.krump);
+            expect(this.player1).not.toBeAbleToSelect(this.dewFaerie);
+            this.player1.clickCard(this.krump);
+            expect(this.krump.location).toBe('discard');
+            expect(this.player1).toBeAbleToSelect(this.rowdySkald);
+            expect(this.player1).toBeAbleToSelect(this.troll);
+            expect(this.player1).not.toBeAbleToSelect(this.dewFaerie);
+            this.player1.clickCard(this.troll);
+            expect(this.troll.location).toBe('discard');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should kill wards for free', function () {
+            this.krump.ward();
+            this.player1.play(this.deltaP);
+            this.player1.clickCard(this.krump);
+            expect(this.krump.location).toBe('play area');
+            this.player1.clickCard(this.krump);
+            expect(this.krump.location).toBe('discard');
+            this.player1.clickCard(this.troll);
+            expect(this.troll.location).toBe('discard');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should always destroy one creature', function () {
+            this.player1.playCreature(this.emberImp);
+            this.player1.playCreature(this.shooler);
+            this.player1.playCreature(this.charette);
+            this.player1.play(this.deltaP);
+            this.player1.clickCard(this.krump);
+            expect(this.krump.location).toBe('discard');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
