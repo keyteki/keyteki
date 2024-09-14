5711f882150dde3b7889b3dc856dfd130589c845
Jeremy Stribling <strib@alum.mit.edu>
2024-09-13 18:04:39 -0700

    AS: implement Raise Your Glass

---
+++ server/game/cards/08-AS/RaiseYourGlass.js
@@ -0,0 +1,26 @@
+const Card = require('../../Card.js');
+
+class RaiseYourGlass extends Card {
+    // Play: Ready and use the least powerful friendly creature.
+    setupCardAbilities(ability) {
+        this.play({
+            target: {
+                cardType: 'creature',
+                controller: 'self',
+                mode: 'leastStat',
+                numCards: 1,
+                cardStat: (card) => card.power,
+                gameAction: ability.actions.sequential([
+                    ability.actions.ready(),
+                    ability.actions.use()
+                ])
+            },
+            effect: 'ready and use {1}',
+            effectArgs: (context) => [context.target]
+        });
+    }
+}
+
+RaiseYourGlass.id = 'raise-your-glass';
+
+module.exports = RaiseYourGlass;
