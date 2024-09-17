011131fa0fc37c9f094698f6d347855b2e1bee62
Jeremy Stribling <strib@alum.mit.edu>
2024-09-17 14:41:41 -0700

    AS: implement Ace Jonavan

---
+++ server/game/cards/08-AS/AceJonavan.js
@@ -0,0 +1,20 @@
+const Card = require('../../Card.js');
+
+class AceJonavan extends Card {
+    // For each A on Ace Jonavan, keys cost +1A.
+    // Play: Capture 1A.
+    setupCardAbilities(ability) {
+        this.play({
+            gameAction: ability.actions.capture()
+        });
+
+        this.persistentEffect({
+            targetController: 'any',
+            effect: ability.effects.modifyKeyCost((_, context) => context.source.amber)
+        });
+    }
+}
+
+AceJonavan.id = 'ace-jonavan';
+
+module.exports = AceJonavan;
