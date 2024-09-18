9557438a9d934f7e2c8fed3dddb5f48022e8cf68
Jeremy Stribling <strib@alum.mit.edu>
2024-09-17 21:19:41 -0700

    AS: change Barter and Games to choose which player first

--- server/game/cards/08-AS/BarterAndGames.js
+++ server/game/cards/08-AS/BarterAndGames.js
@@ -20,97 +20,119 @@ function cardForEvent(event) {
     return event ? [event.card] : [];
 }
 
+function locationFromContext(context) {
+    return context.select === 'My Hand' || context.select === "Opponent's Hand"
+        ? 'hand'
+        : 'archives';
+}
+
+function zoneFromContext(context) {
+    return context.select === 'My Hand' || context.select === 'My Archives'
+        ? context.select === 'My Hand'
+            ? context.player.hand
+            : context.player.archives
+        : context.select === "Opponent's Hand"
+        ? context.player.opponent.hand
+        : context.player.opponent.archives;
+}
+
 class BarterAndGames extends Card {
-    // Play: Each player reveals a random card from their hand or
-    // archives. Each player gains 1A for each A bonus icon on their
+    // Play: Each player reveals a random card from their Hand or
+    // Archives. Each player gains 1A for each A bonus icon on their
     // revealed card. Destroy each creature that shares a house with
     // at least one of the revealed cards. Discard the revealed cards.
     setupCardAbilities(ability) {
         this.play({
-            targets: {
-                self: {
-                    activePromptTitle: 'Where to reveal a random card for you?',
-                    mode: 'select',
-                    choices: {
-                        Hand: () => true,
-                        Archives: () => true
-                    }
-                },
-                opponent: {
-                    activePromptTitle: 'Where to reveal a random card for opponent?',
-                    mode: 'select',
-                    choices: {
-                        Hand: () => true,
-                        Archives: () => true
-                    }
+            target: {
+                activePromptTitle: 'Where to reveal a card from first?',
+                mode: 'select',
+                choices: {
+                    'My Hand': () => true,
+                    'My Archives': () => true,
+                    "Opponent's Hand": (context) => !!context.player.opponent,
+                    "Opponent's Archives": (context) => !!context.player.opponent
                 }
             },
-            gameAction: [
-                ability.actions.reveal((context) => ({
-                    location:
-                        context.selects.self && context.selects.self.choice === 'Hand'
-                            ? 'hand'
-                            : 'archives',
-                    chatMessage: false,
-                    target: _.shuffle(
-                        context.selects.self && context.selects.self.choice === 'Hand'
-                            ? context.player.hand
-                            : context.player.archives
-                    ).slice(0, 1)
-                })),
-                ability.actions.reveal((context) => ({
-                    location:
-                        context.selects.opponent && context.selects.opponent.choice === 'Hand'
-                            ? 'hand'
-                            : 'archives',
-                    chatMessage: false,
-                    target: _.shuffle(
-                        context.player.opponent
-                            ? context.selects.opponent && context.selects.opponent.choice === 'Hand'
-                                ? context.player.opponent.hand
-                                : context.player.opponent.archives
-                            : []
-                    ).slice(0, 1)
-                }))
-            ],
-            effect: 'reveal cards from both players',
-            then: {
+            gameAction: ability.actions.reveal((context) => ({
+                location: locationFromContext(context),
+                target: _.shuffle(zoneFromContext(context)).slice(0, 1)
+            })),
+            then: (preThenContext) => ({
                 alwaysTriggers: true,
-                gameAction: ability.actions.sequential([
-                    ability.actions.gainAmber((context) => ({
-                        amount: amountForEvents(context.preThenEvents, context.player)
-                    })),
-                    ability.actions.gainAmber((context) => ({
-                        target: context.player.opponent,
-                        amount: amountForEvents(context.preThenEvents, context.player.opponent)
-                    })),
-                    ability.actions.destroy((context) => ({
-                        target: context.game.creaturesInPlay.filter(
-                            (c) =>
-                                hasHouseForEvent(c, context.preThenEvents[0]) ||
-                                hasHouseForEvent(c, context.preThenEvents[1])
-                        )
-                    })),
-                    ability.actions.discard((context) => ({
-                        target: cardForEvent(context.preThenEvents[0]).concat(
-                            cardForEvent(context.preThenEvents[1])
-                        )
-                    }))
-                ]),
-                message:
-                    '{0} uses {1} to reveal {3}, gain {4} amber, {5}destroy creatures sharing a house with the revealed cards, and discard the revealed cards',
-                messageArgs: (context) => [
-                    context.preThenEvents.map((e) => e.card),
-                    amountForEvents(context.preThenEvents, context.player),
-                    context.player.opponent
-                        ? 'cause ' +
-                          context.player.opponent.name +
-                          ' to gain ' +
-                          amountForEvents(context.preThenEvents, context.player.opponent) +
-                          ' amber, '
-                        : ''
-                ]
-            }
+                target: {
+                    activePromptTitle: 'Where to reveal a card from second?',
+                    mode: 'select',
+                    choices: {
+                        'My Hand': () =>
+                            preThenContext.select !== 'My Hand' &&
+                            preThenContext.select !== 'My Archives',
+                        'My Archives': () =>
+                            preThenContext.select !== 'My Hand' &&
+                            preThenContext.select !== 'My Archives',
+                        "Opponent's Hand": (context) =>
+                            context.player.opponent &&
+                            preThenContext.select !== "Opponent's Hand" &&
+                            preThenContext.select !== "Opponent's Archives",
+                        "Opponent's Archives": (context) =>
+                            context.player.opponent &&
+                            preThenContext.select !== "Opponent's Hand" &&
+                            preThenContext.select !== "Opponent's Archives"
+                    }
+                },
+                gameAction: ability.actions.reveal((context) => ({
+                    location: locationFromContext(context),
+                    chatMessage: true,
+                    target: _.shuffle(zoneFromContext(context)).slice(0, 1)
+                })),
+                then: (preThenContext) => ({
+                    alwaysTriggers: true,
+                    gameAction: ability.actions.sequential([
+                        ability.actions.gainAmber((context) => ({
+                            amount: amountForEvents(
+                                context.preThenEvents.concat(preThenContext.preThenEvents),
+                                context.player
+                            )
+                        })),
+                        ability.actions.gainAmber((context) => ({
+                            target: context.player.opponent,
+                            amount: amountForEvents(
+                                context.preThenEvents.concat(preThenContext.preThenEvents),
+                                context.player.opponent
+                            )
+                        })),
+                        ability.actions.destroy((context) => ({
+                            target: context.game.creaturesInPlay.filter(
+                                (c) =>
+                                    hasHouseForEvent(c, context.preThenEvents[0]) ||
+                                    hasHouseForEvent(c, preThenContext.preThenEvents[0])
+                            )
+                        })),
+                        ability.actions.discard((context) => ({
+                            target: cardForEvent(context.preThenEvents[0]).concat(
+                                cardForEvent(preThenContext.preThenEvents[0])
+                            )
+                        }))
+                    ]),
+                    message:
+                        '{0} uses {1} to gain {3} amber, {4}destroy creatures sharing a house with the revealed cards, and discard the revealed cards',
+                    messageArgs: (context) => [
+                        amountForEvents(
+                            context.preThenEvents.concat(preThenContext.preThenEvents),
+                            context.player
+                        ),
+                        context.player.opponent
+                            ? 'cause ' +
+                              context.player.opponent.name +
+                              ' to gain ' +
+                              amountForEvents(
+                                  context.preThenEvents.concat(preThenContext.preThenEvents),
+                                  context.player.opponent
+                              ) +
+                              ' amber, '
+                            : ''
+                    ]
+                })
+            })
         });
     }
 }
--- test/server/cards/08-AS/BarterAndGames.spec.js
+++ test/server/cards/08-AS/BarterAndGames.spec.js
@@ -19,8 +19,10 @@ describe('Barter and Games', function () {
 
         it('should reveal a card from each hand', function () {
             this.player1.play(this.barterAndGames);
-            this.player1.clickPrompt('Hand');
-            this.player1.clickPrompt('Hand');
+            this.player1.clickPrompt('My Hand');
+            expect(this.player1).not.toHavePromptButton('My Hand');
+            expect(this.player1).not.toHavePromptButton('My Archives');
+            this.player1.clickPrompt("Opponent's Hand");
             expect(this.player1.amber).toBe(3);
             expect(this.player2.amber).toBe(2);
             expect(this.ancientBear.location).toBe('discard');
@@ -36,8 +38,8 @@ describe('Barter and Games', function () {
 
         it('should reveal a card from each archive', function () {
             this.player1.play(this.barterAndGames);
-            this.player1.clickPrompt('Archives');
-            this.player1.clickPrompt('Archives');
+            this.player1.clickPrompt('My Archives');
+            this.player1.clickPrompt("Opponent's Archives");
             expect(this.player1.amber).toBe(1);
             expect(this.player2.amber).toBe(1);
             expect(this.ancientBear.location).toBe('play area');
@@ -55,8 +57,26 @@ describe('Barter and Games', function () {
         it('should work with no cards in hand', function () {
             this.player1.moveCard(this.dustPixie, 'discard');
             this.player1.play(this.barterAndGames);
-            this.player1.clickPrompt('Hand');
-            this.player1.clickPrompt('Hand');
+            this.player1.clickPrompt('My Hand');
+            this.player1.clickPrompt("Opponent's Hand");
+            expect(this.player1.amber).toBe(1);
+            expect(this.player2.amber).toBe(2);
+            expect(this.ancientBear.location).toBe('play area');
+            expect(this.theOldTinker.location).toBe('play area');
+            expect(this.monsquito.location).toBe('discard');
+            expect(this.huntingWitch.location).toBe('play area');
+            expect(this.player1.hand.length).toBe(0);
+            expect(this.player1.discard.length).toBe(2);
+            expect(this.player2.hand.length).toBe(1);
+            expect(this.player2.discard.length).toBe(2);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should work with no cards in hand going second', function () {
+            this.player1.moveCard(this.dustPixie, 'discard');
+            this.player1.play(this.barterAndGames);
+            this.player1.clickPrompt("Opponent's Hand");
+            this.player1.clickPrompt('My Hand');
             expect(this.player1.amber).toBe(1);
             expect(this.player2.amber).toBe(2);
             expect(this.ancientBear.location).toBe('play area');
