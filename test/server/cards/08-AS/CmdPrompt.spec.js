2dc21800f8b2361df87abf6c2d28c7c0e58a8a3e
Jeremy Stribling <strib@alum.mit.edu>
2024-09-18 19:01:55 -0700

    AS: implement Cmd. Prompt

---
+++ test/server/cards/08-AS/CmdPrompt.spec.js
@@ -0,0 +1,23 @@
+describe('Cmd. Prompt', function () {
+    describe("Cmd. Prompt's abilities", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'logos',
+                    hand: ['umbra', 'lamindra'],
+                    inPlay: ['cmd-prompt']
+                },
+                player2: {
+                    amber: 1
+                }
+            });
+        });
+
+        it('should allow one non-Logos card on reap', function () {
+            this.player1.reap(this.cmdPrompt);
+            this.player1.playCreature(this.umbra);
+            this.player1.clickCard(this.lamindra);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
