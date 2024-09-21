a6021409bfce5e49447cee92d1405511c2aee4ea
Jeremy Stribling <strib@alum.mit.edu>
2024-09-21 07:49:40 -0700

    AS: implement Prop Dusting

---
+++ test/server/cards/08-AS/PropDusting.spec.js
@@ -0,0 +1,30 @@
+describe('Prop Dusting', function () {
+    describe("Prop Dusting's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 1,
+                    house: 'skyborn',
+                    hand: ['prop-dusting'],
+                    inPlay: ['hunting-witch', 'bosun-creen']
+                },
+                player2: {
+                    amber: 4,
+                    inPlay: ['dust-pixie', 'eldest-bear', 'troll']
+                }
+            });
+        });
+
+        it('should destroy an enemy flank creature', function () {
+            this.player1.play(this.propDusting);
+            expect(this.player1).not.toBeAbleToSelect(this.huntingWitch);
+            expect(this.player1).not.toBeAbleToSelect(this.bosunCreen);
+            expect(this.player1).toBeAbleToSelect(this.dustPixie);
+            expect(this.player1).not.toBeAbleToSelect(this.eldestBear);
+            expect(this.player1).toBeAbleToSelect(this.troll);
+            this.player1.clickCard(this.troll);
+            expect(this.troll.location).toBe('discard');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
