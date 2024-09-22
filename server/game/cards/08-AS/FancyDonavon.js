1945ffa16dae9b05ccbbbbc71792f2a98d348880
Jeremy Stribling <strib@users.noreply.github.com>
2024-09-22 10:45:30 -0700

    AS: implement Fancy Donavon (#4015)

---
+++ server/game/cards/08-AS/FancyDonavon.js
@@ -0,0 +1,25 @@
+const Card = require('../../Card.js');
+
+class FancyDonavon extends Card {
+    // Scrap: Ready and fight with a friendly non-Brobnar creature.
+    setupCardAbilities(ability) {
+        this.scrap({
+            target: {
+                cardType: 'creature',
+                controller: 'self',
+                numCards: 1,
+                cardCondition: (card) => !card.hasHouse('brobnar'),
+                gameAction: ability.actions.sequential([
+                    ability.actions.ready(),
+                    ability.actions.fight()
+                ])
+            },
+            effect: 'ready and fight with {1}',
+            effectArgs: (context) => [context.target]
+        });
+    }
+}
+
+FancyDonavon.id = 'fancy-donavon';
+
+module.exports = FancyDonavon;
