3094436a15ceb3637411b2ef7e3d42554ea99926
Jeremy Stribling <strib@users.noreply.github.com>
2024-09-22 10:36:07 -0700

    AS: implement Belcher (#3996)
    * AS: implement Belcher
    
    * f
    
    ---------
    
    Co-authored-by: Stuart Walsh <stu@ipng.org.uk>

---
+++ server/game/cards/08-AS/Belcher.js
@@ -0,0 +1,56 @@
+const Card = require('../../Card.js');
+
+function discard(deck) {
+    const index = deck.findIndex((card) => card.type === 'creature');
+    if (index > -1) {
+        return { target: deck.slice(0, index + 1) };
+    }
+
+    return { target: deck };
+}
+
+function wasCreatureDiscarded(context) {
+    return (
+        context.preThenEvents.length > 0 &&
+        context.preThenEvents[context.preThenEvents.length - 1].card.type === 'creature'
+    );
+}
+
+class Belcher extends Card {
+    // Action: Discard cards from the top of your deck until you
+    // discard a creature or run out of cards. If you discarded a
+    // creature this way, deal D equal to the discarded creature’s
+    // power to an enemy creature.
+    setupCardAbilities(ability) {
+        this.action({
+            gameAction: ability.actions.discard((context) => discard(context.player.deck)),
+            then: {
+                alwaysTriggers: true,
+                condition: wasCreatureDiscarded,
+                target: {
+                    cardType: 'creature',
+                    controller: 'opponent',
+                    gameAction: ability.actions.dealDamage((context) => ({
+                        amount:
+                            context.preThenEvents.length > 0
+                                ? context.preThenEvents[
+                                      context.preThenEvents.length - 1
+                                  ].card.getPower()
+                                : 0
+                    }))
+                },
+                message: '{0} uses {1} to deal {3} damage to {4}',
+                messageArgs: (context) => [
+                    context.preThenEvents.length > 0
+                        ? context.preThenEvents[context.preThenEvents.length - 1].card.getPower()
+                        : 0,
+                    context.target
+                ]
+            }
+        });
+    }
+}
+
+Belcher.id = 'belcher';
+
+module.exports = Belcher;
