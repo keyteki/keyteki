a7579657d8233710d73e46c21decda7d901f1f7e
Jeremy Stribling <strib@users.noreply.github.com>
2024-09-22 10:43:38 -0700

    AS: implement Brunx and Bongle (#4009)
    Co-authored-by: Stuart Walsh <stu@ipng.org.uk>

---
+++ server/game/cards/08-AS/BrunxAndBongle.js
@@ -0,0 +1,23 @@
+const Card = require('../../Card.js');
+
+class BrunxAndBongle extends Card {
+    // After Fight: If this was the first time Brunx and Bongle was
+    // used this turn, you may ready and reap with it.
+    setupCardAbilities(ability) {
+        this.fight({
+            condition: (context) =>
+                context.game.cardsUsed.filter((card) => card === context.source).length === 1,
+            optional: true,
+            effect: 'ready {1}',
+            effectArgs: (context) => [context.source],
+            gameAction: ability.actions.sequential([
+                ability.actions.ready(),
+                ability.actions.reap()
+            ])
+        });
+    }
+}
+
+BrunxAndBongle.id = 'brunx-and-bongle';
+
+module.exports = BrunxAndBongle;
---
+++ test/server/cards/08-AS/BrunxAndBongle.spec.js
@@ -0,0 +1,45 @@
+describe('Brunx and Bongle', function () {
+    describe("Brunx and Bongle's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 1,
+                    house: 'brobnar',
+                    hand: ['anger'],
+                    inPlay: ['brunx-and-bongle']
+                },
+                player2: {
+                    inPlay: ['lamindra']
+                }
+            });
+        });
+
+        it('should allow a reap after a fight', function () {
+            this.player1.fightWith(this.brunxAndBongle, this.lamindra);
+            this.player1.clickCard(this.brunxAndBongle);
+            expect(this.player1.amber).toBe(2);
+            expect(this.brunxAndBongle.exhausted).toBe(true);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should be optional', function () {
+            this.player1.fightWith(this.brunxAndBongle, this.lamindra);
+            this.player1.clickPrompt('Done');
+            expect(this.player1.amber).toBe(1);
+            expect(this.brunxAndBongle.exhausted).toBe(true);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should only work once', function () {
+            this.player1.fightWith(this.brunxAndBongle, this.lamindra);
+            this.player1.clickCard(this.brunxAndBongle);
+            this.player1.play(this.anger);
+            this.player1.clickCard(this.brunxAndBongle);
+            this.player1.clickCard(this.lamindra);
+            expect(this.player1.amber).toBe(3); // One reap + anger
+            expect(this.brunxAndBongle.exhausted).toBe(true);
+            expect(this.lamindra.location).toBe('discard');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
