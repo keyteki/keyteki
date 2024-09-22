0b0493cc417e7f2dfa17be8b768abf905c6d52b0
Jeremy Stribling <strib@alum.mit.edu>
2024-09-22 13:28:30 -0700

    AS: implement Lancet

---
+++ test/server/cards/08-AS/Lancet.spec.js
@@ -0,0 +1,27 @@
+describe('Lancet', function () {
+    describe("Lancet 's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 1,
+                    house: 'dis',
+                    inPlay: ['lancet', 'gub']
+                },
+                player2: {
+                    amber: 3,
+                    inPlay: ['lamindra']
+                }
+            });
+        });
+
+        it('should give each friendly creature a fight ability to capture 1', function () {
+            this.player1.fightWith(this.gub, this.lamindra);
+            expect(this.gub.amber).toBe(1);
+            expect(this.player2.amber).toBe(2);
+            this.player1.fightWith(this.lancet, this.lamindra);
+            expect(this.gub.amber).toBe(1);
+            expect(this.player2.amber).toBe(1);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
