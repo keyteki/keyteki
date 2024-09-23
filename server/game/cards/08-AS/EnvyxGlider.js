455bf88e0412bd8f93b85e29a8fa9bc11468be50
David Brokaw <dgbrokaw2@gmail.com>
2024-09-23 02:49:53 -0600

    AS: implement Envyx Glider (Mars) (#4170)

---
+++ server/game/cards/08-AS/EnvyxGlider.js
@@ -0,0 +1,24 @@
+const Card = require('../../Card.js');
+
+class EnvyxGlider extends Card {
+    // After Fight: During your opponent's next turn, keys cost +1A for each friendly Mars creature in play.
+    setupCardAbilities(ability) {
+        this.fight({
+            targetController: 'opponent',
+            gameAction: ability.actions.nextRoundEffect({
+                targetController: 'any',
+                effect: ability.effects.modifyKeyCost(
+                    (player) =>
+                        (player.opponent &&
+                            player.opponent.creaturesInPlay.filter((card) => card.hasHouse('mars'))
+                                .length) ||
+                        0
+                )
+            })
+        });
+    }
+}
+
+EnvyxGlider.id = 'envyx-glider';
+
+module.exports = EnvyxGlider;
