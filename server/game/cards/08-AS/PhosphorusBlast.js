760ea47ecfc6cafa256b63fbe033903ac8ce867c
David Brokaw <dgbrokaw2@gmail.com>
2024-09-23 02:51:13 -0600

    AS: implement Phosphorus Blast (Mars) (#4164)

---
+++ server/game/cards/08-AS/PhosphorusBlast.js
@@ -0,0 +1,18 @@
+const Card = require('../../Card.js');
+
+class PhosphorusBlast extends Card {
+    // Play: Deal 2D to each non-Mars creature.
+    setupCardAbilities(ability) {
+        this.play({
+            effect: 'deal 2 damage to all non-Mars creatures',
+            gameAction: ability.actions.dealDamage((context) => ({
+                amount: 2,
+                target: context.game.creaturesInPlay.filter((card) => !card.hasHouse('mars'))
+            }))
+        });
+    }
+}
+
+PhosphorusBlast.id = 'phosphorus-blast';
+
+module.exports = PhosphorusBlast;
