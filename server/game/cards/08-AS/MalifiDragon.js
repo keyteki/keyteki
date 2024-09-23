c95f966436c9411dfeeb8812710f60506444bd13
Jeremy Stribling <strib@users.noreply.github.com>
2024-09-23 02:18:21 -0700

    AS: implement Malifi Dragon (#4149)

---
+++ server/game/cards/08-AS/MalifiDragon.js
@@ -0,0 +1,18 @@
+const Card = require('../../Card.js');
+
+class MalifiDragon extends Card {
+    // At the end your turn, if you have 4A or fewer, gain 2A.
+    setupCardAbilities(ability) {
+        this.interrupt({
+            when: {
+                onRoundEnded: (event, context) =>
+                    context.player === this.game.activePlayer && context.player.amber <= 4
+            },
+            gameAction: ability.actions.gainAmber({ amount: 2 })
+        });
+    }
+}
+
+MalifiDragon.id = 'malifi-dragon';
+
+module.exports = MalifiDragon;
