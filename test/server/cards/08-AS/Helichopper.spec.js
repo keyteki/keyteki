9aeeda92755f81c8af8fa62c2ba1782c5848698e
Jeremy Stribling <strib@users.noreply.github.com>
2024-09-26 01:22:50 -0700

    AS: implement Helichopper (#4060)

---
+++ test/server/cards/08-AS/Helichopper.spec.js
@@ -0,0 +1,34 @@
+describe('Helichopper', function () {
+    describe("Helichopper's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'geistoid',
+                    hand: ['a-strong-feeling'],
+                    inPlay: ['helichopper'],
+                    discard: new Array(9).fill('poke') // not yet haunted
+                },
+                player2: {
+                    amber: 4,
+                    inPlay: ['thing-from-the-deep']
+                }
+            });
+            this.player1.chains = 36;
+        });
+
+        it('should capture 2 amber as an action', function () {
+            this.player1.useAction(this.helichopper);
+            expect(this.helichopper.amber).toBe(2);
+            expect(this.player2.amber).toBe(2);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('gains +2 power when haunted for each amber', function () {
+            this.helichopper.amber = 2;
+            expect(this.helichopper.power).toBe(5);
+            this.player1.scrap(this.aStrongFeeling);
+            expect(this.helichopper.power).toBe(11);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
