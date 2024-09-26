69b3671417c88d8b061dbcd0dc0c1088c18a70da
Jeremy Stribling <strib@users.noreply.github.com>
2024-09-26 01:22:59 -0700

    AS: implement Wurm Tunnel (#4085)

---
+++ server/game/cards/08-AS/WurmTunnel.js
@@ -0,0 +1,25 @@
+const Card = require('../../Card.js');
+
+class WurmTunnel extends Card {
+    // Play: Move 1A from a creature to the common supply. If you do,
+    // draw 2 cards.
+    setupCardAbilities(ability) {
+        this.play({
+            target: {
+                mode: 'exactly',
+                numCards: 1,
+                cardType: 'creature',
+                gameAction: ability.actions.removeAmber({ amount: 1 })
+            },
+            then: {
+                gameAction: ability.actions.draw({
+                    amount: 2
+                })
+            }
+        });
+    }
+}
+
+WurmTunnel.id = 'wurm-tunnel';
+
+module.exports = WurmTunnel;
