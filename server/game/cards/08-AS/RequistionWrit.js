3f8a2a10984a53739264876cf38ccf7b267f5de8
Jeremy Stribling <strib@users.noreply.github.com>
2024-09-26 01:27:57 -0700

    AS: implement Requisition Writ (#4055)

---
+++ server/game/cards/08-AS/RequistionWrit.js
@@ -0,0 +1,32 @@
+const Card = require('../../Card.js');
+
+class RequisitionWrit extends Card {
+    // This creature gains, “After Reap: Pay your opponent 1A. If you
+    // do, take control of an enemy creature."
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.gainAbility('reap', {
+                gameAction: ability.actions.transferAmber((context) => ({
+                    target: context.player,
+                    amount: 1
+                })),
+                then: {
+                    target: {
+                        controller: 'opponent',
+                        cardType: 'creature',
+                        gameAction: ability.actions.cardLastingEffect((context) => ({
+                            duration: 'lastingEffect',
+                            effect: ability.effects.takeControl(context.player)
+                        }))
+                    },
+                    message: '{0} uses {1} to take control of {3}',
+                    messageArgs: (context) => [context.target]
+                }
+            })
+        });
+    }
+}
+
+RequisitionWrit.id = 'requisition-writ';
+
+module.exports = RequisitionWrit;
