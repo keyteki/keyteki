71441902f8607bc6b621bf1d72f8b9fb782a7016
Jeremy Stribling <strib@users.noreply.github.com>
2024-09-26 01:28:35 -0700

    AS: implement Ley, Earl of Hurl (#4095)

---
+++ server/game/cards/08-AS/LeyEarlOfHurl.js
@@ -0,0 +1,32 @@
+const Card = require('../../Card.js');
+
+class LeyEarlOfHurl extends Card {
+    // Destroyed: If Ley, Earl of Hurl is not on a flank, fully heal
+    // it, exhaust it, ward it, and move it to a flank instead.
+    setupCardAbilities(ability) {
+        this.destroyed({
+            condition: (context) => !context.source.isOnFlank(),
+            effect: 'heal all damage from {0}, exhaust it, ward it and move it to a flank',
+            effectArgs: () => this,
+            gameAction: [
+                ability.actions.heal({ fully: true }),
+                ability.actions.exhaust(),
+                ability.actions.ward(),
+                ability.actions.moveToFlank(),
+                ability.actions.changeEvent((context) => ({
+                    event: context.event,
+                    cancel: true,
+                    postHandler: (context) => (context.source.moribund = false)
+                })),
+                ability.actions.changeEvent((context) => ({
+                    event: context.event.triggeringEvent,
+                    cancel: true
+                }))
+            ]
+        });
+    }
+}
+
+LeyEarlOfHurl.id = 'ley-earl-of-hurl';
+
+module.exports = LeyEarlOfHurl;
