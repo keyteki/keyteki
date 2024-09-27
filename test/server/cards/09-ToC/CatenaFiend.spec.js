10faed40d58806a10967c2862dc313b70741b869
Jeremy Stribling <strib@alum.mit.edu>
2024-09-26 17:05:38 -0700

    ToC: implement Catena Fiend

---
+++ test/server/cards/09-ToC/CatenaFiend.spec.js
@@ -0,0 +1,45 @@
+describe('Catena Fiend', function () {
+    describe("Catena Fiend's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 1,
+                    house: 'dis',
+                    token: 'catena-fiend',
+                    inPlay: ['shooler', 'catena-fiend:toad']
+                },
+                player2: {
+                    amber: 6,
+                    inPlay: ['pelf', 'troll']
+                }
+            });
+
+            this.catenaFiend = this.player1.player.creaturesInPlay[1];
+        });
+
+        it('should steal one on fight', function () {
+            this.player1.fightWith(this.catenaFiend, this.pelf);
+            expect(this.player1.amber).toBe(2);
+            expect(this.player2.amber).toBe(5);
+        });
+
+        it('should deal 3 damage to a friendly creature on fight', function () {
+            this.player1.fightWith(this.catenaFiend, this.pelf);
+            expect(this.player1).toBeAbleToSelect(this.shooler);
+            expect(this.player1).toBeAbleToSelect(this.catenaFiend);
+            expect(this.player1).not.toBeAbleToSelect(this.troll);
+            this.player1.clickCard(this.shooler);
+            expect(this.shooler.tokens.damage).toBe(3);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should steal even when killing self', function () {
+            this.player1.fightWith(this.catenaFiend, this.pelf);
+            this.player1.clickCard(this.catenaFiend);
+            expect(this.catenaFiend.location).toBe('discard');
+            expect(this.player1.amber).toBe(2);
+            expect(this.player2.amber).toBe(5);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
