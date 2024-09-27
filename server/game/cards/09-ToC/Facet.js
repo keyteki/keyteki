6ab7c1f9010128b2e09869a45fc497a5aca6feb8
Jeremy Stribling <strib@alum.mit.edu>
2024-09-26 17:10:03 -0700

    ToC: implement Facet

---
+++ server/game/cards/09-ToC/Facet.js
@@ -0,0 +1,15 @@
+const Card = require('../../Card.js');
+
+class Facet extends Card {
+    // Destroyed: If it is your turn, gain 1A.
+    setupCardAbilities(ability) {
+        this.destroyed({
+            condition: (context) => context.source.controller === context.game.activePlayer,
+            gameAction: ability.actions.gainAmber()
+        });
+    }
+}
+
+Facet.id = 'facet';
+
+module.exports = Facet;
