c52a23cc076213b74d219edf5c5739d553ec3148
Jeremy Stribling <strib@alum.mit.edu>
2024-09-27 17:53:59 -0700

    ToC: implement Phantasm

---
+++ test/server/cards/09-ToC/Phantasm.spec.js
@@ -0,0 +1,30 @@
+describe('Phantasm', function () {
+    describe("Phantasm's abilities", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'geistoid',
+                    token: 'phantasm',
+                    hand: ['a-strong-feeling', 'touchstone'],
+                    inPlay: ['phantasm:toad']
+                },
+                player2: {
+                    amber: 1,
+                    hand: ['helper-bot']
+                }
+            });
+
+            this.phantasm = this.player1.player.creaturesInPlay[0];
+        });
+
+        it('should discard a card from hand on reap', function () {
+            this.player1.reap(this.phantasm);
+            expect(this.player1).toBeAbleToSelect(this.aStrongFeeling);
+            expect(this.player1).toBeAbleToSelect(this.touchstone);
+            expect(this.player1).not.toBeAbleToSelect(this.helperBot);
+            this.player1.clickCard(this.touchstone);
+            expect(this.touchstone.location).toBe('discard');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
