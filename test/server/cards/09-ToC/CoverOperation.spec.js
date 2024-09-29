0f3b75f5d5ee6cdbfeaacd15c9f40c2f5516c781
Jeremy Stribling <strib@alum.mit.edu>
2024-09-29 10:33:08 -0700

    ToC: implement Cover Operation

---
+++ test/server/cards/09-ToC/CoverOperation.spec.js
@@ -0,0 +1,26 @@
+describe('Cover Operation', function () {
+    describe("Cover Operation's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 1,
+                    house: 'shadows',
+                    token: 'stooge',
+                    hand: ['cover-operation']
+                },
+                player2: {
+                    amber: 1
+                }
+            });
+
+            this.stooge1 = this.player1.player.deck[0];
+        });
+
+        it('should make and ready a token', function () {
+            this.player1.play(this.coverOperation);
+            expect(this.stooge1.location).toBe('play area');
+            expect(this.stooge1.exhausted).toBe(false);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
