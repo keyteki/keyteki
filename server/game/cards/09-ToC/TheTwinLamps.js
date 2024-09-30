0322467a0e86b5644601a7e5ab5e224fe62501b5
Jeremy Stribling <strib@alum.mit.edu>
2024-09-30 14:09:40 -0700

    ToC: implement The Twin Lamps

---
+++ server/game/cards/09-ToC/TheTwinLamps.js
@@ -0,0 +1,22 @@
+const Card = require('../../Card.js');
+
+class TheTwinLamps extends Card {
+    // Play: Steal 1A. If you have more A in your pool than your
+    // opponent, make a token creature.
+    setupCardAbilities(ability) {
+        this.play({
+            gameAction: ability.actions.steal(),
+            then: {
+                alwaysTriggers: true,
+                condition: (context) =>
+                    context.player.opponent && context.player.amber > context.player.opponent.amber,
+                gameAction: ability.actions.makeTokenCreature(),
+                message: '{0} uses {1} to make a token creature'
+            }
+        });
+    }
+}
+
+TheTwinLamps.id = 'the-twin-lamps';
+
+module.exports = TheTwinLamps;
