b798ce8d8cee5d93e4a0d1d236d0ad63b87b02e1
Jeremy Stribling <strib@users.noreply.github.com>
2024-09-30 00:06:55 -0700

    GR: fix non-anomaly images for Grim Reaper and Near-Future Lens (#4179)
    The Grim Reaper will be fixed on the next fetchdata run.
    
    Near-Future Lens must be fixed by updating every row in `DeckCards`
    where "ImageURL" is "near-future-lens-star-alliance" to
    "near-future-lens-staralliance" instead.
    
    Closes: #3942

--- server/services/DeckService.js
+++ server/services/DeckService.js
@@ -715,7 +715,7 @@ class DeckService {
 
         let anomalies = {
             'ecto-charge': { anomalySet: 600, house: 'geistoid' },
-            'near-future-lens': { anomalySet: 600, house: 'star-alliance' },
+            'near-future-lens': { anomalySet: 600, house: 'staralliance' },
             'orb-of-wonder': { anomalySet: 453, house: 'sanctum' },
             'the-grim-reaper': { anomalySet: 453, house: 'geistoid' },
             valoocanth: { anomalySet: 453, house: 'unfathomable' }
