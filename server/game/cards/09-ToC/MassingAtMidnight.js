29e2edb5baad91e7883b8f68f2429d54b6964f1e
Jeremy Stribling <strib@alum.mit.edu>
2024-09-30 18:13:55 -0700

    ToC: implement Massing at Midnight

---
+++ server/game/cards/09-ToC/MassingAtMidnight.js
@@ -0,0 +1,22 @@
+const Card = require('../../Card.js');
+
+class MassingAtMidnight extends Card {
+    // Play: Make 3 token creatures. Purge Massing at Midnight.
+    setupCardAbilities(ability) {
+        this.play({
+            gameAction: ability.actions.makeTokenCreature({
+                amount: 3
+            }),
+            then: {
+                alwaysTriggers: true,
+                gameAction: ability.actions.purge((context) => ({
+                    target: context.source
+                }))
+            }
+        });
+    }
+}
+
+MassingAtMidnight.id = 'massing-at-midnight';
+
+module.exports = MassingAtMidnight;
