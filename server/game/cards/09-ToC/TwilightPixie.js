0e2dabc1a8ab3b810e907edf4e7be8a7a81dc6da
Jeremy Stribling <strib@alum.mit.edu>
2024-09-30 19:46:45 -0700

    ToC: implement Twilight Pixie

---
+++ server/game/cards/09-ToC/TwilightPixie.js
@@ -0,0 +1,19 @@
+const Card = require('../../Card.js');
+
+class TwilightPixie extends Card {
+    // Each of Twilight Pixie’s non-Faerie neighbors gains, “After
+    // Reap: Gain 1A.”
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: (card, context) =>
+                context.source.neighbors.includes(card) && !card.hasTrait('faerie'),
+            effect: ability.effects.gainAbility('reap', {
+                gameAction: ability.actions.gainAmber()
+            })
+        });
+    }
+}
+
+TwilightPixie.id = 'twilight-pixie';
+
+module.exports = TwilightPixie;
