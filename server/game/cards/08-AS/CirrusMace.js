2c6a79f5aecaee431d23a374fd2c494986bb8820
Jeremy Stribling <strib@users.noreply.github.com>
2024-10-03 08:44:34 -0700

    AS: fix Cirrus Mace reading `neighbors` on undefined target (#4183)

--- server/game/cards/08-AS/CirrusMace.js
+++ server/game/cards/08-AS/CirrusMace.js
@@ -6,20 +6,19 @@ class CirrusMace extends Card {
     setupCardAbilities(ability) {
         this.action({
             target: {
-                cardType: 'creature'
+                cardType: 'creature',
+                gameAction: ability.actions.addPowerCounter((context) => ({
+                    amount: 2,
+                    target: context.target
+                }))
             },
-            message:
-                "{0} uses {1} to add two +1 power counter to {2} and deal 2 damage to {2}'s neighbors",
-            messageArgs: (context) => [context.player, context.source, context.target],
-            gameAction: ability.actions.addPowerCounter((context) => ({
-                amount: 2,
-                target: context.target
-            })),
             then: (preThenContext) => ({
                 gameAction: ability.actions.dealDamage({
                     amount: 2,
-                    target: preThenContext.target.neighbors
-                })
+                    target: preThenContext.target ? preThenContext.target.neighbors : []
+                }),
+                message: "{0} uses {1} to deal 2 damage to {3}'s neighbors",
+                messageArgs: [preThenContext.target]
             })
         });
     }
