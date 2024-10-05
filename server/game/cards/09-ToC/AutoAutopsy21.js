5a342256fdc30e68ffd01e5bad2e2a6cd9df304f
Jeremy Stribling <strib@users.noreply.github.com>
2024-10-05 00:39:40 -0700

    ToC: implement Auto-Autopsy 2.1 (#4222)

---
+++ server/game/cards/09-ToC/AutoAutopsy21.js
@@ -0,0 +1,27 @@
+const Card = require('../../Card.js');
+
+class AutoAutopsy21 extends Card {
+    // Play: Make a token creature.
+    // Omni: Destroy a friendly creature. If you do, gain 1A.
+    setupCardAbilities(ability) {
+        this.play({
+            gameAction: ability.actions.makeTokenCreature()
+        });
+
+        this.omni({
+            target: {
+                cardType: 'creature',
+                controller: 'self',
+                gameAction: ability.actions.destroy()
+            },
+            then: {
+                gameAction: ability.actions.gainAmber(),
+                message: '{0} uses {1} to gain 1 amber'
+            }
+        });
+    }
+}
+
+AutoAutopsy21.id = 'auto-autopsy-21';
+
+module.exports = AutoAutopsy21;
---
+++ test/server/cards/09-ToC/AutoAutopsy21.spec.js
@@ -0,0 +1,48 @@
+describe('Auto-Autopsy 2.1', function () {
+    describe("Auto Autopsy 2.1's abilities", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 1,
+                    house: 'logos',
+                    token: 'alpha-gamma',
+                    hand: ['auto-autopsy-21'],
+                    inPlay: ['flaxia', 'helper-bot'],
+                    deck: new Array(12).fill('toad')
+                },
+                player2: {
+                    amber: 1,
+                    inPlay: ['troll']
+                }
+            });
+
+            this.alphaGamma1 = this.player1.player.deck[0];
+            this.player1.chains = 36;
+        });
+
+        it('should make a token creature on play', function () {
+            this.player1.play(this.autoAutopsy21);
+            this.player1.clickPrompt('Left');
+            expect(this.alphaGamma1.location).toBe('play area');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should destroy a friendly creature and gain amber on omni', function () {
+            this.player1.play(this.autoAutopsy21);
+            this.player1.clickPrompt('Left');
+            this.player1.endTurn();
+            this.player2.clickPrompt('brobnar');
+            this.player2.endTurn();
+            this.player1.clickPrompt('untamed');
+            this.player1.useAction(this.autoAutopsy21, true);
+            expect(this.player1).toBeAbleToSelect(this.flaxia);
+            expect(this.player1).toBeAbleToSelect(this.helperBot);
+            expect(this.player1).toBeAbleToSelect(this.alphaGamma1);
+            expect(this.player1).not.toBeAbleToSelect(this.troll);
+            this.player1.clickCard(this.helperBot);
+            expect(this.player1.amber).toBe(3);
+            expect(this.helperBot.location).toBe('discard');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
