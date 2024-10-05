f4d2bb569a40f64d0450a5d846ed507eea639c49
Jeremy Stribling <strib@users.noreply.github.com>
2024-10-05 00:40:01 -0700

    ToC: implement Rich Score (#4242)

---
+++ test/server/cards/09-ToC/RichScore.spec.js
@@ -0,0 +1,31 @@
+describe('Rich Score', function () {
+    describe("Rich Score's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 1,
+                    house: 'shadows',
+                    token: 'stooge',
+                    hand: ['rich-score'],
+                    inPlay: ['stooge:toad', 'stooge:toad']
+                },
+                player2: {
+                    amber: 4,
+                    token: 'prospector',
+                    inPlay: ['prospector:dust-pixie']
+                }
+            });
+
+            this.stooge3 = this.player1.player.deck[0];
+        });
+
+        it('shoud make a token, and steal amber for half of friendly token', function () {
+            this.player1.play(this.richScore);
+            this.player1.clickPrompt('Right');
+            expect(this.stooge3.location).toBe('play area');
+            expect(this.player1.amber).toBe(3);
+            expect(this.player2.amber).toBe(2);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
