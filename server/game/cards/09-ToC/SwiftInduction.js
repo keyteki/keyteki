93baaaee921577a5befe89297e0361600999185c
Jeremy Stribling <strib@users.noreply.github.com>
2024-10-05 00:39:56 -0700

    ToC: implement Swift Induction (#4237)

---
+++ server/game/cards/09-ToC/SwiftInduction.js
@@ -0,0 +1,25 @@
+const Card = require('../../Card.js');
+
+class SwiftInduction extends Card {
+    // Play: Make a token creature. If that token creature enters play
+    // adjacent to a Mutant creature, archive Swift Induction.
+    setupCardAbilities(ability) {
+        this.play({
+            gameAction: ability.actions.makeTokenCreature(),
+            then: {
+                condition: (context) =>
+                    context.preThenEvent.card.neighbors.some((c) => c.hasTrait('mutant')),
+                gameAction: ability.actions.archive((context) => ({
+                    effect: 'archive {1}',
+                    target: context.source
+                })),
+                message: '{0} uses {1} to archive {3}',
+                messageArgs: (context) => [context.source]
+            }
+        });
+    }
+}
+
+SwiftInduction.id = 'swift-induction';
+
+module.exports = SwiftInduction;
