b4f822fc4b5871b52d139bc3b0a543da589ab985
Jeremy Stribling <strib@users.noreply.github.com>
2024-10-05 00:39:31 -0700

    ToC: implement Sidekick (#4213)

---
+++ server/game/cards/09-ToC/Sidekick.js
@@ -0,0 +1,19 @@
+const Card = require('../../Card.js');
+
+class Sidekick extends Card {
+    // Play: Choose a card in your hand and put it into play as a token creature.
+    setupCardAbilities(ability) {
+        this.play({
+            target: {
+                controller: 'self',
+                location: 'hand',
+                gameAction: ability.actions.makeTokenCreature()
+            },
+            effect: 'put a card from their hand into play as a token creature'
+        });
+    }
+}
+
+Sidekick.id = 'sidekick';
+
+module.exports = Sidekick;
