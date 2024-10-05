f83895c872db9c867b10ff80de40d495b18eb79d
Jeremy Stribling <strib@users.noreply.github.com>
2024-10-05 00:40:23 -0700

    ToC: implement Harlock (#4264)

---
+++ test/server/cards/09-ToC/Harlock.spec.js
@@ -0,0 +1,40 @@
+describe('Harlock', function () {
+    describe("Harlock's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 1,
+                    house: 'untamed',
+                    token: 'niffle-brute',
+                    inPlay: ['harlock']
+                },
+                player2: {
+                    amber: 2,
+                    inPlay: ['umbra', 'old-bruno']
+                }
+            });
+
+            this.niffleBrute1 = this.player1.player.deck[0];
+        });
+
+        it('should make a token creature if fought creature was destroyed', function () {
+            this.player1.fightWith(this.harlock, this.umbra);
+            this.player1.clickPrompt('Right');
+            expect(this.niffleBrute1.location).toBe('play area');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should not make a token creature if fought creature was not destroyed', function () {
+            this.player1.fightWith(this.harlock, this.oldBruno);
+            expect(this.niffleBrute1.location).toBe('deck');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should not make a token creature if fought creature was warded', function () {
+            this.umbra.ward();
+            this.player1.fightWith(this.harlock, this.umbra);
+            expect(this.niffleBrute1.location).toBe('deck');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
