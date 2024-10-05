43409367232055841ef2127b01be30cdf402533a
Jeremy Stribling <strib@users.noreply.github.com>
2024-10-05 00:39:57 -0700

    ToC: implement Paragon (#4238)

---
+++ server/game/cards/09-ToC/Paragon.js
@@ -0,0 +1,16 @@
+const Card = require('../../Card.js');
+
+class Paragon extends Card {
+    // If there is a Mutant creature in play, Paragon enters play enraged.
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: (context) => context.game.creaturesInPlay.some((c) => c.hasTrait('mutant')),
+            effect: [ability.effects.entersPlayEnraged()],
+            location: 'any'
+        });
+    }
+}
+
+Paragon.id = 'paragon';
+
+module.exports = Paragon;
---
+++ test/server/cards/09-ToC/Paragon.spec.js
@@ -0,0 +1,45 @@
+describe('Paragon', function () {
+    describe("Paragon's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'logos',
+                    token: 'paragon',
+                    hand: ['dysania'],
+                    inPlay: ['ornate-talking-tray']
+                },
+                player2: {
+                    amber: 1,
+                    inPlay: ['dust-pixie'],
+                    discard: ['snarette']
+                }
+            });
+
+            this.paragon1 = this.player1.player.deck[0];
+        });
+
+        it('should not enter play enraged if no mutants', function () {
+            this.player1.useAction(this.ornateTalkingTray, true);
+            expect(this.paragon1.location).toBe('play area');
+            expect(this.paragon1.enraged).toBe(false);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should enter play enraged if a mutant in play', function () {
+            this.player1.playCreature(this.dysania);
+            this.player1.useAction(this.ornateTalkingTray, true);
+            this.player1.clickPrompt('Right');
+            expect(this.paragon1.location).toBe('play area');
+            expect(this.paragon1.enraged).toBe(true);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should enter play enraged if enemy mutant in play', function () {
+            this.player2.moveCard(this.snarette, 'play area');
+            this.player1.useAction(this.ornateTalkingTray, true);
+            expect(this.paragon1.location).toBe('play area');
+            expect(this.paragon1.enraged).toBe(true);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
