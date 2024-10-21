fb6f7477cf56a8f09596f1b60cc75de57581c9ad
Jeremy Stribling <strib@users.noreply.github.com>
2024-10-21 00:33:19 -0700

    MoMu: implement Boosted B4-RRY (#4284)

---
+++ server/game/cards/10-MoMu/BoostedB4Rry2.js
@@ -0,0 +1,11 @@
+const BoostedB4Rry = require('./BoostedB4Rry.js');
+
+class BoostedB4Rry2 extends BoostedB4Rry {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+    }
+}
+
+BoostedB4Rry2.id = 'boosted-b4-rry2';
+
+module.exports = BoostedB4Rry2;
