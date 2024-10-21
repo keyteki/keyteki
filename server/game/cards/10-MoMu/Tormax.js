22ee85f557bcaeca7afe70cf669721bbc0056b32
Jeremy Stribling <strib@users.noreply.github.com>
2024-10-21 00:34:43 -0700

    MoMu: implement Tormax (#4276)

---
+++ server/game/cards/10-MoMu/Tormax.js
@@ -0,0 +1,33 @@
+const GiganticCard = require('../../GiganticCard.js');
+
+class Tormax extends GiganticCard {
+    // (Play only with the other half of Tormax.)
+    // Play/After Fight/After Reap: Discard your hand. Purge 2 random cards
+    // from your opponent’s hand.
+    constructor(owner, cardData) {
+        super(owner, cardData);
+    }
+
+    setupCardAbilities(ability) {
+        super.setupCardAbilities(ability);
+
+        this.play({
+            fight: true,
+            reap: true,
+            effect: 'discard {1}',
+            effectArgs: (context) => [context.player.hand],
+            gameAction: ability.actions.sequential([
+                ability.actions.discard((context) => ({
+                    target: context.player.hand
+                })),
+                ability.actions.purgeAtRandom({
+                    amount: 2
+                })
+            ])
+        });
+    }
+}
+
+Tormax.id = 'tormax';
+
+module.exports = Tormax;
---
+++ test/server/cards/10-MoMu/Tormax.spec.js
@@ -0,0 +1,77 @@
+describe('Tormax', function () {
+    describe("Tormax's Ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 2,
+                    house: 'dis',
+                    hand: ['tormax', 'tormax2', 'gub']
+                },
+                player2: {
+                    amber: 5,
+                    hand: [
+                        'troll',
+                        'narp',
+                        'zorg',
+                        'vezyma-thinkdrone',
+                        'collector-worm',
+                        'ulyq-megamouth'
+                    ],
+                    inPlay: ['lamindra']
+                }
+            });
+        });
+
+        it('should not be able to play with just part 1', function () {
+            this.player1.moveCard(this.tormax2, 'discard');
+            this.player1.clickCard(this.tormax);
+            expect(this.player1).not.toHavePromptButton('Play this creature');
+        });
+
+        it('should not be able to play with just part 2', function () {
+            this.player1.moveCard(this.tormax, 'discard');
+            this.player1.clickCard(this.tormax2);
+            expect(this.player1).not.toHavePromptButton('Play this creature');
+        });
+
+        it('should be able to play with part 1', function () {
+            this.player1.clickCard(this.tormax);
+            expect(this.player1).toHavePromptButton('Play this creature');
+        });
+
+        it('should be able to play with part 2', function () {
+            this.player1.clickCard(this.tormax2);
+            expect(this.player1).toHavePromptButton('Play this creature');
+        });
+
+        it('should discard hand and purge 2 opponent cards on play', function () {
+            this.player1.playCreature(this.tormax);
+            expect(this.player1.player.hand.length).toBe(0);
+            expect(this.player2.player.hand.length).toBe(4);
+            expect(this.player2.player.purged.length).toBe(2);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should discard hand and purge 2 opponent cards on reap', function () {
+            this.player1.playCreature(this.tormax);
+            this.tormax.exhausted = false;
+            this.player1.moveCard(this.gub, 'hand');
+            this.player1.reap(this.tormax);
+            expect(this.player1.player.hand.length).toBe(0);
+            expect(this.player2.player.hand.length).toBe(2);
+            expect(this.player2.player.purged.length).toBe(4);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should discard hand and purge 2 opponent cards on fight', function () {
+            this.player1.playCreature(this.tormax);
+            this.tormax.exhausted = false;
+            this.player1.moveCard(this.gub, 'hand');
+            this.player1.fightWith(this.tormax, this.lamindra);
+            expect(this.player1.player.hand.length).toBe(0);
+            expect(this.player2.player.hand.length).toBe(2);
+            expect(this.player2.player.purged.length).toBe(4);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
