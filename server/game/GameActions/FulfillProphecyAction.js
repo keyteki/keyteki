084d68c3b9844246f747d716bae8b60db1e6a83a
Jeremy Stribling <strib@alum.mit.edu>
2025-01-19 20:32:36 -0800

    PV: first attempt at prophecies and fates
    Each deck is going to have 4 prophecy cards.  IRL this will be two
    double-sided cards.  For now, in the deck data, we can represent this
    as an array of 4 cards, and we ensure only one of each pair is active
    at any time.
    
    During Step 3, a player may "activate" one of their unactivated
    prophecies by placing one card from their hand underneath it.  Only
    one prophecy may be activated per turn.
    
    The prophecy describes a condition that can be "fulfilled" on your
    opponent's turn.  If it is, and the prophecy is active, your opponent
    resolves the "Fate" ability of the card under the prophecy, if there
    is one.  That card then goes to the discard pile and the fate
    deactivates.
    
    This is just a first draft, as official rules aren't even available
    yet.

---
+++ server/game/GameActions/FulfillProphecyAction.js
@@ -0,0 +1,50 @@
+const PlayerAction = require('./PlayerAction');
+
+class FulfillProphecyAction extends PlayerAction {
+    setDefaultProperties() {
+        this.card = null;
+    }
+
+    setup() {
+        super.setup();
+        this.name = 'fulfillProphecy';
+        this.effectMsg = 'fulfull {1}';
+        if (this.card && this.card.childCards) {
+            this.effectArgs = this.card.childCards;
+        }
+    }
+
+    defaultTargets(context) {
+        return context.game.activePlayer;
+    }
+
+    canAffect(player, context) {
+        if (!player) {
+            return false;
+        }
+
+        return (
+            super.canAffect(player, context) &&
+            player !== this.card.controller &&
+            this.card.childCards &&
+            this.card.childCards.length > 0 &&
+            this.card.activeProphecy
+        );
+    }
+
+    getEvent(player, context) {
+        return super.createEvent(
+            'onFulfillProphecy',
+            { player: player, card: this.card, context: context },
+            () => {
+                this.card.controller.deactivateProphecy(this.card);
+                if (this.card.childCards && this.card.childCards.length > 0) {
+                    let childCard = this.card.childCards[0];
+                    context.game.actions.resolveFate().resolve(childCard, context);
+                }
+            }
+        );
+    }
+}
+
+module.exports = FulfillProphecyAction;
