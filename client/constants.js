24f924fa837a743ab49181686fc99bc197448977
Jeremy Stribling <strib@alum.mit.edu>
2025-01-31 07:47:02 -0800

    Add support for Discovery
    There are no new cards in this set.

--- client/constants.js
+++ client/constants.js
@@ -50,7 +50,8 @@ export const Constants = {
         { value: '737', label: 'VM2024', tideRequired: false },
         { value: '800', label: 'AS', tideRequired: false },
         { value: '855', label: 'ToC', tideRequired: false },
-        { value: '874', label: 'MoMu', tideRequired: false }
+        { value: '874', label: 'MoMu', tideRequired: false },
+        { value: '907', label: 'DISC', tideRequired: false }
     ],
     CardTypes: ['action', 'artifact', 'creature', 'upgrade', 'token creature', 'the tide'],
     SetIconPaths: {},
