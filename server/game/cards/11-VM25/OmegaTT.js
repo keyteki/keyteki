7090a254cf2b5e3b707420d313dcbab3394c6e98
Jeremy Stribling <strib@alum.mit.edu>
2025-04-27 14:37:45 -0700

    VM25: implement Omega TT

---
+++ server/game/cards/11-VM25/OmegaTT.js
@@ -0,0 +1,41 @@
+const Card = require('../../Card.js');
+
+class OmegaTT extends Card {
+    // Play: Purge 2 cards from a discard pile.
+    // Action: Shuffle Omega TT into its owner's deck.
+    setupCardAbilities(ability) {
+        this.play({
+            effectStyle: 'all',
+            targets: {
+                select: {
+                    mode: 'select',
+                    activePromptTitle: 'Choose which discard pile to purge from',
+                    choices: {
+                        Mine: (context) => context.player.discard.length > 0,
+                        "Opponent's": (context) =>
+                            context.player.opponent && context.player.opponent.discard.length > 0
+                    }
+                },
+                cards: {
+                    dependsOn: 'select',
+                    mode: 'exactly',
+                    numCards: 2,
+                    cardCondition: (card, context) =>
+                        context.selects.select.choice === 'Mine'
+                            ? card.owner === context.player
+                            : card.owner === context.player.opponent,
+                    location: 'discard',
+                    gameAction: ability.actions.purge()
+                }
+            }
+        });
+
+        this.action({
+            gameAction: ability.actions.returnToDeck({ shuffle: true })
+        });
+    }
+}
+
+OmegaTT.id = 'omega-tt';
+
+module.exports = OmegaTT;
