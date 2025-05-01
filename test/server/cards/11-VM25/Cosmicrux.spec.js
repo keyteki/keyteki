b8a2d6bdab7d0b2117cb605b25e0a3e810ecf5f6
Jeremy Stribling <strib@alum.mit.edu>
2025-05-01 08:24:34 -0700

    VM25: implement Cosmicrux

---
+++ test/server/cards/11-VM25/Cosmicrux.spec.js
@@ -0,0 +1,43 @@
+describe('Cosmicrux', function () {
+    describe("Cosmicrux's abilities", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'brobnar',
+                    inPlay: ['cosmicrux', 'troll']
+                },
+                player2: {
+                    inPlay: ['charette']
+                }
+            });
+        });
+
+        it('should deal 1 damage to a creature when it readies', function () {
+            this.troll.exhausted = true;
+            this.player1.endTurn();
+            expect(this.troll.tokens.damage).toBe(1);
+            expect(this.cosmicrux.tokens.damage).toBe(undefined);
+            expect(this.charette.tokens.damage).toBe(undefined);
+        });
+
+        it('should deal 1 damage to opponent creature when it readies', function () {
+            this.charette.exhausted = true;
+            this.player1.endTurn();
+            this.player2.clickPrompt('Dis');
+            this.player2.endTurn();
+            expect(this.troll.tokens.damage).toBe(undefined);
+            expect(this.cosmicrux.tokens.damage).toBe(undefined);
+            expect(this.charette.tokens.damage).toBe(1);
+        });
+
+        it('should deal 1 damage to multiple creatures when they ready', function () {
+            this.troll.exhausted = true;
+            this.cosmicrux.exhausted = true;
+            this.player1.endTurn();
+            this.player1.clickCard(this.cosmicrux);
+            expect(this.troll.tokens.damage).toBe(1);
+            expect(this.cosmicrux.tokens.damage).toBe(1);
+            expect(this.charette.tokens.damage).toBe(undefined);
+        });
+    });
+});
