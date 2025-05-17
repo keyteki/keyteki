c237184759859b390164f08a902a327dbb5e8c48
Jeremy Stribling <strib@alum.mit.edu>
2025-05-17 11:30:54 -0700

    PV: implement Trust Your Feelings

--- server/game/player.js
+++ server/game/player.js
@@ -1211,6 +1211,7 @@ class Player extends GameObject {
 
     deactivateProphecy(prophecyCard) {
         prophecyCard.activeProphecy = false;
+        this.game.raiseEvent('onProphecyDeactivated', { prophecyCard: prophecyCard });
     }
 
     flipProphecy(context, prophecyCard) {
@@ -1221,11 +1222,13 @@ class Player extends GameObject {
         if (!flipSide) {
             return false;
         }
-        prophecyCard.activeProphecy = false;
+        this.deactivateProphecy(prophecyCard);
         flipSide.activeProphecy = true;
         // Move any cards under the prophecy to the flipside.
-        prophecyCard.childCards.forEach((card) => {
-            context.game.actions.placeUnder({ parent: flipSide }).resolve(card, context);
+        flipSide.childCards = prophecyCard.childCards;
+        prophecyCard.childCards = [];
+        flipSide.childCards.forEach((card) => {
+            card.parent = flipSide;
         });
         this.game.raiseEvent('onProphecyFlipped', { prophecyCard: flipSide });
         return true;
