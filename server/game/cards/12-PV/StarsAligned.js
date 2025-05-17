1cef7170bd8d853750b8493943f2d13db91d6d94
Jeremy Stribling <strib@alum.mit.edu>
2025-05-17 10:10:42 -0700

    PV: implement Stars Aligned

---
+++ server/game/cards/12-PV/StarsAligned.js
@@ -0,0 +1,27 @@
+const Card = require('../../Card.js');
+
+class StarsAligned extends Card {
+    // At the start of your opponent's turn, if each player has the same number of creatures, fulfill Stars Aligned.
+    setupCardAbilities(ability) {
+        this.prophecyReaction({
+            when: {
+                onBeginRound: (event, context) =>
+                    context.game.activePlayer === context.source.controller.opponent &&
+                    context.game
+                        .getPlayers()
+                        .every(
+                            (player) =>
+                                player.creaturesInPlay.length ===
+                                context.source.controller.creaturesInPlay.length
+                        )
+            },
+            gameAction: ability.actions.fulfillProphecy((context) => ({
+                card: context.source
+            }))
+        });
+    }
+}
+
+StarsAligned.id = 'stars-aligned';
+
+module.exports = StarsAligned;
