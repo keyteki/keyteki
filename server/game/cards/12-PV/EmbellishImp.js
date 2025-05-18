fb8ba05b48a29ce9ed6aee8704a277a50984c495
Jeremy Stribling <strib@alum.mit.edu>
2025-05-17 17:26:50 -0700

    PV: implement Embellish Imp

---
+++ server/game/cards/12-PV/EmbellishImp.js
@@ -0,0 +1,31 @@
+const Card = require('../../Card.js');
+
+class EmbellishImp extends Card {
+    // Each time an enemy creature is destroyed, steal 1.
+    // Fate: Destroy the most powerful friendly creature.
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardDestroyed: (event, context) =>
+                    event.card.type === 'creature' &&
+                    event.card.controller === context.player.opponent
+            },
+            gameAction: ability.actions.steal()
+        });
+
+        this.fate({
+            target: {
+                mode: 'mostStat',
+                cardType: 'creature',
+                controller: 'opponent',
+                numCards: 1,
+                cardStat: (card) => card.power,
+                gameAction: ability.actions.destroy()
+            }
+        });
+    }
+}
+
+EmbellishImp.id = 'embellish-imp';
+
+module.exports = EmbellishImp;
