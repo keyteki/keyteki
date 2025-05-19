26ce7013ad4fdd8b1b4782d6f75f911a1736caed
Jeremy Stribling <strib@alum.mit.edu>
2025-05-18 17:29:12 -0700

    PV: implement Skorpeon

---
+++ server/game/cards/12-PV/Skorpeon.js
@@ -0,0 +1,26 @@
+const Card = require('../../Card.js');
+
+class Skorpeon extends Card {
+    // Enhance .
+    // After Reap: Deal 2 to an enemy creature for each of Skorpeon's Dis neighbors.
+    setupCardAbilities(ability) {
+        this.reap({
+            gameAction: ability.actions.allocateDamage((context) => ({
+                controller: 'opponent',
+                numSteps: context.source.neighbors.filter((card) => card.hasHouse('dis')).length,
+                damageStep: 2
+            })),
+            effect: 'deal 2 damage to an enemy creature {1} time{2}',
+            effectArgs: (context) => [
+                context.source.neighbors.filter((card) => card.hasHouse('dis')).length,
+                context.source.neighbors.filter((card) => card.hasHouse('dis')).length === 1
+                    ? ''
+                    : 's'
+            ]
+        });
+    }
+}
+
+Skorpeon.id = 'skorpeon';
+
+module.exports = Skorpeon;
