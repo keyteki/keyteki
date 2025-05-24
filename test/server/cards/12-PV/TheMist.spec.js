a03c8038de094633df9adc346d849d2d0abbf8c2
Jeremy Stribling <strib@alum.mit.edu>
2025-05-24 12:37:06 -0700

    PV: implement The Mist

---
+++ test/server/cards/12-PV/TheMist.spec.js
@@ -0,0 +1,35 @@
+describe('The Mist', function () {
+    describe("The Mist's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'redemption',
+                    inPlay: ['the-mist', 'ember-imp', 'yurk']
+                },
+                player2: {
+                    inPlay: ['flaxia', 'searine']
+                }
+            });
+        });
+
+        it('should destroy itself and give all creatures the Mutant trait', function () {
+            this.player1.useAction(this.theMist, true);
+            expect(this.theMist.location).toBe('discard');
+            expect(this.emberImp.hasTrait('mutant')).toBe(true);
+            expect(this.yurk.hasTrait('mutant')).toBe(true);
+            expect(this.flaxia.hasTrait('mutant')).toBe(true);
+            expect(this.searine.hasTrait('mutant')).toBe(true);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should last until end of turn', function () {
+            this.player1.useAction(this.theMist, true);
+            expect(this.emberImp.hasTrait('mutant')).toBe(true);
+            this.player1.endTurn();
+            this.player2.clickPrompt('untamed');
+            expect(this.emberImp.hasTrait('mutant')).toBe(false);
+            expect(this.flaxia.hasTrait('mutant')).toBe(false);
+            expect(this.player2).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
