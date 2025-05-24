61e8bce803fa1b43cf86527b36a259f89221ce72
Jeremy Stribling <strib@alum.mit.edu>
2025-05-24 14:45:54 -0700

    PV: implement Knockback

---
+++ server/game/cards/12-PV/Knockback.js
@@ -0,0 +1,32 @@
+const Card = require('../../Card.js');
+
+function discard(deck) {
+    const index = deck.findIndex((card) => card.type === 'creature');
+    if (index > -1) {
+        return { target: deck.slice(0, index + 1) };
+    }
+    return { target: deck };
+}
+
+class Knockback extends Card {
+    // Play: Put a creature on top of its owner's deck.
+    // Fate: Discard cards from the top of your deck until you discard a creature or run out of cards.
+    setupCardAbilities(ability) {
+        this.play({
+            target: {
+                cardType: 'creature',
+                gameAction: ability.actions.returnToDeck()
+            }
+        });
+
+        this.fate({
+            gameAction: ability.actions.discard((context) =>
+                discard(context.game.activePlayer.deck)
+            )
+        });
+    }
+}
+
+Knockback.id = 'knockback';
+
+module.exports = Knockback;
