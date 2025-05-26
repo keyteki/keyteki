db4211d7fc7993ca09535a295e98d85399ab4f9b
Jeremy Stribling <strib@alum.mit.edu>
2025-05-26 10:17:43 -0700

    PV: implement Covetous Idol

---
+++ server/game/cards/12-PV/CovetousIdol.js
@@ -0,0 +1,16 @@
+const Card = require('../../Card.js');
+
+class CovetousIdol extends Card {
+    // During your "draw cards" step, if your opponent has more A than you, refill your hand to 1 additional card.
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: (context) =>
+                context.player.opponent && context.player.opponent.amber > context.player.amber,
+            effect: ability.effects.modifyHandSize(1)
+        });
+    }
+}
+
+CovetousIdol.id = 'covetous-idol';
+
+module.exports = CovetousIdol;
