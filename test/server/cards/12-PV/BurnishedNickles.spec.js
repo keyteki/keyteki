b3762850aa157f65bdc2901b44b540a60e6b483d
Jeremy Stribling <strib@alum.mit.edu>
2025-05-26 20:21:53 -0700

    PV: implement Burnished Nickles

---
+++ test/server/cards/12-PV/BurnishedNickles.spec.js
@@ -0,0 +1,28 @@
+describe('Burnished Nickles', function () {
+    describe("Burnished Nickles's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'shadows',
+                    inPlay: ['burnished-nickles', 'ember-imp', 'urchin']
+                },
+                player2: {
+                    inPlay: ['krump', 'dust-pixie']
+                }
+            });
+        });
+
+        it('should return another creature to hand when destroyed', function () {
+            this.player1.fightWith(this.burnishedNickles, this.krump);
+            expect(this.player1).toBeAbleToSelect(this.emberImp);
+            expect(this.player1).toBeAbleToSelect(this.urchin);
+            expect(this.player1).toBeAbleToSelect(this.krump);
+            expect(this.player1).toBeAbleToSelect(this.dustPixie);
+            expect(this.player1).not.toBeAbleToSelect(this.burnishedNickles);
+            this.player1.clickCard(this.emberImp);
+            expect(this.emberImp.location).toBe('hand');
+            expect(this.burnishedNickles.location).toBe('discard');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
