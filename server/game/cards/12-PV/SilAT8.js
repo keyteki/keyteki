6dab4c82c9f5c2a60065a528979acbfb716cb6c3
Jeremy Stribling <strib@alum.mit.edu>
2025-05-31 15:39:06 -0700

    PV: implement Sil-A-T8

---
+++ server/game/cards/12-PV/SilAT8.js
@@ -0,0 +1,34 @@
+const Card = require('../../Card.js');
+
+class SilAT8 extends Card {
+    // Enhance .
+    // After Fight/After Reap: You may ready a non-Robot creature for each of Sil-A-T8's Star Alliance neighbors.
+    setupCardAbilities(ability) {
+        this.fight({
+            reap: true,
+            effect: 'ready {1} non-Robot creature{2}',
+            effectArgs: (context) => {
+                const count = context.source.neighbors.filter((card) =>
+                    card.hasHouse('staralliance')
+                ).length;
+                return [count, count === 1 ? '' : 's'];
+            },
+            gameAction: ability.actions.sequentialForEach((context) => ({
+                num: context.source.neighbors.filter((card) => card.hasHouse('staralliance'))
+                    .length,
+                action: ability.actions.ready({
+                    promptForSelect: {
+                        cardType: 'creature',
+                        cardCondition: (card) => !card.hasTrait('robot'),
+                        message: '{0} uses {1} to ready {2}',
+                        messageArgs: (card) => [context.player, context.source, card]
+                    }
+                })
+            }))
+        });
+    }
+}
+
+SilAT8.id = 'sil-a-t8';
+
+module.exports = SilAT8;
---
+++ test/server/cards/12-PV/SilAT8.spec.js
@@ -0,0 +1,64 @@
+describe('Sil-A-T8', function () {
+    describe("Sil-A-T8's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'staralliance',
+                    hand: ['sil-a-t8', 'away-team', 'cpo-zytar'],
+                    inPlay: ['ember-imp', 'krump']
+                },
+                player2: {
+                    inPlay: ['urchin']
+                }
+            });
+
+            this.emberImp.exhausted = true;
+            this.krump.exhausted = true;
+            this.urchin.exhausted = true;
+        });
+
+        it('should ready 0 creatures when it has no Star Alliance neighbors', function () {
+            this.player1.playCreature(this.silAT8);
+            this.silAT8.exhausted = false;
+            this.player1.reap(this.silAT8);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should ready 1 creature when it has 1 Star Alliance neighbor', function () {
+            this.player1.playCreature(this.silAT8);
+            this.player1.playCreature(this.awayTeam);
+            this.silAT8.exhausted = false;
+            this.player1.reap(this.silAT8);
+            expect(this.player1).toBeAbleToSelect(this.emberImp);
+            expect(this.player1).toBeAbleToSelect(this.krump);
+            expect(this.player1).not.toBeAbleToSelect(this.awayTeam);
+            expect(this.player1).toBeAbleToSelect(this.urchin);
+            this.player1.clickCard(this.emberImp);
+            expect(this.emberImp.exhausted).toBe(false);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should ready 2 creatures when it has 2 Star Alliance neighbors', function () {
+            this.player1.playCreature(this.cpoZytar);
+            this.player1.playCreature(this.silAT8);
+            this.player1.playCreature(this.awayTeam);
+            this.silAT8.exhausted = false;
+            this.player1.reap(this.silAT8);
+            this.player1.clickCard(this.emberImp);
+            this.player1.clickCard(this.krump);
+            expect(this.emberImp.exhausted).toBe(false);
+            expect(this.krump.exhausted).toBe(false);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should work after fighting', function () {
+            this.player1.playCreature(this.silAT8);
+            this.player1.playCreature(this.awayTeam);
+            this.silAT8.exhausted = false;
+            this.player1.fightWith(this.silAT8, this.urchin);
+            this.player1.clickCard(this.emberImp);
+            expect(this.emberImp.exhausted).toBe(false);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
