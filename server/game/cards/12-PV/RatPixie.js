5d10693c629953a6a6160cde80d1fc36609b23b5
Jeremy Stribling <strib@alum.mit.edu>
2025-05-31 20:17:50 -0700

    PV: implement Rat Pixie

---
+++ server/game/cards/12-PV/RatPixie.js
@@ -0,0 +1,16 @@
+const Card = require('../../Card.js');
+
+class RatPixie extends Card {
+    // Play/After Reap: If you have 4A or fewer, gain 1A.
+    setupCardAbilities(ability) {
+        this.play({
+            reap: true,
+            condition: (context) => context.player.amber <= 4,
+            gameAction: ability.actions.gainAmber()
+        });
+    }
+}
+
+RatPixie.id = 'rat-pixie';
+
+module.exports = RatPixie;
