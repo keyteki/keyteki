624db8b50e6f04af01718c24e30090406a807e6c
Jeremy Stribling <strib@alum.mit.edu>
2025-06-03 20:00:06 -0700

    PV: implement Vesuvius Dome

---
+++ server/game/cards/12-PV/VesuviusDome.js
@@ -0,0 +1,25 @@
+const Card = require('../../Card.js');
+
+class VesuviusDome extends Card {
+    // Action: Move each A from each creature to the common supply. Destroy each creature. Destroy Vesuvius Dome.
+    setupCardAbilities(ability) {
+        this.action({
+            effect:
+                'move all amber from creatures to the common supply, destroy all creatures, and destroy itself',
+            gameAction: ability.actions.sequential([
+                ability.actions.removeAmber((context) => ({
+                    all: true,
+                    target: context.game.creaturesInPlay
+                })),
+                ability.actions.destroy((context) => ({
+                    target: context.game.creaturesInPlay
+                })),
+                ability.actions.destroy()
+            ])
+        });
+    }
+}
+
+VesuviusDome.id = 'vesuvius-dome';
+
+module.exports = VesuviusDome;
