a05bd221c60cb182f6c8c821d4e7e32805e4ac8d
Jeremy Stribling <strib@alum.mit.edu>
2025-06-03 22:12:05 -0700

    PV: implement Kaju Zhan

---
+++ server/game/cards/12-PV/KajuZhan.js
@@ -0,0 +1,23 @@
+const Card = require('../../Card.js');
+
+class KajuZhan extends Card {
+    // While Kaju Zhan is attacking, ignore armor and taunt
+    // After Fight: Put an enemy creature on top of its owner's deck
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            effect: [ability.effects.ignores('armor'), ability.effects.ignores('taunt')]
+        });
+
+        this.fight({
+            target: {
+                cardType: 'creature',
+                controller: 'opponent',
+                gameAction: ability.actions.returnToDeck({ top: true })
+            }
+        });
+    }
+}
+
+KajuZhan.id = 'kaju-zhan';
+
+module.exports = KajuZhan;
