c422d4e204c9bbdd4f13e2471c54be221801df43
Jeremy Stribling <strib@alum.mit.edu>
2025-06-06 07:26:04 -0700

    PV: implement Galactic Tariff (#4447)

---
+++ server/game/cards/12-PV/GalacticTariff.js
@@ -0,0 +1,19 @@
+const Card = require('../../Card.js');
+
+class GalacticTariff extends Card {
+    // This creature gains, "After Fight/After Reap: Capture 1A from your own side."
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.gainAbility('fight', {
+                reap: true,
+                gameAction: ability.actions.capture((context) => ({
+                    player: context.player
+                }))
+            })
+        });
+    }
+}
+
+GalacticTariff.id = 'galactic-tariff';
+
+module.exports = GalacticTariff;
