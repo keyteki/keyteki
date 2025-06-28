f030f9f0b2cfa0a2966e245a501b9c59a4793881
Jeremy Stribling <strib@alum.mit.edu>
2025-06-27 20:34:25 -0700

    Fix Persistence Hunting to affect player's opponent
    Instead of the controller of Persistence Hunting's opponent.

--- test/server/cards/12-PV/DisabledSecurity.spec.js
+++ test/server/cards/12-PV/DisabledSecurity.spec.js
@@ -9,7 +9,14 @@ describe('Disabled Security', function () {
                 },
                 player2: {
                     amber: 4,
-                    discard: ['urchin', 'hunting-witch', 'krump', 'dust-pixie']
+                    inPlay: ['witch-of-the-eye'],
+                    discard: [
+                        'urchin',
+                        'hunting-witch',
+                        'dust-pixie',
+                        'krump',
+                        'persistence-hunting'
+                    ]
                 }
             });
 
@@ -34,10 +41,30 @@ describe('Disabled Security', function () {
             expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
         });
 
+        it("should play action card from opponent's discarded cards", function () {
+            this.player2.moveCard(this.persistenceHunting, 'deck');
+            this.player1.play(this.disabledSecurity);
+            this.player1.clickCard(this.persistenceHunting);
+            this.player1.clickPrompt('untamed');
+            expect(this.witchOfTheEye.exhausted).toBe(true);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
         it('should not play a card if opponent has no cards in deck', function () {
             this.player2.player.deck = [];
             this.player1.play(this.disabledSecurity);
-            expect(this.player2.player.discard.length).toBe(1);
+            expect(this.player2.player.discard.length).toBe(2);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should play a card if opponent has less than 3 cards in deck', function () {
+            this.player2.player.deck = [this.krump, this.huntingWitch];
+            this.player1.play(this.disabledSecurity);
+            this.player1.clickCard(this.krump);
+            this.player1.clickPrompt('Right');
+            expect(this.krump.location).toBe('play area');
+            expect(this.krump.controller).toBe(this.player1.player);
+            expect(this.player2.player.discard.length).toBe(3);
             expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
         });
     });
