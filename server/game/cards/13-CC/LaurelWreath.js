a63b2260312adf9b007a31ac32675432c4d854f9
Jeremy Stribling <strib@alum.mit.edu>
2025-06-29 15:28:30 -0700

    CC: implement Laurel Wreath

---
+++ server/game/cards/13-CC/LaurelWreath.js
@@ -0,0 +1,19 @@
+const Card = require('../../Card.js');
+
+class LaurelWreath extends Card {
+    // This creature gets +1 power for each amber on it.
+    // Play: Exalt this creature.
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.modifyPower((target) => target.amber || 0)
+        });
+
+        this.play({
+            gameAction: ability.actions.exalt((context) => ({ target: context.source.parent }))
+        });
+    }
+}
+
+LaurelWreath.id = 'laurel-wreath';
+
+module.exports = LaurelWreath;
