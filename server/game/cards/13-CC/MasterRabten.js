8ed4d8c85d94d2f7d55e4eb9217eda85a7d5b551
Jeremy Stribling <strib@alum.mit.edu>
2025-06-29 12:57:47 -0700

    CC: implement Master Rabten

---
+++ server/game/cards/13-CC/MasterRabten.js
@@ -0,0 +1,20 @@
+const Card = require('../../Card.js');
+
+class MasterRabten extends Card {
+    // Play/After Reap: Each friendly Monk creature captures 1A.
+    setupCardAbilities(ability) {
+        this.play({
+            reap: true,
+            gameAction: ability.actions.capture((context) => ({
+                target: context.game.cardsInPlay.filter(
+                    (card) => card.controller === context.player && card.hasTrait('monk')
+                ),
+                amount: 1
+            }))
+        });
+    }
+}
+
+MasterRabten.id = 'master-rabten';
+
+module.exports = MasterRabten;
