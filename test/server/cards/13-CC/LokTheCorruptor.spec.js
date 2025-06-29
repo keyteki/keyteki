6ee4d00c23b54b9cd54abf8bdc8cdf1ae52553c4
Jeremy Stribling <strib@alum.mit.edu>
2025-06-28 21:18:35 -0700

    CC: implement Lok the Corruptor

---
+++ test/server/cards/13-CC/LokTheCorruptor.spec.js
@@ -0,0 +1,24 @@
+describe('Lok the Corruptor', function () {
+    describe("Lok the Corruptor's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'dis',
+                    amber: 1,
+                    inPlay: ['lok-the-corruptor', 'ember-imp', 'krump', 'flaxia']
+                },
+                player2: {
+                    amber: 4,
+                    inPlay: ['troll', 'hunting-witch', 'searine']
+                }
+            });
+        });
+
+        it('should make opponent lose 1 amber for each friendly Dis creature when reaping', function () {
+            this.player1.reap(this.lokTheCorruptor);
+            expect(this.player1.amber).toBe(2);
+            expect(this.player2.amber).toBe(2);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
