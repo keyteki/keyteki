007c9cb8d7e5c3134edf347d7de343adf2725993
Jeremy Stribling <strib@alum.mit.edu>
2025-06-29 11:18:47 -0700

    CC: implement Experiment Q

---
+++ server/game/cards/13-CC/ExperimentQ.js
@@ -0,0 +1,32 @@
+const Card = require('../../Card.js');
+
+class ExperimentQ extends Card {
+    // Enhance .
+    // Experiment Q gets +1 power for each Mars creature in play.
+    // Play: For each Mars creature in play, stun a non-Mars creature.
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            effect: ability.effects.modifyPower(
+                (card, context) =>
+                    context.game.creaturesInPlay.filter((c) => c.hasHouse('mars')).length
+            )
+        });
+
+        this.play({
+            gameAction: ability.actions.sequentialForEach((context) => ({
+                num: context.game.creaturesInPlay.filter((card) => card.hasHouse('mars')).length,
+                action: ability.actions.stun({
+                    promptForSelect: {
+                        activePromptTitle: 'Choose a non-Mars creature to stun',
+                        cardType: 'creature',
+                        cardCondition: (card) => !card.hasHouse('mars')
+                    }
+                })
+            }))
+        });
+    }
+}
+
+ExperimentQ.id = 'experiment-q';
+
+module.exports = ExperimentQ;
