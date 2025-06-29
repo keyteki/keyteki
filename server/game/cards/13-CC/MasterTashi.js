4347e8d3920106a58e2db303108fdc16535e28cb
Jeremy Stribling <strib@alum.mit.edu>
2025-06-29 13:56:11 -0700

    CC: implement Master Tashi

---
+++ server/game/cards/13-CC/MasterTashi.js
@@ -0,0 +1,37 @@
+const Card = require('../../Card.js');
+
+class MasterTashi extends Card {
+    // After Fight: Ready and reap with a neighboring creature.
+    // After Reap: Ready and fight with a neighboring creature.
+    setupCardAbilities(ability) {
+        this.fight({
+            target: {
+                cardType: 'creature',
+                controller: 'self',
+                cardCondition: (card, context) => context.source.neighbors.includes(card),
+                gameAction: ability.actions.sequential([
+                    ability.actions.ready(),
+                    ability.actions.reap()
+                ])
+            },
+            effect: 'ready and reap with a neighboring creature'
+        });
+
+        this.reap({
+            target: {
+                cardType: 'creature',
+                controller: 'self',
+                cardCondition: (card, context) => context.source.neighbors.includes(card),
+                gameAction: ability.actions.sequential([
+                    ability.actions.ready(),
+                    ability.actions.fight()
+                ])
+            },
+            effect: 'ready and fight with a neighboring creature'
+        });
+    }
+}
+
+MasterTashi.id = 'master-tashi';
+
+module.exports = MasterTashi;
---
+++ test/server/cards/13-CC/MasterTashi.spec.js
@@ -0,0 +1,39 @@
+describe('Master Tashi', function () {
+    describe("Master Tashi's abilities", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'sanctum',
+                    inPlay: ['blypyp', 'master-tashi', 'troll', 'krump'],
+                    hand: ['revered-monk']
+                },
+                player2: {
+                    inPlay: ['dust-pixie']
+                }
+            });
+        });
+
+        it('should ready and reap with a neighboring creature after fighting', function () {
+            this.player1.fightWith(this.masterTashi, this.dustPixie);
+            expect(this.player1).toBeAbleToSelect(this.troll);
+            expect(this.player1).toBeAbleToSelect(this.blypyp);
+            expect(this.player1).not.toBeAbleToSelect(this.krump);
+            expect(this.player1).not.toBeAbleToSelect(this.dustPixie);
+            this.player1.clickCard(this.troll);
+            expect(this.player1.amber).toBe(1);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should ready and fight with a neighboring creature after reaping', function () {
+            this.player1.reap(this.masterTashi);
+            expect(this.player1).toBeAbleToSelect(this.troll);
+            expect(this.player1).toBeAbleToSelect(this.blypyp);
+            expect(this.player1).not.toBeAbleToSelect(this.krump);
+            expect(this.player1).not.toBeAbleToSelect(this.dustPixie);
+            this.player1.clickCard(this.troll);
+            this.player1.clickCard(this.dustPixie);
+            expect(this.dustPixie.location).toBe('discard');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
