34f56d5846ff89222746f76da06686436b0f417b
Jeremy Stribling <strib@alum.mit.edu>
2025-06-29 14:06:59 -0700

    CC: implement Congregate

---
+++ server/game/cards/13-CC/Congregate.js
@@ -0,0 +1,33 @@
+const Card = require('../../Card.js');
+
+class Congregate extends Card {
+    // Play: Choose a trait. if there are 2 or more friendly creatures with that trait, gain 2A.
+    setupCardAbilities(ability) {
+        this.play({
+            target: {
+                mode: 'trait'
+            },
+            effect: 'choose the {1} trait and gain {2} amber',
+            effectArgs: (context) => [
+                context.trait,
+                context.game.creaturesInPlay.filter(
+                    (card) => card.controller === context.player && card.hasTrait(context.trait)
+                ).length >= 2
+                    ? 2
+                    : 0
+            ],
+            gameAction: ability.actions.gainAmber((context) => ({
+                amount:
+                    context.game.creaturesInPlay.filter(
+                        (card) => card.controller === context.player && card.hasTrait(context.trait)
+                    ).length >= 2
+                        ? 2
+                        : 0
+            }))
+        });
+    }
+}
+
+Congregate.id = 'congregate';
+
+module.exports = Congregate;
---
+++ test/server/cards/13-CC/Congregate.spec.js
@@ -0,0 +1,32 @@
+describe('Congregate', function () {
+    describe("Congregate's play ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'sanctum',
+                    inPlay: ['troll', 'blypyp', 'krump'],
+                    hand: ['congregate']
+                },
+                player2: {
+                    inPlay: ['dust-pixie', 'borka-rikk']
+                }
+            });
+        });
+
+        it('should gain 2 amber when there are 2 or more friendly creatures with the chosen trait', function () {
+            this.player1.play(this.congregate);
+            this.player1.selectTrait('giant');
+
+            expect(this.player1.amber).toBe(2);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should not gain amber when there are less than 2 friendly creatures with the chosen trait', function () {
+            this.player1.play(this.congregate);
+            this.player1.selectTrait('martian');
+
+            expect(this.player1.amber).toBe(0);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
