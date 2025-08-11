b08c4aa5f237c3782072c5abc9de8df282285485
Jeremy Stribling <strib@alum.mit.edu>
2025-08-10 17:57:17 -0700

    Allow ordering of discards when there is a scrap effect
    Closes: #4574

--- test/server/cards/12-PV/Ifraneye.spec.js
+++ test/server/cards/12-PV/Ifraneye.spec.js
@@ -4,7 +4,8 @@ describe('Ifraneye', function () {
             this.setupTest({
                 player1: {
                     house: 'dis',
-                    hand: ['ifraneye', 'ember-imp', 'dust-pixie']
+                    hand: ['ifraneye', 'ember-imp', 'batdrone'],
+                    discard: ['unity-prism']
                 },
                 player2: {
                     hand: ['mighty-tiger', 'hunting-witch', 'troll', 'dust-pixie']
@@ -23,6 +24,18 @@ describe('Ifraneye', function () {
             this.ifraneye.exhausted = false;
             this.player1.reap(this.ifraneye);
             expect(this.player2.hand.length).toBe(2);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should allow ordering if there are scrap effects', function () {
+            this.player1.moveCard(this.unityPrism, 'hand');
+            this.player1.play(this.ifraneye);
+            expect(this.player1).toBeAbleToSelect(this.unityPrism);
+            expect(this.player1).toBeAbleToSelect(this.batdrone);
+            expect(this.player1).toBeAbleToSelect(this.emberImp);
+            this.player1.clickCard(this.unityPrism);
+            expect(this.player1.amber).toBe(2);
+            expect(this.player1.hand.length).toBe(0);
             expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
         });
     });
