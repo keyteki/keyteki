0c1178d3e1236a5e26197a05aef2ce46557fa34f
Jeremy Stribling <strib@alum.mit.edu>
2025-09-08 21:08:53 -0700

    Add test for Cursed Loot + Reclaimed by Natures (#4622)
    Fixes: #4595

--- test/server/cards/12-PV/CursedLoot.spec.js
+++ test/server/cards/12-PV/CursedLoot.spec.js
@@ -7,7 +7,8 @@ describe('Cursed Loot', function () {
                     hand: ['cursed-loot']
                 },
                 player2: {
-                    hand: ['krump', 'ancient-bear', 'ember-imp']
+                    hand: ['krump', 'ancient-bear', 'ember-imp'],
+                    discard: ['reclaimed-by-nature']
                 }
             });
         });
@@ -42,6 +43,18 @@ describe('Cursed Loot', function () {
             this.player2.play(this.emberImp);
             expect(this.player2.player.hand.length).toBe(0);
             expect(this.cursedLoot.location).toBe('discard');
+            expect(this.player2).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should let the player avoid a discard if purging the artifact', function () {
+            this.player1.play(this.cursedLoot);
+            this.player1.endTurn();
+            this.player2.clickPrompt('untamed');
+            this.player2.moveCard(this.reclaimedByNature, 'hand');
+            this.player2.play(this.reclaimedByNature);
+            this.player2.clickPrompt(this.reclaimedByNature.name);
+            this.player2.clickCard(this.cursedLoot);
+            expect(this.player2.player.hand.length).toBe(3);
             expect(this.player2).toHavePrompt('Choose a card to play, discard or use');
         });
     });
