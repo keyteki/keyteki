4719e802470385e7ddce6ad7ca6874af1a346051
Jeremy Stribling <strib@alum.mit.edu>
2025-09-08 20:41:19 -0700

    Fix BookOfLeq to handle house-pipped cards correctly
    Fixes: #4618
    Fixes: #4619

--- server/game/GameActions/ChangeActiveHouse.js
+++ server/game/GameActions/ChangeActiveHouse.js
@@ -1,6 +1,6 @@
 const GameAction = require('./GameAction');
 
-class AddEventToWindowAction extends GameAction {
+class ChangeActiveHouseAction extends GameAction {
     setDefaultProperties() {
         this.house = undefined;
         this.player = undefined;
@@ -8,13 +8,40 @@ class AddEventToWindowAction extends GameAction {
 
     setup() {
         super.setup();
-        this.effectMsg = 'change their active house to ' + this.house;
+        if (Array.isArray(this.house)) {
+            this.effectMsg = 'change their active house';
+        } else {
+            this.effectMsg = 'change their active house to ' + this.house;
+        }
     }
 
     hasLegalTarget(context) {
         this.update(context);
+        return !!this.house && (Array.isArray(this.house) ? this.house.length > 0 : true);
+    }
 
-        return !!this.house;
+    preEventHandler(context) {
+        super.preEventHandler(context);
+
+        // If house is an array with more than one element, prompt for selection
+        if (Array.isArray(this.house) && this.house.length > 1) {
+            let choices = this.house.map((house) => ({ text: house, icon: house }));
+            let handlers = this.house.map((house) => () => {
+                this.house = house;
+                this.effectMsg = 'change their active house to ' + house;
+            });
+
+            context.game.promptWithHandlerMenu(context.player, {
+                activePromptTitle: 'Choose a house',
+                context: context,
+                choices: choices,
+                handlers: handlers
+            });
+        } else if (Array.isArray(this.house) && this.house.length === 1) {
+            // If only one house in array, use it directly
+            this.house = this.house[0];
+            this.effectMsg = 'change their active house to ' + this.house;
+        }
     }
 
     getEventArray(context) {
@@ -30,4 +57,4 @@ class AddEventToWindowAction extends GameAction {
     }
 }
 
-module.exports = AddEventToWindowAction;
+module.exports = ChangeActiveHouseAction;
