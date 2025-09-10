bb507f73977531cd4d6a26ca916257a459ad9c37
David Mikalova <324548+dmikalova@users.noreply.github.com>
2025-09-09 23:30:36 -0700

    fix: update saury about that

--- server/game/cards/03-WC/SauryAboutThat.js
+++ server/game/cards/03-WC/SauryAboutThat.js
@@ -4,17 +4,20 @@ class SauryAboutThat extends Card {
     // Play: Destroy a creature. Its controller gains 1.
     setupCardAbilities(ability) {
         this.play({
-            effect: 'destroy {0} and have {1} gaining 1 amber',
-            effectArgs: (context) => [context.target.controller],
+            effect: 'destroy {0}',
             target: {
                 cardType: 'creature',
-                gameAction: [
+                gameAction: ability.actions.sequential([
                     ability.actions.destroy(),
                     ability.actions.gainAmber((context) => ({
                         target: context.target ? context.target.controller : []
                     }))
-                ]
-            }
+                ])
+            },
+            then: (preThenContext) => ({
+                message: '{3} gains 1 amber',
+                messageArgs: () => [preThenContext.target.controller]
+            })
         });
     }
 }
