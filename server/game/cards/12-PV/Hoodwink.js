6be23acae3571108ff826d493aebb10fabc1302d
David Mikalova <324548+dmikalova@users.noreply.github.com>
2025-09-29 10:21:17 -0700

    fix: Update Hoodwink message

--- server/game/cards/12-PV/Hoodwink.js
+++ server/game/cards/12-PV/Hoodwink.js
@@ -12,9 +12,17 @@ class Hoodwink extends Card {
                 gameAction: ability.actions.steal((context) => ({
                     amount: context.player.hand.filter((card) => !card.hasHouse('shadows')).length
                 })),
+                message: '{0} uses {1} to steal {3} amber',
+                messageArgs: (context) => [
+                    Math.min(
+                        context.player.opponent.amber,
+                        context.player.hand.filter((card) => !card.hasHouse('shadows')).length
+                    )
+                ],
                 then: {
                     alwaysTriggers: true,
-                    gameAction: ability.actions.purge()
+                    gameAction: ability.actions.purge(),
+                    message: '{0} uses {1} to purge {1}'
                 }
             }
         });
