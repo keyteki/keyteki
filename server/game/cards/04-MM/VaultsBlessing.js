90e2d0f5be52e2beaa833808e6b1aad16f4607fa
David Mikalova <324548+dmikalova@users.noreply.github.com>
2025-10-01 09:46:40 -0700

    fix: Vaults Blessing message

--- server/game/cards/04-MM/VaultsBlessing.js
+++ server/game/cards/04-MM/VaultsBlessing.js
@@ -4,7 +4,24 @@ class VaultsBlessing extends Card {
     // Play: Each player gains 1A for each Mutant creature they control.
     setupCardAbilities(ability) {
         this.play({
-            effect: 'give each player 1A for each mutant creature they control',
+            effect:
+                'have each player gain 1 amber for each mutant creature they control, {1} gains a total of {2} amber ({3}) and {4} gains a total of {5} amber ({6})',
+            effectArgs: (context) => [
+                context.player,
+                context.player.creaturesInPlay.filter((card) => card.hasTrait('mutant')).length,
+                context.player.creaturesInPlay.filter((card) => card.hasTrait('mutant')),
+                context.player.opponent,
+                context.player.opponent
+                    ? context.player.opponent.creaturesInPlay.filter((card) =>
+                          card.hasTrait('mutant')
+                      ).length
+                    : 0,
+                context.player.opponent
+                    ? context.player.opponent.creaturesInPlay.filter((card) =>
+                          card.hasTrait('mutant')
+                      )
+                    : []
+            ],
             gameAction: [
                 ability.actions.gainAmber((context) => ({
                     amount: context.player.creaturesInPlay.filter((card) => card.hasTrait('mutant'))
