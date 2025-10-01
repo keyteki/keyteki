ac26f193d3cf54c6615169d70eefff1cac124184
David Mikalova <324548+dmikalova@users.noreply.github.com>
2025-09-30 22:15:29 -0700

    fix: player.opponent guards

--- server/game/cards/07-GR/Circlespeak.js
+++ server/game/cards/07-GR/Circlespeak.js
@@ -17,7 +17,7 @@ class Circlespeak extends Card {
                         }))
                     })),
                     ability.actions.conditional((context) => ({
-                        condition: !!context.player.opponent && context.player.opponent.isHaunted(),
+                        condition: context.player.opponent && context.player.opponent.isHaunted(),
                         trueGameAction: ability.actions.capture((context) => ({
                             amount: 2,
                             player: context.player.opponent,
@@ -29,12 +29,12 @@ class Circlespeak extends Card {
             effect: 'capture {1} amber from {2} and {3} amber from {4} onto {0}',
             effectArgs: (context) => [
                 Math.min(context.player.isHaunted() ? 2 : 0, context.player.amber),
-                context.player.name,
+                context.player,
                 Math.min(
-                    !!context.player.opponent && context.player.opponent.isHaunted() ? 2 : 0,
+                    context.player.opponent && context.player.opponent.isHaunted() ? 2 : 0,
                     context.player.opponent ? context.player.opponent.amber : 0
                 ),
-                context.player.opponent.name
+                context.player.opponent
             ]
         });
     }
