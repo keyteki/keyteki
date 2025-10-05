ae6ed8afd2a51f743f3ca1d25cfb03b8cde37c63
David Mikalova <324548+dmikalova@users.noreply.github.com>
2025-10-05 10:19:46 -0700

    fix: Add tests for cards missing tests (#4647)
    * fix: Add Ammonia Clouds test and update message
    
    * fix: Anomaly Exploiter tests
    
    * fix: Bulleteye tests
    
    * fix: Brother in Battle tests
    
    * fix: Booby Trap tests
    
    * fix: Blood of Titans tests
    
    * fix: Banner of Battle tests
    
    * fix: Bilgum Avalanche tests
    
    * fix: Batdrone tests
    
    * fix: Barehanded test
    
    * fix: PR review
    
    ---------
    
    Co-authored-by: Jeremy Stribling <strib@alum.mit.edu>

---
+++ test/server/cards/01-Core/AmmoniaClouds.spec.js
@@ -0,0 +1,30 @@
+describe('Ammonia Clouds', function () {
+    describe("Ammonia Clouds's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'mars',
+                    hand: ['ammonia-clouds'],
+                    inPlay: ['troll']
+                },
+                player2: {
+                    inPlay: ['hunting-witch', 'mother']
+                }
+            });
+        });
+
+        it('should deal 3 damage to all creatures when played', function () {
+            this.mother.tokens.ward = 1;
+            this.player1.play(this.ammoniaClouds);
+            expect(this.troll.tokens.damage).toBe(3);
+            expect(this.mother.tokens.damage).toBe(undefined);
+        });
+
+        it('should destroy creatures with 3 or less power', function () {
+            this.player1.play(this.ammoniaClouds);
+            expect(this.troll.location).toBe('play area');
+            expect(this.huntingWitch.location).toBe('discard');
+            expect(this.mother.location).toBe('play area');
+        });
+    });
+});
