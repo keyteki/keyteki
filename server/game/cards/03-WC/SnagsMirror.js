3d156e16ce6043a2a0ced69c5f8e86b4f3ae74bf
David Mikalova <324548+dmikalova@users.noreply.github.com>
2025-10-11 23:54:21 -0700

    fix: iterating

--- server/game/cards/03-WC/SnagsMirror.js
+++ server/game/cards/03-WC/SnagsMirror.js
@@ -3,6 +3,7 @@ const Card = require('../../Card.js');
 class SnagsMirror extends Card {
     // After a player chooses an active house, their opponent cannot choose the same house as their active house on their next turn.
     setupCardAbilities(ability) {
+        // TODO: this only allows for the last house - doesn't work with multiple turns in a row
         this.houseSelected = {};
 
         this.persistentEffect({
@@ -15,7 +16,9 @@ class SnagsMirror extends Card {
                 onChooseActiveHouse: (event) => {
                     this.houseSelected[event.player.opponent.uuid] = event.house;
                 }
-            }
+            },
+            effect: 'none'
+            // TODO: this doesn't actually get printed, but without it the overall tests fail that there's no message, but switching to gameAction: doesn't work
         });
     }
 }
