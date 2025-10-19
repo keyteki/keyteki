7508c33b895fd292a585c2b3415edbeb2c6141e9
David Mikalova <324548+dmikalova@users.noreply.github.com>
2025-10-19 12:50:41 -0700

    test: Added 25 more missing CotA tests (#4658)
    Co-authored-by: Jeremy Stribling <strib@alum.mit.edu>

---
+++ test/server/cards/01-Core/DoorstepToHeaven.spec.js
@@ -0,0 +1,32 @@
+describe('Doorstep to Heaven', function () {
+    describe("Doorstep to Heaven's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    amber: 5,
+                    house: 'sanctum',
+                    hand: ['doorstep-to-heaven']
+                },
+                player2: {
+                    amber: 7
+                }
+            });
+        });
+
+        it('should make each player go down to 5 amber', function () {
+            this.player1.play(this.doorstepToHeaven);
+            expect(this.player1.amber).toBe(5);
+            expect(this.player2.amber).toBe(5);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+
+        it('should not make players with less than 6 amber lose any', function () {
+            this.player1.amber = 3;
+            this.player2.amber = 3;
+            this.player1.play(this.doorstepToHeaven);
+            expect(this.player1.amber).toBe(4);
+            expect(this.player2.amber).toBe(3);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
---
+++ test/server/cards/01-Core/Earthshaker.spec.js
@@ -0,0 +1,24 @@
+describe('Earthshaker', function () {
+    describe("Earthshaker's ability", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'brobnar',
+                    hand: ['earthshaker']
+                },
+                player2: {
+                    inPlay: ['urchin', 'umbra', 'krump', 'dodger']
+                }
+            });
+        });
+
+        it('should destroy each creature with power 3 or lower', function () {
+            this.player1.play(this.earthshaker);
+            expect(this.urchin.location).toBe('discard');
+            expect(this.umbra.location).toBe('discard');
+            expect(this.dodger.location).toBe('play area');
+            expect(this.krump.location).toBe('play area');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
