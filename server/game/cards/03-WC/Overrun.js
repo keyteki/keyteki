502dc5a4a87cee8636b1d6295351fc9878e4a44d
David <324548+dmikalova@users.noreply.github.com>
2025-10-19 12:29:05 -0700

    fix: overrun should consider controller, not owner

--- server/game/cards/03-WC/Overrun.js
+++ server/game/cards/03-WC/Overrun.js
@@ -4,35 +4,33 @@ const EventRegistrar = require('../../eventregistrar.js');
 class Overrun extends Card {
     // Play: If 3 or more enemy creatures have been destroyed this turn, your opponent loses 2A.
     setupCardAbilities(ability) {
-        this.creaturesDestroyed = {};
-        this.creaturesDestroyed[this.owner.uuid] = 0;
+        this.creaturesDestroyedThisTurn = {};
+        this.creaturesDestroyedThisTurn[this.owner.uuid] = 0;
         if (this.owner.opponent) {
-            this.creaturesDestroyed[this.owner.opponent.uuid] = 0;
+            this.creaturesDestroyedThisTurn[this.owner.opponent.uuid] = 0;
         }
 
         this.tracker = new EventRegistrar(this.game, this);
-        this.tracker.register(['onCardDestroyed', 'onPhaseStarted']);
+        this.tracker.register(['onCardDestroyed', 'onTurnStart']);
 
         this.play({
             condition: (context) =>
                 context.player.opponent &&
-                this.creaturesDestroyed[context.player.opponent.uuid] >= 3,
+                this.creaturesDestroyedThisTurn[context.player.opponent.uuid] >= 3,
             gameAction: ability.actions.loseAmber({ amount: 2 })
         });
     }
 
     onCardDestroyed(event) {
         if (event.card.type === 'creature') {
-            this.creaturesDestroyed[event.card.owner.uuid] += 1;
+            this.creaturesDestroyedThisTurn[event.card.controller.uuid] += 1;
         }
     }
 
-    onPhaseStarted(event) {
-        if (event.phase === 'main') {
-            this.creaturesDestroyed[this.game.activePlayer.uuid] = 0;
-            if (this.game.activePlayer.opponent) {
-                this.creaturesDestroyed[this.game.activePlayer.opponent.uuid] = 0;
-            }
+    onTurnStart() {
+        this.creaturesDestroyedThisTurn[this.game.activePlayer.uuid] = 0;
+        if (this.game.activePlayer.opponent) {
+            this.creaturesDestroyedThisTurn[this.game.activePlayer.opponent.uuid] = 0;
         }
     }
 }
