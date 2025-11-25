0858b5617cdabf1556a5de7e60892b2c38004622
DKeatonDC <165526945+DKeatonDC@users.noreply.github.com>
2025-11-25 17:30:54 -0500

    additional changes to yandylinx

--- server/game/cards/08-AS/Yandylinx.js
+++ server/game/cards/08-AS/Yandylinx.js
@@ -14,7 +14,7 @@ class Yandylinx extends Card {
                 gameAction: ability.actions.loseAmber((context) => ({
                     amount: 1,
                     target: context.preThenEvent.clone.controller.opponent,
-                    message: '{0} uses {1} to capture {3} amber from {4}',
+                    message: '{0} uses {1} to make {4} lose {3} amber',
                     messageArgs: (context) => [
                         context.player.opponent.amber >= 1 ? 1 : 0,
                         context.player.opponent
