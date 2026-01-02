5b988ed484a57c5e26b2471f6c60afddd7299513
David Mikalova <324548+dmikalova@users.noreply.github.com>
2026-01-01 21:42:36 -0800

    fix: Ihaka of the Depths reacts to draws 1 at a time

--- server/game/cards/07-GR/IhakaOfTheDepths.js
+++ server/game/cards/07-GR/IhakaOfTheDepths.js
@@ -4,13 +4,14 @@ class IhakaOfTheDepths extends Card {
     // After you draw a card during your turn, you may put the top
     // card of your discard on the bottom of your deck.
     setupCardAbilities(ability) {
+        this.persistentEffect({
+            effect: ability.effects.drawOneAtATimeDuringTurn()
+        });
+
         this.reaction({
             when: {
                 onCardPlaced: (event, context) =>
-                    event.drawn &&
-                    context.source.controller === event.card.controller &&
-                    context.source.controller === context.game.activePlayer &&
-                    context.source.controller.discard.length > 0
+                    event.drawn && context.source.controller === event.card.controller
             },
             optional: true,
             gameAction: ability.actions.returnToDeck((context) => ({
