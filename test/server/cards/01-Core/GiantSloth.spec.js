72cac13826ddce804c17001aa9878c0604750486
David Mikalova <324548+dmikalova@users.noreply.github.com>
2026-01-03 10:00:39 -0800

    feat: add more CotA tests - Fear to Horseman of Famine (#4705)
    * feat: add more CotA tests
    
    * fix: remove extraneous amber
    
    * fix: code review
    
    ---------
    
    Co-authored-by: Jeremy Stribling <strib@alum.mit.edu>

---
+++ test/server/cards/01-Core/GiantSloth.spec.js
@@ -0,0 +1,28 @@
+describe('Giant Sloth', function () {
+    describe("Giant Sloth's ability after discarding Untamed card", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'untamed',
+                    inPlay: ['giant-sloth'],
+                    hand: ['dust-pixie']
+                },
+                player2: {}
+            });
+        });
+
+        it('should be able to use after discarding an Untamed card', function () {
+            this.player1.clickCard(this.giantSloth);
+            expect(this.player1).not.toHavePromptButton('Reap with this creature');
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+            this.player1.clickCard(this.dustPixie);
+            this.player1.clickPrompt('Discard this card');
+            this.player1.clickCard(this.giantSloth);
+            expect(this.player1).toHavePromptButton('Reap with this creature');
+            expect(this.player1).not.toHavePrompt('Choose a card to play, discard or use');
+            this.player1.useAction(this.giantSloth);
+            expect(this.player1.amber).toBe(3);
+            expect(this.player1).toHavePrompt('Choose a card to play, discard or use');
+        });
+    });
+});
