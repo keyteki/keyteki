39bf90bb2ef1271021ddf37d940a878af90c7ee7
David Mikalova <324548+dmikalova@users.noreply.github.com>
2026-01-28 11:25:27 -0800

    test: Eng Walls and blaster ordering

--- test/server/cards/03-WC/ChiefEngineerWalls.spec.js
+++ test/server/cards/03-WC/ChiefEngineerWalls.spec.js
@@ -62,4 +62,101 @@ describe('Chief Engineer Walls', function () {
             expect(this.dustPixie.location).toBe('discard');
         });
     });
+
+    describe("when Walls' Blaster is attached and an upgrade is discarded by blaster damage", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'staralliance',
+                    hand: ['walls--blaster', 'access-denied'],
+                    inPlay: ['chief-engineer-walls']
+                },
+                player2: {
+                    inPlay: ['dust-pixie']
+                }
+            });
+        });
+
+        it('should allow ordering both abilities with no blaster in discard', function () {
+            this.player1.playUpgrade(this.accessDenied, this.dustPixie);
+            this.player1.playUpgrade(this.wallsBlaster, this.chiefEngineerWalls);
+            this.player1.clickCard(this.dustPixie);
+            this.player1.reap(this.chiefEngineerWalls);
+
+            // Choose to trigger "may" abilities
+            expect(this.player1).toHavePrompt('Any reactions?');
+            expect(this.player1).toBeAbleToSelect(this.chiefEngineerWalls);
+            expect(this.player1).toHavePromptButton('Done');
+            this.player1.clickCard(this.chiefEngineerWalls);
+
+            // Choose which "may" ability to resolve first
+            expect(this.player1).toHavePrompt('Which ability would you like to use?');
+            expect(this.player1).toHavePromptButton(this.wallsBlaster.name);
+            expect(this.player1).toHavePromptButton(this.chiefEngineerWalls.name);
+            expect(this.player1).toHavePromptButton('Back'); // Done
+            this.player1.clickPrompt(this.chiefEngineerWalls.name);
+
+            // Resolve Walls' ability - no upgrades in discard so fizzles
+
+            // Choose to trigger remaining "may" ability
+            expect(this.player1).toHavePrompt('Any reactions?');
+            expect(this.player1).toBeAbleToSelect(this.chiefEngineerWalls);
+            expect(this.player1).toHavePromptButton('Done');
+            this.player1.clickCard(this.chiefEngineerWalls);
+
+            // Resolve Walls Blaster ability
+            expect(this.player1).toHavePrompt('Select one');
+            expect(this.player1).toHavePromptButton('Deal 2 damage');
+            expect(this.player1).toHavePromptButton('Move Walls’ Blaster');
+            expect(this.player1).not.toHavePrompt('Done');
+            this.player1.clickPrompt('Deal 2 damage');
+            this.player1.clickCard(this.dustPixie);
+            expect(this.dustPixie.location).toBe('discard');
+            expect(this.accessDenied.location).toBe('discard');
+
+            expect(this.player1).isReadyToTakeAction();
+        });
+
+        it("should allow ordering both abilities to skip Walls' search", function () {
+            this.player1.playUpgrade(this.accessDenied, this.dustPixie);
+            this.player1.playUpgrade(this.wallsBlaster, this.chiefEngineerWalls);
+            this.player1.clickCard(this.dustPixie);
+            this.player1.reap(this.chiefEngineerWalls);
+
+            // Choose to trigger "may" abilities
+            expect(this.player1).toHavePrompt('Any reactions?');
+            expect(this.player1).toBeAbleToSelect(this.chiefEngineerWalls);
+            expect(this.player1).toHavePromptButton('Done');
+            this.player1.clickCard(this.chiefEngineerWalls);
+
+            // Choose which "may" ability to resolve first
+            expect(this.player1).toHavePrompt('Which ability would you like to use?');
+            expect(this.player1).toHavePromptButton(this.wallsBlaster.name);
+            expect(this.player1).toHavePromptButton(this.chiefEngineerWalls.name);
+            expect(this.player1).toHavePromptButton('Back'); // Done
+            this.player1.clickPrompt(this.wallsBlaster.name);
+
+            // Resolve Walls Blaster ability
+            expect(this.player1).toHavePrompt('Select one');
+            expect(this.player1).toHavePromptButton('Deal 2 damage');
+            expect(this.player1).toHavePromptButton('Move Walls’ Blaster');
+            expect(this.player1).not.toHavePrompt('Done');
+            this.player1.clickPrompt('Deal 2 damage');
+            this.player1.clickCard(this.dustPixie);
+            expect(this.dustPixie.location).toBe('discard');
+
+            // Choose to trigger remaining "may" ability
+            expect(this.player1).toHavePrompt('Any reactions?');
+            expect(this.player1).toBeAbleToSelect(this.chiefEngineerWalls);
+            expect(this.player1).toHavePromptButton('Done');
+            this.player1.clickCard(this.chiefEngineerWalls);
+
+            // Resolve Walls' ability
+            expect(this.player1).toBeAbleToSelect(this.accessDenied);
+            this.player1.clickCard(this.accessDenied);
+            expect(this.accessDenied.location).toBe('hand');
+
+            expect(this.player1).isReadyToTakeAction();
+        });
+    });
 });
