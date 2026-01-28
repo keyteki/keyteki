68e94b05e7d1025d23cb733bf13d7fcbe20a665f
David Mikalova <324548+dmikalova@users.noreply.github.com>
2026-01-27 23:44:17 -0800

    fix: First Scroll messaging

--- test/server/cards/05-DT/TheFirstScroll.spec.js
+++ test/server/cards/05-DT/TheFirstScroll.spec.js
@@ -39,7 +39,7 @@ describe('TheFirstScroll', function () {
                     this.player1.forgeKey('Red');
                 });
 
-                it('should capture 2 amber', function () {
+                it('should capture 3 amber', function () {
                     expect(this.player2.amber).toBe(1);
                     expect(this.snufflegator.amber).toBe(1);
                     expect(this.halacor.amber).toBe(1);
@@ -51,6 +51,81 @@ describe('TheFirstScroll', function () {
                     expect(this.badPenny.amber).toBe(1);
                 });
             });
+
+            describe('when player forged their creatures should capture', function () {
+                beforeEach(function () {
+                    this.player1.amber = 8;
+                    this.player2.clickPrompt('untamed');
+                    this.player2.endTurn();
+                    this.player1.forgeKey('Red');
+                });
+
+                it('choose which creatures capture', function () {
+                    expect(this.player1).toHavePrompt('Not enough amber, choose creatures');
+                    expect(this.player1).toBeAbleToSelect(this.helperBot);
+                    expect(this.player1).toBeAbleToSelect(this.titanMechanic);
+                    expect(this.player1).toBeAbleToSelect(this.badPenny);
+                    this.player1.clickCard(this.helperBot);
+                    this.player1.clickCard(this.titanMechanic);
+                    this.player1.clickPrompt('Done');
+                    this.player1.clickPrompt('logos');
+
+                    expect(this.player1.amber).toBe(0);
+                    expect(this.helperBot.amber).toBe(1);
+                    expect(this.titanMechanic.amber).toBe(1);
+                    expect(this.badPenny.amber).toBe(0);
+                    expect(this.player1).isReadyToTakeAction();
+                });
+            });
+        });
+
+        describe('when Keyfrog is destroyed and forges a key', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    player1: {
+                        house: 'saurian',
+                        inPlay: ['the-first-scroll', 'censor-philo'],
+                        amber: 2
+                    },
+                    player2: {
+                        inPlay: ['keyfrog', 'snufflegator'],
+                        amber: 7
+                    }
+                });
+            });
+
+            // it('should make opponent creatures capture from opponent pool, not active player pool', function () {
+            //     this.player1.fightWith(this.censorPhilo, this.keyfrog);
+            //     this.player1.clickPrompt('Red');
+            //     expect(this.player1).toHavePrompt('Not enough amber, choose creatures');
+            //     expect(this.player1).toBeAbleToSelect(this.keyfrog);
+            //     expect(this.player1).toBeAbleToSelect(this.snufflegator);
+            //     this.player1.clickCard(this.keyfrog);
+            //     this.player1.clickPrompt('Done');
+
+            //     expect(this.player1.amber).toBe(3);
+            //     expect(this.player2.amber).toBe(0);
+            //     expect(this.keyfrog.location).toBe('discard');
+            //     expect(this.snufflegator.amber).toBe(0);
+            //     expect(this.censorPhilo.amber).toBe(0);
+            //     expect(this.player1).isReadyToTakeAction();
+            // });
+
+            // it('should make opponent creatures capture from opponent pool, not active player pool', function () {
+            //     this.player1.fightWith(this.censorPhilo, this.keyfrog);
+            //     this.player1.clickPrompt('Red');
+            //     expect(this.player1).toHavePrompt(this.theFirstScroll.name);
+            //     expect(this.player1).toBeAbleToSelect(this.keyfrog);
+            //     expect(this.player1).toBeAbleToSelect(this.snufflegator);
+            //     this.player1.clickCard(this.snufflegator); // Capture and then destroyed
+
+            //     expect(this.player1.amber).toBe(2);
+            //     expect(this.player2.amber).toBe(0);
+            //     expect(this.keyfrog.location).toBe('discard');
+            //     expect(this.snufflegator.amber).toBe(1);
+            //     expect(this.censorPhilo.amber).toBe(0);
+            //     expect(this.player1).isReadyToTakeAction();
+            // });
         });
     });
 });
