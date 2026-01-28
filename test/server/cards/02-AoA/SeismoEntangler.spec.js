1d0929f4da3fc694257af7dc81ff848e46d7f394
David Mikalova <324548+dmikalova@users.noreply.github.com>
2026-01-28 00:16:43 -0800

    Add tests for Seismo-Entangler + Decadence interaction (#2931)
    Tests verify that when Seismo-Entangler blocks a house:
    - Creatures of a different house can still reap when used via Decadence
    - Creatures of the blocked house cannot reap when used via Decadence
    
    The bug appears to already be fixed in the current codebase.

--- test/server/cards/02-AoA/SeismoEntangler.spec.js
+++ test/server/cards/02-AoA/SeismoEntangler.spec.js
@@ -86,4 +86,52 @@ describe('Seismo-Entangler', function () {
             expect(this.player2).not.toHavePromptButton('Reap with this creature');
         });
     });
+
+    describe('when Decadence uses a creature of a different house', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'logos',
+                    inPlay: ['seismo-entangler', 'batdrone']
+                },
+                player2: {
+                    house: 'saurian',
+                    hand: ['decadence'],
+                    inPlay: ['dextre', 'senator-shrix']
+                }
+            });
+        });
+
+        it('should allow a non-saurian creature to reap when used via Decadence', function () {
+            this.player1.clickCard(this.seismoEntangler);
+            this.player1.clickPrompt("Use this card's Action ability");
+            this.player1.clickPrompt('saurian');
+            this.player1.endTurn();
+
+            this.player2.clickPrompt('saurian');
+            this.player2.play(this.decadence);
+            this.player2.clickPrompt('Exalt, ready and use');
+            this.player2.clickCard(this.dextre);
+            // Dextre is logos, not saurian, so should be able to reap
+            expect(this.player2).toHavePromptButton('Reap with this creature');
+            this.player2.clickPrompt('Reap with this creature');
+            expect(this.player2).isReadyToTakeAction();
+        });
+
+        it('should prevent a saurian creature from reaping when used via Decadence', function () {
+            this.player1.clickCard(this.seismoEntangler);
+            this.player1.clickPrompt("Use this card's Action ability");
+            this.player1.clickPrompt('saurian');
+            this.player1.endTurn();
+
+            this.player2.clickPrompt('saurian');
+            this.player2.play(this.decadence);
+            this.player2.clickPrompt('Exalt, ready and use');
+            this.player2.clickCard(this.senatorShrix);
+            // Senator Shrix is saurian, so should NOT be able to reap - auto-selects fight
+            expect(this.player2).toHavePrompt('Choose a creature to attack');
+            this.player2.clickCard(this.batdrone);
+            expect(this.player2).isReadyToTakeAction();
+        });
+    });
 });
