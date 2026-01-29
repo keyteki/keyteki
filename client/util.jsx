4f414680f6294ae29f78b67df5b518a639e4c016
Stuart Walsh <stu@ipng.org.uk>
2026-01-29 13:42:43 +0000

    Fixes

--- client/util.jsx
+++ client/util.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const urlMatchingRegex = new RegExp(
-    /(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?/,
-    'ig'
-);
+const urlSchemeRegex = /^(https?|ftp):\/\//i;
+
+const isSafeUrl = (value) => {
+    if (!urlSchemeRegex.test(value)) {
+        return false;
+    }
+
+    try {
+        const url = new URL(value);
+        return url.protocol === 'http:' || url.protocol === 'https:' || url.protocol === 'ftp:';
+    } catch (err) {
+        return false;
+    }
+};
 
 export function tryParseJSON(jsonString) {
     try {
@@ -28,7 +38,7 @@ export function getMessageWithLinks(message) {
 
     let i = 0;
     let parts = tokens.map((token) => {
-        if (token.match(urlMatchingRegex)) {
+        if (isSafeUrl(token)) {
             return (
                 <a key={`link-${i++}`} href={token} target='_blank' rel='noopener noreferrer'>
                     {token}
