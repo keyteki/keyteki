1d7227e0319c48465291182e90d39c25fe842b65
David Mikalova <324548+dmikalova@users.noreply.github.com>
2026-02-01 20:10:49 -0800

    fix: Fiendish Apprentice messaging (#4825)
    * fix: Fiendish Apprentice messaging
    
    * fix: code review
    
    * fix: Fiendish Apprentice messaging
    
    * fix: code review

--- test/server/cards/08-AS/FiendishApprentice.spec.js
+++ test/server/cards/08-AS/FiendishApprentice.spec.js
@@ -30,5 +30,34 @@ describe('Fiendish Apprentice', function () {
             expect(this.troll.damage).toBe(0);
             expect(this.player1).isReadyToTakeAction();
         });
+
+        it('should allow selecting the same creature multiple times', function () {
+            this.player1.play(this.fiendishApprentice);
+            this.player1.clickCard(this.rowdySkald);
+            this.player1.clickCard(this.rowdySkald);
+            expect(this.rowdySkald.location).toBe('discard');
+            expect(this.charette.damage).toBe(0);
+            expect(this.troll.damage).toBe(0);
+            expect(this.player1).isReadyToTakeAction();
+        });
+
+        it('should count house enhancements', function () {
+            this.dewFaerie.enhancements = ['dis'];
+            this.player1.play(this.fiendishApprentice);
+            expect(this.player1).toBeAbleToSelect(this.rowdySkald);
+            expect(this.player1).toBeAbleToSelect(this.troll);
+            expect(this.player1).toBeAbleToSelect(this.charette);
+            expect(this.player1).not.toBeAbleToSelect(this.emberImp);
+            expect(this.player1).not.toBeAbleToSelect(this.dewFaerie);
+            expect(this.player1).not.toBeAbleToSelect(this.dustPixie);
+            expect(this.player1).not.toBeAbleToSelect(this.shooler);
+            this.player1.clickCard(this.rowdySkald);
+            this.player1.clickCard(this.charette);
+            this.player1.clickCard(this.troll);
+            expect(this.rowdySkald.damage).toBe(3);
+            expect(this.charette.damage).toBe(3);
+            expect(this.troll.damage).toBe(3);
+            expect(this.player1).isReadyToTakeAction();
+        });
     });
 });
