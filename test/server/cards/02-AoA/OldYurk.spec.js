8e26b9a6995c0d0c0dd4e3f14f2f30bbb41ab5c8
David Mikalova <324548+dmikalova@users.noreply.github.com>
2026-02-02 10:34:42 -0800

    fix: code review

--- test/server/cards/02-AoA/OldYurk.spec.js
+++ test/server/cards/02-AoA/OldYurk.spec.js
@@ -1,10 +1,10 @@
-describe('Ancient Yurk', function () {
-    describe("Ancient Yurk's ability", function () {
+describe('Old Yurk', function () {
+    describe("Old Yurk's ability", function () {
         beforeEach(function () {
             this.setupTest({
                 player1: {
                     house: 'dis',
-                    hand: ['ancient-yurk', 'dust-pixie', 'flaxia', 'niffle-ape']
+                    hand: ['old-yurk', 'dust-pixie', 'flaxia', 'niffle-ape']
                 },
                 player2: {
                     hand: ['batdrone']
@@ -12,8 +12,8 @@ describe('Ancient Yurk', function () {
             });
         });
 
-        it('should discard 3 cards from hand on play', function () {
-            this.player1.play(this.ancientYurk);
+        it('should discard 2 cards from hand on play', function () {
+            this.player1.play(this.oldYurk);
 
             expect(this.player1).toBeAbleToSelect(this.dustPixie);
             expect(this.player1).toBeAbleToSelect(this.flaxia);
@@ -22,12 +22,11 @@ describe('Ancient Yurk', function () {
 
             this.player1.clickCard(this.dustPixie);
             this.player1.clickCard(this.flaxia);
-            this.player1.clickCard(this.niffleApe);
             this.player1.clickPrompt('Done');
 
             expect(this.dustPixie.location).toBe('discard');
             expect(this.flaxia.location).toBe('discard');
-            expect(this.niffleApe.location).toBe('discard');
+            expect(this.niffleApe.location).toBe('hand');
             expect(this.batdrone.location).toBe('hand');
             expect(this.player1).isReadyToTakeAction();
         });
--- test/server/cards/02-AoA/Yurk.spec.js
+++ test/server/cards/02-AoA/Yurk.spec.js
@@ -1,10 +1,10 @@
-describe('Ancient Yurk', function () {
-    describe("Ancient Yurk's ability", function () {
+describe('Yurk', function () {
+    describe("Yurk's ability", function () {
         beforeEach(function () {
             this.setupTest({
                 player1: {
                     house: 'dis',
-                    hand: ['ancient-yurk', 'dust-pixie', 'flaxia', 'niffle-ape']
+                    hand: ['yurk', 'dust-pixie', 'flaxia']
                 },
                 player2: {
                     hand: ['batdrone']
@@ -12,22 +12,17 @@ describe('Ancient Yurk', function () {
             });
         });
 
-        it('should discard 3 cards from hand on play', function () {
-            this.player1.play(this.ancientYurk);
+        it('should discard 1 card from hand on play', function () {
+            this.player1.play(this.yurk);
 
             expect(this.player1).toBeAbleToSelect(this.dustPixie);
             expect(this.player1).toBeAbleToSelect(this.flaxia);
-            expect(this.player1).toBeAbleToSelect(this.niffleApe);
             expect(this.player1).not.toBeAbleToSelect(this.batdrone);
 
             this.player1.clickCard(this.dustPixie);
-            this.player1.clickCard(this.flaxia);
-            this.player1.clickCard(this.niffleApe);
-            this.player1.clickPrompt('Done');
 
             expect(this.dustPixie.location).toBe('discard');
-            expect(this.flaxia.location).toBe('discard');
-            expect(this.niffleApe.location).toBe('discard');
+            expect(this.flaxia.location).toBe('hand');
             expect(this.batdrone.location).toBe('hand');
             expect(this.player1).isReadyToTakeAction();
         });
