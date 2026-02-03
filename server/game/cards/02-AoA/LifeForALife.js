228b7fb8a6497fa4c4c1a0362873ee2fd58c579a
David Mikalova <324548+dmikalova@users.noreply.github.com>
2026-02-02 23:28:53 -0800

    fix: code review

--- server/game/cards/02-AoA/LifeForALife.js
+++ server/game/cards/02-AoA/LifeForALife.js
@@ -1,16 +1,17 @@
 const Card = require('../../Card.js');
 
 class LifeForALife extends Card {
-    // Play: Sacrifice a creature to deal 6D to a creature.
+    // Play: Destroy a friendly creature. If you do, deal 6D to a creature.
     setupCardAbilities(ability) {
         this.play({
             target: {
-                activePromptTitle: 'Choose a creature to sacrifice',
+                activePromptTitle: 'Choose a friendly creature to destroy',
                 cardType: 'creature',
                 controller: 'self',
-                gameAction: ability.actions.sacrifice()
+                gameAction: ability.actions.destroy()
             },
             then: {
+                message: '{0} uses {1} to deal 6 damage to {2}',
                 target: {
                     cardType: 'creature',
                     gameAction: ability.actions.dealDamage({ amount: 6 })
