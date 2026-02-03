4b81236e20072497816be861f8543d49004eddda
David Mikalova <324548+dmikalova@users.noreply.github.com>
2026-02-02 16:42:16 -0800

    fix: iterating

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,33 +21,5 @@ module.exports = {
         react: {
             version: 'detect'
         }
-    },
-    overrides: [
-        {
-            files: ['**/*.spec.js'],
-            globals: {
-                describe: 'readonly',
-                it: 'readonly',
-                expect: 'readonly',
-                beforeEach: 'readonly',
-                afterEach: 'readonly',
-                beforeAll: 'readonly',
-                afterAll: 'readonly',
-                vi: 'readonly'
-            },
-            rules: {
-                'no-restricted-syntax': [
-                    'error',
-                    {
-                        selector: 'MemberExpression[object.name="describe"][property.name="only"]',
-                        message: 'Remove focused test (describe.only)'
-                    },
-                    {
-                        selector: 'MemberExpression[object.name="it"][property.name="only"]',
-                        message: 'Remove focused test (it.only)'
-                    }
-                ]
-            }
-        }
-    ]
+    }
 };
