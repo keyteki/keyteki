fbbde141ca0c310bef49a548317c01486382294a
David Mikalova <324548+dmikalova@users.noreply.github.com>
2026-02-04 10:10:15 -0800

    fix: code review

--- test/helpers/playerinteractionwrapper.js
+++ test/helpers/playerinteractionwrapper.js
@@ -725,8 +725,7 @@ class PlayerInteractionWrapper {
         // Skip expensive serialization check unless in CI
         // This check takes ~15% of test time
         if (process.env.CI || process.env.DEBUG_TEST) {
-            throw new Error('Skipping unserializable game state check outside of CI');
-            // return;
+            return;
         }
 
         let state = this.game.getState(this.player.name);
