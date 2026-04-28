917460ff9b564889a9b66c819fa1ed7d67cc737d
David Mikalova <324548+dmikalova@users.noreply.github.com>
2026-04-28 14:01:29 -0700

    feat: implement entrench and grammy taps

---
+++ test/server/cards/14-DM/GrammyTaps.spec.js
@@ -0,0 +1,76 @@
+describe('Grammy Taps', function () {
+    describe("Grammy Taps's neighbors", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'shadows',
+                    inPlay: ['troll', 'grammy-taps', 'krump', 'alaka']
+                },
+                player2: {
+                    inPlay: ['bumpsy']
+                }
+            });
+        });
+
+        it('gain elusive while Grammy Taps is exhausted', function () {
+            this.player1.reap(this.grammyTaps);
+            expect(this.grammyTaps.exhausted).toBe(true);
+            expect(this.troll.hasKeyword('elusive')).toBe(true);
+            expect(this.krump.hasKeyword('elusive')).toBe(true);
+        });
+
+        it('do not gain elusive while Grammy Taps is ready', function () {
+            expect(this.grammyTaps.exhausted).toBe(false);
+            expect(this.troll.hasKeyword('elusive')).toBe(false);
+            expect(this.krump.hasKeyword('elusive')).toBe(false);
+        });
+
+        it('lose elusive after Grammy Taps readies at the end of turn', function () {
+            this.player1.reap(this.grammyTaps);
+            expect(this.troll.hasKeyword('elusive')).toBe(true);
+            expect(this.krump.hasKeyword('elusive')).toBe(true);
+            this.player1.endTurn();
+            this.player1.clickCard(this.grammyTaps);
+            this.player1.clickPrompt('done');
+            expect(this.grammyTaps.exhausted).toBe(false);
+            expect(this.troll.hasKeyword('elusive')).toBe(false);
+            expect(this.krump.hasKeyword('elusive')).toBe(false);
+        });
+
+        it('does not affect non-neighbors', function () {
+            this.player1.reap(this.grammyTaps);
+            expect(this.alaka.hasKeyword('elusive')).toBe(false);
+            expect(this.bumpsy.hasKeyword('elusive')).toBe(false);
+        });
+    });
+
+    describe('Entrench', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'shadows',
+                    inPlay: ['grammy-taps']
+                },
+                player2: {
+                    inPlay: ['bumpsy']
+                }
+            });
+
+            this.grammyTaps.exhaust();
+            this.player1.endTurn();
+        });
+
+        it('lets the controller leave Grammy Taps exhausted during their ready phase', function () {
+            expect(this.player1).toHavePrompt('Select entrenched creatures to ready');
+            this.player1.clickPrompt('done');
+            expect(this.grammyTaps.exhausted).toBe(true);
+        });
+
+        it('readies Grammy Taps when selected', function () {
+            expect(this.player1).toHavePrompt('Select entrenched creatures to ready');
+            this.player1.clickCard(this.grammyTaps);
+            this.player1.clickPrompt('done');
+            expect(this.grammyTaps.exhausted).toBe(false);
+        });
+    });
+});
