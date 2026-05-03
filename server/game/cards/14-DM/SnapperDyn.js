e7154c3a8d9a0b96d27dba21a801cf60e1c8305b
David Mikalova <324548+dmikalova@users.noreply.github.com>
2026-05-02 21:25:30 -0700

    fix: DM card comments

--- server/game/cards/14-DM/SnapperDyn.js
+++ server/game/cards/14-DM/SnapperDyn.js
@@ -13,24 +13,7 @@ class SnapperDyn extends Card {
                 controller: 'opponent',
                 numSteps: context.player.opponent ? context.player.opponent.amber : 0
             })),
-            effect: "deal 1 damage to an enemy creature for each amber in their opponent's pool",
-            then: {
-                alwaysTriggers: true,
-                condition: (context) => {
-                    context.preThenEvents
-                        .filter((event) => !event.cancelled && event.amount > 0)
-                        .forEach((event) => {
-                            context.game.addMessage(
-                                '{0} uses {1} to deal {2} damage to {3}',
-                                context.player,
-                                context.source,
-                                event.amount,
-                                event.card
-                            );
-                        });
-                    return false;
-                }
-            }
+            effect: "deal 1 damage to an enemy creature for each amber in their opponent's pool"
         });
     }
 }
