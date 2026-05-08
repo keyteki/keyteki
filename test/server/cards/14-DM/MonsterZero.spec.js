0199cf19eb9df3e0bbe1685410f6a9a0edfd0eed
Jeremy Stribling <strib@alum.mit.edu>
2026-05-07 20:12:53 -0700

    DM: fix deploy keyword on Monster Zero's bottom half
    Closes: #5006

--- test/server/cards/14-DM/MonsterZero.spec.js
+++ test/server/cards/14-DM/MonsterZero.spec.js
@@ -1,4 +1,31 @@
 describe('Monster Zero', function () {
+    describe("Monster Zero's deploy keyword", function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    house: 'mars',
+                    hand: ['monster-zero', 'monster-zero2'],
+                    inPlay: ['troll', 'urchin']
+                },
+                player2: {}
+            });
+        });
+
+        it('can deploy when played by clicking the bottom half', function () {
+            this.player1.clickCard(this.monsterZero);
+            this.player1.clickPrompt('Play this creature');
+            expect(this.player1).toHavePromptButton('Deploy Left');
+            expect(this.player1).toHavePromptButton('Deploy Right');
+        });
+
+        it('can deploy when played by clicking the top half', function () {
+            this.player1.clickCard(this.monsterZero2);
+            this.player1.clickPrompt('Play this creature');
+            expect(this.player1).toHavePromptButton('Deploy Left');
+            expect(this.player1).toHavePromptButton('Deploy Right');
+        });
+    });
+
     describe("Monster Zero's persistent effect", function () {
         beforeEach(function () {
             this.setupTest({
