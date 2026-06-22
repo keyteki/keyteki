81f81b8b31c710af0148c13257856c1c84d5aa5a
Alex <akomar08@gmail.com>
2026-06-22 01:16:40 -0500

    Added .isReadyToTakeAction() checks.

--- test/server/cards/06-WoE/Blorb.spec.js
+++ test/server/cards/06-WoE/Blorb.spec.js
@@ -20,6 +20,8 @@ describe('Blorb', function () {
         it('should not be able to reap', function () {
             this.player1.clickCard(this.blorb);
             expect(this.player1).not.toHavePrompt('Reap with this Creature');
+            this.player1.clickPrompt('Cancel');
+            expect(this.player1).isReadyToTakeAction();
         });
 
         it('should evaluate the destroyed ability without a selected target', function () {
@@ -33,6 +35,7 @@ describe('Blorb', function () {
                 gameActions = destroyedAbility.getGameActions(context);
             }).not.toThrow();
             expect(gameActions).toEqual([]);
+            expect(this.player1).isReadyToTakeAction();
         });
 
         it('bring back Blorb Hive when destroyed', function () {
@@ -40,6 +43,7 @@ describe('Blorb', function () {
             this.player1.clickCard(this.blorbHive);
             expect(this.blorbHive.location).toBe('hand');
             expect(this.toad.location).toBe('discard');
+            expect(this.player1).isReadyToTakeAction();
         });
     });
 
@@ -58,14 +62,13 @@ describe('Blorb', function () {
                     discard: ['blorb-hive']
                 }
             });
-
-            this.toad = this.blorb;
         });
 
         it('should not return blorb-hive from the opponents discard pile', function () {
             this.player1.fightWith(this.blorb, this.troll);
             this.player1.clickCard(this.blorbHive);
             expect(this.blorbHive.location).toBe('discard');
+            expect(this.player1).isReadyToTakeAction();
         });
     });
 });
